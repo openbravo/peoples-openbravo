1593e68fe495dfb3513024460f09f76a040256be
Carlos Romero <carlos.romero@openbravo.com>
2008-02-22 13:06:50 +0000

    Fixed bug [ 1881948 ] System client removed from Delete Client selector
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%402502

--- src/org/openbravo/erpCommon/ad_process/DeleteClient.java
+++ src/org/openbravo/erpCommon/ad_process/DeleteClient.java
@@ -143,7 +143,7 @@ public class DeleteClient extends HttpSecureAppServlet {
         xmlDocument.setParameter("messageMessage", myMessage.getMessage());
       }
     }
-    xmlDocument.setData("reportClientId","liststructure",ClientComboData.selectAllClients1(this));
+    xmlDocument.setData("reportClientId","liststructure",ClientComboData.selectAllClientsNoSystem1(this));
 
     response.setContentType("text/html; charset=UTF-8");
     PrintWriter out = response.getWriter();
