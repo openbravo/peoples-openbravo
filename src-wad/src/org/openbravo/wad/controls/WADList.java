58c363bdd78a7ae2a3123c3661f67cd7c13cf33b
Asier Lostalé <asier.lostale@openbravo.com>
2008-05-08 11:15:42 +0000

    bug fixed: [ 0000180 ] When combos were filled up for first time they didn't trigger their callouts. This error was intruduced by the code cleanup.
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%404059

--- src-wad/src/org/openbravo/wad/controls/WADList.java
+++ src-wad/src/org/openbravo/wad/controls/WADList.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SL 
- * All portions are Copyright (C) 2001-2006 Openbravo SL 
+ * All portions are Copyright (C) 2001-2008 Openbravo SL 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -50,6 +50,7 @@ public class WADList extends WADControl {
       text.append("  }");
       setValidation(replaceHTML(text.toString()));
     }
+    setOnLoad("if (inputValue(key)==\"0\") updateOnChange(frm.inp" + getData("ColumnNameInp") + ");");
     setCalloutJS();
   }
 
