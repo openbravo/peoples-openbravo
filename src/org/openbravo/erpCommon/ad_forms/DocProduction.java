be2c574947a920b47ebbe523726ee1f42f0ee59b
Eduardo Argal <eduardo.argal@openbravo.com>
2008-08-01 15:28:45 +0000

    Merge to undo commit 5938
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%406123

--- src/org/openbravo/erpCommon/ad_forms/DocProduction.java
+++ src/org/openbravo/erpCommon/ad_forms/DocProduction.java
@@ -1,20 +1,18 @@
 /*
-*************************************************************************
-* The contents of this file are subject to the Openbravo  Public  License
-* Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
-* Version 1.1  with a permitted attribution clause; you may not  use this
-* file except in compliance with the License. You  may  obtain  a copy of
-* the License at http://www.openbravo.com/legal/license.html
-* Software distributed under the License  is  distributed  on  an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-* License for the specific  language  governing  rights  and  limitations
-* under the License.
-* The Original Code is Openbravo ERP.
-* The Initial Developer of the Original Code is Openbravo SL
-* All portions are Copyright (C) 2008 Openbravo SL
-* All Rights Reserved.
-* Contributor(s):  ______________________________________.
-************************************************************************
+ ******************************************************************************
+ * The contents of this file are subject to the   Compiere License  Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * You may obtain a copy of the License at http://www.compiere.org/license.html
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is                  Compiere  ERP & CRM  Business Solution
+ * The Initial Developer of the Original Code is Jorg Janke  and ComPiere, Inc.
+ * Portions created by Jorg Janke are Copyright (C) 1999-2001 Jorg Janke, parts
+ * created by ComPiere are Copyright (C) ComPiere, Inc.;   All Rights Reserved.
+ * Contributor(s): Openbravo SL
+ * Contributions are Copyright (C) 2001-2006 Openbravo S.L.
+ ******************************************************************************
 */
 package org.openbravo.erpCommon.ad_forms;
 
@@ -69,7 +67,7 @@ public boolean loadDocumentDetails (FieldProvider [] data,ConnectionProvider con
 }   //  loadDocumentDetails
 
 /**
- *  Load Lines.
+ *  Load AmortizationLine Line.
  *  @return DocLine Array
  */
 private DocLine[] loadLines(ConnectionProvider conn){
@@ -129,6 +127,7 @@ public Fact createFact (AcctSchema as,ConnectionProvider conn,Connection con,Var
         String costs = line.getProductCosts(DateAcct,as, conn, con);
         BigDecimal dCosts = new BigDecimal(costs);
         costs = dCosts.toString();
+//        ProductInfo product = new ProductInfo(line.m_M_Product_ID,conn);
         log4j.debug("DocProduction - createFact - line.m_Productiontype - " + line.m_Productiontype);
       if (line.m_Productiontype.equals("+")){
         fact.createLine(line,line.getAccount(ProductInfo.ACCTTYPE_P_Asset, as, conn),as.getC_Currency_ID(), costs, "",Fact_Acct_Group_ID, nextSeqNo(SeqNo), DocumentType, conn);
@@ -150,13 +149,9 @@ public Fact createFact (AcctSchema as,ConnectionProvider conn,Connection con,Var
       return SeqNo;
     }
 
-    /**
-     *  Get the account for Accounting Schema
-     *  @param as warehouse
-     *  @param as accounting schema
-     *  @return Account
-     */
     public final Account getAccountWarehouse (String M_Warehouse_ID, AcctSchema as,ConnectionProvider conn) throws ServletException{
+      BigDecimal AMT = null;
+      BigDecimal ZERO = new BigDecimal("0");
       AcctServerData [] data=null;
       data = AcctServerData.selectWDifferencesAcct(conn, M_Warehouse_ID, as.getC_AcctSchema_ID());
       //  Get Acct
