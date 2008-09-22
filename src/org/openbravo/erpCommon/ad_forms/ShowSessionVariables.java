05e0040a1b98e8c34d3f4b96d7774752ba7dc699
Carlos Romero <carlos.romero@openbravo.com>
2008-09-22 11:27:34 +0000

    Fixed bug 5125: *invalid number* error message when accessing Session Variables window. Now with UUID identifiers are not more numeric, so we have also removed unneeded Integer validation
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%407635

--- src/org/openbravo/erpCommon/ad_forms/ShowSessionVariables.java
+++ src/org/openbravo/erpCommon/ad_forms/ShowSessionVariables.java
@@ -328,7 +328,6 @@ public class ShowSessionVariables extends HttpSecureAppServlet {
       for (int i=0;i<windows.size();i++) {
         String aux = (String)windows.elementAt(i);
         try {
-          Integer.valueOf(aux).intValue(); // To catch illegal number conversion
           if (i>0) strWindows.append(", ");
           strWindows.append("'").append(aux).append("'");
         } catch (Exception e) {
