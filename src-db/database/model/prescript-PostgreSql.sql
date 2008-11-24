7c7db6835fa265257ac0a886d06dfaf363b5043b
Asier Lostalé <asier.lostale@openbravo.com>
2008-11-24 17:05:44 +0000

    Added PL function for soft trigger disabling
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4010487

--- src-db/database/model/prescript-PostgreSql.sql
+++ src-db/database/model/prescript-PostgreSql.sql
@@ -1367,3 +1367,64 @@ BEGIN
 END ; $BODY$
   LANGUAGE 'plpgsql' VOLATILE;
 /-- END
+
+
+CREATE OR REPLACE FUNCTION AD_disableTriggers()
+  RETURNS void AS
+$BODY$ DECLARE 
+/*************************************************************************
+* The contents of this file are subject to the Openbravo  Public  License
+* Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+* Version 1.1  with a permitted attribution clause; you may not  use this
+* file except in compliance with the License. You  may  obtain  a copy of
+* the License at http://www.openbravo.com/legal/license.html
+* Software distributed under the License  is  distributed  on  an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific  language  governing  rights  and  limitations
+* under the License.
+* The Original Code is Openbravo ERP.
+* The Initial Developer of the Original Code is Openbravo SL
+* All portions are Copyright (C) 2008 Openbravo SL
+* All Rights Reserved.
+* Contributor(s):  ______________________________________.
+************************************************************************/
+  v_session numeric; 
+BEGIN
+  v_session = pg_backend_pid();
+
+  insert into ad_enable_triggers
+    values (v_session,'N');
+  
+END ; $BODY$
+  LANGUAGE 'plpgsql' VOLATILE
+/-- END
+
+CREATE OR REPLACE FUNCTION ad_EnableTriggers()
+  RETURNS void AS
+$BODY$ DECLARE 
+/*************************************************************************
+* The contents of this file are subject to the Openbravo  Public  License
+* Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+* Version 1.1  with a permitted attribution clause; you may not  use this
+* file except in compliance with the License. You  may  obtain  a copy of
+* the License at http://www.openbravo.com/legal/license.html
+* Software distributed under the License  is  distributed  on  an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific  language  governing  rights  and  limitations
+* under the License.
+* The Original Code is Openbravo ERP.
+* The Initial Developer of the Original Code is Openbravo SL
+* All portions are Copyright (C) 2008 Openbravo SL
+* All Rights Reserved.
+* Contributor(s):  ______________________________________.
+************************************************************************/
+  v_session numeric; 
+BEGIN
+  v_session = pg_backend_pid();
+
+  delete from ad_enable_triggers
+    where session_id = v_session;
+  
+END ; $BODY$
+  LANGUAGE 'plpgsql' VOLATILE
+/-- END
