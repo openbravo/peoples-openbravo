feb0a3c49cca1d1543f88bcb4b395a2427745800
Martin Taal <martin.taal@openbravo.com>
2008-12-09 08:01:10 +0000

    Added License and reformatted
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4011034

--- src/org/openbravo/erpCommon/modules/ApplyModuleTask.java
+++ src/org/openbravo/erpCommon/modules/ApplyModuleTask.java
@@ -1,3 +1,22 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html 
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License. 
+ * The Original Code is Openbravo ERP. 
+ * The Initial Developer of the Original Code is Openbravo SL 
+ * All portions are Copyright (C) 2008 Openbravo SL 
+ * All Rights Reserved. 
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
+ */
+
 package org.openbravo.erpCommon.modules;
 
 import org.apache.tools.ant.BuildException;
@@ -6,39 +25,42 @@ import org.openbravo.database.CPStandAlone;
 
 /**
  * Ant task for ApplyModule class
- *
+ * 
  */
-public class ApplyModuleTask extends DalInitializingTask{
- // private String propertiesFile;
-  private String obDir;
-  
-@Override
-public void doExecute() {
-    try {
-      if (obDir==null||obDir.equals("")) obDir= getProject().getBaseDir().toString();
-      if (propertiesFile == null||propertiesFile.equals("")) propertiesFile=obDir+"/config/Openbravo.properties";
-      final ApplyModule am = new ApplyModule(new CPStandAlone (propertiesFile), obDir);
-      am.execute();
-    } catch (final Exception e) {
-      throw new BuildException(e);
+public class ApplyModuleTask extends DalInitializingTask {
+    // private String propertiesFile;
+    private String obDir;
+
+    @Override
+    public void doExecute() {
+        try {
+            if (obDir == null || obDir.equals(""))
+                obDir = getProject().getBaseDir().toString();
+            if (propertiesFile == null || propertiesFile.equals(""))
+                propertiesFile = obDir + "/config/Openbravo.properties";
+            final ApplyModule am = new ApplyModule(new CPStandAlone(
+                    propertiesFile), obDir);
+            am.execute();
+        } catch (final Exception e) {
+            throw new BuildException(e);
+        }
     }
-  }
-  
-/*public void setPropertiesFile(String propertiesFile) {
-    this.propertiesFile = propertiesFile;
-  }*/
-  public void setObDir(String obDir) {
-    this.obDir = obDir;
-  }
-  
-  public static void main(String[] args) {
-    final ApplyModuleTask t = new ApplyModuleTask();
-    t.setObDir("/ws/trunk/openbravo");
-    t.setUserId("0");
-    t.setPropertiesFile("/ws/trunk/openbravo/config/Openbravo.properties");
-    t.execute();
-    
-}
-  
-  
+
+    /*
+     * public void setPropertiesFile(String propertiesFile) {
+     * this.propertiesFile = propertiesFile; }
+     */
+    public void setObDir(String obDir) {
+        this.obDir = obDir;
+    }
+
+    public static void main(String[] args) {
+        final ApplyModuleTask t = new ApplyModuleTask();
+        t.setObDir("/ws/trunk/openbravo");
+        t.setUserId("0");
+        t.setPropertiesFile("/ws/trunk/openbravo/config/Openbravo.properties");
+        t.execute();
+
+    }
+
 }
