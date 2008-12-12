398b67ab41892cd249b8ee008ada44c3b9fb96a0
Asier Lostalé <asier.lostale@openbravo.com>
2008-12-12 08:22:16 +0000

    0005307: Roles that have been given permissions manually with the button "grant access" cannot be edited
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4011137

--- src/org/openbravo/erpCommon/ad_actionButton/InsertAcces.java
+++ src/org/openbravo/erpCommon/ad_actionButton/InsertAcces.java
@@ -18,50 +18,56 @@
 */
 package org.openbravo.erpCommon.ad_actionButton;
 
-import org.openbravo.erpCommon.utility.Utility;
-import org.openbravo.erpCommon.utility.OBError;
-import org.openbravo.utils.FormatUtilities;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.openbravo.base.secureApp.HttpSecureAppServlet;
 import org.openbravo.base.secureApp.VariablesSecureApp;
-import org.openbravo.xmlEngine.XmlDocument;
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-
 import org.openbravo.erpCommon.ad_combos.ModuleComboData;
+import org.openbravo.erpCommon.utility.OBError;
+import org.openbravo.erpCommon.utility.Utility;
+import org.openbravo.utils.FormatUtilities;
+import org.openbravo.xmlEngine.XmlDocument;
 
 public class InsertAcces extends HttpSecureAppServlet {
   private static final long serialVersionUID = 1L;
 
-  public void init (ServletConfig config) {
+  @Override
+public void init (ServletConfig config) {
     super.init(config);
     boolHist = false;
   }
 
-  public void doPost (HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException {
-    VariablesSecureApp vars = new VariablesSecureApp(request);
+  @Override
+public void doPost (HttpServletRequest request, HttpServletResponse response) throws IOException,ServletException {
+    final VariablesSecureApp vars = new VariablesSecureApp(request);
     
     if (vars.commandIn("DEFAULT")) {
-      String strProcessId = vars.getStringParameter("inpProcessId");
-      String strWindow = vars.getStringParameter("inpwindowId");
-      String strTab = vars.getStringParameter("inpTabId");
-      String strKey = vars.getGlobalVariable("inpadRoleId", strWindow + "|AD_Role_ID");
-      String strMessage="";
+      final String strProcessId = vars.getStringParameter("inpProcessId");
+      final String strWindow = vars.getStringParameter("inpwindowId");
+      final String strTab = vars.getStringParameter("inpTabId");
+      final String strKey = vars.getGlobalVariable("inpadRoleId", strWindow + "|AD_Role_ID");
+      final String strMessage="";
       printPage(response, vars, strKey, strWindow, strProcessId, strMessage, strTab);
     } else if(vars.commandIn("GENERATE")){
-      String strKey = vars.getStringParameter("inpadRoleId");
-      String strWindow = vars.getStringParameter("inpwindowId");
+      final String strKey = vars.getStringParameter("inpadRoleId");
+      final String strWindow = vars.getStringParameter("inpwindowId");
       vars.setSessionValue(strWindow + "|AD_Role_ID", strKey);
-      String strTab = vars.getStringParameter("inpTabId");
-      String strModule = vars.getStringParameter("inpModules");
-      String strType = vars.getStringParameter("inpType");
-      ActionButtonDefaultData[] tab = ActionButtonDefaultData.windowName(this, strTab);
+      final String strTab = vars.getStringParameter("inpTabId");
+      final String strModule = vars.getStringParameter("inpModules");
+      final String strType = vars.getStringParameter("inpType");
+      final ActionButtonDefaultData[] tab = ActionButtonDefaultData.windowName(this, strTab);
       String strWindowPath="", strTabName="";
       if (tab!=null && tab.length!=0) {
         strTabName = FormatUtilities.replace(tab[0].name);
         strWindowPath = "../" + FormatUtilities.replace(tab[0].description) + "/" + strTabName + "_Relation.html";
       } else strWindowPath = strDefaultServlet;
-      OBError myMessage = getPrintPage(response, vars, strKey, strModule,strType);
+      final OBError myMessage = getPrintPage(response, vars, strKey, strModule,strType);
       vars.setMessage(strTab, myMessage);
       //vars.setSessionValue(strWindow + "|" + strTabName + ".message", messageResult);
       printPageClosePopUp(response, vars, strWindowPath);      
@@ -82,9 +88,9 @@ public class InsertAcces extends HttpSecureAppServlet {
         strDescription = data[0].description;
         strHelp = data[0].help;
       }
-      String[] discard = {""};
+      final String[] discard = {""};
       if (strHelp.equals("")) discard[0] = new String("helpDiscard");
-      XmlDocument xmlDocument = xmlEngine.readXmlTemplate("org/openbravo/erpCommon/ad_actionButton/InsertAcces", discard).createXmlDocument();
+      final XmlDocument xmlDocument = xmlEngine.readXmlTemplate("org/openbravo/erpCommon/ad_actionButton/InsertAcces", discard).createXmlDocument();
       xmlDocument.setParameter("key", strKey);
       xmlDocument.setParameter("window", windowId);
       xmlDocument.setParameter("tab", strTab);
@@ -98,7 +104,7 @@ public class InsertAcces extends HttpSecureAppServlet {
       //xmlDocument.setParameter("message",strMessage.equals("")?"":"alert('" + strMessage + "');");
 
       {
-        OBError myMessage = vars.getMessage("InsertAcces");
+        final OBError myMessage = vars.getMessage("InsertAcces");
         vars.removeMessage("InsertAcces");
         if (myMessage!=null) {
           xmlDocument.setParameter("messageType", myMessage.getType());
@@ -107,7 +113,7 @@ public class InsertAcces extends HttpSecureAppServlet {
         }
       }
       response.setContentType("text/html; charset=UTF-8");
-      PrintWriter out = response.getWriter();
+      final PrintWriter out = response.getWriter();
       out.println(xmlDocument.print());
       out.close();
     }
@@ -117,13 +123,13 @@ public class InsertAcces extends HttpSecureAppServlet {
 	  myMessage = new OBError();
 	  myMessage.setTitle("");	  
     try{
-    InsertAccesData[] accesData = InsertAccesData.select(this);
+    final InsertAccesData[] accesData = InsertAccesData.select(this);
     generateAcces(vars, accesData, strKey, strModule,strType);
     myMessage.setType("Success");
     myMessage.setMessage(Utility.messageBD(this, "ProcessOK", vars.getLanguage()));
     return myMessage;
     //return Utility.messageBD(this, "ProcessOK", vars.getLanguage());
-    } catch (Exception e) {
+    } catch (final Exception e) {
       log4j.warn(e);
       myMessage.setType("Error");
       myMessage.setMessage(Utility.messageBD(this, "ProcessRunError", vars.getLanguage()));
@@ -141,29 +147,30 @@ public class InsertAcces extends HttpSecureAppServlet {
         else {
           if (accesData[i].action.equals("W") && (InsertAccesData.selectWindow(this, roleid, accesData[i].adwindowid)==null || InsertAccesData.selectWindow(this, roleid, accesData[i].adwindowid).equals("")) && (strType.equals("W") || strType.equals(""))){
               log4j.error("Action: " + accesData[i].action + " window: " + accesData[i].adwindowid);
-              InsertAccesData.insertWindow(this, accesData[i].adwindowid, roleid, vars.getClient(), vars.getOrg(), vars.getUser());
+              InsertAccesData.insertWindow(this, accesData[i].adwindowid, roleid, vars.getClient(), "0", vars.getUser());
           } else if (accesData[i].action.equals("P") && (InsertAccesData.selectProcess(this, roleid, accesData[i].adprocessid)==null || InsertAccesData.selectProcess(this, roleid, accesData[i].adprocessid).equals("")) && (strType.equals("P") || strType.equals(""))) {
               log4j.error("Action: " + accesData[i].action + " process: " + accesData[i].adprocessid);
-              InsertAccesData.insertProcess(this, accesData[i].adprocessid, roleid, vars.getClient(), vars.getOrg(), vars.getUser());
+              InsertAccesData.insertProcess(this, accesData[i].adprocessid, roleid, vars.getClient(), "0", vars.getUser());
           } else if (accesData[i].action.equals("R") && (InsertAccesData.selectProcess(this, roleid, accesData[i].adprocessid)==null || InsertAccesData.selectProcess(this, roleid, accesData[i].adprocessid).equals("")) && (strType.equals("R") || strType.equals(""))) {
               log4j.error("Action: " + accesData[i].action + " report: " + accesData[i].adprocessid);
-              InsertAccesData.insertProcess(this, accesData[i].adprocessid, roleid, vars.getClient(), vars.getOrg(), vars.getUser());
+              InsertAccesData.insertProcess(this, accesData[i].adprocessid, roleid, vars.getClient(), "0", vars.getUser());
           } else if (accesData[i].action.equals("X") && (InsertAccesData.selectForm(this, roleid, accesData[i].adformid)==null || InsertAccesData.selectForm(this, roleid, accesData[i].adformid).equals("")) && (strType.equals("X") || strType.equals(""))) {
               log4j.error("Action: " + accesData[i].action + " form: " + accesData[i].adformid);
-              InsertAccesData.insertForm(this, accesData[i].adformid, roleid, vars.getClient(), vars.getOrg(), vars.getUser());
+              InsertAccesData.insertForm(this, accesData[i].adformid, roleid, vars.getClient(), "0", vars.getUser());
           } else if (accesData[i].action.equals("F") && (InsertAccesData.selectWorkflow(this, roleid, accesData[i].adworkflowid)==null || InsertAccesData.selectWorkflow(this, roleid, accesData[i].adworkflowid).equals("")) && (strType.equals("F") || strType.equals(""))) {
               log4j.error("Action: " + accesData[i].action + " workflow: " + accesData[i].adworkflowid);
-              InsertAccesData.insertWorkflow(this, accesData[i].adworkflowid, roleid, vars.getClient(), vars.getOrg(), vars.getUser());
+              InsertAccesData.insertWorkflow(this, accesData[i].adworkflowid, roleid, vars.getClient(), "0", vars.getUser());
           } else if (accesData[i].action.equals("T") && (InsertAccesData.selectTask(this, roleid, accesData[i].adtaskid)==null || InsertAccesData.selectTask(this, roleid, accesData[i].adtaskid)==null) && (strType.equals("T") || strType.equals(""))) {
               log4j.error("Action: " + accesData[i].action + " task: " + accesData[i].adtaskid);
-              InsertAccesData.insertTask(this, accesData[i].adtaskid, roleid, vars.getClient(), vars.getOrg(), vars.getUser());
+              InsertAccesData.insertTask(this, accesData[i].adtaskid, roleid, vars.getClient(), "0", vars.getUser());
           }
         }
       }
     }
   }
 
-  public String getServletInfo() {
+  @Override
+public String getServletInfo() {
     return "Servlet for the application's roles and permissions generation.";
     // Servlet created by Galder
   } // end of getServletInfo() method
