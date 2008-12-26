46e9fba20efafb754af579ac6791b00694de75ad
David Baz Fayos <david.baz@openbravo.com>
2008-12-26 22:01:18 +0000

    Fixed bug 6367: Now shift+enter is not catched in application popups
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4011593

--- web/js/shortcuts.js
+++ web/js/shortcuts.js
@@ -136,7 +136,8 @@ function getShortcuts(type) {
       );
   } else if (type=='popupSpecificKeys') {
       this.keyArray.splice(keyArray.length-1, 0,
-        new keyArrayItem("ESCAPE", "parent.window.close();", null, null, false, 'onkeydown')
+        new keyArrayItem("ESCAPE", "parent.window.close();", null, null, false, 'onkeydown'),
+        new keyArrayItem("ENTER", "xx();", null, "shiftKey", false, 'onkeydown')
       );
   }
 }
