1d882c3a9e7eb4775a110eadf35a020d7ba4c52f
Stefan Hühner <stefan.huehner@openbravo.com>
2009-01-08 17:10:34 +0000

    Fixed 6531 Commit better fix which works in all cases.
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4011726

--- src/org/openbravo/erpCommon/ad_forms/ProductInfo.java
+++ src/org/openbravo/erpCommon/ad_forms/ProductInfo.java
@@ -219,7 +219,7 @@ public class ProductInfo {
     public static String getConvertedQty(String qty, String C_UOM_From_ID,
             String C_UOM_To_ID, String StdPrecision, ConnectionProvider conn) {
         // Nothing to do
-        if (qty.equals("") || qty.equals(ZERO.toString())
+        if (qty.equals("") || (new BigDecimal(qty).compareTo(BigDecimal.ZERO) == 0)
                 || C_UOM_From_ID.equals(C_UOM_To_ID))
             return qty;
         //
