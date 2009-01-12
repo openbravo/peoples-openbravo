6c16602eb437a8f7e638cf6832f792ceebb882f2
Víctor Martínez <victor.martinez@openbravo.com>
2009-01-12 14:30:14 +0000

    Fixed bug 5501: G/L Journal Batch Copy Lines does not work
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4011797

--- src/org/openbravo/erpCommon/ad_process/CopyFromGLJournal.java
+++ src/org/openbravo/erpCommon/ad_process/CopyFromGLJournal.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SL 
- * All portions are Copyright (C) 2001-2008 Openbravo SL 
+ * All portions are Copyright (C) 2001-2009 Openbravo SL 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -105,7 +105,7 @@ public class CopyFromGLJournal extends HttpSecureAppServlet {
         try {
             conn = this.getTransactionConnection();
             CopyFromGLJournalData[] data = CopyFromGLJournalData.select(this,
-                    strKey, strGLJournalBatch);
+                    strGLJournalBatch, strKey);
             for (int i = 0; data != null && i < data.length; i++) {
                 String strSequence = SequenceIdData.getUUID();
                 String strDocumentNo = Utility.getDocumentNo(this, vars,
