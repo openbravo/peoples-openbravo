db5f5a5f37564e9bd17aa2482bc3ac1a69fe97c9
Víctor Martínez <victor.martinez@openbravo.com>
2009-01-12 17:33:28 +0000

    Fixed bug 6138: Budget Export to Excel - Index out of bounds exception
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4011805

--- src/org/openbravo/erpCommon/ad_reports/ReportBudgetExportExcel.java
+++ src/org/openbravo/erpCommon/ad_reports/ReportBudgetExportExcel.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SL 
- * All portions are Copyright (C) 2001-2006 Openbravo SL 
+ * All portions are Copyright (C) 2001-2009 Openbravo SL 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -58,7 +58,7 @@ public class ReportBudgetExportExcel extends HttpSecureAppServlet {
         data = ReportBudgetGenerateExcelData.selectLines(this, vars
                 .getLanguage(), strBudgetId);
 
-        if (data[0].exportactual.equals("Y")) {
+        if (data.length != 0 && data[0].exportactual.equals("Y")) {
             xmlDocument = xmlEngine
                     .readXmlTemplate(
                             "org/openbravo/erpCommon/ad_reports/ReportBudgetGenerateExcelXLS")
