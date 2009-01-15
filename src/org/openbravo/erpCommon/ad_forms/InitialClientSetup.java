01858719a4113c58327ecbedbe281e96cfbeeafc
Víctor Martínez <victor.martinez@openbravo.com>
2009-01-15 16:06:58 +0000

    Fixed bug 6430: It is not possible to navigate to a Manual Settlement in General Ledger Journal. Modified the InitialClientSetup to ensure the new Manual Settlement documents are created with IsSOTrx=Y
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4011950

--- src/org/openbravo/erpCommon/ad_forms/InitialClientSetup.java
+++ src/org/openbravo/erpCommon/ad_forms/InitialClientSetup.java
@@ -11,7 +11,7 @@
  * Portions created by Jorg Janke are Copyright (C) 1999-2001 Jorg Janke, parts
  * created by ComPiere are Copyright (C) ComPiere, Inc.;   All Rights Reserved.
  * Contributor(s): Openbravo SL
- * Contributions are Copyright (C) 2001-2006 Openbravo S.L.
+ * Contributions are Copyright (C) 2001-2009 Openbravo S.L.
  ******************************************************************************
  */
 package org.openbravo.erpCommon.ad_forms;
@@ -1639,7 +1639,7 @@ public class InitialClientSetup extends HttpSecureAppServlet {
             else
                 IsDocNoControlled = "Y";
             if (DocBaseType.startsWith("AR") || DocBaseType.equals("MMS")
-                    || DocBaseType.equals("SOO"))
+                    || DocBaseType.equals("SOO") || DocBaseType.equals("STM"))
                 IsSOTrx = "Y";
             else
                 IsSOTrx = "N";
