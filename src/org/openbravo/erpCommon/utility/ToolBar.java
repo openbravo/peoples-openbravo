bb73a9f1799a1fd82bb551e9d93a75d860288bc9
Gorka Ion Damián <gorkaion.damian@openbravo.com>
2009-02-12 11:33:27 +0000

    Fixed bug 7428. Applyed villind's proposed view. The links to open the export grids now have html suffix.
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4013090

--- src/org/openbravo/erpCommon/utility/ToolBar.java
+++ src/org/openbravo/erpCommon/utility/ToolBar.java
@@ -159,11 +159,11 @@ public class ToolBar {
     } else if (name.equals("EXCEL")) {
       return "openExcel('" + servlet_action + "_Excel.xls?Command=RELATION_XLS', '_blank');";
     } else if (name.equals("GRIDEXCEL")) {
-      return "openServletNewWindow('EXCEL', false, '../utility/ExportGrid.xls?inpTabId=' + document.forms[0].inpTabId.value + '&inpWindowId=' + document.forms[0].inpwindowId.value + '&inpAccessLevel=' + document.forms[0].inpAccessLevel.value, 'GRIDEXCEL', null, null, 500, 350, true );";
+      return "openServletNewWindow('EXCEL', false, '../utility/ExportGrid.html?inpTabId=' + document.forms[0].inpTabId.value + '&inpWindowId=' + document.forms[0].inpwindowId.value + '&inpAccessLevel=' + document.forms[0].inpAccessLevel.value, 'GRIDEXCEL', null, null, 500, 350, true );";
     } else if (name.equals("GRIDCSV")) {
-      return "openServletNewWindow('CSV', false, '../utility/ExportGrid.csv?inpTabId=' + document.forms[0].inpTabId.value + '&inpWindowId=' + document.forms[0].inpwindowId.value + '&inpAccessLevel=' + document.forms[0].inpAccessLevel.value, 'GRIDCSV', null, null, 500, 350, true );";
+      return "openServletNewWindow('CSV', false, '../utility/ExportGrid.html?inpTabId=' + document.forms[0].inpTabId.value + '&inpWindowId=' + document.forms[0].inpwindowId.value + '&inpAccessLevel=' + document.forms[0].inpAccessLevel.value, 'GRIDCSV', null, null, 500, 350, true );";
     } else if (name.equals("GRIDPDF")) {
-      return "openServletNewWindow('PDF', false, '../utility/ExportGrid.pdf?inpTabId=' + document.forms[0].inpTabId.value + '&inpWindowId=' + document.forms[0].inpwindowId.value + '&inpAccessLevel=' + document.forms[0].inpAccessLevel.value, 'GRIDPDF', null, null, 500, 350, true );";
+      return "openServletNewWindow('PDF', false, '../utility/ExportGrid.html?inpTabId=' + document.forms[0].inpTabId.value + '&inpWindowId=' + document.forms[0].inpwindowId.value + '&inpAccessLevel=' + document.forms[0].inpAccessLevel.value, 'GRIDPDF', null, null, 500, 350, true );";
     } else if (name.equals("PRINT")) {
       return "openPDFSession('"
           + pdf
