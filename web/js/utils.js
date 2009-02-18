b5acfd6a48fbcc5d5ad410aa1e85ce26aef2f20a
David Baz Fayos <david.baz@openbravo.com>
2009-02-18 20:49:27 +0000

    Updated revision control changes number
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4013343

--- web/js/utils.js
+++ web/js/utils.js
@@ -61,7 +61,7 @@ var isUserClick = false;
 * Return a number that would be checked at the Login screen to know if the file is cached with the correct version
 */
 function getCurrentRevision() {
-  var number = '13300';
+  var number = '13343';
   return number;
 }
 
