d388342020b8a3ecfb4e992a69acc903dd313114
David Baz Fayos <david.baz@openbravo.com>
2009-02-20 16:37:53 +0000

    Fixed bug 7725: Changed 'Timeout' text with 'Session expired' in VerticalMenu.html
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4013484

--- web/js/utils.js
+++ web/js/utils.js
@@ -61,7 +61,7 @@ var isUserClick = false;
 * Return a number that would be checked at the Login screen to know if the file is cached with the correct version
 */
 function getCurrentRevision() {
-  var number = '13433';
+  var number = '13484';
   return number;
 }
 
