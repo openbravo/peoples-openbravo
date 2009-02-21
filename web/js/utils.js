58a0077924e0ca7ad8d894c41a98d4dba46750ae
David Baz Fayos <david.baz@openbravo.com>
2009-02-21 22:57:07 +0000

    Fixed bug 6848: Now ErrorPopup.html buttons and shortcuts run ok in TabAttachment context
    --HG--
    extra : convert_revision : svn%3A9509db7c-3d11-0410-ba00-cf6d01345df7/trunk%4013490

--- web/js/utils.js
+++ web/js/utils.js
@@ -61,7 +61,7 @@ var isUserClick = false;
 * Return a number that would be checked at the Login screen to know if the file is cached with the correct version
 */
 function getCurrentRevision() {
-  var number = '13484';
+  var number = '13490';
   return number;
 }
 
