2dd116f4a090866b69f1d56679c606bb482a2855
Víctor Martínez Romanos <victor.martinez@openbravo.com>
2009-05-07 19:09:24 +0200

    Fixed bug 8948: Accounting background process doesn't work
    --HG--
    extra : transplant_source : 9%A2%22%B6%91%29%00%BB%ABV.o%F1b%93%A1%FE%DF%B4%CF

--- src/org/openbravo/erpCommon/ad_process/AcctServerProcess.java
+++ src/org/openbravo/erpCommon/ad_process/AcctServerProcess.java
@@ -1,3 +1,22 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html 
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License. 
+ * The Original Code is Openbravo ERP. 
+ * The Initial Developer of the Original Code is Openbravo SL 
+ * All portions are Copyright (C) 2009 Openbravo SL 
+ * All Rights Reserved. 
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
+ */
+
 package org.openbravo.erpCommon.ad_process;
 
 import java.io.IOException;
@@ -82,8 +101,8 @@ public class AcctServerProcess extends DalBaseProcess {
     adNoteId = saveLog(adNoteId, vars.getClient());
     String[] tables = null;
     String strTable = "";
-    // fill organisation from process request by default
-    String strOrg = ctx.getOrganization();
+    // If it is the background process, we use 0
+    String strOrg = "0";
     // if called by 'Posting by DB tables' get params from ad_pinstance
     if (isDirect) {
       strTable = AcctServerProcessData.selectTable(connection, pinstanceId);
