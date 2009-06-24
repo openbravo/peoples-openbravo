9bdbba74dc68a19813d1342167cc5fd60a3e4fe9
Stefan Hühner <stefan.huehner@openbravo.com>
2009-06-24 16:34:46 +0200

    Fixed 9562: Optimize product selector on oracle by better placement of the rownum where clause

--- src/org/openbravo/erpCommon/info/Product.java
+++ src/org/openbravo/erpCommon/info/Product.java
@@ -148,7 +148,7 @@ public class Product extends HttpSecureAppServlet {
       vars.setSessionValue("Product.priceListVersion", strPriceListVersion);
       ProductData[] data = ProductData.select(this, strWarehouse, "1", strKeyValue + "%", "",
           Utility.getContext(this, vars, "#User_Client", "Product"), Utility.getContext(this, vars,
-              "#User_Org", "Product"), strPriceListVersion, "1", "", "");
+              "#User_Org", "Product"), strPriceListVersion, "1", "", "", "");
       if (data != null && data.length == 1)
         printPageKey(response, vars, data, strWarehouse, strPriceListVersion);
       else
@@ -372,15 +372,16 @@ public class Product extends HttpSecureAppServlet {
 
         // Filtering result
         if (this.myPool.getRDBMS().equalsIgnoreCase("ORACLE")) {
-          String oraLimit = (offset + 1) + " AND " + String.valueOf(offset + pageSize);
+          String oraLimit1 = String.valueOf(offset + pageSize);
+          String oraLimit2 = (offset + 1) + " AND " + oraLimit1;
           data = ProductData.select(this, strWarehouse, "ROWNUM", strKey, strName, Utility
               .getContext(this, vars, "#User_Client", "Product"), Utility.getSelectorOrgs(this,
-              vars, strOrg), strPriceListVersion, strOrderBy, oraLimit, "");
+              vars, strOrg), strPriceListVersion, strOrderBy, oraLimit1, oraLimit2, "");
         } else {
           String pgLimit = pageSize + " OFFSET " + offset;
           data = ProductData.select(this, strWarehouse, "1", strKey, strName, Utility.getContext(
               this, vars, "#User_Client", "Product"), Utility.getSelectorOrgs(this, vars, strOrg),
-              strPriceListVersion, strOrderBy, "", pgLimit);
+              strPriceListVersion, strOrderBy, "", "", pgLimit);
         }
       } catch (ServletException e) {
         log4j.error("Error in print page data: " + e);
