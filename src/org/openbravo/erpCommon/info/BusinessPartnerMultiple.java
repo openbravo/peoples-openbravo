791ea5e846a6a5dbe10a699a26a1f019dc9bf4ab
Iván Perdomo <ivan.perdomo@openbravo.com>
2009-08-10 19:02:00 +0200

    Fixes Business Partner Multiple selector, number formatting

--- src/org/openbravo/erpCommon/info/BusinessPartnerMultiple.java
+++ src/org/openbravo/erpCommon/info/BusinessPartnerMultiple.java
@@ -20,6 +20,8 @@ package org.openbravo.erpCommon.info;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Vector;
 
 import javax.servlet.ServletConfig;
@@ -210,11 +212,8 @@ public class BusinessPartnerMultiple extends HttpSecureAppServlet {
         // build sql orderBy clause
         String strOrderBy = SelectorUtility.buildOrderByClause(strOrderCols, strOrderDirs);
 
-        if (strNewFilter.equals("1") || strNewFilter.equals("")) { // New
-          // filter
-          // or
-          // first
-          // load
+        // New filter or first load
+        if (strNewFilter.equals("1") || strNewFilter.equals("")) {
           strNumRows = BusinessPartnerMultipleData.countRows(this, Utility.getContext(this, vars,
               "#User_Client", "BusinessPartner"), Utility.getContext(this, vars,
               "#AccessibleOrgTree", "BusinessPartner"), strKey, strName, strContact, strZIP,
@@ -271,6 +270,8 @@ public class BusinessPartnerMultiple extends HttpSecureAppServlet {
       }
     }
 
+    DecimalFormat df = Utility.getFormat(vars, "priceEdition");
+
     if (!type.startsWith("<![CDATA["))
       type = "<![CDATA[" + type + "]]>";
     if (!title.startsWith("<![CDATA["))
@@ -292,7 +293,13 @@ public class BusinessPartnerMultiple extends HttpSecureAppServlet {
           strRowsData.append("      <td><![CDATA[");
           String columnname = headers[k].getField("columnname");
 
-          if ((data[j].getField(columnname)) != null) {
+          if (columnname.equalsIgnoreCase("SO_CreditAvailable")
+              || columnname.equalsIgnoreCase("SO_CreditUsed")
+              || columnname.equalsIgnoreCase("ActualLifetimeValue")) {
+            final String value = data[j].getField(columnname).equals("") ? "0" : data[j]
+                .getField(columnname);
+            strRowsData.append(df.format(new BigDecimal(value)));
+          } else if ((data[j].getField(columnname)) != null) {
             if (headers[k].getField("adReferenceId").equals("32"))
               strRowsData.append(strReplaceWith).append("/images/");
             strRowsData.append(data[j].getField(columnname).replaceAll("<b>", "").replaceAll("<B>",
