e673295c5b53981e55cb98d02a4308c19c4b6b66
Stefan Hühner <stefan.huehner@openbravo.com>
2009-08-14 14:30:04 +0200

    [NumberFormat] delete two unused js-method introduced in commit 794612123584

--- web/js/utils.js
+++ web/js/utils.js
@@ -4623,41 +4623,6 @@ function setCaretToPos (input, pos) {
   setSelectionRange(input, pos, pos);
 }
 
-function selectString (input, string) {
-  var match = new RegExp(string, "i").exec(input.value);
-  if (match) {
-    setSelectionRange (input, match.index, match.index + match
-[0].length);
-  }
-}
-
-function replaceSelection (input, replaceString) {
-  if (input.setSelectionRange) {
-    var selectionStart = input.selectionStart;
-    var selectionEnd = input.selectionEnd;
-    input.value = input.value.substring(0, selectionStart)
-                  + replaceString
-                  + input.value.substring(selectionEnd);
-    if (selectionStart != selectionEnd) // has there been a selection
-      setSelectionRange(input, selectionStart, selectionStart + replaceString.length);
-    else // set caret
-      setCaretToPos(input, selectionStart + replaceString.length);
-  }
-  else if (document.selection) {
-    var range = document.selection.createRange();
-    if (range.parentElement() == input) {
-      var isCollapsed = range.text == '';
-      range.text = replaceString;
-      if (!isCollapsed)  { // there has been a selection
-        // it appears range.select() should select the newly
-        // inserted text but that fails with IE
-        range.moveStart('character', -replaceString.length);
-        range.select();
-      }
-    }
-  }
-}
-
 /**
 * Function that manage the numeric dot write in the input
 * @param {Object} obj The input
