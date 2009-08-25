9061ed9e6a6478747592cd89021e49dc43301723
Stefan Hühner <stefan.huehner@openbravo.com>
2009-08-25 18:06:35 +0200

    [PagedGrid] convert product selector to paged grid - Limit count queries to <= 10000 - add record count widget - add js logic to show/hide paging buttons and record count widget - add java logic to maintain and use backendPage - add searchs.js change to take care of record count widget height

--- src/org/openbravo/erpCommon/info/Product.java
+++ src/org/openbravo/erpCommon/info/Product.java
@@ -37,6 +37,7 @@ import org.openbravo.data.FieldProvider;
 import org.openbravo.erpCommon.utility.DateTimeData;
 import org.openbravo.erpCommon.utility.OBError;
 import org.openbravo.erpCommon.utility.SQLReturnObject;
+import org.openbravo.erpCommon.utility.TableSQLData;
 import org.openbravo.erpCommon.utility.Utility;
 import org.openbravo.utils.Replace;
 import org.openbravo.xmlEngine.XmlDocument;
@@ -200,6 +201,7 @@ public class Product extends HttpSecureAppServlet {
     vars.removeSessionValue("Product.warehouse");
     vars.removeSessionValue("Product.priceList");
     vars.removeSessionValue("Product.priceListVersion");
+    vars.removeSessionValue("Product.currentPage");
   }
 
   private String getPriceListVersion(VariablesSecureApp vars, String strPriceList, String strDate)
@@ -369,6 +371,7 @@ public class Product extends HttpSecureAppServlet {
     String title = "";
     String description = "";
     String strNumRows = "0";
+    int page = 0;
     int offset = Integer.valueOf(strOffset).intValue();
     int pageSize = Integer.valueOf(strPageSize).intValue();
 
@@ -377,11 +380,30 @@ public class Product extends HttpSecureAppServlet {
         // build sql orderBy clause
         String strOrderBy = SelectorUtility.buildOrderByClause(strOrderCols, strOrderDirs);
 
+        page = TableSQLData.calcAndGetBackendPage(vars, "Product.currentPage");
+        if (vars.getStringParameter("movePage", "").length() > 0) {
+          // on movePage action force executing countRows again
+          strNewFilter = "";
+        }
+        int oldOffset = offset;
+        offset = (page * TableSQLData.maxRowsPerGridPage) + offset;
+        log4j.debug("relativeOffset: " + oldOffset + " absoluteOffset: " + offset);
+
+        // New filter or first load
         if (strNewFilter.equals("1") || strNewFilter.equals("")) {
-          // New filter or first load
-          strNumRows = ProductData.countRows(this, strKey, strName, strPriceListVersion, Utility
-              .getContext(this, vars, "#User_Client", "Product"), Utility.getSelectorOrgs(this,
-              vars, strOrg));
+
+          String rownum = "0", oraLimit1 = null, oraLimit2 = null, pgLimit = null;
+          if (this.myPool.getRDBMS().equalsIgnoreCase("ORACLE")) {
+            oraLimit1 = String.valueOf(offset + TableSQLData.maxRowsPerGridPage);
+            oraLimit2 = (offset + 1) + " AND " + oraLimit1;
+            rownum = "ROWNUM";
+          } else {
+            pgLimit = TableSQLData.maxRowsPerGridPage + " OFFSET " + offset;
+          }
+
+          strNumRows = ProductData.countRows(this, rownum, strKey, strName, strPriceListVersion,
+              Utility.getContext(this, vars, "#User_Client", "Product"), Utility.getSelectorOrgs(
+                  this, vars, strOrg), pgLimit, oraLimit1, oraLimit2);
           vars.setSessionValue("Product.numrows", strNumRows);
         } else {
           strNumRows = vars.getSessionValue("Product.numrows");
@@ -444,7 +466,8 @@ public class Product extends HttpSecureAppServlet {
     strRowsData.append("    <title>").append(title).append("</title>\n");
     strRowsData.append("    <description>").append(description).append("</description>\n");
     strRowsData.append("  </status>\n");
-    strRowsData.append("  <rows numRows=\"").append(strNumRows).append("\">\n");
+    strRowsData.append("  <rows numRows=\"").append(strNumRows).append(
+        "\" backendPage=\"" + page + "\">\n");
     if (data != null && data.length > 0) {
       for (int j = 0; j < data.length; j++) {
         strRowsData.append("    <tr>\n");
