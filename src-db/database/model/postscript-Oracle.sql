9e8842ea91a243fd76dd351b85875cc49e4bd4b8
Antonio Moreno <antonio.moreno@openbravo.com>
2009-09-01 13:30:18 +0200

    Fixed issue 10444. Moved AD_GET_RDBMS function from prescript to postscript.

--- src-db/database/model/postscript-Oracle.sql
+++ src-db/database/model/postscript-Oracle.sql
@@ -784,6 +784,29 @@ SELECT to_number(a.value) AS value
   WHERE a.value <= 1024
 /-- END
 
+create or replace FUNCTION AD_GET_RDBMS RETURN VARCHAR2
+AS
+/*************************************************************************
+* The contents of this file are subject to the Openbravo  Public  License
+* Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+* Version 1.1  with a permitted attribution clause; you may not  use this
+* file except in compliance with the License. You  may  obtain  a copy of
+* the License at http://www.openbravo.com/legal/license.html
+* Software distributed under the License  is  distributed  on  an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific  language  governing  rights  and  limitations
+* under the License.
+* The Original Code is Openbravo ERP.
+* The Initial Developer of the Original Code is Openbravo SL
+* All portions are Copyright (C) 2009 Openbravo SL
+* All Rights Reserved.
+* Contributor(s):  ______________________________________.
+************************************************************************/
+BEGIN
+ return 'ORACLE';
+END AD_GET_RDBMS;
+/-- END
+
 CALL DBA_RECOMPILE(NULL)
 /-- END
 
