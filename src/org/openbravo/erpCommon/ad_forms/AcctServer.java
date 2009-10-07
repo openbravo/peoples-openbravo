464b0400f46ee91129f35d16d31ac6be25cc0d9f
Eduardo Argal Guibert <eduardo.argal@openbravo.com>
2009-10-07 13:31:06 +0200

    Fixes bug 0010895: Accounting process should remove any existing entry in accounting before posting a document
    --HG--
    extra : rebase_source : f010cafd9ef2fb97e32883506ac3b3e59d7a28f0

--- src/org/openbravo/erpCommon/ad_forms/AcctServer.java
+++ src/org/openbravo/erpCommon/ad_forms/AcctServer.java
@@ -506,7 +506,8 @@ public abstract class AcctServer {
             + strClave);
         Status = "L"; // Status locked document
         return false;
-      }
+      } else
+        AcctServerData.delete(con, connectionProvider, AD_Table_ID, Record_ID);
       if (log4j.isDebugEnabled())
         log4j.debug("AcctServer - Post -TableName -" + tableName + "- ad_client_id -"
             + AD_Client_ID + "- " + tableName + "_id -" + strClave);
