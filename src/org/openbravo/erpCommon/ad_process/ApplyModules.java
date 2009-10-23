ac3c0aed672d7f111b1b14bf7e6f1e4cb8f7a51c
Antonio Moreno <antonio.moreno@openbravo.com>
2009-10-23 15:29:45 +0200

    Fixed issue 10752. Rebuild shouldn't get blocked when the background process is active (because it will stop it before doing anything).
    --HG--
    extra : transplant_source : %13%9EI%FA%E99%7D%B7%89%E4%9B%17%8Ey%F3%E7%80%93%7C%04

--- src/org/openbravo/erpCommon/ad_process/ApplyModules.java
+++ src/org/openbravo/erpCommon/ad_process/ApplyModules.java
@@ -38,6 +38,7 @@ import org.openbravo.erpCommon.utility.AntExecutor;
 import org.openbravo.erpCommon.utility.OBError;
 import org.openbravo.erpCommon.utility.Utility;
 import org.openbravo.model.ad.module.ModuleLog;
+import org.openbravo.scheduling.OBScheduler;
 import org.openbravo.service.system.ReloadContext;
 import org.openbravo.service.system.RestartTomcat;
 import org.openbravo.xmlEngine.XmlDocument;
@@ -208,7 +209,6 @@ public class ApplyModules extends HttpSecureAppServlet {
       final Vector<String> tasks = new Vector<String>();
 
       final String unnappliedModules = getUnnapliedModules();
-
       if (ApplyModulesData.isUpdatingCoreOrTemplate(this)) {
         tasks.add("update.database");
         tasks.add("core.lib");
@@ -227,6 +227,11 @@ public class ApplyModules extends HttpSecureAppServlet {
         tasks.add("apply.modules");
         ant.setProperty("module", unnappliedModules);
       }
+
+      //We first shutdown the background process, so that it doesn't interfere
+      //with the rebuild process
+      OBScheduler.getInstance().getScheduler().shutdown(true);
+
 
       ant.runTask(tasks);
 
