aef5519a32cd1a1ff60603cbe8b776224fa4a177
Martin Taal <martin.taal@openbravo.com>
2009-10-27 21:13:32 +0100

    Made BaseOBObject.setDefaultValue more robust for model changes

--- src/org/openbravo/base/structure/BaseOBObject.java
+++ src/org/openbravo/base/structure/BaseOBObject.java
@@ -67,6 +67,11 @@ public abstract class BaseOBObject implements BaseOBObjectDef, Identifiable, Dyn
   // is used to set default data in a constructor of the generated class
   // without a security check
   protected void setDefaultValue(String propName, Object value) {
+    if (!getEntity().hasProperty(propName)) {
+      log.warn("Property " + propName + " does not exist for entity " + getEntityName()
+          + ". This is not necessarily a problem, this can happen when modules are uninstalled.");
+      return;
+    }
     try {
       getEntity().checkValidPropertyAndValue(propName, value);
       Check.isNotNull(value, "Null default values are not allowed");
