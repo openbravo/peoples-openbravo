e2446a44ac30ee7d84d9db4e279ede3585b39d98
Asier Lostalé <asier.lostale@openbravo.com>
2009-11-12 13:54:56 +0100

    related to issue 11246: clean up FieldGroupLabelsData class
    --HG--
    extra : rebase_source : 80cdd42629c7f37e71ab0afd8e928a51d0b90e51

--- src-core/src/org/openbravo/uiTranslation/FieldGroupLabelsData.java
+++ src-core/src/org/openbravo/uiTranslation/FieldGroupLabelsData.java
@@ -1,4 +1,22 @@
-//Sqlc generated V1.O00-1
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html 
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License. 
+ * The Original Code is Openbravo ERP. 
+ * The Initial Developer of the Original Code is Openbravo SL 
+ * All portions are Copyright (C) 2009 Openbravo SL 
+ * All Rights Reserved. 
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
+ */
+
 package org.openbravo.uiTranslation;
 
 import java.sql.PreparedStatement;
@@ -15,15 +33,10 @@ import org.openbravo.database.ConnectionProvider;
 
 class FieldGroupLabelsData implements FieldProvider {
   static Logger log4j = Logger.getLogger(FieldGroupLabelsData.class);
-  private String InitRecordNumber = "0";
   public String tabname;
   public String fieldgroupid;
   public String fieldgroupname;
   public String fieldgrouptrlname;
-
-  public String getInitRecordNumber() {
-    return InitRecordNumber;
-  }
 
   public String getField(String fieldName) {
     if (fieldName.equalsIgnoreCase("TABNAME"))
@@ -102,10 +115,8 @@ class FieldGroupLabelsData implements FieldProvider {
       UtilSql.setValue(st, ++iParameter, 12, null, ad_tab_id);
 
       result = st.executeQuery();
-      long countRecord = 0;
 
       while (result.next()) {
-        countRecord++;
         FieldGroupLabelsData objectFieldGroupLabelsData = new FieldGroupLabelsData();
         objectFieldGroupLabelsData.tabname = UtilSql.getValue(result, "TABNAME");
         objectFieldGroupLabelsData.fieldgroupid = UtilSql.getValue(result, "FIELDGROUPID");
@@ -116,11 +127,11 @@ class FieldGroupLabelsData implements FieldProvider {
       }
       result.close();
     } catch (SQLException e) {
-      log4j.error("SQL error in query: " + strSql + "Exception:" + e);
+      log4j.error("SQL error in query: " + strSql + "Exception:", e);
       throw new ServletException("@CODE=" + Integer.toString(e.getErrorCode()) + "@"
           + e.getMessage());
     } catch (Exception ex) {
-      log4j.error("Exception in query: " + strSql + "Exception:" + ex);
+      log4j.error("Exception in query: " + strSql + "Exception:", ex);
       throw new ServletException("@CODE=@" + ex.getMessage());
     } finally {
       try {
@@ -134,116 +145,4 @@ class FieldGroupLabelsData implements FieldProvider {
     return (objectFieldGroupLabelsData);
   }
 
-  /**
-   * Do not use. Use instead
-   * {@link FieldGroupLabelsData#selectFieldGroupTrl(ConnectionProvider, String, String)}
-   */
-  @Deprecated
-  public static FieldGroupLabelsData[] selectFieldGroupTrl(ConnectionProvider connectionProvider,
-      String ad_tab_id, String language, String keyValue, String keyName, int numberRegisters)
-      throws ServletException {
-    return selectFieldGroupTrl(connectionProvider, ad_tab_id, language);
-  }
-
-  /**
-   * Do not use. Use instead
-   * {@link FieldGroupLabelsData#selectFieldGroupTrl(ConnectionProvider, String, String)}
-   */
-  @Deprecated
-  public static FieldGroupLabelsData[] selectFieldGroupTrl(ConnectionProvider connectionProvider,
-      String ad_tab_id, String language, int firstRegister, int numberRegisters)
-      throws ServletException {
-    return selectFieldGroupTrl(connectionProvider, ad_tab_id, language);
-  }
-
-  /**
-   * Do not use. Use instead
-   * {@link FieldGroupLabelsData#selectFieldGroupTrl(ConnectionProvider, String, String)}
-   */
-  @Deprecated
-  public static FieldGroupLabelsData[] select(ConnectionProvider connectionProvider,
-      String ad_tab_id) throws ServletException {
-    return select(connectionProvider, ad_tab_id, 0, 0);
-  }
-
-  /**
-   * Do not use. Use instead
-   * {@link FieldGroupLabelsData#selectFieldGroupTrl(ConnectionProvider, String, String)}
-   */
-  @Deprecated
-  public static FieldGroupLabelsData[] select(ConnectionProvider connectionProvider,
-      String ad_tab_id, String keyValue, String keyName, int numberRegisters)
-      throws ServletException {
-    return select(connectionProvider, ad_tab_id, 0, numberRegisters);
-  }
-
-  /**
-   * Do not use. Use instead
-   * {@link FieldGroupLabelsData#selectFieldGroupTrl(ConnectionProvider, String, String)}
-   */
-  @Deprecated
-  public static FieldGroupLabelsData[] select(ConnectionProvider connectionProvider,
-      String ad_tab_id, int firstRegister, int numberRegisters) throws ServletException {
-    String strSql = "";
-    strSql = strSql + "      select " + "  			tab.name as tabName"
-        + "  			, fgroup.ad_fieldgroup_id as fieldGroupId" + "  			, fgroup.name as fieldGroupName"
-        + "			from" + "			  ad_tab tab" + "			  , ad_field field" + "			  , ad_fieldgroup fgroup"
-        + "			where" + "			  tab.ad_tab_id = field.ad_tab_id"
-        + "			  and field.ad_fieldgroup_id = fGroup.ad_fieldGroup_id"
-        + "			  and tab.ad_tab_id = ? " + "		UNION " + "      select " + "  			tab.name as tabName"
-        + "  			, fgroup.ad_fieldgroup_id as fieldGroupId" + "  			, fgroup.name as fieldGroupName"
-        + "			from" + "			  ad_tab tab" + "			  , ad_fieldgroup fgroup" + "			where"
-        + "			  fgroup.ad_fieldgroup_id = '1000100001' " + "			  and tab.ad_tab_id = ? ";
-
-    ResultSet result;
-    Vector<java.lang.Object> vector = new Vector<java.lang.Object>(0);
-    PreparedStatement st = null;
-
-    int iParameter = 0;
-    try {
-      st = connectionProvider.getPreparedStatement(strSql);
-      iParameter++;
-      UtilSql.setValue(st, iParameter, 12, null, ad_tab_id);
-      iParameter++;
-      UtilSql.setValue(st, iParameter, 12, null, ad_tab_id);
-
-      result = st.executeQuery();
-      long countRecord = 0;
-      long countRecordSkip = 1;
-      boolean continueResult = true;
-      while (countRecordSkip < firstRegister && continueResult) {
-        continueResult = result.next();
-        countRecordSkip++;
-      }
-      while (continueResult && result.next()) {
-        countRecord++;
-        FieldGroupLabelsData objectFieldGroupLabelsData = new FieldGroupLabelsData();
-        objectFieldGroupLabelsData.tabname = UtilSql.getValue(result, "TABNAME");
-        objectFieldGroupLabelsData.fieldgroupid = UtilSql.getValue(result, "FIELDGROUPID");
-        objectFieldGroupLabelsData.fieldgroupname = UtilSql.getValue(result, "FIELDGROUPNAME");
-        objectFieldGroupLabelsData.InitRecordNumber = Integer.toString(firstRegister);
-        vector.addElement(objectFieldGroupLabelsData);
-        if (countRecord >= numberRegisters && numberRegisters != 0) {
-          continueResult = false;
-        }
-      }
-      result.close();
-    } catch (SQLException e) {
-      log4j.error("SQL error in query: " + strSql + "Exception:" + e);
-      throw new ServletException("@CODE=" + Integer.toString(e.getErrorCode()) + "@"
-          + e.getMessage());
-    } catch (Exception ex) {
-      log4j.error("Exception in query: " + strSql + "Exception:" + ex);
-      throw new ServletException("@CODE=@" + ex.getMessage());
-    } finally {
-      try {
-        connectionProvider.releasePreparedStatement(st);
-      } catch (Exception ignore) {
-        ignore.printStackTrace();
-      }
-    }
-    FieldGroupLabelsData objectFieldGroupLabelsData[] = new FieldGroupLabelsData[vector.size()];
-    vector.copyInto(objectFieldGroupLabelsData);
-    return (objectFieldGroupLabelsData);
-  }
 }
