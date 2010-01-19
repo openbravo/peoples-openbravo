6513b1ab15855c7cd7fde275f17afe3a81fc3ff5
Asier Lostalé <asier.lostale@openbravo.com>
2010-01-19 09:19:05 +0100

    [audit] Offset and limit for deleted records
    --HG--
    extra : rebase_source : 2aab8fcc28b25ea7363e6c7fc6c7c7c7292251a9

--- src/org/openbravo/erpCommon/businessUtility/AuditTrailDeletedRecords.java
+++ src/org/openbravo/erpCommon/businessUtility/AuditTrailDeletedRecords.java
@@ -29,14 +29,21 @@ import org.openbravo.model.ad.ui.Tab;
 
 public class AuditTrailDeletedRecords {
 
-  public static FieldProvider[] getDeletedRecords(ConnectionProvider conn, String tabId) {
+  public static FieldProvider[] getDeletedRecords(ConnectionProvider conn, String tabId,
+      int startPosition, int rangeLength) {
 
     OBContext.enableAsAdminContext();
     StringBuffer sql = new StringBuffer();
     try {
       Tab tab = OBDal.getInstance().get(Tab.class, tabId);
       String tableName = tab.getTable().getDBTableName();
+      boolean hasRange = !(startPosition == 0 && rangeLength == 0);
+      boolean hasRangeLimit = !(rangeLength == 0);
+
       sql.append("SELECT * FROM (\n");
+      if (hasRange && conn.getRDBMS().equalsIgnoreCase("ORACLE")) {
+        sql.append("SELECT ROWNUM AS RN1, ").append(tableName).append(".* FROM(\n");
+      }
       sql.append("SELECT \n");
       boolean firstItem = true;
       for (Column col : tab.getTable().getADColumnList()) {
@@ -48,7 +55,7 @@ public class AuditTrailDeletedRecords {
           firstItem = false;
         }
         sql
-            .append("(SELECT COALESCE(OLD_CHAR, TO_CHAR(OLD_NCHAR), TO_CHAR(OLD_NUMBER), TO_CHAR(OLD_DATE)) AS OLD\n");
+            .append("(SELECT COALESCE(OLD_CHAR, TO_CHAR(OLD_NCHAR), TO_CHAR(OLD_NUMBER), TO_CHAR(OLD_DATE))\n");
         sql.append("  FROM AD_AUDIT_TRAIL\n");
         sql.append(" WHERE AD_TABLE_ID='").append(tab.getTable().getId()).append("'\n");
         sql.append("   AND AD_COLUMN_ID='").append(col.getId()).append("'\n");
@@ -63,12 +70,31 @@ public class AuditTrailDeletedRecords {
       sql.append("  AND AD_COLUMN_ID = '").append(
           ModelProvider.getInstance().getEntityByTableName(tableName).getIdProperties().get(0)
               .getColumnId()).append("'\n");
-      sql.append(") ").append(tableName);
+      sql.append("  ORDER BY TIME DESC").append(") ").append(tableName);
 
       // apply where clause if exists
       if (tab.getSQLWhereClause() != null) {
         sql.append(" where ").append(tab.getSQLWhereClause());
       }
+
+      if (hasRange) {
+        // wrap end SQL
+        // calc positions
+        String rangeStart = Integer.toString(startPosition + 1);
+        String rangeEnd = Integer.toString(startPosition + rangeLength);
+        if (conn.getRDBMS().equalsIgnoreCase("ORACLE")) {
+          sql.append(") WHERE RN1 ");
+          if (hasRangeLimit)
+            sql.append("BETWEEN " + rangeStart + " AND " + rangeEnd);
+          else
+            sql.append(">= ").append(rangeStart);
+        } else {
+          if (hasRangeLimit)
+            sql.append(" LIMIT " + Integer.toString(rangeLength));
+          sql.append(" OFFSET " + Integer.toString(startPosition) + ")\n");
+        }
+      }
+
       System.out.println(sql);
 
       ExecuteQuery q = new ExecuteQuery(conn, sql.toString(), null);
