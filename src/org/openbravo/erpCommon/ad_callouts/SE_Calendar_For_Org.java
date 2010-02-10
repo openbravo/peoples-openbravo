16cfc292f51a9a54bff79d8bda0a7352081991d1
Sathiyan Sivaprakasam <sathiyan.sivaprakasam@openbravo.com>
2010-02-10 11:40:43 +0530

    variable and its type refactored and licence text has added

--- src/org/openbravo/erpCommon/ad_callouts/SE_Calendar_For_Org.java
+++ src/org/openbravo/erpCommon/ad_callouts/SE_Calendar_For_Org.java
@@ -1,3 +1,21 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html 
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License. 
+ * The Original Code is Openbravo ERP. 
+ * The Initial Developer of the Original Code is Openbravo SL 
+ * All portions are Copyright (C) 2001-2010 Openbravo SL 
+ * All Rights Reserved. 
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
+ */
 package org.openbravo.erpCommon.ad_callouts;
 
 import java.io.IOException;
@@ -43,9 +61,9 @@ public class SE_Calendar_For_Org extends HttpSecureAppServlet {
     XmlDocument xmlDocument = xmlEngine.readXmlTemplate(
         "org/openbravo/erpCommon/ad_callouts/CallOut").createXmlDocument();
 
-    StringBuffer resultado = new StringBuffer();
-    resultado.append("var calloutName='SE_Calendar_For_Org';\n\n");
-    resultado.append("var respuesta = new Array(");
+    StringBuilder result = new StringBuilder();
+    result.append("var calloutName='SE_Calendar_For_Org';\n\n");
+    result.append("var respuesta = new Array(");
     SEPeriodNoData[] tdv = null;
     if (strChanged.equals("inpadOrgId") && !strOrgId.equals("")) {
       // Update the Calendar
@@ -55,22 +73,22 @@ public class SE_Calendar_For_Org extends HttpSecureAppServlet {
         throw new ServletException(ex);
       }
     }
-    resultado.append("new Array(\"inpcCalendarId\", ");
+    result.append("new Array(\"inpcCalendarId\", ");
     if (tdv != null && tdv.length > 0) {
-      resultado.append("new Array(");
+      result.append("new Array(");
       for (int i = 0; i < tdv.length; i++) {
-        resultado.append("new Array(\"" + tdv[i].getField("id") + "\", \""
-            + tdv[i].getField("Name") + "\")");
+        result.append("new Array(\"" + tdv[i].getField("id") + "\", \"" + tdv[i].getField("Name")
+            + "\")");
         if (i < tdv.length - 1)
-          resultado.append(",\n");
+          result.append(",\n");
       }
-      resultado.append("\n)");
+      result.append("\n)");
     } else
-      resultado.append("null");
-    resultado.append("\n)");
+      result.append("null");
+    result.append("\n)");
 
-    resultado.append(");");
-    xmlDocument.setParameter("array", resultado.toString());
+    result.append(");");
+    xmlDocument.setParameter("array", result.toString());
     xmlDocument.setParameter("frameName", "appFrame");
     response.setContentType("text/html; charset=UTF-8");
     PrintWriter out = response.getWriter();
--- src/org/openbravo/erpCommon/ad_callouts/SE_Years_For_Calendar.java
+++ src/org/openbravo/erpCommon/ad_callouts/SE_Years_For_Calendar.java
@@ -1,3 +1,21 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html 
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License. 
+ * The Original Code is Openbravo ERP. 
+ * The Initial Developer of the Original Code is Openbravo SL 
+ * All portions are Copyright (C) 2001-2010 Openbravo SL 
+ * All Rights Reserved. 
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
+ */
 package org.openbravo.erpCommon.ad_callouts;
 
 import java.io.IOException;
@@ -43,9 +61,9 @@ public class SE_Years_For_Calendar extends HttpSecureAppServlet {
     XmlDocument xmlDocument = xmlEngine.readXmlTemplate(
         "org/openbravo/erpCommon/ad_callouts/CallOut").createXmlDocument();
 
-    StringBuffer resultado = new StringBuffer();
-    resultado.append("var calloutName='SE_Years_For_Calendar';\n\n");
-    resultado.append("var respuesta = new Array(");
+    StringBuilder result = new StringBuilder();
+    result.append("var calloutName='SE_Years_For_Calendar';\n\n");
+    result.append("var respuesta = new Array(");
 
     SEPeriodNoData[] tdv = null;
     if (strChanged.equals("inpcCalendarId") && !strCalendarId.equals("")) {
@@ -56,22 +74,22 @@ public class SE_Years_For_Calendar extends HttpSecureAppServlet {
         throw new ServletException(ex);
       }
     }
-    resultado.append("new Array(\"inpcYearId\", ");
+    result.append("new Array(\"inpcYearId\", ");
     if (tdv != null && tdv.length > 0) {
-      resultado.append("new Array(");
+      result.append("new Array(");
       for (int i = 0; i < tdv.length; i++) {
-        resultado.append("new Array(\"" + tdv[i].getField("id") + "\", \""
-            + tdv[i].getField("Name") + "\", \"" + (i == 0 ? "true" : "false") + "\")");
+        result.append("new Array(\"" + tdv[i].getField("id") + "\", \"" + tdv[i].getField("Name")
+            + "\", \"" + (i == 0 ? "true" : "false") + "\")");
         if (i < tdv.length - 1)
-          resultado.append(",\n");
+          result.append(",\n");
       }
-      resultado.append("\n)");
+      result.append("\n)");
     } else
-      resultado.append("null");
-    resultado.append("\n)");
+      result.append("null");
+    result.append("\n)");
 
-    resultado.append(");");
-    xmlDocument.setParameter("array", resultado.toString());
+    result.append(");");
+    xmlDocument.setParameter("array", result.toString());
     xmlDocument.setParameter("frameName", "appFrame");
     response.setContentType("text/html; charset=UTF-8");
     PrintWriter out = response.getWriter();
