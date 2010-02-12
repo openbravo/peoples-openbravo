538ee0b3e035c661760d3e467634d6bb39bd92ce
Martin Taal <martin.taal@openbravo.com>
2010-02-12 15:29:50 +0100

    Updated copyright year

--- src-test/org/openbravo/test/system/OBPropertiesProviderTest.java
+++ src-test/org/openbravo/test/system/OBPropertiesProviderTest.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SL 
- * All portions are Copyright (C) 2009 Openbravo SL 
+ * All portions are Copyright (C) 2010 Openbravo SL 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
