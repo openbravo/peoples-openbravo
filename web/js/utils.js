b596074577ba67eacd24ae75b22c7d2ed2831cf5
David Baz Fayos <david.baz@openbravo.com>
2010-02-17 12:17:55 +0100

    Related to issue 11979: Changed alert text
    --HG--
    extra : transplant_source : %C4G%FB%A6%B7%15%A2%15T%E0%C0%98%2B1%E4%DE%CB%08%10%DB

--- web/js/utils.js
+++ web/js/utils.js
@@ -70,7 +70,7 @@ function isDebugEnabled() {
 * Return a number that would be checked at the Login screen to know if the file is cached with the correct version
 */
 function getCurrentRevision() {
-  var number = '6440';
+  var number = '6446';
   return number;
 }
 
