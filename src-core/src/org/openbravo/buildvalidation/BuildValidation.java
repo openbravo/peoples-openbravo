672cc1d340fe07152bac23cebd85aeb799c4faaf
Antonio Moreno <antonio.moreno@openbravo.com>
2010-03-30 18:39:36 +0200

    [validation] Improvements to the BuildValidation abstract class
    --HG--
    extra : rebase_source : 74c905ba64c2855d7a190399aa2a8462982af106

--- src-core/src/org/openbravo/buildvalidation/BuildValidation.java
+++ src-core/src/org/openbravo/buildvalidation/BuildValidation.java
@@ -1,31 +1,32 @@
 package org.openbravo.buildvalidation;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.List;
 
 import org.openbravo.database.CPStandAlone;
 import org.openbravo.database.ConnectionProvider;
 
 public abstract class BuildValidation {
 
-  private ConnectionProvider cp = null;
-  private File basedir;
+  private ConnectionProvider cp;
 
-  public abstract ArrayList<String> execute();
+  public abstract List<String> execute();
 
-  public ConnectionProvider getConnectionProvider() {
-    if (cp == null) {
-      File f = new File("");
-      f = new File(f.getAbsolutePath());
-      File fProp = null;
-      if (new File("../../config/Openbravo.properties").exists())
-        fProp = new File("../../config/Openbravo.properties");
-      else if (new File("../config/Openbravo.properties").exists())
-        fProp = new File("../config/Openbravo.properties");
-      else if (new File("config/Openbravo.properties").exists())
-        fProp = new File("config/Openbravo.properties");
-      cp = new CPStandAlone(fProp.getAbsolutePath());
+  protected ConnectionProvider getConnectionProvider() {
+    if (cp != null) {
+      return cp;
     }
+
+    File f = new File("");
+    f = new File(f.getAbsolutePath());
+    File fProp = null;
+    if (new File("config/Openbravo.properties").exists())
+      fProp = new File("config/Openbravo.properties");
+    else if (new File("../config/Openbravo.properties").exists())
+      fProp = new File("../config/Openbravo.properties");
+    else if (new File("../../config/Openbravo.properties").exists())
+      fProp = new File("../../config/Openbravo.properties");
+    cp = new CPStandAlone(fProp.getAbsolutePath());
     return cp;
   }
 }
