ec89e891df7e67ca0c948cb55e1ecdd37c615d8e
Antonio Moreno <antonio.moreno@openbravo.com>
2010-03-31 11:34:10 +0200

    [validation] Added javadoc for the two abstract classes
    --HG--
    extra : rebase_source : 644d6bc696d96e42db2f3f331e48f5f82a2f75d8

--- src-core/src/org/openbravo/buildvalidation/BuildValidation.java
+++ src-core/src/org/openbravo/buildvalidation/BuildValidation.java
@@ -10,8 +10,26 @@ public abstract class BuildValidation {
 
   private ConnectionProvider cp;
 
+  /**
+   * This method must be implemented by the BuildValidations, and is used to define the actions that
+   * the script itself will take. This method will be automatically called by the
+   * BuildValidationHandler when the validation process is run (at the beginning of a rebuild,
+   * before the update.database task).
+   * 
+   * This method needs to return a list of error messages. If one or more error messages are
+   * provided, the build will stop, and the messages will be shown to the user. If an empty list is
+   * provided, the validation will be considered succesful, and the build will continue
+   * 
+   * @Return A list of error Strings
+   */
   public abstract List<String> execute();
 
+  /**
+   * This method returns a connection provider, which can be used to execute statements in the
+   * database
+   * 
+   * @return a ConnectionProvider
+   */
   protected ConnectionProvider getConnectionProvider() {
     if (cp != null) {
       return cp;
--- src-core/src/org/openbravo/modulescript/ModuleScript.java
+++ src-core/src/org/openbravo/modulescript/ModuleScript.java
@@ -7,8 +7,20 @@ public abstract class ModuleScript {
 
   private ConnectionProvider cp = null;
 
+  /**
+   * This method must be implemented by the ModuleScripts, and is used to define the actions that
+   * the script itself will take. This method will be automatically called by the
+   * ModuleScriptHandler when the update.database or the update.database.mod tasks are being
+   * executed
+   */
   public abstract void execute();
 
+  /**
+   * This method returns a connection provider, which can be used to execute statements in the
+   * database
+   * 
+   * @return a ConnectionProvider
+   */
   public ConnectionProvider getConnectionProvider() {
     if (cp == null) {
       cp = new CPStandAlone("config/Openbravo.properties");
