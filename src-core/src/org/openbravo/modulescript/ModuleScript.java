f541d552e27fa3d2e9ab097a0ff11f97e519fe0a
Antonio Moreno <antonio.moreno@openbravo.com>
2010-04-15 18:24:43 +0200

    [validation] Fixed potential issue when reading the Openbravo.properties file, in module scripts, in the getConnectionProvider() method
    --HG--
    extra : rebase_source : 9f80e62306b17d7117542bba570961025489cc8e

--- src-core/src/org/openbravo/modulescript/ModuleScript.java
+++ src-core/src/org/openbravo/modulescript/ModuleScript.java
@@ -11,6 +11,8 @@
  */
 package org.openbravo.modulescript;
 
+import java.io.File;
+
 import org.apache.log4j.Logger;
 import org.apache.tools.ant.BuildException;
 import org.openbravo.database.CPStandAlone;
@@ -42,9 +44,20 @@ public abstract class ModuleScript {
    * @return a ConnectionProvider
    */
   protected ConnectionProvider getConnectionProvider() {
-    if (cp == null) {
-      cp = new CPStandAlone("config/Openbravo.properties");
+    if (cp != null) {
+      return cp;
     }
+
+    File f = new File("");
+    f = new File(f.getAbsolutePath());
+    File fProp = null;
+    if (new File("config/Openbravo.properties").exists())
+      fProp = new File("config/Openbravo.properties");
+    else if (new File("../config/Openbravo.properties").exists())
+      fProp = new File("../config/Openbravo.properties");
+    else if (new File("../../config/Openbravo.properties").exists())
+      fProp = new File("../../config/Openbravo.properties");
+    cp = new CPStandAlone(fProp.getAbsolutePath());
     return cp;
   }
 
