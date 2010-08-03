6a67f6ee9f2fe14a9fbb1501aeba8bd3370ac93f
Asier Lostalé <asier.lostale@openbravo.com>
2010-08-03 16:22:35 +0200

    fixed issue 14086: Identify 3.0 instances in about box and in heartbeat
      About box now shows information about 3.0 template in case it is installed
      this information is also send in the heartbeats.
    
    --HG--
    extra : rebase_source : 00cf81618d86ebdb51493e6c750047321bf4563b

--- src/org/openbravo/erpCommon/ad_forms/About.java
+++ src/org/openbravo/erpCommon/ad_forms/About.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2001-2009 Openbravo SLU 
+ * All portions are Copyright (C) 2001-2010 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -21,6 +21,9 @@ package org.openbravo.erpCommon.ad_forms;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -29,7 +32,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.openbravo.base.secureApp.HttpSecureAppServlet;
 import org.openbravo.base.secureApp.VariablesSecureApp;
 import org.openbravo.dal.core.OBContext;
+import org.openbravo.data.FieldProvider;
+import org.openbravo.erpCommon.modules.ModuleTreeData;
 import org.openbravo.erpCommon.obps.ActivationKey;
+import org.openbravo.erpCommon.utility.FieldProviderFactory;
+import org.openbravo.erpCommon.utility.OBVersion;
 import org.openbravo.erpCommon.utility.Utility;
 import org.openbravo.xmlEngine.XmlDocument;
 
@@ -56,14 +63,9 @@ public class About extends HttpSecureAppServlet {
       ActivationKey ak = new ActivationKey();
       response.setContentType("text/html; charset=UTF-8");
       PrintWriter out = response.getWriter();
-      String discard[] = { "", "" };
-      AboutData[] data = AboutData.selectTranslators(this);
-      AboutData ver = AboutData.select(this);
+      String discard[] = { "" };
       XmlDocument xmlDocument = null;
-      if (data.length == 0) {
-        discard[0] = "discard";
-        data = AboutData.set();
-      }
+
       String licenseInfo = "";
       if (ActivationKey.isActiveInstance()) {
         licenseInfo = Utility.messageBD(this, "OPSLicensedTo", vars.getLanguage()) + " "
@@ -71,22 +73,21 @@ public class About extends HttpSecureAppServlet {
       } else {
         licenseInfo = Utility.messageBD(this, "OPSCommunityEdition", vars.getLanguage());
 
-        discard[1] = "paramOPSInfo";
+        discard[0] = "paramOPSInfo";
       }
       xmlDocument = xmlEngine.readXmlTemplate("org/openbravo/erpCommon/ad_forms/About", discard)
           .createXmlDocument();
 
-      String version = ver.version.substring(0, ver.version.lastIndexOf(".")) + " "
-          + ver.versionLabel;
+      OBVersion version = new OBVersion();
 
       xmlDocument.setParameter("directory", "var baseDirectory = \"" + strReplaceWith + "/\";\n");
       xmlDocument.setParameter("language", "defaultLang=\"" + vars.getLanguage() + "\";");
       xmlDocument.setParameter("theme", vars.getTheme());
       xmlDocument.setParameter("paramLicensedTo", licenseInfo);
-      xmlDocument.setData("structure1", data);
-      xmlDocument.setParameter("ver", version);
-      xmlDocument.setParameter("versionId", ver.versionId);
-      xmlDocument.setParameter("versionNo", ver.version);
+      xmlDocument.setParameter("ver", version.getMajorVersion() + " " + version.getMP());
+      xmlDocument.setParameter("versionId", version.getVersionId());
+      xmlDocument.setParameter("versionNo", version.getVersionNumber());
+      xmlDocument.setData("installedModules", getInstalledModules());
 
       if (ActivationKey.isActiveInstance()) {
         xmlDocument.setParameter("paraOPSPurpose", ak.getPurpose(vars.getLanguage()));
@@ -99,6 +100,63 @@ public class About extends HttpSecureAppServlet {
       OBContext.restorePreviousMode();
     }
 
+  }
+
+  /**
+   * Obtains a list of all modules installed in the instance.
+   * 
+   * @return
+   */
+  private FieldProvider[] getInstalledModules() {
+    List<HashMap<String, String>> installedModules = new ArrayList<HashMap<String, String>>();
+    try {
+      ModuleTreeData[] rootNodes = ModuleTreeData.select(this, "");
+      for (ModuleTreeData module : rootNodes) {
+        installedModules.add(getModuleHashMap(module, 1));
+        installedModules.addAll(getSubTree(module, 2));
+      }
+    } catch (ServletException e) {
+      log4j.error("Error obtaining installed modules", e);
+    }
+    return FieldProviderFactory.getFieldProviderArray(installedModules);
+  }
+
+  /**
+   * Obtains the list of the child modules for a parent module. This method is called from
+   * getInstalledModules and it composes a tree of inclusions.
+   */
+  private List<HashMap<String, String>> getSubTree(ModuleTreeData parentModule, int level) {
+    List<HashMap<String, String>> installedModules = new ArrayList<HashMap<String, String>>();
+    try {
+      ModuleTreeData[] modules = ModuleTreeData.selectSubTree(this, "", parentModule.nodeId);
+      for (ModuleTreeData module : modules) {
+        installedModules.add(getModuleHashMap(module, level));
+        installedModules.addAll(getSubTree(module, level + 1));
+      }
+    } catch (ServletException e) {
+      log4j.error("Error obtaining installed modules subtree module: "
+          + (parentModule != null ? parentModule.nodeId : "null"), e);
+    }
+    return installedModules;
+  }
+
+  /**
+   * Converts the module in a hashmap to be displayed as FieldProvider, it takes into account the
+   * level to insert HTML blank spaces.
+   */
+  private HashMap<String, String> getModuleHashMap(ModuleTreeData module, int level) {
+    HashMap<String, String> rt = new HashMap<String, String>();
+    rt.put("moduleName", module.modulename);
+    rt.put("moduleVersion", module.version
+        + (module.versionLabel == null || module.versionLabel.isEmpty() ? "" : " - "
+            + module.versionLabel));
+    rt.put("moduleAuthor", module.author == null || module.author.isEmpty() ? "-" : module.author);
+    String tab = "";
+    for (int i = 0; i < level; i++) {
+      tab += "&nbsp;&nbsp;&nbsp;";
+    }
+    rt.put("modTab", tab);
+    return rt;
   }
 
   public String getServletInfo() {
