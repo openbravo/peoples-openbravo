aaf8087188036b8982ecbe21a25b8fd9dfc3b916
Antonio Moreno <antonio.moreno@openbravo.com>
2010-09-07 11:55:32 +0200

    Fixed issue 14397. Check constraints will be disabled when importing reference data
    --HG--
    extra : rebase_source : dfd5a9229b88478476042d43894e53033e206c89

--- src/org/openbravo/service/db/ImportReferenceDataTask.java
+++ src/org/openbravo/service/db/ImportReferenceDataTask.java
@@ -21,11 +21,21 @@ package org.openbravo.service.db;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Properties;
+import java.util.Vector;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.ddlutils.Platform;
+import org.apache.ddlutils.PlatformFactory;
+import org.apache.ddlutils.model.Database;
 import org.apache.log4j.Logger;
 import org.openbravo.base.exception.OBException;
 import org.openbravo.dal.service.OBDal;
+import org.openbravo.ddlutils.task.DatabaseUtils;
+import org.openbravo.ddlutils.util.DBSMOBUtil;
 
 /**
  * Imports client data for the clients defined in the clients parameter from the
@@ -33,6 +43,19 @@ import org.openbravo.dal.service.OBDal;
  */
 public class ImportReferenceDataTask extends ReferenceDataTask {
   private static final Logger log = Logger.getLogger(ImportReferenceDataTask.class);
+  private Platform platform;
+  private Database xmlModel;
+
+  @Override
+  public void execute() {
+    try {
+      disableConstraints();
+    } catch (Exception e) {
+      log.info("Error disabling check constraint");
+    }
+    super.execute();
+    enableConstraints();
+  }
 
   @Override
   protected void doExecute() {
@@ -69,4 +92,41 @@ public class ImportReferenceDataTask extends ReferenceDataTask {
     OBDal.getInstance().commitAndClose();
   }
 
+  private void disableConstraints() throws FileNotFoundException, IOException {
+    String obDir = getProject().getBaseDir().toString() + "/../";
+    Properties obProp = new Properties();
+    obProp.load(new FileInputStream(new File(obDir, "config/Openbravo.properties")));
+    // We disable check constraints before inserting reference data
+    String driver = obProp.getProperty("bbdd.driver");
+    String url = obProp.getProperty("bbdd.rdbms").equals("POSTGRE") ? obProp
+        .getProperty("bbdd.url")
+        + "/" + obProp.getProperty("bbdd.sid") : obProp.getProperty("bbdd.url");
+    String user = obProp.getProperty("bbdd.user");
+    String password = obProp.getProperty("bbdd.password");
+    BasicDataSource datasource = DBSMOBUtil.getDataSource(driver, url, user, password);
+
+    platform = PlatformFactory.createNewPlatformInstance(datasource);
+
+    Vector<File> dirs = new Vector<File>();
+    dirs.add(new File(obDir, "/src-db/database/model/"));
+    File modules = new File(obDir, "/modules");
+
+    for (int j = 0; j < modules.listFiles().length; j++) {
+      final File dirF = new File(modules.listFiles()[j], "/src-db/database/model/");
+      if (dirF.exists()) {
+        dirs.add(dirF);
+      }
+    }
+    File[] fileArray = new File[dirs.size()];
+    for (int i = 0; i < dirs.size(); i++) {
+      fileArray[i] = dirs.get(i);
+    }
+    xmlModel = DatabaseUtils.readDatabase(fileArray);
+
+    platform.disableCheckConstraints(xmlModel);
+  }
+
+  private void enableConstraints() {
+    platform.enableCheckConstraints(xmlModel);
+  }
 }
