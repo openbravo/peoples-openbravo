9ea7f6aa9cea557ff54fadd7e5655cccf9d356e4
David Baz Fayos <david.baz@openbravo.com>
2010-11-23 22:39:28 +0100

    Improved popups visualization in IE while managing scrolls

--- web/js/searchs.js
+++ web/js/searchs.js
@@ -98,7 +98,7 @@ function windowSearch(strPage, strHeight, strWidth, strTop, strLeft, strWindow, 
   
   if (navigator.appName.indexOf("Netscape"))
     complementsNS4 = "alwaysRaised=1, dependent=1, directories=0, hotkeys=0, menubar=0, ";
-  var complements = complementsNS4 + "height=" + strHeight + ", width=" + strWidth + ", left=" + strLeft + ", top=" + strTop + ", screenX=" + strLeft + ", screenY=" + strTop + ", location=0, resizable=0, scrollbars=1, status=0, toolbar=0, titlebar=0";
+  var complements = complementsNS4 + "height=" + strHeight + ", width=" + strWidth + ", left=" + strLeft + ", top=" + strTop + ", screenX=" + strLeft + ", screenY=" + strTop + ", location=0, resizable=1, scrollbars=1, status=0, toolbar=0, titlebar=0";
   winSelector = window.open(strPage + ((auxField=="")?"":"?" + auxField), strWindow, complements);
   if (winSelector!=null) {
     /*if (hidden) window.focus();  //Useless since window.focus() after window.open it doesn't run in most browsers. Also in case of selectors with a non-existing search value can open the popup in the background in FF3.6
