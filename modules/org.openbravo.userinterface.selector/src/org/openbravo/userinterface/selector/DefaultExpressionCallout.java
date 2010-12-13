82479421c24f07e07ab89241ce84ed6aefc42967
Gorka Ion Damián <gorkaion.damian@openbravo.com>
2010-12-13 19:38:46 +0100

    Refactored DefaultExpressionCallout to implement JSExpressionCallout.

--- modules/org.openbravo.userinterface.selector/src/org/openbravo/userinterface/selector/DefaultExpressionCallout.java
+++ modules/org.openbravo.userinterface.selector/src/org/openbravo/userinterface/selector/DefaultExpressionCallout.java
@@ -18,120 +18,26 @@
  */
 package org.openbravo.userinterface.selector;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.openbravo.base.secureApp.HttpSecureAppServlet;
 import org.openbravo.base.secureApp.VariablesSecureApp;
-import org.openbravo.client.application.OBBindings;
-import org.openbravo.dal.core.OBContext;
-import org.openbravo.erpCommon.utility.Utility;
-import org.openbravo.xmlEngine.XmlDocument;
+import org.openbravo.client.application.JSExpressionCallout;
 
 /**
  * 
  * @author iperdomo
  */
-public class DefaultExpressionCallout extends HttpSecureAppServlet {
+public class DefaultExpressionCallout extends JSExpressionCallout {
 
   private static final long serialVersionUID = 1L;
-  private static final String CLEAR_MSG_CODE = "['MESSAGE','']";
-  private static final String JS_ERROR_MSG_PREFIX = "sun.org.mozilla.javascript.internal.EcmaError:";
 
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException,
-      ServletException {
-
-    VariablesSecureApp vars = new VariablesSecureApp(request);
-
-    if (vars.commandIn("DEFAULT")) {
-      try {
-        printPage(vars, request, response);
-      } catch (ServletException ex) {
-        pageErrorCallOut(response);
-      }
-    } else {
-      pageError(response);
-    }
-  }
-
-  private void printPage(VariablesSecureApp vars, HttpServletRequest request,
-      HttpServletResponse response) throws IOException, ServletException {
-
-    XmlDocument xmlDocument = xmlEngine.readXmlTemplate(
-        "org/openbravo/erpCommon/ad_callouts/CallOut").createXmlDocument();
-
-    xmlDocument.setParameter("array", getResponse(vars, request));
-    xmlDocument.setParameter("frameName", "appFrame");
-    response.setContentType("text/html; charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    out.println(xmlDocument.print());
-    out.close();
-  }
-
-  private String getResponse(VariablesSecureApp vars, HttpServletRequest request)
-      throws ServletException {
-
-    StringBuffer sb = new StringBuffer();
-    sb.append("var calloutName='").append(this.getClass().getName()).append("';\n");
-    sb.append("var respuesta = [");
-
+  protected String getExpression(VariablesSecureApp vars) {
     String inpLastFieldChanged = vars.getStringParameter("inpLastFieldChanged");
-    String js = "";
+    String expression = "";
     if ("inpdefaultExpression".equals(inpLastFieldChanged)) {
-      js = vars.getStringParameter("inpdefaultExpression");
+      expression = vars.getStringParameter("inpdefaultExpression");
     } else if ("inpfilterExpression".equals(inpLastFieldChanged)) {
-      js = vars.getStringParameter("inpfilterExpression");
+      expression = vars.getStringParameter("inpfilterExpression");
     }
-
-    if (js.equals("")) {
-      sb.append(CLEAR_MSG_CODE + "];\n");
-      return sb.toString();
-    }
-
-    sb.append(CLEAR_MSG_CODE);
-
-    try {
-      final ScriptEngineManager manager = new ScriptEngineManager();
-      final ScriptEngine engine = manager.getEngineByName("js");
-      final Map<String, String> parameters = getParameterMap(request);
-
-      engine.put("OB", new OBBindings(OBContext.getOBContext(), parameters, request
-          .getSession(false)));
-
-      Object result = engine.eval(js);
-
-      sb.append(", ['INFO','"
-          + Utility.messageBD(this, "OBUISEL_CALLOUT_JS_EXPR_RESULT", vars.getLanguage()) + " "
-          + (result) + "']");
-
-    } catch (Exception e) {
-      String errorMsg = e.getMessage().substring(JS_ERROR_MSG_PREFIX.length());
-      sb.append(", ['WARNING','"
-          + Utility.messageBD(this, "OBUISEL_CALLOUT_JS_EXPR_ERROR", vars.getLanguage()) + " "
-          + errorMsg + "']");
-    }
-    sb.append("];\n");
-    return sb.toString();
-
-  }
-
-  @SuppressWarnings("unchecked")
-  private Map<String, String> getParameterMap(HttpServletRequest request) {
-    final Map<String, String> parameterMap = new HashMap<String, String>();
-    for (Enumeration keys = request.getParameterNames(); keys.hasMoreElements();) {
-      final String key = (String) keys.nextElement();
-      parameterMap.put(key, request.getParameter(key));
-    }
-    return parameterMap;
+    return expression;
   }
 }
