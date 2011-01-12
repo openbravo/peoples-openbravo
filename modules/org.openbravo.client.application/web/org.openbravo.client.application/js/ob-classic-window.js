8e5b7cc8565a3d15e08a0c526bf9dd53f54fa953
Asier Lostalé <asier.lostale@openbravo.com>
2011-01-12 09:24:45 +0100

    [process] Opening classic window, replace ? with & in manual url
    --HG--
    extra : rebase_source : 5ff45e16d2abb844b8bad4354536bd6e7919a825

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/ob-classic-window.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/ob-classic-window.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010 Openbravo SLU
+ * All portions are Copyright (C) 2010-2011 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -107,6 +107,8 @@ isc.OBClassicWindow.addMethods({
       this.recordId +
       '&noprefs=true&Command=DIRECT&hideMenu=true';
     } else if (this.obManualURL && this.obManualURL !== '') {
+      this.obManualURL = this.obManualURL.replace('?', '&');
+      
       this.contentsURL = this.appURL + urlCharacter + 'url=' + this.obManualURL +
       '&noprefs=true&hideMenu=true';
       
