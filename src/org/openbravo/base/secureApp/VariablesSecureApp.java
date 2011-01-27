51f92566c22b4f782c028bcd2dd73b5b17117a63
Asier Lostalé <asier.lostale@openbravo.com>
2011-01-27 17:20:20 +0100

    fixed issue 15210: 2.50..3.00 compat skin should be enabled by default
    --HG--
    extra : rebase_source : 49df67390e661a41fc72fcbfbd61fecc563e749a

--- src/org/openbravo/base/secureApp/VariablesSecureApp.java
+++ src/org/openbravo/base/secureApp/VariablesSecureApp.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2001-2006 Openbravo S.L.U.
+ * Copyright (C) 2001-2011 Openbravo S.L.U.
  * Licensed under the Apache Software License version 2.0
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to  in writing,  software  distributed
@@ -224,7 +224,7 @@ public class VariablesSecureApp extends VariablesBase {
         }
       } finally {
         if (strTheme.isEmpty()) {
-          strTheme = "ltr/Default";
+          strTheme = "ltr/org.openbravo.userinterface.skin.250to300Comp/250to300Comp";
         }
         return strTheme;
       }
