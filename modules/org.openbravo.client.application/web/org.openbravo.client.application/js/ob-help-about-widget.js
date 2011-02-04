a662b4c0826f5a50590d3e52127d50a0bb574bd8
Martin Taal <martin.taal@openbravo.com>
2011-02-04 08:59:24 +0100

    Help about button registered in test registry, reformatted

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/ob-help-about-widget.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/ob-help-about-widget.js
@@ -4,98 +4,100 @@
  * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
  * Version 1.1  with a permitted attribution clause; you may not  use this
  * file except in compliance with the License. You  may  obtain  a copy of
- * the License at http://www.openbravo.com/legal/license.html 
+ * the License at http://www.openbravo.com/legal/license.html
  * Software distributed under the License  is  distributed  on  an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
  * License for the specific  language  governing  rights  and  limitations
- * under the License. 
- * The Original Code is Openbravo ERP. 
- * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2010-2011 Openbravo SLU 
- * All Rights Reserved. 
+ * under the License.
+ * The Original Code is Openbravo ERP.
+ * The Initial Developer of the Original Code is Openbravo SLU
+ * All portions are Copyright (C) 2010-2011 Openbravo SLU
+ * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
  */
-
 isc.ClassFactory.defineClass('OBHelpAbout', isc.OBQuickRun);
 
 // = OB Help About =
 // Provides the help/about widget in the navigation bar. It displays two
 // links: about and help. The help link will only be
 // displayed if the current selected window has a help view.
-isc.OBHelpAbout
-    .addProperties( {
+isc.OBHelpAbout.addProperties({
 
-      layoutProperties : {},
-
-      title : OB.I18N.getLabel('UINAVBA_Help'),
-
-      // Set to empty to prevent an icon from being displayed on the button.
-      src : '',
-
-      showTitle : true,
-
-      beforeShow : function() {
-        // determine if the help should be displayed or not
-      var tabPane = null, aboutLink = null, helpLink = null, helpView = null;
-
-      aboutLink = {
-        name: 'aboutLink',
-        editorType : 'link',
-        value : null,
-        showTitle : false,
-        target : 'javascript',
-        shouldSaveValue : false,
-        linkTitle : OB.I18N.getLabel('UINAVBA_About'),
-        handleClick : function() {
-          isc.OBQuickRun.hide();
-
-          OB.Layout.ClassicOBCompatibility.Popup.open('About', 620, 500,
-              OB.Application.contextUrl + '/ad_forms/about.html', '', window);
-        }
-      };
-
-      helpLink = {
-        name: 'helpLink',
-        editorType : 'link',
-        value : null,
-        showTitle : false,
-        target : 'javascript',
-        shouldSaveValue : false,
-        linkTitle : OB.I18N.getLabel('UINAVBA_Help'),
-        handleClick : function() {
-          isc.OBQuickRun.hide();
-          OB.Layout.ViewManager.openView(helpView.viewId, helpView);
-        }
-      };
-
-      // get the selected tab
-      var selectedTab = OB.MainView.TabSet.getSelectedTab();
-      if (selectedTab && selectedTab.pane && selectedTab.pane.getHelpView) {
-        tabPane = selectedTab.pane;
+  layoutProperties: {},
+  
+  title: OB.I18N.getLabel('UINAVBA_Help'),
+  
+  // Set to empty to prevent an icon from being displayed on the button.
+  src: '',
+  
+  showTitle: true,
+  
+  initWidget: function(){
+    OB.TestRegistry.register('org.openbravo.client.application.HelpAboutWidget', this);
+    return this.Super('initWidget', arguments);
+  },
+  
+  beforeShow: function(){
+    // determine if the help should be displayed or not
+    var tabPane = null, aboutLink = null, helpLink = null, helpView = null;
+    
+    aboutLink = {
+      name: 'aboutLink',
+      editorType: 'link',
+      value: null,
+      showTitle: false,
+      target: 'javascript',
+      shouldSaveValue: false,
+      linkTitle: OB.I18N.getLabel('UINAVBA_About'),
+      handleClick: function(){
+        isc.OBQuickRun.hide();
+        
+        OB.Layout.ClassicOBCompatibility.Popup.open('About', 620, 500, OB.Application.contextUrl + '/ad_forms/about.html', '', window);
       }
-      // determine if a help link should be shown or not
-      if (!tabPane) {
-        this.members[0].setFields( [ aboutLink ]);
+    };
+    
+    helpLink = {
+      name: 'helpLink',
+      editorType: 'link',
+      value: null,
+      showTitle: false,
+      target: 'javascript',
+      shouldSaveValue: false,
+      linkTitle: OB.I18N.getLabel('UINAVBA_Help'),
+      handleClick: function(){
+        isc.OBQuickRun.hide();
+        OB.Layout.ViewManager.openView(helpView.viewId, helpView);
+      }
+    };
+    
+    // get the selected tab
+    var selectedTab = OB.MainView.TabSet.getSelectedTab();
+    if (selectedTab && selectedTab.pane && selectedTab.pane.getHelpView) {
+      tabPane = selectedTab.pane;
+    }
+    // determine if a help link should be shown or not
+    if (!tabPane) {
+      this.members[0].setFields([aboutLink]);
+    } else {
+      helpView = tabPane.getHelpView();
+      if (!helpView) {
+        this.members[0].setFields([aboutLink]);
       } else {
-        helpView = tabPane.getHelpView();
-        if (!helpView) {
-          this.members[0].setFields( [ aboutLink ]);
-        } else {
-          this.members[0].setFields( [ helpLink, aboutLink ]);
-        }
-      }      
-      OB.TestRegistry.register('org.openbravo.client.application.HelpAboutForm.HelpLink', this.members[0].getField('helpLink'));
-      OB.TestRegistry.register('org.openbravo.client.application.HelpAboutForm.AboutLink', this.members[0].getField('aboutLink'));
-    },
-
-    members : [ isc.DynamicForm.create( {
-      numCols : 1,
-      initWidget: function() {
-        OB.TestRegistry.register('org.openbravo.client.application.HelpAboutForm', this);
-        return this.Super('initWidget', arguments);
+        this.members[0].setFields([helpLink, aboutLink]);
       }
-    }) ],
-
-    keyboardShortcutId : 'NavBar_OBHelpAbout'
-    });
+    }
+    OB.TestRegistry.register('org.openbravo.client.application.HelpAboutForm.HelpLink', this.members[0].getField('helpLink'));
+    OB.TestRegistry.register('org.openbravo.client.application.HelpAboutForm.AboutLink', this.members[0].getField('aboutLink'));
+  },
+  
+  members: [isc.DynamicForm.create({
+    numCols: 1,
+    initWidget: function(){
+      OB.TestRegistry.register('org.openbravo.client.application.HelpAboutForm', this);
+      return this.Super('initWidget', arguments);
+    }
+  })],
+  
+  keyboardShortcutId: 'NavBar_OBHelpAbout'
+});
