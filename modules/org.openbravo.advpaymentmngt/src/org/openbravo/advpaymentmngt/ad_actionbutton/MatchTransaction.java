4274e6bf4c1ed09ece8dca42b540ee525c579598
Eduardo Argal Guibert <eduardo.argal@openbravo.com>
2011-02-15 20:05:02 +0100

    Fixes bug 15991: Cancel button inside matching againts bankstatement implements no logic + Modigies some labels for amount fields inside bank statement line tab
    --HG--
    extra : rebase_source : 16d971577925eb5299938baef4f15c7e8327ca1a

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/ad_actionbutton/MatchTransaction.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/ad_actionbutton/MatchTransaction.java
@@ -24,6 +24,7 @@ import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -36,11 +37,13 @@ import org.hibernate.criterion.Expression;
 import org.openbravo.advpaymentmngt.dao.AdvPaymentMngtDao;
 import org.openbravo.advpaymentmngt.dao.MatchTransactionDao;
 import org.openbravo.advpaymentmngt.dao.TransactionsDao;
+import org.openbravo.advpaymentmngt.process.FIN_AddPayment;
 import org.openbravo.advpaymentmngt.utility.FIN_MatchedTransaction;
 import org.openbravo.advpaymentmngt.utility.FIN_MatchingTransaction;
 import org.openbravo.advpaymentmngt.utility.FIN_Utility;
 import org.openbravo.base.exception.OBException;
 import org.openbravo.base.filter.IsIDFilter;
+import org.openbravo.base.provider.OBProvider;
 import org.openbravo.base.secureApp.HttpSecureAppServlet;
 import org.openbravo.base.secureApp.VariablesSecureApp;
 import org.openbravo.base.session.OBPropertiesProvider;
@@ -48,16 +51,25 @@ import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.service.OBCriteria;
 import org.openbravo.dal.service.OBDal;
 import org.openbravo.data.FieldProvider;
+import org.openbravo.database.ConnectionProvider;
 import org.openbravo.erpCommon.utility.ComboTableData;
 import org.openbravo.erpCommon.utility.FieldProviderFactory;
 import org.openbravo.erpCommon.utility.OBError;
 import org.openbravo.erpCommon.utility.Utility;
+import org.openbravo.model.common.businesspartner.BusinessPartner;
+import org.openbravo.model.common.enterprise.DocumentType;
+import org.openbravo.model.common.enterprise.Organization;
 import org.openbravo.model.financialmgmt.payment.FIN_BankStatementLine;
 import org.openbravo.model.financialmgmt.payment.FIN_FinaccTransaction;
 import org.openbravo.model.financialmgmt.payment.FIN_FinancialAccount;
+import org.openbravo.model.financialmgmt.payment.FIN_Payment;
+import org.openbravo.model.financialmgmt.payment.FIN_PaymentMethod;
+import org.openbravo.model.financialmgmt.payment.FIN_PaymentScheduleDetail;
 import org.openbravo.model.financialmgmt.payment.FIN_Reconciliation;
+import org.openbravo.model.financialmgmt.payment.FIN_ReconciliationLineTemp;
 import org.openbravo.model.financialmgmt.payment.FIN_ReconciliationLine_v;
 import org.openbravo.model.financialmgmt.payment.MatchingAlgorithm;
+import org.openbravo.service.db.DalConnectionProvider;
 import org.openbravo.xmlEngine.XmlDocument;
 
 public class MatchTransaction extends HttpSecureAppServlet {
@@ -91,6 +103,7 @@ public class MatchTransaction extends HttpSecureAppServlet {
           OBContext.restorePreviousMode();
         }
       }
+      getSnapShot(reconciliation);
       if (MatchTransactionDao.getUnMatchedBankStatementLines(account).size() == 0
           && reconciledItems == 0) {
         OBError message = Utility.translateError(this, vars, vars.getLanguage(), Utility
@@ -137,6 +150,15 @@ public class MatchTransaction extends HttpSecureAppServlet {
           .getRequiredStringParameter("inpFinBankStatementLineId");
       unMatchBankStatementLine(response, strUnmatchBankStatementLineId);
 
+    } else if (vars.commandIn("CANCEL")) {
+      String strReconciliationId = vars.getRequiredStringParameter("inpfinReconciliationId");
+      restoreSnapShot(OBDal.getInstance().get(FIN_Reconciliation.class, strReconciliationId));
+      String strTabId = vars.getGlobalVariable("inpTabId", "MatchTransaction.adTabId");
+      String strWindowPath = Utility.getTabURL(strTabId, "R", true);
+      if (strWindowPath.equals(""))
+        strWindowPath = strDefaultServlet;
+
+      printPageClosePopUp(response, vars, strWindowPath);
     } else if (vars.commandIn("SAVE", "RECONCILE")) {
       OBContext.setAdminMode();
       try {
@@ -389,56 +411,15 @@ public class MatchTransaction extends HttpSecureAppServlet {
 
   private void unMatchBankStatementLine(HttpServletResponse response,
       String strUnmatchBankStatementLineId) throws IOException, ServletException {
-
-    OBContext.setAdminMode();
     try {
-      FIN_BankStatementLine bsline = OBDal.getInstance().get(FIN_BankStatementLine.class,
-          strUnmatchBankStatementLineId);
-
-      FIN_FinaccTransaction finTrans = bsline.getFinancialAccountTransaction();
-      if (finTrans != null) {
-        finTrans.setReconciliation(null);
-        bsline.setFinancialAccountTransaction(null);
-        OBDal.getInstance().save(finTrans);
-        OBDal.getInstance().flush();
-      }
-      bsline.setMatchingtype(null);
-      OBDal.getInstance().save(bsline);
-      OBDal.getInstance().flush();
-
-      if (finTrans != null) {
-        if (finTrans.getFinPayment() != null) {
-          finTrans.getFinPayment().setStatus(
-              (finTrans.getFinPayment().isReceipt()) ? "RDNC" : "PWNC");
-        }
-        boolean isReceipt = false;
-        if (finTrans.getFinPayment() != null) {
-          isReceipt = finTrans.getFinPayment().isReceipt();
-        } else {
-          isReceipt = finTrans.getDepositAmount().compareTo(finTrans.getPaymentAmount()) > 0;
-        }
-        finTrans.setStatus(isReceipt ? "RDNC" : "PWNC");
-        finTrans.setReconciliation(null);
-        OBDal.getInstance().save(finTrans);
-        OBDal.getInstance().flush();
-      }
-
-      // Execute un-matching logic defined by algorithm
-      MatchingAlgorithm ma = bsline.getBankStatement().getAccount().getMatchingAlgorithm();
-      FIN_MatchingTransaction matchingTransaction = new FIN_MatchingTransaction(ma
-          .getJavaClassName());
-      matchingTransaction.unmatch(finTrans);
-
+      unmatch(OBDal.getInstance().get(FIN_BankStatementLine.class, strUnmatchBankStatementLineId));
       response.setContentType("text/html; charset=UTF-8");
       PrintWriter out = response.getWriter();
       out.println("");
       out.close();
     } catch (Exception e) {
       throw new OBException(e);
-    } finally {
-      OBContext.restorePreviousMode();
     }
-
   }
 
   private FieldProvider[] getMatchedBankStatementLinesData(VariablesSecureApp vars,
@@ -609,6 +590,249 @@ public class MatchTransaction extends HttpSecureAppServlet {
       return (rec.size() != 0);
     } finally {
       OBContext.restorePreviousMode();
+    }
+  }
+
+  private void getSnapShot(FIN_Reconciliation reconciliation) {
+    // First remove old temp info if exists
+    OBContext.setAdminMode();
+    try {
+      List<FIN_ReconciliationLineTemp> oldTempLines = reconciliation
+          .getFINReconciliationLineTempList();
+      for (FIN_ReconciliationLineTemp oldtempLine : oldTempLines) {
+        OBDal.getInstance().remove(oldtempLine);
+        OBDal.getInstance().flush();
+      }
+      // Now copy info taken from the reconciliation when first opened
+      List<FIN_ReconciliationLine_v> reconciledlines = reconciliation
+          .getFINReconciliationLineVList();
+      for (FIN_ReconciliationLine_v reconciledLine : reconciledlines) {
+        FIN_ReconciliationLineTemp lineTemp = OBProvider.getInstance().get(
+            FIN_ReconciliationLineTemp.class);
+        lineTemp.setClient(reconciledLine.getClient());
+        lineTemp.setOrganization(reconciledLine.getOrganization());
+        lineTemp.setReconciliation(reconciledLine.getReconciliation());
+        lineTemp.setBankStatementLine(reconciledLine.getBankStatementLine());
+        if (reconciledLine.getFinancialAccountTransaction() != null
+            && reconciledLine.getFinancialAccountTransaction().isCreatedByAlgorithm()) {
+          if (reconciledLine.getFinancialAccountTransaction().getFinPayment() != null
+              && !reconciledLine.getFinancialAccountTransaction().getFinPayment()
+                  .isCreatedByAlgorithm()) {
+            lineTemp.setPayment(reconciledLine.getPayment());
+          } else if (reconciledLine.getFinancialAccountTransaction().getFinPayment()
+              .getFINPaymentDetailList().get(0).getFINPaymentScheduleDetailList() != null
+              && reconciledLine.getFinancialAccountTransaction().getFinPayment()
+                  .getFINPaymentDetailList().get(0).getFINPaymentScheduleDetailList().size() > 0
+              && (reconciledLine.getFinancialAccountTransaction().getFinPayment()
+                  .getFINPaymentDetailList().get(0).getFINPaymentScheduleDetailList().get(0)
+                  .getInvoicePaymentSchedule() != null || reconciledLine
+                  .getFinancialAccountTransaction().getFinPayment().getFINPaymentDetailList()
+                  .get(0).getFINPaymentScheduleDetailList().get(0).getOrderPaymentSchedule() != null)) {
+            lineTemp.setPaymentScheduleDetail(reconciledLine.getFinancialAccountTransaction()
+                .getFinPayment().getFINPaymentDetailList().get(0).getFINPaymentScheduleDetailList()
+                .get(0));
+          }
+        } else {
+          lineTemp.setFinancialAccountTransaction(reconciledLine.getFinancialAccountTransaction());
+        }
+        if (reconciledLine.getFinancialAccountTransaction().getFinPayment() != null) {
+          lineTemp.setPaymentDocumentno(reconciledLine.getFinancialAccountTransaction()
+              .getFinPayment().getDocumentNo());
+        }
+        lineTemp
+            .setMatched(reconciledLine.getBankStatementLine().getFinancialAccountTransaction() != null);
+        lineTemp.setMatchlevel(reconciledLine.getBankStatementLine().getMatchingtype());
+        OBDal.getInstance().save(lineTemp);
+        OBDal.getInstance().flush();
+      }
+    } finally {
+      OBContext.restorePreviousMode();
+    }
+  }
+
+  private void restoreSnapShot(FIN_Reconciliation reconciliation) {
+    OBContext.setAdminMode();
+    List<FIN_BankStatementLine> tempBSL = new ArrayList<FIN_BankStatementLine>();
+    List<FIN_BankStatementLine> finalBSL = new ArrayList<FIN_BankStatementLine>();
+    try {
+      // First get the list of BSL which were reconciled at the beginning (L1)
+      List<FIN_ReconciliationLineTemp> oldTempLines = reconciliation
+          .getFINReconciliationLineTempList();
+      for (FIN_ReconciliationLineTemp oldTempLine : oldTempLines) {
+        tempBSL.add(oldTempLine.getBankStatementLine());
+      }
+      // Then get the list of BSL reconciled at last (L2)
+      List<FIN_ReconciliationLine_v> oldReconciliationLines = reconciliation
+          .getFINReconciliationLineVList();
+      for (FIN_ReconciliationLine_v oldReconciliationLine : oldReconciliationLines) {
+        finalBSL.add(oldReconciliationLine.getBankStatementLine());
+      }
+      // Unmatch L2-L1
+      finalBSL.removeAll(tempBSL);
+      unmatch(finalBSL);
+      // Match L1
+      match(reconciliation);
+    } finally {
+      OBContext.restorePreviousMode();
+    }
+  }
+
+  private void unmatch(List<FIN_BankStatementLine> toUnmatch) {
+    for (FIN_BankStatementLine bsl : toUnmatch) {
+      unmatch(bsl);
+    }
+  }
+
+  private void unmatch(FIN_BankStatementLine bsline) {
+    OBContext.setAdminMode();
+    try {
+      FIN_FinaccTransaction finTrans = bsline.getFinancialAccountTransaction();
+      if (finTrans != null) {
+        finTrans.setReconciliation(null);
+        bsline.setFinancialAccountTransaction(null);
+        OBDal.getInstance().save(finTrans);
+        OBDal.getInstance().flush();
+      }
+      bsline.setMatchingtype(null);
+      OBDal.getInstance().save(bsline);
+      OBDal.getInstance().flush();
+
+      if (finTrans != null) {
+        if (finTrans.getFinPayment() != null) {
+          finTrans.getFinPayment().setStatus(
+              (finTrans.getFinPayment().isReceipt()) ? "RDNC" : "PWNC");
+        }
+        boolean isReceipt = false;
+        if (finTrans.getFinPayment() != null) {
+          isReceipt = finTrans.getFinPayment().isReceipt();
+        } else {
+          isReceipt = finTrans.getDepositAmount().compareTo(finTrans.getPaymentAmount()) > 0;
+        }
+        finTrans.setStatus(isReceipt ? "RDNC" : "PWNC");
+        finTrans.setReconciliation(null);
+        OBDal.getInstance().save(finTrans);
+        OBDal.getInstance().flush();
+      }
+      // Execute un-matching logic defined by algorithm
+      MatchingAlgorithm ma = bsline.getBankStatement().getAccount().getMatchingAlgorithm();
+      FIN_MatchingTransaction matchingTransaction = new FIN_MatchingTransaction(ma
+          .getJavaClassName());
+      matchingTransaction.unmatch(finTrans);
+    } catch (Exception e) {
+      throw new OBException(e);
+    } finally {
+      OBContext.restorePreviousMode();
+    }
+  }
+
+  private void match(FIN_Reconciliation reconciliation) {
+    List<FIN_ReconciliationLineTemp> snapShotList = reconciliation
+        .getFINReconciliationLineTempList();
+    for (FIN_ReconciliationLineTemp toMatch : snapShotList) {
+      FIN_BankStatementLine bsl = toMatch.getBankStatementLine();
+      if (bsl.getFinancialAccountTransaction() != null) {
+        continue;
+      }
+      FIN_FinaccTransaction transaction = getTransactionFromTemp(toMatch);
+      bsl.setFinancialAccountTransaction(transaction);
+      bsl.setMatchingtype(toMatch.getMatchlevel());
+      transaction.setStatus("RPPC");
+      transaction.setReconciliation(reconciliation);
+      if (transaction.getFinPayment() != null) {
+        transaction.getFinPayment().setStatus("RPPC");
+      }
+      OBDal.getInstance().save(transaction);
+      OBDal.getInstance().save(bsl);
+      OBDal.getInstance().flush();
+    }
+  }
+
+  private FIN_FinaccTransaction getTransactionFromTemp(FIN_ReconciliationLineTemp toMatch) {
+    if (toMatch.getFinancialAccountTransaction() != null) {
+      return toMatch.getFinancialAccountTransaction();
+    } else if (toMatch.getPayment() != null) {
+      AdvPaymentMngtDao dao = new AdvPaymentMngtDao();
+      return dao.getFinancialTransaction(toMatch.getPayment());
+    } else {
+      // get DocumentNo
+      String strPaymentDocumentNo = toMatch.getPaymentDocumentno();
+      // If the payment remains in the system, it is used
+      FIN_Payment alreadyExistingPayment = getAlreadyExistingPayment(strPaymentDocumentNo, toMatch
+          .getBankStatementLine().getOrganization());
+      if (alreadyExistingPayment != null) {
+        AdvPaymentMngtDao dao = new AdvPaymentMngtDao();
+        return dao.getFinancialTransaction(alreadyExistingPayment);
+      }
+      // Create a payment and a transaction to match
+      AdvPaymentMngtDao dao = new AdvPaymentMngtDao();
+      FIN_PaymentScheduleDetail paymentScheduleDetail = toMatch.getPaymentScheduleDetail();
+      BigDecimal amount = toMatch.getBankStatementLine().getCramount().subtract(
+          toMatch.getBankStatementLine().getDramount());
+      boolean isReceipt = amount.signum() > 0;
+      BusinessPartner bp = paymentScheduleDetail == null ? toMatch.getBankStatementLine()
+          .getBusinessPartner()
+          : (paymentScheduleDetail.getInvoicePaymentSchedule() != null ? paymentScheduleDetail
+              .getInvoicePaymentSchedule().getInvoice().getBusinessPartner()
+              : (paymentScheduleDetail.getOrderPaymentSchedule() != null ? paymentScheduleDetail
+                  .getOrderPaymentSchedule().getOrder().getBusinessPartner()
+                  : paymentScheduleDetail.getPaymentDetails().getFinPayment().getBusinessPartner()));
+      FIN_PaymentMethod pm = paymentScheduleDetail == null ? (isReceipt ? bp.getPaymentMethod()
+          : bp.getPOPaymentMethod())
+          : (paymentScheduleDetail.getInvoicePaymentSchedule() != null ? paymentScheduleDetail
+              .getInvoicePaymentSchedule().getInvoice().getPaymentMethod() : (paymentScheduleDetail
+              .getOrderPaymentSchedule() != null ? paymentScheduleDetail.getOrderPaymentSchedule()
+              .getOrder().getPaymentMethod() : paymentScheduleDetail.getPaymentDetails()
+              .getFinPayment().getPaymentMethod()));
+      DocumentType docType = FIN_Utility.getDocumentType(toMatch.getBankStatementLine()
+          .getOrganization(), isReceipt ? "ARR" : "APP");
+      HashMap<String, BigDecimal> hm = new HashMap<String, BigDecimal>();
+      List<FIN_PaymentScheduleDetail> psdList = new ArrayList<FIN_PaymentScheduleDetail>();
+      if (paymentScheduleDetail != null) {
+        hm.put(paymentScheduleDetail.getId(), amount);
+        psdList.add(paymentScheduleDetail);
+      }
+      if ("".equals(strPaymentDocumentNo))
+        strPaymentDocumentNo = FIN_Utility.getDocumentNo(docType,
+            docType.getTable() != null ? docType.getTable().getDBTableName() : "");
+      FIN_Payment payment = FIN_AddPayment.savePayment(null, isReceipt, docType,
+          strPaymentDocumentNo, bp, pm, toMatch.getBankStatementLine().getBankStatement()
+              .getAccount(), amount.abs().toString(), toMatch.getBankStatementLine()
+              .getTransactionDate(), toMatch.getBankStatementLine().getOrganization(), toMatch
+              .getBankStatementLine().getReferenceNo(), psdList, hm, false, false);
+      // Flag payment as created by algorithm
+      payment.setCreatedByAlgorithm(true);
+      OBDal.getInstance().save(payment);
+      OBDal.getInstance().flush();
+      try {
+        ConnectionProvider conn = new DalConnectionProvider();
+        FIN_AddPayment.processPayment(new VariablesSecureApp(OBContext.getOBContext().getUser()
+            .getId(), OBContext.getOBContext().getCurrentClient().getId(), OBContext.getOBContext()
+            .getCurrentOrganization().getId(), OBContext.getOBContext().getRole().getId()), conn,
+            "P", payment);
+      } catch (Exception e) {
+        return null;
+      }
+      FIN_FinaccTransaction transaction = dao.getFinancialTransaction(payment);
+      // Flag transaction as created by algorithm
+      transaction.setCreatedByAlgorithm(true);
+      OBDal.getInstance().save(transaction);
+      OBDal.getInstance().flush();
+      TransactionsDao.process(transaction);
+      return transaction;
+    }
+  }
+
+  private FIN_Payment getAlreadyExistingPayment(String strPaymentDocumentNo,
+      Organization organization) {
+    final OBCriteria<FIN_Payment> obc = OBDal.getInstance().createCriteria(FIN_Payment.class);
+    obc.add(Expression.eq(FIN_Payment.PROPERTY_DOCUMENTNO, strPaymentDocumentNo));
+    obc.add(Expression.eq(FIN_Payment.PROPERTY_ORGANIZATION, organization));
+    obc.setMaxResults(1);
+    final List<FIN_Payment> payments = obc.list();
+    if (payments.size() == 0) {
+      return null;
+    } else {
+      return payments.get(0);
     }
   }
 
