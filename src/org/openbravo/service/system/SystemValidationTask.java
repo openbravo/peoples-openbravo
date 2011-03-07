3fcaeccf89590224f8d542ecb33c8be37088ded4
Asier Lostalé <asier.lostale@openbravo.com>
2011-03-07 09:37:30 +0100

    fixed bug 15870: Do not include same module more than once
      When packaging module in obx file, it is checked the same module is not included
      more than once.
    
    --HG--
    extra : rebase_source : f9142db91ad08c453b93d0b5b258348d408cbc00

--- src/org/openbravo/service/system/SystemValidationTask.java
+++ src/org/openbravo/service/system/SystemValidationTask.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2009 Openbravo SLU 
+ * All portions are Copyright (C) 2009-2011 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -102,7 +102,7 @@ public class SystemValidationTask extends DalInitializingTask {
         log.warn("Validation successfull no warnings or errors");
       } else {
         final String errors = SystemService.getInstance().logValidationResult(log, result);
-        if (failOnError) {
+        if (!result.getErrors().isEmpty() && failOnError) {
           throw new OBException(errors);
         }
       }
