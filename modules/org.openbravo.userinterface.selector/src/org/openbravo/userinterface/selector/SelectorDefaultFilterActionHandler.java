a0599811c8688d229e351fd5bbd63545776fff6b
Martin Taal <martin.taal@openbravo.com>
2011-03-07 23:41:13 +0100

    Removed unused code

--- modules/org.openbravo.userinterface.selector/src/org/openbravo/userinterface/selector/SelectorDefaultFilterActionHandler.java
+++ modules/org.openbravo.userinterface.selector/src/org/openbravo/userinterface/selector/SelectorDefaultFilterActionHandler.java
@@ -29,16 +29,9 @@ import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONObject;
 import org.hibernate.criterion.Expression;
-import org.openbravo.base.model.Entity;
-import org.openbravo.base.model.ModelProvider;
-import org.openbravo.base.model.Property;
-import org.openbravo.base.model.Reference;
-import org.openbravo.base.model.domaintype.DomainType;
-import org.openbravo.base.util.Check;
 import org.openbravo.client.application.OBBindings;
 import org.openbravo.client.kernel.BaseActionHandler;
 import org.openbravo.client.kernel.KernelConstants;
-import org.openbravo.dal.core.DalUtil;
 import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.service.OBCriteria;
 import org.openbravo.dal.service.OBDal;
@@ -114,30 +107,5 @@ public class SelectorDefaultFilterActionHandler extends BaseActionHandler {
       params.put(key, (String) parameters.get(key));
     }
     return params;
-  }
-
-  private DomainType getDomainType(SelectorField selectorField) {
-    if (selectorField.getObuiselSelector().getTable() != null
-        && selectorField.getProperty() != null) {
-      final String entityName = selectorField.getObuiselSelector().getTable().getName();
-      final Entity entity = ModelProvider.getInstance().getEntity(entityName);
-      final Property property = DalUtil.getPropertyFromPath(entity, selectorField.getProperty());
-      Check.isNotNull(property, "Property " + selectorField.getProperty() + " not found in Entity "
-          + entity);
-      return property.getDomainType();
-    } else if (selectorField.getObuiselSelector().getTable() != null
-        && selectorField.getObuiselSelector().isCustomQuery()
-        && selectorField.getReference() != null) {
-      return getDomainType(selectorField.getReference().getId());
-    } else if (selectorField.getObserdsDatasourceField().getReference() != null) {
-      return getDomainType(selectorField.getObserdsDatasourceField().getReference().getId());
-    }
-    return null;
-  }
-
-  private DomainType getDomainType(String referenceId) {
-    final Reference reference = ModelProvider.getInstance().getReference(referenceId);
-    Check.isNotNull(reference, "No reference found for referenceid " + referenceId);
-    return reference.getDomainType();
   }
 }
