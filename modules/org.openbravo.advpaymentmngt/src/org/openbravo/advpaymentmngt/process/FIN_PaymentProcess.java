9d3fe247fe2bbcfa159755afac4cdca2b52bd2fd
Ben Sommerville <ben.sommerville@eintel.com.au>
2011-03-12 20:19:14 +1100

    [aprm multi] - Payment in/out add exchange rate & converted amount to add payment popup

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/process/FIN_PaymentProcess.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/process/FIN_PaymentProcess.java
@@ -280,6 +280,7 @@ public class FIN_PaymentProcess implements org.openbravo.scheduling.Process {
         OBDal.getInstance().flush();
         payment.setWriteoffAmount(BigDecimal.ZERO);
         payment.setAmount(BigDecimal.ZERO);
+        payment.setFinancialTransactionAmount(BigDecimal.ZERO);
 
         if (payment.getGeneratedCredit().compareTo(BigDecimal.ZERO) == 0
             && payment.getUsedCredit().compareTo(BigDecimal.ZERO) == 1)
