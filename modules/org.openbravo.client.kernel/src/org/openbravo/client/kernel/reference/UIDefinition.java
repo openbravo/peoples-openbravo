42b5bda4b39348c56811e1f7443f00282e9459cd
Antonio Moreno <antonio.moreno@openbravo.com>
2011-03-21 09:59:15 +0100

    Fixed issue 16392. @#Date@ defaults will be computed correctly in case of non-standard date format
    --HG--
    extra : transplant_source : %0F%CA%97%26%9E%A57%C2oK%DBA%7B%C1%12%B8%24%14q%22

--- modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/reference/UIDefinition.java
+++ modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/reference/UIDefinition.java
@@ -23,6 +23,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -162,7 +163,9 @@ public abstract class UIDefinition {
         if (defaultS == null || defaultS.equals("\"\"")) {
           defaultS = "";
         }
-        if (!defaultS.startsWith("@SQL=")) {
+        if (defaultS.equalsIgnoreCase("@#Date@")) {
+          columnValue = this.convertToClassicString(new Date());
+        } else if (!defaultS.startsWith("@SQL=")) {
           columnValue = Utility.getDefault(new DalConnectionProvider(false), rq
               .getVariablesSecureApp(), field.getColumn().getDBColumnName(), defaultS, field
               .getTab().getWindow().getId(), "");
