4fecc320940cad461f91b6f1b4a9d66c5e91a71b
David Baz Fayos <david.baz@openbravo.com>
2011-04-20 21:29:00 +0200

    Fixed issue 16561: Now Dojo date inputs autocomplete the date if required

--- web/js/utils.js
+++ web/js/utils.js
@@ -95,7 +95,7 @@ function isDebugEnabled() {
 * Return a number that would be checked at the Login screen to know if the file is cached with the correct version
 */
 function getCurrentRevision() {
-  var number = '11564';
+  var number = '11622';
   return number;
 }
 
