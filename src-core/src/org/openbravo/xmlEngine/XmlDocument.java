24ecef71f6b2c481fac690fdc1453781e49c25ea
Asier Lostalé <asier.lostale@openbravo.com>
2011-05-17 15:41:26 +0200

    fixed bug 16493: Fixed translation of 2.50 forms within non org.* modules
    --HG--
    extra : rebase_source : eebda952beea5286e60e46b097698adfbb6ccca2

--- src-core/src/org/openbravo/xmlEngine/XmlDocument.java
+++ src-core/src/org/openbravo/xmlEngine/XmlDocument.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2001-2010 Openbravo S.L.U.
+ * Copyright (C) 2001-2011 Openbravo S.L.U.
  * Licensed under the Apache Software License version 2.0
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to  in writing,  software  distributed
@@ -230,7 +230,8 @@ public class XmlDocument implements XmlComponentValue {
       final TranslationHandler handler = new TranslationHandler(
           this.xmlTemplate.xmlEngine.connProvider, this);
       String baseDirectory = xmlTemplate.strName.substring(0, xmlTemplate.strName.lastIndexOf("/"))
-          .replace("designorg/", "/org/");
+          .replaceFirst("design", "/");
+
       String templateFileName = xmlTemplate.configuration.strTemplate;
       String absoluteTemplateFile = baseDirectory + "/" + templateFileName;
       handler.setFileName(absoluteTemplateFile);
