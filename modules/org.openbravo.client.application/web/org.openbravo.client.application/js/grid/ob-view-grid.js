0315ccd2d08b42e942669d3ea77b289a12d809fe
Martin Taal <martin.taal@openbravo.com>
2011-06-10 13:07:27 +0200

    Fixes issue 17593: IE9: Location/Address tab is not shown Added variable declaration

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-view-grid.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-view-grid.js
@@ -302,7 +302,7 @@ isc.OBViewGrid.addProperties({
   },
   
   draw: function() {
-    var drawnBefore = this.isDrawn(), form;
+    var drawnBefore = this.isDrawn(), form, item;
     this.Super('draw', arguments);
     // set the focus in the filter editor
     if (this.view && this.view.isActiveView() && !drawnBefore && this.isVisible() &&
