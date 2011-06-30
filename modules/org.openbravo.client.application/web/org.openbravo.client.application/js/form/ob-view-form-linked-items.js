fd6b7d4596052d133fade88e19bf83f6ccdbf2bf
David Baz Fayos <david.baz@openbravo.com>
2011-07-01 00:08:53 +0200

    [SKIN] Improved 'Linked Items' link cell style

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/ob-view-form-linked-items.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/ob-view-form-linked-items.js
@@ -244,7 +244,7 @@ isc.OBLinkedItemLayout.addProperties({
     this.linkedItemListGrid = isc.OBGrid.create({
       width: '50%',
       height: 300,
-      baseStyle: 'OBLinksGrid',
+      baseStyle: 'OBGridLinkCell',
       dataSource: this.linkedItemDS,
       autoFetchData: true,
       showFilterEditor: true,
