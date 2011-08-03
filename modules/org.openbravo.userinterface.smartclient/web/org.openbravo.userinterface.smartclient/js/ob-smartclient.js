7796a3fc03602016d403026584a2e822ca81b992
Martin Taal <martin.taal@openbravo.com>
2011-08-03 13:14:30 +0200

    Fixed a typo in calling the errorhandler of the RPCManager

--- modules/org.openbravo.userinterface.smartclient/web/org.openbravo.userinterface.smartclient/js/ob-smartclient.js
+++ modules/org.openbravo.userinterface.smartclient/web/org.openbravo.userinterface.smartclient/js/ob-smartclient.js
@@ -308,7 +308,7 @@ isc.RPCManager.addClassProperties({
   _handleError: isc.RPCManager.getPrototype().handleError,
   handleError : function (response, request) {
     if (!request.willHandleError) {
-      isc.RPCManager._handleError(response, request);
+      isc.RPCManager.handleError(response, request);
     }
   }
 });
