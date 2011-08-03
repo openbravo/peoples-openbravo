239f831d4e877ff190abe01f749b3e411833fabb
Antonio Moreno <antonio.moreno@openbravo.com>
2011-08-03 17:22:44 +0200

    [attachments] Changed name for better consistency
    --HG--
    extra : rebase_source : e9fd700d9218d6e25c2a0340c391f8c639766edf

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/window/AttachmentsAH.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/window/AttachmentsAH.java
@@ -112,7 +112,7 @@ public class AttachmentsAH extends BaseActionHandler {
         attachmentobj.put("id", attachment.getId());
         attachmentobj.put("name", attachment.getName());
         attachmentobj.put("age", (new Date().getTime() - attachment.getUpdated().getTime()));
-        attachmentobj.put("createdby", attachment.getUpdatedBy().getName());
+        attachmentobj.put("updatedby", attachment.getUpdatedBy().getName());
       } catch (Exception e) {
         throw new OBException("Error while reading attachments:", e);
       }
