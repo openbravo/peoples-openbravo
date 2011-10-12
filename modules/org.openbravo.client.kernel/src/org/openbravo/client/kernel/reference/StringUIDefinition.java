afbe493fff55033603bdcfea407d4ed431813463
Martin Taal <martin.taal@openbravo.com>
2011-10-12 11:29:24 +0200

    Fixes issue 18748: Field value gets truncated when editing in grid mode length not necessary in grid fields as truncation happens automatically

--- modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/reference/StringUIDefinition.java
+++ modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/reference/StringUIDefinition.java
@@ -52,7 +52,6 @@ public class StringUIDefinition extends UIDefinition {
     if (field.getColumn().getDBColumnName().compareToIgnoreCase("documentno") == 0) {
       length = new Long(20);
     }
-    return getShowHoverGridFieldSettings(field) + ", length: " + length
-        + super.getGridFieldProperties(field);
+    return getShowHoverGridFieldSettings(field) + super.getGridFieldProperties(field);
   }
 }
