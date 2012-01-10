f65b820cb314225e1c15e5c9ebf7aa1dc255c7b0
Asier Lostalé <asier.lostale@openbravo.com>
2012-01-10 15:16:05 +0100

    fixed bug 19382: implemented 'triggers autosave' in buttons
    --HG--
    extra : rebase_source : 427d892e4b6e12f6eebbc1be7241048e8d13a0f2

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-view-grid.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-view-grid.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2011 Openbravo SLU
+ * All portions are Copyright (C) 2010-2012 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -1705,7 +1705,8 @@ isc.OBViewGrid.addProperties({
   },
   
   createNewRecordForEditing: function(rowNum){
-    // note: the id is dummy, will be replaced when the save succeeds 
+    // note: the id is dummy, will be replaced when the save succeeds, 
+    // it MUST start with _ to identify it is a temporary id 
     var record = {
       _new: true,
       id: '_' + new Date().getTime()
