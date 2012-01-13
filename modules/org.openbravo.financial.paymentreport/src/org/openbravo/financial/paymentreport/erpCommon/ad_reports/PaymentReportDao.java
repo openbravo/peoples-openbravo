480e0739cfa6e3e8a9c8603db85a311dc5e140d5
Eduardo Argal Guibert <eduardo.argal@openbravo.com>
2012-01-13 18:27:34 +0100

    [Payment report]: propper status shorting and payment method in xls
    --HG--
    extra : rebase_source : efa5107adc708e27bf12072615637adb2ccee80e

--- modules/org.openbravo.financial.paymentreport/src/org/openbravo/financial/paymentreport/erpCommon/ad_reports/PaymentReportDao.java
+++ modules/org.openbravo.financial.paymentreport/src/org/openbravo/financial/paymentreport/erpCommon/ad_reports/PaymentReportDao.java
@@ -111,7 +111,8 @@ public class PaymentReportDao {
     OBContext.setAdminMode();
     try {
 
-      hsqlScript.append(" as fpsd ");
+      hsqlScript
+          .append("select fpsd.id, (select a.sequenceNumber from ADList a where a.reference.id = '575BCB88A4694C27BC013DE9C73E6FE7' and a.searchKey = coalesce(pay.status, 'RPAP')) as a from FIN_Payment_ScheduleDetail as fpsd ");
       hsqlScript.append(" left outer join fpsd.paymentDetails.finPayment pay");
       hsqlScript.append(" left outer join pay.businessPartner paybp");
       hsqlScript.append(" left outer join paybp.businessPartnerCategory paybpc");
@@ -336,7 +337,7 @@ public class PaymentReportDao {
         hsqlScript.append("), ");
       }
 
-      hsqlScript.append(" coalesce(pay.");
+      hsqlScript.append(" a, coalesce(pay.");
       hsqlScript.append(FIN_Payment.PROPERTY_STATUS);
       hsqlScript.append(", 'RPAP')");
 
@@ -383,11 +384,25 @@ public class PaymentReportDao {
       hsqlScript.append(FIN_PaymentScheduleDetail.PROPERTY_INVOICEPAYMENTSCHEDULE);
       hsqlScript.append(".");
       hsqlScript.append(FIN_PaymentSchedule.PROPERTY_ID);
+      final Session session = OBDal.getInstance().getSession();
+      final Query query = session.createQuery(hsqlScript.toString());
 
-      final OBQuery<FIN_PaymentScheduleDetail> obqPSD = OBDal.getInstance().createQuery(
-          FIN_PaymentScheduleDetail.class, hsqlScript.toString(), parameters);
-      obqPSD.setFilterOnReadableOrganization(false);
-      java.util.List<FIN_PaymentScheduleDetail> obqPSDList = obqPSD.list();
+      boolean firstMember = true;
+      java.util.List<FIN_PaymentScheduleDetail> obqPSDList = new ArrayList<FIN_PaymentScheduleDetail>();
+      for (Object resultObject : query.list()) {
+        if (resultObject.getClass().isArray()) {
+          final Object[] values = (Object[]) resultObject;
+          for (Object value : values) {
+            if (firstMember) {
+              obqPSDList.add(OBDal.getInstance().get(FIN_PaymentScheduleDetail.class,
+                  (String) value));
+              firstMember = false;
+            } else {
+              firstMember = true;
+            }
+          }
+        }
+      }
       data = FieldProviderFactory.getFieldProviderArray(obqPSDList);
 
       FIN_PaymentScheduleDetail[] FIN_PaymentScheduleDetail = new FIN_PaymentScheduleDetail[0];
@@ -484,7 +499,8 @@ public class PaymentReportDao {
               .getCurrency();
           FieldProviderFactory.setField(data[i], "TRANS_CURRENCY", transCurrency.getISOCode());
           // paymentMethod
-          FieldProviderFactory.setField(data[i], "PAYMENT_METHOD", "");
+          FieldProviderFactory.setField(data[i], "PAYMENT_METHOD", FIN_PaymentScheduleDetail[i]
+              .getInvoicePaymentSchedule().getFinPaymentmethod().getIdentifier());
           // payment
           FieldProviderFactory.setField(data[i], "PAYMENT", "");
           // payment_id
