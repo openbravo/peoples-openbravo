e8120c55ba79266138c7c41e4c75a6053aacc11c
Javier Etxarri <javier.echarri@openbravo.com>
2012-01-23 16:43:22 +0100

    issue 19341: number of Alerts do not match. Changed the verification condition.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/alert-management/ob-alert-grid.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/alert-management/ob-alert-grid.js
@@ -119,7 +119,7 @@ isc.OBAlertGrid.addProperties({
     
     OB.Datasource.get('ADAlert', this, null, true);
     
-    if (OB.AlertManager && OB.AlertManager.lastData.data){
+    if (OB.AlertManager && OB.AlertManager.lastData && isc.is.Array(OB.AlertManager.lastData.data)){
         this.dataPageSize = OB.AlertManager.lastData.data.length;
     }
 
