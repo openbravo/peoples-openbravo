794b4f4830fe80a2fb404d9a07d3545fc5e86ae1
Martin Taal <martin.taal@openbravo.com>
2012-02-23 10:43:28 +0100

    Related to issue 19689: It is not possible to change Role twice in a row backout of changeset: 3565776dbdb7 Rolled back change: Do the reload of the window in a separate thread independent of the isc call thread

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/navbar/ob-user-profile-widget.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/navbar/ob-user-profile-widget.js
@@ -303,8 +303,6 @@ isc.OBUserProfile.addProperties({
         OB.RemoteCallManager.call(this.widgetInstance.formActionHandler, this.getValues(), {
           'command': 'save'
         }, this.doSaveCallback);
-        // hide ourselves
-        isc.OBQuickRun.hide();
       },
 
       // and reload
@@ -318,10 +316,7 @@ isc.OBUserProfile.addProperties({
           // https://issues.openbravo.com/view.php?id=19689
           // https://issues.openbravo.com/view.php?id=19659
           // window.location.href = OB.Utilities.getLocationUrlWithoutFragment();
-          // do this in a separate thread
-          setTimeout(function() {
-            window.location.replace(OB.Utilities.getLocationUrlWithoutFragment());  
-          }, 100);          
+          window.location.replace(OB.Utilities.getLocationUrlWithoutFragment());
         }
       },
 
@@ -446,8 +441,6 @@ isc.OBUserProfile.addProperties({
         OB.RemoteCallManager.call(passwordForm.formActionHandler, passwordForm.getValues(), {
           'command': 'changePwd'
         }, passwordForm.doSaveCallback);
-        // hide ourselves
-        isc.OBQuickRun.hide();
       },
 
       // the callback displays an info dialog and then hides the form
