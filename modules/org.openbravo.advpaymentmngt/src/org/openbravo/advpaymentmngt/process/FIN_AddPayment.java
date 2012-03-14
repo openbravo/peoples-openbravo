48fc1680c9ab5f6d8409ed865c1206b08e6d37c8
David Miguelez <david.miguelez@openbravo.com>
2012-03-14 17:56:21 +0100

    Related to Issue 20017: Removes unnecesary check in conditional.
    --HG--
    extra : rebase_source : a443f1b7b6b04b6eb4b6487ff00b0f4a4461d442

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/process/FIN_AddPayment.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/process/FIN_AddPayment.java
@@ -165,7 +165,7 @@ public class FIN_AddPayment {
             .get(paymentScheduleDetail.getId());
         // If detail to be added is zero amount, skip it
         if (paymentDetailAmount.signum() == 0
-            && paymentScheduleDetail.getWriteoffAmount().signum() == 0 && paymentScheduleDetail.getWriteoffAmount().signum()==0) {
+            && paymentScheduleDetail.getWriteoffAmount().signum() == 0) {
           continue;
         }
         BigDecimal amountDifference = paymentScheduleDetail.getAmount().subtract(
