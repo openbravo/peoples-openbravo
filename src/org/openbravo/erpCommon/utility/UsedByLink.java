fc90b63cb71271ad7c3da6952ac939a7e640b9af
Miguel de Juana <miguel.dejuana@openbravo.com>
2012-03-20 10:47:05 +0100

    Fixed issue 0020039: Incorrect message above linked items doesn't appear more When you click in a Linked Items Category and no Linked Item appear on the right, there is an ArrayIndexOutofBound and it shows a '0' above the linked items grid.
    --HG--
    extra : rebase_source : 80c569014232b1ad7490c4424477b5de2f6453eb

--- src/org/openbravo/erpCommon/utility/UsedByLink.java
+++ src/org/openbravo/erpCommon/utility/UsedByLink.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2001-2011 Openbravo SLU 
+ * All portions are Copyright (C) 2001-2012 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -283,17 +283,12 @@ public class UsedByLink extends HttpSecureAppServlet {
           windowId + "|" + columnName);
 
       final UsedByLinkData[] data = UsedByLinkData.keyColumns(this, adTabId);
-      if (data == null || data.length == 0) {
-        // // in order to preserve old behavior we use bdError method to report errors
-        // bdError(request, response, "RecordError", vars.getLanguage());
-        // return;
+      if (data != null && data.length > 0) {
+        final SearchResult searchResult = getLinkedItems(vars, data, windowId, columnName, keyId,
+            adTabId, tableName, inpcolumnName, tableId);
+        // jsonObject
+        jsonObject = buildJsonObject(jsonObject, searchResult);
       }
-
-      final SearchResult searchResult = getLinkedItems(vars, data, windowId, columnName, keyId,
-          adTabId, tableName, inpcolumnName, tableId);
-
-      jsonObject = buildJsonObject(jsonObject, searchResult);
-      // jsonObject
 
     } catch (Exception e) {
       try {
