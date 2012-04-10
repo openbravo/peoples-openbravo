a64ec29b5d2b7f66faa5b2e91d90fe21200f1f33
Iván Perdomo <ivan.perdomo@openbravo.com>
2012-04-10 12:20:42 +0200

    Fixes JSLint warnings

--- web/org.openbravo.retail.posterminal/js/model/productprice.js
+++ web/org.openbravo.retail.posterminal/js/model/productprice.js
@@ -1,4 +1,4 @@
-
+/*global define,Backbone */
 
 define([], function () {
   
@@ -24,21 +24,28 @@ define([], function () {
     },
     exec : function (filter) {
       var me = this;
-      this.dsProduct.exec(filter, function (data) {   
+
+      this.dsProduct.exec(filter, function (data) {
+        var i, find;
+        
+        find = function (item) {
+          me.dsProductPrice.find({'priceListVersion': me.priceListVersion, 'product': item.product.id}, function(price) {
+            if (price) {
+              item.price = price;
+              me.add(item);
+            }
+          });
+        };
+        
         me.reset();
+        
         if (data.exception) {
           alert(data.exception.message);
         } else {
-          for (var i in data) {
-            var item = data[i];
-            (function (item) {
-              me.dsProductPrice.find({'priceListVersion': me.priceListVersion, 'product': item.product.id}, function(price) {
-                if (price) {
-                  item.price = price;
-                  me.add(item);
-                }
-              });
-            }(item));            
+          for (i in data) {
+            if(data.hasOwnProperty(i)) {
+              find(data[i]);
+            }
           }
         }
       });
