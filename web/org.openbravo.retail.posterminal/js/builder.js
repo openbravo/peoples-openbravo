6a2343206a88d8e9bb921927b58fd6d6c2446864
Iván Perdomo <ivan.perdomo@openbravo.com>
2012-04-10 11:50:50 +0200

    Fixes JSLint warnings, code reformatted

--- web/org.openbravo.retail.posterminal/js/builder.js
+++ web/org.openbravo.retail.posterminal/js/builder.js
@@ -1,94 +1,113 @@
+/*global define,$,_,Backbone */
+
 define([], function () {
-// depends on jQuery
-  
-  var B = function (b, context) {
-        
-      var mycontext = context || new B.Context();
-      var inst = new b.kind(context, b.id);
-      
-      // attributes
-      if (b.attr) {
-        if (inst.attr) {
-          for (var attr in b.attr) {
+  // depends on jQuery
+  var B;
+
+  B = function (b, context) {
+
+    var mycontext = context || new B.Context();
+    var inst = new b.kind(context, b.id);
+    var attr, i, max, child;
+
+    // attributes
+    if (b.attr) {
+      if (inst.attr) {
+        for (attr in b.attr) {
+          if (b.attr.hasOwnProperty(attr)) {
             inst.attr(attr, b.attr[attr]);
           }
-        } else if (inst.attrs) {
-          inst.attrs(b.attr);
+        }
+      } else if (inst.attrs) {
+        inst.attrs(b.attr);
+      }
+    }
+
+    // content array
+    if (b.content) {
+      for (i = 0, max = b.content.length; i < max; i++) {
+        child = b.content[i];
+        if (typeof (child) === "string") {
+          inst.append(B({
+            kind: B.KindText(child)
+          }, mycontext));
+        } else if (child.kind) {
+          inst.append(B(child, mycontext));
+        } else {
+          inst.append(child);
         }
       }
-      
-      // content array
-      if (b.content) {
-        for (var i = 0, max = b.content.length; i < max; i++) {
-          var child = b.content[i];
-          if (typeof (child) === "string") {
-            inst.append(B({kind: B.KindText(child) }, mycontext));
-          } else if (child.kind) {
-            inst.append(B(child, mycontext));
-          } else {
-            inst.append(child);
-          }
-        }
-      }     
-      
-      // initialize
-      if (inst.inithandler) {
-        inst.inithandler(b.init);
-      }
-      
-      return inst;
-   };
-   
+    }
+
+    // initialize
+    if (inst.inithandler) {
+      inst.inithandler(b.init);
+    }
+
+    return inst;
+  };
+
   B.Context = function () {
     this.models = [];
     _.extend(this, Backbone.Events);
-  }
+  };
+
   B.Context.prototype.set = function (name, value) {
     this.models[name] = value;
-  }  
+  };
+
   B.Context.prototype.get = function (name) {
     return this.models[name];
-  }   
-  
-  B.Kind = {  
-     inithandler: function (init) {
-       if (init) {
-         init.call(this);
-       }
-     }      
   };
-  
+
+  B.Kind = {
+    inithandler: function (init) {
+      if (init) {
+        init.call(this);
+      }
+    }
+  };
+
   B.KindText = function (text) {
-     var F = function (context) {
-       this.$ = $(document.createTextNode(text));
-     };
-     F.prototype.attr = function (attr, value) {
-     };
-     F.prototype.append = function (child) {
-     };        
-     _.extend(F.prototype, B.Kind);    
-     return F;
+    var F;
+
+    F = function (context) {
+      this.$ = $(document.createTextNode(text));
+    };
+
+    F.prototype.attr = function (attr, value) {};
+
+    F.prototype.append = function (child) {};
+
+    _.extend(F.prototype, B.Kind);
+    return F;
   };
-   
+
   B.KindHTML = function (html) {
-    var F = function (context) {
+    var F;
+
+    F = function (context) {
       this.$ = $(html);
       this.context = context;
     };
+
     F.prototype.attr = function (attr, value) {
       this.$.attr(attr, value);
     };
+
     F.prototype.append = function (child) {
       if (child.$) {
         this.$.append(child.$);
       }
-    };      
-     _.extend(F.prototype, B.Kind);    
-     return F;
-  };  
-  
+    };
+    _.extend(F.prototype, B.Kind);
+    return F;
+  };
+
   B.KindJQuery = function (tag) {
-    var F = function (context) {
+    var F;
+
+    F = function (context) {
       if (tag.jquery) {
         this.$ = tag;
       } else if (tag.nodeType) {
@@ -98,18 +117,21 @@ define([], function () {
       }
       this.context = context;
     };
+
     F.prototype.attr = function (attr, value) {
       this.$.attr(attr, value);
     };
+
     F.prototype.append = function (child) {
       if (child.$) {
         this.$.append(child.$);
       }
-    };         
+    };
+
     _.extend(F.prototype, B.Kind);
     return F;
   };
-      
+
   return B;
-  
+
 });
