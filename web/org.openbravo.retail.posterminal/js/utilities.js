074469ab825daf2c7c70fe4647a4c4eddac08ce0
Adrián Romero <adrianromero@openbravo.com>
2012-04-10 13:39:38 +0200

    Fixes JSLint Warnings. Fixes quotation style.

--- web/org.openbravo.retail.posterminal/js/utilities.js
+++ web/org.openbravo.retail.posterminal/js/utilities.js
@@ -41,15 +41,15 @@ define([], function () {
   
   
   OB.UTIL.getParameterByName = function (name) {
-    var n = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regexS = "[\\?&]" + n + "=([^&#]*)";
+    var n = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regexS = '[\\?&]' + n + '=([^&#]*)';
     var regex = new RegExp(regexS);
     var results = regex.exec(window.location.search);
-    return (results) ? decodeURIComponent(results[1].replace(/\+/g, " ")) : "";
+    return (results) ? decodeURIComponent(results[1].replace(/\+/g, ' ')) : '';
   };
     
   OB.UTIL.escapeRegExp = function (text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+    return text.replace(/[\-\[\]{}()+?.,\\\^$|#\s]/g, '\\$&');
   };
   
   OB.UTIL.recontext = function (model, defaultid) {
