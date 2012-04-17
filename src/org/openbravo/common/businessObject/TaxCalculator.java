601d84e590af5f42ad2fdd32905cb6dc320c2309
meeravalli <meeravalli.r@promantia.com>
2012-04-17 19:01:56 +0530

    rounding for tax calculation and the alignments of the purchase order window.

--- src/org/openbravo/common/businessObject/TaxCalculator.java
+++ src/org/openbravo/common/businessObject/TaxCalculator.java
@@ -1,10 +1,12 @@
 package org.openbravo.common.businessObject;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.hibernate.criterion.Restrictions;
 import org.openbravo.base.exception.OBException;
 import org.openbravo.dal.service.OBCriteria;
@@ -19,6 +21,7 @@ public class TaxCalculator {
 
   private static final int PERCENT_TO_VALUE = 100;
   private TaxRate taxRate;
+  protected Logger log4j = Logger.getLogger(this.getClass());
   Map<TaxRate, BigDecimal> taxAmtsMap = new HashMap<TaxRate, BigDecimal>();
 
   public TaxCalculator(TaxRate aTaxRate) {
@@ -68,16 +71,17 @@ public class TaxCalculator {
     BigDecimal taxDivider = new BigDecimal(0);
     taxAmtsMap.clear();
     if (isInclusiveOfTax) {
-      taxDivider = getTaxAmount(new BigDecimal(1), taxRate, isSale);
+      taxDivider = getTaxAmount(new BigDecimal(1), taxRate, isSale, ccy);
       taxDivider = taxDivider.add(new BigDecimal(1));
       taxAmt = amount.subtract(amount.divide(taxDivider, ccy.getStandardPrecision().intValue()));
     } else
-      taxAmt = getTaxAmount(amount, taxRate, isSale);
-    return taxAmt;
+      taxAmt = getTaxAmount(amount, taxRate, isSale, ccy);
+    log4j.debug("Tax Amt: " + taxAmt);
+    return taxAmt.divide(new BigDecimal(1));
 
   }
 
-  private BigDecimal getTaxAmount(BigDecimal amount, TaxRate rate, boolean isSale) {
+  private BigDecimal getTaxAmount(BigDecimal amount, TaxRate rate, boolean isSale, Currency ccy) {
     BigDecimal amt = new BigDecimal(0);
     if ((isSale && rate.getSalesPurchaseType().equals("P"))
         || (!isSale && rate.getSalesPurchaseType().equals("S"))) {
@@ -87,27 +91,32 @@ public class TaxCalculator {
     if (rate.isSummaryLevel()) {
       List<TaxRate> rateList = getChildTaxRates(rate);
       for (TaxRate childRate : rateList) {
-        amt = amt.add(getTaxAmount(amount, childRate, isSale));
+        amt = amt.add(getTaxAmount(amount, childRate, isSale, ccy));
       }
     } else {
       if (rate.getBaseAmount().equals(""))
         throw new OBException("Tax BaseAmount is null");
       if (rate.getBaseAmount().equals("LNA")) {
         amt = amount.multiply(rate.getRate()).divide(new BigDecimal(PERCENT_TO_VALUE));
+        log4j.debug("Tax Amt: " + amt);
       } else if (rate.getBaseAmount().equals("LNATAX")) {
         amt = taxAmtsMap.get(rate.getTaxBase()).add(amount).multiply(rate.getRate()).divide(
             new BigDecimal(PERCENT_TO_VALUE));
+        log4j.debug("Tax Amt: " + amt);
       } else if (rate.getBaseAmount().equals("TAX")) {
         if (rate.getTaxBase().equals(""))
           throw new OBException("TaxBase is null");
         amt = taxAmtsMap.get(rate.getTaxBase()).multiply(rate.getRate()).divide(
             new BigDecimal(PERCENT_TO_VALUE));
+        log4j.debug("Tax Amt: " + amt);
       }
+      amt = amt.round(new MathContext(ccy.getStandardPrecision().intValue()));
     }
     taxAmtsMap.put(rate, amt);
     return amt;
   }
 
+  // Get tax rate list
   private List<TaxRate> getChildTaxRates(TaxRate rate) {
     OBCriteria<TaxRate> taxRateCriteria = OBDal.getInstance().createCriteria(TaxRate.class);
     taxRateCriteria.add(Restrictions.eq(TaxRate.PROPERTY_PARENTTAXRATE, rate));
