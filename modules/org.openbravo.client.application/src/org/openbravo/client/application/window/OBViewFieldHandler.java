30c0b7e9a3ae09622a1ab5a4331a5d13df1fab26
Asier Lostalé <asier.lostale@openbravo.com>
2012-05-08 12:53:58 +0200

    fixed bug 20384: No updateable columns can be set as mandatory
     Note value for these fields can be changed on new but not afterwards, so it
     makes sense to be able to set them as mandatory.

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/window/OBViewFieldHandler.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/window/OBViewFieldHandler.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2011 Openbravo SLU
+ * All portions are Copyright (C) 2011-2012 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -1199,10 +1199,6 @@ public class OBViewFieldHandler {
     public boolean getRequired() {
       // booleans are never required as their input only allows 2 values
       if (property.isBoolean()) {
-        return false;
-      }
-
-      if (!getUpdatable()) {
         return false;
       }
 
