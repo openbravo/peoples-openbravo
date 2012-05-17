2602e9f2ce6467511c058dc1ab36230d83aab12f
Asier Lostalé <asier.lostale@openbravo.com>
2012-05-17 11:42:14 +0200

    fixed bug 20536: Heartbeat popup not shown after setting purpose

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/HeartBeatPopupActionHandler.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/HeartBeatPopupActionHandler.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2009-2011 Openbravo SLU 
+ * All portions are Copyright (C) 2009-2012 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -121,7 +121,7 @@ public class HeartBeatPopupActionHandler extends BaseActionHandler {
       result.put("showSuccessUpgrade", isUpgrading && usingAprm && !exportConfigScript);
       result.put("showInstancePurpose",
           showHeartBeatOrRegistration == HeartbeatProcess.HeartBeatOrRegistration.InstancePurpose);
-      result.put("showHeartBeat",
+      result.put("showHeartbeat",
           showHeartBeatOrRegistration == HeartbeatProcess.HeartBeatOrRegistration.HeartBeat);
       result.put("showRegistration",
           showHeartBeatOrRegistration == HeartbeatProcess.HeartBeatOrRegistration.Registration);
