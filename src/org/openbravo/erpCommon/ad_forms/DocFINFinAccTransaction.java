72b5b109475eab2e4e4ce6ad5a7aa06d449d5403
Ioritz Cia <ioritz.cia@openbravo.com>
2012-05-17 18:36:17 +0200

    Fixes issue 20433: Detail of a Journal Entry without description.
    --HG--
    extra : rebase_source : 455f7474b676d5a4274638c90f3d7b5c921ff993

--- src/org/openbravo/erpCommon/ad_forms/DocFINFinAccTransaction.java
+++ src/org/openbravo/erpCommon/ad_forms/DocFINFinAccTransaction.java
@@ -524,8 +524,11 @@ public class DocFINFinAccTransaction extends AcctServer {
           isReceipt ? convertedAmount.toString() : "", Fact_Acct_Group_ID, nextSeqNo(SeqNo),
           DocumentType, conn);
     }
+    DocLine_FINFinAccTransaction line = new DocLine_FINFinAccTransaction(DocumentType,
+        transaction.getId(), "");
+    line.m_description = transaction.getFinPayment().getDescription();
     fact.createLine(
-        null,
+        line,
         getAccountUponDepositWithdrawal(conn, transaction.getFinPayment().getPaymentMethod(),
             transaction.getAccount(), as, transaction.getFinPayment().isReceipt()), C_Currency_ID,
         transaction.getDepositAmount().toString(), transaction.getPaymentAmount().toString(),
