9a4f6bb565094e678450c29ce0f343383b4d8e2c
David Baz Fayos <david.baz@openbravo.com>
2012-05-27 18:10:39 +0200

    Login layout refactor

--- web/org.openbravo.retail.posterminal/js/windows/login.js
+++ web/org.openbravo.retail.posterminal/js/windows/login.js
@@ -10,50 +10,84 @@ define(['builder', 'i18n',
   OB.COMP.Login = OB.COMP.CustomView.extend({
     createView: function () {
       return (
-        {kind: B.KindJQuery('section'), content: [  
+        {kind: B.KindJQuery('section'), content: [
+          {kind: B.KindJQuery('div'), attr: {'class': 'row login-header-row'}, content: [
+            {kind: B.KindJQuery('div'), attr: {'class': 'span12'}, content: [
+              {kind: B.KindJQuery('div'), attr: {'class': 'login-header-company'}},
+              {kind: B.KindJQuery('div'), attr: {'class': 'login-header-ob'}}
+            ]}
+          ]},
           {kind: B.KindJQuery('div'), attr: {'class': 'row'}, content: [
             {kind: B.KindJQuery('div'), attr: {'class': 'span6'}, content: [
-              {kind: B.KindJQuery('div'), attr: {'class': 'row-fluid'}, content: [
-                {kind: B.KindJQuery('div'), attr: {'class': 'span6'}, content: [   
-                  {kind: B.KindJQuery('strong'), attr: {'style': 'color: white;'}, content: [   
-                     "User Name"                                                                
-                  ]}                                                             
-                ]},
-                {kind: B.KindJQuery('div'), attr: {'class': 'span6'}, content: [   
-                  {kind: B.KindJQuery('input'), id: 'username', attr: {'id': 'username', 'type': 'text', 'value': 'Openbravo'}}                                                                              
-                ]}
-              ]},                                                                          
-              {kind: B.KindJQuery('div'), attr: {'class': 'row-fluid'}, content: [
-                {kind: B.KindJQuery('div'), attr: {'class': 'span6'}, content: [   
-                  {kind: B.KindJQuery('strong'), attr: {'style': 'color: white;'}, content: [   
-                     "Password"                                                                
-                  ]}                                                                  
-                ]},
-                {kind: B.KindJQuery('div'), attr: {'class': 'span6'}, content: [   
-                  {kind: B.KindJQuery('input'), id: 'password', attr: {'id': 'password', 'type': 'password', 'value': 'openbravo'}, init: function () {
-                    this.$el.keyup(function (e) {
-                        if(event.keyCode === 13){
-                            $("#loginaction").click();
-                        }
-                    });                  
-                  }}                                                                              
-                ]}
-              ]},
-              {kind: B.KindJQuery('div'), attr: {'class': 'row-fluid'}, content: [
-                {kind: B.KindJQuery('a'), attr: {'id': 'loginaction', 'class': 'btnlink', 'href': '#'}, content: [
-                  {kind: B.KindJQuery('i'), attr: {'class': 'icon-ok  icon-white'}}, ' Log in'
+              {kind: B.KindJQuery('div'), attr: {'class': 'row login-user-container'}, content: [
+                {kind: B.KindJQuery('div'), attr: {'class': 'login-user-button'}, content: [
+                  {kind: B.KindJQuery('div'), attr: {'class': 'login-user-button-bottom'}, content: [
+                    {kind: B.KindJQuery('span'), attr: {'class': 'login-user-button-bottom-icon'}, content: [' ']},
+                    {kind: B.KindJQuery('span'), attr: {'class': 'login-user-button-bottom-text'}, content: ['Openbravo']}
+                  ]}
                 ], init: function () {
-                    this.$el.click(function (e) {
-                      e.preventDefault();
-                      var u = $('#username').val();
-                      var p = $('#password').val();
-                      if (!u || !p) {
-                        alert('Please enter your username and password');
-                      } else {
-                        OB.POS.modelterminal.load(u, p);
-                      }
-                    });
+                  this.$el.click(function (e) {
+                    e.preventDefault();
+                    var u = $('#username').val('Openbravo');
+                    var p = $('#password').val('');
+                     OB.POS.modelterminal.load(u, 'openbravo');
+                  });
                 }}
+              ]}
+            ]},
+            {kind: B.KindJQuery('div'), attr: {'class': 'span6'}, content: [
+              {kind: B.KindJQuery('div'), attr: {'class': 'row-fluid login-inputs-container'}, content: [
+                {kind: B.KindJQuery('div'), attr: {'class': 'row'}, content: [
+                  {kind: B.KindJQuery('div'), attr: {'class': 'span6 login-inputs-screenlocked'}, content: ['Screen locked']}
+                ]},
+                {kind: B.KindJQuery('div'), attr: {'class': 'row'}, content: [
+                  {kind: B.KindJQuery('div'), attr: {'class': 'span6 login-inputs-userpassword'}, content: [
+                    {kind: B.KindJQuery('input'), id: 'username', attr: {'id': 'username', 'type': 'text', 'placeholder': 'User'}}
+                  ]}
+                ]},
+                {kind: B.KindJQuery('div'), attr: {'class': 'row'}, content: [
+                  {kind: B.KindJQuery('div'), attr: {'class': 'span6 login-inputs-userpassword'}, content: [
+                    {kind: B.KindJQuery('input'), id: 'username', attr: {'id': 'password', 'type': 'password', 'placeholder': 'Password'}}
+                  ]}
+                ]},
+                {kind: B.KindJQuery('div'), attr: {'class': 'row'}, content: [
+                  {kind: B.KindJQuery('div'), attr: {'class': 'span1'}, content: [' ']},
+                  {kind: B.KindJQuery('div'), attr: {'class': 'span1'}, content: [' ']},
+                  {kind: B.KindJQuery('div'), attr: {'class': 'span2'}, content: [
+                    {kind: B.KindJQuery('a'), attr: {'id': 'loginaction', 'class': 'login-inputs-button', 'href': '#'}, content: ['Log In'],
+                      init: function () {
+                        this.$el.click(function (e) {
+                          e.preventDefault();
+                          var u = $('#username').val();
+                          var p = $('#password').val();
+                          if (!u || !p) {
+                            alert('Please enter your username and password');
+                          } else {
+                            OB.POS.modelterminal.load(u, p);
+                          }
+                        });
+                      }
+                    }
+                  ]},
+                  {kind: B.KindJQuery('div'), attr: {'class': 'span1'}, content: [' ']},
+                  {kind: B.KindJQuery('div'), attr: {'class': 'span1'}, content: [
+                    {kind: B.KindJQuery('div'), attr: {'class': 'login-clock-container'}, content: [ 
+                      {kind: B.KindJQuery('div'), id: 'clock', attr: {'class': 'login-clock-time'}},
+                      {kind: B.KindJQuery('div'), id: 'date', attr: {'class': 'login-clock-date'}}
+                    ], init: function () {
+                        var clock = this.context.clock.$el;
+                        var date = this.context.date.$el;
+                        var updateclock = function () {
+                          var d = new Date();
+                          clock.text(OB.I18N.formatHour(d));
+                          date.text(OB.I18N.formatDate(d));
+                        };
+                        updateclock();
+                        setInterval(updateclock, 1000);
+                      }
+                    }
+                  ]}
+                ]}
               ]}
             ]}
           ]}
@@ -63,9 +97,9 @@ define(['builder', 'i18n',
           }, this);
         }}
 
-      );           
-    }   
+      );
+    }
   });
-  
+
   return OB.COMP.Login;
 });
