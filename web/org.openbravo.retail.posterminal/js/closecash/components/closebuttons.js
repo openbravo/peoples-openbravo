eaaa53e75459ad17da9df5712f884c6275b5969b
Miguel de Juana <miguel.dejuana@openbravo.com>
2012-06-12 15:07:27 +0200

    Restore deleted class in closebuttons
    --HG--
    extra : rebase_source : 87721066ab5ed71c941c7503d0e96cec61467c21

--- web/org.openbravo.retail.posterminal/js/closecash/components/closebuttons.js
+++ web/org.openbravo.retail.posterminal/js/closecash/components/closebuttons.js
@@ -7,7 +7,7 @@ define(['builder', 'utilities', 'i18n', 'components/commonbuttons'], function (B
 
   OB.COMP.ButtonPrev = OB.COMP.ToolbarButton.extend({
 	_id: 'closeprevbutton',
-    icon: 'icon-chevron-left icon-white',
+    icon: 'icon-chevron-left icon-white  btn-icon-left',
     label: OB.I18N.getLabel('OBPOS_LblPrevStep'),
     clickEvent: function (e) {
     if(this.options.modeldaycash.defaults.step===2){
@@ -27,7 +27,7 @@ define(['builder', 'utilities', 'i18n', 'components/commonbuttons'], function (B
 
   OB.COMP.ButtonNext = OB.COMP.ToolbarButton.extend({
 	_id: 'closenextbutton',
-    iconright: 'icon-chevron-right icon-white',
+    iconright: 'icon-chevron-right icon-white  btn-icon-right',
     label: OB.I18N.getLabel('OBPOS_LblNextStep'),
 
     clickEvent: function (e) {
