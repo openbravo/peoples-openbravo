dabf724fbc3e25a7dfcc3572686ef1df4414dc16
Iván Perdomo <ivan.perdomo@openbravo.com>
2012-06-15 16:20:23 +0200

    Implements the list of dependencies for pointofsale window

--- src/org/openbravo/retail/posterminal/OBPOSComponentProvider.java
+++ src/org/openbravo/retail/posterminal/OBPOSComponentProvider.java
@@ -18,11 +18,14 @@
  */
 package org.openbravo.retail.posterminal;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.enterprise.context.ApplicationScoped;
 
 import org.openbravo.client.kernel.BaseComponentProvider;
+import org.openbravo.client.kernel.BaseComponentProvider.ComponentResource.ComponentResourceType;
 import org.openbravo.client.kernel.Component;
 import org.openbravo.client.kernel.ComponentProvider;
 
@@ -52,5 +55,36 @@ public class OBPOSComponentProvider extends BaseComponentProvider {
       return component;
     }
     throw new IllegalArgumentException("Component id " + componentId + " not supported.");
+  }
+
+  @Override
+  public List<ComponentResource> getGlobalComponentResources() {
+
+    final List<ComponentResource> globalResources = new ArrayList<ComponentResource>();
+    final String prefix = "web/" + POSUtils.MODULE_JAVA_PACKAGE + "/js/";
+
+    final String[] resourceDependency = { "builder", "datasource", "utilities", "utilitiesui",
+        "arithmetic", "i18n", "components/clock", "model/order", "model/terminal",
+        "components/commonbuttons", "components/hwmanager", "components/table",
+        "components/toolbarpayment", "components/toolbarscan", "components/keyboard",
+        "windows/login", "windows/posbuttons", "components/modalprofile", "components/terminal",
+        "components/modallogout", "data/datamaster", "data/dal", "data/dataordersave",
+        "data/dataordertaxes", "data/dataorderdiscount", "components/renderorder",
+        "components/listreceipts", "components/modalreceipts", "components/renderbusinesspartner",
+        "components/searchbps", "components/modalbps", "components/scan", "components/tabscan",
+        "components/rendercategory", "components/listcategories", "components/renderproduct",
+        "components/listproducts", "components/tabbrowse", "components/searchproducts",
+        "components/tabsearch", "components/editline", "components/tabeditline",
+        "components/total", "components/modalpayment", "components/payment",
+        "components/paymentcoins", "components/tabpayment", "components/renderorderline",
+        "components/order", "components/orderdetails", "components/businesspartner",
+        "components/listreceiptscounter", "windows/pointofsale" };
+
+    for (String resource : resourceDependency) {
+      globalResources.add(createComponentResource(ComponentResourceType.Static, prefix + resource
+          + ".js", POSUtils.APP_NAME));
+    }
+
+    return globalResources;
   }
 }
