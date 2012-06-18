326c983420ba159dddcd2dc080dd10601f2086b8
Miguel de Juana <miguel.dejuana@openbravo.com>
2012-06-18 13:46:50 +0200

    related to Printing Payments

---
+++ src/org/openbravo/retail/posterminal/term/CloseCashPayments.java
@@ -0,0 +1,22 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2012 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+package org.openbravo.retail.posterminal.term;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.retail.posterminal.ProcessHQLQuery;
+
+public class CloseCashPayments extends ProcessHQLQuery {
+
+  @Override
+  protected String getQuery(JSONObject jsonsent) throws JSONException {
+    return "select p.commercialName as commercialName, f.currentBalance as currentBalance from OBPOS_App_Payment as p, FIN_Financial_Account as f "
+        + "where p.financialAccount=f";
+  }
+}
