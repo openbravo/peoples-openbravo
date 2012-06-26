b3b8083aef6abbe7e896b27c54668bc5fd577c73
Adrián Romero <adrianromero@openbravo.com>
2012-06-26 16:17:55 +0200

    Improving payment providers implementation
    --HG--
    extra : rebase_source : 2db332de92b79ebdb6a67ac6683da1c26ad4b36b

--- web/org.openbravo.retail.posterminal/js/components/modalpayment.js
+++ web/org.openbravo.retail.posterminal/js/components/modalpayment.js
@@ -16,35 +16,9 @@
       
       this.paymentcomponent = new providerview().render();      
       this.contentview.$el.empty().append(this.paymentcomponent.$el);
-//        
-//      this.contentview.$paymenttype.text(name);
-//      this.contentview.$paymentamount.text(OB.I18N.formatCurrency(amount));
       this.paymentcomponent.show(receipt, key, name, amount);       
       this.$el.modal();
     }
   });
-  
-  OB.COMP.ContentPayment = Backbone.View.extend({
-    tagName: 'div',
-    initialize: function () {
-      this.info = B(                
-        {kind: B.KindJQuery('div'), content: [
-          {kind: B.KindJQuery('div'), attr: {'class': 'row-fluid'}, content: [
-            {kind: B.KindJQuery('div'), attr: {'class': 'span6'}, content: [ OB.I18N.getLabel('OBPOS_LblModalType') ]},
-            {kind: B.KindJQuery('div'), id: 'paymenttype', attr: {'class': 'span6',  style:'font-weight: bold;'}}          
-          ]},
-          {kind: B.KindJQuery('div'), attr: {'class': 'row-fluid'}, content: [
-            {kind: B.KindJQuery('div'), attr: {'class': 'span6'}, content: [ OB.I18N.getLabel('OBPOS_LblModalAmount') ]},
-            {kind: B.KindJQuery('div'), id: 'paymentamount', attr: {'class': 'span6', style:'font-weight: bold;'}}          
-          ]},
-          {kind: B.KindJQuery('div'), id: 'paymentcontainer'}
-        ]}      
-      );
-      this.$paymenttype = this.info.context.paymenttype.$el;
-      this.$paymentamount = this.info.context.paymentamount.$el;
-      this.$paymentcontainer = this.info.context.paymentcontainer.$el;
-      this.$el.append(this.info.$el);
-    }
-  });  
 
 }());
