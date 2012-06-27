2068d36266b766edc7bd753f1284e002378b7f67
Augusto Mauch <augusto.mauch@openbravo.com>
2012-06-27 13:00:29 +0200

    Fixes issue 20874: Prevented exception when pushing the down arrow key
    In the fix of issue 17910 some logic was introduced that checked the id of the last record of the grid. If that record was yet to be loaded, the exception 'Cannot read property from undefined' was raised.
    
    Now, before executing that statement, it is checked if the last grid record is defined.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-grid.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-grid.js
@@ -103,7 +103,7 @@ isc.OBGrid.addProperties({
   },
 
   bodyKeyPress: function (event, eventInfo) {
-    if ((eventInfo.keyName === isc.OBViewGrid.ARROW_UP_KEY_NAME && this.data.localData[0].id === this.lastSelectedRecord.id) || (eventInfo.keyName === isc.OBViewGrid.ARROW_DOWN_KEY_NAME && this.data.localData[this.data.localData.length - 1].id === this.lastSelectedRecord.id)) {
+    if ((eventInfo.keyName === isc.OBViewGrid.ARROW_UP_KEY_NAME && this.data.localData[0].id === this.lastSelectedRecord.id) || (eventInfo.keyName === isc.OBViewGrid.ARROW_DOWN_KEY_NAME && this.data.localData[this.data.localData.length - 1] && this.data.localData[this.data.localData.length - 1].id === this.lastSelectedRecord.id)) {
       return true;
     }
     var response = OB.KeyboardManager.Shortcuts.monitor('OBGrid.body');
