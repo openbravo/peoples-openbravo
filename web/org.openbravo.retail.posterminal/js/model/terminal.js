c5bb478c367d014dd4566ff1a35196b3fc9d84c5
Iván Perdomo <ivan.perdomo@openbravo.com>
2012-06-28 23:00:49 +0200

    Format model/terminal.js

--- web/org.openbravo.retail.posterminal/js/model/terminal.js
+++ web/org.openbravo.retail.posterminal/js/model/terminal.js
@@ -10,37 +10,36 @@
       this.ds = data.ds;
       Backbone.Collection.prototype.constructor.call(this);
     },
-    inithandler : function (init) {
-       if (init) {
-         init.call(this);
-       }
+    inithandler: function (init) {
+      if (init) {
+        init.call(this);
+      }
     },
-    exec : function (filter) {
+    exec: function (filter) {
       var me = this;
-      if(this.ds){
-      this.ds.exec(filter, function (data, info) {
-        var i;
-        me.reset();
-        me.trigger('info', info);
-        if (data.exception) {
-          OB.UTIL.showError(data.exception.message);
-        } else {
-          for (i in data) {
-            if(data.hasOwnProperty(i)) {
-              me.add(data[i]);
+      if (this.ds) {
+        this.ds.exec(filter, function (data, info) {
+          var i;
+          me.reset();
+          me.trigger('info', info);
+          if (data.exception) {
+            OB.UTIL.showError(data.exception.message);
+          } else {
+            for (i in data) {
+              if (data.hasOwnProperty(i)) {
+                me.add(data[i]);
+              }
             }
           }
-        }
-      });
+        });
       }
     }
   });
 
   // Terminal model.
-
   OB.MODEL.Terminal = Backbone.Model.extend({
 
-    defaults : {
+    defaults: {
       terminal: null,
       context: null,
       permissions: null,
@@ -65,11 +64,17 @@
       this.set('currency', null);
       $.ajax({
         url: '../../org.openbravo.retail.posterminal/POSLoginHandler',
-        data: {'user': user, 'password': password, 'terminal': OB.POS.paramTerminal, 'Command': 'DEFAULT', 'IsAjaxCall': 1},
+        data: {
+          'user': user,
+          'password': password,
+          'terminal': OB.POS.paramTerminal,
+          'Command': 'DEFAULT',
+          'IsAjaxCall': 1
+        },
         type: 'POST',
         success: function (data, textStatus, jqXHR) {
           var pos, baseUrl;
-          if(data && data.showMessage) {
+          if (data && data.showMessage) {
             me.triggerLoginFail(401, mode, data);
             return;
           }
@@ -130,36 +135,35 @@
       // Starting app
       var me = this;
       var params = {
-          terminal: OB.POS.paramTerminal
+        terminal: OB.POS.paramTerminal
       };
 
       new OB.DS.Query('org.openbravo.retail.posterminal.term.Terminal').exec(
-        params,
-        function (data) {
-          if (data.exception) {
-            me.logout();
-          } else if (data[0]) {
-            me.set('terminal', data[0]);
-            me.loadPayments();
-            me.loadContext();
-            me.loadPermissions();
-            me.loadBP();
-            me.loadBPLocation();
-            me.loadLocation();
-            me.loadPriceList();
-            me.loadPriceListVersion();
-            me.loadCurrency();
-          } else {
-            OB.UTIL.showError("Terminal does not exists: " + params.terminal);
-          }
+      params, function (data) {
+        if (data.exception) {
+          me.logout();
+        } else if (data[0]) {
+          me.set('terminal', data[0]);
+          me.loadPayments();
+          me.loadContext();
+          me.loadPermissions();
+          me.loadBP();
+          me.loadBPLocation();
+          me.loadLocation();
+          me.loadPriceList();
+          me.loadPriceListVersion();
+          me.loadCurrency();
+        } else {
+          OB.UTIL.showError("Terminal does not exists: " + params.terminal);
         }
-      );
+      });
     },
 
     loadPayments: function () {
       var me = this;
-      new OB.DS.Query('org.openbravo.retail.posterminal.term.Payments').exec({ pos: this.get('terminal').id }
-      , function (data) {
+      new OB.DS.Query('org.openbravo.retail.posterminal.term.Payments').exec({
+        pos: this.get('terminal').id
+      }, function (data) {
         if (data) {
           var i, max;
           me.set('payments', data);
@@ -174,8 +178,7 @@
 
     loadContext: function () {
       var me = this;
-      new OB.DS.Query('org.openbravo.retail.posterminal.term.Context').exec({}
-      , function (data) {
+      new OB.DS.Query('org.openbravo.retail.posterminal.term.Context').exec({}, function (data) {
         if (data[0]) {
           me.set('context', data[0]);
           me.triggerReady();
@@ -185,8 +188,7 @@
 
     loadPermissions: function () {
       var me = this;
-      new OB.DS.Query('org.openbravo.retail.posterminal.term.RolePreferences').exec({}
-      , function (data) {
+      new OB.DS.Query('org.openbravo.retail.posterminal.term.RolePreferences').exec({}, function (data) {
         var i, max, permissions = {};
         if (data) {
           for (i = 0, max = data.length; i < max; i++) {
