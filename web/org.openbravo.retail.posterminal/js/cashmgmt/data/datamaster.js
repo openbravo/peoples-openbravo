720fc6f37d1f56a7e0713389dcc4ef8b31a45eed
Iván Perdomo <ivan.perdomo@openbravo.com>
2012-07-04 09:29:26 +0200

    Remove duplicated code, code format

--- web/org.openbravo.retail.posterminal/js/cashmgmt/data/datamaster.js
+++ web/org.openbravo.retail.posterminal/js/cashmgmt/data/datamaster.js
@@ -7,75 +7,40 @@
   OB = window.OB || {};
   OB.DATA = window.OB.DATA || {};
 
-  OB.DATA.Container = function (context) {
-    this.context = context;
-    this.datachildren = [];
-  };
-
-  OB.DATA.Container.prototype.append = function (child) {
-    this.datachildren.push(child);
-  };
-
-  OB.DATA.Container.prototype.inithandler = function () {
-    var i, max;
-    for (i = 0, max = this.datachildren.length; i < max; i++) {
-      this.datachildren[i].load();
-    }
-  };
-
-  OB.DATA.Base = {
-    load: function () {
-       this.ds.load(this.loadparams);
-    }
-  };
-
   OB.DATA.DepositsDrops = function (context, id) {
-	    this._id = 'DataDepositsDrops';
-	    this.context = context;
-	    this.ds = new OB.DS.DataSource(new OB.DS.Query(
-	      'org.openbravo.retail.posterminal.term.CashMgmtDepositsDrops',
-	      OB.POS.modelterminal.get('terminal').client,
-	      OB.POS.modelterminal.get('terminal').organization));
-	    this.loadparams = {pos: OB.POS.modelterminal.get('terminal').id};
-	  };
-_.extend(OB.DATA.DepositsDrops.prototype, OB.DATA.Base);
+    this._id = 'DataDepositsDrops';
+    this.context = context;
+    this.ds = new OB.DS.DataSource(new OB.DS.Query('org.openbravo.retail.posterminal.term.CashMgmtDepositsDrops', OB.POS.modelterminal.get('terminal').client, OB.POS.modelterminal.get('terminal').organization));
+    this.loadparams = {
+      pos: OB.POS.modelterminal.get('terminal').id
+    };
+  };
+  _.extend(OB.DATA.DepositsDrops.prototype, OB.DATA.Base);
 
-OB.DATA.CashMgmtPaymentMethod = function (context, id) {
-  this._id = 'DataCashMgmtPaymentMethod';
-  this.context = context;
-  this.ds = new OB.DS.DataSource(new OB.DS.Query(
-    'org.openbravo.retail.posterminal.term.CashMgmtPayments',
-    OB.POS.modelterminal.get('terminal').client,
-    OB.POS.modelterminal.get('terminal').organization));
-  this.loadparams = {pos: OB.POS.modelterminal.get('terminal').id};
-};
-_.extend(OB.DATA.CashMgmtPaymentMethod.prototype, OB.DATA.Base);
+  OB.DATA.CashMgmtPaymentMethod = function (context, id) {
+    this._id = 'DataCashMgmtPaymentMethod';
+    this.context = context;
+    this.ds = new OB.DS.DataSource(new OB.DS.Query('org.openbravo.retail.posterminal.term.CashMgmtPayments', OB.POS.modelterminal.get('terminal').client, OB.POS.modelterminal.get('terminal').organization));
+    this.loadparams = {
+      pos: OB.POS.modelterminal.get('terminal').id
+    };
+  };
+  _.extend(OB.DATA.CashMgmtPaymentMethod.prototype, OB.DATA.Base);
 
-OB.DATA.DropEvents = function (context, id) {
-  this._id = 'DataDropEvents';
-  this.context = context;
-  this.ds = new OB.DS.DataSource(new OB.DS.Query(
-    'org.openbravo.retail.posterminal.term.CashMgmtDropEvents',
-    OB.POS.modelterminal.get('terminal').client,
-    OB.POS.modelterminal.get('terminal').organization));
-  this.loadparams = {};
-};
-_.extend(OB.DATA.DropEvents.prototype, OB.DATA.Base);
+  OB.DATA.DropEvents = function (context, id) {
+    this._id = 'DataDropEvents';
+    this.context = context;
+    this.ds = new OB.DS.DataSource(new OB.DS.Query('org.openbravo.retail.posterminal.term.CashMgmtDropEvents', OB.POS.modelterminal.get('terminal').client, OB.POS.modelterminal.get('terminal').organization));
+    this.loadparams = {};
+  };
+  _.extend(OB.DATA.DropEvents.prototype, OB.DATA.Base);
 
-OB.DATA.DepositEvents = function (context, id) {
-  this._id = 'DataDepositEvents';
-  this.context = context;
-  this.ds = new OB.DS.DataSource(new OB.DS.Query(
-    'org.openbravo.retail.posterminal.term.CashMgmtDepositEvents',
-    OB.POS.modelterminal.get('terminal').client,
-    OB.POS.modelterminal.get('terminal').organization));
-  this.loadparams = {};
-};
-_.extend(OB.DATA.DepositEvents.prototype, OB.DATA.Base);
-
-  if(!window.openDatabase) {
-    window.console.error("Your browser doesn't support WebSQL");
-    return;
-  }
+  OB.DATA.DepositEvents = function (context, id) {
+    this._id = 'DataDepositEvents';
+    this.context = context;
+    this.ds = new OB.DS.DataSource(new OB.DS.Query('org.openbravo.retail.posterminal.term.CashMgmtDepositEvents', OB.POS.modelterminal.get('terminal').client, OB.POS.modelterminal.get('terminal').organization));
+    this.loadparams = {};
+  };
+  _.extend(OB.DATA.DepositEvents.prototype, OB.DATA.Base);
 
 }());
--- web/org.openbravo.retail.posterminal/js/closecash/data/datamaster.js
+++ web/org.openbravo.retail.posterminal/js/closecash/data/datamaster.js
@@ -2,84 +2,34 @@
 
 (function () {
 
-  var db, dbSize, dbSuccess, dbError, fetchTaxes;
-
-  OB = window.OB || {};
-  OB.DATA = window.OB.DATA || {};
-
-  OB.DATA.Container = function (context) {
-    this.context = context;
-    this.datachildren = [];
-  };
-
-  OB.DATA.Container.prototype.append = function (child) {
-    this.datachildren.push(child);
-  };
-
-  OB.DATA.Container.prototype.inithandler = function () {
-    var i, max;
-    for (i = 0, max = this.datachildren.length; i < max; i++) {
-      this.datachildren[i].load();
-    }
-  };
-
-  OB.DATA.Base = {
-    load: function () {
-       this.ds.load(this.loadparams);
-    }
-  };
-
   OB.DATA.PaymentMethod = function (context, id) {
-	    this._id = 'DataPaymentMethod';
-	    this.context = context;
-	    this.ds = new OB.DS.DataSource(new OB.DS.Query(
-	      'org.openbravo.retail.posterminal.term.Payments',
-	      OB.POS.modelterminal.get('terminal').client,
-	      OB.POS.modelterminal.get('terminal').organization));
-	    this.loadparams = {pos: OB.POS.modelterminal.get('terminal').id};
-	  };
+    this._id = 'DataPaymentMethod';
+    this.context = context;
+    this.ds = new OB.DS.DataSource(new OB.DS.Query('org.openbravo.retail.posterminal.term.Payments', OB.POS.modelterminal.get('terminal').client, OB.POS.modelterminal.get('terminal').organization));
+    this.loadparams = {
+      pos: OB.POS.modelterminal.get('terminal').id
+    };
+  };
   _.extend(OB.DATA.PaymentMethod.prototype, OB.DATA.Base);
 
   OB.DATA.CloseCashPaymentMethod = function (context, id) {
-	    this._id = 'DataCloseCashPaymentMethod';
-	    this.context = context;
-	    this.ds = new OB.DS.DataSource(new OB.DS.Query(
-	      'org.openbravo.retail.posterminal.term.CloseCashPayments',
-	      OB.POS.modelterminal.get('terminal').client,
-	      OB.POS.modelterminal.get('terminal').organization));
-	    this.loadparams = {pos: OB.POS.modelterminal.get('terminal').id};
-	  };
-_.extend(OB.DATA.CloseCashPaymentMethod.prototype, OB.DATA.Base);
+    this._id = 'DataCloseCashPaymentMethod';
+    this.context = context;
+    this.ds = new OB.DS.DataSource(new OB.DS.Query('org.openbravo.retail.posterminal.term.CloseCashPayments', OB.POS.modelterminal.get('terminal').client, OB.POS.modelterminal.get('terminal').organization));
+    this.loadparams = {
+      pos: OB.POS.modelterminal.get('terminal').id
+    };
+  };
+  _.extend(OB.DATA.CloseCashPaymentMethod.prototype, OB.DATA.Base);
 
-OB.DATA.CashCloseReport = function (context, id) {
-  this._id = 'DataCashCloseReport';
-  this.context = context;
-  this.ds = new OB.DS.DataSource(new OB.DS.Query(
-    'org.openbravo.retail.posterminal.term.CashCloseReport',
-    OB.POS.modelterminal.get('terminal').client,
-    OB.POS.modelterminal.get('terminal').organization));
-  this.loadparams = {pos: OB.POS.modelterminal.get('terminal').id};
-};
-_.extend(OB.DATA.CashCloseReport.prototype, OB.DATA.Base);
-
-//  OB.MODEL.Payment = Backbone.Model.extend({});
-//
-//  OB.MODEL.PaymentList = Backbone.Collection.extend({
-//    model: OB.MODEL.Payment,
-//    url: '../../org.openbravo.retail.posterminal.datasource/OBPOS_App_Payment',
-//    parse: function (response, error) {
-//      if (response && response.response) {
-//        return response.response.data;
-//      }
-//    }
-//  });
-
-  //OB.MODEL.Payments = new OB.MODEL.PaymentList;
-  // Offline based on WebSQL
-
-  if(!window.openDatabase) {
-    window.console.error("Your browser doesn't support WebSQL");
-    return;
-  }
+  OB.DATA.CashCloseReport = function (context, id) {
+    this._id = 'DataCashCloseReport';
+    this.context = context;
+    this.ds = new OB.DS.DataSource(new OB.DS.Query('org.openbravo.retail.posterminal.term.CashCloseReport', OB.POS.modelterminal.get('terminal').client, OB.POS.modelterminal.get('terminal').organization));
+    this.loadparams = {
+      pos: OB.POS.modelterminal.get('terminal').id
+    };
+  };
+  _.extend(OB.DATA.CashCloseReport.prototype, OB.DATA.Base);
 
 }());
