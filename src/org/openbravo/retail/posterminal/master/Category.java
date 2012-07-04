0b346c55594f970e099b2b94a494c489568548b3
Guillermo Álvarez de Eulate <guillermo.alvarez@openbravo.com>
2012-07-04 13:11:22 +0200

    Products which are stored locally must belong to a defined product list and ALSO appears in the defined Product price list.
    --HG--
    extra : rebase_source : 7e425a7aacee525b6b22a484d869abbe973cc097

--- src/org/openbravo/retail/posterminal/master/Category.java
+++ src/org/openbravo/retail/posterminal/master/Category.java
@@ -10,6 +10,7 @@ package org.openbravo.retail.posterminal.master;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.model.pricing.pricelist.PriceList;
 import org.openbravo.retail.config.OBRETCOProductList;
 import org.openbravo.retail.posterminal.POSUtils;
 import org.openbravo.retail.posterminal.ProcessHQLQuery;
@@ -22,13 +23,29 @@ public class Category extends ProcessHQLQuery {
     final OBRETCOProductList productList = POSUtils.getProductListByOrgId(jsonsent
         .getString("organization"));
 
-    // final PriceList priceList =
-    // POSUtils.getPriceListByTerminalId(jsonsent.getString("terminal"));
+    final PriceList priceList = POSUtils.getPriceListByOrgId(jsonsent.getString("organization"));
 
     if (productList != null) {
       return "select distinct pli.product.productCategory.id as id, pli.product.productCategory.searchKey as searchKey,pli.product.productCategory.name as name, pli.product.productCategory.name as _identifier, img.bindaryData as img "
-          + "from OBRETCO_Prol_Product pli left outer join pli.product.productCategory.image img "
-          + "where pli.obretcoProductlist = '" + productList.getId() + "'";
+          + "from OBRETCO_Prol_Product pli left outer join pli.product.productCategory.image img, "
+          + "PricingProductPrice ppp, "
+          + "PricingPriceListVersion pplv "
+          + "WHERE (pli.obretcoProductlist = '"
+          + productList.getId()
+          + "') "
+          + "AND ("
+          + "pplv.priceList.id = '"
+          + priceList.getId()
+          + "' AND "
+          + "pplv.validFromDate = (select max(a.validFromDate) "
+          + "  FROM PricingPriceListVersion a "
+          + "  WHERE a.priceList.id = '"
+          + priceList.getId()
+          + "')"
+          + ") AND ("
+          + "ppp.priceListVersion.id = pplv.id"
+          + ") AND ("
+          + "pli.product.id = ppp.product.id" + ")";
     } else {
       throw new JSONException("Product list not found");
     }
--- src/org/openbravo/retail/posterminal/master/Product.java
+++ src/org/openbravo/retail/posterminal/master/Product.java
@@ -10,6 +10,7 @@ package org.openbravo.retail.posterminal.master;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.model.pricing.pricelist.PriceList;
 import org.openbravo.retail.config.OBRETCOProductList;
 import org.openbravo.retail.posterminal.POSUtils;
 import org.openbravo.retail.posterminal.ProcessHQLQuery;
@@ -21,13 +22,34 @@ public class Product extends ProcessHQLQuery {
     final OBRETCOProductList productList = POSUtils.getProductListByOrgId(jsonsent
         .getString("organization"));
 
-    // final PriceList priceList =
-    // POSUtils.getPriceListByTerminalId(jsonsent.getString("terminal"));
+    final PriceList priceList = POSUtils.getPriceListByOrgId(jsonsent.getString("organization"));
 
     if (productList != null) {
-      return "select pli.product.id as id, pli.product.name as _identifier, pli.product.taxCategory.id as taxCategory, pli.product.productCategory.id as productCategory, pli.product.obposScale as obposScale, pli.product.uOM.id as uOM, pli.product.uPCEAN as uPCEAN, img.bindaryData as img "
-          + "from OBRETCO_Prol_Product pli left outer join pli.product.image img "
-          + "where pli.obretcoProductlist = '" + productList.getId() + "'";
+      return "select pli.product.id as id, pli.product.name as _identifier, pli.product.taxCategory.id as taxCategory, "
+          + "pli.product.productCategory.id as productCategory, pli.product.obposScale as obposScale, pli.product.uOM.id as uOM, pli.product.uPCEAN as uPCEAN, img.bindaryData as img "
+          + "FROM OBRETCO_Prol_Product pli left outer join pli.product.image img, "
+          + "PricingProductPrice ppp, "
+          + "PricingPriceListVersion pplv "
+          + "WHERE (pli.obretcoProductlist = '"
+          + productList.getId()
+          + "') "
+          + "AND ("
+          + "pplv.priceList.id = '"
+          + priceList.getId()
+          + "' AND "
+          + "pplv.validFromDate = (select max(a.validFromDate) "
+          + "  FROM PricingPriceListVersion a "
+          + "  WHERE a.priceList.id = '"
+          + priceList.getId()
+          + "')"
+          + ") AND ("
+          + "ppp.priceListVersion.id = pplv.id"
+          + ") AND ("
+          + "pli.product.id = ppp.product.id" + ")";
+      // return
+      // "select pli.product.id as id, pli.product.name as _identifier, pli.product.taxCategory.id as taxCategory, pli.product.productCategory.id as productCategory, pli.product.obposScale as obposScale, pli.product.uOM.id as uOM, pli.product.uPCEAN as uPCEAN, img.bindaryData as img "
+      // + "from OBRETCO_Prol_Product pli left outer join pli.product.image img "
+      // + "where pli.obretcoProductlist = '" + productList.getId() + "'";
     } else {
       throw new JSONException("Product list not found");
     }
