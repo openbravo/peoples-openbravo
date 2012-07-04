87581b4c7b082ad7ef9edf6b5cc5a755c150dd13
Augusto Mauch <augusto.mauch@openbravo.com>
2012-07-04 17:14:41 +0200

    Checks navigator.onLine before processing order
    If the navigator is offLine when an order is being closed, the isbeingprocessed attribute of the order will not be set to 'Y' and the server ProcessOrder process will not be called.

--- web/org.openbravo.retail.posterminal/js/data/dataordersave.js
+++ web/org.openbravo.retail.posterminal/js/data/dataordersave.js
@@ -17,24 +17,29 @@
       json = this.receipt.serializeToJSON(),
       receiptCopy = this.receipt.clone();
       receiptCopy.set('hasbeenpaid', 'Y');
-      receiptCopy.set('isbeingprocessed', 'Y');
+      // The order will not be processed if the navigator is offline
+      if (navigator.onLine) {
+        receiptCopy.set('isbeingprocessed', 'Y');
+      }
       OB.Dal.save(receiptCopy, function(){
         delete json.json;
-        me.proc.exec({
-          order: json
-        }, function (data, message) {
-          if (data && data.exception) {
-            OB.UTIL.showError(OB.I18N.getLabel('OBPOS_MsgReceiptNotSaved', [docno]));
-            //If there is an error while sending the order to the backend,
-            //we set the order as not being processed so that it can be sent again
-            receiptCopy.set('isbeingprocessed', 'N');
-            OB.Dal.save(receiptCopy, null, null);
-          } else {  
-            //In this case, the order was sent correctly, so we remove it from the buffer
-            OB.UTIL.showSuccess(OB.I18N.getLabel('OBPOS_MsgReceiptSaved', [docno]));
-            OB.Dal.remove(receiptCopy, null,null);
-          }
-        });
+        if (navigator.onLine) {
+          me.proc.exec({
+            order: json
+          }, function (data, message) {
+            if (data && data.exception) {
+              OB.UTIL.showError(OB.I18N.getLabel('OBPOS_MsgReceiptNotSaved', [docno]));
+              //If there is an error while sending the order to the backend,
+              //we set the order as not being processed so that it can be sent again
+              receiptCopy.set('isbeingprocessed', 'N');
+              OB.Dal.save(receiptCopy, null, null);
+            } else {
+              //In this case, the order was sent correctly, so we remove it from the buffer
+              OB.UTIL.showSuccess(OB.I18N.getLabel('OBPOS_MsgReceiptSaved', [docno]));
+              OB.Dal.remove(receiptCopy, null,null);
+            }
+          });
+        }
       }, function(){
           OB.UTIL.showError(OB.I18N.getLabel('OBPOS_MsgReceiptNotSaved', [docno]));
       });
