8b55c28a2945bc243a7434bb1115c29f8a64f174
Guillermo Álvarez de Eulate <guillermo.alvarez@openbravo.com>
2012-07-04 11:10:17 +0200

    BusinessPartner now stores taxCategory
    --HG--
    extra : rebase_source : f553a815c6682344291b7ae73885623687d0eff0

--- web/org.openbravo.retail.posterminal/js/model/businesspartner.js
+++ web/org.openbravo.retail.posterminal/js/model/businesspartner.js
@@ -1,4 +1,5 @@
 //jslint
+
 /*global Backbone */
 
 
@@ -9,21 +10,33 @@
     tableName: 'c_bpartner',
     entityName: 'BusinessPartner',
     source: 'org.openbravo.retail.posterminal.master.BusinessPartner',
-    properties: ['id', 'searchKey', 'name', 'description', 'taxId', 'locId', 'locName', '_identifier', '_idx'],
+    properties: [
+     'id',
+     'searchKey',
+     'name',
+     'description',
+     'taxId',
+     'taxCategory',
+     'locId',
+     'locName',
+     '_identifier',
+     '_idx'
+    ],
     propertyMap: {
-      'id': 'c_bpartner_id',
-      'searchKey': 'value',
-      'name': 'name',
-      'description': 'description',
-      'taxId': 'taxID',
-      'locId': 'c_bpartnerlocation_id',
-      'locName': 'c_bpartnerlocation_name',
-      '_identifier': '_identifier',
-      '_idx': '_idx'
+     'id': 'c_bpartner_id',
+     'searchKey': 'value',
+     'name': 'name',
+     'description': 'description',
+     'taxId': 'taxID',
+     'taxCategory': 'so_bp_taxcategory_id',
+     'locId': 'c_bpartnerlocation_id',
+     'locName': 'c_bpartnerlocation_name',
+     '_identifier': '_identifier',
+     '_idx': '_idx'
     },
-    createStatement: 'CREATE TABLE IF NOT EXISTS c_bpartner (c_bpartner_id TEXT PRIMARY KEY , value TEXT , name TEXT , description TEXT , taxID TEXT , c_bpartnerlocation_id TEXT , c_bpartnerlocation_name TEXT , _identifier TEXT , _idx NUMERIC)',
+    createStatement: 'CREATE TABLE IF NOT EXISTS c_bpartner (c_bpartner_id TEXT PRIMARY KEY , value TEXT , name TEXT , description TEXT , taxID TEXT , so_bp_taxcategory_id TEXT, c_bpartnerlocation_id TEXT , c_bpartnerlocation_name TEXT , _identifier TEXT , _idx NUMERIC)',
     dropStatement: 'DROP TABLE IF EXISTS c_bpartner',
-    insertStatement: 'INSERT INTO c_bpartner(c_bpartner_id, value, name, description, taxID, c_bpartnerlocation_id, c_bpartnerlocation_name, _identifier, _idx)  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+    insertStatement: 'INSERT INTO c_bpartner(c_bpartner_id, value, name, description, taxID, so_bp_taxcategory_id, c_bpartnerlocation_id, c_bpartnerlocation_name, _identifier, _idx)  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
     updateStatement: ''
   });
 
