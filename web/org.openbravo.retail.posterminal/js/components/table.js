2256fcde0f93342198a034573921ad256147eb5a
Adrián Romero <adrianromero@openbravo.com>
2012-07-05 18:31:42 +0200

    Formatting table.js
    --HG--
    extra : rebase_source : 34118dcc8d0cbcb0c27e39584ea533606d436c1d

--- web/org.openbravo.retail.posterminal/js/components/table.js
+++ web/org.openbravo.retail.posterminal/js/components/table.js
@@ -11,7 +11,9 @@
       tagName: tag,
       _addModelToCollection: function (model, index) {
         var me = this,
-            tr = (new this.renderLine({model: model})).render().$el;
+            tr = (new this.renderLine({
+            model: model
+          })).render().$el;
         if (_.isNumber(index) && index < this.collection.length - 1) {
           this.$el.children().eq(index + this.header).before(tr);
         } else {
@@ -26,21 +28,23 @@
         this.renderHeader = attr.renderHeader;
         this.header = this.renderHeader ? 1 : 0;
         this.collection = attr.collection;
-  
+
         this.collection.on('change', function (model, prop) {
           var index = this.collection.indexOf(model);
-          this.$el.children().eq(index + this.header).replaceWith((new this.renderLine({model : model})).render().$el);
+          this.$el.children().eq(index + this.header).replaceWith((new this.renderLine({
+            model: model
+          })).render().$el);
         }, this);
-  
+
         this.collection.on('add', function (model, prop, options) {
           this._addModelToCollection(model, options.index);
         }, this);
-  
+
         this.collection.on('remove', function (model, prop, options) {
           var index = options.index;
           this.$el.children().eq(index + this.header).remove();
         }, this);
-  
+
         this.collection.on('reset', function () {
           this.$el.empty();
           if (this.renderHeader) {
@@ -50,7 +54,7 @@
             this._addModelToCollection(model);
           }, this);
         }, this);
-  
+
         // Init clear...
         this.$el.empty();
         if (this.htmlId) {
@@ -77,26 +81,23 @@
       this.theader = $('<div/>');
       this.tbody = $('<ul/>').addClass('unstyled').css('display', 'none');
       this.tempty = $('<div/>');
-      this.tinfo = $('<div/>').css('display', 'none')
-                              .css('border-bottom', '1px solid #cccccc')
-                              .css('padding', '15px')
-                              .css('font-weight', 'bold')
-                              .css('color', '#cccccc');
-      this.$el.append(this.theader)
-              .append(this.tbody)
-              .append(this.tinfo)
-              .append(this.tempty);
+      this.tinfo = $('<div/>').css('display', 'none').css('border-bottom', '1px solid #cccccc').css('padding', '15px').css('font-weight', 'bold').css('color', '#cccccc');
+      this.$el.append(this.theader).append(this.tbody).append(this.tinfo).append(this.tempty);
     },
-    
+
     _addModelToCollection: function (model, index) { // means after...
       var me = this,
           tr = $('<li/>');
-      tr.append((new this.renderLine({model: model})).render().$el);
+      tr.append((new this.renderLine({
+        model: model
+      })).render().$el);
       // tr.click(stdClickEvent);
       model.on('change', function () {
-        tr.empty().append((new this.renderLine({model: model})).render().$el);
+        tr.empty().append((new this.renderLine({
+          model: model
+        })).render().$el);
       }, this);
-  
+
       model.on('selected', function () {
         if (this.style) {
           if (this.selected) {
@@ -107,14 +108,14 @@
           OB.UTIL.makeElemVisible(this.$el, this.selected);
         }
       }, this);
-  
+
       if (_.isNumber(index) && index < this.collection.length - 1) {
         this.tbody.children().eq(index).before(tr);
       } else {
         this.tbody.append(tr);
       }
     },
-  
+
     attr: function (attr) {
       this.style = attr.style; // none, "edit", "list"
       this.renderHeader = attr.renderHeader;
@@ -122,15 +123,15 @@
       this.renderEmpty = attr.renderEmpty;
       this.collection = attr.collection;
       this.selected = null;
-  
+
       if (this.renderHeader) {
         this.theader.append((new this.renderHeader()).render().$el);
       }
-  
+
       if (this.renderEmpty) {
         this.tempty.append((new this.renderEmpty()).render().$el);
       }
-  
+
       this.collection.on('selected', function (model) {
         if (!model && this.style) {
           if (this.selected) {
@@ -139,14 +140,14 @@
           this.selected = null;
         }
       }, this);
-  
+
       this.collection.on('add', function (model, prop, options) {
-  
+
         this.tempty.hide();
         this.tbody.show();
-  
+
         this._addModelToCollection(model, options.index);
-  
+
         if (this.style === 'list') {
           if (!this.selected) {
             model.trigger('selected', model);
@@ -155,11 +156,11 @@
           model.trigger('selected', model);
         }
       }, this);
-  
+
       this.collection.on('remove', function (model, prop, options) {
         var index = options.index;
         this.tbody.children().eq(index).remove();
-  
+
         if (index >= this.collection.length) {
           if (this.collection.length === 0) {
             this.collection.trigger('selected');
@@ -169,21 +170,21 @@
         } else {
           this.collection.at(index).trigger('selected', this.collection.at(index));
         }
-  
+
         if (this.collection.length === 0) {
           this.tbody.hide();
           this.tempty.show();
         }
       }, this);
-  
+
       this.collection.on('reset', function (a, b, c) {
         var lastmodel;
-  
+
         this.tbody.hide();
         this.tempty.show();
-  
+
         this.tbody.empty();
-  
+
         if (this.collection.size() === 0) {
           this.tbody.hide();
           this.tempty.show();
@@ -194,14 +195,14 @@
           this.collection.each(function (model) {
             this._addModelToCollection(model);
           }, this);
-  
+
           if (this.style === 'list' || this.style === 'edit') {
             lastmodel = this.collection.at(this.collection.size() - 1);
             lastmodel.trigger('selected', lastmodel);
           }
         }
       }, this);
-  
+
       this.collection.on('info', function (info) {
         if (info) {
           this.tinfo.text(OB.I18N.getLabel(info));
