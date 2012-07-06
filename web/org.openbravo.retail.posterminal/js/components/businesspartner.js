24ea857dd9960900c1c17080b34d10c48dc9d20b
David Baz Fayos <david.baz@openbravo.com>
2012-07-06 17:17:46 +0200

    Small change in BP button

--- web/org.openbravo.retail.posterminal/js/components/businesspartner.js
+++ web/org.openbravo.retail.posterminal/js/components/businesspartner.js
@@ -5,13 +5,12 @@
   OB = window.OB || {};
   OB.COMP = window.OB.COMP || {};
 
-  OB.COMP.BusinessPartner = Backbone.View.extend({
-      tagName: 'button',
+  OB.COMP.BusinessPartner = OB.COMP.SmallButton.extend({
       className: 'btnlink btnlink-small btnlink-gray',
       attributes: {'href': '#modalcustomer', 'data-toggle': 'modal'},
       initialize: function () {
-
-        this.receipt =  this.options.modelorder;
+        OB.COMP.SmallButton.prototype.initialize.call(this); // super.initialize();
+        this.receipt = this.options.modelorder;
         this.receipt.on('clear change:bp change:bploc', function () {
           this.$el.text(this.receipt.get('bp') ? this.receipt.get('bp').get('_identifier') : '');
         }, this);
