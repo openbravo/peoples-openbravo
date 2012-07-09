ced1e12938a4029196bc235a9f0cb727671528c2
Adrián Romero <adrianromero@openbravo.com>
2012-07-09 18:25:14 +0200

    Refactoring ListProducts
    --HG--
    extra : rebase_source : 9f5654f0e40bcfa53c91d0b750496233dfe39ae4

--- web/org.openbravo.retail.posterminal/js/components/listproducts.js
+++ web/org.openbravo.retail.posterminal/js/components/listproducts.js
@@ -1,85 +1,99 @@
-/*global B, _ */
+/*global Backbone, _ */
 
 (function () {
 
   OB = window.OB || {};
   OB.COMP = window.OB.COMP || {};
 
-  OB.COMP.ListProducts = function (context) {
-    var me = this;
+  OB.COMP.ListProducts = Backbone.View.extend({
+    optionsid: 'ListProducts',
+    tag: 'div',
+    contentView: [{
+      tag: 'div',
+      attributes: {
+        'style': 'padding: 10px; border-bottom: 1px solid #cccccc;'
+      },
+      content: [{
+        id: 'title',
+        tag: 'h3'
+      }]
+    }, {
+      id: 'tableview',
+      view: OB.UI.TableView.extend({
+        renderEmpty: OB.COMP.RenderEmpty,
+        renderLine: OB.COMP.RenderProduct
+      })
+    }],
+    initialize: function () {
 
-    this._id = 'ListProducts';
-    this.receipt = context.modelorder;
-    this.products = new OB.Collection.ProductList();
+      this.options[this.optionsid] = this;
+      OB.UTIL.initContentView(this);
 
-    this.products.on('click', function (model) {
-      this.receipt.addProduct(model);
-    }, this);
+      var me = this;
+      this.receipt = this.options.modelorder;
+      this.products = new OB.Collection.ProductList();
+      this.tableview.registerCollection(this.products);
 
-    this.component = B(
-      {kind: B.KindJQuery('div'), content: [
-        {kind: B.KindJQuery('div'), attr: {'style': 'padding: 10px; border-bottom: 1px solid #cccccc;'}, content: [
-          {kind: B.KindJQuery('h3'), id: 'title'}
-        ]},
-        {kind: OB.UI.TableView, id: 'tableview', attr: {
-          collection: this.products,
-          renderEmpty: OB.COMP.RenderEmpty,          
-          renderLine: OB.COMP.RenderProduct
-        }}
-      ]}
-    );
-    this.$el = this.component.$el;
-    this.titleProd = this.component.context.title.$el;
-    this.tableview = this.component.context.tableview;
-  };
+      this.products.on('click', function (model) {
+        this.receipt.addProduct(model);
+      }, this);
+    },
+    loadCategory: function (category) {
+      var criteria, me = this;
 
-  OB.COMP.ListProducts.prototype.loadCategory = function (category) {
-    var criteria, me = this;
-
-    function successCallbackPrices(dataPrices, dataProducts) {
-      if(dataPrices && dataPrices.length > 0){
-        _.each(dataPrices.models, function(currentPrice){
-          if(dataProducts.get(currentPrice.get('product'))){
-            dataProducts.get(currentPrice.get('product')).set('price', currentPrice);
-          }
-        });
-        _.each(dataProducts.models, function(currentProd){
-          if(currentProd.get('price')===undefined){
-            var price = new OB.Model.ProductPrice({'listPrice': 0});
-            dataProducts.get(currentProd.get('id')).set('price', price);
-            OB.UTIL.showWarning("No price found for product " + currentProd.get('_identifier'));
-          }
-        });
-      }else{
-        OB.UTIL.showWarning("OBDAL No prices found for products");
-        _.each(dataProducts.models, function(currentProd){
-          var price = new OB.Model.ProductPrice({'listPrice': 0});
-          currentProd.set('price', price);
-        });
+      function successCallbackPrices(dataPrices, dataProducts) {
+        if (dataPrices && dataPrices.length > 0) {
+          _.each(dataPrices.models, function (currentPrice) {
+            if (dataProducts.get(currentPrice.get('product'))) {
+              dataProducts.get(currentPrice.get('product')).set('price', currentPrice);
+            }
+          });
+          _.each(dataProducts.models, function (currentProd) {
+            if (currentProd.get('price') === undefined) {
+              var price = new OB.Model.ProductPrice({
+                'listPrice': 0
+              });
+              dataProducts.get(currentProd.get('id')).set('price', price);
+              OB.UTIL.showWarning("No price found for product " + currentProd.get('_identifier'));
+            }
+          });
+        } else {
+          OB.UTIL.showWarning("OBDAL No prices found for products");
+          _.each(dataProducts.models, function (currentProd) {
+            var price = new OB.Model.ProductPrice({
+              'listPrice': 0
+            });
+            currentProd.set('price', price);
+          });
+        }
+        me.products.reset(dataProducts.models);
       }
-      me.products.reset(dataProducts.models);
-    }
 
-    function errorCallback(tx, error) {
-      OB.UTIL.showError("OBDAL error: " + error);
-    }
-
-    function successCallbackProducts(dataProducts) {
-      if(dataProducts && dataProducts.length > 0){
-        criteria = {'priceListVersion' : OB.POS.modelterminal.get('pricelistversion').id};
-        OB.Dal.find(OB.Model.ProductPrice, criteria, successCallbackPrices, errorCallback, dataProducts);
-      }else{
-        me.products.reset();
+      function errorCallback(tx, error) {
+        OB.UTIL.showError("OBDAL error: " + error);
       }
-      me.titleProd.text(category.get('_identifier'));
-    }
 
-    if (category) {
-      criteria = {'productCategory': category.get('id')};
-      OB.Dal.find(OB.Model.Product, criteria , successCallbackProducts, errorCallback);
-    } else {
-      this.products.reset();
-      this.titleProd.text(OB.I18N.getLabel('OBPOS_LblNoCategory'));
+      function successCallbackProducts(dataProducts) {
+        if (dataProducts && dataProducts.length > 0) {
+          criteria = {
+            'priceListVersion': OB.POS.modelterminal.get('pricelistversion').id
+          };
+          OB.Dal.find(OB.Model.ProductPrice, criteria, successCallbackPrices, errorCallback, dataProducts);
+        } else {
+          me.products.reset();
+        }
+        me.title.text(category.get('_identifier'));
+      }
+
+      if (category) {
+        criteria = {
+          'productCategory': category.get('id')
+        };
+        OB.Dal.find(OB.Model.Product, criteria, successCallbackProducts, errorCallback);
+      } else {
+        this.products.reset();
+        this.title.text(OB.I18N.getLabel('OBPOS_LblNoCategory'));
+      }
     }
-  };
+  });
 }());
