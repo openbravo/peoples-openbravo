73ec753898721cc999f980ceb89a10baa13b5c27
Guillermo Álvarez de Eulate <guillermo.alvarez@openbravo.com>
2012-07-09 09:39:04 +0200

    paymentcoins.js has been refectored

--- web/org.openbravo.retail.posterminal/js/components/paymentcoins.js
+++ web/org.openbravo.retail.posterminal/js/components/paymentcoins.js
@@ -1,21 +1,48 @@
 /*global B, $ */
 
-(function () {
+(function() {
 
   OB = window.OB || {};
-  OB.COMP = window.OB.COMP || {};
+  OB.UI = window.OB.UI || {};
 
-  OB.COMP.PaymentCoins = OB.COMP.Payment.extend({
+  OB.UI.PaymentCoins = OB.COMP.Payment.extend({
     paymentButtons: [
-      OB.COMP.PaymentButton.extend({paymenttype: 'OBPOS_payment.cash', amount:50, classcolor: 'btnlink-lightblue'}),
-      OB.COMP.PaymentButton.extend({paymenttype: 'OBPOS_payment.cash', amount:20, classcolor: 'btnlink-lightpink'}),
-      OB.COMP.PaymentButton.extend({paymenttype: 'OBPOS_payment.cash', amount:10, classcolor: 'btnlink-lightgreen'}),
-      OB.COMP.PaymentButton.extend({paymenttype: 'OBPOS_payment.cash', amount:5, classcolor: 'btnlink-wheat'}),
-      OB.COMP.PaymentButton.extend({paymenttype: 'OBPOS_payment.cash', amount:1, classcolor: 'btnlink-lightgreen'}),
-      OB.COMP.PaymentButton.extend({paymenttype: 'OBPOS_payment.cash', amount:0.50, classcolor: 'btnlink-orange'}),
-      OB.COMP.PaymentButton.extend({paymenttype: 'OBPOS_payment.cash', amount:0.20, classcolor: 'btnlink-gray'}),
-      OB.COMP.PaymentButton.extend({paymenttype: 'OBPOS_payment.cash', amount:0.10, classcolor: 'btnlink-lightblue'}),
-      OB.COMP.PaymentButton.extend({paymenttype: 'OBPOS_payment.cash', amount:0.05, classcolor: 'btnlink-lightpink'})
-    ]
+    OB.COMP.PaymentButton.extend({
+      paymenttype: 'OBPOS_payment.cash',
+      amount: 50,
+      classcolor: 'btnlink-lightblue'
+    }), OB.COMP.PaymentButton.extend({
+      paymenttype: 'OBPOS_payment.cash',
+      amount: 20,
+      classcolor: 'btnlink-lightpink'
+    }), OB.COMP.PaymentButton.extend({
+      paymenttype: 'OBPOS_payment.cash',
+      amount: 10,
+      classcolor: 'btnlink-lightgreen'
+    }), OB.COMP.PaymentButton.extend({
+      paymenttype: 'OBPOS_payment.cash',
+      amount: 5,
+      classcolor: 'btnlink-wheat'
+    }), OB.COMP.PaymentButton.extend({
+      paymenttype: 'OBPOS_payment.cash',
+      amount: 1,
+      classcolor: 'btnlink-lightgreen'
+    }), OB.COMP.PaymentButton.extend({
+      paymenttype: 'OBPOS_payment.cash',
+      amount: 0.50,
+      classcolor: 'btnlink-orange'
+    }), OB.COMP.PaymentButton.extend({
+      paymenttype: 'OBPOS_payment.cash',
+      amount: 0.20,
+      classcolor: 'btnlink-gray'
+    }), OB.COMP.PaymentButton.extend({
+      paymenttype: 'OBPOS_payment.cash',
+      amount: 0.10,
+      classcolor: 'btnlink-lightblue'
+    }), OB.COMP.PaymentButton.extend({
+      paymenttype: 'OBPOS_payment.cash',
+      amount: 0.05,
+      classcolor: 'btnlink-lightpink'
+    })]
   });
 }());
