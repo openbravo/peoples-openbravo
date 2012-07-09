08841f04202a0278d5857ff6f1ada4c80d543b33
Guillermo Álvarez de Eulate <guillermo.alvarez@openbravo.com>
2012-07-09 12:30:02 +0200

    Moving var statemnet to the top of the function
    --HG--
    extra : rebase_source : a2e4e6e6a06542bd13b388a2c23bb9f4c26a91a6

--- web/org.openbravo.retail.posterminal/js/cashmgmt/components/modaldepositevents.js
+++ web/org.openbravo.retail.posterminal/js/cashmgmt/components/modaldepositevents.js
@@ -9,18 +9,18 @@
     id: 'modaldepositevents',
     header: 'Select Destinations',
     initialize: function () {
-        OB.COMP.Modal.prototype.initialize.call(this); // super.initialize();
-        var theModal = this.$el,
-            theHeader = theModal.children(':first'),
-            theBody = theModal.children(':nth-child(2)'),
-            theHeaderText = theHeader.children(':nth-child(2)');
-        theModal.addClass('modal-dialog');
-        theBody.addClass('modal-dialog-body');
-        theHeaderText.attr('text-align','left');
-        theHeaderText.attr('font-weight', '150%');
-        theHeaderText.attr('padding-top', '10px');
-        theHeaderText.attr('color', 'black');
-      },
+      var theModal = this.$el,
+        theHeader = theModal.children(':first'),
+        theBody = theModal.children(':nth-child(2)'),
+        theHeaderText = theHeader.children(':nth-child(2)');
+      OB.COMP.Modal.prototype.initialize.call(this); // super.initialize();
+      theModal.addClass('modal-dialog');
+      theBody.addClass('modal-dialog-body');
+      theHeaderText.attr('text-align','left');
+      theHeaderText.attr('font-weight', '150%');
+      theHeaderText.attr('padding-top', '10px');
+      theHeaderText.attr('color', 'black');
+    },
     getContentView: function () {
       return ({kind:OB.COMP.SearchDepositEvents});
     },
