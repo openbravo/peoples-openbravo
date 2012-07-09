0c4d3a6ab49def2e789142bab482b383dfce0e7f
Adrián Romero <adrianromero@openbravo.com>
2012-07-09 18:38:28 +0200

    Refactor ListReceiptsCounter
    --HG--
    extra : rebase_source : 5fad29307ffba6c9d9ba864e6c12d891ff4a4df1

--- web/org.openbravo.retail.posterminal/js/components/listreceiptscounter.js
+++ web/org.openbravo.retail.posterminal/js/components/listreceiptscounter.js
@@ -1,4 +1,4 @@
-/*global B, Backbone */
+/*global Backbone */
 
 (function () {
 
@@ -7,37 +7,44 @@
 
   OB.COMP.ReceiptsCounter = Backbone.View.extend({
     tagName: 'div',
-    attributes: {'style': 'position: absolute; top:0px; right: 0px;'},
-    initialize: function () {
-
-      this.component = B(
-        {kind: B.KindJQuery('button'), attr: {
+    attributes: {
+      'style': 'position: absolute; top:0px; right: 0px;'
+    },
+    contentView: [{
+      tag: 'button',
+      attributes: {
         'class': 'btnlink btnlink-gray',
         'style': 'position: relative; overflow: hidden; margin:0px; padding:0px; height:50px; width: 50px;',
-        'href': '#modalreceipts', 'data-toggle': 'modal'}, content: [
-          {kind: B.KindJQuery('div'), attr: {
-          'style': 'position: absolute; top: -35px; right:-35px; background: #404040; height:70px; width: 70px; -webkit-transform: rotate(45deg); -moz-transform: rotate(45deg); -ms-transform: rotate(45deg); -transform: rotate(45deg);'  // sqrt(2) * 50
-          }},
-          {kind: B.KindJQuery('div'), id:'counter', attr:{
+        'href': '#modalreceipts',
+        'data-toggle': 'modal'
+      },
+      content: [{
+        tag: 'div',
+        attributes: {
+          'style': 'position: absolute; top: -35px; right:-35px; background: #404040; height:70px; width: 70px; -webkit-transform: rotate(45deg); -moz-transform: rotate(45deg); -ms-transform: rotate(45deg); -transform: rotate(45deg);' // sqrt(2) * 50
+        }
+      }, {
+        id: 'counter',
+        tag: 'div',
+        attributes: {
           'style': 'position: absolute; top: 0px; right:0px; padding-top: 5px; padding-right: 10px; font-weight: bold; color: white;'
-          }}
-        ]}
-      );
-      this.$el.append(this.component.$el);
-      this.$counter = this.component.context.counter.$el;
+        }
+      }]
+    }],
+    initialize: function () {
+
+      OB.UTIL.initContentView(this);
 
       this.receiptlist = this.options.modelorderlist;
       this.receiptlist.on('reset add remove', function () {
         if (this.receiptlist.length > 1) {
           this.$el.show();
-          this.$counter.text((this.receiptlist.length - 1));
+          this.counter.text((this.receiptlist.length - 1));
         } else {
           this.$el.hide();
-          this.$counter.html('&nbsp;');
+          this.counter.html('&nbsp;');
         }
-
       }, this);
     }
   });
 }());
-
