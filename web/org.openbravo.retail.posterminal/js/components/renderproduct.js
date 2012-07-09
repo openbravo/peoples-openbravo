af002cf0354ec7941c630eb1babe6b8a802c57e2
Adrián Romero <adrianromero@openbravo.com>
2012-07-09 12:53:38 +0200

    Refactor RenderProduct
    --HG--
    extra : rebase_source : 43536fa1f3f76b6df2ce21befe12e11bbb77d39f

--- web/org.openbravo.retail.posterminal/js/components/renderproduct.js
+++ web/org.openbravo.retail.posterminal/js/components/renderproduct.js
@@ -5,26 +5,45 @@
   OB = window.OB || {};
   OB.COMP = window.OB.COMP || {};
 
-  OB.COMP.RenderProduct =  OB.COMP.SelectButton.extend({
-    render: function() {
-      this.$el.append(B(
-        {kind: B.KindJQuery('div'), content: [
-          {kind: B.KindJQuery('div'), attr: {style: 'float: left; width: 20%'}, content: [
-            {kind: OB.UTIL.Thumbnail, attr: {img: this.model.get('img')}}
-          ]},
-          {kind: B.KindJQuery('div'), attr: {style: 'float: left; width: 60%;'}, content: [
-            {kind: B.KindJQuery('div'), attr: {style: 'padding-left: 5px;'}, content: [
-              this.model.get('_identifier')
-            ]}
-          ]},
-          {kind: B.KindJQuery('div'), attr: {style: 'float: left; width: 20%; text-align:right;'}, content: [
-            {kind: B.KindJQuery('strong'), content: [
-              OB.I18N.formatCurrency(this.model.get('price').get('listPrice'))
-            ]}
-          ]},
-          {kind: B.KindJQuery('div'), attr: {style: 'clear: both;'}}
-        ]}
-      ).$el);
+  OB.COMP.RenderProduct = OB.COMP.SelectButton.extend({
+    contentView: [{
+      tag: 'div',
+      attributes: {
+        style: 'float: left; width: 20%'
+      },
+      content: [{
+        id: 'viewthumbnail',
+        view: OB.UTIL.Thumbnail
+      }]
+    }, {
+      tag: 'div',
+      attributes: {
+        style: 'float: left; width: 60%;'
+      },
+      content: [{
+        id: 'dividentifier',
+        tag: 'div',
+        attributes: {
+          style: 'padding-left: 5px;'
+        }
+      }]
+    }, {
+      id: 'divprice',
+      tag: 'div',
+      attributes: {
+        style: 'float: left; width: 20%; text-align: right; font-weight:bold;'
+      }
+    }, {
+      tag: 'div',
+      attributes: {
+        style: 'clear: both;'
+      }
+    }],
+    render: function () {
+      this.viewthumbnail.img = this.model.get('img');
+      this.viewthumbnail.render();
+      this.dividentifier.text(this.model.get('_identifier'));
+      this.divprice.text(OB.I18N.formatCurrency(this.model.get('price').get('listPrice')));
       return this;
     }
   });
