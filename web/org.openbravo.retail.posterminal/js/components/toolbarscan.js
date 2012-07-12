9f08fff97ee8f0de2b2a2d70cf66d3cd5f10d197
Adrián Romero <adrianromero@openbravo.com>
2012-07-12 16:36:23 +0200

    Removing commented code
    --HG--
    extra : rebase_source : 624a723fc01452edffd1694217979c4d889018a6

--- web/org.openbravo.retail.posterminal/js/components/toolbarscan.js
+++ web/org.openbravo.retail.posterminal/js/components/toolbarscan.js
@@ -4,15 +4,6 @@
 
   OB = window.OB || {};
   OB.COMP = window.OB.COMP || {};
-
-//  OB.COMP.ToolbarScan = function (context) {
-//    this.toolbar = [{
-//        command: 'code', 
-//        classButtonActive: 'btnactive-blue',
-//        label: OB.I18N.getLabel('OBPOS_KbCode')
-//      }];
-//  };
-//  
   
   OB.COMP.ToolbarScan = OB.COMP.KeyboardComponent.extend({
     tagName: 'div',
