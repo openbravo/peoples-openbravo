0433061497a88564d0f97fdef296a5e5b7b02c88
Adrián Romero <adrianromero@openbravo.com>
2012-07-13 14:17:44 +0200

    In receipts list now it is printed whether the order is an invoice and a return
    --HG--
    extra : rebase_source : 0f6d43ef1f7e2a7f3e688e2088b29944559d8b96

--- web/org.openbravo.retail.posterminal/js/components/renderorder.js
+++ web/org.openbravo.retail.posterminal/js/components/renderorder.js
@@ -39,17 +39,39 @@
           }
         }]
       }, {
-        id: 'divgross',
         tag: 'div',
-        attributes: {
-          style: 'text-align: right; font-weight:bold;'
-        }
+        content: [{
+          id: 'divreturn',
+          tag: 'div',
+          attributes: {
+            style: 'float: left; width: 25%; color: #f8941d; font-weight:bold;'
+          }
+        }, {
+          id: 'divinvoice',
+          tag: 'div',
+          attributes: {
+            style: 'float: left; width: 25%; font-weight:bold;'
+          }
+        }, {
+          id: 'divgross',
+          tag: 'div',
+          attributes: {
+            style: 'float: right; width: 25%; text-align: right; font-weight:bold;'
+          }
+        }, {
+          tag: 'div',
+          attributes: {
+            style: 'clear: both;'
+          }
+        }]
       }]
     }],
     render: function () {
       this.divdate.text(OB.I18N.formatHour(this.model.get('orderDate')));
       this.divdocumentno.text(this.model.get('documentNo'));
       this.divbp.text(this.model.get('bp').get('_identifier'));
+      this.divreturn.text(this.model.get('orderType') === 1 ? OB.I18N.getLabel('OBPOS_ToReturn') : '');
+      this.divinvoice.text(this.model.get('generateInvoice') ? OB.I18N.getLabel('OBPOS_ToInvoice') : '');
       this.divgross.text(this.model.printGross());
 
       return this;
