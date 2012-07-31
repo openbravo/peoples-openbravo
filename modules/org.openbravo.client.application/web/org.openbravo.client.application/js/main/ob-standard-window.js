7d00835e93458dc3282757054de511d5d5592f54
Martin Taal <martin.taal@openbravo.com>
2012-07-31 15:20:54 +0200

    Fixes issue 21249: Wrong grid layout when restoring original view in grid Filtereditor had focus, unfocus during set viewstate resulted in an error because the filtereditor was using an old field definition, resolve by resetting focus and then recomputing it after viewstate. Recompute focus after viewstate needs to be done anyway as the setviewstate can have removed the current focus field.
    --HG--
    extra : transplant_source : %F9%D1I%85J%7F%02g%29%0D%0D%3D%A8%E3N1%F1nx%E1

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/main/ob-standard-window.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/main/ob-standard-window.js
@@ -410,6 +410,10 @@ isc.OBStandardWindow.addProperties({
         return 1;
       });
     }
+    
+    // restore focus as the focusitem may have been hidden now
+    // https://issues.openbravo.com/view.php?id=21249
+    this.setFocusInView();
   },
 
   clearLastViewPersonalization: function () {
