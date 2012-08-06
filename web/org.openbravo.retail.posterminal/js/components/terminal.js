3e349709450875acf53e50472d711fa5c6d598f0
Asier Lostalé <asier.lostale@openbravo.com>
2012-08-06 18:27:06 +0200

    [refactor] company widget
    --HG--
    extra : rebase_source : 716b3a33d0435ea9f4814548c449bfba5afb990c

--- web/org.openbravo.retail.posterminal/js/components/terminal.js
+++ web/org.openbravo.retail.posterminal/js/components/terminal.js
@@ -9,218 +9,7 @@
 
 /*global B, $ */
 
-(function() {
-  OB = window.OB || {};
-  OB.COMP = window.OB.COMP || {};
-
-  OB.COMP.Terminal = function(yourterminal, yourcompany, yourcompanyproperties, loggeduser, loggeduserproperties) {
-    this.yourterminal = yourterminal;
-    this.yourcompany = yourcompany;
-    this.yourcompanyproperties = yourcompanyproperties;
-    this.loggeduser = loggeduser;
-    this.loggeduserproperties = loggeduserproperties;
-  };
-
-  OB.COMP.Terminal.prototype.setModel = function(terminal) {
-    this.terminal = terminal;
-
-    this.terminal.on('change:context', function() {
-      var ctx = this.terminal.get('context');
-      if (ctx) {
-        this.loggeduser.text(ctx.user._identifier);
-        this.loggeduserproperties.empty();
-        this.loggeduserproperties.append(B({
-          kind: B.KindJQuery('div'),
-          attr: {
-            style: 'height: 60px; background-color: #FFF899;'
-          },
-          content: [{
-            kind: B.KindJQuery('div'),
-            attr: {
-              style: 'float: left; width: 55px; margin: 6px 0px 0px 6px;'
-            },
-            content: [{
-              kind: OB.UTIL.Thumbnail,
-              attr: {
-                img: ctx.img,
-                'default': 'img/anonymous-icon.png'
-              }
-            }]
-          }, {
-            kind: B.KindJQuery('div'),
-            attr: {
-              style: 'float: left; margin: 6px 0px 0px 0px; line-height: 150%;'
-            },
-            content: [{
-              kind: B.KindJQuery('div'),
-              content: [{
-                kind: B.KindJQuery('span'),
-                attr: {
-                  'style': 'font-weight: 600; margin: 0px 0px 0px 5px;'
-                },
-                content: [
-                ctx.user._identifier]
-              }]
-            }, {
-              kind: B.KindJQuery('div'),
-              content: [{
-                kind: B.KindJQuery('span'),
-                attr: {
-                  'style': 'font-weight: 600; margin: 0px 0px 0px 5px;'
-                },
-                content: [
-                ctx.role._identifier]
-              }]
-            }]
-          }]
-        }).$el);
-        this.loggeduserproperties.append(B({
-          kind: B.KindJQuery('div'),
-          content: [{
-            kind: B.KindJQuery('div'),
-            attr: {
-              style: 'height: 5px;'
-            }
-          }, {
-            kind: OB.COMP.MenuAction.extend({
-              clickEvent: function() {
-                $('#profileDialog').modal('show');
-              },
-              label: OB.I18N.getLabel('OBPOS_LblProfile')
-            })
-          }, {
-            kind: B.KindJQuery('div'),
-            attr: {
-              style: 'height: 5px;'
-            }
-          },
-/*{kind: OB.COMP.MenuAction.extend({clickEvent: function() { OB.POS.lock(); }, label: OB.I18N.getLabel('OBPOS_LogoutDialogLock')})},
-            {kind: B.KindJQuery('div'), attr: {style: 'height: 5px;'}},*/
-          //Disabled until feature be ready
-          {
-            kind: OB.COMP.MenuAction.extend({
-              clickEvent: function() {
-                $('#logoutDialog').modal('show');
-              },
-              label: OB.I18N.getLabel('OBPOS_LogoutDialogLogout')
-            })
-          }, {
-            kind: B.KindJQuery('div'),
-            attr: {
-              style: 'height: 5px;'
-            }
-          }]
-        }).$el);
-      } else {
-        this.loggeduser.text('');
-        this.loggeduserproperties.empty();
-      }
-
-    }, this);
-
-    this.terminal.on('change:terminal change:bplocation change:location change:pricelist change:pricelistversion', function() {
-      var name = '';
-      var clientname = '';
-      var orgname = '';
-      var pricelistname = '';
-      var currencyname = '';
-      var locationname = '';
-
-      if (this.terminal.get('terminal')) {
-        name = this.terminal.get('terminal')._identifier;
-        clientname = this.terminal.get('terminal')['client' + OB.Constants.FIELDSEPARATOR + '_identifier'];
-        orgname = this.terminal.get('terminal')['organization' + OB.Constants.FIELDSEPARATOR + '_identifier'];
-      }
-      if (this.terminal.get('pricelist')) {
-        pricelistname = this.terminal.get('pricelist')._identifier;
-        currencyname = this.terminal.get('pricelist')['currency' + OB.Constants.FIELDSEPARATOR + '_identifier'];
-      }
-      if (this.terminal.get('location')) {
-        locationname = this.terminal.get('location')._identifier;
-      }
-
-      this.yourterminal.text(name);
-      this.yourcompany.text(orgname);
-      this.yourcompanyproperties.empty().append(B({
-        kind: B.KindJQuery('div'),
-        content: [{
-          kind: B.KindJQuery('div'),
-          content: [{
-            kind: B.KindJQuery('span'),
-            content: [
-            OB.I18N.getLabel('OBPOS_CompanyClient')]
-          }, {
-            kind: B.KindJQuery('span'),
-            attr: {
-              'style': 'font-weight: bold; margin: 0px 0px 0px 5px;'
-            },
-            content: [
-            clientname]
-          }]
-        }, {
-          kind: B.KindJQuery('div'),
-          content: [{
-            kind: B.KindJQuery('span'),
-            content: [
-            OB.I18N.getLabel('OBPOS_CompanyOrg')]
-          }, {
-            kind: B.KindJQuery('span'),
-            attr: {
-              'style': 'font-weight: bold; margin: 0px 0px 0px 5px;'
-            },
-            content: [
-            orgname]
-          }]
-        }, {
-          kind: B.KindJQuery('div'),
-          content: [{
-            kind: B.KindJQuery('span'),
-            content: [
-            OB.I18N.getLabel('OBPOS_CompanyPriceList')]
-          }, {
-            kind: B.KindJQuery('span'),
-            attr: {
-              'style': 'font-weight: bold; margin: 0px 0px 0px 5px;'
-            },
-            content: [
-            pricelistname]
-          }]
-        }, {
-          kind: B.KindJQuery('div'),
-          content: [{
-            kind: B.KindJQuery('span'),
-            content: [
-            OB.I18N.getLabel('OBPOS_CompanyCurrency')]
-          }, {
-            kind: B.KindJQuery('span'),
-            attr: {
-              'style': 'font-weight: bold; margin: 0px 0px 0px 5px;'
-            },
-            content: [
-            currencyname]
-          }]
-        }, {
-          kind: B.KindJQuery('div'),
-          content: [{
-            kind: B.KindJQuery('span'),
-            content: [
-            OB.I18N.getLabel('OBPOS_CompanyLocation')]
-          }, {
-            kind: B.KindJQuery('span'),
-            attr: {
-              'style': 'font-weight: bold; margin: 0px 0px 0px 5px;'
-            },
-            content: [
-            locationname]
-          }]
-        }]
-      }).$el);
-    }, this);
-
-  };
-
-}());
-
+// Container for the whole POS application
 enyo.kind({
   name: 'OB.UI.Terminal',
   classes: 'container',
@@ -240,11 +29,11 @@ enyo.kind({
             style: 'display: inline-block; margin-left: 15px;',
             components: [{
               tag: 'span',
-              style: 'display: inline-block; width: 20px; color: transparent; background-image: url("./img/login-connected.png"); background-repeat: no-repeat; background-position: 2px 3px;',
+              style: 'display: inline-block; width: 20px; color: transparent; background-image: url(\'./img/iconOnline.png\'); background-repeat: no-repeat; background-position: 2px 3px;',
               content: '.',
             }, {
               tag: 'span',
-              content: 'Online'
+              content: 'Online' //TODO: trl
             }]
           }, {
             name: 'terminal',
@@ -264,7 +53,7 @@ enyo.kind({
               classes: 'dropdown-menu',
               style: 'color: black; width: 350px;',
               components: [{
-                style: 'height: 60px; background-repeat: no-repeat; background-position: center center; background-image: url("../../utility/ShowImageLogo?logo=yourcompanymenu");'
+                style: 'height: 60px; background: no-repeat center center url(\'../../utility/ShowImageLogo?logo=yourcompanymenu\');'
               }, {
                 name: 'yourcompanyproperties',
                 style: 'display: block; padding: 10px; float: left; background-color: #FFF899; line-height: 23px;'
@@ -322,7 +111,7 @@ enyo.kind({
           }]
         }],
         makeId: function() {
-        	return 'containerLoading';
+          return 'containerLoading';
         }
       }, {
         makeId: function() {
@@ -349,110 +138,47 @@ enyo.kind({
           role: ctx.role._identifier
         }).render();
       } else {
-        //        this.loggeduser.text('');
-        //        this.loggeduserproperties.empty();
+        this.$.loggeduser.destroyComponents();
+        this.$.loggeduserproperties.destroyComponents();
       }
-
     }, this);
 
-    //    this.terminal.on('change:terminal change:bplocation change:location change:pricelist change:pricelistversion', function() {
-    //      var name = '';
-    //      var clientname = '';
-    //      var orgname = '';
-    //      var pricelistname = '';
-    //      var currencyname = '';
-    //      var locationname = '';
-    //
-    //      if (this.terminal.get('terminal')) {
-    //        name = this.terminal.get('terminal')._identifier;
-    //        clientname = this.terminal.get('terminal')['client' + OB.Constants.FIELDSEPARATOR + '_identifier'];
-    //        orgname = this.terminal.get('terminal')['organization' + OB.Constants.FIELDSEPARATOR + '_identifier'];
-    //      }
-    //      if (this.terminal.get('pricelist')) {
-    //        pricelistname = this.terminal.get('pricelist')._identifier;
-    //        currencyname = this.terminal.get('pricelist')['currency' + OB.Constants.FIELDSEPARATOR + '_identifier'];
-    //      }
-    //      if (this.terminal.get('location')) {
-    //        locationname = this.terminal.get('location')._identifier;
-    //      }
-    //
-    //      this.yourterminal.text(name);
-    //      this.yourcompany.text(orgname);
-    //      this.yourcompanyproperties.empty().append(B({
-    //        kind: B.KindJQuery('div'),
-    //        content: [{
-    //          kind: B.KindJQuery('div'),
-    //          content: [{
-    //            kind: B.KindJQuery('span'),
-    //            content: [
-    //            OB.I18N.getLabel('OBPOS_CompanyClient')]
-    //          }, {
-    //            kind: B.KindJQuery('span'),
-    //            attr: {
-    //              'style': 'font-weight: bold; margin: 0px 0px 0px 5px;'
-    //            },
-    //            content: [
-    //            clientname]
-    //          }]
-    //        }, {
-    //          kind: B.KindJQuery('div'),
-    //          content: [{
-    //            kind: B.KindJQuery('span'),
-    //            content: [
-    //            OB.I18N.getLabel('OBPOS_CompanyOrg')]
-    //          }, {
-    //            kind: B.KindJQuery('span'),
-    //            attr: {
-    //              'style': 'font-weight: bold; margin: 0px 0px 0px 5px;'
-    //            },
-    //            content: [
-    //            orgname]
-    //          }]
-    //        }, {
-    //          kind: B.KindJQuery('div'),
-    //          content: [{
-    //            kind: B.KindJQuery('span'),
-    //            content: [
-    //            OB.I18N.getLabel('OBPOS_CompanyPriceList')]
-    //          }, {
-    //            kind: B.KindJQuery('span'),
-    //            attr: {
-    //              'style': 'font-weight: bold; margin: 0px 0px 0px 5px;'
-    //            },
-    //            content: [
-    //            pricelistname]
-    //          }]
-    //        }, {
-    //          kind: B.KindJQuery('div'),
-    //          content: [{
-    //            kind: B.KindJQuery('span'),
-    //            content: [
-    //            OB.I18N.getLabel('OBPOS_CompanyCurrency')]
-    //          }, {
-    //            kind: B.KindJQuery('span'),
-    //            attr: {
-    //              'style': 'font-weight: bold; margin: 0px 0px 0px 5px;'
-    //            },
-    //            content: [
-    //            currencyname]
-    //          }]
-    //        }, {
-    //          kind: B.KindJQuery('div'),
-    //          content: [{
-    //            kind: B.KindJQuery('span'),
-    //            content: [
-    //            OB.I18N.getLabel('OBPOS_CompanyLocation')]
-    //          }, {
-    //            kind: B.KindJQuery('span'),
-    //            attr: {
-    //              'style': 'font-weight: bold; margin: 0px 0px 0px 5px;'
-    //            },
-    //            content: [
-    //            locationname]
-    //          }]
-    //        }]
-    //      }).$el);
-    //    }, this);
+
+    this.terminal.on('change:terminal change:bplocation change:location change:pricelist change:pricelistversion', function() {
+      var name = '';
+      var clientname = '';
+      var orgname = '';
+      var pricelistname = '';
+      var currencyname = '';
+      var locationname = '';
+
+      if (this.terminal.get('terminal')) {
+        name = this.terminal.get('terminal')._identifier;
+        clientname = this.terminal.get('terminal')['client' + OB.Constants.FIELDSEPARATOR + '_identifier'];
+        orgname = this.terminal.get('terminal')['organization' + OB.Constants.FIELDSEPARATOR + '_identifier'];
+      }
+      if (this.terminal.get('pricelist')) {
+        pricelistname = this.terminal.get('pricelist')._identifier;
+        currencyname = this.terminal.get('pricelist')['currency' + OB.Constants.FIELDSEPARATOR + '_identifier'];
+      }
+      if (this.terminal.get('location')) {
+        locationname = this.terminal.get('location')._identifier;
+      }
+
+      this.$.terminal.setContent(name);
+      
+      this.$.yourcompany.setContent(orgname);
+      this.$.yourcompanyproperties.destroyComponents();
+      this.$.yourcompanyproperties.createComponent({
+        kind: 'OB.UI.Terminal.CompanyWidget',
+        clientName: clientname,
+        orgName: orgname,
+        priceName: pricelistname,
+        currencyName: currencyname,
+        locationName: locationname
+      }).render();
+
+    }, this);
   }
 
 });
@@ -501,7 +227,7 @@ enyo.kind({
       //TODO: OB.I18N.getLabel('OBPOS_LblProfile'),
       tap: function() {
         console.log('tap');
-        $('#profileDialog').modal('show'); //TODO: add profileDialog
+        $('#profileDialog').modal('show'); 
       }
     }, {
       style: 'height: 5px;'
@@ -511,7 +237,7 @@ enyo.kind({
       //TODO: OB.I18N.getLabel('OBPOS_LblProfile'),
       tap: function() {
         console.log('tap');
-        $('#logoutDialog').modal('show'); //TODO: add profileDialog
+        $('#logoutDialog').modal('show'); 
       }
     }, {
       style: 'height: 5px;'
@@ -521,5 +247,72 @@ enyo.kind({
     this.inherited(arguments);
     this.$.username.setContent(this.username);
     this.$.role.setContent(this.role);
+  }
+});
+
+enyo.kind({
+  name: 'OB.UI.Terminal.CompanyWidget',
+  components: [{
+    components: [{
+      tag: 'span',
+      name: 'clientLbl'
+    }, {
+      tag: 'span',
+      name: 'clientName',
+      style: 'font-weight: bold; margin: 0px 0px 0px 5px;'
+    }]
+  }, {
+    components: [{
+      tag: 'span',
+      name: 'orgLbl'
+    }, {
+      tag: 'span',
+      name: 'orgName',
+      style: 'font-weight: bold; margin: 0px 0px 0px 5px;'
+    }]
+  }, {
+    components: [{
+      tag: 'span',
+      name: 'priceLbl'
+    }, {
+      tag: 'span',
+      name: 'priceName',
+      style: 'font-weight: bold; margin: 0px 0px 0px 5px;'
+    }]
+  }, {
+    components: [{
+      tag: 'span',
+      name: 'currencyLbl'
+    }, {
+      tag: 'span',
+      name: 'currencyName',
+      style: 'font-weight: bold; margin: 0px 0px 0px 5px;'
+    }]
+  }, {
+    components: [{
+      tag: 'span',
+      name: 'locationLbl'
+    }, {
+      tag: 'span',
+      name: 'locationName',
+      style: 'font-weight: bold; margin: 0px 0px 0px 5px;'
+    }]
+  }],
+  initComponents: function() {
+    this.inherited(arguments);
+    this.$.clientLbl.setContent(OB.I18N.getLabel('OBPOS_CompanyClient'));
+    this.$.clientName.setContent(this.clientName);
+
+    this.$.orgLbl.setContent(OB.I18N.getLabel('OBPOS_CompanyOrg'));
+    this.$.orgName.setContent(this.orgName);
+
+    this.$.priceLbl.setContent(OB.I18N.getLabel('OBPOS_CompanyPriceList'));
+    this.$.priceName.setContent(this.priceName);
+
+    this.$.currencyLbl.setContent(OB.I18N.getLabel('OBPOS_CompanyCurrency'));
+    this.$.currencyName.setContent(this.currencyName);
+
+    this.$.locationLbl.setContent(OB.I18N.getLabel('OBPOS_CompanyLocation'));
+    this.$.locationName.setContent(this.locationName);
   }
 });
