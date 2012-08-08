36b7d67a7b5dcc98c44ea0dc82b58e9bd4ffb1e7
Asier Lostalé <asier.lostale@openbravo.com>
2012-08-08 11:52:59 +0200

    [refactor] start populating keyboard

--- web/org.openbravo.retail.posterminal/js/components/keyboardorder.js
+++ web/org.openbravo.retail.posterminal/js/components/keyboardorder.js
@@ -11,19 +11,131 @@
 
 
 enyo.kind({
- name: 'OB.UI.KeyboardOrder',
- kind: 'OB.UI.Keyboard'
+  name: 'OB.UI.KeyboardOrder',
+  kind: 'OB.UI.Keyboard',
+  sideBarEnabled: true,
+
+
+  initComponents: function() {
+
+    this.addCommand('line:qty', {
+      action: function(txt) {
+        console.log('qty', txt);
+      }
+    });
+
+    this.addCommand('line:price', {
+      permission: 'OBPOS_order.changePrice',
+      action: function(txt) {
+        console.log('price', txt);
+        //        if (this.line) {
+        //          this.receipt.setPrice(this.line, OB.I18N.parseNumber(txt));
+        //          this.receipt.trigger('scan');
+        //        }
+      }
+    });
+    this.addCommand('line:dto', {
+      permission: 'OBPOS_order.discount',
+      action: function(txt) {
+        console.log('dto', txt);
+        //        if (this.line) {
+        //          this.receipt.trigger('discount', this.line, OB.I18N.parseNumber(txt));
+        //        }
+      }
+    });
+    this.addCommand('code', {
+      action: function(txt) {
+        console.log('code', txt);
+        //        var criteria, me = this;
+        //
+        //        function successCallbackPrices(dataPrices, dataProducts) {
+        //          if (dataPrices) {
+        //            _.each(dataPrices.models, function(currentPrice) {
+        //              if (dataProducts.get(currentPrice.get('product'))) {
+        //                dataProducts.get(currentPrice.get('product')).set('price', currentPrice);
+        //              }
+        //            });
+        //            _.each(dataProducts.models, function(currentProd) {
+        //              if (currentProd.get('price') === undefined) {
+        //                var price = new OB.Model.ProductPrice({
+        //                  'listPrice': 0
+        //                });
+        //                dataProducts.get(currentProd.get('id')).set('price', price);
+        //                OB.UTIL.showWarning("No price found for product " + currentProd.get('_identifier'));
+        //              }
+        //            });
+        //          } else {
+        //            OB.UTIL.showWarning("OBDAL No prices found for products");
+        //            _.each(dataProducts.models, function(currentProd) {
+        //              var price = new OB.Model.ProductPrice({
+        //                'listPrice': 0
+        //              });
+        //              currentProd.set('price', price);
+        //            });
+        //          }
+        //          me.receipt.addProduct(new Backbone.Model(dataProducts.at(0)));
+        //          me.receipt.trigger('scan');
+        //        }
+        //
+        //        function errorCallback(tx, error) {
+        //          OB.UTIL.showError("OBDAL error: " + error);
+        //        }
+        //
+        //        function successCallbackProducts(dataProducts) {
+        //          if (dataProducts && dataProducts.length > 0) {
+        //            criteria = {
+        //              'priceListVersion': OB.POS.modelterminal.get('pricelistversion').id
+        //            };
+        //            OB.Dal.find(OB.Model.ProductPrice, criteria, successCallbackPrices, errorCallback, dataProducts);
+        //          } else {
+        //            // 'UPC/EAN code not found'
+        //            OB.UTIL.showWarning(OB.I18N.getLabel('OBPOS_KbUPCEANCodeNotFound', [txt]));
+        //          }
+        //        }
+        //
+        //        criteria = {
+        //          'uPCEAN': txt
+        //        };
+        //        OB.Dal.find(OB.Model.Product, criteria, successCallbackProducts, errorCallback);
+      }
+    });
+    this.addCommand('+', {
+      stateless: true,
+      action: function(txt) {
+        console.log('+', txt);
+        //        if (this.line) {
+        //          this.receipt.addUnit(this.line, OB.I18N.parseNumber(txt));
+        //          this.receipt.trigger('scan');
+        //        }
+      }
+    });
+    this.addCommand('-', {
+      stateless: true,
+      action: function(txt) {
+        console.log('-', txt);
+        //        if (this.line) {
+        //          this.receipt.removeUnit(this.line, OB.I18N.parseNumber(txt));
+        //          this.receipt.trigger('scan');
+        //        }
+      }
+    });
+
+    // calling super after setting keyboard properties
+    this.inherited(arguments);
+    
+    this.addToolbar([]);
+  }
 });
 
-(function () {
+(function() {
 
   OB = window.OB || {};
   OB.COMP = window.OB.COMP || {};
 
   OB.COMP.KeyboardOrder = OB.COMP.Keyboard.extend({
-    initialize: function () {
+    initialize: function() {
       this.addCommand('line:qty', {
-        'action': function (txt) {
+        'action': function(txt) {
           if (this.line) {
             this.receipt.setUnit(this.line, OB.I18N.parseNumber(txt));
             this.receipt.trigger('scan');
@@ -32,7 +144,7 @@ enyo.kind({
       });
       this.addCommand('line:price', {
         'permission': 'OBPOS_order.changePrice',
-        'action': function (txt) {
+        'action': function(txt) {
           if (this.line) {
             this.receipt.setPrice(this.line, OB.I18N.parseNumber(txt));
             this.receipt.trigger('scan');
@@ -41,24 +153,24 @@ enyo.kind({
       });
       this.addCommand('line:dto', {
         'permission': 'OBPOS_order.discount',
-        'action': function (txt) {
+        'action': function(txt) {
           if (this.line) {
             this.receipt.trigger('discount', this.line, OB.I18N.parseNumber(txt));
           }
         }
       });
       this.addCommand('code', {
-        'action': function (txt) {
+        'action': function(txt) {
           var criteria, me = this;
 
           function successCallbackPrices(dataPrices, dataProducts) {
             if (dataPrices) {
-              _.each(dataPrices.models, function (currentPrice) {
+              _.each(dataPrices.models, function(currentPrice) {
                 if (dataProducts.get(currentPrice.get('product'))) {
                   dataProducts.get(currentPrice.get('product')).set('price', currentPrice);
                 }
               });
-              _.each(dataProducts.models, function (currentProd) {
+              _.each(dataProducts.models, function(currentProd) {
                 if (currentProd.get('price') === undefined) {
                   var price = new OB.Model.ProductPrice({
                     'listPrice': 0
@@ -69,7 +181,7 @@ enyo.kind({
               });
             } else {
               OB.UTIL.showWarning("OBDAL No prices found for products");
-              _.each(dataProducts.models, function (currentProd) {
+              _.each(dataProducts.models, function(currentProd) {
                 var price = new OB.Model.ProductPrice({
                   'listPrice': 0
                 });
@@ -104,7 +216,7 @@ enyo.kind({
       });
       this.addCommand('+', {
         'stateless': true,
-        'action': function (txt) {
+        'action': function(txt) {
           if (this.line) {
             this.receipt.addUnit(this.line, OB.I18N.parseNumber(txt));
             this.receipt.trigger('scan');
@@ -113,7 +225,7 @@ enyo.kind({
       });
       this.addCommand('-', {
         'stateless': true,
-        'action': function (txt) {
+        'action': function(txt) {
           if (this.line) {
             this.receipt.removeUnit(this.line, OB.I18N.parseNumber(txt));
             this.receipt.trigger('scan');
@@ -125,13 +237,12 @@ enyo.kind({
       this.receipt = this.options.root.modelorder;
       this.line = null;
 
-      this.receipt.get('lines').on('selected', function (line) {
+      this.receipt.get('lines').on('selected', function(line) {
         this.line = line;
         this.clear();
       }, this);
 
       OB.COMP.Keyboard.prototype.initialize.call(this); // super.initialize();
-
       // First the keypads
       this.addKeypad(OB.COMP.KeypadCoins);
 
