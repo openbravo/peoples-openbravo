13aaecf3433c4501ae64215832af82ab07358b19
Asier Lostalé <asier.lostale@openbravo.com>
2012-08-09 16:40:45 +0200

    [refactor] discounts

--- web/org.openbravo.retail.posterminal/js/data/dataorderdiscount.js
+++ web/org.openbravo.retail.posterminal/js/data/dataorderdiscount.js
@@ -9,25 +9,20 @@
 
 /*global B */
 
-(function () {
+(function() {
 
   OB = window.OB || {};
   OB.DATA = window.OB.DATA || {};
 
-  OB.DATA.OrderDiscount = function(context) {
-    this._id = 'logicOrderDiscounts';
-
-    this.receipt =  context.modelorder;
-
-    this.receipt.on('discount', function(line, percentage) {
+  OB.DATA.OrderDiscount = function(receipt) {
+    receipt.on('discount', function(line, percentage) {
 
       if (line) {
         if (OB.DEC.compare(percentage) > 0 && OB.DEC.compare(OB.DEC.sub(percentage, OB.DEC.number(100))) <= 0) {
-          this.receipt.setPrice(line, OB.DEC.div(
-              OB.DEC.mul(line.get('priceList'), OB.DEC.sub(OB.DEC.number(100), percentage)),
-              OB.DEC.number(100)));
+          receipt.setPrice(line, OB.DEC.div(
+          OB.DEC.mul(line.get('priceList'), OB.DEC.sub(OB.DEC.number(100), percentage)), OB.DEC.number(100)));
         } else if (OB.DEC.compare(percentage) === 0) {
-          this.receipt.setPrice(line, line.get('priceList'));
+          receipt.setPrice(line, line.get('priceList'));
         }
       }
     }, this);
--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
@@ -6,8 +6,11 @@ OB.OBPOSPointOfSale.UI = OB.OBPOSPointOfSale.UI || {};
 OB.OBPOSPointOfSale.Model.PointOfSale = OB.Model.WindowModel.extend({
   models: [OB.Model.TaxRate, OB.Model.Product, OB.Model.ProductPrice, OB.Model.ProductCategory, OB.Model.BusinessPartner, OB.Model.Order, OB.Model.DocumentSequence],
   init: function() {
-    debugger;
-    this.set('order', new OB.Model.Order());
+    var modelOrder = new OB.Model.Order(), discounts;
+    this.set('order', modelOrder);
     this.set('orderList', new OB.Collection.OrderList());
+    
+    discounts = new OB.DATA.OrderDiscount(modelOrder);
+    
   }
 });
