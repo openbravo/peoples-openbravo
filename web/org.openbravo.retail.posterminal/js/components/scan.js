320492accca0be02b039974e8096f01deba75edc
Asier Lostalé <asier.lostale@openbravo.com>
2012-08-10 17:02:59 +0200

    [refactor] removed old scan impl

--- web/org.openbravo.retail.posterminal/js/components/scan.js
+++ web/org.openbravo.retail.posterminal/js/components/scan.js
@@ -7,9 +7,6 @@
  ************************************************************************************
  */
 
-/*global setInterval, Backbone */
-
-
 enyo.kind({
   name: 'OB.UI.Scan',
   published: {
@@ -75,100 +72,4 @@ enyo.kind({
       delete this.$.undobutton.undoclick;
     }
   }
-
 });
-
-(function() {
-
-  OB = window.OB || {};
-  OB.COMP = window.OB.COMP || {};
-
-  OB.COMP.Scan = Backbone.View.extend({
-    tagName: 'div',
-    contentView: [{
-      tag: 'div',
-      attributes: {
-        'style': 'position:relative; background-color: #7da7d9; background-size: cover; color: white; height: 200px; margin: 5px; padding: 5px'
-      },
-      content: [{
-        view: OB.COMP.Clock.extend({
-          'className': 'pos-clock'
-        })
-      }, {
-        tag: 'div',
-        content: [{
-          tag: 'div',
-          id: 'msgwelcome',
-          attributes: {
-            'style': 'padding: 10px; display: none;'
-          },
-          content: [{
-            tag: 'div',
-            attributes: {
-              'style': 'float:right;'
-            },
-            content: [
-            OB.I18N.getLabel('OBPOS_WelcomeMessage')]
-          }]
-        }, {
-          tag: 'div',
-          id: 'msgaction',
-          attributes: {
-            'style': 'display: none;'
-          },
-          content: [{
-            tag: 'div',
-            id: 'txtaction',
-            attributes: {
-              'style': 'padding: 10px; float: left; width: 320px; line-height: 23px;'
-            }
-          }, {
-            tag: 'div',
-            attributes: {
-              'style': 'float: right;'
-            },
-            content: [{
-              view: OB.COMP.SmallButton.extend({
-                'label': OB.I18N.getLabel('OBPOS_LblUndo'),
-                'className': 'btnlink-white btnlink-fontblue'
-              }),
-              id: 'btnundo'
-            }]
-          }]
-        }]
-      }]
-    }],
-    initialize: function() {
-
-      OB.UTIL.initContentView(this);
-      var me = this;
-      this.undoclick = null;
-      this.receipt = this.options.root.modelorder;
-
-      this.btnundo.clickEvent = function() {
-        if (me.undoclick) {
-          me.undoclick();
-        }
-      };
-
-      this.receipt.on('clear change:undo', function() {
-        this.render();
-      }, this);
-    },
-    render: function() {
-      var undoaction = this.receipt.get('undo');
-      if (undoaction) {
-        this.msgwelcome.hide();
-        this.msgaction.show();
-        this.txtaction.text(undoaction.text);
-        this.undoclick = undoaction.undo;
-      } else {
-        this.msgaction.hide();
-        this.msgwelcome.show();
-        this.undoclick = null;
-      }
-      return this;
-    }
-  });
-
-}());
