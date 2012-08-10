73a2bec5712cae0f09245c5137bd28d2fe5f15a2
Asier Lostalé <asier.lostale@openbravo.com>
2012-08-10 12:08:51 +0200

    [refactor] load orders

--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
@@ -5,13 +5,78 @@ OB.OBPOSPointOfSale.UI = OB.OBPOSPointOfSale.UI || {};
 //Window model
 OB.OBPOSPointOfSale.Model.PointOfSale = OB.Model.WindowModel.extend({
   models: [OB.Model.TaxRate, OB.Model.Product, OB.Model.ProductPrice, OB.Model.ProductCategory, OB.Model.BusinessPartner, OB.Model.Order, OB.Model.DocumentSequence],
+
+  loadUnpaidOrders: function() {
+    // Shows a modal window with the orders pending to be paid
+    var orderlist = this.get('orderList');
+    criteria = {
+      'hasbeenpaid': 'N'
+    };
+    OB.Dal.find(OB.Model.Order, criteria, function(ordersNotPaid) { //OB.Dal.find success
+      var currentOrder = {},
+          loadOrderStr;
+      if (!ordersNotPaid || ordersNotPaid.length === 0) {
+        // If there are no pending orders,
+        //  add an initial empty order
+        orderlist.addNewOrder();
+      } else {
+        // The order object is stored in the json property of the row fetched from the database
+        orderlist.reset(ordersNotPaid.models);
+        // At this point it is sure that there exists at least one order
+        currentOrder = ordersNotPaid.models[0];
+        orderlist.load(currentOrder);
+        loadOrderStr = OB.I18N.getLabel('OBPOS_Order') + currentOrder.get('documentNo') + OB.I18N.getLabel('OBPOS_Loaded');
+        OB.UTIL.showAlert(loadOrderStr, OB.I18N.getLabel('OBUIAPP_Info'));
+      }
+    }, function() { //OB.Dal.find error
+      // If there is an error fetching the pending orders,
+      // add an initial empty order
+      orderlist.addNewOrder();
+    });
+
+  },
+
   init: function() {
-    var modelOrder = new OB.Model.Order(), discounts, me = this;
+    var modelOrder = new OB.Model.Order(),
+        discounts, ordersave;
     this.set('order', modelOrder);
-    this.set('orderList', new OB.Collection.OrderList(this.get('order')));
-    
-    this.get('orderList').addNewOrder();
+    this.set('orderList', new OB.Collection.OrderList(modelOrder));
+
     discounts = new OB.DATA.OrderDiscount(modelOrder);
-    
+    ordersave = new OB.DATA.OrderSave(this);
+
+
+    var loadUnpaidOrders;
+    //   modelterminal.saveDocumentSequenceInDB();
+
+    processPaidOrders = function() {
+      // Processes the paid, unprocessed orders
+      var orderlist = me.context.modelorderlist,
+          criteria = {
+          hasbeenpaid: 'Y'
+          };
+      if (OB.POS.modelterminal.get('connectedToERP')) {
+        OB.Dal.find(OB.Model.Order, criteria, function(ordersPaidNotProcessed) { //OB.Dal.find success
+          var successCallback, errorCallback;
+          if (!ordersPaidNotProcessed) {
+            return;
+          }
+          successCallback = function() {
+            $('.alert:contains("' + OB.I18N.getLabel('OBPOS_ProcessPendingOrders') + '")').alert('close');
+            OB.UTIL.showSuccess(OB.I18N.getLabel('OBPOS_MsgSuccessProcessOrder'));
+          };
+          errorCallback = function() {
+            $('.alert:contains("' + OB.I18N.getLabel('OBPOS_ProcessPendingOrders') + '")').alert('close');
+            OB.UTIL.showError(OB.I18N.getLabel('OBPOS_MsgErrorProcessOrder'));
+          };
+          OB.UTIL.showAlert(OB.I18N.getLabel('OBPOS_ProcessPendingOrders'), OB.I18N.getLabel('OBUIAPP_Info'));
+          OB.UTIL.processOrders(me.context, ordersPaidNotProcessed, successCallback, errorCallback);
+        });
+      }
+    };
+
+    //processPaidOrders();
+    this.loadUnpaidOrders();
+
   }
 });
