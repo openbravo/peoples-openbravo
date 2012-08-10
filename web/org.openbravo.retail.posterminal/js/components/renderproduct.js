02b19f6f788243fb5b5cbc533eca37688b356b73
Asier Lostalé <asier.lostale@openbravo.com>
2012-08-10 17:34:24 +0200

    [refactor] removed old impl of render prod and cat

--- web/org.openbravo.retail.posterminal/js/components/renderproduct.js
+++ web/org.openbravo.retail.posterminal/js/components/renderproduct.js
@@ -7,81 +7,30 @@
  ************************************************************************************
  */
 
-/*global B, $ */
+enyo.kind({
+  name: 'OB.UI.RenderProduct',
 
-(function() {
-
-  OB = window.OB || {};
-  OB.COMP = window.OB.COMP || {};
-
-  enyo.kind({
-    name: 'OB.UI.RenderProduct',
-
-    kind: 'OB.UI.SelectButton',
+  kind: 'OB.UI.SelectButton',
+  components: [{
+    style: 'float: left; width: 25%',
     components: [{
-      style: 'float: left; width: 25%',
-      components: [{
-        kind: 'OB.UI.Thumbnail'
-      }]
-    }, {
-      style: 'float: left; width: 55%;',
-      components: [{
-        name: 'identifier',
-        style: 'padding-left: 5px;'
-      }]
-    },{
-    	name: 'price',
-    	style: 'float: left; width: 20%; text-align: right; font-weight:bold;'
-    },{
-    	style:'clear:both;'
-    }],
-    initComponents: function() {
-      this.inherited(arguments);
-      this.$.identifier.setContent(this.model.get('_identifier'));
-      this.$.price.setContent(OB.I18N.formatCurrency(this.model.get('price').get('listPrice')));
-    }
-  });
-
-  OB.COMP.RenderProduct = OB.COMP.SelectButton.extend({
-    contentView: [{
-      tag: 'div',
-      attributes: {
-        style: 'float: left; width: 25%'
-      },
-      content: [{
-        id: 'viewthumbnail',
-        view: OB.UTIL.Thumbnail
-      }]
-    }, {
-      tag: 'div',
-      attributes: {
-        style: 'float: left; width: 55%;'
-      },
-      content: [{
-        id: 'dividentifier',
-        tag: 'div',
-        attributes: {
-          style: 'padding-left: 5px;'
-        }
-      }]
-    }, {
-      id: 'divprice',
-      tag: 'div',
-      attributes: {
-        style: 'float: left; width: 20%; text-align: right; font-weight:bold;'
-      }
-    }, {
-      tag: 'div',
-      attributes: {
-        style: 'clear: both;'
-      }
-    }],
-    render: function() {
-      this.viewthumbnail.img = this.model.get('img');
-      this.viewthumbnail.render();
-      this.dividentifier.text(this.model.get('_identifier'));
-      this.divprice.text(OB.I18N.formatCurrency(this.model.get('price').get('listPrice')));
-      return this;
-    }
-  });
-}());
+      kind: 'OB.UI.Thumbnail'
+    }]
+  }, {
+    style: 'float: left; width: 55%;',
+    components: [{
+      name: 'identifier',
+      style: 'padding-left: 5px;'
+    }]
+  }, {
+    name: 'price',
+    style: 'float: left; width: 20%; text-align: right; font-weight:bold;'
+  }, {
+    style: 'clear:both;'
+  }],
+  initComponents: function() {
+    this.inherited(arguments);
+    this.$.identifier.setContent(this.model.get('_identifier'));
+    this.$.price.setContent(OB.I18N.formatCurrency(this.model.get('price').get('listPrice')));
+  }
+});
