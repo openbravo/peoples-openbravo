98ed6bb80510ce42dcdc13d0c9e57b2da589b4c6
Guillermo Álvarez de Eulate <guillermo.alvarez@openbravo.com>
2012-08-14 15:56:47 +0200

    count cash UI has been corrected

--- web/org.openbravo.retail.posterminal/js/closecash/view/tabcountcash.js
+++ web/org.openbravo.retail.posterminal/js/closecash/view/tabcountcash.js
@@ -34,49 +34,59 @@
 enyo.kind({
   name: 'OB.OBPOSCashUp.UI.RenderPaymentsLine',
   components: [{
-    classes: 'display: table-row; height: 42px;',
     components: [{
-      name: 'name',
-      style: 'padding: 10px 20px 10px 10px; float: left; width: 10%'
-    }, {
-      name: 'expected',
-      style: 'padding: 10px 20px 10px 10px; float: left; width: 20%'
-    }, {
-      style: 'float: left;',
+      classes: 'row-fluid',
       components: [{
-        name: 'buttonEdit',
-        kind: 'OB.UI.SmallButton',
-        classes: 'btnlink-orange btnlink-cashup-ok btn-icon-small btn-icon-edit',
-        ontap: 'lineEdit'
+        classes: 'span12',
+        style: 'border-bottom: 1px solid #cccccc;',
+        components: [{
+          name: 'name',
+          style: 'padding: 10px 20px 10px 10px; float: left; width: 20%'
+        }, {
+          name: 'expected',
+          style: 'padding: 10px 20px 10px 10px; float: left; width: 20%'
+        }, {
+          style: 'float: left;',
+          components: [{
+            name: 'buttonEdit',
+            kind: 'OB.UI.SmallButton',
+            classes: 'btnlink-orange btnlink-cashup-edit btn-icon-small btn-icon-edit',
+            ontap: 'lineEdit'
+          }]
+        }, {
+          style: 'float: left;',
+          components: [{
+            name: 'buttonOk',
+            kind: 'OB.UI.SmallButton',
+            classes: 'btnlink-green btnlink-cashup-ok btn-icon-small btn-icon-check',
+            ontap: 'lineOK'
+          }]
+        }, {
+          name: 'counted',
+          style: 'float: left; padding: 10px 0px 10px 10px;',
+          showing: false
+        }]
       }]
-    }, {
-      style: 'float: left;',
-      components: [{
-        name: 'buttonOk',
-        kind: 'OB.UI.SmallButton',
-        classes: 'btnlink-green btnlink-cashup-ok btn-icon-small btn-icon-check',
-        ontap: 'lineOK'
-      }]
-    }, {
-      name: 'counted',
-      style: 'float: left; padding: 10px 0px 10px 10px;',
-      showing: false
     }]
   }],
-  create: function () {
+  create: function() {
     this.inherited(arguments);
     this.$.name.setContent(this.model.get('name'));
     this.$.expected.setContent(OB.I18N.formatCurrency(OB.DEC.add(0, this.model.get('expected'))));
   },
-  lineEdit: function (inSender, inEvent) {
+  lineEdit: function(inSender, inEvent) {
     this.log('lineEdit');
   },
-  lineOK: function (inSender, inEvent) {
+  lineOK: function(inSender, inEvent) {
     debugger;
-    this.model.set('counted', this.model.get('expected'));
+    this.model.set('counted', this.model.get('expected'), {
+      silent: true
+    });
     this.$.counted.setContent(OB.I18N.formatCurrency(OB.DEC.add(0, this.model.get('counted'))));
     this.$.counted.setShowing(true);
-    this.doLineOK({model: this.model}); // add this counted to window model totalCounted
+    this.doLineOK({
+      model: this.model
+    }); // add this counted to window model totalCounted
   }
 });
 
@@ -86,10 +96,10 @@ enyo.kind({
   published: {
     total: OB.DEC.Zero
   },
-  create: function () {
+  create: function() {
     this.inherited(arguments);
   },
-  totalChanged: function (oldValue) {
+  totalChanged: function(oldValue) {
     this.setContent(this.total);
     if (OB.DEC.compare(this.total) < 0) {
       this.applyStyle('color', 'red');
@@ -117,51 +127,79 @@ enyo.kind({
             }]
           }]
         }, {
-          classes: 'row-fluid',
+          style: 'background-color: #ffffff; color: black;',
           components: [{
-            classes: 'span12',
-            style: 'border-bottom: 1px solid #cccccc;',
-            components: [{
-              style: 'padding: 10px 20px 10px 10px; float: left; width: 20%',
-              content: OB.I18N.getLabel('OBPOS_LblPaymentMethod')
-            }, {
-              style: 'padding: 10px 20px 10px 10px; float: left; width: 20%',
-              content: OB.I18N.getLabel('OBPOS_LblExpected')
-            }, {
-              style: 'padding: 10px 0px 10px 0px;  float: left;',
-              content: OB.I18N.getLabel('OBPOS_LblCounted')
-            }]
-          }]
-        }, {
-          classes: 'row-fluid',
-          components: [{
-            style: 'span12',
             components: [{
               classes: 'row-fluid',
               components: [{
-                name: 'paymentsList',
-                kind: 'OB.UI.Table',
-                renderLine: 'OB.OBPOSCashUp.UI.RenderPaymentsLine',
-                renderEmpty: 'OB.UI.RenderEmpty',
-                listStyle: 'list'
+                classes: 'span12',
+                style: 'border-bottom: 1px solid #cccccc;',
+                components: [{
+                  style: 'padding: 10px 20px 10px 10px; float: left; width: 20%',
+                  content: OB.I18N.getLabel('OBPOS_LblPaymentMethod')
+                }, {
+                  style: 'padding: 10px 20px 10px 10px; float: left; width: 20%',
+                  content: OB.I18N.getLabel('OBPOS_LblExpected')
+                }, {
+                  style: 'padding: 10px 0px 10px 0px;  float: left;',
+                  content: OB.I18N.getLabel('OBPOS_LblCounted')
+                }]
               }]
-            }]
-          }]
-        }, {
-          classes: 'row-fluid',
-          components: [{
-            classes: 'span12',
-            style: 'padding: 10px 5px 10px 0px; float: left; ',
-            components: [{
-              name: 'totalLbl',
-              style: 'padding: 10px 20px 10px 10px; float: left; width: 70%; font-weight:bold;',
-              content: OB.I18N.getLabel('OBPOS_ReceiptTotal')
             }, {
-              style: 'padding: 10px 20px 10px 0px;  float: right;',
+              name: 'paymentsList',
+              kind: 'OB.UI.Table',
+              renderLine: 'OB.OBPOSCashUp.UI.RenderPaymentsLine',
+              renderEmpty: 'OB.UI.RenderEmpty',
+              listStyle: 'list'
+            }, {
               components: [{
-                name: 'total',
-                kind: 'OB.OBPOSCashUp.UI.RenderTotal',
-                style: 'float:right; font-weight: bold;'
+                classes: 'row-fluid',
+                components: [{
+                  classes: 'span12',
+                  style: 'border-bottom: 1px solid #cccccc;',
+                  components: [{
+                    style: 'padding: 10px 20px 10px 10px;  float: left; width: 20%;'
+                  }, {
+                    style: 'padding: 10px 20px 10px 10px;  float: left; width: 20%;'
+                  }, {
+                    style: 'padding: 10px 20px 10px 10px;  float: left; width: 30px;'
+                  }, {
+                    style: 'float: left;',
+                    components: [{
+                      name: 'buttonAllOk',
+                      kind: 'OB.UI.SmallButton',
+                      classes: 'btnlink-green btnlink-cashup-ok btn-icon-small btn-icon-check',
+                      ontap: 'lineOK'
+                    }]
+                  }]
+                }]
+              }]
+            }, {
+              classes: 'row-fluid',
+              components: [{
+                classes: 'span12',
+                style: 'border-bottom: 1px solid #cccccc;',
+                components: [{
+                  name: 'totalLbl',
+                  style: 'padding: 10px 20px 10px 10px; float: left; width: 20%;',
+                  content: OB.I18N.getLabel('OBPOS_ReceiptTotal')
+                }, {
+                  style: 'padding: 10px 20px 10px 0px; float: left; width: 20%;',
+                  components: [{
+                    name: 'total',
+                    kind: 'OB.OBPOSCashUp.UI.RenderTotal',
+                    style: 'font-weight: bold;'
+                  }]
+                },{
+                  style: 'padding: 17px 10px 17px 10px; float: left; width: 44px'
+                },{
+                  style: 'padding: 10px 5px 10px 0px; float: left;',
+                  components: [{
+                    name: 'diference',
+                    kind: 'OB.OBPOSCashUp.UI.RenderTotal',
+                    style: 'font-weight: bold;'
+                  }]
+                }]
               }]
             }]
           }]
@@ -169,7 +207,7 @@ enyo.kind({
       }]
     }]
   }],
-  setCollection: function (col) {
+  setCollection: function(col) {
     this.$.paymentsList.setCollection(col);
   }
 });
