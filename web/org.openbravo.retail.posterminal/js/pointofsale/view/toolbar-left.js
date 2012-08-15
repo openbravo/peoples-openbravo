a485f960b6b93727dff06a10f9bdd59dcb48e847
Guillermo Álvarez de Eulate <guillermo.alvarez@openbravo.com>
2012-08-16 00:29:52 +0200

    Adding left toolbar file to repository

---
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/toolbar-left.js
@@ -0,0 +1,85 @@
+/*left toolbar*/
+enyo.kind({
+  name: 'OB.OBPOSPointOfSale.UI.LeftToolbarButton',
+  tag: 'li',
+  classes: 'span3',
+  components: [{
+    name: 'theButton',
+    attributes: {
+      style: 'margin: 0px 5px 0px 5px;'
+    }
+  }],
+  initComponents: function() {
+    this.inherited(arguments);
+    this.$.theButton.createComponent(this.button);
+  }
+});
+
+enyo.kind({
+  name: 'OB.OBPOSPointOfSale.UI.LeftToolbar',
+  classes: 'span4',
+  components: [{
+    tag: 'ul',
+    classes: 'unstyled nav-pos row-fluid',
+    name: 'toolbar'
+  }],
+  initComponents: function() {
+    this.inherited(arguments);
+    enyo.forEach(this.buttons, function(btn) {
+      this.$.toolbar.createComponent({
+        kind: 'OB.OBPOSPointOfSale.UI.LeftToolbarButton',
+        button: btn
+      });
+    }, this);
+  }
+});
+
+enyo.kind({
+  name: 'OB.UI.ButtonNew',
+  kind: 'OB.UI.ToolbarButton',
+  icon: 'btn-icon btn-icon-new',
+  events: {
+    onAddNewOrder: ''
+  },
+  tap: function() {
+    this.doAddNewOrder();
+  }
+});
+
+
+enyo.kind({
+  name: 'OB.UI.ButtonDelete',
+  kind: 'OB.UI.ToolbarButton',
+  icon: 'btn-icon btn-icon-delete',
+  attributes: {
+    href: '#modalConfirmReceiptDelete',
+    'data-toggle': 'modal'
+  }
+});
+
+enyo.kind({
+  name: 'OB.UI.ButtonPrint',
+  kind: 'OB.UI.ToolbarButton',
+  icon: 'btn-icon btn-icon-print',
+  tap: function() {
+    var receipt = this.options.modelorder;
+    receipt.calculateTaxes(function() {
+      receipt.trigger('print');
+    });
+  }
+});
+
+
+enyo.kind({
+  name: 'OB.OBPOSPointOfSale.UI.LeftToolbarImpl',
+  kind: 'OB.OBPOSPointOfSale.UI.LeftToolbar',
+  buttons: [{
+    kind: 'OB.UI.ButtonNew'
+  }, {
+    kind: 'OB.UI.ButtonDelete'
+  }, {
+    kind: 'OB.UI.ButtonPrint'
+  }, {
+    kind: 'OB.OBPOSPointOfSale.UI.StandardMenu'
+  }]
+});
