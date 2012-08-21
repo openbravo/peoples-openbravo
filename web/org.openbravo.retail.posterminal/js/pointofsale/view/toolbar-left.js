4fe662b80be6e4b9096b6f1cc9a69b8180bd6d30
Adrián Romero <adrianromero@openbravo.com>
2012-08-21 14:07:34 +0200

    Fixing jslint warnings

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/toolbar-left.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/toolbar-left.js
@@ -1,3 +1,14 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2012 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+/*global enyo, $ */
+
 /*left toolbar*/
 enyo.kind({
   name: 'OB.OBPOSPointOfSale.UI.LeftToolbarButton',
