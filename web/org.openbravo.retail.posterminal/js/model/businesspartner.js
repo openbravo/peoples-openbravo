83026402b0fbaeb050fa295f453acd303f8d782e
Asier Lostalé <asier.lostale@openbravo.com>
2012-09-19 10:35:00 +0200

    [discounts] Include BP group in Model

--- web/org.openbravo.retail.posterminal/js/model/businesspartner.js
+++ web/org.openbravo.retail.posterminal/js/model/businesspartner.js
@@ -31,6 +31,7 @@
      'invoiceTerms',
      'locId',
      'locName',
+     'businessPartnerCategory',
      '_identifier',
      '_idx'
     ],
@@ -46,12 +47,13 @@
      'invoiceTerms': 'invoicerule',
      'locId': 'c_bpartnerlocation_id',
      'locName': 'c_bpartnerlocation_name',
+     'businessPartnerCategory':'C_BP_Group_ID',
      '_identifier': '_identifier',
      '_idx': '_idx'
     },
-    createStatement: 'CREATE TABLE IF NOT EXISTS c_bpartner (c_bpartner_id TEXT PRIMARY KEY , value TEXT , name TEXT , description TEXT , taxID TEXT , so_bp_taxcategory_id TEXT, FIN_Paymentmethod_ID TEXT, c_paymentterm_id TEXT, invoicerule TEXT, c_bpartnerlocation_id TEXT , c_bpartnerlocation_name TEXT , _identifier TEXT , _idx NUMERIC)',
+    createStatement: 'CREATE TABLE IF NOT EXISTS c_bpartner (c_bpartner_id TEXT PRIMARY KEY , value TEXT , name TEXT , description TEXT , taxID TEXT , so_bp_taxcategory_id TEXT, FIN_Paymentmethod_ID TEXT, c_paymentterm_id TEXT, invoicerule TEXT, c_bpartnerlocation_id TEXT , c_bpartnerlocation_name TEXT , c_bp_group_id TEXT, _identifier TEXT , _idx NUMERIC)',
     dropStatement: 'DROP TABLE IF EXISTS c_bpartner',
-    insertStatement: 'INSERT INTO c_bpartner(c_bpartner_id, value, name, description, taxID, so_bp_taxcategory_id, FIN_Paymentmethod_ID, c_paymentterm_id, invoicerule, c_bpartnerlocation_id, c_bpartnerlocation_name, _identifier, _idx)  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+    insertStatement: 'INSERT INTO c_bpartner(c_bpartner_id, value, name, description, taxID, so_bp_taxcategory_id, FIN_Paymentmethod_ID, c_paymentterm_id, invoicerule, c_bpartnerlocation_id, c_bpartnerlocation_name, c_bp_group_id, _identifier, _idx)  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
     updateStatement: ''
   });
 
