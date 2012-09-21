e970e17d17aa80d06cea6d84294999811a74e1f1
Asier Lostalé <asier.lostale@openbravo.com>
2012-09-21 17:53:32 +0200

    [discount] format discount

--- web/org.openbravo.retail.posterminal/js/components/renderorderline.js
+++ web/org.openbravo.retail.posterminal/js/components/renderorderline.js
@@ -61,7 +61,7 @@ enyo.kind({
               style: 'float: left; width: 60%;'
             }
           }, {
-            content: -d.gross,
+            content: OB.I18N.formatCurrency(-d.gross),
             attributes: {
               style: 'float: right; width: 20%; text-align: right;'
             }
