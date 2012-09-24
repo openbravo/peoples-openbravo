c21134c38f6d2404eed9df7c75a70b50e7b6607f
Augusto Mauch <augusto.mauch@openbravo.com>
2012-09-24 13:00:34 +0200

    Fixes issue 21687: Alerts are not executed when the system is being rebuilt
    If the system is being rebuilt, there is no point in executing the alerts, so now if the system is being rebuilt using the ERP, the alerts will not be executed.
    
    --HG--
    extra : rebase_source : 5941f2d2ffc3b5d79a48bf8f02f7326d48b4255b

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/AlertActionHandler.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/AlertActionHandler.java
@@ -83,54 +83,56 @@ public class AlertActionHandler extends BaseActionHandler {
       }
 
       final VariablesSecureApp vars = new VariablesSecureApp(request);
-
-      // select the alert rules
-      final String hql = "select distinct(e.alertRule) from  " + AlertRecipient.ENTITY_NAME
-          + " e where e.alertRule.active = true and (e.userContact.id=? "
-          + " or (e.userContact.id = null and e.role.id = ?))";
-      final Query qry = OBDal.getInstance().getSession().createQuery(hql);
-      qry.setParameter(0, OBContext.getOBContext().getUser().getId());
-      qry.setParameter(1, OBContext.getOBContext().getRole().getId());
-
+      // Do not execute the alerts if the system if being rebuilt
       Long total = 0L;
-      for (Object o : qry.list()) {
-        final AlertRule alertRule = (AlertRule) o;
-        final String whereClause = new UsedByLink().getWhereClause(vars, "",
-            alertRule.getFilterClause() == null ? "" : alertRule.getFilterClause());
-        final String sql = "select count(*) from AD_ALERT where COALESCE(to_char(STATUS), 'NEW')='NEW'"
-            + " AND AD_CLIENT_ID "
-            + OBDal.getInstance().getReadableClientsInClause()
-            + " AND AD_ORG_ID "
-            + OBDal.getInstance().getReadableOrganizationsInClause()
-            + " AND AD_ALERTRULE_ID = ? " + (whereClause == null ? "" : whereClause);
+      if (!"Y".equals(vars.getSessionValue("ApplyModules|BuildRunning"))) {
+        // select the alert rules
+        final String hql = "select distinct(e.alertRule) from  " + AlertRecipient.ENTITY_NAME
+            + " e where e.alertRule.active = true and (e.userContact.id=? "
+            + " or (e.userContact.id = null and e.role.id = ?))";
+        final Query qry = OBDal.getInstance().getSession().createQuery(hql);
+        qry.setParameter(0, OBContext.getOBContext().getUser().getId());
+        qry.setParameter(1, OBContext.getOBContext().getRole().getId());
 
-        PreparedStatement sqlQuery = null;
-        ResultSet rs = null;
-        try {
-          sqlQuery = new DalConnectionProvider(false).getPreparedStatement(sql);
-          sqlQuery.setString(1, alertRule.getId());
-          sqlQuery.execute();
-          rs = sqlQuery.getResultSet();
-          if (rs.next()) {
-            long rows = rs.getLong(1);
-            total += rs.getLong(1);
-            log4j.debug("Alert " + alertRule.getName() + " (" + alertRule.getId() + ") - SQL:'"
-                + sql + "' - Rows: " + rows);
-          }
-        } catch (Exception e) {
-          log4j.error("An error has ocurred when trying to process the alerts: " + e.getMessage(),
-              e);
-        } finally {
+        for (Object o : qry.list()) {
+          final AlertRule alertRule = (AlertRule) o;
+          final String whereClause = new UsedByLink().getWhereClause(vars, "",
+              alertRule.getFilterClause() == null ? "" : alertRule.getFilterClause());
+          final String sql = "select count(*) from AD_ALERT where COALESCE(to_char(STATUS), 'NEW')='NEW'"
+              + " AND AD_CLIENT_ID "
+              + OBDal.getInstance().getReadableClientsInClause()
+              + " AND AD_ORG_ID "
+              + OBDal.getInstance().getReadableOrganizationsInClause()
+              + " AND AD_ALERTRULE_ID = ? " + (whereClause == null ? "" : whereClause);
+
+          PreparedStatement sqlQuery = null;
+          ResultSet rs = null;
           try {
-            if (sqlQuery != null) {
-              sqlQuery.close();
-            }
-            if (rs != null) {
-              rs.close();
+            sqlQuery = new DalConnectionProvider(false).getPreparedStatement(sql);
+            sqlQuery.setString(1, alertRule.getId());
+            sqlQuery.execute();
+            rs = sqlQuery.getResultSet();
+            if (rs.next()) {
+              long rows = rs.getLong(1);
+              total += rs.getLong(1);
+              log4j.debug("Alert " + alertRule.getName() + " (" + alertRule.getId() + ") - SQL:'"
+                  + sql + "' - Rows: " + rows);
             }
           } catch (Exception e) {
             log4j.error(
-                "An error has ocurred when trying to close the statement: " + e.getMessage(), e);
+                "An error has ocurred when trying to process the alerts: " + e.getMessage(), e);
+          } finally {
+            try {
+              if (sqlQuery != null) {
+                sqlQuery.close();
+              }
+              if (rs != null) {
+                rs.close();
+              }
+            } catch (Exception e) {
+              log4j.error(
+                  "An error has ocurred when trying to close the statement: " + e.getMessage(), e);
+            }
           }
         }
       }
