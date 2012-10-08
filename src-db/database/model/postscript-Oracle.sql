16d870dfe56fee27b21b157c2c662279b637414f
Asier Lostalé <asier.lostale@openbravo.com>
2012-10-08 16:00:24 +0200

    [discount] Utility function

--- src-db/database/model/postscript-Oracle.sql
+++ src-db/database/model/postscript-Oracle.sql
@@ -837,6 +837,34 @@ WHEN OTHERS THEN
 END AD_CREATE_AUDIT_TRIGGERS;
 /-- END
 
+create or replace
+FUNCTION AD_Execute_Function(p_code IN VARCHAR2) RETURN VARCHAR2
+AS
+/*************************************************************************
+* The contents of this file are subject to the Openbravo  Public  License
+* Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
+* Version 1.1  with a permitted attribution clause; you may not  use this
+* file except in compliance with the License. You  may  obtain  a copy of
+* the License at http://www.openbravo.com/legal/license.html
+* Software distributed under the License  is  distributed  on  an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific  language  governing  rights  and  limitations
+* under the License.
+* The Original Code is Openbravo ERP.
+* The Initial Developer of the Original Code is Openbravo SLU
+* All portions are Copyright (C) 2012 Openbravo SLU
+* All Rights Reserved.
+* Contributor(s):  ______________________________________.
+************************************************************************/
+
+  v_response varchar2(32);
+begin
+   execute immediate 'begin :1 := '||p_code||'; end;' using out v_response;
+   return v_response;
+end;
+/-- END
+ 
+
 CALL DBA_RECOMPILE(NULL)
 /-- END
 
