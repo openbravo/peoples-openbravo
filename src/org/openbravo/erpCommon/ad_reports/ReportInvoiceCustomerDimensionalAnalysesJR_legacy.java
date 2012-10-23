0c11ba8f8b2c3bfc6eb3d0e0481e7e92fa8229fa
Naiara Garcia <naiara.garcia@openbravo.com>
2012-10-23 10:57:04 +0200

    Fixed issue 22060: Product name does not appear in Sales Invoice Dimensional Report Legacy if the UOM symbol is not defined
    --HG--
    extra : rebase_source : bbd4b571a788bc86abd99f83f72e409618f39ccc

--- src/org/openbravo/erpCommon/ad_reports/ReportInvoiceCustomerDimensionalAnalysesJR_legacy.java
+++ src/org/openbravo/erpCommon/ad_reports/ReportInvoiceCustomerDimensionalAnalysesJR_legacy.java
@@ -547,7 +547,8 @@ public class ReportInvoiceCustomerDimensionalAnalysesJR_legacy extends HttpSecur
         levelsconcat.append("M_PRODUCT_CATEGORY.M_PRODUCT_CATEGORY_ID");
       } else if (strShownArray[i].equals("4")) {
         strTextShow[i] = "AD_COLUMN_IDENTIFIER(to_char('M_Product'), to_char( M_PRODUCT.M_PRODUCT_ID), to_char( '"
-            + vars.getLanguage() + "'))||' ('||UOMSYMBOL||')'";
+            + vars.getLanguage()
+            + "'))|| CASE WHEN uomsymbol IS NULL THEN '' ELSE to_char(' ('||uomsymbol||')') END";
         intAuxDiscard = i;
         intOrder++;
         intProductLevel = i + 1;
