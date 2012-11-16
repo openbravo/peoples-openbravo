984669311112cc95a4eae4c96ff505443003bc4d
Gorka Ion Damián <gorkaion.damian@openbravo.com>
2012-11-16 09:15:09 +0100

    [Reservations]Add manage prereservation P&E.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/materialmgmt/ob-reservation.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/materialmgmt/ob-reservation.js
@@ -29,17 +29,14 @@ OB.Reservation.QuantityValidate = function(item, validator, value, record) {
       quantity = null,
       reservedQty = BigDecimal.prototype.ZERO,
       totalQty = BigDecimal.prototype.ZERO,
-      totalReleased = BigDecimal.prototype.ZERO,
       selectedRecords = item.grid.getSelectedRecords(),
       selectedRecordsLength = selectedRecords.length,
       editedRecord = null,
       i;
   if (item.grid.view.parentWindow.activeView.getContextInfo().inpTableId === '260') {
     totalQty = new BigDecimal(String(item.grid.view.parentWindow.activeView.getContextInfo().inpqtyordered));
-    totalReleased = isc.isA.Number(item.grid.view.parentWindow.activeView.getContextInfo().inpqtydelivered) ? new BigDecimal(String(item.grid.view.parentWindow.activeView.getContextInfo().inpqtydelivered)) : BigDecimal.prototype.ZERO;
   } else {
     totalQty = new BigDecimal(String(item.grid.view.parentWindow.activeView.getContextInfo().inpquantity));
-    totalReleased = isc.isA.Number(item.grid.view.parentWindow.activeView.getContextInfo().inpreleasedqty) ? new BigDecimal(String(item.grid.view.parentWindow.activeView.getContextInfo().inpreleasedqty)) : BigDecimal.prototype.ZERO;
   }
 
   if (!isc.isA.Number(value)) {
@@ -50,9 +47,11 @@ OB.Reservation.QuantityValidate = function(item, validator, value, record) {
   }
   quantity = new BigDecimal(String(value));
   if (quantity.compareTo(availableQty) > 0) {
+    isc.warn(OB.I18N.getLabel('OBUIAPP_Res_MoreQtyThanAvailable', [record.availableQty]));
     return false;
   }
   if (quantity.compareTo(releasedQty) < 0) {
+    isc.warn(OB.I18N.getLabel('OBUIAPP_Res_LessThanReleased', [record.released]));
     return false;
   }
   for (i = 0; i < selectedRecordsLength; i++) {
@@ -62,11 +61,58 @@ OB.Reservation.QuantityValidate = function(item, validator, value, record) {
     }
   }
   if (reservedQty.compareTo(totalQty) > 0) {
-    return false;
-  }
-  if (reservedQty.compareTo(releasedQty) < 0) {
+    isc.warn(OB.I18N.getLabel('OBUIAPP_Res_MoreThanReservationQty', [totalQty.toString()]));
     return false;
   }
   // get reservation quantity and released quantity to check totals
+  return true;
+};
+
+OB.Reservation.ManageStockSelectionChange = function (grid, record, state) {
+  var releasedQty = isc.isA.Number(record.released) ? new BigDecimal(String(record.released)) : BigDecimal.prototype.ZERO;
+  if (!state && releasedQty.compareTo(BigDecimal.prototype.ZERO) > 0) {
+    state = true;
+  }
+};
+
+OB.Reservation.PrereservationQuantityValidate = function(item, validator, value, record) {
+  var reservedQty = isc.isA.Number(record.reservedQty) ? new BigDecimal(String(record.reservedQty)) : BigDecimal.prototype.ZERO,
+      purchasedQty = isc.isA.Number(record.purchasedQty) ? new BigDecimal(String(record.purchasedQty)) : BigDecimal.prototype.ZERO,
+      receivedQty = isc.isA.Number(record.receivedQty) ? new BigDecimal(String(record.receivedQty)) : BigDecimal.prototype.ZERO,
+      pendingQty = purchasedQty.subtract(receivedQty),
+      orderedQuantity = isc.isA.Number(record.orderedQuantity) ? new BigDecimal(String(record.orderedQuantity)) : BigDecimal.prototype.ZERO,
+      deliveredQuantity = isc.isA.Number(record.deliveredQuantity) ? new BigDecimal(String(record.deliveredQuantity)) : BigDecimal.prototype.ZERO,
+      otherReservedQty = isc.isA.Number(record.otherReservedQty) ? new BigDecimal(String(record.otherReservedQty)) : BigDecimal.prototype.ZERO,
+      solTotalReserved = BigDecimal.prototype.ZERO,
+      totalQty = BigDecimal.prototype.ZERO,
+      selectedRecords = item.grid.getSelectedRecords(),
+      selectedRecordsLength = selectedRecords.length,
+      editedRecord = null,
+      i;
+
+
+  if (!isc.isA.Number(value)) {
+    return false;
+  }
+  if (value === null || value < 0) {
+    return false;
+  }
+  reservedQty = new BigDecimal(String(value));
+  solTotalReserved = reservedQty.add(otherReservedQty);
+  if (solTotalReserved.compareTo(orderedQuantity) > 0) {
+    isc.warn(OB.I18N.getLabel('OBUIAPP_Res_MoreThanOrderedQty', [solTotalReserved.toString()]));
+    return false;
+  }
+
+  for (i = 0; i < selectedRecordsLength; i++) {
+    editedRecord = isc.addProperties({}, selectedRecords[i], item.grid.getEditedRecord(selectedRecords[i]));
+    if (isc.isA.Number(editedRecord.reservedQty)) {
+      totalQty = totalQty.add(new BigDecimal(String(editedRecord.reservedQty)));
+    }
+  }
+  if (totalQty.compareTo(pendingQty) > 0 ) {
+    isc.warn(OB.I18N.getLabel('OBUIAPP_Res_MoreThanPendingQty', [solTotalReserved.toString()]));
+    return false;
+  }
   return true;
 };
