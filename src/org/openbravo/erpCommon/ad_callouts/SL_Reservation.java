9e57293a23c75b1ca2f828aad556dccba9c18304
Gorka Ion Damián <gorkaion.damian@openbravo.com>
2012-11-22 13:18:00 +0100

    [Reservations]Add callout to update UOM.Fix read only logic.
    --HG--
    extra : rebase_source : 0e71586fd99573edc916d42ba39b116af3eeff57

--- src/org/openbravo/erpCommon/ad_callouts/SL_Reservation.java
+++ src/org/openbravo/erpCommon/ad_callouts/SL_Reservation.java
@@ -2,10 +2,11 @@ package org.openbravo.erpCommon.ad_callouts;
 
 import javax.servlet.ServletException;
 
+import org.apache.commons.lang.StringUtils;
 import org.openbravo.base.filter.IsIDFilter;
 import org.openbravo.dal.core.DalUtil;
 import org.openbravo.dal.service.OBDal;
-import org.openbravo.model.common.order.OrderLine;
+import org.openbravo.model.common.plm.Product;
 
 public class SL_Reservation extends SimpleCallout {
 
@@ -15,15 +16,11 @@ public class SL_Reservation extends SimpleCallout {
   protected void execute(CalloutInfo info) throws ServletException {
     String lastChanged = info.getLastFieldChanged();
 
-    if ("inpcOrderlineId".equals(lastChanged)) {
-      final String strSalesOrderLineId = info.getStringParameter("inpcOrderlineId",
-          IsIDFilter.instance);
-      if (!"".equals(strSalesOrderLineId)) {
-        final OrderLine soLine = OBDal.getInstance().get(OrderLine.class, strSalesOrderLineId);
-        info.addResult("inpquantity", soLine.getOrderedQuantity());
-        info.addResult("inppendingqty",
-            soLine.getOrderedQuantity().subtract(soLine.getDeliveredQuantity()));
-        info.addResult("inpmProductId", (String) DalUtil.getId(soLine.getProduct()));
+    if ("inpmProductId".equals(lastChanged)) {
+      final String strProductId = info.getStringParameter("inpmProductId", IsIDFilter.instance);
+      if (StringUtils.isNotEmpty(strProductId)) {
+        final Product product = OBDal.getInstance().get(Product.class, strProductId);
+        info.addResult("inpcUomId", (String) DalUtil.getId(product.getUOM()));
       }
     }
   }
