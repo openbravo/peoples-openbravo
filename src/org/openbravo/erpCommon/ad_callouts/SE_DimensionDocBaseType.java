d678f6a204cb6859bd98e2b87f334c5ce9662d88
Mikel Irurita <mikel.irurita@openbravo.com>
2012-11-28 12:55:07 +0100

    [Managerial Accounting] SE_DimensionDocBaseType: license year and reformat

--- src/org/openbravo/erpCommon/ad_callouts/SE_DimensionDocBaseType.java
+++ src/org/openbravo/erpCommon/ad_callouts/SE_DimensionDocBaseType.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010 Openbravo SLU
+ * All portions are Copyright (C) 2012 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -26,23 +26,13 @@ import org.hibernate.criterion.Restrictions;
 import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.service.OBCriteria;
 import org.openbravo.dal.service.OBDal;
+import org.openbravo.erpCommon.utility.DimensionDisplayUtility;
 import org.openbravo.model.ad.system.Client;
 import org.openbravo.model.ad.system.DimensionMapping;
 
 public class SE_DimensionDocBaseType extends SimpleCallout {
 
   private static final long serialVersionUID = 1L;
-  // LEVELS
-  final String LEVEL_HEADER = "H";
-  final String LEVEL_LINE = "L";
-  final String LEVEL_BREAKDOWN = "BD";
-  // DIMENSIONS
-  final String DIMENSION_BUSINESSPARTNER = "BP";
-  final String DIMENSION_PRODUCT = "PR";
-  final String DIMENSION_PROJECT = "PJ";
-  final String DIMENSION_COSTCENTER = "CC";
-  final String DIMENSION_USER1 = "U1";
-  final String DIMENSION_USER2 = "U2";
 
   @Override
   protected void execute(CalloutInfo info) throws ServletException {
@@ -50,7 +40,7 @@ public class SE_DimensionDocBaseType extends SimpleCallout {
     final String strDimension = info.getStringParameter("inpdimension", null);
     // Compute header
     java.util.List<DimensionMapping> dMapping = getMapping(strDocBaseType, strDimension,
-        LEVEL_HEADER);
+        DimensionDisplayUtility.DIM_Header);
     if (dMapping.size() > 0) {
       info.addResult("inpdimShowH", "Y");
       info.addResult("inpdimRoH", dMapping.get(0).isMandatory() ? "Y" : "N");
@@ -60,7 +50,7 @@ public class SE_DimensionDocBaseType extends SimpleCallout {
     }
 
     // Compute Lines
-    dMapping = getMapping(strDocBaseType, strDimension, LEVEL_LINE);
+    dMapping = getMapping(strDocBaseType, strDimension, DimensionDisplayUtility.DIM_Lines);
     if (dMapping.size() > 0) {
       info.addResult("inpdimShowL", "Y");
       info.addResult("inpdimRoL", dMapping.get(0).isMandatory() ? "Y" : "N");
@@ -70,7 +60,7 @@ public class SE_DimensionDocBaseType extends SimpleCallout {
     }
 
     // Compute breakdown
-    dMapping = getMapping(strDocBaseType, strDimension, LEVEL_BREAKDOWN);
+    dMapping = getMapping(strDocBaseType, strDimension, DimensionDisplayUtility.DIM_BreakDown);
     if (dMapping.size() > 0) {
       info.addResult("inpdimShowBd", "Y");
       info.addResult("inpdimRoBd", dMapping.get(0).isMandatory() ? "Y" : "N");
@@ -79,9 +69,12 @@ public class SE_DimensionDocBaseType extends SimpleCallout {
       info.addResult("inpdimRoBd", "N");
     }
     Client client = OBContext.getOBContext().getCurrentClient();
-    info.addResult("inpshowInHeader", getValue(client, LEVEL_HEADER, strDimension));
-    info.addResult("inpshowInLines", getValue(client, LEVEL_LINE, strDimension));
-    info.addResult("inpshowInBreakdown", getValue(client, LEVEL_BREAKDOWN, strDimension));
+    info.addResult("inpshowInHeader",
+        getValue(client, DimensionDisplayUtility.DIM_Header, strDimension));
+    info.addResult("inpshowInLines",
+        getValue(client, DimensionDisplayUtility.DIM_Lines, strDimension));
+    info.addResult("inpshowInBreakdown",
+        getValue(client, DimensionDisplayUtility.DIM_BreakDown, strDimension));
 
   }
 
@@ -94,50 +87,50 @@ public class SE_DimensionDocBaseType extends SimpleCallout {
   }
 
   private String getValue(Client client, String level, String dimension) {
-    if (DIMENSION_BUSINESSPARTNER.equals(dimension)) {
-      if (LEVEL_HEADER.equals(level)) {
+    if (DimensionDisplayUtility.DIM_BPartner.equals(dimension)) {
+      if (DimensionDisplayUtility.DIM_Header.equals(level)) {
         return client.isBpartnerAcctdimHeader() ? "Y" : "N";
-      } else if (LEVEL_LINE.equals(level)) {
+      } else if (DimensionDisplayUtility.DIM_Lines.equals(level)) {
         return client.isBpartnerAcctdimLines() ? "Y" : "N";
       } else {
-        return client.isBpartnerAcctdimLines() ? "Y" : "N";
+        return client.isBpartnerAcctdimBreakdown() ? "Y" : "N";
       }
-    } else if (DIMENSION_PRODUCT.equals(dimension)) {
-      if (LEVEL_HEADER.equals(level)) {
+    } else if (DimensionDisplayUtility.DIM_Product.equals(dimension)) {
+      if (DimensionDisplayUtility.DIM_Header.equals(level)) {
         return client.isProductAcctdimHeader() ? "Y" : "N";
-      } else if (LEVEL_LINE.equals(level)) {
+      } else if (DimensionDisplayUtility.DIM_Lines.equals(level)) {
         return client.isProductAcctdimLines() ? "Y" : "N";
       } else {
         return client.isProductAcctdimBreakdown() ? "Y" : "N";
       }
-    } else if (DIMENSION_PROJECT.equals(dimension)) {
-      if (LEVEL_HEADER.equals(level)) {
+    } else if (DimensionDisplayUtility.DIM_Project.equals(dimension)) {
+      if (DimensionDisplayUtility.DIM_Header.equals(level)) {
         return client.isProjectAcctdimHeader() ? "Y" : "N";
-      } else if (LEVEL_LINE.equals(level)) {
+      } else if (DimensionDisplayUtility.DIM_Lines.equals(level)) {
         return client.isProjectAcctdimLines() ? "Y" : "N";
       } else {
         return client.isProjectAcctdimBreakdown() ? "Y" : "N";
       }
-    } else if (DIMENSION_COSTCENTER.equals(dimension)) {
-      if (LEVEL_HEADER.equals(level)) {
+    } else if (DimensionDisplayUtility.DIM_CostCenter.equals(dimension)) {
+      if (DimensionDisplayUtility.DIM_Header.equals(level)) {
         return client.isCostcenterAcctdimHeader() ? "Y" : "N";
-      } else if (LEVEL_LINE.equals(level)) {
+      } else if (DimensionDisplayUtility.DIM_Lines.equals(level)) {
         return client.isCostcenterAcctdimLines() ? "Y" : "N";
       } else {
         return client.isCostcenterAcctdimBreakdown() ? "Y" : "N";
       }
-    } else if (DIMENSION_USER1.equals(dimension)) {
-      if (LEVEL_HEADER.equals(level)) {
+    } else if (DimensionDisplayUtility.DIM_User1.equals(dimension)) {
+      if (DimensionDisplayUtility.DIM_Header.equals(level)) {
         return client.isUser1AcctdimHeader() ? "Y" : "N";
-      } else if (LEVEL_LINE.equals(level)) {
+      } else if (DimensionDisplayUtility.DIM_Lines.equals(level)) {
         return client.isUser1AcctdimLines() ? "Y" : "N";
       } else {
         return client.isUser1AcctdimBreakdown() ? "Y" : "N";
       }
-    } else if (DIMENSION_USER2.equals(dimension)) {
-      if (LEVEL_HEADER.equals(level)) {
+    } else if (DimensionDisplayUtility.DIM_User2.equals(dimension)) {
+      if (DimensionDisplayUtility.DIM_Header.equals(level)) {
         return client.isUser2AcctdimHeader() ? "Y" : "N";
-      } else if (LEVEL_LINE.equals(level)) {
+      } else if (DimensionDisplayUtility.DIM_Lines.equals(level)) {
         return client.isUser2AcctdimLines() ? "Y" : "N";
       } else {
         return client.isUser2AcctdimBreakdown() ? "Y" : "N";
