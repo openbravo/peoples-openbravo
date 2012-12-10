76c1c0ee3205846cc15b6e5f39cb75a608cd5cad
Eduardo Argal Guibert <eduardo.argal@openbravo.com>
2012-12-10 17:45:49 +0100

    [managerial accounting] Applying code review
    --HG--
    extra : rebase_source : 17a59d7e11f41a25363ceb1f7fe95b645b12d347

--- src/org/openbravo/erpCommon/utility/DimensionDisplayUtility.java
+++ src/org/openbravo/erpCommon/utility/DimensionDisplayUtility.java
@@ -42,7 +42,7 @@ import org.openbravo.model.common.enterprise.Organization;
 
 public class DimensionDisplayUtility {
 
-  public static final Logger log4j = Logger.getLogger(DimensionDisplayUtility.class);
+  private static final Logger log4j = Logger.getLogger(DimensionDisplayUtility.class);
 
   /** Accounting Dimensions **/
   public static final String DIM_Header = "H";
