8c2499c879438a6e9eeee80ffa195735aea4d95a
Guillermo Alvarez de Eulate <guillermo.alvarez@openbravo.com>
2013-01-18 08:15:41 +0100

    related to issue 22335:  [bpcatselector] Default value is loaded correctly

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/subwindows/customers/components/sharedcomponents.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/subwindows/customers/components/sharedcomponents.js
@@ -123,7 +123,7 @@ enyo.kind({
         }
       } else {
         //New: select default value
-        if (categ.get(this.defaultValue) === this.defaultValue) {
+        if (categ.get(this.retrievedPropertyForValue) === this.defaultValue) {
           return true;
         }
       }
