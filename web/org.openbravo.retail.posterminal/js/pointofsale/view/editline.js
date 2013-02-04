990e32fb123bc5f48e73e791b2d1ed554eed3f3c
Guillermo Alvarez de Eulate <guillermo.alvarez@openbravo.com>
2013-02-04 12:49:26 +0100

    related to issue 22967: Refactor the edit component and adding the new button which will be shown when a line which shows a product marked as  show stock screen field is selected
    --HG--
    extra : rebase_source : 214666f9d2736a20f2806ba831d5e23401e28e96

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/editline.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/editline.js
@@ -60,64 +60,63 @@ enyo.kind({
       classes: 'row-fluid',
       showing: false,
       components: [{
-        classes: 'span7',
+        classes: 'span12',
         components: [{
-          style: 'padding: 5px; width:100%',
-          components: [{
-            classes: 'row-fluid',
-            components: [{
-              classes: 'span12',
-              components: [{
-                kind: 'OB.UI.SmallButton',
-                content: OB.I18N.getLabel('OBPOS_ButtonDelete'),
-                classes: 'btnlink-orange',
-                tap: function () {
-                  this.owner.doDeleteLine({
-                    line: this.owner.line
-                  });
-                },
-                init: function (model) {
-                  this.model = model;
-                  this.model.get('order').on('change:isPaid', function (newValue) {
-                    if (newValue) {
-                      if (newValue.get('isPaid') === true) {
-                        this.setShowing(false);
-                        return;
-                      }
-                    }
-                    this.setShowing(true);
-                  }, this);
+          kind: 'OB.UI.SmallButton',
+          content: OB.I18N.getLabel('OBPOS_ButtonDelete'),
+          classes: 'btnlink-orange',
+          tap: function () {
+            this.owner.doDeleteLine({
+              line: this.owner.line
+            });
+          },
+          init: function (model) {
+            this.model = model;
+            this.model.get('order').on('change:isPaid', function (newValue) {
+              if (newValue) {
+                if (newValue.get('isPaid') === true) {
+                  this.setShowing(false);
+                  return;
                 }
-              }, {
-                kind: 'OB.UI.SmallButton',
-                content: OB.I18N.getLabel('OBPOS_LblDescription'),
-                classes: 'btnlink-orange',
-                tap: function () {
-                  this.owner.doEditLine({
-                    line: this.owner.line
-                  });
-                },
-                init: function (model) {
-                  this.model = model;
-                  this.model.get('order').on('change:isPaid', function (newValue) {
-                    if (newValue) {
-                      if (newValue.get('isPaid') === true) {
-                        this.setShowing(false);
-                        return;
-                      }
-                    }
-                    this.setShowing(true);
-                  }, this);
-                }
-              }]
-            }]
-          }]
+              }
+              this.setShowing(true);
+            }, this);
+          }
         }, {
+          kind: 'OB.UI.SmallButton',
+          content: OB.I18N.getLabel('OBPOS_LblDescription'),
+          classes: 'btnlink-orange',
+          tap: function () {
+            this.owner.doEditLine({
+              line: this.owner.line
+            });
+          },
+          init: function (model) {
+            this.model = model;
+            this.model.get('order').on('change:isPaid', function (newValue) {
+              if (newValue) {
+                if (newValue.get('isPaid') === true) {
+                  this.setShowing(false);
+                  return;
+                }
+              }
+              this.setShowing(true);
+            }, this);
+          }
+        }, {
+          kind: 'OB.OBPOSPointOfSale.UI.EditLine.OpenStockButton',
+          name: 'checkStockButton',
+          showing: false
+        }]
+      }, {
+        classes: 'span12',
+        components: [{
+          classes: 'span7',
           kind: 'Scroller',
           maxHeight: '120px',
           thumb: true,
           horizontal: 'hidden',
-          style: 'padding: 0px 0px 0px 25px; width:100%; line-height: 140%;',
+          style: 'padding: 16px 0px 10px 25px; line-height: 140%;',
           components: [{
             classes: 'row-fluid',
             style: 'clear: both;',
@@ -184,27 +183,27 @@ enyo.kind({
               }]
             }]
           }]
-        }]
-      }, {
-        classes: 'span5',
-        sytle: 'text-align: right',
-        components: [{
-          style: 'padding: 60px 10px 20px 10px;',
+        }, {
+          classes: 'span4',
+          sytle: 'text-align: right',
           components: [{
-            name: 'editlineimage',
-            kind: 'OB.UI.Thumbnail',
-            classes: 'image-wrap image-editline',
-            width: '128px',
-            height: '128px'
+            style: 'padding: 2px 10px 10px 10px;',
+            components: [{
+              name: 'editlineimage',
+              kind: 'OB.UI.Thumbnail',
+              classes: 'image-wrap image-editline',
+              width: '128px',
+              height: '128px'
+            }]
           }]
         }]
-      }]
-    }, {
-      name: 'msgaction',
-      style: 'padding: 10px;',
-      components: [{
-        name: 'txtaction',
-        style: 'float:left;'
+      }, {
+        name: 'msgaction',
+        style: 'padding: 10px;',
+        components: [{
+          name: 'txtaction',
+          style: 'float:left;'
+        }]
       }]
     }]
   }],
@@ -215,6 +214,11 @@ enyo.kind({
     this.line = line;
     if (this.line) {
       this.line.on('change', this.render, this);
+    }
+    if (this.line && this.line.get('product').get('showstock') && !this.line.get('product').get('ispack') && OB.POS.modelterminal.get('connectedToERP')) {
+      this.$.checkStockButton.show();
+    } else {
+      this.$.checkStockButton.hide();
     }
     this.render();
   },
@@ -252,6 +256,26 @@ enyo.kind({
       this.$.editlinediscount.setContent('');
       this.$.editlineprice.setContent('');
       this.$.editlinegross.setContent('');
+    }
+  }
+});
+
+enyo.kind({
+  kind: 'OB.UI.SmallButton',
+  name: 'OB.OBPOSPointOfSale.UI.EditLine.OpenStockButton',
+  events: {
+    onShowLeftSubWindow: ''
+  },
+  content: 'Stock Check',
+  classes: 'btnlink-orange',
+  tap: function () {
+    var product = this.owner.line.get('product');
+    //show always or just when the product has been set to show stock screen?
+    if (product.get('showstock') && !product.get('ispack') && OB.POS.modelterminal.get('connectedToERP')) {
+      params = {};
+      params.leftSubWindow = OB.OBPOSPointOfSale.UICustomization.stockLeftSubWindow;
+      params.product = product;
+      this.doShowLeftSubWindow(params);
     }
   }
 });
