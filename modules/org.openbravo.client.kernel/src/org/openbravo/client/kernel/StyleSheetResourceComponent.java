34fa5b7063a2f61c40ff4f5f7d90e1b3a4aba9f1
Asier Lostalé <asier.lostale@openbravo.com>
2013-02-21 10:40:09 +0100

    fixed issue 23134: StyleSheet components should bypass authentication
    --HG--
    extra : rebase_source : e10800e7f898239667fe164f5bab2c7ab19f9267

--- modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/StyleSheetResourceComponent.java
+++ modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/StyleSheetResourceComponent.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2012 Openbravo SLU
+ * All portions are Copyright (C) 2010-2013 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -246,5 +246,10 @@ public class StyleSheetResourceComponent extends BaseComponent {
       log.error("Error processing file: " + path + " - " + e.getMessage(), e);
     }
     return "";
+  }
+
+  @Override
+  public boolean bypassAuthentication() {
+    return true;
   }
 }
