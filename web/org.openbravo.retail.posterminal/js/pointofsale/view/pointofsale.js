5abcdb64d2103a97318bace5642b0c41cb326348
Miguel de Juana <miguel.dejuana@openbravo.com>
2013-03-19 15:56:40 +0100

    [JSLint]

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
@@ -1,13 +1,13 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012 Openbravo S.L.U.
+ * Copyright (C) 2013 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
  ************************************************************************************
  */
 
-/*global OB, Backbone, enyo, $, confirm */
+/*global OB, Backbone, enyo, $, confirm, _ */
 
 // Point of sale main window view
 enyo.kind({
