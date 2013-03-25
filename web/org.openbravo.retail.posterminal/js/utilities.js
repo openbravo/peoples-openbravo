84828203f5ab18f3f5ea6a5c558ec21f2032a6fa
Miguel de Juana <miguel.dejuana@openbravo.com>
2013-03-25 17:09:32 +0100

    Related to issue 23125

--- web/org.openbravo.retail.posterminal/js/utilities.js
+++ web/org.openbravo.retail.posterminal/js/utilities.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012 Openbravo S.L.U.
+ * Copyright (C) 2013 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -151,6 +151,7 @@
     this.proc = new OB.DS.Process(OB.UTIL.processCustomerClass);
     if (OB.POS.modelterminal.get('connectedToERP')) {
       this.proc.exec({
+        terminalId: OB.POS.modelterminal.get('terminal').id,
         customer: customersToJson
       }, function (data, message) {
         if (data && data.exception) {
