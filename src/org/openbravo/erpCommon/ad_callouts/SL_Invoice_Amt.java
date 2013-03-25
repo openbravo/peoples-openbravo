72ce27b2cc645844904b769c510d047f9ca49e93
David Miguelez <david.miguelez@openbravo.com>
2013-03-25 17:36:11 +0100

    Rollback undesired change.

--- src/org/openbravo/erpCommon/ad_callouts/SL_Invoice_Amt.java
+++ src/org/openbravo/erpCommon/ad_callouts/SL_Invoice_Amt.java
@@ -148,7 +148,8 @@ public class SL_Invoice_Amt extends HttpSecureAppServlet {
     if (priceActual.compareTo(BigDecimal.ZERO) == 0) {
       lineNetAmt = BigDecimal.ZERO;
     }
-    // If unit price (actual price) changes, recalculates standard price (std price)
+    // If unit price (actual price) changes, recalculates standard price
+    // (std price) applying price adjustments (offers) if any
     if (strChanged.equals("inppriceactual") || strChanged.equals("inplinenetamt")) {
       if (log4j.isDebugEnabled())
         log4j.debug("priceActual:" + Double.toString(priceActual.doubleValue()));
@@ -158,7 +159,8 @@ public class SL_Invoice_Amt extends HttpSecureAppServlet {
       resultado.append("new Array(\"inptaxbaseamt\", " + priceActual.multiply(qtyInvoice) + "),");
     }
 
-    // If quantity changes, recalculates unit price (actual price)
+    // If quantity changes, recalculates unit price (actual price) applying
+    // price adjustments (offers) if any
     if (strChanged.equals("inpqtyinvoiced")) {
       if (log4j.isDebugEnabled())
         log4j.debug("strPriceList: " + strPriceList.replace("\"", "") + " product:" + strProduct
