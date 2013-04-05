3494c36d55ab6d4c2c4849120a2456c5c17ab717
David Miguelez <david.miguelez@openbravo.com>
2013-04-05 12:29:56 +0200

    Fixes Issue 23491. Fixes problem when Writing-Off amount in Payment Proposal Pick and Execute.
    --HG--
    extra : rebase_source : 2d5c3e8bed46a4c7f8098c74b4fc97fdb7090b1d

--- modules/org.openbravo.advpaymentmngt/web/org.openbravo.advpaymentmngt/js/ob-aprm-utilities.js
+++ modules/org.openbravo.advpaymentmngt/web/org.openbravo.advpaymentmngt/js/ob-aprm-utilities.js
@@ -136,7 +136,7 @@ OB.APRM.validatePaymentProposalPickAndEdit = function (item, validator, value, r
   if (row) {
     row.difference = Number(outstanding.subtract(paidamount));
     row.payment = Number(record.payment);
-    if (contextInfo.inplimitwriteoff !== "") {
+    if (contextInfo.inplimitwriteoff && contextInfo.inplimitwriteoff !== "") {
       var differencewriteoff = OB.Utilities.Number.JSToOBMasked((row.difference * contextInfo.inpfinaccTxnConvertRate), OB.Format.defaultNumericMask, OB.Format.defaultDecimalSymbol, OB.Format.defaultGroupingSymbol, OB.Format.defaultGroupingSize);
       if (differencewriteoff > contextInfo.inplimitwriteoff && record.writeoff === true) {
         isc.warn(OB.I18N.getLabel('APRM_NotAllowWriteOff'));
