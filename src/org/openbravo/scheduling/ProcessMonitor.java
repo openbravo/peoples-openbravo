754805f2c963ab78ea6532e828d7f291246c9d34
Asier Lostalé <asier.lostale@openbravo.com>
2013-04-24 17:21:50 +0200

    related to issue 20671: don't spam log when single instance process is executed

--- src/org/openbravo/scheduling/ProcessMonitor.java
+++ src/org/openbravo/scheduling/ProcessMonitor.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2008-2010 Openbravo SLU 
+ * All portions are Copyright (C) 2008-2013 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -256,7 +256,7 @@ class ProcessMonitor implements SchedulerListener, JobListener, TriggerListener 
       }
     }
 
-    log.info("No other instance");
+    log.debug("No other instance");
     return false;
   }
 
