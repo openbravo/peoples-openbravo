f8e0648215a2f923aee2edb0b43f593809586a85
Augusto Mauch <augusto.mauch@openbravo.com>
2013-05-08 16:26:46 +0200

    [DS Based Tables] Updates the Copyright date of files modified in this project

--- src/org/openbravo/dal/core/DalUtil.java
+++ src/org/openbravo/dal/core/DalUtil.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2008-2012 Openbravo SLU 
+ * All portions are Copyright (C) 2008-2013 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -130,7 +130,7 @@ public class DalUtil {
 
   /**
    * Finds a property using the db column name and table id.
-   *
+   * 
    * @param tableId
    *          the table id, is used to find the {@link Entity}
    * @param dbColumnName
