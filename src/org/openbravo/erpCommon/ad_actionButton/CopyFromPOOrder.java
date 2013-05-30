5a42af38ed31f3d9c35e271e118a9ac72b546dfa
Pandeeswari Ramakrishnan <pandeeswari.ramakrishnan@openbravo.com>
2013-05-30 12:38:25 +0530

    Fixes Issue 23928: Copy from Order in Purchase Order
    --HG--
    extra : rebase_source : 914842705e2e420f7f84003b64e410fcca318c56

--- src/org/openbravo/erpCommon/ad_actionButton/CopyFromPOOrder.java
+++ src/org/openbravo/erpCommon/ad_actionButton/CopyFromPOOrder.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2001-2012 Openbravo SLU 
+ * All portions are Copyright (C) 2001-2013 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -202,7 +202,7 @@ public class CopyFromPOOrder extends HttpSecureAppServlet {
               this,
               strCOrderlineID,
               orderData[0].adClientId,
-              data[i].adOrgId,
+              orderData[0].adOrgId,
               vars.getUser(),
               strKey,
               Integer.toString(line),
