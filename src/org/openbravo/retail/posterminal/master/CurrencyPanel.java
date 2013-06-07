710c7cf4d06a6e35a740e4f2254974f70295aee7
Guillermo Alvarez de Eulate <guillermo.alvarez@openbravo.com>
2013-06-07 14:51:20 +0200

    keys of currency panels are get using order by lineNo
    --HG--
    extra : rebase_source : ed3c70a9a7cc2125d2fb857ae250a1e2a03f18dd

--- src/org/openbravo/retail/posterminal/master/CurrencyPanel.java
+++ src/org/openbravo/retail/posterminal/master/CurrencyPanel.java
@@ -25,7 +25,7 @@ public class CurrencyPanel extends ProcessHQLQuery {
   @Override
   protected List<String> getQuery(JSONObject jsonsent) throws JSONException {
 
-    String hql = "from OBPOS_CurrencyPanel where $readableClientCriteria";
+    String hql = "from OBPOS_CurrencyPanel where $readableClientCriteria order by lineNo asc";
 
     return Arrays.asList(new String[] { hql });
   }
