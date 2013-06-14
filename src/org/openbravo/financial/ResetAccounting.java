e680dc07aba58dc295c9f1af8c0cf4fc39eeb4c1
Sandra Huguet <sandra.huguet@openbravo.com>
2013-06-14 14:00:52 +0200

    Related to issue 24055 backout 6cf2880eecc5

--- src/org/openbravo/financial/ResetAccounting.java
+++ src/org/openbravo/financial/ResetAccounting.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -304,22 +303,9 @@ public class ResetAccounting {
       Set<String> orgIds, String calendarId, String tableId, String recordId, String datefrom,
       String dateto) {
     if (!"".equals(recordId)) {
-      String organization = "";
-      Iterator<String> iterator = orgIds.iterator();
-      while (iterator.hasNext()) {
-        organization = iterator.next();
-        String myQuery = "select p.id from Organization p where ad_org_getperiodcontrolallow(:organization)=p.id";
-        Query query = OBDal.getInstance().getSession().createQuery(myQuery);
-        query.setString("organization", organization);
-        query.setMaxResults(1);
-        if (query.uniqueResult() != null) {
-          List<Period> periods = new ArrayList<Period>();
-          periods.add(getDocumentPeriod(clientId, tableId, recordId, docBaseType, organization));
-          return periods;
-        }
-
-      }
-
+      List<Period> periods = new ArrayList<Period>();
+      periods.add(getDocumentPeriod(clientId, tableId, recordId, docBaseType));
+      return periods;
     }
     String myQuery = "select distinct p from FinancialMgmtPeriodControl e left join e.period p left join p.year y left join y.calendar c where c.id = :calendarId and e.client.id = :clientId and e.documentCategory = :docbasetype and e.periodStatus = 'O'";
 
@@ -349,14 +335,13 @@ public class ResetAccounting {
   }
 
   private static Period getDocumentPeriod(String clientId, String tableId, String recordId,
-      String docBaseType, String organization) {
-    String myQuery = "select distinct e.period from FinancialMgmtAccountingFact e , FinancialMgmtPeriodControl p where p.period=e.period and p.periodStatus = 'O' and e.client.id = :clientId and e.table.id = :tableId and e.recordID=:recordId and p.documentCategory = :docbasetype and p.organization= :organization";
+      String docBaseType) {
+    String myQuery = "select distinct e.period from FinancialMgmtAccountingFact e , FinancialMgmtPeriodControl p where p.period=e.period and p.periodStatus = 'O' and e.client.id = :clientId and e.table.id = :tableId and e.recordID=:recordId and p.documentCategory = :docbasetype";
     Query query = OBDal.getInstance().getSession().createQuery(myQuery);
     query.setString("clientId", clientId);
     query.setString("tableId", tableId);
     query.setString("recordId", recordId);
     query.setString("docbasetype", docBaseType);
-    query.setString("organization", organization);
     query.setMaxResults(1);
     Period period = (Period) query.uniqueResult();
     if (period == null) {
