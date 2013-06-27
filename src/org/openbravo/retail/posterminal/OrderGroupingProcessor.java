d57feea4e104ecba5b25fdefc6b84b3c83e7a112
Antonio Moreno <antonio.moreno@openbravo.com>
2013-06-27 12:56:16 +0200

    Fixed issue 24212. Instead of using the deliveryStatus, compare the ordered quantity of the line with the delivery quantity.
    --HG--
    extra : transplant_source : %D3%10%AC%00%93%0C%14%B5%90%B6%8F%9F%F8x%A6%D5%E2%18%0EX

--- src/org/openbravo/retail/posterminal/OrderGroupingProcessor.java
+++ src/org/openbravo/retail/posterminal/OrderGroupingProcessor.java
@@ -70,7 +70,7 @@ public class OrderGroupingProcessor {
         + "or not exists (select 1 "
         + "                 from FIN_Payment_ScheduleDetail d"
         + "              where d.orderPaymentSchedule.order = line.salesOrder))"
-        + " and line.salesOrder.obposApplications = :terminal and line.salesOrder.deliveryStatus > 0"
+        + " and line.salesOrder.obposApplications = :terminal and line.deliveredQuantity=line.orderedQuantity"
         + " and not exists (select 1 from OrderLine as ord where invoicedQuantity<>0 and ord.salesOrder = line.salesOrder)"
         + " order by line.businessPartner.id";
 
