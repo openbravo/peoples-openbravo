191c1b6bdce03056f565f78fe9a71378f182ac74
Ioritz Cia <ioritz.cia@openbravo.com>
2013-07-01 10:25:06 +0200

    Fixes issue 23735: Create price list for costing based price lists. Now the cost is taken too.

--- src-db/database/model/prescript-Oracle.sql
+++ src-db/database/model/prescript-Oracle.sql
@@ -210,3 +210,29 @@ SELECT 0 AS value
    FROM DUAL
 /-- END
 
+create or replace
+FUNCTION ADD_HMS(p_date IN DATE, p_hours IN NUMBER, p_minutes IN NUMBER, p_seconds IN NUMBER) RETURN TIMESTAMP
+
+AS
+/*************************************************************************
+* The contents of this file are subject to the Openbravo  Public  License
+* Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
+* Version 1.1  with a permitted attribution clause; you may not  use this
+* file except in compliance with the License. You  may  obtain  a copy of
+* the License at http://www.openbravo.com/legal/license.html
+* Software distributed under the License  is  distributed  on  an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific  language  governing  rights  and  limitations
+* under the License.
+* The Original Code is Openbravo ERP.
+* The Initial Developer of the Original Code is Openbravo SLU
+* All portions are Copyright (C) 2013 Openbravo SLU
+* All Rights Reserved.
+* Contributor(s):  ______________________________________.
+************************************************************************/
+  v_Display VARCHAR2(4000) ;
+  v_SQL     VARCHAR2(4000):='';
+begin
+  RETURN p_date + p_hours/24 + p_minutes/1440 + p_seconds/86400;
+end ADD_HMS;
+/-- END
