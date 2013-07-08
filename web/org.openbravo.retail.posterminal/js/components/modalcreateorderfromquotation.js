88452380c6820ab4ea403cb116cfbd4fb75ae3a6
Asier Lostalé <asier.lostale@openbravo.com>
2013-07-08 13:46:28 +0200

    firm quotation default value is defined by preference

--- web/org.openbravo.retail.posterminal/js/components/modalcreateorderfromquotation.js
+++ web/org.openbravo.retail.posterminal/js/components/modalcreateorderfromquotation.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012 Openbravo S.L.U.
+ * Copyright (C) 2012-2013 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -38,7 +38,7 @@ enyo.kind({
   classes: 'modal-dialog-btn-check',
   checked: false,
   init: function () {
-    this.checked = true;
+    this.checked = !OB.MobileApp.model.get('permissions')['OBPOS_quotation.defaultNotFirm'];
     this.addRemoveClass('active', this.checked);
   }
 });
