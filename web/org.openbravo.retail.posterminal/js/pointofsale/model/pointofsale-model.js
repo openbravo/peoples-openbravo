55daa0bdf7a29a60234d5dcebf32af1c998da616
Asier Lostalé <asier.lostale@openbravo.com>
2013-07-10 11:19:03 +0200

    [approval] moved business logic to model

--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
@@ -316,5 +316,99 @@ OB.OBPOSPointOfSale.Model.PointOfSale = OB.Model.WindowModel.extend({
         approved: true
       });
     }
+  },
+
+  checkApproval: function (approvalType, username, password) {
+    OB.Dal.initCache(OB.Model.Supervisor, [], null, null);
+    if (OB.MobileApp.model.get('connectedToERP')) {
+      new OB.DS.Process('org.openbravo.retail.posterminal.utility.CheckApproval').exec({
+        u: username,
+        p: password,
+        approvalType: approvalType
+      }, enyo.bind(this, function (response, message) {
+        var approved = false;
+        if (response.exception) {
+          OB.UTIL.showError(response.exception.message);
+          approved = false;
+        } else {
+          approved = response.canApprove;
+          if (!approved) {
+            OB.UTIL.showError("User cannot approve");
+          }
+
+          // saving supervisor in local so next time it is possible to approve offline
+          OB.Dal.find(OB.Model.Supervisor, {
+            'id': response.userId
+          }, enyo.bind(this, function (users) {
+            var supervisor, date, permissions = [];
+            if (users.models.length === 0) {
+              // new user
+              if (response.canApprove) {
+                // insert in local db only in case it is supervisor for current type
+                date = new Date().toString();
+                supervisor = new OB.Model.Supervisor();
+
+                supervisor.set('id', response.userId);
+                supervisor.set('name', username);
+                supervisor.set('password', OB.MobileApp.model.generate_sha1(password + date));
+                supervisor.set('created', date);
+                supervisor.set('permissions', JSON.stringify([approvalType]));
+                OB.Dal.save(supervisor, null, null, true);
+              }
+            } else {
+              // update existent user granting or revoking permission
+              supervisor = users.models[0];
+
+              supervisor.set('password', OB.MobileApp.model.generate_sha1(password + supervisor.get('created')));
+              if (supervisor.get('permissions')) {
+                permissions = JSON.parse(supervisor.get('permissions'));
+              }
+
+              if (response.canApprove) {
+                // grant permission if it does not exist
+                if (!_.contains(permissions, approvalType)) {
+                  permissions.push(approvalType);
+                }
+              } else {
+                // revoke permission if it exists
+                if (_.contains(permissions, approvalType)) {
+                  permissions = _.without(permissions, approvalType);
+                }
+              }
+              supervisor.set('permissions', JSON.stringify(permissions));
+
+              OB.Dal.save(supervisor);
+            }
+          }));
+        }
+        this.trigger('approvalChecked', {
+          approved: approved
+        });
+      }));
+    } else { // offline
+      console.log('offline')
+      OB.Dal.find(OB.Model.Supervisor, {
+        'name': username
+      }, enyo.bind(this, function (users) {
+        var supervisor, approved = false;
+        if (users.models.length === 0) {
+          alert(OB.I18N.getLabel('No offline user'));
+        } else {
+          supervisor = users.models[0];
+          if (supervisor.get('password') === OB.MobileApp.model.generate_sha1(password + supervisor.get('created'))) {
+            if (_.contains(JSON.parse(supervisor.get('permissions')), approvalType)) {
+              approved = true;
+            } else {
+              OB.UTIL.showError('User cannot approve');
+            }
+          } else {
+            alert('incorrect password');
+          }
+        }
+        this.trigger('approvalChecked', {
+          approved: approved
+        });
+      }), function () {});
+    }
   }
 });
