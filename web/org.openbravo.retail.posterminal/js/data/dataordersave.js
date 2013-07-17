3ce8515551f6090c0e36de7812db19c14d356be7
Miguel de Juana <miguel.dejuana@openbravo.com>
2013-07-17 09:40:25 +0200

    Fixed issue 24302: A warning is shown when a Layaway in Multiorder
    --HG--
    extra : rebase_source : 54794ad9014a65f07e7e09c85e856f9981b6bbc5

--- web/org.openbravo.retail.posterminal/js/data/dataordersave.js
+++ web/org.openbravo.retail.posterminal/js/data/dataordersave.js
@@ -18,6 +18,7 @@
     this.context = model;
     this.receipt = model.get('order');
     this.ordersToSend = OB.DEC.Zero;
+    this.hasLayaways = false;
 
     this.receipt.on('closed', function () {
       this.receipt = model.get('order');
@@ -129,11 +130,18 @@
             var successCallback, errorCallback;
             successCallback = function () {
               OB.UTIL.showLoading(false);
+              if (me.hasLayaways) {
+                OB.UTIL.showWarning(OB.I18N.getLabel('OBPOS_noInvoiceIfLayaway'));
+                me.hasLayaways = false;
+              }
               OB.UTIL.showSuccess(OB.I18N.getLabel('OBPOS_MsgAllReceiptSaved'));
             };
             errorCallback = function () {
               OB.UTIL.showError(OB.I18N.getLabel('OBPOS_MsgAllReceiptNotSaved'));
             };
+            if (!_.isUndefined(receipt.get('amountToLayaway')) && !_.isNull(receipt.get('amountToLayaway'))) {
+              me.hasLayaways = true;
+            }
             model.get('orderList').current = receipt;
             model.get('orderList').deleteCurrent();
             me.ordersToSend += 1;
