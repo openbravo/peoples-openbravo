68c16dbadb8ffa25c4f8aa655096df4201f7e53f
Pandeeswari Ramakrishnan <pandeeswari.ramakrishnan@openbravo.com>
2013-09-19 23:05:29 +0530

    Fixes Issue 24620: Sales quotation is modifying qty reserved in m_storage_pending
    When the sales quotation is booked or reactivated, the quantity should not be updated in the m_storage_pending table and
    when the sales quotation is converted into sales order, the quantity should be updated in the m_storage_pending table.

--- src/org/openbravo/erpCommon/ad_process/ConvertQuotationIntoOrder.java
+++ src/org/openbravo/erpCommon/ad_process/ConvertQuotationIntoOrder.java
@@ -286,6 +286,9 @@ public class ConvertQuotationIntoOrder extends DalBaseProcess {
       OBDal.getInstance().refresh(objCloneOrder);
       OBDal.getInstance().refresh(objOrder);
 
+      for (OrderLine orderLine : objCloneOrder.getOrderLineList()) {
+        callUpdateStoragePending(objCloneOrder, orderLine);
+      }
       OBDal.getInstance().commitAndClose();
       OBError result = OBErrorBuilder.buildMessage(null, "success", "@SalesOrderDocumentno@ "
           + objCloneOrder.getDocumentNo() + " @beenCreated@");
@@ -367,6 +370,45 @@ public class ConvertQuotationIntoOrder extends DalBaseProcess {
       parameters.add(null);
       parameters.add(objCloneOrder.getId());
       final String procedureName = "c_order_post1";
+      CallStoredProcedure.getInstance().call(procedureName, parameters, null, true, false);
+    } catch (Exception e) {
+      throw new OBException(e);
+    }
+  }
+
+  /**
+   * Update storage details
+   */
+  private void callUpdateStoragePending(Order objCloneOrder, OrderLine objCloneOrderLine) {
+    BigDecimal qtySo = BigDecimal.ZERO;
+    BigDecimal qtyOrderSo = BigDecimal.ZERO;
+    String productUom = null;
+    try {
+      final List<Object> parameters = new ArrayList<Object>();
+      parameters.add(objCloneOrder.getClient().getId());
+      parameters.add(objCloneOrder.getOrganization().getId());
+      parameters.add(objCloneOrder.getUpdatedBy().getId());
+      parameters.add(objCloneOrderLine.getProduct().getId());
+      parameters.add(objCloneOrderLine.getWarehouse().getId());
+      parameters.add(objCloneOrderLine.getAttributeSetValue() != null ? objCloneOrderLine
+          .getAttributeSetValue().getId() : null);
+      parameters.add(objCloneOrderLine.getUOM().getId());
+      productUom = objCloneOrderLine.getOrderUOM() != null ? objCloneOrderLine.getOrderUOM()
+          .getId() : null;
+      parameters.add(productUom);
+      if (objCloneOrderLine.isDirectShipment()) {
+        qtySo = BigDecimal.ZERO;
+      } else {
+        qtySo = objCloneOrderLine.getOrderedQuantity();
+      }
+      qtySo = qtySo.subtract(objCloneOrderLine.getReservedQuantity());
+      qtySo = qtySo.subtract(objCloneOrderLine.getDeliveredQuantity());
+      parameters.add(qtySo);
+      qtyOrderSo = objCloneOrderLine.getOrderQuantity();
+      parameters.add(qtyOrderSo);
+      parameters.add(BigDecimal.ZERO); // PO quantity
+      parameters.add(null);
+      final String procedureName = "m_update_storage_pending";
       CallStoredProcedure.getInstance().call(procedureName, parameters, null, true, false);
     } catch (Exception e) {
       throw new OBException(e);
