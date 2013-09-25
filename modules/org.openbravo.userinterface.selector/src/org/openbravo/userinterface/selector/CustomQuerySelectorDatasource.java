6894e95f0f1bde40b94aea659c1ada0fb29c3f25
Asier Lostalé <asier.lostale@openbravo.com>
2013-09-25 12:33:35 +0200

    fixed bug 24740: filter list reference columns in HQL defined selectors
      Taking into account this case both in the view generation and in the DataSource
    
    --HG--
    extra : rebase_source : 40aa2e8094496e253d40b735892944b2288a003c

--- modules/org.openbravo.userinterface.selector/src/org/openbravo/userinterface/selector/CustomQuerySelectorDatasource.java
+++ modules/org.openbravo.userinterface.selector/src/org/openbravo/userinterface/selector/CustomQuerySelectorDatasource.java
@@ -45,6 +45,7 @@ import org.openbravo.base.model.domaintype.DateDomainType;
 import org.openbravo.base.model.domaintype.DomainType;
 import org.openbravo.base.model.domaintype.ForeignKeyDomainType;
 import org.openbravo.base.model.domaintype.LongDomainType;
+import org.openbravo.base.model.domaintype.StringEnumerateDomainType;
 import org.openbravo.base.model.domaintype.UniqueIdDomainType;
 import org.openbravo.client.application.ParameterUtils;
 import org.openbravo.client.kernel.RequestContext;
@@ -361,8 +362,38 @@ public class CustomQuerySelectorDatasource extends ReadOnlyDataSourceService {
     } else if (domainType instanceof ForeignKeyDomainType) {
       // Assume left part definition is full object reference from HQL select
       whereClause = field.getClauseLeftPart() + ".id = '" + value + "'";
-    } else {
+    } else if (domainType instanceof StringEnumerateDomainType) {
+      // For enumerations value can be in two formats:
+      // 1- VAL: in this case the expression should be property='VAL'
+      // 2- ["VAL1", "VAL2"] (JSONArray): the expression should be property in ('VAL1', 'VAL2')
+      JSONArray values = null;
+      if (value.startsWith("[")) {
+        try {
+          values = new JSONArray(value);
+        } catch (JSONException ignore) {
+          // It is not a JSONArray: assuming format 1
+        }
+      }
 
+      if (values == null) {
+        // format 1
+        whereClause = field.getClauseLeftPart() + " = '" + value + "'";
+      } else {
+        // format 2
+        whereClause = field.getClauseLeftPart() + " IN (";
+        for (int i = 0; i < values.length(); i++) {
+          if (i > 0) {
+            whereClause += ", ";
+          }
+          try {
+            whereClause += "'" + values.getString(i) + "'";
+          } catch (JSONException e) {
+            log.error("Error parsing values as JSONArray:" + value, e);
+          }
+        }
+        whereClause += ")";
+      }
+    } else {
       if ("iStartsWith".equals(operator)) {
         whereClause = "lower(" + field.getClauseLeftPart() + ") LIKE '"
             + value.toLowerCase().replaceAll(" ", "%") + "%'";
