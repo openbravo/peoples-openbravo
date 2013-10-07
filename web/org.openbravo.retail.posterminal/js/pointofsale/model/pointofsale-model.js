2e73f1c2fe466bf563f1b0e0231221a86ad34a96
Asier Lostalé <asier.lostale@openbravo.com>
2013-10-07 12:33:48 +0200

    fixed bug 24894: promotions code is executed twice

--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
@@ -465,7 +465,7 @@ OB.OBPOSPointOfSale.Model.PointOfSale = OB.Model.WindowModel.extend({
     this.printLine = new OB.OBPOSPointOfSale.Print.ReceiptLine(receipt);
 
     // Listening events that cause a discount recalculation
-    receipt.get('lines').on('add change:qty change:gross change:net', function (line) {
+    receipt.get('lines').on('add change:qty change:price', function (line) {
       if (!receipt.get('isEditable')) {
         return;
       }
