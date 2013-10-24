c279d95c09bb92dc4d9cdaf236747c8de0028bed
Augusto Mauch <augusto.mauch@openbravo.com>
2013-10-24 10:33:39 +0200

    Related to issue 24995: Enables tab key while creating a record in the grid
    --HG--
    extra : rebase_source : 4a8dc97678a3629df9d9e20319edf07f8d9a3bc0

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-view-grid.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-view-grid.js
@@ -2956,7 +2956,7 @@ isc.OBViewGrid.addProperties({
       return;
     }
 
-    if (this.getEditForm().isNew && editCompletionEvent === 'tab' && !ficCallDone) {
+    if (newRow && this.getEditForm().isNew && editCompletionEvent === 'tab' && !ficCallDone) {
       this.setEditValue(rowNum, 'actionAfterFicReturn', {
         target: this,
         method: this.cellEditEnd,
