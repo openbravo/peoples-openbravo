0a4bcf44e5066d142568a1e457e3bcefb732de02
Miguel de Juana <miguel.dejuana@openbravo.com>
2013-10-29 16:43:07 +0100

    Adapt component provider with return reasons

--- src/org/openbravo/retail/posterminal/OBPOSComponentProvider.java
+++ src/org/openbravo/retail/posterminal/OBPOSComponentProvider.java
@@ -89,6 +89,7 @@ public class OBPOSComponentProvider extends BaseComponentProvider {
         "model/changedbusinesspartners",
         "model/changedbplocation",
         "model/executor",
+        "model/returnreasons",
         "model/terminal-model",
         "components/modalcancel",
         "components/subwindow",
@@ -169,7 +170,7 @@ public class OBPOSComponentProvider extends BaseComponentProvider {
         "pointofsale/view/subwindows/customers/customersadvancedsearch",
         "pointofsale/view/subwindows/customers/editcreatecustomerform",
         "pointofsale/view/subwindows/customers/customerdetailview",
-        
+
         "pointofsale/view/subwindows/customeraddress/components/sharedcomponents",
         "pointofsale/view/subwindows/customeraddress/editcreatecustomeraddress",
         "pointofsale/view/subwindows/customeraddress/customeraddrsearch",
