7041f5a6ea21b185a1d4c67e7de392bd734adad9
Augusto Mauch <augusto.mauch@openbravo.com>
2013-10-29 13:05:23 +0100

    Fixes issue 25033: Problems with personalization and grid adaptive filtering
    There was a problem when a saved view with no filter clause is applied over a view that had an active filter clause.
    
    Consider the following sequence of events:
    - A window (i.e. Sales Order) is opened
    - An initial request to the datasource before the personalized view is applied. Suppose the datasource returns fewer than 100 records
    - The personalized view is applied. As there is fewer than 100 records, the filtering is done locally. This is wrong, because the initial dataset was built over a filterClause, and the personalize view that is being applied does not have a filter clause. In this case the filtering should not be done locally.
    
    --HG--
    extra : transplant_source : %AB%07_%83%C2%F8%CC%8C%19%8D%8B%25A%EF%90%DE14%FB%BC

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-view-grid.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-view-grid.js
@@ -1082,6 +1082,11 @@ isc.OBViewGrid.addProperties({
     }
 
     if (localState.noFilterClause) {
+      if (OB.Utilities.isNonEmptyString(this.filterClause)) {
+        if (this.data) {
+          this.data.forceRefresh = true;
+        }
+      }
       this.filterClause = null;
       if (this.view.messageBar) {
         this.view.messageBar.hide();
