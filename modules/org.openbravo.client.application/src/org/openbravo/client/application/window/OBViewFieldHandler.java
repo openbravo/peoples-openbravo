a8c1c4d97e2695da46ce20600e20ac81d24cac98
Augusto Mauch <augusto.mauch@openbravo.com>
2014-01-14 15:57:44 +0100

    Fixes issue 25460: Doc action button shows the proper message
    The processNow property should be included in the property subset that is fetched always when obtaining data from the grid. If this property is not loaded initially, the option list sho
    wn the first time a doc action button is opened can be wrong.
    
    --HG--
    extra : transplant_source : %27S8%3E%0C%87.%88%19%7B%04%E8%5E%9F%2Ax%06%F0%A3%A1

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/window/OBViewFieldHandler.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/window/OBViewFieldHandler.java
@@ -2115,4 +2115,10 @@ public class OBViewFieldHandler {
     return propertiesInButtonFieldDisplayLogic;
   }
 
+  public boolean hasProcessNowProperty() {
+    final Entity entity = ModelProvider.getInstance().getEntityByTableId(
+        getTab().getTable().getId());
+    return (entity == null) ? false : entity.hasProperty("processNow");
+  }
+
 }
