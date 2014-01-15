ab9d2473cc7905b484ea3a39356f8fbb80105bb3
Adrián Romero <adrianromero@openbravo.com>
2014-01-15 18:49:00 +0100

    Fixes issue 0025478: Edit coins footer is not visible in cash up process

--- src/org/openbravo/retail/posterminal/master/Product.java
+++ src/org/openbravo/retail/posterminal/master/Product.java
@@ -115,12 +115,13 @@ public class Product extends ProcessHQLQuery {
         );
 
     // generic products
-    products
-        .add("select "
-            + regularProductsHQLProperties.getHqlSelect()
-            + " from Product product left outer join product.image img left join product.oBRETCOProlProductList as pli left outer join product.pricingProductPriceList ppp where exists (select 1 from Product product2 left join product2.oBRETCOProlProductList as pli2, PricingProductPrice ppp2 where product.id = product2.genericProduct.id and product2 = ppp2.product and ppp2.priceListVersion.id = '"
-            + priceListVersion.getId() + "' and pli2.obretcoProductlist.id = '"
-            + productList.getId() + "')");
+    // products
+    // .add("select "
+    // + regularProductsHQLProperties.getHqlSelect()
+    // +
+    // " from Product product left outer join product.image img left join product.oBRETCOProlProductList as pli left outer join product.pricingProductPriceList ppp where exists (select 1 from Product product2 left join product2.oBRETCOProlProductList as pli2, PricingProductPrice ppp2 where product.id = product2.genericProduct.id and product2 = ppp2.product and ppp2.priceListVersion.id = '"
+    // + priceListVersion.getId() + "' and pli2.obretcoProductlist.id = '"
+    // + productList.getId() + "')");
 
     return products;
 
