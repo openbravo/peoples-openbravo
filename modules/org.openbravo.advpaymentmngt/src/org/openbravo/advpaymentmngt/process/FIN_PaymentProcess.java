5da52e178d7ecb25669c11cd2cfe6d63049dfe2f
Atul Gaware <atul.gaware@openbravo.com>
2014-01-16 20:24:31 +0530

    Fixes Issue 0025323:Description is too long error when reactivating a payment

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/process/FIN_PaymentProcess.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/process/FIN_PaymentProcess.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2013 Openbravo SLU
+ * All portions are Copyright (C) 2010-2014 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  *************************************************************************
@@ -872,7 +872,9 @@ public class FIN_PaymentProcess implements org.openbravo.scheduling.Process {
                       newDesc.append("\n");
                   }
                 }
-                creditPayment.setDescription(newDesc.toString());
+                // Truncate Description to keep length as 255
+                creditPayment.setDescription(newDesc.toString().length() > 255 ? newDesc.toString()
+                    .substring(0, 255) : newDesc.toString());
               }
             }
           }
@@ -1196,7 +1198,9 @@ public class FIN_PaymentProcess implements org.openbravo.scheduling.Process {
                 newDesc.append("\n");
             }
           }
-          payment.setDescription(newDesc.toString());
+          // Truncate Description to keep length as 255
+          payment.setDescription(newDesc.toString().length() > 255 ? newDesc.toString().substring(
+              0, 255) : newDesc.toString());
         }
       }
     }
