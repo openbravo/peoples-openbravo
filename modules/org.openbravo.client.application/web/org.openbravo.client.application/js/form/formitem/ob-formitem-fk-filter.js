9da3a260dabcd380dde6806fbbc205953462d99f
Augusto Mauch <augusto.mauch@openbravo.com>
2014-02-03 12:38:14 +0100

    Fixes issue 25633: Symbol of default operator is not shown
    The problem was that even though the operator of the column was iStartsWith this.getOperator() returned iContains. That meant the iContains was considered the
    default operator. The default operator symbol is not shown in the filter, that is why the '~' (iContains symbol) was removed after filtering. Then, if the grid
     was filtered again there was no explicit operator, so the operator defined in the grid configuration was used.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-fk-filter.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-fk-filter.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2011-2013 Openbravo SLU
+ * All portions are Copyright (C) 2011-2014 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -290,6 +290,14 @@ isc.OBFKFilterTextItem.addProperties({
     }
 
     return crit;
+  },
+
+  getOperator: function (textMatchStyle) {
+    if (this.operator && this.operator !== 'iContains') {
+      return this.operator;
+    } else {
+      return this.Super('getOperator', arguments);
+    }
   },
 
   replaceCriterionOperator: function (criterion, value, newOperator) {
