4e1169609df4adaa28025c1edfd475449db5bf7b
David Baz Fayos <david.baz@openbravo.com>
2014-02-05 15:58:44 +0100

    [scupgrade] Now calendar controls are shown as before (always persistent)

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/calendar/ob-calendar.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/calendar/ob-calendar.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2013 Openbravo SLU
+ * All portions are Copyright (C) 2013-2014 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -60,6 +60,7 @@ isc.ClassFactory.defineClass('OBCalendar', isc.Calendar);
 isc.OBCalendar.addProperties({
   autoFetchData: true,
   mainViewConstructor: isc.OBCalendarTabSet,
+  useEventCanvasRolloverControls: false,
   eventDialogConstructor: isc.OBCalendar_EventDialogBridge,
   initWidget: function () {
     var calendar = this,
