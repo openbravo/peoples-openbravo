fc034764253d6fbeb244edf585a4694c20730e70
Augusto Mauch <augusto.mauch@openbravo.com>
2014-02-12 16:10:18 +0100

    Fixes issue 25581:  Display logic is not working fine using auxiliary input
    The problem was that the display logic was using an auxiliary input defined for the parent tab, but its value was being wrongly overwritten by the subtab (the tab being hidden/shown). I
    t has been fixed by adding the subtab session properties only if they are not an auxiliary input in the parent tab.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/main/ob-standard-view.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/main/ob-standard-view.js
@@ -1383,11 +1383,16 @@ isc.OBStandardView.addProperties({
         // Calling getContextInfo with (false, true, true) in order to obtain also the value of the
         // session attributes of the form
         contextInfo = this.getContextInfo(false, true, true);
-        isc.addProperties(contextInfo, tabViewPane.sessionAttributes);
+        this.addSessionAttributes(contextInfo, tabViewPane);
         if (tabViewPane.showTabIf && !(tabViewPane.showTabIf(contextInfo))) {
           this.childTabSet.tabBar.members[i].hide();
           tabViewPane.hidden = true;
         } else {
+          if (!tabViewPane.data && !tabViewPane.refreshingData) {
+            // If the child tab does not have data yet, refresh it
+            tabViewPane.refreshingData = true;
+            tabViewPane.refresh();
+          }
           if (this.childTabSet.visibility === 'hidden') {
             this.childTabSet.show();
           }
@@ -1417,6 +1422,21 @@ isc.OBStandardView.addProperties({
         } else {
           this.childTabSet.hide();
         }
+      }
+    }
+  },
+
+  // Adds to contextInfo the session attributes of the childView, 
+  // unless the session attribute is an auxiliary input of its parent tab
+  addSessionAttributes: function (contextInfo, childView) {
+    var auxInputs = {},
+        p;
+    if (this.viewForm && this.viewForm.auxInputs) {
+      auxInputs = this.viewForm.auxInputs;
+    }
+    for (p in childView.sessionAttributes) {
+      if (childView.sessionAttributes.hasOwnProperty(p) && !auxInputs.hasOwnProperty(p)) {
+        contextInfo[p] = childView.sessionAttributes[p];
       }
     }
   },
