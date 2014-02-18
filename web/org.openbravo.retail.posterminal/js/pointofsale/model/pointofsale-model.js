706ca22d17088930ec6e451927af78a56fc7ab32
Gorka Ion Damián <gorkaion.damian@openbravo.com>
2014-02-18 18:03:28 +0100

    Fixed issue 25760. Set the location defined in the org to the bp.

--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2013 Openbravo S.L.U.
+ * Copyright (C) 2012-2014 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -216,13 +216,26 @@ OB.OBPOSPointOfSale.Model.PointOfSale = OB.Model.TerminalWindowModel.extend({
     this.set('brandFilter', []);
 
     function searchCurrentBP() {
-      function errorCallback(tx, error) {
+      var errorCallback = function(tx, error) {
         OB.UTIL.showError("OBDAL error while getting BP info: " + error);
-      }
+      };
 
       function successCallbackBPs(dataBps) {
+        var partnerAddressId = OB.MobileApp.model.get('terminal').partnerAddress,
+            successCallbackBPLoc;
+        
         if (dataBps) {
-          OB.POS.modelterminal.set('businessPartner', dataBps);
+          if (partnerAddressId && dataBps.get('locId') !== partnerAddressId) {
+            // Set default location
+            successCallbackBPLoc = function(bpLoc) {
+              dataBps.set('locId', bpLoc.get('id'));
+              dataBps.set('locName', bpLoc.get('name'));
+              OB.POS.modelterminal.set('businessPartner', dataBps);
+            };
+            OB.Dal.get(OB.Model.BPLocation, partnerAddressId, successCallbackBPLoc, errorCallback);
+          } else {
+            OB.POS.modelterminal.set('businessPartner', dataBps);
+          }
         }
       }
       OB.Dal.get(OB.Model.BusinessPartner, OB.POS.modelterminal.get('businesspartner'), successCallbackBPs, errorCallback);
