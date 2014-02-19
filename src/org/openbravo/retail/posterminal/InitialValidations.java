225f1aafb3a76886ede2a8a8d33c5fb9b3e1b07c
Antonio Moreno <antonio.moreno@openbravo.com>
2014-02-19 17:29:16 +0100

    Fixed issue 25771. Added more validations to the InitialValidations infrastructure

--- src/org/openbravo/retail/posterminal/InitialValidations.java
+++ src/org/openbravo/retail/posterminal/InitialValidations.java
@@ -9,6 +9,8 @@
 package org.openbravo.retail.posterminal;
 
 import org.codehaus.jettison.json.JSONException;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.dal.service.OBQuery;
 
 public class InitialValidations {
 
@@ -26,9 +28,35 @@ public class InitialValidations {
 
     if (POSUtils.getWarehouseForTerminal(posTerminal) == null) {
       throw new JSONException("OBPOS_WarehouseNotConfigured");
-
     }
 
-  }
+    if (posTerminal.getOrganization().getCurrency() == null) {
+      throw new JSONException("OBPOS_OrgCurrencyConfigured");
+    }
 
+    if (posTerminal.getOrganization().getOrganizationInformationList().get(0).getLocationAddress() == null) {
+      throw new JSONException("OBPOS_OrgLocationConfigured");
+    }
+
+    String whereclausePM = " as e where e.obposApplications=:terminal and not exists "
+        + "(select 1 from FinancialMgmtFinAccPaymentMethod as pmacc where "
+        + "pmacc.paymentMethod = e.paymentMethod.paymentMethod and pmacc.account = e.financialAccount"
+        + ")";
+    OBQuery<OBPOSAppPayment> queryFinAccounts = OBDal.getInstance().createQuery(
+        OBPOSAppPayment.class, whereclausePM);
+    queryFinAccounts.setNamedParameter("terminal", posTerminal);
+    if (queryFinAccounts.list().size() > 0) {
+      throw new JSONException("OBPOS_PayMethodNotConfiguredInAccount");
+    }
+
+    String whereclauseCMEV = " as e where e.obposApplications=:terminal and exists "
+        + "(select 1 from OBRETCO_CashManagementEvents as cmev where "
+        + "cmev.financialAccount = e.financialAccount)";
+    OBQuery<OBPOSAppPayment> queryEventAccounts = OBDal.getInstance().createQuery(
+        OBPOSAppPayment.class, whereclauseCMEV);
+    queryEventAccounts.setNamedParameter("terminal", posTerminal);
+    if (queryEventAccounts.list().size() > 0) {
+      throw new JSONException("OBPOS_CMEVAccountIsUsedInPayMethod");
+    }
+  }
 }
