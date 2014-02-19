63c44cb36f273cf0bd7763cfad997fa75521d401
Gorka Ion Damián <gorkaion.damian@openbravo.com>
2014-02-19 12:17:37 +0100

    Fixed issue 25766.Add to_char to where clause.

--- src/org/openbravo/retail/posterminal/utility/CheckApproval.java
+++ src/org/openbravo/retail/posterminal/utility/CheckApproval.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2013 Openbravo S.L.U.
+ * Copyright (C) 2013-2014 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -57,7 +57,7 @@ public class CheckApproval extends JSONProcessSimple {
         String whereClause = "as p" + //
             " where property in (" + approvals + ")" + //
             "   and active = true" + //
-            "   and searchKey = 'Y'" + //
+            "   and to_char(searchKey) = 'Y'" + //
             "   and (userContact = :user" + //
             "        or exists (from ADUserRoles r" + //
             "                  where r.role = p.visibleAtRole" + //
