2ef6fa8f78b977c65af5bc2c3530118034611f33
Augusto Mauch <augusto.mauch@openbravo.com>
2014-02-25 11:21:33 +0100

    Fixes issue 25827: Time fields work properly in timezones with negative offsets
    Depending on who calls the formSaved function of OBTimeItem, the provided time can be already converted to the client local time or not. To know the time has a
    lready been converted, the function used to check if the full year of the time was 1970. If that was the case, then the time was converted from UTC to local ti
    me.
    
    The problem is that if the user is working from a timezone with negative offset the full year will not be 1970, but 1969. This has been fixed by replacing data
    [this.name].getFullYear() === 1970 with data[this.name].getFullYear() <= 1970

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-time.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-time.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2011-2013 Openbravo SLU
+ * All portions are Copyright (C) 2011-2014 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -152,7 +152,7 @@ isc.OBTimeItem.addProperties({
     var UTCOffsetInMiliseconds;
     if (this.getValue() !== data[this.name]) {
       // it has not been converted to the local time yet, do it now
-      if (data[this.name].getFullYear() === 1970) {
+      if (data[this.name].getFullYear() <= 1970) {
         UTCOffsetInMiliseconds = OB.Utilities.Date.getUTCOffsetInMiliseconds();
         data[this.name].setTime(data[this.name].getTime() + UTCOffsetInMiliseconds);
       }
