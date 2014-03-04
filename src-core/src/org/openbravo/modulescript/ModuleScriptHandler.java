bc7cc3787c4e641503729e9a23109673a1da4ce4
Asier Lostalé <asier.lostale@openbravo.com>
2014-03-04 08:02:13 +0100

    fixed bug 25887: Hidden stack executing failing module script

--- src-core/src/org/openbravo/modulescript/ModuleScriptHandler.java
+++ src-core/src/org/openbravo/modulescript/ModuleScriptHandler.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2010 Openbravo S.L.U.
+ * Copyright (C) 2010-2014 Openbravo S.L.U.
  * Licensed under the Apache Software License version 2.0
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to  in writing,  software  distributed
@@ -64,7 +64,7 @@ public class ModuleScriptHandler extends Task {
           callExecute(myClass, instance);
         }
       } catch (Exception e) {
-        log4j.error("Error executing moduleScript: " + s);
+        log4j.error("Error executing moduleScript: " + s, e);
         throw new BuildException("Execution of moduleScript " + s + "failed.");
       }
     }
