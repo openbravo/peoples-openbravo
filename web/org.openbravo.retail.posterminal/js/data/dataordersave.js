c84a27617bda540cdf5eca0a1b5f24d5263bc1cd
Guillermo Alvarez de Eulate <guillermo.alvarez@openbravo.com>
2014-03-11 00:35:44 +0100

    related to issue 25940: Order synchronization now is working fine
    --HG--
    extra : transplant_source : %F0%3F.%EA%03za%C8%93%FC%FB%8D%C1%04hR%23%A1%1B%F2

--- web/org.openbravo.retail.posterminal/js/data/dataordersave.js
+++ web/org.openbravo.retail.posterminal/js/data/dataordersave.js
@@ -54,10 +54,6 @@
         var receipt = args.context.receipt,
             currentDocNo = receipt.get('documentNo') || docno;
 
-        if (eventParams && eventParams.callback) {
-          eventParams.callback();
-        }
-
         receipt.set('json', JSON.stringify(receipt.toJSON()));
 
         OB.Dal.save(receipt, function () {
@@ -76,6 +72,9 @@
                 }
               }
               };
+          if (eventParams && eventParams.callback) {
+            eventParams.callback();
+          }
           OB.MobileApp.model.runSyncProcess(model, successCallback);
         }, function () {
           //We do nothing: we don't need to alert the user, as the order is still present in the database, so it will be resent as soon as the user logs in again
