11d10458be45906dc0e309ae3ce08b464c6662e0
Sandra Huguet <sandra.huguet@openbravo.com>
2014-03-13 17:34:54 +0100

    [pw3] Param position in row Add logic to order the field in the column number defined. To order the fields in the column number defined it should be add many spaces as needed. For this reason the subclass OBViewParamSpacer has been created.

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/window/OBViewParameterHandler.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/window/OBViewParameterHandler.java
@@ -42,6 +42,7 @@ import org.openbravo.model.ad.ui.Window;
 public class OBViewParameterHandler {
   private static final Logger log = Logger.getLogger(OBViewParameterHandler.class);
   private static final String WINDOW_REFERENCE_ID = "FF80818132D8F0F30132D9BC395D0038";
+  private static final int NUMBER_COLUMNS = 4;
   private Process process;
   private ParameterWindowComponent paramWindow;
 
@@ -88,7 +89,9 @@ public class OBViewParameterHandler {
     List<OBViewParameter> params = new ArrayList<OBViewParameterHandler.OBViewParameter>();
     OBViewParamGroup currentGroup = null;
     FieldGroup currentADFieldGroup = null;
+    int pos = 1;
     for (Parameter param : process.getOBUIAPPParameterList()) {
+
       if (!(param.isActive()
           && (!param.isFixed() || param.getReference().getId().equals(WINDOW_REFERENCE_ID)) && (!param
           .getReference().getId().equals(ParameterWindowComponent.BUTTON_LIST_REFERENCE_ID)))) {
@@ -125,7 +128,31 @@ public class OBViewParameterHandler {
         parameter.addListReferenceValues(param.getReferenceSearchKey());
       }
 
+      // Add spacers to order the field in the column number defined
+      if (param.isStartinnewline()) {
+        pos = 0;
+      }
+      if (pos > NUMBER_COLUMNS) {
+        pos = pos - NUMBER_COLUMNS;
+      }
+
+      if (param.getNumcolumn() != null) {
+        int spaces = 0;
+        if (pos > param.getNumcolumn().intValue()) {
+          spaces = NUMBER_COLUMNS - (pos - param.getNumcolumn().intValue());
+        } else {
+          spaces = param.getNumcolumn().intValue() - pos;
+        }
+        for (int i = 0; i < spaces; i++) {
+          final OBViewParamSpacer spacer = new OBViewParamSpacer();
+          params.add(spacer);
+          pos++;
+        }
+
+      }
       params.add(parameter);
+      pos++;
+
     }
     return params;
   }
@@ -358,6 +385,47 @@ public class OBViewParameterHandler {
     public boolean isExpanded() {
       return !(fieldGroup.isCollapsed() == null ? false : fieldGroup.isCollapsed());
     }
+  }
+
+  public class OBViewParamSpacer extends OBViewParameter {
+    @Override
+    public String getType() {
+      return "spacer";
+    }
+
+    public String getName() {
+      return "";
+    }
+
+    public boolean getPersonalizable() {
+      return false;
+
+    }
+
+    public boolean isGrid() {
+      return false;
+    }
+
+    public String getTitle() {
+      return "";
+    }
+
+    public String getId() {
+      return "";
+    }
+
+    public String getWidth() {
+      return "";
+    }
+
+    public boolean isRequired() {
+      return false;
+    }
+
+    public String getParameterProperties() {
+      return "";
+    }
+
   }
 
   public void setParamWindow(ParameterWindowComponent parameterWindowComponent) {
