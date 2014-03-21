60ca82739f053716795f22b1be3b2199ee7d94ec
Miguel de Juana <miguel.dejuana@openbravo.com>
2014-03-21 13:12:19 +0100

    Fixed issue 0026058: In Pay open tickets, after the process, an empty order should appear, not a suspended one

--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
@@ -308,6 +308,9 @@ OB.OBPOSPointOfSale.Model.PointOfSale = OB.Model.TerminalWindowModel.extend({
       function readyToSendFunction() {
         //this function is executed when all orders are ready to be sent
         me.get('leftColumnViewManager').setOrderMode();
+        if (me.get('orderList').length > me.get('multiOrders').get('multiOrdersList').length) {
+          me.get('orderList').addNewOrder();
+        }
       }
 
       function prepareToSendCallback(order) {
