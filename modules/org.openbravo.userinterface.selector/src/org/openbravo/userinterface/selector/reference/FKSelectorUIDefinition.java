64cc04e2b753312e6bab07e562e7a3deb95cc5b3
Augusto Mauch <augusto.mauch@openbravo.com>
2014-03-28 12:06:25 +0100

    [hqltables] Updates UIDefinitions to support hql tables
    If the table is based on a db table, use DalUtil.getProperty to obtain the property associated with a column. If not, use DalUtil.getPropertyByTableId.

--- modules/org.openbravo.userinterface.selector/src/org/openbravo/userinterface/selector/reference/FKSelectorUIDefinition.java
+++ modules/org.openbravo.userinterface.selector/src/org/openbravo/userinterface/selector/reference/FKSelectorUIDefinition.java
@@ -145,7 +145,7 @@ public class FKSelectorUIDefinition extends ForeignKeyUIDefinition {
     final String tableId = field.getColumn().getTable().getId();
 
     Property property = null;
-    if (ApplicationConstants.DATASOURCEBASEDTABLE.equals(field.getColumn().getTable()
+    if (!ApplicationConstants.TABLEBASEDTABLE.equals(field.getColumn().getTable()
         .getDataOriginType())) {
       property = DalUtil.getPropertyByTableId(tableId, columnName);
     } else {
