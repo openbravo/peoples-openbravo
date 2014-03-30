3c14393be528fe8fe00cee643a551a29f7b8c421
Pandeeswari Ramakrishnan <pandeeswari.ramakrishnan@openbravo.com>
2014-03-30 18:42:52 +0530

    Related to Issue 26073: When a payment is processed, even if the process fails, the ProcessBundle returns 'success'

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/process/FIN_PaymentProcess.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/process/FIN_PaymentProcess.java
@@ -39,6 +39,7 @@ import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.service.OBCriteria;
 import org.openbravo.dal.service.OBDal;
 import org.openbravo.database.ConnectionProvider;
+import org.openbravo.erpCommon.utility.OBDateUtils;
 import org.openbravo.erpCommon.utility.OBError;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.erpCommon.utility.Utility;
@@ -153,15 +154,18 @@ public class FIN_PaymentProcess implements org.openbravo.scheduling.Process {
 
         boolean orgLegalWithAccounting = FIN_Utility.periodControlOpened(payment.TABLE_NAME,
             payment.getId(), payment.TABLE_NAME + "_ID", "LE");
-        /*
-         * if (!FIN_Utility.isPeriodOpen(payment.getClient().getId(), payment.getDocumentType()
-         * .getDocumentCategory(), payment.getOrganization().getId(), OBDateUtils
-         * .formatDate(payment.getPaymentDate())) && orgLegalWithAccounting) { msg.setType("Error");
-         * msg.setTitle(Utility.messageBD(conProvider, "Error", language));
-         * msg.setMessage(Utility.parseTranslation(conProvider, vars, language,
-         * "@PeriodNotAvailable@")); bundle.setResult(msg); OBDal.getInstance().rollbackAndClose();
-         * return; }
-         */
+        if (!FIN_Utility.isPeriodOpen(payment.getClient().getId(), payment.getDocumentType()
+            .getDocumentCategory(), payment.getOrganization().getId(), OBDateUtils
+            .formatDate(payment.getPaymentDate()))
+            && orgLegalWithAccounting) {
+          msg.setType("Error");
+          msg.setTitle(Utility.messageBD(conProvider, "Error", language));
+          msg.setMessage(Utility.parseTranslation(conProvider, vars, language,
+              "@PeriodNotAvailable@"));
+          bundle.setResult(msg);
+          OBDal.getInstance().rollbackAndClose();
+          return;
+        }
         Set<String> documentOrganizations = OBContext.getOBContext()
             .getOrganizationStructureProvider(payment.getClient().getId())
             .getNaturalTree(payment.getOrganization().getId());
