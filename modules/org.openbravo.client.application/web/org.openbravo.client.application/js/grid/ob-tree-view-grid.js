b04590df5f5ba69a4e32dbcb40d6c578520e5c65
Augusto Mauch <augusto.mauch@openbravo.com>
2014-04-09 22:15:13 +0200

    Fixes issue 26231: Auxiliary Inputs are not computed in the Tree Grid View
    Now when a record is selected in the tree grid view, the toolBar.updateButtonState() is called. This results in a FIC call in SETSESSION mode, that loads the auxiliary inputs and session attributes.
    
    Also some changes have been done to ensure that if the tree grid view is shown, its selected record is used instead of the record selected in the standard grid.
    
    --HG--
    extra : transplant_source : %BFQ%FFF%0D%C8%BE%BA%E8%1A%10%D5%8C9%B6%0B8%B1gL

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-tree-view-grid.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/grid/ob-tree-view-grid.js
@@ -383,7 +383,7 @@ isc.OBTreeViewGrid.addProperties({
     var selectedRecordId = this.getSelectedRecord() ? this.getSelectedRecord().id : null,
         length, tabViewPane, i;
     this.view.updateSubtabVisibility();
-    this.view.toolBar.updateButtonState();
+    this.toolBar.updateButtonState();
     // refresh the tabs
     if (this.view.childTabSet) {
       length = this.view.childTabSet.tabs.length;
