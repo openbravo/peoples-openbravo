5fa063d8c92c6b50c79bfe21e442c8664c6c4a5f
Guillermo Gil <guillermo.gil@openbravo.com>
2014-04-10 11:00:04 +0200

    Fixed issue 26197:Display logic on tab based on auxiliary input is not working
    Windows that do not have buttons update subtab visibility
    
    --HG--
    extra : rebase_source : a452b943e33878ce40321ef432fbf9f73aa583a0

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/toolbar/ob-toolbar.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/toolbar/ob-toolbar.js
@@ -1339,6 +1339,12 @@ isc.OBToolbar.addProperties({
       OB.RemoteCallManager.call('org.openbravo.client.application.window.FormInitializationComponent', allProperties, requestParams, function (response, data, request) {
         var attachmentExists = data.attachmentExists;
         me.view.attachmentExists = attachmentExists;
+
+        // Added sessionAttributes to updateSubtabVisibility 
+        me.view.viewForm.sessionAttributes = data.sessionAttributes;
+        me.view.viewForm.auxInputs = data.auxiliaryInputValues;
+        me.view.updateSubtabVisibility();
+
         //Call to refresh the buttons. As its called with noSetSession=true, it will not cause an infinite recursive loop
         me.updateButtonState(true);
       });
