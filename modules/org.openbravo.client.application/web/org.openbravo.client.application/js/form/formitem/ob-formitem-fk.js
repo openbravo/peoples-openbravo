04d0bf5454efee7970e5b8c2e8c34f18ae0fce58
Shankar Balachandran <shankar.balachandran@openbravo.com>
2014-04-24 21:45:05 +0530

    [combos refactor] fixes html escape issue
    When text contained & it was shown as &amp;

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-fk.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-fk.js
@@ -30,13 +30,72 @@ isc.OBFKItem.addProperties({
   showOptionsFromDataSource: true,
   autoFetchData: false,
   hasPickList: true,
+  tempValueMap: this.valueMap,
+
+  mapValueToDisplay: function (value) {
+    var i, ret = this.Super('mapValueToDisplay', arguments),
+        result;
+
+    // the datasource should handle it
+/*if (this.optionDataSource) {
+      return ret;
+    }*/
+
+    if (this.valueMap) {
+      // handle multi-select
+      if (isc.isA.Array(value)) {
+        this.lastSelectedValue = value;
+        for (i = 0; i < value.length; i++) {
+          if (i > 0) {
+            result += this.multipleValueSeparator;
+          }
+          // encode or and and
+          result += OB.Utilities.encodeSearchOperator(this.Super('mapValueToDisplay', value[i]));
+        }
+      } else if (this.valueMap[value]) {
+        this.lastSelectedValue = value;
+        return this.valueMap[value].unescapeHTML();
+      }
+    }
+
+    if (ret === value && this.isDisabled()) {
+      return '';
+    }
+
+    // don't update the valuemap if the value is null or undefined
+    if (ret === value && value) {
+      if (!this.valueMap) {
+        this.valueMap = {};
+        this.valueMap[value] = '';
+        return '';
+      } //there may be cases if the value is an number within 10 digits, it is identified as an UUID. In that case check is done to confirm whether it is indeed UUID by checking if it is available in the valueMap.
+      else if (!this.valueMap[value] && OB.Utilities.isUUID(value) && this.valueMap.hasOwnProperty(value)) {
+        return '';
+      }
+    }
+    return ret;
+  },
+
+  dataArrived: function (startRow, endRow, data) {
+    var i, allRows;
+    if (data && data.allRows) {
+      allRows = data.allRows;
+      for (i = 0; i < allRows.length; i++) {
+        this.valueMap[allRows[i].id] = allRows[i]._identifier;
+      }
+    }
+  },
 
   // set the identifier field also, that's what gets displayed in the grid
   changed: function (form, item, value) {
     if (!this._pickedValue && value) {
       return;
     }
-
+    if (value === undefined) {
+      if (this.getElementValue() !== undefined) {
+        value = this.getElementValue();
+      }
+    }
     var display = this.mapValueToDisplay(value),
         identifierFieldName = this.name + OB.Constants.FIELDSEPARATOR + OB.Constants.IDENTIFIER;
     form.setValue(identifierFieldName, display);
