a2d615dfe6d73d5933aa93bb889222a960a7fac0
Augusto Mauch <augusto.mauch@openbravo.com>
2014-04-24 10:06:42 +0200

    Fixes issue 26355: Date fields are not displayed properly in the tree grid view
    The problem was that the fix done to fix the issue [1] was only being applied to root nodes, so the date fields were still not being displayed properly for non root nodes.
    
    [1] https://issues.openbravo.com/view.php?id=26228
    
    --HG--
    extra : transplant_source : %EB%ED%DF%8B01%E2%C2%C4.g%A2%A2%AB%A0D%1A%F6i%9A

--- modules/org.openbravo.userinterface.smartclient/web/org.openbravo.userinterface.smartclient/js/ob-smartclient.js
+++ modules/org.openbravo.userinterface.smartclient/web/org.openbravo.userinterface.smartclient/js/ob-smartclient.js
@@ -114,7 +114,7 @@ isc.ResultTree.addProperties({
   },
 
   dataArrived: function (parentNode) {
-    var children = this.getChildren(),
+    var children = this.getChildren(parentNode),
         target = window[this.componentId];
     target.transformData(children);
   }
