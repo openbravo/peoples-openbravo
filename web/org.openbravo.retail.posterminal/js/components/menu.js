9e2bcac1ad064db605326a026ca9ffac3e8dbb9d
Orekaria <ral@openbravo.com>
2014-04-29 11:03:30 +0200

    Fixed issue 26374: Last commit was for another issue

--- web/org.openbravo.retail.posterminal/js/components/menu.js
+++ web/org.openbravo.retail.posterminal/js/components/menu.js
@@ -600,7 +600,7 @@ enyo.kind({
     }
   },
   updateVisibility: function (model) {
-    if (OB.POS.modelterminal.hasPermission(this.permission) && model.get('isQuotation') && model.get('hasbeenpaid') === 'Y') {
+    if (model.get('isQuotation') && model.get('hasbeenpaid') === 'Y') {
       this.show();
     } else {
       this.hide();
