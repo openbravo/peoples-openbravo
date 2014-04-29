71d85cacfc9177dea13134b4a55116eb013c8fb5
Orekaria <ral@openbravo.com>
2014-04-29 16:22:41 +0200

    Fixed issue 26191: Admin mode propertly deactivated

--- src/org/openbravo/retail/posterminal/ProcessCashClose.java
+++ src/org/openbravo/retail/posterminal/ProcessCashClose.java
@@ -44,8 +44,7 @@ public class ProcessCashClose extends JSONProcessSimple {
     try {
       if (jsonCashup.has("cashUpDate")) {
         String strCashUpDate = (String) jsonCashup.getString("cashUpDate");
-        cashUpDate = (Date) JsonToDataConverter.convertJsonToPropertyValue(PropertyByType.DATETIME,
-            ((String) strCashUpDate).subSequence(0, ((String) strCashUpDate).lastIndexOf(".")));
+        cashUpDate = (Date) JsonToDataConverter.convertJsonToPropertyValue(PropertyByType.DATETIME, ((String) strCashUpDate).subSequence(0, ((String) strCashUpDate).lastIndexOf(".")));
       } else {
         log.error("Error processing cash close: error retrieving cashUp date. Using current date");
       }
@@ -53,23 +52,21 @@ public class ProcessCashClose extends JSONProcessSimple {
       log.error("Error processing cash close: error retrieving cashUp date. Using current date");
     }
 
-    OBPOSApplications posTerminal = OBDal.getInstance().get(OBPOSApplications.class,
-        jsonCashup.getString("terminalId"));
-    OBContext.setOBContext(jsonCashup.getString("userId"), OBContext.getOBContext().getRole()
-        .getId(), OBContext.getOBContext().getCurrentClient().getId(), posTerminal
-        .getOrganization().getId());
+    OBPOSApplications posTerminal = OBDal.getInstance().get(OBPOSApplications.class, jsonCashup.getString("terminalId"));
+    OBContext.setOBContext(jsonCashup.getString("userId"), OBContext.getOBContext().getRole().getId(), OBContext.getOBContext().getCurrentClient().getId(), posTerminal.getOrganization().getId());
     OBPOSAppCashup cashUp = OBDal.getInstance().get(OBPOSAppCashup.class, cashUpId);
 
     // check if there is a reconciliation in draft status
     for (OBPOSAppPayment payment : posTerminal.getOBPOSAppPaymentList()) {
-      final OBCriteria<FIN_Reconciliation> recconciliations = OBDal.getInstance().createCriteria(
-          FIN_Reconciliation.class);
+      final OBCriteria<FIN_Reconciliation> recconciliations = OBDal.getInstance().createCriteria(FIN_Reconciliation.class);
       recconciliations.add(Restrictions.eq(FIN_Reconciliation.PROPERTY_DOCUMENTSTATUS, "DR"));
-      recconciliations.add(Restrictions.eq(FIN_Reconciliation.PROPERTY_ACCOUNT,
-          payment.getFinancialAccount()));
+      recconciliations.add(Restrictions.eq(FIN_Reconciliation.PROPERTY_ACCOUNT, payment.getFinancialAccount()));
       for (final FIN_Reconciliation r : recconciliations.list()) {
-        log.error("Error processing cash close: the reconciliation " + r.getDocumentNo() + " ("
-            + r.getAccount().getName() + ") is in draft status");
+        log.error("Error processing cash close: the reconciliation "
+            + r.getDocumentNo()
+            + " ("
+            + r.getAccount().getName()
+            + ") is in draft status");
         jsonData.put("error", "1");
         jsonData.put("errorMessage", "OBPOS_LblCashupWithReconciliationDraft");
         jsonData.put("errorDetail", payment.getCommercialName());
@@ -83,22 +80,18 @@ public class ProcessCashClose extends JSONProcessSimple {
     errorsQuery.add(Restrictions.ne(OBPOSErrors.PROPERTY_TYPEOFDATA, "CU"));
     errorsQuery.add(Restrictions.eq(OBPOSErrors.PROPERTY_ORDERSTATUS, "N"));
     if (errorsQuery.count() > 0) {
-      throw new OBException(
-          "There are errors related to non-created customers, orders, or cash management movements pending to be processed. Process them before processing the cash ups");
+      throw new OBException("There are errors related to non-created customers, orders, or cash management movements pending to be processed. Process them before processing the cash ups");
     }
 
     if (cashUp == null) {
       TriggerHandler.getInstance().disable();
       try {
         new OrderGroupingProcessor().groupOrders(posTerminal, cashUpId, cashUpDate);
-        posTerminal = OBDal.getInstance().get(OBPOSApplications.class,
-            jsonCashup.getString("terminalId"));
+        posTerminal = OBDal.getInstance().get(OBPOSApplications.class, jsonCashup.getString("terminalId"));
 
-        CashCloseProcessor processor = WeldUtils
-            .getInstanceFromStaticBeanManager(CashCloseProcessor.class);
+        CashCloseProcessor processor = WeldUtils.getInstanceFromStaticBeanManager(CashCloseProcessor.class);
         JSONArray cashMgmtIds = jsonCashup.getJSONArray("cashMgmtIds");
-        JSONObject result = processor.processCashClose(posTerminal, jsonCashup, cashMgmtIds,
-            cashUpDate);
+        JSONObject result = processor.processCashClose(posTerminal, jsonCashup, cashMgmtIds, cashUpDate);
 
         // add the messages returned by processCashClose...
         jsonData.put("messages", result.opt("messages"));
@@ -113,47 +106,49 @@ public class ProcessCashClose extends JSONProcessSimple {
   @Override
   public JSONObject exec(JSONObject jsonsent) throws JSONException, ServletException {
     OBContext.setAdminMode(false);
-    JSONArray jsonCashups = jsonsent.getJSONArray("cashups");
+    try {
+      JSONArray jsonCashups = jsonsent.getJSONArray("cashups");
 
-    jsonResponse.put(JsonConstants.RESPONSE_STATUS, JsonConstants.RPCREQUEST_STATUS_SUCCESS);
-    jsonResponse.put("result", "0");
+      jsonResponse.put(JsonConstants.RESPONSE_STATUS, JsonConstants.RPCREQUEST_STATUS_SUCCESS);
+      jsonResponse.put("result", "0");
 
-    for (int i = 0; i < jsonCashups.length(); i++) {
-      JSONObject jsonCashup = jsonCashups.getJSONObject(i);
-      try {
-        JSONObject jsonData = saveCashUp(jsonCashup);
-
-        jsonResponse.put(JsonConstants.RESPONSE_DATA, jsonData);
-        if (i % 1 == 0) {
-          OBDal.getInstance().getConnection(false).commit();
-          OBDal.getInstance().getSession().clear();
-        }
-        if (TriggerHandler.getInstance().isDisabled()) {
-          TriggerHandler.getInstance().enable();
-        }
-      } catch (Exception e) {
-        OBDal.getInstance().rollbackAndClose();
-        if (TriggerHandler.getInstance().isDisabled()) {
-          TriggerHandler.getInstance().enable();
-        }
-        OBPOSErrors errorEntry = OBProvider.getInstance().get(OBPOSErrors.class);
-        errorEntry.setError(OrderLoader.getErrorMessage(e));
-        errorEntry.setOrderstatus("N");
-        errorEntry.setJsoninfo(jsonCashup.toString());
-        errorEntry.setTypeofdata("CU");
-        errorEntry.setObposApplications(OBDal.getInstance().get(OBPOSApplications.class,
-            jsonCashup.getString("terminalId")));
-        OBDal.getInstance().save(errorEntry);
-        OBDal.getInstance().flush();
-        log.error("Error processing cash close", e);
+      for (int i = 0; i < jsonCashups.length(); i++) {
+        JSONObject jsonCashup = jsonCashups.getJSONObject(i);
         try {
-          OBDal.getInstance().getConnection().commit();
-        } catch (SQLException e1) {
-          // this won't happen
+          JSONObject jsonData = saveCashUp(jsonCashup);
+
+          jsonResponse.put(JsonConstants.RESPONSE_DATA, jsonData);
+          if (i % 1 == 0) {
+            OBDal.getInstance().getConnection(false).commit();
+            OBDal.getInstance().getSession().clear();
+          }
+          if (TriggerHandler.getInstance().isDisabled()) {
+            TriggerHandler.getInstance().enable();
+          }
+        } catch (Exception e) {
+          OBDal.getInstance().rollbackAndClose();
+          if (TriggerHandler.getInstance().isDisabled()) {
+            TriggerHandler.getInstance().enable();
+          }
+          OBPOSErrors errorEntry = OBProvider.getInstance().get(OBPOSErrors.class);
+          errorEntry.setError(OrderLoader.getErrorMessage(e));
+          errorEntry.setOrderstatus("N");
+          errorEntry.setJsoninfo(jsonCashup.toString());
+          errorEntry.setTypeofdata("CU");
+          errorEntry.setObposApplications(OBDal.getInstance().get(OBPOSApplications.class, jsonCashup.getString("terminalId")));
+          OBDal.getInstance().save(errorEntry);
+          OBDal.getInstance().flush();
+          log.error("Error processing cash close", e);
+          try {
+            OBDal.getInstance().getConnection().commit();
+          } catch (SQLException e1) {
+            // this won't happen
+          }
         }
       }
+    } finally {
+      OBContext.restorePreviousMode();
     }
-
     return jsonResponse;
   }
 
