f6ec65b39a687ac58e0ecfa07592cd2b4ffe0129
Guillermo Gil <guillermo.gil@openbravo.com>
2014-04-30 15:23:58 +0200

    Fixed issue 26195: FIC Error message is missing window name
    Added window name to error message in addition to tab name.

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/window/FormInitializationComponent.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/window/FormInitializationComponent.java
@@ -1250,8 +1250,8 @@ public class FormInitializationComponent extends BaseActionHandler {
               "inp" + Sqlc.TransformaNombreColumna(columnName), currentValue.toString());
         }
       } else {
-    	  RequestContext.get().setRequestParameter(
-                  "inp" + Sqlc.TransformaNombreColumna(columnName), null);
+        RequestContext.get().setRequestParameter("inp" + Sqlc.TransformaNombreColumna(columnName),
+            null);
       }
     } catch (Exception e) {
       log.error("Couldn't get the value for column " + columnName, e);
@@ -1829,7 +1829,8 @@ public class FormInitializationComponent extends BaseActionHandler {
       return fvalue;
     } catch (Exception e) {
       log.error("Error while computing auxiliary input parameter: " + auxIn.getName()
-          + " from tab: " + auxIn.getTab().getName(), e);
+          + " from tab: " + auxIn.getTab().getName() + " of window: "
+          + auxIn.getTab().getWindow().getName(), e);
     }
     return null;
   }
