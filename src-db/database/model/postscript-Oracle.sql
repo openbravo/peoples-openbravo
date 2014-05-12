bcd374ac1126750b8a4530d6b6611fe29878337c
Gorka Ion Damián <gorkaion.damian@openbravo.com>
2014-05-12 13:27:18 +0200

    [aprm-windows]Add stragg function for Oracle db.

--- src-db/database/model/postscript-Oracle.sql
+++ src-db/database/model/postscript-Oracle.sql
@@ -878,7 +878,102 @@ begin
 end;
 /-- END
  
+/*
+ * Add STRAGG function. Use of LISTAGG is prefered but it cannot be used until support for oracle
+ * versions prior to 11g R2 is discountinued.
+ */
+create or replace type stragg_type as object
+(
+  string varchar2(32767),
+
+  static function ODCIAggregateInitialize
+    ( sctx in out stragg_type )
+    return number ,
+
+  member function ODCIAggregateIterate
+    ( self  in out stragg_type ,
+      value in     varchar2
+    ) return number ,
+
+  member function ODCIAggregateTerminate
+    ( self        in  stragg_type,
+      returnvalue out varchar2,
+      flags in number
+    ) return number ,
+
+  member function ODCIAggregateMerge
+    ( self in out stragg_type,
+      ctx2 in     stragg_type
+    ) return number
+);
+/-- END
+
+create or replace type body stragg_type
+is
+
+  static function ODCIAggregateInitialize
+  ( sctx in out stragg_type )
+  return number
+  is
+  begin
+
+    sctx := stragg_type( null ) ;
+
+    return ODCIConst.Success ;
+
+  end;
+
+  member function ODCIAggregateIterate
+  ( self  in out stragg_type ,
+    value in     varchar2
+  ) return number
+  is
+  begin
+
+    self.string := self.string || ', ' || value ;
+
+    return ODCIConst.Success;
+
+  end;
+
+  member function ODCIAggregateTerminate
+  ( self        in  stragg_type ,
+    returnvalue out varchar2 ,
+    flags       in  number
+  ) return number
+  is
+  begin
+
+    returnValue := ltrim( self.string, ', ' );
+
+    return ODCIConst.Success;
+
+  end;
+
+  member function ODCIAggregateMerge
+  ( self in out stragg_type ,
+    ctx2 in     stragg_type
+  ) return number
+  is
+  begin
+
+    self.string := self.string || ctx2.string;
+
+    return ODCIConst.Success;
+
+  end;
+
+end;
+/-- END
+
+create or replace function stragg
+  ( input varchar2 )
+  return varchar2
+  deterministic
+  parallel_enable
+  aggregate using stragg_type
+;
+/-- END
 
 CALL DBA_RECOMPILE(NULL)
 /-- END
-
