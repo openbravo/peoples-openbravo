9c0c47c547bc5bab241bf99cca733f7e424be284
Miguel de Juana <miguel.dejuana@openbravo.com>
2014-05-16 12:45:12 +0200

    Fixed issue 0026537: Message "approvals required" does not work in offline mode and without supervisors
    --HG--
    extra : rebase_source : f2e8d24540c53a1f6675b4e3d1013d7e6e22d05e

--- web/org.openbravo.retail.posterminal/js/components/obpos-approval.js
+++ web/org.openbravo.retail.posterminal/js/components/obpos-approval.js
@@ -42,7 +42,7 @@ enyo.kind({
           callback: callback
         });
 
-        dialog.start();
+        dialog.show();
       }
     }
   },
@@ -126,9 +126,7 @@ enyo.kind({
       msg = msg + ' ' + (OB.I18N.labels[approval] || OB.I18N.getLabel('OBPOS_ApprovalTextHeader'));
     });
     this.$.bodyContent.$.explainApprovalTxt.setContent(msg);
-  },
 
-  start: function () {
     this.postRenderActions();
   },
 
@@ -166,32 +164,17 @@ enyo.kind({
   },
 
   renderUserButtons: function (name, userName, image) {
-
-    if (name.length === 0) {
-      // no supervisors. Show warning.
-      OB.MobileApp.view.$.containerWindow.getRoot().doShowPopup({
-        popup: 'OB_UI_MessageDialog',
-        args: {
-          header: OB.I18N.getLabel('OBPOS_ApprovalRequiredTitle'),
-          message: OB.I18N.getLabel('OBPOS_UserCannotApprove') + ' ' + OB.I18N.getLabel(this.approvalType)
-        }
+    var i, target = this.$.bodyContent.$.loginUserContainer;
+    for (i = 0; i < name.length; i++) {
+      target.createComponent({
+        kind: 'OB.OBPOSLogin.UI.UserButton',
+        user: userName[i],
+        userImage: image[i],
+        showConnectionStatus: false
       });
-    } else {
-      // Show users.
-      var i, target = this.$.bodyContent.$.loginUserContainer;
-      for (i = 0; i < name.length; i++) {
-        target.createComponent({
-          kind: 'OB.OBPOSLogin.UI.UserButton',
-          user: userName[i],
-          userImage: image[i],
-          showConnectionStatus: false
-        });
-      }
-      target.render();
-
-      // Dialog started.
-      this.show();
     }
+    target.render();
+    return true;
   },
 
   postRenderActions: function () {
