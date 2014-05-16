b956fa8517e06475ca5739db6d18a88aa6f4ed9a
Unai Martirena <unai.martirena@openbravo.com>
2014-05-16 08:57:54 +0200

    [aprm-windows] Add Payment Action Handler, Use Credit grid
    --HG--
    extra : rebase_source : dc8db58779cf73e9c46a1efddfac2bd20b851efb

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/actionHandler/AddPaymentActionHandler.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/actionHandler/AddPaymentActionHandler.java
@@ -20,22 +20,28 @@
 package org.openbravo.advpaymentmngt.actionHandler;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-import org.apache.log4j.Logger;
+import javax.servlet.ServletException;
+
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 import org.hibernate.criterion.Restrictions;
 import org.openbravo.advpaymentmngt.ad_actionbutton.AddPaymentFromInvoice;
 import org.openbravo.advpaymentmngt.dao.AdvPaymentMngtDao;
 import org.openbravo.advpaymentmngt.process.FIN_AddPayment;
+import org.openbravo.advpaymentmngt.process.FIN_PaymentProcess;
 import org.openbravo.advpaymentmngt.utility.FIN_Utility;
+import org.openbravo.base.filter.IsIDFilter;
 import org.openbravo.base.secureApp.VariablesSecureApp;
 import org.openbravo.client.application.process.BaseProcessActionHandler;
 import org.openbravo.client.kernel.RequestContext;
@@ -43,6 +49,7 @@ import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.service.OBCriteria;
 import org.openbravo.dal.service.OBDal;
 import org.openbravo.database.ConnectionProvider;
+import org.openbravo.erpCommon.utility.OBDateUtils;
 import org.openbravo.erpCommon.utility.OBError;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.erpCommon.utility.Utility;
@@ -51,30 +58,41 @@ import org.openbravo.model.common.currency.Currency;
 import org.openbravo.model.common.enterprise.DocumentType;
 import org.openbravo.model.common.enterprise.Organization;
 import org.openbravo.model.common.invoice.Invoice;
+import org.openbravo.model.common.plm.Product;
+import org.openbravo.model.financialmgmt.accounting.Costcenter;
+import org.openbravo.model.financialmgmt.accounting.UserDimension1;
+import org.openbravo.model.financialmgmt.accounting.UserDimension2;
+import org.openbravo.model.financialmgmt.gl.GLItem;
 import org.openbravo.model.financialmgmt.payment.FIN_FinancialAccount;
 import org.openbravo.model.financialmgmt.payment.FIN_Payment;
 import org.openbravo.model.financialmgmt.payment.FIN_PaymentMethod;
 import org.openbravo.model.financialmgmt.payment.FIN_PaymentScheduleDetail;
+import org.openbravo.model.marketing.Campaign;
+import org.openbravo.model.materialmgmt.cost.ABCActivity;
 import org.openbravo.model.pricing.pricelist.PriceList;
+import org.openbravo.model.project.Project;
 import org.openbravo.service.db.CallStoredProcedure;
 import org.openbravo.service.db.DalConnectionProvider;
 import org.openbravo.service.db.DbUtility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AddPaymentActionHandler extends BaseProcessActionHandler {
-  private static Logger log = Logger.getLogger(AddPaymentActionHandler.class);
+  private static Logger log = LoggerFactory.getLogger(AddPaymentActionHandler.class);
   private AdvPaymentMngtDao dao;
 
   @Override
   protected JSONObject doExecute(Map<String, Object> parameters, String content) {
     JSONObject jsonRequest = null;
     OBContext.setAdminMode();
-    ConnectionProvider conn = new DalConnectionProvider();
+    ConnectionProvider conn = new DalConnectionProvider(true);
     VariablesSecureApp vars = RequestContext.get().getVariablesSecureApp();
     try {
       // Get Params
       jsonRequest = new JSONObject(content);
       String strAction = null;
       OBError message = null;
+      dao = new AdvPaymentMngtDao();
 
       JSONObject jsonparams = jsonRequest.getJSONObject("_params");
       String strOrgId = jsonRequest.getString("inpadOrgId");
@@ -84,6 +102,7 @@ public class AddPaymentActionHandler extends BaseProcessActionHandler {
       boolean isReceipt = "Y".equals(strIssotrx);
       JSONObject orderInvoiceGrid = jsonparams.getJSONObject("order_invoice");
       JSONObject creditToUseGrid = jsonparams.getJSONObject("credit_to_use");
+      JSONObject strAddedGLItems = jsonparams.getJSONObject("glitem");
 
       strAction = (isReceipt ? "PRP" : "PPP");
 
@@ -95,22 +114,43 @@ public class AddPaymentActionHandler extends BaseProcessActionHandler {
       String strFinancialAccountId = jsonparams.getString("fin_financial_account_id");
       String strActualPayment = jsonparams.getString("actual_payment");
       String strConvertedAmount = jsonparams.getString("converted_amount");
+
+      // Format Date
       String strPaymentDate = jsonparams.getString("payment_date");
+      Date date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(strPaymentDate);
+      String formattedDate = OBDateUtils.formatDate(date);
+
+      // String formattedDate = new SimpleDateFormat("dd-MM-yyyy",
+      // Locale.getDefault()).format(date);
+      strPaymentDate = formattedDate;
+
       String strPaymentMethodId = jsonparams.getString("fin_paymentmethod_id");
       String strExpectedPayment = jsonparams.getString("expected_payment");
       String strConversionRate = jsonparams.getString("conversion_rate");
 
-      // ¿QUE ES ESTO?
+      // TODO
       String strDifferenceAction = "";
       BigDecimal refundAmount = BigDecimal.ZERO;
 
-      // String selection = orderInvoiceGrid.getString("_selection");
+      // Orders Invoices Grid
       JSONArray allselection = orderInvoiceGrid.getJSONArray("_selection");
-      String allrows = orderInvoiceGrid.getString("_allRows");
-      String strSelectedScheduledPaymentDetailIds = getSelectedRowIds(allselection);
+      String strSelectedScheduledPaymentDetailIds = null;
+      if (allselection.length() > 0) {
+        strSelectedScheduledPaymentDetailIds = getSelectedRowIds(allselection);
+      }
 
-      BigDecimal exchangeRate = new BigDecimal(strConversionRate);
-      BigDecimal convertedAmount = new BigDecimal(strConvertedAmount);
+      // GL Items
+      JSONArray addedGLITemsArray = strAddedGLItems.getJSONArray("_selection");
+
+      BigDecimal exchangeRate = BigDecimal.ZERO;
+      BigDecimal convertedAmount = new BigDecimal(0);
+      if (!"null".equals(strConversionRate)) {
+        exchangeRate = new BigDecimal(strConversionRate);
+      }
+      if (!"null".equals(strConvertedAmount)) {
+        convertedAmount = new BigDecimal(strConvertedAmount);
+      }
+
       BusinessPartner businessPartner = OBDal.getInstance().get(BusinessPartner.class,
           strReceivedFromId);
       PriceList priceList = isReceipt ? businessPartner.getPriceList() : businessPartner
@@ -135,8 +175,9 @@ public class AddPaymentActionHandler extends BaseProcessActionHandler {
       params.add(isReceipt ? "ARR" : "APP");
       String strDocTypeId = (String) CallStoredProcedure.getInstance().call("AD_GET_DOCTYPE",
           params, null);
+      DocumentType documentType = OBDal.getInstance().get(DocumentType.class, strDocTypeId);
       boolean documentEnabled = true;
-      String strDocBaseType = parameters.get(2).toString();
+      String strDocBaseType = documentType.getDocumentCategory();
       boolean orgLegalWithAccounting = FIN_Utility.periodControlOpened(Invoice.TABLE_NAME,
           strInvoiceId, Invoice.TABLE_NAME + "_ID", "LE");
 
@@ -177,6 +218,118 @@ public class AddPaymentActionHandler extends BaseProcessActionHandler {
           strReferenceNo, selectedPaymentDetails, selectedPaymentDetailAmounts,
           strDifferenceAction.equals("writeoff"), strDifferenceAction.equals("refund"),
           dao.getObject(Currency.class, strCurrencyId), exchangeRate, convertedAmount);
+
+      // Credit to Use Grid
+      JSONArray selectedCreditLines = creditToUseGrid.getJSONArray("_selection");
+      String strSelectedCreditLinesIds = null;
+      if (selectedCreditLines.length() > 0) {
+        strSelectedCreditLinesIds = getSelectedCreditLinesIds(selectedCreditLines);
+        List<FIN_Payment> selectedCreditPayment = FIN_Utility.getOBObjectList(FIN_Payment.class,
+            strSelectedCreditLinesIds);
+        HashMap<String, BigDecimal> selectedCreditPaymentAmounts = getSelectedCreditLinesAndAmount(
+            selectedCreditLines, selectedCreditPayment);
+
+        BigDecimal totalUsedCreditAmt = BigDecimal.ZERO;
+        for (final FIN_Payment creditPayment : selectedCreditPayment) {
+          // TODO: Añadir en la descripcion del payment de credito usado en que payment se usa
+          final BigDecimal usedCreditAmt = selectedCreditPaymentAmounts.get(creditPayment.getId());
+          final StringBuffer description = new StringBuffer();
+          if (creditPayment.getDescription() != null && !creditPayment.getDescription().equals(""))
+            description.append(creditPayment.getDescription()).append("\n");
+          description.append(String.format(
+              Utility.messageBD(conn, "APRM_CreditUsedPayment", vars.getLanguage()),
+              payment.getDocumentNo()));
+          String truncateDescription = (description.length() > 255) ? description.substring(0, 251)
+              .concat("...").toString() : description.toString();
+          creditPayment.setDescription(truncateDescription);
+          // Set Used Credit = Amount + Previous used credit introduced by the user
+          creditPayment.setUsedCredit(usedCreditAmt.add(creditPayment.getUsedCredit()));
+          FIN_PaymentProcess.linkCreditPayment(payment, usedCreditAmt, creditPayment);
+          OBDal.getInstance().save(creditPayment);
+        }
+      }
+
+      // Add GL Item lines
+      if (addedGLITemsArray.length() > 0) {
+        for (int i = 0; i < addedGLITemsArray.length(); i++) {
+          JSONObject glItem = addedGLITemsArray.getJSONObject(i);
+          BigDecimal glItemOutAmt = BigDecimal.ZERO;
+          BigDecimal glItemInAmt = BigDecimal.ZERO;
+
+          if (glItem.has("paidOut")) {
+            glItemOutAmt = new BigDecimal(glItem.getString("paidOut"));
+          }
+          if (glItem.has("receivedIn")) {
+            glItemInAmt = new BigDecimal(glItem.getString("receivedIn"));
+          }
+
+          BigDecimal glItemAmt = BigDecimal.ZERO;
+          if (isReceipt) {
+            glItemAmt = glItemInAmt.subtract(glItemOutAmt);
+          } else {
+            glItemAmt = glItemOutAmt.subtract(glItemInAmt);
+          }
+          String strGLItemId = null;
+          if (glItem.has("gLItem")) {
+            strGLItemId = glItem.getString("gLItem");
+            checkID(strGLItemId);
+          }
+
+          // Accounting Dimensions
+          BusinessPartner businessPartnerGLItem = null;
+          if (glItem.has("businessPartner")) {
+            final String strElement_BP = glItem.getString("businessPartner");
+            checkID(strElement_BP);
+            businessPartnerGLItem = dao.getObject(BusinessPartner.class, strElement_BP);
+          }
+          Product product = null;
+          if (glItem.has("product")) {
+            final String strElement_PR = glItem.getString("product");
+            checkID(strElement_PR);
+            product = dao.getObject(Product.class, strElement_PR);
+          }
+          Project project = null;
+          if (glItem.has("project")) {
+            final String strElement_PJ = glItem.getString("project");
+            checkID(strElement_PJ);
+            project = dao.getObject(Project.class, strElement_PJ);
+          }
+          ABCActivity activity = null;
+          if (glItem.has("cActivityDim")) {
+            final String strElement_AY = glItem.getString("cActivityDim");
+            checkID(strElement_AY);
+            activity = null;
+          }
+          Costcenter costCenter = null;
+          if (glItem.has("costCenter")) {
+            final String strElement_CC = glItem.getString("costCenter");
+            checkID(strElement_CC);
+            costCenter = dao.getObject(Costcenter.class, strElement_CC);
+          }
+          Campaign campaign = null;
+          if (glItem.has("cCampaignDim")) {
+            final String strElement_MC = glItem.getString("cCampaignDim");
+            checkID(strElement_MC);
+            campaign = null;
+          }
+          UserDimension1 user1 = null;
+          if (glItem.has("stDimension")) {
+            final String strElement_U1 = glItem.getString("stDimension");
+            checkID(strElement_U1);
+            user1 = dao.getObject(UserDimension1.class, strElement_U1);
+          }
+          UserDimension2 user2 = null;
+          if (glItem.has("ndDimension")) {
+            final String strElement_U2 = glItem.getString("ndDimension");
+            checkID(strElement_U2);
+            user2 = dao.getObject(UserDimension2.class, strElement_U2);
+          }
+          FIN_AddPayment
+              .saveGLItem(payment, glItemAmt, dao.getObject(GLItem.class, strGLItemId),
+                  businessPartner, product, project, campaign, activity, null, costCenter, user1,
+                  user2);
+        }
+      }
 
       if (strAction.equals("PRP") || strAction.equals("PPP") || strAction.equals("PRD")
           || strAction.equals("PPW")) {
@@ -246,7 +399,7 @@ public class AddPaymentActionHandler extends BaseProcessActionHandler {
         JSONObject selectedRow = allselection.getJSONObject(i);
         sb.append("'" + selectedRow.getString("id") + "', ");
       }
-      sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1, ")");
+      sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 2, ")");
       return sb.toString();
     } catch (Exception e) {
       OBDal.getInstance().rollbackAndClose();
@@ -366,4 +519,59 @@ public class AddPaymentActionHandler extends BaseProcessActionHandler {
     return orderedPSDs.list();
   }
 
+  private void checkID(final String id) throws ServletException {
+    if (!IsIDFilter.instance.accept(id)) {
+      log.error("Input: " + id + " not accepted by filter: IsIDFilter");
+      throw new ServletException("Input: " + id + " is not an accepted input");
+    }
+  }
+
+  /**
+   * @param allselection
+   *          : Selected Rows in Order Invoice grid
+   * @return
+   */
+  private String getSelectedCreditLinesIds(JSONArray allselection) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("(");
+    try {
+      for (int i = 0; i < allselection.length(); i++) {
+        JSONObject selectedRow = allselection.getJSONObject(i);
+        sb.append(selectedRow.getString("id") + ",");
+      }
+      sb.replace(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1, ")");
+      return sb.toString();
+    } catch (Exception e) {
+      OBDal.getInstance().rollbackAndClose();
+      log.error(e.getMessage(), e);
+      return null;
+    }
+  }
+
+  /**
+   * @param allselection
+   * @param _selectedCreditPayments
+   * @return
+   */
+  private HashMap<String, BigDecimal> getSelectedCreditLinesAndAmount(JSONArray allselection,
+      List<FIN_Payment> _selectedCreditPayments) {
+    HashMap<String, BigDecimal> selectedCreditLinesAmounts = new HashMap<String, BigDecimal>();
+
+    try {
+      for (FIN_Payment creditPayment : _selectedCreditPayments) {
+        for (int i = 0; i < allselection.length(); i++) {
+          JSONObject selectedRow = allselection.getJSONObject(i);
+          if (selectedRow.getString("id").equals(creditPayment.getId())) {
+            selectedCreditLinesAmounts.put(creditPayment.getId(),
+                new BigDecimal(selectedRow.getString("paymentAmount")));
+          }
+        }
+      }
+    } catch (Exception e) {
+      OBDal.getInstance().rollbackAndClose();
+      log.error(e.getMessage(), e);
+      return null;
+    }
+    return selectedCreditLinesAmounts;
+  }
 }
