96e65154e23d546c530cd66e4de2df052269ede7
Sandra Huguet <sandra.huguet@openbravo.com>
2014-05-21 10:35:22 +0200

    [aprm-windows] Distribute amount and used credit parameter

--- modules/org.openbravo.advpaymentmngt/web/org.openbravo.advpaymentmngt/js/ob-aprm-addPayment.js
+++ modules/org.openbravo.advpaymentmngt/web/org.openbravo.advpaymentmngt/js/ob-aprm-addPayment.js
@@ -20,6 +20,7 @@ OB.APRM.AddPayment = {};
 
 OB.APRM.AddPayment.onLoad = function (view) {
   OB.APRM.AddPayment.paymentMethodMulticurrency(null, view, null, null);
+  OB.APRM.AddPayment.actualPaymentOnLoad(view);
 };
 
 OB.APRM.AddPayment.addNewGLItem = function (grid) {
@@ -88,18 +89,81 @@ OB.APRM.AddPayment.transactionTypeOnChangeFunction = function (item, view, form,
   form.redraw();
 };
 
-OB.APRM.AddPayment.distributeAmount = function (view) {
-  var actualPayment = view.theForm.getItem('actual_payment').getValue(),
-      distributedAmount = 0,
-      keepSelection = false,
-      chk = view.theForm.getItem('order_invoice').canvas.viewGrid.selectedIds,
-      scheduledPaymentDetailId, outstandingAmount, j, i, isGLItemEnabled = view.theForm.getItem('glitem').canvas.viewGrid.isVisible();
-  if (isGLItemEnabled) {
-    actualPayment = actualPayment - view.theForm.getItem('amount_gl_items').getValue();
+OB.APRM.AddPayment.actualPaymentOnChange = function (item, view, form, grid) {
+  var issotrx = form.getItem('issotrx').getValue();
+  if (issotrx) {
+    OB.APRM.AddPayment.distributeAmount(view, form);
   }
 
+};
 
+OB.APRM.AddPayment.actualPaymentOnLoad = function (view) {
 
+  var orderInvoiceGrid = view.theForm.getItem('order_invoice').canvas.viewGrid;
+  if (!isc.isA.ResultSet(orderInvoiceGrid.data) || !orderInvoiceGrid.data.lengthIsKnown()) {
+    setTimeout(function () {
+      OB.APRM.AddPayment.actualPaymentOnLoad(view);
+    }, 500);
+    return;
+  }
+  var issotrx = view.theForm.getItem('issotrx').getValue(),
+      form = view.theForm;
+  if (issotrx) {
+    OB.APRM.AddPayment.distributeAmount(view, form);
+  }
+
+};
+OB.APRM.AddPayment.distributeAmount = function (view, form) {
+  var amount = form.getItem('actual_payment').getValue(),
+      distributedAmount = 0,
+      keepSelection = false,
+      orderInvoice = form.getItem('order_invoice').canvas.viewGrid,
+      scheduledPaymentDetailId, outstandingAmount, j, i, isGLItemEnabled = form.getItem('glitem').canvas.viewGrid.isVisible(),
+      total, chk, credit, glitem;
+  //cambiar   
+  if (amount.Class === "String") {
+    amount = OB.Utilities.Number.OBMaskedToJS(amount, form.getItem('actual_payment').typeInstance.decSeparator, form.getItem('actual_payment').typeInstance.groupSeparator);
+  }
+  //amounts de gl items
+  if (isGLItemEnabled) {
+    glitem = form.getItem('amount_gl_items').getValue() || 0;
+    amount = amount - glitem;
+  }
+  //amount de credito
+  credit = form.getItem('used_credit').getValue() || 0;
+  amount = amount + credit;
+
+  chk = orderInvoice.selectedIds;
+  total = orderInvoice.data.totalRows;
+
+  if ((amount > distributedAmount) || (amount === distributedAmount)) {
+    amount = amount - distributedAmount;
+  }
+  for (i = 0; i < total; i++) {
+    if (keepSelection) {
+      continue;
+    }
+    outstandingAmount = orderInvoice.getRecord(i).outstandingAmount;
+    if ((outstandingAmount < 0) && (amount < 0)) {
+      if (Math.abs(outstandingAmount) > Math.abs(amount)) {
+        outstandingAmount = amount;
+      }
+    } else {
+      if (outstandingAmount > amount) {
+        outstandingAmount = amount;
+      }
+    }
+    if (amount === 0) {
+      orderInvoice.setEditValue((i), 'amount', "");
+      orderInvoice.deselectRecord(i);
+
+    } else {
+      orderInvoice.setEditValue((i), 'amount', outstandingAmount);
+      orderInvoice.selectRecord(i);
+      amount = amount - outstandingAmount;
+    }
+
+  }
   OB.APRM.AddPayment.updateTotal();
   return true;
 
@@ -107,5 +171,10 @@ OB.APRM.AddPayment.distributeAmount = function (view) {
 
 
 OB.APRM.AddPayment.updateTotal = function () {
+
+};
+
+OB.APRM.AddPayment.updateData = function (key, mark, drivenByGrid, all) {
+
 
 };
