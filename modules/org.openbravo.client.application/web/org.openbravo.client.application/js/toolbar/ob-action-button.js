41b2684230c7df149feae093645c65b18676ae20
Augusto Mauch <augusto.mauch@openbravo.com>
2014-05-22 17:37:58 +0200

    [pw3] The title of P&E popups is not underlined

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/toolbar/ob-action-button.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/toolbar/ob-action-button.js
@@ -91,7 +91,7 @@ isc.OBToolbarActionButton.addProperties({
           paramWindow: true,
           processId: me.processId,
           windowId: me.windowId,
-          windowTitle: me.windowTitle || me.title,
+          windowTitle: me.windowTitle || me.realTitle,
           actionHandler: me.command,
           button: me,
           buttons: me.labelValue,
