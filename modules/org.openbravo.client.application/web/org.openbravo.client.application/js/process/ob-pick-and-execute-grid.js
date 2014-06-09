ed0ef9924ad613e868637b55542aedc676277f58
Augusto Mauch <augusto.mauch@openbravo.com>
2014-06-09 11:08:47 +0200

    [aprm-windows] Do not show error is 0 is entered in a required field
    --HG--
    extra : rebase_source : e394b08ca0b3e4d198a71fbcad142076fed24a38

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/process/ob-pick-and-execute-grid.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/process/ob-pick-and-execute-grid.js
@@ -317,8 +317,13 @@ isc.OBPickAndExecuteGrid.addProperties({
         colNum = this.getEditCol(),
         editField = this.getEditField(colNum),
         undef;
-    // if not value is provided use the value from the edit form. If it does not exist, use the stored value
-    newValue = newValue || this.getEditValue(rowNum, colNum) || this.getRecord(rowNum)[editField.name];
+    // if no value is provided use the value from the edit form. If it does not exist, use the stored value
+    if (newValue === null || newValue === undefined) {
+      newValue = this.getEditValue(rowNum, colNum);
+    }
+    if (newValue === null || newValue === undefined) {
+      newValue = this.getRecord(rowNum)[editField.name];
+    }
     // Execute onChangeFunctions if they exist
     if (this && OB.OnChangeRegistry.hasOnChange(this.view.viewId, editField)) {
       OB.OnChangeRegistry.call(this.ID, editField, this.view, this.view.theForm, this);
