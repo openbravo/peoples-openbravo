4528ae785f7ad4e25bc0764f1b3f579110d27c45
Asier Lostalé <asier.lostale@openbravo.com>
2014-06-18 09:12:58 +0200

    fixed bug 26846: removed inactive session check in context initialization   this was causing problems with load balanced and it orginal purpose of   handling concurrent users is not valid anymore as per fix of #11039
    --HG--
    extra : rebase_source : 413d603a86d75ee3c34f4763fa98979d90982068

--- src/org/openbravo/erpCommon/security/SessionListener.java
+++ src/org/openbravo/erpCommon/security/SessionListener.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2009-2012 Openbravo SLU 
+ * All portions are Copyright (C) 2009-2014 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -19,17 +19,12 @@
 
 package org.openbravo.erpCommon.security;
 
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.SocketTimeoutException;
-import java.net.URL;
 import java.util.Vector;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
@@ -101,24 +96,6 @@ public class SessionListener implements HttpSessionListener, ServletContextListe
   public void contextInitialized(ServletContextEvent event) {
     SessionListener.context = event.getServletContext();
 
-    // Look orphan sessions and close them, these sessions are:
-    // -All the ones in the current context that will generate a time out when querying the servlet
-    // -The ones in other contexts that are no longer active
-
-    SessionLoginData activeSessions[];
-    try {
-      activeSessions = SessionLoginData.activeSessions((ConnectionProvider) SessionListener.context
-          .getAttribute("openbravoPool"));
-
-      for (SessionLoginData session : activeSessions) {
-        if (!checkSessionInRemoteContext(session.adSessionId, session.serverUrl)) {
-          deactivateSession(session.adSessionId);
-        }
-      }
-    } catch (ServletException e) {
-      log.error("Error checking orphan sessions", e);
-    }
-
     // Decide whether audit trail is active
     try {
       SessionInfo.setAuditActive(SessionLoginData
@@ -134,43 +111,6 @@ public class SessionListener implements HttpSessionListener, ServletContextListe
     } catch (Exception e) {
       log.error("Error activating usage audit", e);
     }
-  }
-
-  private boolean checkSessionInRemoteContext(String sessionId, String serverUrl) {
-    try {
-      log.info("Checking session " + sessionId + " in context " + serverUrl);
-      URL url = new URL(serverUrl + "/security/SessionActive?id=" + sessionId);
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setRequestProperty("Keep-Alive", "300");
-      conn.setRequestProperty("Connection", "keep-alive");
-      conn.setRequestMethod("POST");
-      conn.setDoInput(true);
-      conn.setDoOutput(true);
-      conn.setUseCaches(false);
-      conn.setAllowUserInteraction(false);
-
-      // Set short timeouts because for current context sessions timeout will be raised
-      conn.setConnectTimeout(500);
-      conn.setReadTimeout(500);
-
-      if (conn.getResponseCode() == HttpServletResponse.SC_OK) {
-        InputStream is = conn.getInputStream();
-        byte buff[] = new byte[100];
-        int len;
-        String result = "";
-        while ((len = is.read(buff)) != -1) {
-          result += new String(buff, 0, len);
-        }
-        return result.equals("true");
-      } else {
-        return false;
-      }
-    } catch (SocketTimeoutException e) {
-      log.debug("Timeout connecting to " + serverUrl + " to check session " + sessionId);
-    } catch (Exception e) {
-      log.debug("Error checking remote session " + sessionId + " in context " + serverUrl, e);
-    }
-    return false;
   }
 
   @Override
