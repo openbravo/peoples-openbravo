114803881dbaf82b064d4334ec5a36ac8404c4a1
Sandra Huguet <sandra.huguet@openbravo.com>
2014-06-25 13:25:14 +0200

    Backed out changeset 3aaaf391b34d
    --HG--
    extra : transplant_source : %C0%1C%06w%FF%02E%23%DE%14%D5%99%E0A%B9%EF6%C5%22%B5

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/process/FIN_ExecutePayment.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/process/FIN_ExecutePayment.java
@@ -182,8 +182,11 @@ public class FIN_ExecutePayment {
             paymentRunPayment.getPayment().setPosted("N");
             try {
               for (FIN_PaymentDetail pd : payment.getFINPaymentDetailList()) {
+                if (pd.getGLItem() != null) {
+                  continue;
+                }
                 for (FIN_PaymentScheduleDetail psd : pd.getFINPaymentScheduleDetailList()) {
-                  if ((pd.getGLItem() != null || psd.isInvoicePaid())
+                  if (psd.isInvoicePaid()
                       && FIN_Utility.isAutomaticDepositWithdrawn(paymentRunPayment.getPayment())
                       && paymentRunPayment.getPayment().getAmount().compareTo(BigDecimal.ZERO) != 0) {
                     FIN_FinaccTransaction transaction = TransactionsDao
@@ -208,8 +211,11 @@ public class FIN_ExecutePayment {
           try {
             OBContext.setAdminMode(true);
             for (FIN_PaymentDetail pd : payment.getFINPaymentDetailList()) {
+              if (pd.getGLItem() != null) {
+                continue;
+              }
               for (FIN_PaymentScheduleDetail psd : pd.getFINPaymentScheduleDetailList()) {
-                if (pd.getGLItem() != null || psd.isInvoicePaid()) {
+                if (psd.isInvoicePaid()) {
                   updatePaymentAmounts(paymentRunPayment.getPayment());
                 }
               }
