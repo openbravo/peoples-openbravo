5cb4f23a60c995447188606e311c2209dd66b4fe
Rafa Alonso <rafael.alonso@openbravo.com>
2014-09-02 11:07:16 +0200

    Fixes issue 27425: Cashup invoices are created as paid

--- src/org/openbravo/retail/posterminal/OrderGroupingProcessor.java
+++ src/org/openbravo/retail/posterminal/OrderGroupingProcessor.java
@@ -496,7 +496,8 @@ public class OrderGroupingProcessor {
       paymentSchedule.setOutstandingAmount(grossamount.subtract(totalPaid));
 
     }
-    if (grossamount.compareTo(BigDecimal.ZERO) != 0) {
+
+    if (grossamount.compareTo(BigDecimal.ZERO) == 0) {
       totalPaid = BigDecimal.ZERO;
     }
 
