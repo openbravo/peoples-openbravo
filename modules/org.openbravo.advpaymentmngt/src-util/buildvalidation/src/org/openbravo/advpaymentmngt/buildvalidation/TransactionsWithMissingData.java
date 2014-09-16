b2162ca79ae9a9b75a36f523ef4acab40ebfd5e0
Unai Martirena <unai.martirena@openbravo.com>
2014-09-16 13:02:24 +0200

    [Reconcilation-refactor] Build Validation for invalid transactions

--- modules/org.openbravo.advpaymentmngt/src-util/buildvalidation/src/org/openbravo/advpaymentmngt/buildvalidation/TransactionsWithMissingData.java
+++ modules/org.openbravo.advpaymentmngt/src-util/buildvalidation/src/org/openbravo/advpaymentmngt/buildvalidation/TransactionsWithMissingData.java
@@ -24,7 +24,7 @@ import java.util.List;
 import org.openbravo.buildvalidation.BuildValidation;
 import org.openbravo.database.ConnectionProvider;
 
-public class ZeroPaymentamtDepositAmtTransactions extends BuildValidation {
+public class TransactionsWithMissingData extends BuildValidation {
 
   @Override
   public List<String> execute() {
@@ -32,19 +32,19 @@ public class ZeroPaymentamtDepositAmtTransactions extends BuildValidation {
     ArrayList<String> errors = new ArrayList<String>();
     try {
       // Prevent error when upgrading from a pure 2.50
-      if (ZeroPaymentamtDepositAmtTransactionsData.existAPRMbasetables(cp)) {
+      if (TransactionsWithMissingDataData.existAPRMbasetables(cp)) {
 
-        ZeroPaymentamtDepositAmtTransactionsData[] listofTransactions = ZeroPaymentamtDepositAmtTransactionsData
-            .selectZeroPaymentamtDepositamtTransactions(cp);
+        TransactionsWithMissingDataData[] listofTransactions = TransactionsWithMissingDataData
+            .selectTransactionsWithMissingData(cp);
         if (listofTransactions != null && listofTransactions.length > 0) {
           String message = "You cannot apply this Advanced Payables and Receivables Management module version because your instance fails in a pre-validation. "
-              + "It is not allowed to upgrade to this version having transactions with Payment Amount and Deposit Amount of ZERO. "
-              + "To fix this problem in your instance, have a look to generated alerts (Transactions with Deposit Amount and Payment Amount of Zero) and identify the affected transactions. "
-              + "If you have a transaction like that, fix the problem updating Payment Amount or Deposit Amount to an Amount different to ZERO. ";
+              + "It is not allowed to upgrade to this version having transactions with Payment Amount and Deposit Amount of ZERO and/or null Transaction Date and/or null Accounting Date. "
+              + "To fix this problem in your instance, have a look to generated alerts (Transactions with Missing Data) and identify the affected transactions. "
+              + "If you have a transaction like that, fix the problem updating Payment Amount or Deposit Amount to an Amount different to ZERO and/or filling Transaction Date and/or Accounting Date. ";
           errors.add(message);
         }
 
-        for (ZeroPaymentamtDepositAmtTransactionsData transaction : listofTransactions) {
+        for (TransactionsWithMissingDataData transaction : listofTransactions) {
           processAlert(cp, transaction);
         }
 
@@ -55,14 +55,14 @@ public class ZeroPaymentamtDepositAmtTransactions extends BuildValidation {
     return errors;
   }
 
-  private void processAlert(ConnectionProvider cp, ZeroPaymentamtDepositAmtTransactionsData transaction)
+  private void processAlert(ConnectionProvider cp, TransactionsWithMissingDataData transaction)
       throws Exception {
     final String TRANSACTION_TAB = "23691259D1BD4496BCC5F32645BCA4B9";
     final String FINANCIAL_ACCOUNT_WINDOW = "94EAA455D2644E04AB25D93BE5157B6D";
 
-    String ALERT_RULE_NAME = "Transactions with Deposit Amount and Payment Amount of Zero";
-    String alertDescription = "Transaction has Deposit Amount and Payment Amount of Zero, at least one of them has to be different to Zero. "
-        + "Update Payment Amount or Deposit Amount to an Amount different to Zero";
+    String ALERT_RULE_NAME = "Transactions with Missing Data";
+    String alertDescription = "Transaction has Deposit Amount and Payment Amount of Zero (at least one of them has to be different to Zero), and/or Transaction Date and/or Accounting Date are null. "
+        + "Update Payment Amount or Deposit Amount to an Amount different to Zero and/or fill Transaction Date and/or Accounting Date. ";
     
     String alertRuleId = "";
 
@@ -74,30 +74,31 @@ public class ZeroPaymentamtDepositAmtTransactions extends BuildValidation {
         + " 'Y' as isActive, ad_org_id, ad_client_id,"
         + " now() as created, 0 as createdBy, now() as updated, 0 as updatedBy"
         + " FROM fin_finacc_transaction"
-        + " WHERE depositamt = 0"
-        + " AND paymentamt = 0";
+        + " WHERE (depositamt = 0 AND paymentamt = 0) "
+        + " OR statementdate IS NULL "
+        + " OR dateacct IS NULL ";
 
     // Check if exists the alert rule
-    if (!ZeroPaymentamtDepositAmtTransactionsData.existsAlertRule(cp, ALERT_RULE_NAME, transaction.adClientId)) {
-      ZeroPaymentamtDepositAmtTransactionsData.insertAlertRule(cp, transaction.adClientId, transaction.adOrgId,
+    if (!TransactionsWithMissingDataData.existsAlertRule(cp, ALERT_RULE_NAME, transaction.adClientId)) {
+      TransactionsWithMissingDataData.insertAlertRule(cp, transaction.adClientId, transaction.adOrgId,
           ALERT_RULE_NAME, TRANSACTION_TAB, ALERT_RULE_SQL);
 
-      alertRuleId = ZeroPaymentamtDepositAmtTransactionsData.getAlertRuleId(cp, ALERT_RULE_NAME,
+      alertRuleId = TransactionsWithMissingDataData.getAlertRuleId(cp, ALERT_RULE_NAME,
           transaction.adClientId);
-      ZeroPaymentamtDepositAmtTransactionsData[] roles = ZeroPaymentamtDepositAmtTransactionsData.getRoleId(cp,
+      TransactionsWithMissingDataData[] roles = TransactionsWithMissingDataData.getRoleId(cp,
           FINANCIAL_ACCOUNT_WINDOW, transaction.adClientId);
-      for (ZeroPaymentamtDepositAmtTransactionsData role : roles) {
-        ZeroPaymentamtDepositAmtTransactionsData.insertAlertRecipient(cp, transaction.adClientId,
+      for (TransactionsWithMissingDataData role : roles) {
+        TransactionsWithMissingDataData.insertAlertRecipient(cp, transaction.adClientId,
             transaction.adOrgId, alertRuleId, role.adRoleId);
       }
     } else {
-      alertRuleId = ZeroPaymentamtDepositAmtTransactionsData.getAlertRuleId(cp, ALERT_RULE_NAME,
+      alertRuleId = TransactionsWithMissingDataData.getAlertRuleId(cp, ALERT_RULE_NAME,
           transaction.adClientId);
     }
 
     // Check if exist the concrete alert for the payment
-    if (!ZeroPaymentamtDepositAmtTransactionsData.existsAlert(cp, alertRuleId, transaction.finFinaccTransactionId)) {
-      ZeroPaymentamtDepositAmtTransactionsData.insertAlert(cp, transaction.adClientId, alertDescription,
+    if (!TransactionsWithMissingDataData.existsAlert(cp, alertRuleId, transaction.finFinaccTransactionId)) {
+      TransactionsWithMissingDataData.insertAlert(cp, transaction.adClientId, alertDescription,
           alertRuleId, transaction.documentno, transaction.finFinaccTransactionId);
     }
 
--- modules/org.openbravo.advpaymentmngt/src-util/buildvalidation/src/src/org/openbravo/advpaymentmngt/buildvalidation/TransactionsWithMissingDataData.java
+++ modules/org.openbravo.advpaymentmngt/src-util/buildvalidation/src/src/org/openbravo/advpaymentmngt/buildvalidation/TransactionsWithMissingDataData.java
@@ -12,8 +12,8 @@ import org.openbravo.database.ConnectionProvider;
 import org.openbravo.data.UtilSql;
 import java.util.*;
 
-class ZeroPaymentamtDepositAmtTransactionsData implements FieldProvider {
-static Logger log4j = Logger.getLogger(ZeroPaymentamtDepositAmtTransactionsData.class);
+class TransactionsWithMissingDataData implements FieldProvider {
+static Logger log4j = Logger.getLogger(TransactionsWithMissingDataData.class);
   private String InitRecordNumber="0";
   public String documentno;
   public String finFinaccTransactionId;
@@ -42,11 +42,11 @@ static Logger log4j = Logger.getLogger(ZeroPaymentamtDepositAmtTransactionsData.
    }
  }
 
-  public static ZeroPaymentamtDepositAmtTransactionsData[] dummy(ConnectionProvider connectionProvider)    throws ServletException {
+  public static TransactionsWithMissingDataData[] dummy(ConnectionProvider connectionProvider)    throws ServletException {
     return dummy(connectionProvider, 0, 0);
   }
 
-  public static ZeroPaymentamtDepositAmtTransactionsData[] dummy(ConnectionProvider connectionProvider, int firstRegister, int numberRegisters)    throws ServletException {
+  public static TransactionsWithMissingDataData[] dummy(ConnectionProvider connectionProvider, int firstRegister, int numberRegisters)    throws ServletException {
     String strSql = "";
     strSql = strSql + 
       "      SELECT '' AS documentno, '' AS fin_finacc_transaction_id, '' AS ad_client_id," +
@@ -70,14 +70,14 @@ static Logger log4j = Logger.getLogger(ZeroPaymentamtDepositAmtTransactionsData.
       }
       while(continueResult && result.next()) {
         countRecord++;
-        ZeroPaymentamtDepositAmtTransactionsData objectZeroPaymentamtDepositAmtTransactionsData = new ZeroPaymentamtDepositAmtTransactionsData();
-        objectZeroPaymentamtDepositAmtTransactionsData.documentno = UtilSql.getValue(result, "documentno");
-        objectZeroPaymentamtDepositAmtTransactionsData.finFinaccTransactionId = UtilSql.getValue(result, "fin_finacc_transaction_id");
-        objectZeroPaymentamtDepositAmtTransactionsData.adClientId = UtilSql.getValue(result, "ad_client_id");
-        objectZeroPaymentamtDepositAmtTransactionsData.adOrgId = UtilSql.getValue(result, "ad_org_id");
-        objectZeroPaymentamtDepositAmtTransactionsData.adRoleId = UtilSql.getValue(result, "ad_role_id");
-        objectZeroPaymentamtDepositAmtTransactionsData.InitRecordNumber = Integer.toString(firstRegister);
-        vector.addElement(objectZeroPaymentamtDepositAmtTransactionsData);
+        TransactionsWithMissingDataData objectTransactionsWithMissingDataData = new TransactionsWithMissingDataData();
+        objectTransactionsWithMissingDataData.documentno = UtilSql.getValue(result, "documentno");
+        objectTransactionsWithMissingDataData.finFinaccTransactionId = UtilSql.getValue(result, "fin_finacc_transaction_id");
+        objectTransactionsWithMissingDataData.adClientId = UtilSql.getValue(result, "ad_client_id");
+        objectTransactionsWithMissingDataData.adOrgId = UtilSql.getValue(result, "ad_org_id");
+        objectTransactionsWithMissingDataData.adRoleId = UtilSql.getValue(result, "ad_role_id");
+        objectTransactionsWithMissingDataData.InitRecordNumber = Integer.toString(firstRegister);
+        vector.addElement(objectTransactionsWithMissingDataData);
         if (countRecord >= numberRegisters && numberRegisters != 0) {
           continueResult = false;
         }
@@ -96,28 +96,29 @@ static Logger log4j = Logger.getLogger(ZeroPaymentamtDepositAmtTransactionsData.
         ignore.printStackTrace();
       }
     }
-    ZeroPaymentamtDepositAmtTransactionsData objectZeroPaymentamtDepositAmtTransactionsData[] = new ZeroPaymentamtDepositAmtTransactionsData[vector.size()];
-    vector.copyInto(objectZeroPaymentamtDepositAmtTransactionsData);
-    return(objectZeroPaymentamtDepositAmtTransactionsData);
+    TransactionsWithMissingDataData objectTransactionsWithMissingDataData[] = new TransactionsWithMissingDataData[vector.size()];
+    vector.copyInto(objectTransactionsWithMissingDataData);
+    return(objectTransactionsWithMissingDataData);
   }
 
 /**
-This query returns payments that are wrongly linked to more than one transaction
+This query returns transactions with deposit and payment amount equal to Zero and/or Transaction Date null and/or Accounting Date null
  */
-  public static ZeroPaymentamtDepositAmtTransactionsData[] selectZeroPaymentamtDepositamtTransactions(ConnectionProvider connectionProvider)    throws ServletException {
-    return selectZeroPaymentamtDepositamtTransactions(connectionProvider, 0, 0);
+  public static TransactionsWithMissingDataData[] selectTransactionsWithMissingData(ConnectionProvider connectionProvider)    throws ServletException {
+    return selectTransactionsWithMissingData(connectionProvider, 0, 0);
   }
 
 /**
-This query returns payments that are wrongly linked to more than one transaction
+This query returns transactions with deposit and payment amount equal to Zero and/or Transaction Date null and/or Accounting Date null
  */
-  public static ZeroPaymentamtDepositAmtTransactionsData[] selectZeroPaymentamtDepositamtTransactions(ConnectionProvider connectionProvider, int firstRegister, int numberRegisters)    throws ServletException {
+  public static TransactionsWithMissingDataData[] selectTransactionsWithMissingData(ConnectionProvider connectionProvider, int firstRegister, int numberRegisters)    throws ServletException {
     String strSql = "";
     strSql = strSql + 
       "      SELECT ad_column_identifier('fin_finacc_transaction_id', fin_finacc_transaction_id, 'en_US') as documentno, fin_finacc_transaction_id, ad_client_id, ad_org_id " +
       "      FROM fin_finacc_transaction" +
-      "      WHERE depositamt = 0" +
-      "      AND paymentamt = 0";
+      "      WHERE (depositamt = 0 AND paymentamt = 0)" +
+      "      OR statementdate IS NULL " +
+      "      OR dateacct IS NULL";
 
     ResultSet result;
     Vector<java.lang.Object> vector = new Vector<java.lang.Object>(0);
@@ -136,13 +137,13 @@ This query returns payments that are wrongly linked to more than one transaction
       }
       while(continueResult && result.next()) {
         countRecord++;
-        ZeroPaymentamtDepositAmtTransactionsData objectZeroPaymentamtDepositAmtTransactionsData = new ZeroPaymentamtDepositAmtTransactionsData();
-        objectZeroPaymentamtDepositAmtTransactionsData.documentno = UtilSql.getValue(result, "documentno");
-        objectZeroPaymentamtDepositAmtTransactionsData.finFinaccTransactionId = UtilSql.getValue(result, "fin_finacc_transaction_id");
-        objectZeroPaymentamtDepositAmtTransactionsData.adClientId = UtilSql.getValue(result, "ad_client_id");
-        objectZeroPaymentamtDepositAmtTransactionsData.adOrgId = UtilSql.getValue(result, "ad_org_id");
-        objectZeroPaymentamtDepositAmtTransactionsData.InitRecordNumber = Integer.toString(firstRegister);
-        vector.addElement(objectZeroPaymentamtDepositAmtTransactionsData);
+        TransactionsWithMissingDataData objectTransactionsWithMissingDataData = new TransactionsWithMissingDataData();
+        objectTransactionsWithMissingDataData.documentno = UtilSql.getValue(result, "documentno");
+        objectTransactionsWithMissingDataData.finFinaccTransactionId = UtilSql.getValue(result, "fin_finacc_transaction_id");
+        objectTransactionsWithMissingDataData.adClientId = UtilSql.getValue(result, "ad_client_id");
+        objectTransactionsWithMissingDataData.adOrgId = UtilSql.getValue(result, "ad_org_id");
+        objectTransactionsWithMissingDataData.InitRecordNumber = Integer.toString(firstRegister);
+        vector.addElement(objectTransactionsWithMissingDataData);
         if (countRecord >= numberRegisters && numberRegisters != 0) {
           continueResult = false;
         }
@@ -161,9 +162,9 @@ This query returns payments that are wrongly linked to more than one transaction
         ignore.printStackTrace();
       }
     }
-    ZeroPaymentamtDepositAmtTransactionsData objectZeroPaymentamtDepositAmtTransactionsData[] = new ZeroPaymentamtDepositAmtTransactionsData[vector.size()];
-    vector.copyInto(objectZeroPaymentamtDepositAmtTransactionsData);
-    return(objectZeroPaymentamtDepositAmtTransactionsData);
+    TransactionsWithMissingDataData objectTransactionsWithMissingDataData[] = new TransactionsWithMissingDataData[vector.size()];
+    vector.copyInto(objectTransactionsWithMissingDataData);
+    return(objectTransactionsWithMissingDataData);
   }
 
   public static String getAlertRuleId(ConnectionProvider connectionProvider, String name, String client)    throws ServletException {
@@ -371,11 +372,11 @@ This query returns payments that are wrongly linked to more than one transaction
     return(updateCount);
   }
 
-  public static ZeroPaymentamtDepositAmtTransactionsData[] getRoleId(ConnectionProvider connectionProvider, String window, String clientId)    throws ServletException {
+  public static TransactionsWithMissingDataData[] getRoleId(ConnectionProvider connectionProvider, String window, String clientId)    throws ServletException {
     return getRoleId(connectionProvider, window, clientId, 0, 0);
   }
 
-  public static ZeroPaymentamtDepositAmtTransactionsData[] getRoleId(ConnectionProvider connectionProvider, String window, String clientId, int firstRegister, int numberRegisters)    throws ServletException {
+  public static TransactionsWithMissingDataData[] getRoleId(ConnectionProvider connectionProvider, String window, String clientId, int firstRegister, int numberRegisters)    throws ServletException {
     String strSql = "";
     strSql = strSql + 
       "       SELECT distinct r.ad_role_id" +
@@ -405,10 +406,10 @@ This query returns payments that are wrongly linked to more than one transaction
       }
       while(continueResult && result.next()) {
         countRecord++;
-        ZeroPaymentamtDepositAmtTransactionsData objectZeroPaymentamtDepositAmtTransactionsData = new ZeroPaymentamtDepositAmtTransactionsData();
-        objectZeroPaymentamtDepositAmtTransactionsData.adRoleId = UtilSql.getValue(result, "ad_role_id");
-        objectZeroPaymentamtDepositAmtTransactionsData.InitRecordNumber = Integer.toString(firstRegister);
-        vector.addElement(objectZeroPaymentamtDepositAmtTransactionsData);
+        TransactionsWithMissingDataData objectTransactionsWithMissingDataData = new TransactionsWithMissingDataData();
+        objectTransactionsWithMissingDataData.adRoleId = UtilSql.getValue(result, "ad_role_id");
+        objectTransactionsWithMissingDataData.InitRecordNumber = Integer.toString(firstRegister);
+        vector.addElement(objectTransactionsWithMissingDataData);
         if (countRecord >= numberRegisters && numberRegisters != 0) {
           continueResult = false;
         }
@@ -427,9 +428,9 @@ This query returns payments that are wrongly linked to more than one transaction
         ignore.printStackTrace();
       }
     }
-    ZeroPaymentamtDepositAmtTransactionsData objectZeroPaymentamtDepositAmtTransactionsData[] = new ZeroPaymentamtDepositAmtTransactionsData[vector.size()];
-    vector.copyInto(objectZeroPaymentamtDepositAmtTransactionsData);
-    return(objectZeroPaymentamtDepositAmtTransactionsData);
+    TransactionsWithMissingDataData objectTransactionsWithMissingDataData[] = new TransactionsWithMissingDataData[vector.size()];
+    vector.copyInto(objectTransactionsWithMissingDataData);
+    return(objectTransactionsWithMissingDataData);
   }
 
   public static int insertAlertRecipient(ConnectionProvider connectionProvider, String client, String org, String adAlertRuleId, String role)    throws ServletException {
