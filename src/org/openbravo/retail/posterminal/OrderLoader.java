5984afb3fd0cb167e6af6fbe6b77fabe35a69b67
Miguel de Juana <miguel.dejuana@openbravo.com>
2014-10-21 10:10:15 +0200

    Fixed issue 0027921: Layaways are not being invoiced since Q2 cash up oflline change
    When a layaway is created we assign a cashupid, but if we payfull or partially again this layaway in another cashup, this transactions will not be reconciliated and invoice will not be created. We need to update the layaway with the new cashupid.
    
    --HG--
    extra : transplant_source : %0B%E6%BE%C9%C7%C8L%40%02%A3J%82%E3%11%94%23%E3%B23%C3

--- src/org/openbravo/retail/posterminal/OrderLoader.java
+++ src/org/openbravo/retail/posterminal/OrderLoader.java
@@ -175,6 +175,7 @@ public class OrderLoader extends POSDataSynchronizationProcess {
       JSONArray orderlines = jsonorder.getJSONArray("lines");
       if (fullpayLayaway) {
         order = OBDal.getInstance().get(Order.class, jsonorder.getString("id"));
+        order.setObposAppCashup(jsonorder.getString("obposAppCashup"));
         order.setDelivered(true);
         for (int i = 0; i < order.getOrderLineList().size(); i++) {
           lineReferences.add(order.getOrderLineList().get(i));
@@ -183,6 +184,7 @@ public class OrderLoader extends POSDataSynchronizationProcess {
         }
       } else if (partialpayLayaway) {
         order = OBDal.getInstance().get(Order.class, jsonorder.getString("id"));
+        order.setObposAppCashup(jsonorder.getString("obposAppCashup"));
       } else {
         order = OBProvider.getInstance().get(Order.class);
         createOrder(order, jsonorder);
