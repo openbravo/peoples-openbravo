e5d6d36d50a219f089b2a49516abbdb45558fbc2
Miguel de Juana <miguel.dejuana@openbravo.com>
2014-10-23 17:27:20 +0200

    Fixed issue 0027956: Duplicate receipt id gets generated, when sales order is created from Quotation. Add it properly

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -2257,6 +2257,13 @@
       this.loadCurrent(true);
     },
 
+    addThisOrder: function (model) {
+      this.saveCurrent();
+      this.current = model;
+      this.add(this.current);
+      this.loadCurrent();
+    },
+
     addFirstOrder: function () {
       this.addNewOrder();
     },
