e23b5d1e91e90021076ea57d07d29e09537ac518
Unai Martirena <unai.martirena@openbravo.com>
2014-11-04 10:20:30 +0100

    Fixes issue 28019: Recalculate the cost at the end of averageCostAdjustment
    The extra lines in Costing tab in this case are correct, because there is an out transaction (movement from), and these transactions they dont modify the average cost.
    Also, at the end of the Average Cost Adjustment the last cost has to be recalculated in order to prevent more out transactions with bad cost until another in transaction happens that recalculates the cost.
    
    --HG--
    extra : rebase_source : 155bf5ce15550acf5ea47c8fd3239e9e81684154

--- src/org/openbravo/costing/AverageCostAdjustment.java
+++ src/org/openbravo/costing/AverageCostAdjustment.java
@@ -321,6 +321,10 @@ public class AverageCostAdjustment extends CostingAlgorithmAdjustmentImp {
       // This is the current costing rule. Check if current average cost needs to be updated.
       Costing currentCosting = AverageAlgorithm.getProductCost(new Date(), basetrx.getProduct(),
           getCostDimensions(), getCostOrg());
+      if (currentStock.signum() != 0) {
+        cost = currentValueAmt.add(adjustmentBalance).divide(currentStock, costCurPrecission,
+            RoundingMode.HALF_UP);
+      }
       if (currentCosting.getCost().compareTo(cost) != 0) {
         basetrx = getTransaction();
         Date newDate = new Date();
