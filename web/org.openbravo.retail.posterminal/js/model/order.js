769be283ac0b9f427b391b811e4c323efb4e7356
Rafa Alonso <rafael.alonso@openbravo.com>
2014-11-11 19:12:07 +0100

    Fixes issue 27911 (Q2+): Backward compatibility for the new document sequence algorithm - deprecation code has been tagged as deprecated - removed rogue code - 'quotationDocNoPrefix' is initialized where its of use - some variables have been renamed to be aligned
    --HG--
    extra : transplant_source : E%FF%FB%8D%25%A9T/%29%B9%21%D9%85%3D%FF%2B%C2%C0%0D%90

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -267,8 +267,8 @@
         this.set('posTerminal', attributes.posTerminal);
         this.set('posTerminal' + OB.Constants.FIELDSEPARATOR + OB.Constants.IDENTIFIER, attributes['posTerminal' + OB.Constants.FIELDSEPARATOR + OB.Constants.IDENTIFIER]);
         this.set('orderDate', new Date(attributes.orderDate));
-        this.set('quotationDocNoPrefix', attributes.quotationDocNoPrefix);
-        this.set('docNoPrefix', attributes.docNoPrefix);
+        this.set('documentnoPrefix', attributes.documentnoPrefix);
+        this.set('quotationnoPrefix', attributes.quotationnoPrefix);
         this.set('documentnoSuffix', attributes.documentnoSuffix);
         this.set('quotationnoSuffix', attributes.quotationnoSuffix);
         this.set('documentNo', attributes.documentNo);
@@ -577,8 +577,8 @@
       this.set('posTerminal', null);
       this.set('posTerminal' + OB.Constants.FIELDSEPARATOR + OB.Constants.IDENTIFIER, null);
       this.set('orderDate', new Date());
-      this.set('quotationDocNoPrefix', -1);
-      this.set('docNoPrefix', -1);
+      this.set('documentnoPrefix', -1);
+      this.set('quotationnoPrefix', -1);
       this.set('documentnoSuffix', -1);
       this.set('quotationnoSuffix', -1);
       this.set('documentNo', '');
@@ -1398,9 +1398,8 @@
       this.set('isPaid', false);
       this.set('isEditable', true);
       this.set('orderDate', new Date());
-      this.set('quotationDocNoPrefix', OB.MobileApp.model.get('terminal').quotationDocNoPrefix);
-      this.set('docNoPrefix', OB.MobileApp.model.get('terminal').docNoPrefix);
       var nextDocumentno = OB.MobileApp.model.getNextDocumentno();
+      this.set('documentnoPrefix', OB.MobileApp.model.get('terminal').docNoPrefix);
       this.set('documentnoSuffix', nextDocumentno.documentnoSuffix);
       this.set('documentNo', nextDocumentno.documentNo);
       this.set('posTerminal', OB.MobileApp.model.get('terminal').id);
@@ -2095,9 +2094,8 @@
       order.set('isLayaway', false);
       order.set('taxes', null);
 
-      order.set('quotationDocNoPrefix', OB.MobileApp.model.get('terminal').quotationDocNoPrefix);
-      order.set('docNoPrefix', OB.MobileApp.model.get('terminal').docNoPrefix);
       var nextDocumentno = OB.MobileApp.model.getNextDocumentno();
+      order.set('documentnoPrefix', OB.MobileApp.model.get('terminal').docNoPrefix);
       order.set('documentnoSuffix', nextDocumentno.documentnoSuffix);
       order.set('documentNo', nextDocumentno.documentNo);
 
@@ -2302,11 +2300,11 @@
       var documentseq, documentseqstr;
       this.saveCurrent();
       this.current = this.newOrder();
-      OB.MobileApp.model.set('documentsequence', OB.MobileApp.model.get('documentsequence') - 1);
       this.current.set('isQuotation', true);
       this.current.set('generateInvoice', false);
       this.current.set('documentType', OB.MobileApp.model.get('terminal').terminalType.documentTypeForQuotations);
       var nextQuotationno = OB.MobileApp.model.getNextQuotationno();
+      this.current.set('quotationDocNoPrefix', OB.MobileApp.model.get('terminal').quotationDocNoPrefix);
       this.current.set('quotationnoSuffix', nextQuotationno.quotationnoSuffix);
       this.current.set('documentNo', nextQuotationno.documentNo);
 
