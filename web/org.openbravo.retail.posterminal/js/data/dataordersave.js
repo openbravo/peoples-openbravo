d1330eec7acd7b248bd23b3a2f02d641a64d5fca
Miguel de Juana <miguel.dejuana@openbravo.com>
2014-11-11 17:56:50 +0100

    Fixed issue 0028140: Print last module does not work. Add a trigger before deleting the order
    --HG--
    extra : transplant_source : %9D%8DV%F5%D4%0A3o%A9%B6%17o%18v%A5%BA%B3%0F%18%A2

--- web/org.openbravo.retail.posterminal/js/data/dataordersave.js
+++ web/org.openbravo.retail.posterminal/js/data/dataordersave.js
@@ -91,7 +91,7 @@
         }
       }
       OB.trace('Receipt integrity: OK');
-
+      this.receipt.trigger('integrityOk');
       OB.MobileApp.model.updateDocumentSequenceWhenOrderSaved(this.receipt.get('documentnoSuffix'), this.receipt.get('quotationnoSuffix'));
 
       delete this.receipt.attributes.json;
@@ -213,7 +213,7 @@
           creationDateTransformed = new Date(creationDate.getUTCFullYear(), creationDate.getUTCMonth(), creationDate.getUTCDate(), creationDate.getUTCHours(), creationDate.getUTCMinutes(), creationDate.getUTCSeconds());
 
       this.receipt.set('hasbeenpaid', 'Y');
-
+      this.receipt.trigger('integrityOk');
       OB.MobileApp.model.updateDocumentSequenceWhenOrderSaved(this.receipt.get('documentnoSuffix'), this.receipt.get('quotationnoSuffix'));
 
       delete this.receipt.attributes.json;
