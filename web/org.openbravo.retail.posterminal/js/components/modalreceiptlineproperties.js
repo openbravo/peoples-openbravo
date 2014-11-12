7a3110ccf8758cbfcd45ad06f5ac407e0b39123f
Miguel A. Alsasua <miguel.alsasua@openbravo.com>
2014-11-12 11:30:32 +0100

    fixed issue 27927: Line Properties in a POS ticket don't reset if the properties pop-up is closed by cancelling or clicking outside

--- web/org.openbravo.retail.posterminal/js/components/modalreceiptlineproperties.js
+++ web/org.openbravo.retail.posterminal/js/components/modalreceiptlineproperties.js
@@ -16,6 +16,15 @@ enyo.kind({
     onApplyChanges: 'applyChanges'
   },
   executeOnShow: function () {
+    if (this.currentLine) {
+      var diff = this.propertycomponents;
+      var att;
+      for (att in diff) {
+        if (diff.hasOwnProperty(att)) {
+          this.loadValue(att, diff[att]);
+        }
+      }
+    }
     this.autoDismiss = true;
     if (this && this.args && this.args.autoDismiss === false) {
       this.autoDismiss = false;
