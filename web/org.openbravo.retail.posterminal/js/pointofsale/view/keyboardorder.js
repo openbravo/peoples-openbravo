a856415ab774475b822c6d377cb43403491daf02
Aaron Calero <aaron.calero@openbravo.com>
2014-11-17 11:55:47 +0100

    Fixed issue 0028173: When you read a barcode it should find by upc doing a case-insensitive search
    --HG--
    extra : rebase_source : a4f414aee8a93d9d0985eec86304243c93a6c130

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/keyboardorder.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/keyboardorder.js
@@ -466,7 +466,7 @@ enyo.kind({
   name: 'OB.UI.BarcodeActionHandler',
   kind: 'OB.UI.AbstractBarcodeActionHandler',
   addWhereFilter: function (txt) {
-    return "where product.upc = '" + txt + "'";
+    return "where product.upc like '" + txt + "'";
   },
   findProductByBarcode: function (txt, callback) {
     var criteria;
