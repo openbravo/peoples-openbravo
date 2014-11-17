d0e124198370500496c8d5596b8477c08cc00ed5
Mario Castello <mario.castello@peoplewalking.com>
2014-11-17 11:37:51 -0600

    fixed issue 27967: It is possible to return different BPs ticket in the same ticket

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
@@ -307,6 +307,12 @@ enyo.kind({
     }
   },
   paidReceipts: function (inSender, inEvent) {
+    var receipt = this.model.get('order');
+    if(inEvent.isReturn){
+      if(receipt.get('bp').get('id') !== OB.MobileApp.model.get('businessPartner').get('id')){
+        inEvent.bpartner = receipt.get('bp');
+      }
+    }
     this.$.modalPaidReceipts.setParams(inEvent);
     this.$.modalPaidReceipts.waterfall('onClearAction');
     this.doShowPopup({
