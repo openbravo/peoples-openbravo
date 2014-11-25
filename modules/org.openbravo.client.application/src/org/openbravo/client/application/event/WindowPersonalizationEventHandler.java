9098cc0ab391f4d132bb5531f9b61f1a329e71d4
Augusto Mauch <augusto.mauch@openbravo.com>
2014-11-25 11:51:55 +0100

    Related with issue 28242: Delete preference along with window personalization
    When a grid view is saved and set as default two records are added:
    - One to the OBUIAPP_UIPersonalization with the description of the view
    - One to the Preference table, using the OBUIAPP_DefaultSavedView property, whose value is the ID of the record added to the OBUIAPP_UIPersonalization table
    
    The problem of this issue was that when the a default saved view was removed from the OBUIAPP_UIPersonalization table, its corresponding entry from the Preference table was not removed. This resulted in not making the initial request to load the grid because it was detected that the view had a default saved view (using the preference), and not doing it after loading the window personalization because the view description was removed from OBUIAPP_UIPersonalization.
    
    The first step of the issue, included in this changeset, removes the Preference associated with a default saved view when it is deleted from OBUIAPP_UIPersonalization. This prevents the data from being inconsistent. The second and final step will consist in loading the grid even when the data is inconsistent, to fix the cases where the data is already inconsistent.
    
    --HG--
    extra : transplant_source : %F2%86%87%EC%B0%81%E2o%FBYi%89%B5%C9%B6%C9%89%B4%2B%9E

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/event/WindowPersonalizationEventHandler.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/event/WindowPersonalizationEventHandler.java
@@ -19,18 +19,25 @@
 
 package org.openbravo.client.application.event;
 
+import java.util.List;
+
 import javax.enterprise.event.Observes;
 
 import org.apache.log4j.Logger;
+import org.hibernate.criterion.Restrictions;
 import org.openbravo.base.exception.OBException;
 import org.openbravo.base.model.Entity;
 import org.openbravo.base.model.ModelProvider;
 import org.openbravo.client.application.UIPersonalization;
+import org.openbravo.client.kernel.event.EntityDeleteEvent;
 import org.openbravo.client.kernel.event.EntityPersistenceEventObserver;
 import org.openbravo.client.kernel.event.EntityUpdateEvent;
 import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBCriteria;
+import org.openbravo.dal.service.OBDal;
 import org.openbravo.database.ConnectionProvider;
 import org.openbravo.erpCommon.utility.Utility;
+import org.openbravo.model.ad.domain.Preference;
 import org.openbravo.model.ad.ui.Tab;
 import org.openbravo.model.ad.ui.Window;
 import org.openbravo.service.db.DalConnectionProvider;
@@ -46,8 +53,7 @@ public class WindowPersonalizationEventHandler extends EntityPersistenceEventObs
     return entities;
   }
 
-  public void onUpdate(@Observes
-  EntityUpdateEvent event) {
+  public void onUpdate(@Observes EntityUpdateEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
@@ -65,5 +71,50 @@ public class WindowPersonalizationEventHandler extends EntityPersistenceEventObs
       ConnectionProvider conn = new DalConnectionProvider(false);
       throw new OBException(Utility.messageBD(conn, "OBUIAPP_TabFieldMandatory", language));
     }
+  }
+
+  public void onDelete(@Observes EntityDeleteEvent event) {
+    if (!isValidEvent(event)) {
+      return;
+    }
+    final UIPersonalization uiPersonalization = (UIPersonalization) event.getTargetInstance();
+    deleteDefaultViewPreferences(uiPersonalization);
+  }
+
+  /**
+   * Given an UIPersonalization, deletes the OBUIAPP_DefaultSavedView preferences that reference it
+   */
+  private void deleteDefaultViewPreferences(UIPersonalization uiPersonalization) {
+    try {
+      List<Preference> preferenceList = this
+          .getDefaultViewPreferencesForUiPersonalization(uiPersonalization);
+      // don't do a client access check, the preference to delete might belong to another client
+      // (i.e. System)
+      OBContext.setAdminMode(false);
+      for (Preference preference : preferenceList) {
+        OBDal.getInstance().remove(preference);
+      }
+    } catch (Exception e) {
+      logger.error("Error while deleting a default view preference", e);
+    } finally {
+      OBContext.restorePreviousMode();
+    }
+  }
+
+  /**
+   * Given an UIPersonalization, returns the list of OBUIAPP_DefaultSavedView preferences that
+   * reference it
+   */
+  private List<Preference> getDefaultViewPreferencesForUiPersonalization(
+      UIPersonalization uiPersonalization) {
+    OBCriteria<Preference> preferenceCriteria = OBDal.getInstance()
+        .createCriteria(Preference.class);
+    // filter out the preferences that do not store the default view
+    preferenceCriteria.add(Restrictions
+        .eq(Preference.PROPERTY_PROPERTY, "OBUIAPP_DefaultSavedView"));
+    // filter out the preferences whose default view is not the one being deleted
+    preferenceCriteria
+        .add(Restrictions.eq(Preference.PROPERTY_SEARCHKEY, uiPersonalization.getId()));
+    return preferenceCriteria.list();
   }
 }
