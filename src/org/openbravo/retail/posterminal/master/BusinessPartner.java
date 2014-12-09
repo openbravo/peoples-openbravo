9555e81aec073a431f3798436e00f70c313d27af
Martin Taal <martin.taal@openbravo.com>
2014-12-09 23:36:56 +0100

    Related to issue 28370: Various NPEs are thrown if wrong terminal key is used or user is in wrong organization Prevent NPE in some cases

--- src/org/openbravo/retail/posterminal/master/BusinessPartner.java
+++ src/org/openbravo/retail/posterminal/master/BusinessPartner.java
@@ -33,9 +33,8 @@ public class BusinessPartner extends ProcessHQLQuery {
 
   @Override
   protected List<String> getQuery(JSONObject jsonsent) throws JSONException {
-    Long lastUpdated = jsonsent.has("lastUpdated") && jsonsent.get("lastUpdated") != null
-        && !jsonsent.get("lastUpdated").equals("undefined") ? jsonsent.getLong("lastUpdated")
-        : null;
+    Long lastUpdated = (jsonsent.has("lastUpdated") && jsonsent.get("lastUpdated") != null && !jsonsent
+        .get("lastUpdated").equals("undefined")) ? jsonsent.getLong("lastUpdated") : null;
     // if it is a total refresh we need to ensure that all(AND) entities are active. In a
     // incremental refresh, we need to retrieve it if some (OR) ot the entities have changed
     String operator = lastUpdated == null ? " AND " : " OR ";
