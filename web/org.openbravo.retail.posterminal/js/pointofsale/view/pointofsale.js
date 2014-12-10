43e700bb497ee11699a5c5305861750307fe6baa
Miguel A. Alsasua <miguel.alsasua@openbravo.com>
2014-12-10 12:29:52 +0100

    fixed issue 28317: quotations will not be deleted from c_order table when hasbeenpaid is Y
    --HG--
    extra : transplant_source : %25%8E%9De%D0%1CM.%ED%8C%B9E%D6%D8%F2%22%B7c%E3%B6

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
@@ -333,7 +333,7 @@ enyo.kind({
   },
   deleteCurrentOrder: function (inSender, inEvent) {
     function removeOrder(context) {
-      if (context.model.get('order').get('id')) {
+      if (context.model.get('order').get('id') && !(context.model.get('order').has('isQuotation') && context.model.get('order').get('isQuotation') && context.model.get('order').has('hasbeenpaid') && context.model.get('order').get('hasbeenpaid') === 'Y')) {
         context.model.get('orderList').saveCurrent();
         OB.Dal.remove(context.model.get('orderList').current, null, null);
       }
