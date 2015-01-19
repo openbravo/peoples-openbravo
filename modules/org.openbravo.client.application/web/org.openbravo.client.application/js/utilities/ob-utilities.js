926291e7d3306a2bfb57e7a09a33793e6e74e7e2
Naroa Iriarte <naroa.iriarte@openbravo.com>
2015-01-19 14:42:57 +0100

    Fixed issue 28623: Window's behaviour changes when creating a new record.
    When a window is opened using the "quick create new" widget, the same window in the recent views layout
    does not allow to create a new record (the green small button for creating a new record, which should be
    by the window name, disappears).
    
    The problem was in the "ob-utilities.js", inside the "{OB.Utilities.openView}" function, the logic for
    setting the "singleRecord" value (this is the Edit Only attribute) was wrong. If the value of the
    variable singleRecord of that function was undefined or null, as it was in the case of clicking the "quick create new"
    widget, that value was set to true and the window became an "edit only" window.That was why the small
    green "create new" icon disappeared from the recent views layout.
    
    For fixing this the condition has been removed, so we know that the value of the variable "singleRecord" is not
    going to be modified and it will be set correctly.
    
    --HG--
    extra : rebase_source : fb3f4635fedf045f6f8323fc5ab2e6a2f022d814

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/utilities/ob-utilities.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/utilities/ob-utilities.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2011-2014 Openbravo SLU
+ * All portions are Copyright (C) 2011-2015 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -424,18 +424,13 @@ OB.Utilities.removeFragment = function (str) {
 // Returns the object used to open the window.
 OB.Utilities.openView = function (windowId, tabId, tabTitle, recordId, command, icon, readOnly, singleRecord, direct, editOrDeleteOnly) {
   var isClassicEnvironment = OB.Utilities.useClassicMode(windowId),
-      openObject, isDirect = direct,
-      isSingleRecord;
+      openObject, isDirect = direct;
   if (recordId) {
     if (direct !== false) {
       isDirect = true;
     }
 
   }
-  if (singleRecord !== false) {
-    isSingleRecord = true;
-  }
-
   if (isClassicEnvironment) {
     if (recordId) {
       OB.Layout.ClassicOBCompatibility.openLinkedItem(tabId, recordId);
@@ -459,7 +454,7 @@ OB.Utilities.openView = function (windowId, tabId, tabTitle, recordId, command, 
       tabTitle: tabTitle,
       windowId: windowId,
       readOnly: readOnly,
-      singleRecord: isSingleRecord,
+      singleRecord: singleRecord,
       editOrDeleteOnly: editOrDeleteOnly
     };
   } else {
@@ -471,7 +466,7 @@ OB.Utilities.openView = function (windowId, tabId, tabTitle, recordId, command, 
       windowId: windowId,
       icon: icon,
       readOnly: readOnly,
-      singleRecord: isSingleRecord,
+      singleRecord: singleRecord,
       editOrDeleteOnly: editOrDeleteOnly
     };
   }
