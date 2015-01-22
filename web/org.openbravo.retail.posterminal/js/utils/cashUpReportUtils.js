4d668c9bee8ad024cf3518a8fc239e086607bfc2
Rafa Alonso <rafael.alonso@openbravo.com>
2015-01-22 17:09:16 +0100

    Fixes issue 28714: The amount to be returned is substracted instead of added
    The problem was that the fix for the issue 28491 inverted the sign of the return variable
    
    --HG--
    extra : transplant_source : %BC%C4%85kn%99%02%DC%AB%E39j%ADD%95%80%A3%2B%87%8A

--- web/org.openbravo.retail.posterminal/js/utils/cashUpReportUtils.js
+++ web/org.openbravo.retail.posterminal/js/utils/cashUpReportUtils.js
@@ -499,7 +499,7 @@
             totalDrops = 0;
           }
           var cashMgmt = OB.DEC.sub(totalDeps, totalDrops);
-          cash = OB.DEC.add(OB.DEC.add(startingCash, OB.DEC.add(totalSales, totalReturns)), cashMgmt);
+          cash = OB.DEC.add(OB.DEC.add(startingCash, OB.DEC.sub(totalSales, totalReturns)), cashMgmt);
           payment.currentCash = OB.UTIL.currency.toDefaultCurrency(payment.paymentMethod.currency, cash);
           payment.foreignCash = OB.UTIL.currency.toForeignCurrency(payment.paymentMethod.currency, cash);
 
