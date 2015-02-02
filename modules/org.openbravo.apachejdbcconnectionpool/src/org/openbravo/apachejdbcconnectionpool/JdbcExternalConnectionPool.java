a6ce822a1b8477a3076e09ae17495ec3a659a427
Asier Lostalé <asier.lostale@openbravo.com>
2015-02-02 11:24:04 +0100

    [pool] pool properties are read from standard Openbravo.properties

--- modules/org.openbravo.apachejdbcconnectionpool/src/org/openbravo/apachejdbcconnectionpool/JdbcExternalConnectionPool.java
+++ modules/org.openbravo.apachejdbcconnectionpool/src/org/openbravo/apachejdbcconnectionpool/JdbcExternalConnectionPool.java
@@ -1,6 +1,6 @@
 /*
  ***********************************************************************************
- * Copyright (C) 2014 Openbravo S.L.U.
+ * Copyright (C) 2014-2015 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -8,19 +8,12 @@
  */
 package org.openbravo.apachejdbcconnectionpool;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.URL;
 import java.sql.Connection;
 import java.util.List;
 import java.util.Properties;
 
-import javax.servlet.ServletContext;
-
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.apache.tomcat.jdbc.pool.PoolProperties;
-import org.openbravo.base.provider.OBConfigFileProvider;
 import org.openbravo.base.session.OBPropertiesProvider;
 import org.openbravo.database.ExternalConnectionPool;
 import org.openbravo.database.PoolInterceptorProvider;
@@ -30,9 +23,6 @@ import org.slf4j.LoggerFactory;
 public class JdbcExternalConnectionPool extends ExternalConnectionPool {
 
   final static private Logger log = LoggerFactory.getLogger(JdbcExternalConnectionPool.class);
-
-  private final static String CONFIG_FILE_NAME = "connectionPool.properties";
-  private final static String PATH_FROM_OB_ROOT = "modules/org.openbravo.apachejdbcconnectionpool/config/";
 
   private DataSource dataSource = null;
 
@@ -93,40 +83,34 @@ public class JdbcExternalConnectionPool extends ExternalConnectionPool {
     poolProperties.setUsername(username);
     poolProperties.setPassword(password);
 
-    File propertiesURL = getPropertiesFile();
     Properties poolPropertiesConfig = new Properties();
-    try {
-      FileInputStream fis = new FileInputStream(propertiesURL);
-      poolPropertiesConfig.load(fis);
-      poolProperties.setInitialSize(getIntProperty(poolPropertiesConfig, "initialSize", "5"));
-      poolProperties.setMaxActive(getIntProperty(poolPropertiesConfig, "maxActive", "30"));
-      poolProperties.setMaxIdle(getIntProperty(poolPropertiesConfig, "maxIdle", "30"));
-      poolProperties.setMinIdle(getIntProperty(poolPropertiesConfig, "minIdle", "5"));
-      poolProperties.setMaxWait(getIntProperty(poolPropertiesConfig, "maxWait", "30"));
-      poolProperties.setTimeBetweenEvictionRunsMillis(getIntProperty(poolPropertiesConfig,
-          "timeBetweenEvictionRunsMillis", "30000"));
-      poolProperties.setMinEvictableIdleTimeMillis(getIntProperty(poolPropertiesConfig,
-          "minEvictableIdleTimeMillis", "30000"));
-      poolProperties.setRemoveAbandoned(getBooleanProperty(poolPropertiesConfig, "removeAbandoned",
-          "true"));
-      poolProperties.setRemoveAbandonedTimeout(getIntProperty(poolPropertiesConfig,
-          "removeAbandonedTimeout", "60"));
-      poolProperties.setLogAbandoned(getBooleanProperty(poolPropertiesConfig, "logAbandoned",
-          "false"));
-      poolProperties.setJmxEnabled(getBooleanProperty(poolPropertiesConfig, "jmxEnabled", "true"));
-      poolProperties.setTestWhileIdle(getBooleanProperty(poolPropertiesConfig, "testWhileIdle",
-          "false"));
-      poolProperties.setTestOnBorrow(getBooleanProperty(poolPropertiesConfig, "testOnBorrow",
-          "true"));
-      poolProperties.setTestOnReturn(getBooleanProperty(poolPropertiesConfig, "testOnReturn",
-          "false"));
-      poolProperties.setValidationInterval(getIntProperty(poolPropertiesConfig,
-          "validationInterval", "30000"));
-      poolProperties.setValidationQuery(poolPropertiesConfig.getProperty("validationQuery",
-          "SELECT 1 FROM DUAL"));
-    } catch (IOException e) {
-      log.error("Error while loading connection pool properties", e);
-    }
+    poolPropertiesConfig = OBPropertiesProvider.getInstance().getOpenbravoProperties();
+    poolProperties.setInitialSize(getIntProperty(poolPropertiesConfig, "initialSize", "5"));
+    poolProperties.setMaxActive(getIntProperty(poolPropertiesConfig, "maxActive", "30"));
+    poolProperties.setMaxIdle(getIntProperty(poolPropertiesConfig, "maxIdle", "30"));
+    poolProperties.setMinIdle(getIntProperty(poolPropertiesConfig, "minIdle", "5"));
+    poolProperties.setMaxWait(getIntProperty(poolPropertiesConfig, "maxWait", "30"));
+    poolProperties.setTimeBetweenEvictionRunsMillis(getIntProperty(poolPropertiesConfig,
+        "timeBetweenEvictionRunsMillis", "30000"));
+    poolProperties.setMinEvictableIdleTimeMillis(getIntProperty(poolPropertiesConfig,
+        "minEvictableIdleTimeMillis", "30000"));
+    poolProperties.setRemoveAbandoned(getBooleanProperty(poolPropertiesConfig, "removeAbandoned",
+        "true"));
+    poolProperties.setRemoveAbandonedTimeout(getIntProperty(poolPropertiesConfig,
+        "removeAbandonedTimeout", "60"));
+    poolProperties
+        .setLogAbandoned(getBooleanProperty(poolPropertiesConfig, "logAbandoned", "false"));
+    poolProperties.setJmxEnabled(getBooleanProperty(poolPropertiesConfig, "jmxEnabled", "true"));
+    poolProperties.setTestWhileIdle(getBooleanProperty(poolPropertiesConfig, "testWhileIdle",
+        "false"));
+    poolProperties
+        .setTestOnBorrow(getBooleanProperty(poolPropertiesConfig, "testOnBorrow", "true"));
+    poolProperties
+        .setTestOnReturn(getBooleanProperty(poolPropertiesConfig, "testOnReturn", "false"));
+    poolProperties.setValidationInterval(getIntProperty(poolPropertiesConfig, "validationInterval",
+        "30000"));
+    poolProperties.setValidationQuery(poolPropertiesConfig.getProperty("validationQuery",
+        "SELECT 1 FROM DUAL"));
     poolProperties.setJdbcInterceptors("org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"
         + "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer;"
         + "org.openbravo.apachejdbcconnectionpool.ConnectionInitializerInterceptor;");
@@ -139,34 +123,5 @@ public class JdbcExternalConnectionPool extends ExternalConnectionPool {
 
   private int getIntProperty(Properties properties, String propertyName, String defaultValue) {
     return Integer.parseInt(properties.getProperty(propertyName, defaultValue));
-  }
-
-  private File getPropertiesFile() {
-    File propertiesFile = null;
-
-    // get config from Servlet context
-    ServletContext ctx = OBConfigFileProvider.getInstance().getServletContext();
-    if (ctx != null) {
-      propertiesFile = new File(ctx.getRealPath("/WEB-INF/" + CONFIG_FILE_NAME));
-      if (propertiesFile.exists()) {
-        return propertiesFile;
-      }
-    }
-
-    // get the location of the current class file
-    final URL url = this.getClass().getResource(getClass().getSimpleName() + ".class");
-    File f = new File(url.getPath());
-    while (f.getParentFile() != null && f.getParentFile().exists()) {
-      f = f.getParentFile();
-      final File configDirectory = new File(f, "config");
-      if (configDirectory.exists()) {
-        propertiesFile = new File(f, PATH_FROM_OB_ROOT + CONFIG_FILE_NAME);
-        if (propertiesFile.exists()) {
-          // found it and break
-          break;
-        }
-      }
-    }
-    return propertiesFile;
   }
 }
