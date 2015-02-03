a7b4dff1ac8a8695e455a8c190ccd40ffeae649e
Atul Gaware <atul.gaware@openbravo.com>
2015-02-03 15:57:48 +0530

    Fixes Issue 28841:NullPointerException error when posting a work effort
    Work effort lines with products having setting stocked=No does not
    have records in m_ transaction. They should be skipped.
    
    --HG--
    extra : rebase_source : 9831994280abfdbe71f4cfe44112a723ad10bc70

--- src/org/openbravo/erpCommon/ad_forms/DocProduction.java
+++ src/org/openbravo/erpCommon/ad_forms/DocProduction.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2008-2014 Openbravo SLU
+ * All portions are Copyright (C) 2008-2015 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -169,7 +169,7 @@ public class DocProduction extends AcctServer {
     int countProductionlinesWithTrnCostZero = 0;
     for (int i = 0; i < p_lines.length; i++) {
       DocLine_Material line = (DocLine_Material) p_lines[i];
-      if (line.transaction.getTransactionCost() != null
+      if (line.transaction != null && line.transaction.getTransactionCost() != null
           && line.transaction.getTransactionCost().compareTo(ZERO) == 0) {
         countProductionlinesWithTrnCostZero++;
       }
