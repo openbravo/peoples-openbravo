d9bbb3adba66eac77c886619a70ac496ad91681e
Unai Martirena <unai.martirena@openbravo.com>
2015-02-12 13:35:34 +0100

    Related to bug 28896: Missing entry on group by clause
    --HG--
    extra : transplant_source : w%ED9%8F%85%9C%28Qk%00%2A%26%01V%85%A5%0Cn%A7%C8

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/hqlinjections/AddPaymentOrderInvoicesTransformer.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/hqlinjections/AddPaymentOrderInvoicesTransformer.java
@@ -270,6 +270,7 @@ public class AddPaymentOrderInvoicesTransformer extends HqlQueryTransformer {
       groupByClause.append(" COALESCE(ipriority.priority, opriority.priority), ");
       groupByClause.append(" inv.salesTransaction, ");
       groupByClause.append(" oinfo.organization, ");
+      groupByClause.append(" oinfo.aPRMPaymentDescription, ");
       groupByClause.append(" inv.orderReference, ");
     } else if ("O".equals(transactionType)) {
       groupByClause.append(" ord.documentNo, ");
@@ -286,6 +287,7 @@ public class AddPaymentOrderInvoicesTransformer extends HqlQueryTransformer {
       groupByClause.append(" COALESCE(ipriority.priority, opriority.priority), ");
       groupByClause.append(" inv.salesTransaction, ");
       groupByClause.append(" oinfo.organization, ");
+      groupByClause.append(" oinfo.aPRMPaymentDescription, ");
       groupByClause.append(" inv.orderReference, ");
     }
     groupByClause.append(" COALESCE(invbp.id, ordbp.id), ");
