195af5e155fa7315c8b598e0df0f49dee7acf788
Miguel de Juana <miguel.dejuana@openbravo.com>
2015-02-12 12:42:21 +0100

    Fixed issue 0028874: It is not possible to modularize filter clause in JAVA PaidReceiptsHeader
    Add a hook in order to be able to introduce columns to filter by text introduced by user.
    
    --HG--
    extra : rebase_source : 2b52f33a6960b70da2a9c8a16fc55c182e5b5f79

---
+++ src/org/openbravo/retail/posterminal/PaidReceiptsHeaderHook.java
@@ -0,0 +1,14 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2013-2015 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+package org.openbravo.retail.posterminal;
+
+public interface PaidReceiptsHeaderHook {
+  public String exec(String hqlFilter, String filterText) throws Exception;
+}
