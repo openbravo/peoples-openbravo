fbd7101d6e4b9d041c3cb32fc7149844af3ca5f3
Unai Martirena <unai.martirena@openbravo.com>
2015-02-12 18:43:18 +0100

    Related to bug 28854: Fix on whereClause, add missing or clause
    --HG--
    extra : rebase_source : 9a94db0d53f755ae5378074aeecfa54caa9ef96a

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/hqlinjections/AddPaymentOrderInvoicesTransformer.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/hqlinjections/AddPaymentOrderInvoicesTransformer.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2014 Openbravo SLU
+ * All portions are Copyright (C) 2014 - 2015 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  *************************************************************************
@@ -242,7 +242,7 @@ public class AddPaymentOrderInvoicesTransformer extends HqlQueryTransformer {
           isFirst = true;
         }
       }
-      whereClause.append(")");
+      whereClause.append(") or ");
     }
     if ("I".equals(transactionType)) {
 
