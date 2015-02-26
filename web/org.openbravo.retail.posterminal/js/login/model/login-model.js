a6cc32ee4974b32278c5ac580c841dd7fa5b23f9
Miguel de Juana <miguel.dejuana@openbravo.com>
2015-02-26 11:29:41 +0100

    Fixed issue 0029072: Pending Synchronization, update copyright date

--- web/org.openbravo.retail.posterminal/js/login/model/login-model.js
+++ web/org.openbravo.retail.posterminal/js/login/model/login-model.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2013 Openbravo S.L.U.
+ * Copyright (C) 2012-2015 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -386,7 +386,7 @@
         timeout: 600000,
         timePerRecord: 10000,
         criteria: {},
-        changesPendingCriteria:  {
+        changesPendingCriteria: {
           'isprocessed': 'Y'
         },
         postProcessingFunction: function (data, callback) {
