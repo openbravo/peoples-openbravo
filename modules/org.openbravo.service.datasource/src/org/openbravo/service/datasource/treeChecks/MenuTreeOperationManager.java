1c6c032220202e1177cccfc6708457283d73ecfb
Augusto Mauch <augusto.mauch@openbravo.com>
2015-03-02 13:45:56 +0100

    Fixes bug 29097: A menu tree node can be moved with a template in development
    When the Openbravo Trees were reimplemented [1], there was a problem reimplementing the logic done when a Menu tree item is moved. Before the reimplementation, it was possible to move a menu tree item if the module it belonged to was in development or if there was any template module in development. The reimplementation did not take into account the templates, so from then on it has not been possible to moved menu tree nodes having a template in development.
    
    To fix this, templates are taken into accout in the MenuTreeOperationManager class.
    
    [1] https://issues.openbravo.com/view.php?id=25906
    
    --HG--
    extra : rebase_source : 9c427545e9f34b6a16eca18fa6538b66870a6f50

--- modules/org.openbravo.service.datasource/src/org/openbravo/service/datasource/treeChecks/MenuTreeOperationManager.java
+++ modules/org.openbravo.service.datasource/src/org/openbravo/service/datasource/treeChecks/MenuTreeOperationManager.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2014 Openbravo SLU
+ * All portions are Copyright (C) 2014-2015 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -23,7 +23,9 @@ import java.util.Map;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.hibernate.criterion.Restrictions;
 import org.openbravo.client.kernel.ComponentProvider.Qualifier;
+import org.openbravo.dal.service.OBCriteria;
 import org.openbravo.dal.service.OBDal;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.model.ad.module.Module;
@@ -33,6 +35,7 @@ import org.openbravo.service.datasource.CheckTreeOperationManager;
 @ApplicationScoped
 @Qualifier("ADMenu")
 public class MenuTreeOperationManager extends CheckTreeOperationManager {
+  private final String TEMPLATE_TYPE = "T";
 
   /**
    * Only allows to move a node if it belong to a module that is in development
@@ -42,11 +45,19 @@ public class MenuTreeOperationManager extends CheckTreeOperationManager {
       String newParentId, String prevNodeId, String nextNodeId) {
     Menu menuEntry = OBDal.getInstance().get(Menu.class, nodeId);
     Module module = menuEntry.getModule();
-    if (module != null && !module.isInDevelopment()) {
+
+    if (module != null && !module.isInDevelopment() && !areThereTemplatesInDevelopment()) {
       return new ActionResponse(false, "error",
           OBMessageUtils.messageBD("OBSERDS_ReparentNotInDevTreeNode"));
     } else {
       return new ActionResponse(true);
     }
+  }
+
+  private boolean areThereTemplatesInDevelopment() {
+    OBCriteria<Module> templatesInDevelopmentCriteria = OBDal.getInstance().createCriteria(Module.class);
+    templatesInDevelopmentCriteria.add(Restrictions.eq(Module.PROPERTY_TYPE, TEMPLATE_TYPE));
+    templatesInDevelopmentCriteria.add(Restrictions.eq(Module.PROPERTY_INDEVELOPMENT, true));
+    return (templatesInDevelopmentCriteria.count() > 0);
   }
 }
