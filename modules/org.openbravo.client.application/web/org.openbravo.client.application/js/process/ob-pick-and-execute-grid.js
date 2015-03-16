5f41f98adc84abb4f30653a241b0659b01fab26b
Augusto Mauch <augusto.mauch@openbravo.com>
2015-03-16 18:49:22 +0100

    Fixes bug 29294: valueMap and value of combo item is properly set in P&E window
    There were two things missing in the previous fix:
    - The attribute names of the valueMap were not proper
    - The value of the combo item should be set after defining its valueMap, otherwise the value is lost
    
    --HG--
    extra : rebase_source : 9e397f1b3eacfc2619743d8af4924f051c890c45

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/process/ob-pick-and-execute-grid.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/process/ob-pick-and-execute-grid.js
@@ -628,11 +628,16 @@ isc.OBPickAndExecuteGrid.addProperties({
     if (columnValue.entries) {
       this.setValueMap(field.name, columnValue.entries);
     } else if (field.fkField && columnValue.value && columnValue.identifier) {
-      valueMap[0] = {
-        id: columnValue.value,
-        identifier: columnValue.identifier
-      };
+      // build the valueMap manually, set it and set the value of the
+      // fk combo item in the edit form if possible
+      valueMap[0] = {};
+      valueMap[0][OB.Constants.ID] = columnValue.value;
+      valueMap[0][OB.Constants.IDENTIFIER] = columnValue.identifier;
       this.setValueMap(field.name, valueMap);
+      if (this.isEditing()) {
+        this.setEditValue(this.getEditRow(), field.name, columnValue.value);
+      }
+
     }
   },
 
