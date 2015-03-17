13998d097f9cf1d1234fb68dec104c2aa2cf5b7a
Mario Castello <mario.castello@peoplewalking.com>
2015-03-17 11:35:07 -0600

    Related to issue 29285: Add validation

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
@@ -323,7 +323,7 @@ enyo.kind({
   paidReceipts: function (inSender, inEvent) {
     var receipt = this.model.get('order');
     if (inEvent && inEvent.isReturn) {
-      if (receipt.get('bp').get('id') !== OB.MobileApp.model.get('businessPartner').get('id')) {
+      if (receipt && receipt.get('bp') && receipt.get('bp').get('id') !== OB.MobileApp.model.get('businessPartner').get('id')) {
         inEvent.bpartner = receipt.get('bp');
       } else if (receipt.get('lines').length > 0) {
         inEvent.bpartner = receipt.get('bp');
