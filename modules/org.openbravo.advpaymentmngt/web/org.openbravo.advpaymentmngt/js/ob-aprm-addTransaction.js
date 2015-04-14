83b5b3511b5f72447e01c52a93455687f053e8f7
Unai Martirena <unai.martirena@openbravo.com>
2015-04-14 18:56:15 +0200

    Fixes bug 29534: Accounting Date is updated when Transaction Date changes
    A callout has been added in Transaction Date field of transaction tab under Financial Account.
    
    An onchange function has been added in Transaction Date in Add Transaction Process Definition.

--- modules/org.openbravo.advpaymentmngt/web/org.openbravo.advpaymentmngt/js/ob-aprm-addTransaction.js
+++ modules/org.openbravo.advpaymentmngt/web/org.openbravo.advpaymentmngt/js/ob-aprm-addTransaction.js
@@ -114,3 +114,7 @@ OB.APRM.AddTransaction.glitemOnChangeFunction = function (item, view, form, grid
     strGLItemId: strGLItemId
   }, {}, callback);
 };
+
+OB.APRM.AddTransaction.trxDateOnChangeFunction = function (item, view, form, grid) {
+  form.getItem('dateacct').setDateParameterValue(new Date(item.getValue()));
+};
