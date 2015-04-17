7b63683c05473b95008bc0004658976ace9494b1
Miguel de Juana <miguel.dejuana@openbravo.com>
2015-04-17 13:24:57 +0200

    [HGVOL] Define for Product model hig volume reference value to be able to use high volume or not.
    --HG--
    extra : rebase_source : 7854ede8e35a72e59894bcbb8ad6ca6ed2de2d75

--- web/org.openbravo.retail.posterminal/js/model/product.js
+++ web/org.openbravo.retail.posterminal/js/model/product.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2013 Openbravo S.L.U.
+ * Copyright (C) 2013-2015 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -18,6 +18,7 @@
     source: 'org.openbravo.retail.posterminal.master.Product',
     dataLimit: 100,
     includeTerminalDate: true,
+    hgvol: 'OBPOS_highVolume.product',
     initialize: function () {
       this.set('originalStandardPrice', this.get('standardPrice'));
     }
