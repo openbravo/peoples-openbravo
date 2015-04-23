7f3e38ba4cf45a85ed1aa369e3f4ba6d0c097a87
Unai Martirena <unai.martirena@openbravo.com>
2015-04-23 10:20:01 +0200

    Fixes bug 29666: Fix NPE in Costing Background with Standard Costing Rule
    In Cost Adjustment project new method has been added to calculate an opening inventory cost. This method uses 'CostinUtils.getStandardCostDefinition' method, that tries to get an standard cost for that opening inventory. If it does not found any standard or legacy cost, it returns null. So, in this moment a NPE error was happening. To fix this problem, at this point a controlled error is raised to notify the user that no cost has been found.

--- src/org/openbravo/costing/StandardAlgorithm.java
+++ src/org/openbravo/costing/StandardAlgorithm.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2012-2014 Openbravo SLU
+ * All portions are Copyright (C) 2012-2015 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  *************************************************************************
@@ -21,8 +21,10 @@ package org.openbravo.costing;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import org.openbravo.base.exception.OBException;
 import org.openbravo.dal.core.DalUtil;
 import org.openbravo.dal.service.OBDal;
+import org.openbravo.erpCommon.utility.OBDateUtils;
 import org.openbravo.financial.FinancialUtils;
 import org.openbravo.model.common.enterprise.Organization;
 import org.openbravo.model.materialmgmt.cost.Costing;
@@ -60,6 +62,12 @@ public class StandardAlgorithm extends CostingAlgorithm {
     Costing stdCost = CostingUtils.getStandardCostDefinition(transaction.getProduct(), costOrg,
         transaction.getTransactionProcessDate(), costDimensions);
 
+    if (stdCost == null) {
+      // If no standard cost is found throw an exception.
+      throw new OBException("@NoStandardCostDefined@ @Organization@:" + costOrg.getName()
+          + ", @Product@: " + transaction.getProduct().getName() + ", @Date@: "
+          + OBDateUtils.formatDate(transaction.getTransactionProcessDate()));
+    }
     BigDecimal currentCost = stdCost.getCost();
     if (stdCost.getCurrency().getId().equals(costCurrency.getId())) {
       currentCost = FinancialUtils.getConvertedAmount(currentCost, stdCost.getCurrency(),
