bee88248578071a485caa8f1d271a7a66634d90a
Ander Iraceburu <ander.iraceburu@openbravo.com>
2015-04-29 10:44:53 +0200

    Changed JUnit Test acording to the code review v2

--- src-test/src/org/openbravo/test/datasource/ExtendedNavigationModelTest.java
+++ src-test/src/org/openbravo/test/datasource/ExtendedNavigationModelTest.java
@@ -48,6 +48,31 @@ public class ExtendedNavigationModelTest extends BaseDataSourceTestDal {
   // Warehouse with name: Spain warehouse
   private static String WAREHOUSE1_ID = "4028E6C72959682B01295ECFEF4502A0";
 
+  // Sales order's lines row
+  private static String row1 = "16A91C971E0F4E4E93F3D46587E6A02B";
+
+  // Return from customer's lines row
+  private static String row2 = "DE5E2B27DE494F9B8497E58FCA5EB427";
+
+  // Sales invoice window
+  private static String salesInvoiceWindow = "167";
+
+  // Return material receipt window
+  private static String returnMaterialReceiptWindow = "123271B9AD60469BAE8A924841456B63";
+
+  // Sales order's lines tab
+  private static String salesOrderLinesTab = "187";
+
+  // Sales order window
+  private static String salesOrderWindow = "143";
+
+  // Return from customer's lines tab
+
+  private static String returnFromCustomerLinesTab = "AF4090093D471431E040007F010048A5";
+
+  // Return from customer window
+  private static String returnFromCustomerWindow = "FF808081330213E60133021822E40007";
+
   // Test the link to the lines tab of the sales order window
   @Test
   public void testTableLevelRulesSalesOrder() throws Exception {
@@ -57,16 +82,18 @@ public class ExtendedNavigationModelTest extends BaseDataSourceTestDal {
     Map<String, String> params = new HashMap<String, String>();
     params.put("Command", "JSON");
     params.put("inpEntityName", "OrderLine");
-    params.put("inpKeyReferenceId", "16A91C971E0F4E4E93F3D46587E6A02B");
-    params.put("inpwindowId", "167");
+    params.put("inpKeyReferenceId", row1);
+    params.put("inpwindowId", salesInvoiceWindow);
     params.put("inpKeyReferenceColumnName", "C_OrderLine_ID");
     // Send the request
     String responseString = doRequest("/utility/ReferencedLink.html", params, 200, "POST");
     JSONObject responseJson = new JSONObject(responseString);
     // Check that the returned tab is the lines tab of the sales order window
-    assertThat("187", equalTo(responseJson.getString("tabId")));
+    assertThat("The destination tab is not the lines tab of the sales order window",
+        responseJson.getString("tabId"), equalTo(salesOrderLinesTab));
     // Check that the returned window is the sales order window
-    assertThat("143", equalTo(responseJson.getString("windowId")));
+    assertThat("The destination window is not the sales order window",
+        responseJson.getString("windowId"), equalTo(salesOrderWindow));
   }
 
   // Test the link to the lines tab of the return from customer window
@@ -78,15 +105,17 @@ public class ExtendedNavigationModelTest extends BaseDataSourceTestDal {
     Map<String, String> params2 = new HashMap<String, String>();
     params2.put("Command", "JSON");
     params2.put("inpEntityName", "OrderLine");
-    params2.put("inpKeyReferenceId", "DE5E2B27DE494F9B8497E58FCA5EB427");
-    params2.put("inpwindowId", "123271B9AD60469BAE8A924841456B63");
+    params2.put("inpKeyReferenceId", row2);
+    params2.put("inpwindowId", returnMaterialReceiptWindow);
     params2.put("inpKeyReferenceColumnName", "C_OrderLine_ID");
     // Send the request
     String responseString2 = doRequest("/utility/ReferencedLink.html", params2, 200, "POST");
     JSONObject responseJson2 = new JSONObject(responseString2);
     // Check that the returned tab is the lines tab of the return from customer window
-    assertThat("AF4090093D471431E040007F010048A5", equalTo(responseJson2.getString("tabId")));
+    assertThat("The destination tab is not the lines tab of the return from customer window",
+        responseJson2.getString("tabId"), equalTo(returnFromCustomerLinesTab));
     // Check that the returned window is the return from customer window
-    assertThat("FF808081330213E60133021822E40007", equalTo(responseJson2.getString("windowId")));
+    assertThat("The destination window is not the return from customer window",
+        responseJson2.getString("windowId"), equalTo(returnFromCustomerWindow));
   }
 }
