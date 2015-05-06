8c0a8a59b6fb732582c8147bffd1b9a64defc957
Gorka Ion Damián <gorkaion.damian@openbravo.com>
2015-05-06 12:00:17 +0200

    Fixed issue 29785.Added Allow Multiple Tabs preference.
    Added a new preference property Allow Multiple Tabs of a Window. This
    preference is checked when a new tab is opened. If the preference exists for
    the role, user and/or window a new tab is created if not the default behavior
    is followed.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/main/ob-standard-window.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/main/ob-standard-window.js
@@ -1205,6 +1205,8 @@ isc.OBStandardWindow.addProperties({
     // always return false to force new tabs
     if (this.multiDocumentEnabled) {
       return false;
+    } else if (OB.PropertyStore.get('AllowMultiTab', this.windowId) === 'Y'){
+      return false;
     }
     return this.isEqualParams(params) && viewName === this.getClassName();
   },
