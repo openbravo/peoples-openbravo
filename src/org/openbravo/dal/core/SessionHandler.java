3fa300086bc366c93d5c26847f0bd095d617d86f
Inigo Sanchez <inigo.sanchez@openbravo.com>
2015-05-13 11:47:35 +0200

    [pool] Added extra condition to get poolClassName properly
    Added an extra condition for taking into account "db.externalPoolClassName"
    property without value

--- src/org/openbravo/dal/core/SessionHandler.java
+++ src/org/openbravo/dal/core/SessionHandler.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2008-2014 Openbravo SLU 
+ * All portions are Copyright (C) 2008-2015 Openbravo SLU
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -56,7 +56,7 @@ public class SessionHandler implements OBNotSingleton {
   {
     String poolClassName = OBPropertiesProvider.getInstance().getOpenbravoProperties()
         .getProperty("db.externalPoolClassName");
-    if (poolClassName != null) {
+    if (poolClassName != null && !"".equals(poolClassName)) {
       try {
         externalConnectionPool = ExternalConnectionPool.getInstance(poolClassName);
       } catch (Throwable e) {
