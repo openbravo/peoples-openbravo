e9a7a0c10c6a33aa4f22253315bee77b486a5a59
Alvaro Ferraz <alvaro.ferraz@openbravo.com>
2015-05-20 14:06:47 +0200

    Fixes issue 29981: NPE in Add Payment process request
    When selecting a business partner with the magnifying glass in Add Payment process request, which does not match to the type of the document (a Vendor when the type is Received In); the payment method of the business partner was null and when accessing to its id, a NPE was raised.
    Now, if payment method is null will be checked.

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/actionHandler/ReceivedFromPaymentMethodActionHandler.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/actionHandler/ReceivedFromPaymentMethodActionHandler.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2014 Openbravo SLU
+ * All portions are Copyright (C) 2014-2015 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -48,7 +48,8 @@ public class ReceivedFromPaymentMethodActionHandler extends BaseActionHandler {
           BusinessPartner businessPartner = OBDal.getInstance().get(BusinessPartner.class,
               receivedFrom);
           if (jsonData.getString("isSOTrx").toString().equals("true")) {
-            paymentMethod = businessPartner.getPaymentMethod().getId();
+            paymentMethod = businessPartner.getPaymentMethod() != null ? businessPartner
+                .getPaymentMethod().getId() : "";
           } else {
             paymentMethod = businessPartner.getPOPaymentMethod() != null ? businessPartner
                 .getPOPaymentMethod().getId() : "";
