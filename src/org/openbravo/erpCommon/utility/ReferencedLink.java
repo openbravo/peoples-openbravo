20a709ad3e585b43fb5bc373262fd224d2271c63
Alvaro Ferraz <alvaro.ferraz@openbravo.com>
2015-06-02 11:59:51 +0200

    Fixes issue 30070: NPE when navigating from Payment In/Out Lines tab
    ReferencedLink was checking reference record when navigating from Order Payment Schedule or Invoice Payment Schedule fields from Payment In Lines or Payment Out Lines tab; in order to know where the record had to navigate to.
    When the value of this field was empty, a Null Pointer Exception was raised. Now, in this case, a "Record empty" exception will be raised.
    Also, those fields will be now not displayed if their value is empty.
    Duplicated Order Payment Schedule and Invoice Payment Schedule fields have been removed also in those tabs, what was causing an error in int-full-oracle-compile-all job.
    
    --HG--
    extra : transplant_source : R%9F%0DQ%D8%B2%CE%5C%0B%CA%7Bok_%1B%D0%D80%18%BB

--- src/org/openbravo/erpCommon/utility/ReferencedLink.java
+++ src/org/openbravo/erpCommon/utility/ReferencedLink.java
@@ -27,6 +27,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.openbravo.base.model.Entity;
@@ -235,33 +236,38 @@ public class ReferencedLink extends HttpSecureAppServlet {
 
       }
 
-      else if (strTableReferenceId.equals("CE9AFF5F51B846AEAB0B6CBF829092CD")
-          && (strWindowId.equals("E547CE89D4C04429B6340FFA44E70716") || strWindowId
-              .equals("6F8F913FA60F4CBD93DC1D3AA696E76E"))) {
+      else if (StringUtils.equals(strTableReferenceId, "CE9AFF5F51B846AEAB0B6CBF829092CD")
+          && (StringUtils.equals(strWindowId, "E547CE89D4C04429B6340FFA44E70716") || StringUtils
+              .equals(strWindowId, "6F8F913FA60F4CBD93DC1D3AA696E76E"))) {
         // Navigate to FIN_Payment_Schedule from Payment In or Payment Out: select destination
         // window depending on the source window
         try {
           OBContext.setAdminMode();
-          FIN_PaymentSchedule paymentSchedule = OBDal.getInstance().get(FIN_PaymentSchedule.class,
-              strKeyReferenceId);
-          if (paymentSchedule.getOrder() != null) {
-            // Order Payment Schedule
-            strTableReferenceId = "70E57DEA195843729FF303C9A71EBCA3";
-            if (strWindowId.equals("E547CE89D4C04429B6340FFA44E70716")) {
-              // Payment In window -> Sales Order
-              strWindowId = "143";
-            } else if (strWindowId.equals("6F8F913FA60F4CBD93DC1D3AA696E76E")) {
-              // Payment Out window -> Purchase Order
-              strWindowId = "181";
-            }
-          } else if (paymentSchedule.getInvoice() != null) {
-            // Invoice Payment Schedule
-            if (strWindowId.equals("E547CE89D4C04429B6340FFA44E70716")) {
-              // Payment In window -> Sales Invoice
-              strWindowId = "167";
-            } else if (strWindowId.equals("6F8F913FA60F4CBD93DC1D3AA696E76E")) {
-              // Payment Out window -> Purchase Invoice
-              strWindowId = "183";
+          if (StringUtils.isEmpty(strKeyReferenceId)
+              || StringUtils.equals(strKeyReferenceId, "null")) {
+            throw new ServletException("Record empty");
+          } else {
+            FIN_PaymentSchedule paymentSchedule = OBDal.getInstance().get(
+                FIN_PaymentSchedule.class, strKeyReferenceId);
+            if (paymentSchedule.getOrder() != null) {
+              // Order Payment Schedule
+              strTableReferenceId = "70E57DEA195843729FF303C9A71EBCA3";
+              if (strWindowId.equals("E547CE89D4C04429B6340FFA44E70716")) {
+                // Payment In window -> Sales Order
+                strWindowId = "143";
+              } else if (strWindowId.equals("6F8F913FA60F4CBD93DC1D3AA696E76E")) {
+                // Payment Out window -> Purchase Order
+                strWindowId = "181";
+              }
+            } else if (paymentSchedule.getInvoice() != null) {
+              // Invoice Payment Schedule
+              if (strWindowId.equals("E547CE89D4C04429B6340FFA44E70716")) {
+                // Payment In window -> Sales Invoice
+                strWindowId = "167";
+              } else if (strWindowId.equals("6F8F913FA60F4CBD93DC1D3AA696E76E")) {
+                // Payment Out window -> Purchase Invoice
+                strWindowId = "183";
+              }
             }
           }
         } finally {
