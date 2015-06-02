6534c7b357f2ee176cbcb1d1a51adaa5f37abd07
Naroa Iriarte <naroa.iriarte@openbravo.com>
2015-06-02 10:46:21 +0200

    Related with 29946: Some tests have been performed.
    Some test cases have been performed for testing the correct behaviour of apllying the
    "_noActiveFilter" to the dal and JSON WebServices.
    There are four tests. Two per WebService, one of them is putting the "_noActiveFilter" to true,
    it works if it gets the inactive records in the JSON WebServices and also in dal WebServices.
    The other test is putting the "_noActiveFilter" to false, it works if it don't get the inactive
    records.

--- src-test/src/org/openbravo/test/AllWebserviceTests.java
+++ src-test/src/org/openbravo/test/AllWebserviceTests.java
@@ -35,6 +35,7 @@ import org.openbravo.test.webservice.PerformanceTest;
 import org.openbravo.test.webservice.WSAddRecordWithComputedColumns;
 import org.openbravo.test.webservice.WSReadTest;
 import org.openbravo.test.webservice.WSUpdateTest;
+import org.openbravo.test.webservice.WebServicesWithNoActiveFilterTest;
 
 /**
  * This test suite should only contain test cases which are to run the webservices included in core.
@@ -58,6 +59,7 @@ WSReadTest.class, //
     FICTest.class, //
     HQLDataSourceTest.class, //
     TestNoteDatasource.class,//
-    TestCosting.class })
+    TestCosting.class,//
+    WebServicesWithNoActiveFilterTest.class })
 public class AllWebserviceTests {
 }
