7ffeddc1d1eb9ba05c7ead2d4f5e6da0cf723ab1
Eduardo Argal Guibert <eduardo.argal@openbravo.com>
2015-06-22 18:52:51 +0200

    [Product Service] Adds action handler. Missing from previous commit

---
+++ src/org/openbravo/common/actionhandler/ServiceOrderLineRelate.java
@@ -0,0 +1,116 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html 
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License. 
+ * The Original Code is Openbravo ERP. 
+ * The Initial Developer of the Original Code is Openbravo SLU 
+ * All portions are Copyright (C) 2015 Openbravo SLU 
+ * All Rights Reserved. 
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
+ */
+
+package org.openbravo.common.actionhandler;
+
+import java.util.Map;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+import org.hibernate.Query;
+import org.openbravo.base.provider.OBProvider;
+import org.openbravo.client.application.process.BaseProcessActionHandler;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.erpCommon.utility.OBMessageUtils;
+import org.openbravo.model.ad.access.OrderlineServiceRelation;
+import org.openbravo.model.ad.system.Client;
+import org.openbravo.model.common.enterprise.Organization;
+import org.openbravo.model.common.order.OrderLine;
+import org.openbravo.service.db.DbUtility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServiceOrderLineRelate extends BaseProcessActionHandler {
+  private static final Logger log = LoggerFactory.getLogger(ServiceOrderLineRelate.class);
+
+  @Override
+  protected JSONObject doExecute(Map<String, Object> parameters, String content) {
+    JSONObject jsonRequest = null;
+    OBContext.setAdminMode(true);
+    JSONObject errorMessage = new JSONObject();
+    try {
+      jsonRequest = new JSONObject(content);
+      log.debug("{}", jsonRequest);
+
+      JSONArray selectedLines = jsonRequest.getJSONObject("_params").getJSONObject("grid")
+          .getJSONArray("_selection");
+      if (selectedLines.length() == 0) {
+        errorMessage.put("severity", "error");
+        errorMessage.put("title", OBMessageUtils.messageBD("NotSelected"));
+        jsonRequest.put("message", errorMessage);
+        return jsonRequest;
+      }
+
+      final Client serviceProductClient = (Client) OBDal.getInstance().getProxy(Client.ENTITY_NAME,
+          jsonRequest.getString("inpadClientId"));
+      final Organization serviceProductOrg = (Organization) OBDal.getInstance().getProxy(
+          Organization.ENTITY_NAME, jsonRequest.getString("inpadOrgId"));
+      final OrderLine mainOrderLine = (OrderLine) OBDal.getInstance().getProxy(
+          OrderLine.ENTITY_NAME, jsonRequest.getString("inpcOrderlineId"));
+      // Remove existing rows
+      String strDelete = "delete from OrderlineServiceRelation where salesOrderLine.id=:orderLineId";
+      Query query = OBDal.getInstance().getSession().createQuery(strDelete);
+      query.setParameter("orderLineId", mainOrderLine.getId());
+      query.executeUpdate();
+      // Adding new rows
+      for (int i = 0; i < selectedLines.length(); i++) {
+        JSONObject selectedLine = selectedLines.getJSONObject(i);
+        log.debug("{}", selectedLine);
+
+        final OrderLine orderLine = (OrderLine) OBDal.getInstance().getProxy(OrderLine.ENTITY_NAME,
+            selectedLine.getString(OrderLine.PROPERTY_ID));
+
+        OrderlineServiceRelation olsr = OBProvider.getInstance()
+            .get(OrderlineServiceRelation.class);
+        olsr.setClient(serviceProductClient);
+        olsr.setOrganization(serviceProductOrg);
+        olsr.setOrderlineRelated(orderLine);
+        olsr.setSalesOrderLine(mainOrderLine);
+        OBDal.getInstance().save(olsr);
+        if ((i % 100) == 0) {
+          OBDal.getInstance().flush();
+          OBDal.getInstance().getSession().clear();
+        }
+      }
+
+      errorMessage.put("severity", "success");
+      errorMessage.put("title", OBMessageUtils.messageBD("Success"));
+      jsonRequest.put("message", errorMessage);
+    } catch (Exception e) {
+      log.error("Error in ServiceOrderLineRelate Action Handler", e);
+      OBDal.getInstance().rollbackAndClose();
+      try {
+        jsonRequest = new JSONObject();
+        Throwable ex = DbUtility.getUnderlyingSQLException(e);
+        String message = OBMessageUtils.translateError(ex.getMessage()).getMessage();
+        errorMessage = new JSONObject();
+        errorMessage.put("severity", "error");
+        errorMessage.put("text", message);
+        jsonRequest.put("message", errorMessage);
+
+      } catch (Exception e2) {
+        log.error(e.getMessage(), e2);
+      }
+    } finally {
+      OBContext.restorePreviousMode();
+    }
+    return jsonRequest;
+  }
+}
