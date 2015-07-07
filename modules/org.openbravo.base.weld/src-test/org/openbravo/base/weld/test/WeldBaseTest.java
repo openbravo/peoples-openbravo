c3867de451814413b6cfa771c8eb439c6c3dd3d2
Asier Lostalé <asier.lostale@openbravo.com>
2015-07-07 14:11:46 +0200

    [cdi-junit] proper initialization of interceptors in WeldBaseTest
       to allow execution of test suites.

--- modules/org.openbravo.base.weld/src-test/org/openbravo/base/weld/test/WeldBaseTest.java
+++ modules/org.openbravo.base.weld/src-test/org/openbravo/base/weld/test/WeldBaseTest.java
@@ -30,11 +30,14 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.openbravo.base.session.OBPropertiesProvider;
+import org.openbravo.base.session.SessionFactoryController;
 import org.openbravo.base.weld.WeldUtils;
 import org.openbravo.client.kernel.KernelInitializer;
+import org.openbravo.dal.core.OBInterceptor;
 import org.openbravo.test.base.OBBaseTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,27 +51,32 @@ import org.slf4j.LoggerFactory;
 public class WeldBaseTest extends OBBaseTest {
   private static final Logger log = LoggerFactory.getLogger(WeldBaseTest.class);
 
+  private static boolean initialized = false;
+  private static JavaArchive archive = null;
+
   @Deployment
   public static JavaArchive createTestArchive() {
-    log.info("Creating cdi archive...");
-    final String sourcePath = OBPropertiesProvider.getInstance().getOpenbravoProperties()
-        .getProperty("source.path");
-    final JavaArchive archive = ShrinkWrap.create(JavaArchive.class);
+    if (archive == null) {
+      log.info("Creating cdi archive...");
+      final String sourcePath = OBPropertiesProvider.getInstance().getOpenbravoProperties()
+          .getProperty("source.path");
+      archive = ShrinkWrap.create(JavaArchive.class);
 
-    // add all beans without exclusions so cdi can also be used for *test* packages
-    archive.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+      // add all beans without exclusions so cdi can also be used for *test* packages
+      archive.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 
-    // include all classes deployed in webapp container
-    archive.as(ExplodedImporter.class).importDirectory(sourcePath + "/build/classes/");
+      // include all classes deployed in webapp container
+      archive.as(ExplodedImporter.class).importDirectory(sourcePath + "/build/classes/");
 
-    // ...and all the jUnit ones
-    archive.as(ExplodedImporter.class).importDirectory(sourcePath + "/src-test/build/classes/");
+      // ...and all the jUnit ones
+      archive.as(ExplodedImporter.class).importDirectory(sourcePath + "/src-test/build/classes/");
 
-    // include all libraries deployed in webapp container
-    archive.addAsDirectory(sourcePath + "/WebContent/WEB-INF/lib");
+      // include all libraries deployed in webapp container
+      archive.addAsDirectory(sourcePath + "/WebContent/WEB-INF/lib");
 
-    log.debug(archive.toString(true));
-    log.info("... cdi archive created");
+      log.debug(archive.toString(true));
+      log.info("... cdi archive created");
+    }
     return archive;
   }
 
@@ -79,14 +87,37 @@ public class WeldBaseTest extends OBBaseTest {
   @Inject
   private BeanManager beanManager;
 
+  @Inject
+  private KernelInitializer kernelInitializer;
+
   /**
-   * sets static instance bean manager in WeldUtils so it is globally accessible and initializes
-   * kernel
+   * Sets static instance bean manager in WeldUtils so it is globally accessible and initializes
+   * kernel.
+   * 
+   * Arquillian creates a new cdi container for each test class but keeps existent one for all tests
+   * within same class, let's initialize it once per class but we cannot use @BeforeClass at this
+   * point because we require of beanManager to be injected.
    */
   @Before
   public void setManager() {
-    WeldUtils.setStaticInstanceBeanManager(beanManager);
-    WeldUtils.getInstanceFromStaticBeanManager(KernelInitializer.class).initialize();
+    if (!initialized) {
+      WeldUtils.setStaticInstanceBeanManager(beanManager);
+      kernelInitializer.setInterceptor();
+      initialized = true;
+    }
+  }
+
+  /**
+   * Once we are done with the class execution, OBInterceptor needs to be reset other case when
+   * executing a suite it will reuse the container created for the previous classes instead of the
+   * new one.
+   */
+  @AfterClass
+  public static void resetOBInterceptors() {
+    final OBInterceptor interceptor = (OBInterceptor) SessionFactoryController.getInstance()
+        .getConfiguration().getInterceptor();
+    interceptor.setInterceptorListener(null);
+    initialized = false;
   }
 
   @SuppressWarnings("unchecked")
