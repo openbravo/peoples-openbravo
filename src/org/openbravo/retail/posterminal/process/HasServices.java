371714aec84672e5a064186dd062c0262c070317
Javier Etxarri <javier.echarri@openbravo.com>
2015-07-08 15:16:16 +0200

    Modify hasService JAVA to filter by assortment, pricelist version and organization tree
    --HG--
    extra : rebase_source : 306a8845af419939f9004f4b1c9ab9c3539a994e

--- src/org/openbravo/retail/posterminal/process/HasServices.java
+++ src/org/openbravo/retail/posterminal/process/HasServices.java
@@ -10,6 +10,7 @@
 package org.openbravo.retail.posterminal.process;
 
 import java.io.StringWriter;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -20,9 +21,16 @@ import org.codehaus.jettison.json.JSONObject;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.openbravo.base.weld.WeldUtils;
+import org.openbravo.dal.core.DalUtil;
 import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.service.OBDal;
+import org.openbravo.mobile.core.process.SimpleQueryBuilder;
+import org.openbravo.mobile.core.utils.OBMOBCUtils;
+import org.openbravo.model.common.enterprise.Organization;
+import org.openbravo.model.pricing.pricelist.PriceListVersion;
 import org.openbravo.retail.posterminal.JSONProcessSimple;
+import org.openbravo.retail.posterminal.OBPOSApplications;
+import org.openbravo.retail.posterminal.POSUtils;
 import org.openbravo.service.json.JsonConstants;
 
 public class HasServices extends JSONProcessSimple {
@@ -33,49 +41,74 @@ public class HasServices extends JSONProcessSimple {
     JSONObject data = new JSONObject();
 
     try {
-      String productId = jsonData.getString("product");
-      String productCategoryId = jsonData.getString("productCategory");
+      JSONObject request = createRequestForProduct(jsonData, "Services_Filter");
+      String productId = request.getString("product");
+      String productCategoryId = request.getString("productCategory");
       StringWriter queryWriter = new StringWriter();
+      Organization terminalOrganization = (OBDal.getInstance().get(OBPOSApplications.class,
+          request.getString("pos"))).getOrganization();
+
+      Date terminalDate = OBMOBCUtils.calculateServerDate(
+          request.getJSONObject("parameters").getString("terminalTime"),
+          request.getJSONObject("parameters").getJSONObject("terminalTimeOffset").getLong("value"));
+
+      PriceListVersion priceListVersion = POSUtils
+          .getPriceListVersionByOrgId((String) DalUtil.getId(terminalOrganization), terminalDate);
 
       final StringBuilder hqlString = new StringBuilder();
 
       hqlString.append("select count(*), s.obposProposalType ");
-      hqlString.append("from Product as s ");
+      hqlString.append("from OBRETCO_Prol_Product as assort left outer join assort.product as s ");
       hqlString.append("where s.productType = 'S'  and s.linkedToProduct = true ");
+      hqlString.append("and s.$orgCriteria ");
+      hqlString
+          .append("and assort.obretcoProductlist.id = '"
+              + DalUtil.getId(
+                  POSUtils.getProductListByOrgId((String) DalUtil.getId(terminalOrganization)))
+          + "' ");
+      hqlString
+          .append("and exists (select 1 from PricingProductPrice as ppp where ppp.product.id = '"
+              + productId + "' and ppp.priceListVersion.id= '" + DalUtil.getId(priceListVersion)
+              + "' ) ");
       hqlString.append("and ((s.includedProducts = 'Y' and ");
-      hqlString
-          .append("not exists (select 1 from ServiceProduct sp where s = sp.product and sp.relatedProduct.id = '"
+      hqlString.append(
+          "not exists (select 1 from ServiceProduct sp where s = sp.product and sp.relatedProduct.id = '"
               + productId + "')) ");
-      hqlString
-          .append("or (s.includedProducts = 'N' and exists (select 1 from ServiceProduct sp where s = sp.product and sp.relatedProduct.id = '"
+      hqlString.append(
+          "or (s.includedProducts = 'N' and exists (select 1 from ServiceProduct sp where s = sp.product and sp.relatedProduct.id = '"
               + productId + "')) ");
       hqlString.append("or s.includedProducts is null) ");
       hqlString.append("and ((s.includedProductCategories = 'Y' and ");
-      hqlString
-          .append("not exists (select 1 from ServiceProductCategory spc where s = spc.product and spc.productCategory.id = '"
+      hqlString.append(
+          "not exists (select 1 from ServiceProductCategory spc where s = spc.product and spc.productCategory.id = '"
               + productCategoryId + "')) ");
-      hqlString
-          .append("or (s.includedProductCategories = 'N' and exists (select 1 from ServiceProductCategory spc where s = spc.product and spc.productCategory.id = '"
+      hqlString.append(
+          "or (s.includedProductCategories = 'N' and exists (select 1 from ServiceProductCategory spc where s = spc.product and spc.productCategory.id = '"
               + productCategoryId + "')) ");
       hqlString.append("or s.includedProductCategories is null) ");
       hqlString.append("group by s.obposProposalType ");
 
+      SimpleQueryBuilder querybuilder = new SimpleQueryBuilder(hqlString.toString(),
+          OBContext.getOBContext().getCurrentClient().getId(),
+          (String) DalUtil.getId(terminalOrganization), null, null, null);
+
       final Session session = OBDal.getInstance().getSession();
-      final Query query = session.createQuery(hqlString.toString());
+      final Query query = session.createQuery(querybuilder.getHQLQuery());
 
       List<?> services = query.list();
       if (services.size() > 0) {
         for (Object resultObject : services) {
           Object[] serviceLine = (Object[]) resultObject;
           if ("MP".equals(serviceLine[1])) {
-            JSONObject request = createRequestForProduct(jsonData, "Services_Filter");
             org.openbravo.retail.posterminal.master.Product product = WeldUtils
-                .getInstanceFromStaticBeanManager(org.openbravo.retail.posterminal.master.Product.class);
+                .getInstanceFromStaticBeanManager(
+                    org.openbravo.retail.posterminal.master.Product.class);
             product.exec(queryWriter, request);
             data.put("mandatoryservices",
                 new JSONObject("{" + queryWriter.toString() + "}").get("data"));
             data.put("hasservices", true);
             result.put("data", data);
+            break;
           } else {
             data.put("hasservices", true);
             result.put("data", data);
