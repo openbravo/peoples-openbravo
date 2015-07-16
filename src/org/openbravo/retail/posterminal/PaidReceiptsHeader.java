d6e1ed73b18f749d64f44312979817dfbc2ee31a
Antonio Moreno <antonio.moreno@openbravo.com>
2015-07-16 11:42:05 +0200

    Fixed issue 30284. slow query for orders
    Replaced two count subqueries with exist subqueries

--- src/org/openbravo/retail/posterminal/PaidReceiptsHeader.java
+++ src/org/openbravo/retail/posterminal/PaidReceiptsHeader.java
@@ -93,7 +93,7 @@ public class PaidReceiptsHeader extends ProcessHQLQuery {
       hqlPaidReceipts += " and (select sum(deliveredQuantity) from ord.orderLineList where orderedQuantity > 0)=0 and ord.documentStatus = 'CO' ";
     } else if (json.getBoolean("isReturn")) {
       // (It is not Layaway and It is not a Return)
-      hqlPaidReceipts += " and ((select count(deliveredQuantity) from ord.orderLineList where deliveredQuantity != 0) > 0 and (select count(orderedQuantity) from ord.orderLineList where orderedQuantity > 0) > 0) ";
+      hqlPaidReceipts += " and (exists( select 1 from ord.orderLineList where deliveredQuantity != 0) and exists( select 1 from ord.orderLineList where orderedQuantity > 0)) ";
     } else {
       // (It is not Layaway or it is a Return)
       hqlPaidReceipts += " and exists(select 1 from ord.orderLineList where deliveredQuantity != 0) ";
