27f51ae2a39502406fb4eab6e0782127d963634f
Alvaro Ferraz <alvaro.ferraz@openbravo.com>
2015-07-22 12:38:35 +0200

    Fixes issue 30396: Modify CreateLandedCostCostAcctConf modulescript
    Modify CreateLandedCostCostAcctConf modulescript to execute it only if updating from before this changeset: https://code.openbravo.com/erp/devel/main/rev/6625af338e5e

--- src-util/modulescript/src/org/openbravo/modulescript/CreateLandedCostCostAcctConf.java
+++ src-util/modulescript/src/org/openbravo/modulescript/CreateLandedCostCostAcctConf.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2011 Openbravo SLU
+ * All portions are Copyright (C) 2014-2015 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  *************************************************************************
@@ -59,5 +59,11 @@ public class CreateLandedCostCostAcctConf extends ModuleScript {
         }
       }
     }
+  }
+  
+  @Override
+  protected ModuleScriptExecutionLimits getModuleScriptExecutionLimits() {
+    return new ModuleScriptExecutionLimits("0", null, 
+        new OpenbravoVersion(3,0,24861));
   }
 }
