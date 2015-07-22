ddf4fa7d74ee7f5ebf3e9ebd9d983c3b88fb9bdc
Alvaro Ferraz <alvaro.ferraz@openbravo.com>
2015-07-22 19:35:11 +0200

    Fixes issue 30396: Modify InitializeAlertStatus modulescript
    Modify InitializeAlertStatus modulescript to execute it only if updating from before this changeset: https://code.openbravo.com/erp/devel/main/rev/b26053672b18

--- src-util/modulescript/src/org/openbravo/modulescript/InitializeAlertStatus.java
+++ src-util/modulescript/src/org/openbravo/modulescript/InitializeAlertStatus.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2011 Openbravo SLU
+ * All portions are Copyright (C) 2011-2015 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -36,5 +36,11 @@ public class InitializeAlertStatus extends ModuleScript {
     } catch (Exception e) {
       handleError(e);
     }
+  }
+  
+  @Override
+  protected ModuleScriptExecutionLimits getModuleScriptExecutionLimits() {
+    return new ModuleScriptExecutionLimits("0", null, 
+        new OpenbravoVersion(3,0,11365));
   }
 }
