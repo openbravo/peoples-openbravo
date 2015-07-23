1b9aedc8031559deba39483e63c486d6a5d04e89
Alvaro Ferraz <alvaro.ferraz@openbravo.com>
2015-07-23 18:23:48 +0200

    Fixes issue 30396: Modify UpdateCostingRulesWhenNotCalculatedCosts modulescript
    Modify UpdateCostingRulesWhenNotCalculatedCosts modulescript to execute it only if updating from before this changeset: https://code.openbravo.com/erp/devel/main/rev/8af60f70a49c

--- src-util/modulescript/src/org/openbravo/modulescript/UpdateCostingRulesWhenNotCalculatedCosts.java
+++ src-util/modulescript/src/org/openbravo/modulescript/UpdateCostingRulesWhenNotCalculatedCosts.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2012 Openbravo SLU
+ * All portions are Copyright (C) 2012-2015 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  *************************************************************************
@@ -47,5 +47,11 @@ public class UpdateCostingRulesWhenNotCalculatedCosts extends ModuleScript {
     } catch (Exception e) {
       handleError(e);
     }
+  }
+  
+  @Override
+  protected ModuleScriptExecutionLimits getModuleScriptExecutionLimits() {
+    return new ModuleScriptExecutionLimits("0", null, 
+        new OpenbravoVersion(3,0,17837));
   }
 }
