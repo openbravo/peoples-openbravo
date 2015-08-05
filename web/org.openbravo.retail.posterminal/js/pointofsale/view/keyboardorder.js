ca457bcd7411d45626ec255447b4b420cd16590e
Asier Martirena <asier.martirena@openbravo.com>
2015-08-05 11:49:01 +0200

    [Product Service] Fixed error with approvals during returns.
    Now, during a blind return when trying to return a product with a service related approval is asked

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/keyboardorder.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/keyboardorder.js
@@ -390,7 +390,7 @@ enyo.kind({
               }
             }
           }
-          for (var i = 0; i < OB.MobileApp.model.receipt.get('lines').length; i++) {
+          for (var i = 0; i < OB.MobileApp.model.receipt.get('lines').length; i++) { // Check if there is any not returnable related product to a selected line
             var line = OB.MobileApp.model.receipt.get('lines').models[i];
             if (line.get('product').get('productType') === 'S' && !line.isReturnable()) {
               if (line.get('relatedLines')) {
@@ -401,6 +401,18 @@ enyo.kind({
                     if (lineFromSelected.id === relatedLine.orderlineId) {
                       OB.UTIL.showConfirmation.display(OB.I18N.getLabel('OBPOS_UnreturnableRelatedService'), OB.I18N.getLabel('OBPOS_UnreturnableRelatedServiceMessage', [line.get('product').get('_identifier'), relatedLine.productName]));
                       return;
+                    }
+                  }
+                }
+              }
+            } else if (!approvalNeeded && line.get('product').get('productType') === 'S') {
+              if (line.get('relatedLines')) {
+                for (var j = 0; j < line.get('relatedLines').length; j++) {
+                  var relatedLine = line.get('relatedLines')[j];
+                  for (var k = 0; k < me.selectedModels.length; k++) {
+                    var lineFromSelected = me.selectedModels[k];
+                    if (lineFromSelected.id === relatedLine.orderlineId) {
+                      approvalNeeded = true;
                     }
                   }
                 }
