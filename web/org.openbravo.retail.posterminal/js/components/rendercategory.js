57b1e5746ba15ca6d140be9609812e9e4e4f502a
Eduardo Becerra <ebec70@yahoo.com>
2015-09-04 13:42:29 +0200

    [Category Tree] Internal code review

--- web/org.openbravo.retail.posterminal/js/components/rendercategory.js
+++ web/org.openbravo.retail.posterminal/js/components/rendercategory.js
@@ -39,17 +39,16 @@ enyo.kind({
   name: 'OB.UI.RenderCategoryExpand',
   kind: 'Image',
   src: '../org.openbravo.retail.posterminal/img/iconExpand.png',
-  sizing: "constrain",
-  position: 'center',
-  width: 32,
-  height: 32
+  sizing: 'cover',
+  width: 26,
+  height: 26
 });
 
 enyo.kind({
   name: 'OB.UI.RenderCategoryCollapse',
   kind: 'Image',
   src: '../org.openbravo.retail.posterminal/img/iconCollapse.png',
-  sizing: "cover",
+  sizing: 'cover',
   width: 26,
   height: 26,
   showing: false
@@ -94,7 +93,7 @@ enyo.kind({
     style: 'float:left; width: 40px; text-align: right; margin-top: -8px;',
     components: [{
       name: 'expandCollapse',
-      style: 'height: 41px; padding-right: 7px; padding-top: 7px;',
+      style: 'height: 33px; padding-right: 7px; padding-top: 7px;',
       components: [{
         kind: 'OB.UI.RenderCategoryExpand',
         name: 'expand'
