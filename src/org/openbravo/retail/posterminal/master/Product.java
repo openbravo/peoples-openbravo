364bb1bba834c6d0a81213b5b890e9bd0af4b614
Adrián Romero <adrianromero@openbravo.com>
2015-09-07 09:37:24 +0200

    Reverting empty changes of Product.java

--- src/org/openbravo/retail/posterminal/master/Product.java
+++ src/org/openbravo/retail/posterminal/master/Product.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2015 Openbravo S.L.U.
+ * Copyright (C) 2012-2013 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -182,6 +182,7 @@ public class Product extends ProcessHQLQuery {
     }
 
     return products;
+
   }
 
   @Override
