f864b9fc77152811545827153c22704298f629e4
Atul Gaware <atul.gaware@openbravo.com>
2015-09-08 10:27:21 +0530

    Fixes Issue 30735:All price lists are displayed without organization role access check.
    Validation is provided to check the organization of price list being loaded
    is accessible by Role of logged in user. SL_ProductPrice_PriceListVersion
    is deprecated as is not required and also unlinked from the column.
    
    --HG--
    extra : transplant_source : %B9gS%F2%B7%AB%16%8B%FB%05%22%E27%F4/%D7j%40%28.

--- src/org/openbravo/erpCommon/ad_callouts/SL_ProductPrice_PriceListVersion.java
+++ src/org/openbravo/erpCommon/ad_callouts/SL_ProductPrice_PriceListVersion.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2012 Openbravo SLU 
+ * All portions are Copyright (C) 2012-2015 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -33,6 +33,7 @@ import org.openbravo.model.ad.access.Role;
 import org.openbravo.model.pricing.pricelist.PriceListVersion;
 import org.openbravo.xmlEngine.XmlDocument;
 
+@Deprecated
 public class SL_ProductPrice_PriceListVersion extends HttpSecureAppServlet {
   private static final long serialVersionUID = 1L;
 
