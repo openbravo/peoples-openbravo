65b2a38b4fbd6097964698ef25f1b9ebb11a92f4
Rafa Alonso <ral@openbravo.com>
2015-09-25 11:00:30 +0200

    Fixes issue 30928: Removed unnecessary saveCurrent

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
@@ -424,9 +424,7 @@ enyo.kind({
       if (args.cancelOperation && args.cancelOperation === true) {
         return true;
       }
-      args.context.model.get('order').addProduct(args.productToAdd, args.qtyToAdd, args.options, args.attrs, function (success) {
-        args.context.model.get('orderList').saveCurrent();
-      });
+      args.context.model.get('order').addProduct(args.productToAdd, args.qtyToAdd, args.options, args.attrs, null);
     });
     return true;
   },
