271606913c99de0626b180dfaed315e9eb47e34d
Víctor Martínez Romanos <victor.martinez@openbravo.com>
2015-09-30 16:13:10 +0200

    Fixed bug 31088: Scheduled Organization for Costing background process
    The Costing Background Process should only be scheduled for a legal entity or any of its ancestor.
    
    The fix adds a validation in both the ProcessRequestEventHandler, so each time a record is saved/updated the validation is run, and in the CostingBackground class itself to control wrong process request scheduled before this validation.
    
    --HG--
    extra : transplant_source : %9B%CC%5C%DA%F9%99k%5B%B2%9A%EE4%22%F6oy%C5%89%F09

--- src/org/openbravo/event/ProcessRequestEventHandler.java
+++ src/org/openbravo/event/ProcessRequestEventHandler.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2014 Openbravo SLU 
+ * All portions are Copyright (C) 2014-2015 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -28,10 +28,14 @@ import org.openbravo.base.model.Property;
 import org.openbravo.client.kernel.event.EntityNewEvent;
 import org.openbravo.client.kernel.event.EntityPersistenceEvent;
 import org.openbravo.client.kernel.event.EntityPersistenceEventObserver;
+import org.openbravo.client.kernel.event.EntityUpdateEvent;
+import org.openbravo.costing.CostingUtils;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.model.ad.ui.Process;
 import org.openbravo.model.ad.ui.ProcessGroup;
+import org.openbravo.model.ad.ui.ProcessGroupList;
 import org.openbravo.model.ad.ui.ProcessRequest;
+import org.openbravo.model.common.enterprise.Organization;
 
 /**
  * Event Handler to check Process Group fields consistency
@@ -47,12 +51,20 @@ public class ProcessRequestEventHandler extends EntityPersistenceEventObserver {
     return entities;
   }
 
-  public void onSave(@Observes
-  EntityNewEvent event) {
+  public void onSave(@Observes EntityNewEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
     checkMandatoryFields(event);
+    checkValidOrganizationCostingServer(event);
+  }
+
+  public void onUpdate(@Observes EntityUpdateEvent event) {
+    if (!isValidEvent(event)) {
+      return;
+    }
+
+    checkValidOrganizationCostingServer(event);
   }
 
   private void checkMandatoryFields(EntityPersistenceEvent event) {
@@ -87,4 +99,45 @@ public class ProcessRequestEventHandler extends EntityPersistenceEventObserver {
     }
 
   }
+
+  /**
+   * Returns and exception when the Costing Background Process has been scheduled for a child of a
+   * legal entity organization.
+   * 
+   * This method checks a process request for a single process or for a group of processes
+   * (iterating over the process group list)
+   */
+  private void checkValidOrganizationCostingServer(EntityPersistenceEvent event) {
+    if (event != null && (event instanceof EntityNewEvent || event instanceof EntityUpdateEvent)) {
+      final Entity processRequestEntity = ModelProvider.getInstance().getEntity(
+          ProcessRequest.ENTITY_NAME);
+
+      final Property isGroupNameProperty = processRequestEntity
+          .getProperty(ProcessRequest.PROPERTY_ISGROUP);
+      final Property orgProperty = processRequestEntity
+          .getProperty(ProcessRequest.PROPERTY_ORGANIZATION);
+
+      final Boolean isGroup = (Boolean) event.getCurrentState(isGroupNameProperty);
+      final Organization scheduledOrg = (Organization) event.getCurrentState(orgProperty);
+
+      if (isGroup != null && isGroup.booleanValue()) {
+        // Check for all the Processes defined in the group
+        final Property processGroupProperty = processRequestEntity
+            .getProperty(ProcessRequest.PROPERTY_PROCESSGROUP);
+        final ProcessGroup processGroup = (ProcessGroup) event
+            .getCurrentState(processGroupProperty);
+
+        for (final ProcessGroupList pgl : processGroup.getProcessGroupListList()) {
+          CostingUtils.checkValidOrganization(pgl.getProcess().getId(), scheduledOrg);
+        }
+      } else {
+        final Property processProperty = processRequestEntity
+            .getProperty(ProcessRequest.PROPERTY_PROCESS);
+        final Process scheduledProcess = (Process) event.getCurrentState(processProperty);
+
+        CostingUtils.checkValidOrganization(scheduledProcess.getId(), scheduledOrg);
+      }
+    }
+  }
+
 }
