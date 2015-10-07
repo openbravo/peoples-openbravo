7a255a6f96e2e35aaeb94bf879edb1726fb517a3
Asier Lostalé <asier.lostale@openbravo.com>
2015-10-07 16:41:23 +0200

    [pos-license] warn when limit is reached

--- src/org/openbravo/retail/posterminal/security/POSLicenseRestrictions.java
+++ src/org/openbravo/retail/posterminal/security/POSLicenseRestrictions.java
@@ -126,7 +126,7 @@ public class POSLicenseRestrictions implements ModuleLicenseRestrictions {
     if (warnLimit == 0) {
       return OBMessageUtils.messageBD(new DalConnectionProvider(false), "OBPOS_TerminalNotAllowed",
           lang);
-    } else if (actualNumberOfTerminals > warnLimit) {
+    } else if (actualNumberOfTerminals >= warnLimit) {
       return OBMessageUtils
           .messageBD(new DalConnectionProvider(false), "OBPOS_TerminalLimitWarn", lang)
           .replace("%0", Integer.toString(actualNumberOfTerminals))
