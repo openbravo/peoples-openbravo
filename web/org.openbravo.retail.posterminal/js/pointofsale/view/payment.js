c39a5ce4d226a93cac7de4005cd0a4a8b234241d
Mario Castello <mario.castello@peoplewalking.com>
2015-10-23 10:46:21 -0600

    Fixed issue 31188: Hide exact and credit button

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/payment.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/payment.js
@@ -281,9 +281,10 @@ enyo.kind({
       }
       var payment = OB.MobileApp.model.paymentnames[OB.MobileApp.model.get('paymentcash')];
       if ((model.get('orderType') === 2 || (model.get('isLayaway'))) && model.get('orderType') !== 3 && !model.getPaymentStatus().done) {
-        this.$.creditsalesaction.show();
+        this.$.creditsalesaction.hide();
         this.$.layawayaction.setContent(OB.I18N.getLabel('OBPOS_LblLayaway'));
         this.$.layawayaction.show();
+        this.$.exactbutton.hide();
       } else if (model.get('orderType') === 3) {
         this.$.creditsalesaction.hide();
         this.$.layawayaction.hide();
