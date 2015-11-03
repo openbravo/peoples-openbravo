b79b5b4d48229967289a0753c1820654ded69a86
Ranjith <ranjith@qualiantech.com>
2015-11-03 14:43:41 +0530

    Fixed Issue 31303 : Adding Maxlength to receiptProperty description field
    --HG--
    extra : rebase_source : a21dd63d2b6fe691939951cda6521ddc35cdf9af

--- src/org/openbravo/retail/posterminal/OrderLoader.java
+++ src/org/openbravo/retail/posterminal/OrderLoader.java
@@ -24,6 +24,7 @@ import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
@@ -1176,6 +1177,9 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
 
   protected void createOrder(Order order, JSONObject jsonorder) throws JSONException {
     Entity orderEntity = ModelProvider.getInstance().getEntity(Order.class);
+    if (jsonorder.has("description") && StringUtils.length(jsonorder.getString("description")) > 255) {
+      jsonorder.put("description", StringUtils.substring(jsonorder.getString("description"), 0, 255));
+    }
     JSONPropertyToEntity.fillBobFromJSON(orderEntity, order, jsonorder,
         jsonorder.getLong("timezoneOffset"));
     order.setNewOBObject(true);
