c36f1a97f28b17280e114d8865e9a72b144938c1
Martin Taal <martin.taal@openbravo.com>
2015-11-20 17:58:07 +0100

    Fixes issue 31541: Ensure that in case of multi-server that the all the generated documents in the orderloader have the same document no Added count to make unique predictable doc numbers

--- src/org/openbravo/retail/posterminal/OrderLoader.java
+++ src/org/openbravo/retail/posterminal/OrderLoader.java
@@ -29,6 +29,7 @@ import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.hibernate.Query;
 import org.hibernate.ScrollMode;
 import org.hibernate.ScrollableResults;
 import org.hibernate.criterion.Restrictions;
@@ -84,7 +85,9 @@ import org.openbravo.model.financialmgmt.payment.FIN_FinancialAccount;
 import org.openbravo.model.financialmgmt.payment.FIN_OrigPaymentScheduleDetail;
 import org.openbravo.model.financialmgmt.payment.FIN_Payment;
 import org.openbravo.model.financialmgmt.payment.FIN_PaymentDetail;
+import org.openbravo.model.financialmgmt.payment.FIN_PaymentDetailV;
 import org.openbravo.model.financialmgmt.payment.FIN_PaymentMethod;
+import org.openbravo.model.financialmgmt.payment.FIN_PaymentSchedOrdV;
 import org.openbravo.model.financialmgmt.payment.FIN_PaymentSchedule;
 import org.openbravo.model.financialmgmt.payment.FIN_PaymentScheduleDetail;
 import org.openbravo.model.financialmgmt.payment.Fin_OrigPaymentSchedule;
@@ -774,9 +777,13 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
     invoice.setTransactionDocument(getInvoiceDocumentType((String) DalUtil.getId(order
         .getDocumentType())));
 
-    invoice.setDocumentNo(getDummyDocumentNo());
-    addDocumentNoHandler(invoice, invoiceEntity, invoice.getTransactionDocument(),
-        invoice.getDocumentType());
+    if (useOrderDocumentNoForRelatedDocs) {
+      invoice.setDocumentNo(order.getDocumentNo());
+    } else {
+      invoice.setDocumentNo(getDummyDocumentNo());
+      addDocumentNoHandler(invoice, invoiceEntity, invoice.getTransactionDocument(),
+          invoice.getDocumentType());
+    }
 
     invoice.setAccountingDate(order.getOrderDate());
     invoice.setInvoiceDate(order.getOrderDate());
@@ -1074,7 +1081,11 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
         .setDocumentType(getShipmentDocumentType((String) DalUtil.getId(order.getDocumentType())));
 
     if (useOrderDocumentNoForRelatedDocs) {
-      shipment.setDocumentNo(order.getDocumentNo());
+      String docNum = order.getDocumentNo();
+      if (order.getMaterialMgmtShipmentInOutList().size() > 0) {
+        docNum += "-" + order.getMaterialMgmtShipmentInOutList().size();
+      }
+      shipment.setDocumentNo(docNum);
     } else {
       addDocumentNoHandler(shipment, shpEntity, null, shipment.getDocumentType());
     }
@@ -1683,7 +1694,11 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
 
       String paymentDocNo;
       if (useOrderDocumentNoForRelatedDocs) {
+        final int paymentCount = countPayments(order);
         paymentDocNo = order.getDocumentNo();
+        if (paymentCount > 0) {
+          paymentDocNo = paymentDocNo + "-" + paymentCount;
+        }
       } else {
         paymentDocNo = getDocumentNo(paymentEntity, null, paymentDocType);
       }
@@ -1750,6 +1765,15 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
       OBContext.restorePreviousMode();
     }
 
+  }
+
+  private int countPayments(Order order) {
+    final String countHql = "select count(*) from FIN_Payment_Detail_V where "
+        + FIN_PaymentDetailV.PROPERTY_ORDERPAYMENTPLAN + "."
+        + FIN_PaymentSchedOrdV.PROPERTY_SALESORDER + "=:order";
+    final Query qry = OBDal.getInstance().getSession().createQuery(countHql);
+    qry.setEntity("order", order);
+    return ((Number) qry.uniqueResult()).intValue();
   }
 
   protected void verifyCashupStatus(JSONObject jsonorder) throws JSONException, OBException {
