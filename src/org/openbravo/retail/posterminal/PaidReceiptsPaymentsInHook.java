b0e2ee22aa5291db0053a7f787e95a09f962a7fb
Unai Martirena <unai.martirena@openbravo.com>
2016-01-15 12:57:59 +0100

    [Digital Coupons] Add PaidReceiptsPaymentsInHook java file
    --HG--
    extra : rebase_source : f20298a6e1061365e59c4fc7cc029aac23f3d88c

---
+++ src/org/openbravo/retail/posterminal/PaidReceiptsPaymentsInHook.java
@@ -0,0 +1,17 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2016 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+package org.openbravo.retail.posterminal;
+
+import org.codehaus.jettison.json.JSONObject;
+
+public interface PaidReceiptsPaymentsInHook {
+
+  public void exec(JSONObject paymentsIn, String paymentId) throws Exception;
+}
