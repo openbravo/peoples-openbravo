8abc5bdea79aafb127611aa2de726275340eefe5
Asier Lostalé <asier.lostale@openbravo.com>
2016-02-23 16:37:30 +0100

    fixed bug 32309: error on update if an observer loaded current object in memory
      In case an entity observer loaded current object in memory (ie. observing orderLine)
      doing orderLine.getSalesOrder().getOrderLineList() there were two different instances
      in memory representing the same DB row. This caused problems when trying to evict it.
      Causing an exception to be thrown when updating from UI in this case.
    
      As solution eviction is no longer performed, object is forced to be fetched from DB by
      executing a Criteria.
    
    --HG--
    extra : transplant_source : %0DZ%A3%09%A8%87%19%93c%86%9C%0A%1B%89%0D%19%C3%B0%0E%B9

--- modules/org.openbravo.service.json/src/org/openbravo/service/json/DefaultJsonDataService.java
+++ modules/org.openbravo.service.json/src/org/openbravo/service/json/DefaultJsonDataService.java
@@ -767,17 +767,19 @@ public class DefaultJsonDataService implements JsonDataService {
           }
         }
 
-        // refresh the objects from the db as they can have changed
-        // put the refreshed objects into a new array as we are going to retrieve them using
-        // OBDal.getInstance().get as performs better than OBDal.getInstance().getSession().refresh
-        // See issue https://issues.openbravo.com/view.php?id=30308
+        // Objects might have been modified in DB through triggers, let's force them to be fetched
+        // DB again, to do so session is cleared (any possible modification is already persisted by
+        // previous flush).
+        // Using OBDal.refresh does not perform well, see issue
+        // https://issues.openbravo.com/view.php?id=30308
+        OBDal.getInstance().getSession().clear();
+
         final List<BaseOBObject> refreshedBobs = new ArrayList<BaseOBObject>();
         for (BaseOBObject bob : bobs) {
-          // Remove the bob instance from the session cache with evict
-          OBDal.getInstance().getSession().evict(bob);
-          // With get() we retrieve the object from db as we have cleared it from cache with evict()
+          // forcing fetch from DB
           BaseOBObject refreshedBob = OBDal.getInstance().get(bob.getEntityName(),
               DalUtil.getId(bob));
+
           // if object has computed columns refresh from the database too
           if (refreshedBob.getEntity().hasComputedColumns()) {
             OBDal.getInstance().getSession()
@@ -786,7 +788,7 @@ public class DefaultJsonDataService implements JsonDataService {
           refreshedBobs.add(refreshedBob);
         }
 
-        // almost successfull, now create the response
+        // almost successful, now create the response
         // needs to be done before the close of the session
         final DataToJsonConverter toJsonConverter = OBProvider.getInstance().get(
             DataToJsonConverter.class);
