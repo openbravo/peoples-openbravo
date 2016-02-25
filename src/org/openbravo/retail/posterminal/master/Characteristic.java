ba456cff636b4681695c35e49904e1543d1ada9a
Sandra Huguet <sandra.huguet@openbravo.com>
2016-02-25 11:07:00 +0100

    related to issue 32279 Updated Copyright

--- src/org/openbravo/retail/posterminal/master/Characteristic.java
+++ src/org/openbravo/retail/posterminal/master/Characteristic.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2015 Openbravo S.L.U.
+ * Copyright (C) 2015-2016 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -83,7 +83,7 @@ public class Characteristic extends ProcessHQLQuery {
             + "where  $filtersCriteria AND $hqlCriteria and "
             + isFilterOnWebPos
             + " ch.$naturalOrgCriteria and ch.$readableSimpleClientCriteria and (ch.$incrementalUpdateCriteria) "
-            + " order by ch.name");
+            + "order by ch.name");
 
     return hqlQueries;
   }
