e0613906ccfa0dc951edba0fb3b1b24180808247
Inigo Sanchez <inigo.sanchez@openbravo.com>
2016-02-29 23:51:51 +0100

    Fixes issue 32237: Payment In and Payment Out lines are not exported to CSV.
    The problem was that export to CSV does not work properly in some subtabs without
    link to parent column (e.g.Lines subtab of Payment In/Out). Before, It was used
    "tabId" to obtaing data for  export to CSV and the value of the "tabId" was sent
    in a variable called "tab". The Criteria that is run when it is displayed the tab
    uses a property named "tabId" instead of "tab".
    
    The solution for this case (and other related cases) is to adapt the code that
    exports to CSV to use the same Criteria run when displaying the tab. Now export to
    CSV uses tabId property. Besides it has been refactored some piece of code related
    with export data in DataSourceServlet to use "tabId" instead of "tab".

--- modules/org.openbravo.service.datasource/src/org/openbravo/service/datasource/DataSourceServlet.java
+++ modules/org.openbravo.service.datasource/src/org/openbravo/service/datasource/DataSourceServlet.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2009-2015 Openbravo SLU
+ * All portions are Copyright (C) 2009-2016 Openbravo SLU
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -226,9 +226,10 @@ public class DataSourceServlet extends BaseKernelServlet {
           try {
             OBContext.setAdminMode();
             try {
-              Window window = parameters.get("tab") == null
-                  || parameters.get("tab").equals("undefined") ? null : OBDal.getInstance()
-                  .get(Tab.class, parameters.get("tab")).getWindow();
+              Window window = parameters.get(JsonConstants.TAB_PARAMETER) == null
+                  || parameters.get(JsonConstants.TAB_PARAMETER).equals("undefined") ? null : OBDal
+                  .getInstance().get(Tab.class, parameters.get(JsonConstants.TAB_PARAMETER))
+                  .getWindow();
               String encoding = Preferences.getPreferenceValue("OBSERDS_CSVTextEncoding", true,
                   OBContext.getOBContext().getCurrentClient(), OBContext.getOBContext()
                       .getCurrentOrganization(), OBContext.getOBContext().getUser(), OBContext
@@ -299,8 +300,9 @@ public class DataSourceServlet extends BaseKernelServlet {
         response.setHeader("Content-Disposition", "attachment; filename=ExportedData.csv");
         writer = response.getWriter();
         VariablesSecureApp vars = new VariablesSecureApp(request);
-        Window window = parameters.get("tab") == null || parameters.get("tab").equals("undefined") ? null
-            : OBDal.getInstance().get(Tab.class, parameters.get("tab")).getWindow();
+        Window window = parameters.get(JsonConstants.TAB_PARAMETER) == null
+            || parameters.get(JsonConstants.TAB_PARAMETER).equals("undefined") ? null : OBDal
+            .getInstance().get(Tab.class, parameters.get(JsonConstants.TAB_PARAMETER)).getWindow();
         try {
           prefDecimalSeparator = Preferences.getPreferenceValue("OBSERDS_CSVDecimalSeparator",
               true, OBContext.getOBContext().getCurrentClient(), OBContext.getOBContext()
@@ -414,8 +416,10 @@ public class DataSourceServlet extends BaseKernelServlet {
               } else {
                 niceFieldProperties.put(propKey, col.getName());
               }
-            } else if (parameters.get("tab") != null && !parameters.get("tab").equals("")) {
-              Tab tab = OBDal.getInstance().get(Tab.class, parameters.get("tab"));
+            } else if (parameters.get(JsonConstants.TAB_PARAMETER) != null
+                && !parameters.get(JsonConstants.TAB_PARAMETER).equals("")) {
+              Tab tab = OBDal.getInstance().get(Tab.class,
+                  parameters.get(JsonConstants.TAB_PARAMETER));
               for (Field field : tab.getADFieldList()) {
                 if (field.getColumn() == null || !field.getColumn().getId().equals(col.getId())) {
                   continue;
@@ -685,9 +689,10 @@ public class DataSourceServlet extends BaseKernelServlet {
       try {
         String csvMessage = null;
         try {
-          Window window = parameters.get("tab") == null
-              || parameters.get("tab").equals("undefined") ? null : OBDal.getInstance()
-              .get(Tab.class, parameters.get("tab")).getWindow();
+          Window window = parameters.get(JsonConstants.TAB_PARAMETER) == null
+              || parameters.get(JsonConstants.TAB_PARAMETER).equals("undefined") ? null : OBDal
+              .getInstance().get(Tab.class, parameters.get(JsonConstants.TAB_PARAMETER))
+              .getWindow();
           csvMessage = Preferences.getPreferenceValue(property, true, OBContext.getOBContext()
               .getCurrentClient(), OBContext.getOBContext().getCurrentOrganization(), OBContext
               .getOBContext().getUser(), OBContext.getOBContext().getRole(), window);
