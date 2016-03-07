ef03d641b2c8bcfcec815b3d3d2a3c12b5195bbe
Jorge Garcia <jorge.garcia@openbravo.com>
2016-03-07 11:08:11 +0100

    Related to issue 32016: error message should be displayed when manifest does not exists
    It's necessary to change the popup message to be more easy to the user to
    understand.

--- web/org.openbravo.retail.posterminal/js/login/model/login-model.js
+++ web/org.openbravo.retail.posterminal/js/login/model/login-model.js
@@ -157,8 +157,8 @@
                 OB.MobileApp.model.loadingErrorsActions("The terminal.usermodel should be loaded at this point");
               } else if (OB.MobileApp.model.attributes.loadManifeststatus && OB.MobileApp.model.attributes.loadManifeststatus.type === 'error') {
                 var error = OB.MobileApp.model.attributes.loadManifeststatus;
-                var msg = error.reason + ' failed to load: ' + error.url;
-                OB.UTIL.showConfirmation.display(OB.I18N.getLabel('OBPOS_TitleFailedAppCache'), OB.I18N.getLabel('OBPOS_FailedAppCache', [msg]), [{
+                OB.debug(error.reason + ' failed to load: ' + error.url);
+                OB.UTIL.showConfirmation.display(OB.I18N.getLabel('OBPOS_TitleFailedAppCache'), OB.I18N.getLabel('OBPOS_FailedAppCache'), [{
                   label: OB.I18N.getLabel('OBMOBC_LblOk'),
                   isConfirmButton: true
                 }], {
