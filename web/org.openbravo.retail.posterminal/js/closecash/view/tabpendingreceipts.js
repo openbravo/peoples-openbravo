1699331b9d4f74fdf49c036d4e8ae54d66e0cc58
Ranjith S R <ranjith@qualiantech.com>
2016-03-23 17:41:05 +0530

    Fixes issue 32056 : Changes in Cashup Pending Receipt List design

--- web/org.openbravo.retail.posterminal/js/closecash/view/tabpendingreceipts.js
+++ web/org.openbravo.retail.posterminal/js/closecash/view/tabpendingreceipts.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2015 Openbravo S.L.U.
+ * Copyright (C) 2012-2016 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -95,6 +95,8 @@ enyo.kind({
                 this.setContent(OB.I18N.getLabel('OBPOS_LblStepNumber', [step, count]) + " " + OB.I18N.getLabel('OBPOS_LblStepPendingOrders') + OB.OBPOSCashUp.UI.CashUp.getTitleExtensions());
               }
             }]
+          }, {
+            style: 'clear: both;'
           }]
         }, {
           name: 'rowDeleteAll',
