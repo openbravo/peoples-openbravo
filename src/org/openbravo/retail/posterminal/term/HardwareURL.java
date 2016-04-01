267d32a2702a9a0a4fe5bb6045dff7e1c875abe0
Reinaldo Guerra <reinaldo.guerra@peoplewalking.com>
2016-04-01 08:55:36 -0500

    Refactoring print configuration at Pos Terminal Type level to use the POS central printer's definition
    --HG--
    extra : source : 5fe49f966ac824226cc1ce1cd9f17626e846c2f5

--- src/org/openbravo/retail/posterminal/term/HardwareURL.java
+++ src/org/openbravo/retail/posterminal/term/HardwareURL.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2015 Openbravo S.L.U.
+ * Copyright (C) 2015-2016 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -14,6 +14,8 @@ import java.util.List;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.openbravo.client.kernel.RequestContext;
+import org.openbravo.retail.posterminal.OBPOSApplications;
+import org.openbravo.retail.posterminal.POSUtils;
 
 public class HardwareURL extends QueryTerminalProperty {
 
@@ -25,11 +27,14 @@ public class HardwareURL extends QueryTerminalProperty {
   @Override
   protected List<String> getQuery(JSONObject jsonsent) throws JSONException {
 
-    String posId = RequestContext.get().getSessionAttribute("POSTerminal").toString();
+    final OBPOSApplications posDetail = POSUtils.getTerminalById(RequestContext.get()
+        .getSessionAttribute("POSTerminal").toString());
+    String terminalType = posDetail.getObposTerminaltype().getId();
 
     return Arrays.asList(new String[] { //
-        "select terminal.obposTerminaltype.oBPOSHardwareURLList " //
-            + "from OBPOS_Applications terminal where terminal.id = '" + posId + "'" //
+        "select p.id as id, p.obposHardwaremng.name as _identifier, p.obposHardwaremng.hardwareURL as hardwareURL, p.obposHardwaremng.hasReceiptPrinter as hasReceiptPrinter, " //
+            + "p.obposHardwaremng.hasPDFPrinter as hasPDFPrinter, p.active as active " //
+            + "from OBPOS_HardwareURL as p where p.pOSTerminalType.id = '" + terminalType + "'" //
         });
   }
 
