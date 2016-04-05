07ddf9dcbec184c41acbd2039f09ae9f1cb5edbb
Asier Lostalé <asier.lostale@openbravo.com>
2016-04-05 17:12:02 +0200

    related to bug 32605: widget summary not displayed if HQL has group by clause
      Added log in case exception is thrown when executing summary query.
    
    --HG--
    extra : rebase_source : 3401a84efd6298e586a396274168b4605d93533c

--- modules/org.openbravo.client.querylist/src/org/openbravo/client/querylist/QueryListDataSource.java
+++ modules/org.openbravo.client.querylist/src/org/openbravo/client/querylist/QueryListDataSource.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2014 Openbravo SLU
+ * All portions are Copyright (C) 2010-2016 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -260,6 +260,9 @@ public class QueryListDataSource extends ReadOnlyDataSourceService implements Po
           }
           summaryData.put("isGridSummary", true);
         } catch (Exception e) {
+          log.error(
+              "Exception fetching summary columns of the widget " + widgetClass.getWidgetTitle()
+                  + ". \n Query = " + HQL, e);
         }
         result.add(summaryData);
 
