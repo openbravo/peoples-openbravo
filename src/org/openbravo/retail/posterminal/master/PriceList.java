1fd1750da8a1dccde4c9b970fae0b919cc651cee
Miguel de Juana <miguel.dejuana@openbravo.com>
2016-04-15 13:37:56 +0200

    Fixed issue 0032657: WebPOS is Loading all ProductPrices even if MultiPricelist is not activated
    - If preference is disabled, do not load prices lits and product prices

--- src/org/openbravo/retail/posterminal/master/PriceList.java
+++ src/org/openbravo/retail/posterminal/master/PriceList.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2015 Openbravo S.L.U.
+ * Copyright (C) 2015-2016 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -17,10 +17,13 @@ import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
+import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.openbravo.client.kernel.ComponentProvider.Qualifier;
 import org.openbravo.dal.core.OBContext;
+import org.openbravo.erpCommon.businessUtility.Preferences;
+import org.openbravo.erpCommon.utility.PropertyException;
 import org.openbravo.mobile.core.model.HQLPropertyList;
 import org.openbravo.mobile.core.model.ModelExtension;
 import org.openbravo.mobile.core.model.ModelExtensionUtils;
@@ -28,6 +31,7 @@ import org.openbravo.retail.posterminal.ProcessHQLQuery;
 
 public class PriceList extends ProcessHQLQuery {
   public static final String priceListPropertyExtension = "OBPOS_PriceListExtension";
+  public static final Logger log = Logger.getLogger(PriceList.class);
 
   @Inject
   @Any
@@ -36,33 +40,40 @@ public class PriceList extends ProcessHQLQuery {
 
   @Override
   protected List<String> getQuery(JSONObject jsonsent) throws JSONException {
+    boolean multiPrices = false;
     String orgId = OBContext.getOBContext().getCurrentOrganization().getId();
     List<String> hqlQueries = new ArrayList<String>();
 
     HQLPropertyList priceListHQLProperties = ModelExtensionUtils.getPropertyExtensions(extensions);
 
-    hqlQueries
-        .add("select "
-            + priceListHQLProperties.getHqlSelect()
-            + " from PricingPriceList pl "
-            + "where pl.id in (select distinct priceList.id from BusinessPartner where customer = 'Y') "
-            + "and pl.id <> (select obretcoPricelist.id from Organization where id = '"
-            + orgId
-            + "') "
-            + "and $naturalOrgCriteria and $readableClientCriteria and ($incrementalUpdateCriteria)");
+    try {
+      multiPrices = "Y".equals(Preferences.getPreferenceValue("OBPOS_EnableMultiPriceList", true,
+          OBContext.getOBContext().getCurrentClient(),
+          OBContext.getOBContext().getCurrentOrganization(), OBContext.getOBContext().getUser(),
+          OBContext.getOBContext().getRole(), null));
+    } catch (PropertyException e1) {
+      log.error("Error getting Preference: " + e1.getMessage(), e1);
+    }
+
+    if (multiPrices) {
+      hqlQueries.add("select " + priceListHQLProperties.getHqlSelect()
+          + " from PricingPriceList pl "
+          + "where pl.id in (select distinct priceList.id from BusinessPartner where customer = 'Y') "
+          + "and pl.id <> (select obretcoPricelist.id from Organization where id = '" + orgId
+          + "') "
+          + "and $naturalOrgCriteria and $readableClientCriteria and ($incrementalUpdateCriteria)");
+    }
 
     return hqlQueries;
   }
 
   public static String getSelectPriceListVersionIds(String orgId, Date terminalDate) {
     SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
-    return "select plv.id from PricingPriceListVersion AS plv "
-        + "where plv.active = true"
+    return "select plv.id from PricingPriceListVersion AS plv " + "where plv.active = true"
         + " and plv.validFromDate = ("
         + "  select max(pplv.validFromDate) from PricingPriceListVersion as pplv "
         + "  where pplv.active=true and pplv.priceList.id = plv.priceList.id "
-        + "    and to_char(pplv.validFromDate, 'yyyy-mm-dd') <= '"
-        + format.format(terminalDate)
+        + "    and to_char(pplv.validFromDate, 'yyyy-mm-dd') <= '" + format.format(terminalDate)
         + " ') and (plv.priceList.id in (select distinct priceList.id from BusinessPartner where customer = 'Y') "
         + " and plv.priceList.id <> (select obretcoPricelist.id from Organization where id = '"
         + orgId + "'))";
--- src/org/openbravo/retail/posterminal/master/ProductPrice.java
+++ src/org/openbravo/retail/posterminal/master/ProductPrice.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2015 Openbravo S.L.U.
+ * Copyright (C) 2015-2016 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -18,10 +18,13 @@ import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
+import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.openbravo.client.kernel.ComponentProvider.Qualifier;
 import org.openbravo.dal.core.OBContext;
+import org.openbravo.erpCommon.businessUtility.Preferences;
+import org.openbravo.erpCommon.utility.PropertyException;
 import org.openbravo.mobile.core.model.HQLPropertyList;
 import org.openbravo.mobile.core.model.ModelExtension;
 import org.openbravo.mobile.core.model.ModelExtensionUtils;
@@ -32,6 +35,7 @@ import org.openbravo.retail.posterminal.ProcessHQLQuery;
 
 public class ProductPrice extends ProcessHQLQuery {
   public static final String productPricePropertyExtension = "OBPOS_ProductPriceExtension";
+  public static final Logger log = Logger.getLogger(ProductPrice.class);
 
   @Inject
   @Any
@@ -51,30 +55,40 @@ public class ProductPrice extends ProcessHQLQuery {
   @Override
   protected List<String> getQuery(JSONObject jsonsent) throws JSONException {
     String orgId = OBContext.getOBContext().getCurrentOrganization().getId();
-
+    boolean multiPrices = false;
     OBRETCOProductList productList = POSUtils.getProductListByOrgId(orgId);
     if (productList == null) {
       throw new JSONException("Product list not found");
     }
 
-    Date terminalDate = OBMOBCUtils.calculateServerDate(jsonsent.getJSONObject("parameters")
-        .getString("terminalTime"),
+    try {
+      multiPrices = "Y".equals(Preferences.getPreferenceValue("OBPOS_EnableMultiPriceList", true,
+          OBContext.getOBContext().getCurrentClient(),
+          OBContext.getOBContext().getCurrentOrganization(), OBContext.getOBContext().getUser(),
+          OBContext.getOBContext().getRole(), null));
+    } catch (PropertyException e1) {
+      log.error("Error getting Preference: " + e1.getMessage(), e1);
+    }
+
+    Date terminalDate = OBMOBCUtils.calculateServerDate(
+        jsonsent.getJSONObject("parameters").getString("terminalTime"),
         jsonsent.getJSONObject("parameters").getJSONObject("terminalTimeOffset").getLong("value"));
 
     List<String> hqlQueries = new ArrayList<String>();
 
     HQLPropertyList priceListHQLProperties = ModelExtensionUtils.getPropertyExtensions(extensions);
 
-    hqlQueries
-        .add("select "
-            + priceListHQLProperties.getHqlSelect()
-            + "from OBRETCO_Prol_Product as pli, PricingProductPrice ppp "
-            + "where pli.product.id = ppp.product.id and pli.obretcoProductlist = '"
-            + productList.getId()
-            + "' and ppp.priceListVersion.id in ("
-            + PriceList.getSelectPriceListVersionIds(orgId, terminalDate)
-            + ") and $filtersCriteria AND $hqlCriteria "
-            + "and pli.$naturalOrgCriteria and pli.$readableClientCriteria and (ppp.$incrementalUpdateCriteria)");
+    if (multiPrices) {
+      hqlQueries
+          .add("select " + priceListHQLProperties.getHqlSelect()
+              + "from OBRETCO_Prol_Product as pli, PricingProductPrice ppp "
+              + "where pli.product.id = ppp.product.id and pli.obretcoProductlist = '"
+              + productList.getId() + "' and ppp.priceListVersion.id in ("
+              + PriceList.getSelectPriceListVersionIds(orgId,
+                  terminalDate)
+              + ") and $filtersCriteria AND $hqlCriteria "
+              + "and pli.$naturalOrgCriteria and pli.$readableClientCriteria and (ppp.$incrementalUpdateCriteria)");
+    }
 
     return hqlQueries;
   }
