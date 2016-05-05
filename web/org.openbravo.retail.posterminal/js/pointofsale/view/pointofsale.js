842815952a7716c401d02f77c2c0ff15cf1eca28
Shivanand Madiwalar <shivanand.madiwalar@promantia.com>
2016-05-05 10:05:01 +0530

    Fixes Issue 32787 : Logic Has Been Added to Hide the ReturnLine Button When Its Return Receipt or LayAway .

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/pointofsale.js
@@ -93,7 +93,8 @@ enyo.kind({
     onToggleLineSelection: 'toggleLineSelection',
     onFinishServiceProposal: 'finishServiceProposal',
     onkeydown: 'keyDownHandler',
-    onkeyup: 'keyUpHandler'
+    onkeyup: 'keyUpHandler',
+    onRearrangeEditButtonBar:'rearrangeEditButtonBar'
   },
   events: {
     onShowPopup: '',
@@ -1289,6 +1290,9 @@ enyo.kind({
   },
   setMultiSelectionItems: function (inSender, inEvent) {
     this.waterfall('onTableMultiSelectedItems', inEvent);
+  },
+  rearrangeEditButtonBar:function (inSender, inEvent) {
+	  this.waterfall('onRearrangedEditButtonBar', inEvent);
   },
   init: function () {
     var receipt, receiptList, LeftColumnCurrentView;
