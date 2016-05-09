f7e74323f89aa16be6286e38b23f60aeaf0fdb77
Antonio Moreno <antonio.moreno@openbravo.com>
2016-05-09 11:09:35 +0200

    Related to issue 32589. Also update prices in case the property is true
    --HG--
    extra : rebase_source : b2f93647d6f9a23076ecc0b297b504c3e90fcac4

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -1631,7 +1631,7 @@
         OB.Dal.find(OB.Model.ProductPrice, criteria, function (productPrices) {
           if (productPrices.length > 0) {
             p = p.clone();
-            if (OB.UTIL.isNullOrUndefined(p.get('updatePriceFromPricelist'))) {
+            if (OB.UTIL.isNullOrUndefined(p.get('updatePriceFromPricelist')) || p.get('updatePriceFromPricelist')) {
               p.set('standardPrice', productPrices.at(0).get('pricestd'));
               p.set('listPrice', productPrices.at(0).get('pricelist'));
             }
