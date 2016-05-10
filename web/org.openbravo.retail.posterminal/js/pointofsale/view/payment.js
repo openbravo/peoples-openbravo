83a66b683ca17709f9010da4fbe25281e7d041ca
Mario Castello <mario.castello@peoplewalking.com>
2016-05-10 08:15:05 -0600

    Fixed issue 32814: Added validation in payment view to show messages

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/payment.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/payment.js
@@ -666,15 +666,10 @@ enyo.kind({
       this.$.donebutton.setLocalDisabled(false);
       this.$.exactbutton.setLocalDisabled(false);
     } else {
-      if (this.$.overpaymentnotavailable.showing || this.$.overpaymentexceedlimit.showing) {
+      if (this.$.overpaymentnotavailable.showing || this.$.overpaymentexceedlimit.showing || this.$.onlycashpaymentmethod.showing) {
         this.$.noenoughchangelbl.setStyle("position: absolute; bottom: 20px; height: 20px; color: #ff0000;");
         this.$.noenoughchangelbl.hide();
       } else {
-        if (this.$.onlycashpaymentmethod.showing) {
-          this.$.noenoughchangelbl.setStyle("position: absolute; bottom: 20px; height: 20px; color: #ff0000;");
-        } else {
-          this.$.noenoughchangelbl.setStyle("position: absolute; bottom: 0px; height: 20px; color: #ff0000;");
-        }
         this.$.noenoughchangelbl.show();
       }
       this.$.payments.scrollAreaMaxHeight = '130px';
