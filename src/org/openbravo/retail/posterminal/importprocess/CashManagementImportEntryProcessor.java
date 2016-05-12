dd9d1025a03f057d69d25e243979fe141c71cf0f
Jorge Garcia <jorge.garcia@openbravo.com>
2016-05-12 16:37:54 +0200

    Fixed issue 32872: cash management: error in data import entry window by dead lock
    The solution here is to check if there is an order or a transaction beeing
    processed in the Import Entry Processor to avoid transaction colitions in the
    database.
    
    --HG--
    extra : rebase_source : d7efcb69bf6b103352b800f64273b8912e2c2201

--- src/org/openbravo/retail/posterminal/importprocess/CashManagementImportEntryProcessor.java
+++ src/org/openbravo/retail/posterminal/importprocess/CashManagementImportEntryProcessor.java
@@ -10,8 +10,12 @@ package org.openbravo.retail.posterminal.importprocess;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.hibernate.Query;
+import org.openbravo.base.exception.OBException;
 import org.openbravo.base.weld.WeldUtils;
 import org.openbravo.dal.core.DalUtil;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBDal;
 import org.openbravo.mobile.core.process.DataSynchronizationProcess;
 import org.openbravo.mobile.core.process.MobileImportEntryProcessorRunnable;
 import org.openbravo.retail.posterminal.ProcessCashMgmt;
@@ -44,6 +48,58 @@ public class CashManagementImportEntryProcessor extends ImportEntryProcessor {
     protected Class<? extends DataSynchronizationProcess> getDataSynchronizationClass() {
       return ProcessCashMgmt.class;
     }
+
+    protected void processEntry(ImportEntry importEntry) throws Exception {
+      // check that there are no other import entries for the terminal
+      // which have not yet been processed
+
+      try {
+        OBContext.setAdminMode(false);
+
+        if (thereIsDataInImportQueue(importEntry)) {
+          // close and commit
+          OBDal.getInstance().commitAndClose();
+          return;
+        }
+
+      } finally {
+        OBContext.restorePreviousMode();
+      }
+      super.processEntry(importEntry);
+    }
+
+    private boolean thereIsDataInImportQueue(ImportEntry importEntry) {
+      try {
+        OBContext.setAdminMode(false);
+
+        if (0 < countEntries("Error", importEntry)) {
+          // if there are related error entries before this one then this is an error
+          // throw an exception to move this entry also to error status
+          throw new OBException("There are error records before this record " + importEntry
+              + ", moving this entry also to error status.");
+        }
+
+        return 0 < countEntries("Initial", importEntry);
+      } finally {
+        OBContext.restorePreviousMode();
+      }
+    }
+
+    private int countEntries(String importStatus, ImportEntry importEntry) {
+      final String whereClause = ImportEntry.PROPERTY_IMPORTSTATUS + "='" + importStatus
+          + "' and (" + ImportEntry.PROPERTY_TYPEOFDATA + "='Order' or "
+          + ImportEntry.PROPERTY_TYPEOFDATA + "='FIN_Finacc_Transaction') and "
+          + ImportEntry.PROPERTY_CREATIONDATE + "<:creationDate and "
+          + ImportEntry.PROPERTY_OBPOSPOSTERMINAL + "=:terminal and id!=:id";
+      final Query qry = OBDal.getInstance().getSession()
+          .createQuery("select count(*) from " + ImportEntry.ENTITY_NAME + " where " + whereClause);
+      qry.setParameter("id", importEntry.getId());
+      qry.setTimestamp("creationDate", importEntry.getCreationDate());
+      qry.setParameter("terminal", importEntry.getOBPOSPOSTerminal());
+
+      return ((Number) qry.uniqueResult()).intValue();
+    }
+
   }
 
 }
