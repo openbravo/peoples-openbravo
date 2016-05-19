ed90b8ba042d33039e1cf21ca8feeb6a8c62cc04
Carlos Aristu <carlos.aristu@openbravo.com>
2016-05-19 17:33:29 +0200

    [pi-selector-performance] Selected properties in selector drop-down requests The requests performed by the drop-down of the selector now include just the needed properties: 1- The fields to be displayed on the pick list are requested using the SELECTED_PROPERTIES parameter. 2- The display field, value field and out fields are requested using the EXTRA_PROPERTIES parameter. This is because this parameter is always used on the requests as it is part of the selector template definition within the view.

--- modules/org.openbravo.userinterface.selector/web/org.openbravo.userinterface.selector/js/ob-selector-item.js
+++ modules/org.openbravo.userinterface.selector/web/org.openbravo.userinterface.selector/js/ob-selector-item.js
@@ -143,12 +143,13 @@ isc.OBSelectorPopupWindow.addProperties({
       },
 
       getFetchRequestParams: function (params) {
+        var requestType = 'Window';
         params = params || {};
         if (this.selector) {
-          isc.OBSelectorItem.prepareDSRequest(params, this.selector);
+          isc.OBSelectorItem.prepareDSRequest(params, this.selector, requestType);
         }
 
-        params._requestType = 'Window';
+        params._requestType = requestType;
 
         if (this.getSelectedRecord()) {
           params._targetRecordId = this.targetRecordId;
@@ -1003,7 +1004,7 @@ isc.OBSelectorItem.addProperties({
     requestProperties = requestProperties || {};
     requestProperties.params = requestProperties.params || {};
 
-    isc.OBSelectorItem.prepareDSRequest(requestProperties.params, this);
+    isc.OBSelectorItem.prepareDSRequest(requestProperties.params, this, 'PickList');
 
     // sometimes the value is passed as a filter criteria remove it
     if (this.getValueFieldName() && requestProperties.params[this.getValueFieldName()]) {
@@ -1087,6 +1088,36 @@ isc.OBSelectorItem.addProperties({
     return criteria;
   },
 
+  getSelectedPropertiesString: function () {
+    var i, fieldName, selectedProperties = OB.Constants.ID;
+    for (i = 0; i < this.pickListFields.length; i++) {
+      fieldName = this.pickListFields[i].name;
+      if (fieldName === OB.Constants.ID || fieldName === OB.Constants.IDENTIFIER) {
+        continue;
+      }
+      selectedProperties += ',' + fieldName;
+    }
+    return selectedProperties;
+  },
+
+  getExtraPropertiesString: function () {
+    var i, outFieldName, outFieldsNames, extraProperties = this.valueField || '';
+    if (this.displayField && this.displayField !== OB.Constants.IDENTIFIER) {
+      extraProperties += ',' + this.displayField;
+    }
+    if (this.outFields) {
+      outFieldsNames = isc.getKeys(this.outFields);
+      for (i = 0; i < outFieldsNames.length; i++) {
+        outFieldName = outFieldsNames[i];
+        if (outFieldName === this.valueField || outFieldName === this.displayField) {
+          continue;
+        }
+        extraProperties += ',' + outFieldName;
+      }
+    }
+    return extraProperties;
+  },
+
   mapValueToDisplay: function (value) {
     var ret = this.Super('mapValueToDisplay', arguments);
     if (ret === value && this.isDisabled()) {
@@ -1137,7 +1168,7 @@ isc.OBSelectorItem.addProperties({
 isc.OBSelectorItem.addClassMethods({
   // Prepares requestProperties adding contextInfo, this is later used in backed
   // to prepare filters 
-  prepareDSRequest: function (params, selector) {
+  prepareDSRequest: function (params, selector, requestType) {
     function setOrgIdParam(params) {
       if (!params.inpadOrgId) {
         // look for an ad_org_id parameter. If there is no such parameter or its value is empty, use the current user organization
@@ -1204,6 +1235,13 @@ isc.OBSelectorItem.addClassMethods({
     // and sort according to the display field
     // initially
     params[OB.Constants.SORTBY_PARAMETER] = selector.displayField;
+
+    if (requestType === 'PickList') {
+      // Add the fields to be displayed in the picklist as selected properties
+      params[OB.Constants.SELECTED_PROPERTIES] = selector.getSelectedPropertiesString();
+      // Include value field, display field and out fields into additional properties
+      params[OB.Constants.EXTRA_PROPERTIES] = selector.getExtraPropertiesString();
+    }
 
     // Parameter windows
     if (selector.form.paramWindow) {
