1c9ab8487c36eab81958f0ff05c8bfd9d709df26
Asier Lostalé <asier.lostale@openbravo.com>
2016-05-24 10:12:04 +0200

    related to issue 30680: clean up SystemValidatorTest
      Removed unused (ignored) test cases

--- src-test/src/org/openbravo/test/system/SystemValidatorTest.java
+++ src-test/src/org/openbravo/test/system/SystemValidatorTest.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2009-2014 Openbravo SLU 
+ * All portions are Copyright (C) 2009-2016 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -21,21 +21,10 @@ package org.openbravo.test.system;
 
 import static org.junit.Assert.fail;
 
-import java.io.File;
 import java.util.List;
-import java.util.Properties;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.apache.ddlutils.Platform;
-import org.apache.ddlutils.PlatformFactory;
-import org.apache.ddlutils.model.Database;
-import org.apache.ddlutils.platform.ExcludeFilter;
 import org.apache.log4j.Logger;
 import org.junit.Test;
-import org.openbravo.base.session.OBPropertiesProvider;
-import org.openbravo.dal.service.OBDal;
-import org.openbravo.ddlutils.util.DBSMOBUtil;
-import org.openbravo.model.ad.module.Module;
 import org.openbravo.service.system.DatabaseValidator;
 import org.openbravo.service.system.ModuleValidator;
 import org.openbravo.service.system.SystemValidationResult;
@@ -54,65 +43,6 @@ import org.openbravo.test.base.OBBaseTest;
 public class SystemValidatorTest extends OBBaseTest {
 
   private static final Logger log = Logger.getLogger(SystemValidatorTest.class);
-
-  /**
-   * Executes the {@link DatabaseValidator#validate()} method on the current database.
-   */
-  public void noTestSystemValidation() {
-    // disabled as covered by dbconsistency test and quite slow
-    setSystemAdministratorContext();
-    final DatabaseValidator databaseValidator = new DatabaseValidator();
-    databaseValidator.setDatabase(createDatabaseObject(null));
-    final SystemValidationResult result = databaseValidator.validate();
-    printResult(result, true);
-  }
-
-  /**
-   * Executes the {@link DatabaseValidator#validate()} method on the current database for all
-   * modules.
-   */
-  public void noTestSystemValidationModules() {
-    // disabled as covered by dbconsistency test and quite slow
-    setSystemAdministratorContext();
-    for (Module module : OBDal.getInstance().createQuery(Module.class, "").list()) {
-      final DatabaseValidator databaseValidator = new DatabaseValidator();
-      if (module.getModuleDBPrefixList().isEmpty()) {
-        // ignore these ones
-        continue;
-      }
-      databaseValidator.setDatabase(createDatabaseObject(module));
-      databaseValidator.setValidateModule(module);
-      final SystemValidationResult result = databaseValidator.validate();
-      printResult(result, false);
-    }
-  }
-
-  private Database createDatabaseObject(Module module) {
-    final Properties props = OBPropertiesProvider.getInstance().getOpenbravoProperties();
-
-    final BasicDataSource ds = new BasicDataSource();
-    ds.setDriverClassName(props.getProperty("bbdd.driver"));
-    if (props.getProperty("bbdd.rdbms").equals("POSTGRE")) {
-      ds.setUrl(props.getProperty("bbdd.url") + "/" + props.getProperty("bbdd.sid"));
-    } else {
-      ds.setUrl(props.getProperty("bbdd.url"));
-    }
-    ds.setUsername(props.getProperty("bbdd.user"));
-    ds.setPassword(props.getProperty("bbdd.password"));
-    Platform platform = PlatformFactory.createNewPlatformInstance(ds);
-    platform.getModelLoader().setOnlyLoadTableColumns(true);
-
-    if (module != null) {
-      final String dbPrefix = module.getModuleDBPrefixList().get(0).getName();
-      final ExcludeFilter filter = DBSMOBUtil.getInstance().getExcludeFilter(
-          new File(props.getProperty("source.path")));
-      filter.addPrefix(dbPrefix);
-
-      return platform.loadModelFromDatabase(filter, dbPrefix, true, module.getId());
-    }
-
-    return platform.loadModelFromDatabase(null);
-  }
 
   /**
    * Performs module validation using the {@link ModuleValidator}.
