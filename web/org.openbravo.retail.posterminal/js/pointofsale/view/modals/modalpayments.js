1f29a2a546972df9a6e9bd4eec83f73b71121d70
Ranjith S R <ranjith@qualiantech.com>
2016-05-29 10:16:19 +0530

    Fixes issue 33037 : Adding payment "options" Parameter to "onStatusChanged" event
    --HG--
    extra : rebase_source : 8ed938c7f3d5dbb0a1b64113d6ddfe8136b9dfb9

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/modals/modalpayments.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/modals/modalpayments.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2015 Openbravo S.L.U.
+ * Copyright (C) 2015-2016 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -84,7 +84,8 @@ enyo.kind({
                 dialog.bubble('onStatusChanged', {
                   payment: this.payment,
                   status: this.payment.payment.searchKey,
-                  amount: dialog.args.amount
+                  amount: dialog.args.amount,
+                  options: dialog.args.options
                 });
                 dialog.bubble('onPaymentChanged', {
                   payment: this.payment,
