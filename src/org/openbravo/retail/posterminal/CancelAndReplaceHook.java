1f8120a1e1bdcf17083eba0adc5bcea09f0acb9d
Unai Martirena <unai.martirena@openbravo.com>
2016-05-30 16:23:47 +0200

    [Cancel & Replace] Add cancel and replace hook
    --HG--
    extra : rebase_source : 74fa11d9ae70e2bb0a1cedab472659447474fc95

---
+++ src/org/openbravo/retail/posterminal/CancelAndReplaceHook.java
@@ -0,0 +1,23 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2016 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+package org.openbravo.retail.posterminal;
+
+import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.erpCommon.businessUtility.CancelAndReplaceOrderHook;
+import org.openbravo.model.common.order.Order;
+
+public class CancelAndReplaceHook extends CancelAndReplaceOrderHook {
+
+  @Override
+  public void exec(Boolean replaceOrder, Boolean triggersDisabled, Order oldOrder, Order newOrder,
+      Order inverseOrder, JSONObject jsonorder) throws Exception {
+    inverseOrder.setObposAppCashup(jsonorder.getString("obposAppCashup"));
+  }
+}
