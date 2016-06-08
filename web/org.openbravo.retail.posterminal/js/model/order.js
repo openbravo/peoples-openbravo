257842412a689faf9280d05ae4ffcfdadc806dbc
Aaron Calero <aaron.calero@openbravo.com>
2016-06-08 14:59:10 +0200

    [Reverse Payments] Fixed 'Cancel and Replace of undefined' message when loading a C&R receipt
    --HG--
    extra : transplant_source : %CB%86p%84Q%5E%DC%A38%80%BF0%19z%9Eb%AA%C5%99G

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -1002,11 +1002,11 @@
         this.set('replacedorder_documentNo', _order.get('replacedorder_documentNo'));
       }
 
-      if (_order.get('replacedorder')) {
-        this.set('replacedorder', _order.get('replacedorder'));
-      }
       if (_order.get('replacedorder_documentNo')) {
         this.set('replacedorder_documentNo', _order.get('replacedorder_documentNo'));
+      }
+      if (_order.get('replacedorder')) {
+        this.set('replacedorder', _order.get('replacedorder'));
       }
 
       // the idExecution is saved so only this execution of clearWith will check cloningReceipt to false
