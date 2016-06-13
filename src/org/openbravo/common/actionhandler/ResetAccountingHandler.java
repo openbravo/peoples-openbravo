1869ad259c3326b53427044f3cf23c8760e4dab0
Alvaro Ferraz <alvaro.ferraz@openbravo.com>
2016-06-13 10:23:13 +0200

    Related to issue 33063: Code review improvements
    - Client parameter: change selector, add default value and remove selector fields.
    - Organization parameter: change selector and where clause, add default value and remove selector fields.
    - Table parameter: change selector and where clause, change table from ADTable to ADTablePostV and remove selector fields.
    - Remove unnecessary created ad_ref_table and ad_val_rule records.
    - Deactivate and deprecate old Reset Accounting process.
    - Remove recordId parameter and get correctly datefrom and dateto fields in ResetAccountingHandler.java class.
    - Add getLegalOrBusinessOrgsChilds method in ResetAccounting.java class to retrieve every legal and business unit together with their child organizations from selected client in case * organization has been selected.
    - Call getCalendarId for each organization in the loop to avoid errors when adOrgId is '0'.

--- src/org/openbravo/common/actionhandler/ResetAccountingHandler.java
+++ src/org/openbravo/common/actionhandler/ResetAccountingHandler.java
@@ -23,12 +23,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 import org.openbravo.client.application.process.BaseProcessActionHandler;
+import org.openbravo.erpCommon.utility.OBDateUtils;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.financial.ResetAccounting;
 import org.openbravo.service.db.DbUtility;
+import org.openbravo.service.json.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,53 +42,53 @@ public class ResetAccountingHandler extends BaseProcessActionHandler {
   @Override
   protected JSONObject doExecute(Map<String, Object> parameters, String content) {
 
-    JSONObject result = new JSONObject();
-
+    JSONObject result = null;
     try {
+      result = new JSONObject();
       JSONObject request = new JSONObject(content);
       JSONObject params = request.getJSONObject("_params");
 
       String adClientId = params.getString("AD_Client_ID");
       String adOrgId = params.getString("AD_Org_ID");
       String deletePosting = params.getString("DeletePosting");
-      String recordId = params.has("recordId") ? params.getString("recordId") : null;
-      String datefrom = params.getString("datefrom").equals("null") ? ""
-          : params.getString("datefrom");
-      String dateto = params.getString("dateto").equals("null") ? "" : params.getString("dateto");
+      String datefrom = StringUtils.equals(params.getString("datefrom"), "null") ? "" : OBDateUtils
+          .formatDate(JsonUtils.createDateFormat().parse(params.getString("datefrom")));
+      String dateto = StringUtils.equals(params.getString("dateto"), "null") ? "" : OBDateUtils
+          .formatDate(JsonUtils.createDateFormat().parse(params.getString("dateto")));
       JSONArray tableIds = params.getJSONArray("AD_Table_ID");
       List<String> tableIdsList = new ArrayList<String>();
       for (int i = 0; i < tableIds.length(); i++) {
         String tableId = tableIds.getString(i);
         tableIdsList.add(tableId);
       }
+
       HashMap<String, Integer> results = new HashMap<String, Integer>();
-      if ("true".equals(deletePosting)) {
-        results = ResetAccounting.delete(adClientId, adOrgId, tableIdsList, recordId, datefrom,
-            dateto);
+      if (StringUtils.equals(deletePosting, "true")) {
+        results = ResetAccounting.delete(adClientId, adOrgId, tableIdsList, datefrom, dateto);
       } else {
         results = ResetAccounting.restore(adClientId, adOrgId, tableIdsList, datefrom, dateto);
       }
+
       int counter = results.get("updated");
       int counterDeleted = results.get("deleted");
       JSONObject successMessage = new JSONObject();
       successMessage.put("severity", "success");
       StringBuilder message = new StringBuilder();
-      message.append(OBMessageUtils.parseTranslation("@UnpostedDocuments@")).append(" = ")
-          .append(counter).append(", ").append(OBMessageUtils.parseTranslation("@DeletedEntries@"))
-          .append(" = ").append(counterDeleted);
+      message.append(OBMessageUtils.parseTranslation("@UnpostedDocuments@ = " + counter
+          + ", @DeletedEntries@ = " + counterDeleted));
       successMessage.put("text", message);
       result.put("message", successMessage);
+
     } catch (Exception e) {
       log.error("Error in Reset Accounting Action Handler", e);
-
       try {
+        result = new JSONObject();
         Throwable ex = DbUtility.getUnderlyingSQLException(e);
         String message = OBMessageUtils.translateError(ex.getMessage()).getMessage();
         JSONObject errorMessage = new JSONObject();
         errorMessage.put("severity", "error");
         errorMessage.put("text", message);
         result.put("message", errorMessage);
-
       } catch (Exception e2) {
         log.error(e.getMessage(), e2);
       }
