931b53f39868484b409fd55d60b27140215daa51
Asier Martirena <asier.martirena@openbravo.com>
2016-06-30 12:42:23 +0200

    [Cancel and Replace] New event handler to change the documentNo for payments of amount 0. Also compilation problems solved.
    --HG--
    extra : transplant_source : %8A%1F%D0%20J%10%B0%07%9E%B6%1A%19%8B%DD8%C8%B8%E7%8B%FC

---
+++ src/org/openbravo/event/FINPaymentEventHandler.java
@@ -0,0 +1,67 @@
+package org.openbravo.event;
+
+import java.math.BigDecimal;
+
+import javax.enterprise.event.Observes;
+
+import org.apache.log4j.Logger;
+import org.openbravo.base.model.Entity;
+import org.openbravo.base.model.ModelProvider;
+import org.openbravo.base.model.Property;
+import org.openbravo.client.kernel.event.EntityDeleteEvent;
+import org.openbravo.client.kernel.event.EntityNewEvent;
+import org.openbravo.client.kernel.event.EntityPersistenceEventObserver;
+import org.openbravo.client.kernel.event.EntityUpdateEvent;
+import org.openbravo.model.financialmgmt.payment.FIN_Payment;
+
+public class FINPaymentEventHandler extends EntityPersistenceEventObserver {
+  private static Entity[] entities = { ModelProvider.getInstance().getEntity(
+      FIN_Payment.ENTITY_NAME) };
+  protected Logger logger = Logger.getLogger(this.getClass());
+
+  @Override
+  protected Entity[] getObservedEntities() {
+    return entities;
+  }
+
+  public void onUpdate(@Observes EntityUpdateEvent event) {
+    if (!isValidEvent(event)) {
+      return;
+    }
+    FIN_Payment payment = (FIN_Payment) event.getTargetInstance();
+    if (payment.getAmount().compareTo(BigDecimal.ZERO) == 0) {
+      final Entity paymentEntity = ModelProvider.getInstance().getEntity(FIN_Payment.ENTITY_NAME);
+      final Property paymentDocumentNoProperty = paymentEntity
+          .getProperty(FIN_Payment.PROPERTY_DOCUMENTNO);
+      event.setCurrentState(paymentDocumentNoProperty, payment.getDocumentNo() + "*R*");
+    } else {
+      int index = payment.getDocumentNo().indexOf("*R*");
+      if (index != -1) {
+        final Entity paymentEntity = ModelProvider.getInstance().getEntity(FIN_Payment.ENTITY_NAME);
+        final Property paymentDocumentNoProperty = paymentEntity
+            .getProperty(FIN_Payment.PROPERTY_DOCUMENTNO);
+        event.setCurrentState(paymentDocumentNoProperty,
+            payment.getDocumentNo().substring(0, index - 1));
+      }
+    }
+  }
+
+  public void onSave(@Observes EntityNewEvent event) {
+    if (!isValidEvent(event)) {
+      return;
+    }
+    FIN_Payment payment = (FIN_Payment) event.getTargetInstance();
+    if (payment.getAmount().compareTo(BigDecimal.ZERO) == 0) {
+      final Entity paymentEntity = ModelProvider.getInstance().getEntity(FIN_Payment.ENTITY_NAME);
+      final Property paymentDocumentNoProperty = paymentEntity
+          .getProperty(FIN_Payment.PROPERTY_DOCUMENTNO);
+      event.setCurrentState(paymentDocumentNoProperty, payment.getDocumentNo() + "*R*");
+    }
+  }
+
+  public void onDelete(@Observes EntityDeleteEvent event) {
+    if (!isValidEvent(event)) {
+      return;
+    }
+  }
+}
