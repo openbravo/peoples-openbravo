6e552f266c6a9dc1781f55107cb3a1e2cfeb4048
Inigo Sanchez <inigo.sanchez@openbravo.com>
2016-07-13 14:00:35 +0200

    [callout-performance] Added infrastructure for new SimpleCallout implementation

---
+++ src/org/openbravo/erpCommon/ad_callouts/SimpleCalloutConstants.java
@@ -0,0 +1,46 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html 
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License. 
+ * The Original Code is Openbravo ERP. 
+ * The Initial Developer of the Original Code is Openbravo SLU 
+ * All portions are Copyright (C) 2016 Openbravo SLU 
+ * All Rights Reserved. 
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
+ */
+package org.openbravo.erpCommon.ad_callouts;
+
+import org.openbravo.client.application.window.FormInitializationComponent;
+
+/**
+ * Defines constants used for parsing SimpleCallouts response in the
+ * {@link FormInitializationComponent} and {@link SimpleCallout} classes.
+ * 
+ * @author inigo.sanchez
+ */
+public class SimpleCalloutConstants {
+
+  public static final String SC_COLUMN_VALUES = "columnValues";
+  public static final String SC_HIDDEN_INPUTS = "hiddenInputs";
+  public static final String SC_OVERWRITTEN_AUXILIARY = "overwrittenAuxiliaryInputs";
+  public static final String SC_DYNAMIC_COLS = "dynamicCols";
+  public static final String SC_CHANGED_COLS = "changedCols";
+  public static final String SC_RESULT_CALLOUT = "result";
+  public static final String SC_INP_FIELDS = "inpFields";
+  public static final String SC_TAB = "tab";
+
+  public static final String VALUE = "value";
+  public static final String CLASSIC_VALUE = "classicValue";
+  public static final String ENTRIES = "entries";
+
+  public static final String SC_JSEXECUTE = "JSEXECUTE";
+  public static final String SC_EXECUTE = "EXECUTE";
+}
