80033431e7f6a4185522851f0dc4669a017d77d3
Martin Taal <martin.taal@openbravo.com>
2016-08-15 19:01:22 +0200

    Fixes issue 33701: NPE in POSImportEntryProcessor when the posterminal json property has an illegal value If no posterminal id can be found then still create the import entry but with status error. Also use static finals were appropriate. The changeset for issue 33555 has been included in the commit as it seemed better to keep than to rework the code to a new version.

--- src/org/openbravo/retail/posterminal/importprocess/POSImportEntryProcessor.java
+++ src/org/openbravo/retail/posterminal/importprocess/POSImportEntryProcessor.java
@@ -13,6 +13,8 @@ import java.util.List;
 
 import javax.enterprise.event.Observes;
 
+import org.apache.log4j.Logger;
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.openbravo.base.exception.OBException;
@@ -23,6 +25,7 @@ import org.openbravo.client.kernel.event.EntityNewEvent;
 import org.openbravo.client.kernel.event.EntityPersistenceEventObserver;
 import org.openbravo.dal.service.OBDal;
 import org.openbravo.mobile.core.servercontroller.SynchronizedServerProcessCaller;
+import org.openbravo.model.common.enterprise.Organization;
 import org.openbravo.retail.posterminal.OBPOSApplications;
 import org.openbravo.service.importprocess.ImportEntry;
 import org.openbravo.service.importprocess.ImportProcessUtils;
@@ -34,8 +37,20 @@ import org.openbravo.service.importprocess.ImportProcessUtils;
  */
 public class POSImportEntryProcessor extends EntityPersistenceEventObserver {
 
-  private static Entity[] ENTITIES = { ModelProvider.getInstance().getEntity(
-      ImportEntry.ENTITY_NAME) };
+  private static final Logger log = Logger.getLogger(POSImportEntryProcessor.class);
+
+  private static final Entity IMPORT_ENTRY_ENTITY = ModelProvider.getInstance().getEntity(
+      ImportEntry.ENTITY_NAME);
+
+  private static final Entity[] ENTITIES = { IMPORT_ENTRY_ENTITY };
+  private static final Property STATUSPROPERTY = IMPORT_ENTRY_ENTITY
+      .getProperty(ImportEntry.PROPERTY_IMPORTSTATUS);
+  private static final Property ERRORINFO_PROPERTY = IMPORT_ENTRY_ENTITY
+      .getProperty(ImportEntry.PROPERTY_ERRORINFO);
+  private static final Property POSTERMINAL_PROPERTY = IMPORT_ENTRY_ENTITY
+      .getProperty(ImportEntry.PROPERTY_OBPOSPOSTERMINAL);
+  private static final Property ORGANIZATION_PROPERTY = IMPORT_ENTRY_ENTITY
+      .getProperty(ImportEntry.PROPERTY_ORGANIZATION);
 
   private static List<String> POSTTYPEOFDATA = Arrays.asList("Order", "BusinessPartner",
       "BusinessPartnerLocation", "OBPOS_App_Cashup", "FIN_Finacc_Transaction",
@@ -55,34 +70,60 @@ public class POSImportEntryProcessor extends EntityPersistenceEventObserver {
     final ImportEntry importEntry = (ImportEntry) event.getTargetInstance();
 
     try {
-      if (POSTTYPEOFDATA.contains(importEntry.getTypeofdata())) {
-        JSONObject jsonObject = new JSONObject(importEntry.getJsonInfo());
+      if (!POSTTYPEOFDATA.contains(importEntry.getTypeofdata())) {
+        return;
+      }
+      JSONObject jsonObject = new JSONObject(importEntry.getJsonInfo());
 
-        // TODO: using 2 different ways of writing posTerminal is just not nice...
-        String posTerminalId = ImportProcessUtils.getJSONProperty(jsonObject, "posterminal");
-        if (posTerminalId == null) {
-          posTerminalId = ImportProcessUtils.getJSONProperty(jsonObject, "posTerminal");
-        }
-        if (posTerminalId == null) {
-          posTerminalId = ImportProcessUtils.getJSONProperty(jsonObject, "pos");
-        }
-        if (posTerminalId != null) {
-          OBPOSApplications posTerminal = OBDal.getInstance().get(OBPOSApplications.class,
-              posTerminalId);
-          final Entity importEntryEntity = ModelProvider.getInstance().getEntity(
-              ImportEntry.ENTITY_NAME);
+      // TODO: using 2 different ways of writing posTerminal is just not nice...
+      String posTerminalId = ImportProcessUtils.getJSONProperty(jsonObject, "posterminal");
+      if (posTerminalId == null) {
+        posTerminalId = ImportProcessUtils.getJSONProperty(jsonObject, "posTerminal");
+      }
+      if (posTerminalId == null) {
+        posTerminalId = ImportProcessUtils.getJSONProperty(jsonObject, "pos");
+      }
+      // handle special case that webpos can send "null" in case of coding error in webpos
+      // handle it here
+      if (posTerminalId == null || "null".equals(posTerminalId)) {
+        log.warn("No posterminal can be determined from json " + jsonObject);
+        event.setCurrentState(STATUSPROPERTY, "Error");
+        event.setCurrentState(ERRORINFO_PROPERTY, "No posterminal can be determined from the json");
+        return;
+      }
 
-          final Property posTerminalProperty = importEntryEntity
-              .getProperty(ImportEntry.PROPERTY_OBPOSPOSTERMINAL);
-          event.setCurrentState(posTerminalProperty, posTerminal);
+      // as we are loading proxy the posTerminal object will always be not null
+      // even in case of an invalid id, therefore the check on "null" above
+      final OBPOSApplications posTerminal = OBDal.getInstance().getProxy(OBPOSApplications.class,
+          posTerminalId);
+      event.setCurrentState(POSTERMINAL_PROPERTY, posTerminal);
 
-          final Property orgProperty = importEntryEntity
-              .getProperty(ImportEntry.PROPERTY_ORGANIZATION);
-          event.setCurrentState(orgProperty, posTerminal.getOrganization());
+      // determine the organization without reading the pos terminal
+      // at the moment only specific structured json is supported
+      Organization organization = null;
+      final JSONObject content = new JSONObject(importEntry.getJsonInfo());
+      if (content.has("organization") && content.get("organization") instanceof String) {
+        organization = OBDal.getInstance().getProxy(Organization.class,
+            content.getString("organization"));
+      } else if (content.has("data") && content.get("data") instanceof JSONArray) {
+        final JSONArray data = content.getJSONArray("data");
+        if (data.length() > 0 && data.get(0) instanceof JSONObject) {
+          final JSONObject json = data.getJSONObject(0);
+          if (json.has("organization") && json.get("organization") instanceof String) {
+            organization = OBDal.getInstance().getProxy(Organization.class,
+                json.getString("organization"));
+          }
         }
       }
+      // not found read it from the posterminal which will get loaded
+      if (organization == null) {
+        log.warn("Not possible to determine organization from json, reading the org from posterminal "
+            + content);
+        organization = posTerminal.getOrganization();
+      }
+      event.setCurrentState(ORGANIZATION_PROPERTY, organization);
     } catch (JSONException e) {
-      throw new OBException(e);
+      throw new OBException("Exception occurred " + importEntry.getJsonInfo(), e);
     }
   }
 }
