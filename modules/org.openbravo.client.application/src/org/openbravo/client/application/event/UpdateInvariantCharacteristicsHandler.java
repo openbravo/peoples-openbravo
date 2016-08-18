9794ae0cecf340276265ff13a71052b124441ca3
Naroa Iriarte <naroa.iriarte@openbravo.com>
2016-08-18 10:58:26 +0200

    Fixed issue 33639: Update Characteristic was not showing the correct data
    If there was a product with a subset of a product characteristic, the update characteristic process was not showing the correct data. Instead of showing the products of the subset of the product characteristic, it was showing every product of the product characteristic.
    To fix this, the where clause of the selector reference has been changed, making it more restrictive in the case of having a subset.
    
    --HG--
    extra : rebase_source : 19dad6d4bd2ed8b136182e52faed468ef895e783

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/event/UpdateInvariantCharacteristicsHandler.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/event/UpdateInvariantCharacteristicsHandler.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2013-2014 Openbravo SLU
+ * All portions are Copyright (C) 2013-2016 Openbravo SLU
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -115,6 +115,12 @@ public class UpdateInvariantCharacteristicsHandler extends BaseActionHandler {
           }
           productChar.put("valueMap", productCharValuesValueMap);
           productCharArray.put(productChar);
+          if (characteristic.getCharacteristicSubset() != null) {
+            productChar
+                .put("productCharSubsetId", characteristic.getCharacteristicSubset().getId());
+          } else {
+            productChar.put("productCharSubsetId", "-1");
+          }
         }
         response.put("productCharList", productCharArray);
         response.put("productId", productId);
