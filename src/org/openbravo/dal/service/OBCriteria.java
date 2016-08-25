6aaefa4097805d7d472340d5c210142bf9b85576
Carlos Aristu <carlos.aristu@openbravo.com>
2016-08-25 16:06:43 +0200

    fixes issue 33814: backed out initialization prevention changes
    --HG--
    extra : rebase_source : c61f3fc6c8e9f8a5b109980374356ba0ebf63461

--- src/org/openbravo/dal/service/OBCriteria.java
+++ src/org/openbravo/dal/service/OBCriteria.java
@@ -68,7 +68,6 @@ public class OBCriteria<E extends BaseOBObject> extends CriteriaImpl {
   private boolean filterOnReadableOrganization = true;
   private boolean filterOnActive = true;
   private List<OrderBy> orderBys = new ArrayList<OrderBy>();
-  private boolean initialized = false;
 
   // package visible
 
@@ -137,9 +136,6 @@ public class OBCriteria<E extends BaseOBObject> extends CriteriaImpl {
   }
 
   void initialize() {
-    if (initialized) {
-      return;
-    }
     final OBContext obContext = OBContext.getOBContext();
     final Entity e = getEntity();
 
@@ -184,7 +180,6 @@ public class OBCriteria<E extends BaseOBObject> extends CriteriaImpl {
     if (SessionInfo.getQueryProfile() != null) {
       QueryTimeOutUtil.getInstance().setQueryTimeOut(this, SessionInfo.getQueryProfile());
     }
-    initialized = true;
   }
 
   /**
