967a6337a0a5567faae386d483f0a3102208524b
Naroa Iriarte <naroa.iriarte@openbravo.com>
2016-09-15 16:23:04 +0200

    [pi-accessibility-improvements] Fixed a condition in ob-alert-grid.js

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/alert-management/ob-alert-grid.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/alert-management/ob-alert-grid.js
@@ -139,13 +139,13 @@ isc.OBAlertGrid.addProperties({
     this.Super('destroy', arguments);
   },
 
-  isFullReaderPreferenceEnabled: function () {
-    return (OB.Properties.EnableFullScreenReader && OB.Properties.EnableFullScreenReader === 'Y');
+  isFullScreenReaderPreferenceEnabled: function () {
+    return OB.Properties.EnableFullScreenReader && OB.Properties.EnableFullScreenReader === 'Y';
   },
 
   setDataSource: function (ds) {
     var i;
-    if (this.isFullReaderPreferenceEnabled) {
+    if (this.isFullScreenReaderPreferenceEnabled()) {
       for (i = 0; i < this.gridFields.length; i++) {
         this.gridFields[i].filterEditorProperties = {
           title: OB.I18N.getLabel('OBUIAPP_Filter_By_Column') + this.gridFields[i].title
