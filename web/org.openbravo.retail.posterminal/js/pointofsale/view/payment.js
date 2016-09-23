800f5708d128b01deb73983dae1c7e789bbe2618
Miguel de Juana <miguel.dejuana@openbravo.com>
2016-09-23 09:52:40 +0200

    Fixed issue 0034052: It is still possible to press twice Done or Layaway buttons
    - Block Done button during 1 second to avoid double click
    - Delete current before setting to false calculateReceiptLock in order to do not trigger any receipt event when clearing the order
    - Avoid calling updatePending or checkValidPayment when we are processing the receipt (after pressing Done button)
    - Change message from info to error to be able to track it better

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/payment.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/payment.js
@@ -84,7 +84,7 @@ enyo.kind({
       } else if (!_.isNull(pending) && pending) {
         this.setTotalPending(pending, payment.mulrate, payment.symbol, payment.currencySymbolAtTheRight, inSender, inEvent);
       }
-      if (paymentstatus && inEvent.value.status !== "") {
+      if (paymentstatus && inEvent.value.status !== "" && !this.receipt.isCalculateReceiptLocked && !this.receipt.isCalculateGrossLocked) {
         this.checkValidPayments(paymentstatus, payment);
       }
       if (inEvent.value.amount) {
@@ -247,6 +247,10 @@ enyo.kind({
     this.$.payments.setCollection(this.receipt.get('payments'));
     this.$.multiPayments.setCollection(this.model.get('multiOrders').get('payments'));
     this.receipt.on('change:payment change:change calculategross change:bp change:gross', function () {
+      if (this.receipt.isCalculateReceiptLocked || this.receipt.isCalculateGrossLocked) {
+        //We are processing the receipt, we cannot update pending yet
+        return;
+      }
       this.updatePending();
     }, this);
     this.model.get('leftColumnViewManager').on('change:currentView', function () {
@@ -966,15 +970,32 @@ enyo.kind({
       this.setDisabled(false);
     }, this);
   },
+  blocked: false,
   tap: function () {
     var myModel = this.owner.model,
         me = this,
         payments, avoidPayment = false,
         orderDesc = '';
+    //*** Avoid double click ***
+    if (this.getContent() === OB.I18N.getLabel('OBPOS_LblDone')) {
+      if (me.blocked) {
+        if (this.owner.receipt && this.owner.receipt.getOrderDescription) {
+          orderDesc = this.owner.receipt.getOrderDescription();
+        }
+        OB.error('Time: ' + new Date() + '. Done button has been pressed 2 times and second execution is discarded ' + orderDesc);
+        return;
+      } else {
+        me.blocked = true;
+        setTimeout(function () {
+          me.blocked = false;
+        }, 1000);
+      }
+    }
+
     if (this && this.owner && this.owner.receipt && this.owner.receipt.getOrderDescription) {
       orderDesc = this.owner.receipt.getOrderDescription();
     }
-    OB.info('Payment Button Pressed ( Status: ' + this.disabled + ') ' + orderDesc);
+    OB.info('Time: ' + new Date() + '. Payment Button Pressed ( Status: ' + this.disabled + ') ' + orderDesc);
 
 
     this.allowOpenDrawer = false;
