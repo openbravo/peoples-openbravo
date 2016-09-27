ae1e6d522c467ac12d150b37116f6f9846b98719
Inigo Sanchez <inigo.sanchez@openbravo.com>
2016-09-27 16:32:05 +0200

    Fixes issue 34033: Removed unnecessary check in manageComboData method.

--- src/org/openbravo/erpCommon/ad_callouts/SimpleCalloutInformationProvider.java
+++ src/org/openbravo/erpCommon/ad_callouts/SimpleCalloutInformationProvider.java
@@ -89,8 +89,8 @@ public class SimpleCalloutInformationProvider implements CalloutInformationProvi
   @Override
   public Boolean isComboData(Object element) {
     if (element instanceof JSONObject) {
-      JSONObject json = (JSONObject) element;
-      return json.has(CalloutConstants.ENTRIES);
+      JSONObject comboElement = (JSONObject) element;
+      return comboElement.has(CalloutConstants.ENTRIES);
     }
     return false;
   }
@@ -143,8 +143,6 @@ public class SimpleCalloutInformationProvider implements CalloutInformationProvi
       changedCols.add(col.getDBColumnName());
     }
 
-    if (entry.has(CalloutConstants.ENTRIES)) {
-      selectedEntry.put(CalloutConstants.ENTRIES, newJsonArr);
-    }
+    selectedEntry.put(CalloutConstants.ENTRIES, newJsonArr);
   }
 }
