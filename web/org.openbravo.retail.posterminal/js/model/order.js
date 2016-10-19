e60c0aa5f2850a1032777ff196849032df1ad971
Jorge Garcia <jorge.garcia@openbravo.com>
2016-10-19 15:49:11 +0200

    Fixed issue 34227: It is not possible to do a logout if you have a ticket with all lines deleted
    The problem was to implement different actions for two cases almost
    equals.
    
    The solution is to implement almost the same actions even if the
    receipt has all the lines deleted.
    
    --HG--
    extra : rebase_source : 379c68381797b08cfdf5e5eeb111186b2419484b

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -3886,6 +3886,7 @@
         model.set('created', creationDate.getTime());
         model.set('obposCreatedabsolute', OB.I18N.formatDateISO(creationDate));
         model.set('obposIsDeleted', true);
+        model.set('obposAppCashup', OB.MobileApp.model.get('terminal').cashUpId);
         for (i = 0; i < model.get('lines').length; i++) {
           model.get('lines').at(i).set('obposIsDeleted', true);
         }
@@ -3940,20 +3941,10 @@
           }
         } else if (receipt.has('deletedLines')) {
           if (OB.MobileApp.model.hasPermission('OBPOS_remove_ticket', true)) {
-            receipt.setIsCalculateGrossLockState(true);
-            receipt.set('obposIsDeleted', true);
-            receipt.prepareToSend(function () {
-              receipt.trigger('closed', {
-                callback: function () {
-                  orderList.deleteCurrent();
-                  orderList.synchronizeCurrentOrder();
-                  receipt.setIsCalculateGrossLockState(false);
-                  if (callback && callback instanceof Function) {
-                    callback();
-                  }
-                }
-              });
-            });
+            receipt.set('skipCalculateReceipt', false);
+            receipt.setIsCalculateReceiptLockState(false);
+            receipt.setIsCalculateGrossLockState(false);
+            markOrderAsDeleted(receipt, orderList, callback);
           } else {
             orderList.saveCurrent();
             OB.Dal.remove(orderList.current, null, null);
@@ -4545,7 +4536,7 @@
         me.loadCurrent(createNew);
       }
 
-      if (OB.MobileApp.model.hasPermission('OBPOS_remove_ticket', true) && !this.current.get('isQuotation') && OB.MobileApp.model.receipt.id === this.current.id && this.current.get('lines').length === 0 && (this.current.get('documentnoSuffix') <= OB.MobileApp.model.documentnoThreshold || OB.MobileApp.model.documentnoThreshold === 0)) {
+      if (OB.MobileApp.model.hasPermission('OBPOS_remove_ticket', true) && !this.current.get('isQuotation') && OB.MobileApp.model.receipt.id === this.current.id && this.current.get('lines').length === 0 && !this.current.has('deletedLines') && (this.current.get('documentnoSuffix') <= OB.MobileApp.model.documentnoThreshold || OB.MobileApp.model.documentnoThreshold === 0)) {
         OB.MobileApp.model.receipt.setIsCalculateGrossLockState(true);
         OB.MobileApp.model.receipt.set('obposIsDeleted', true);
         OB.MobileApp.model.receipt.prepareToSend(function () {
