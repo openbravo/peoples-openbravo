d98083c1781209ff25ed69873556c74e08e4bc14
Miguel de Juana <miguel.dejuana@openbravo.com>
2016-10-20 17:56:02 +0200

    Fixed issue 	0034254: Import entries need to be executed in time order (by terminal)
    - We have decided to process imports filtering by terminal in sent order, without restrictions between models. We will process imports in the same order that actions were done in Web POS.
    
    --HG--
    extra : rebase_source : 5f7e0fd1bb881cf595aabdf28075397307e2d58e

--- src/org/openbravo/retail/posterminal/importprocess/OrderImportEntryProcessor.java
+++ src/org/openbravo/retail/posterminal/importprocess/OrderImportEntryProcessor.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2015 Openbravo S.L.U.
+ * Copyright (C) 2015-2016 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -10,7 +10,6 @@ package org.openbravo.retail.posterminal.importprocess;
 
 import javax.enterprise.context.ApplicationScoped;
 
-import org.hibernate.Query;
 import org.openbravo.base.exception.OBException;
 import org.openbravo.base.weld.WeldUtils;
 import org.openbravo.dal.core.OBContext;
@@ -62,34 +61,17 @@ public class OrderImportEntryProcessor extends ImportEntryProcessor {
       try {
         OBContext.setAdminMode(false);
 
-        if (0 < countEntries("Error", importEntry)) {
+        if (0 < POSImportEntryProcessor.countEntries("Error", importEntry)) {
           // if there are related error entries before this one then this is an error
           // throw an exception to move this entry also to error status
           throw new OBException("There are error records before this record " + importEntry
               + ", moving this entry also to error status.");
         }
 
-        return 0 < countEntries("Initial", importEntry);
+        return 0 < POSImportEntryProcessor.countEntries("Initial", importEntry);
       } finally {
         OBContext.restorePreviousMode();
       }
-    }
-
-    private int countEntries(String importStatus, ImportEntry importEntry) {
-      final String whereClause = ImportEntry.PROPERTY_IMPORTSTATUS + "='" + importStatus
-          + "' and (" + ImportEntry.PROPERTY_TYPEOFDATA + "='BusinessPartner' or "
-          + ImportEntry.PROPERTY_TYPEOFDATA + "='BusinessPartnerLocation') and "
-          + ImportEntry.PROPERTY_CREATIONDATE + "<=:creationDate and "
-          + ImportEntry.PROPERTY_CREATEDTIMESTAMP + "<:createdtimestamp and "
-          + ImportEntry.PROPERTY_OBPOSPOSTERMINAL + "=:terminal and id!=:id";
-      final Query qry = OBDal.getInstance().getSession()
-          .createQuery("select 1 from " + ImportEntry.ENTITY_NAME + " where " + whereClause);
-      qry.setParameter("id", importEntry.getId());
-      qry.setTimestamp("creationDate", importEntry.getCreationDate());
-      qry.setParameter("terminal", importEntry.getOBPOSPOSTerminal());
-      qry.setParameter("createdtimestamp", importEntry.getCreatedtimestamp());
-      qry.setMaxResults(1);
-      return qry.list().size();
     }
   }
 
