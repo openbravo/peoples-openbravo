572e16dcbe0647b31a4cbca24b87811c1b2de454
Christian Goni <christian.goni@openbravo.com>
2016-11-03 11:11:37 +0100

    related to RFID: fixed some UI problems
    --HG--
    extra : rebase_source : 34e7bc51a04bb2a21aed39fa10656e5883a034b4

--- web/org.openbravo.retail.posterminal/js/components/menu.js
+++ web/org.openbravo.retail.posterminal/js/components/menu.js
@@ -1022,7 +1022,7 @@ enyo.kind({
   kind: 'OB.UI.MenuAction',
   permission: 'OBPOS_retail.disableEnableRFIDReader',
   i18nLabel: 'OBPOS_RFID',
-  classes: 'menu-switch btn-icon-switchoffline',
+  classes: 'menu-switch',
   handlers: {
     onPointOfSaleLoad: 'pointOfSaleLoad'
   },
@@ -1088,6 +1088,9 @@ enyo.kind({
     if (OB.UTIL.RfidController.isRfidConfigured()) {
       var protocol = OB.POS.hwserver.url.split('/')[0];
       if (window.location.protocol === protocol) {
+        if (OB.UTIL.RfidController.get('connectionLost')) {
+          this.addClass('btn-icon-switchoffline');
+        }
         if (!OB.UTIL.RfidController.get('isRFIDEnabled') || !OB.UTIL.RfidController.get('reconnectOnScanningFocus')) {
           this.addClass('btn-icon-switchoff');
         } else {
