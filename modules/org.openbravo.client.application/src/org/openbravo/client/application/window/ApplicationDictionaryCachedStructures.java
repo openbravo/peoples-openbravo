d8b8e9af8d27fcbbce18c48fa766f290ca3781e8
Asier Lostalé <asier.lostale@openbravo.com>
2016-11-09 16:33:22 +0100

    fixed bug 34454: can't add attachment to Invoices after print and attach
      When trying to add a new attachemnt just after attaching a printed document,
      a LazyInitializationException was thrown because ADCS cache parameter object
      was partially initialized. Programmatic attachement API initialized the parameters
      but not their translation lists.
    
      Fixed by initializating translation lists for parameters whenever it is added
      to ADCS.
    
    --HG--
    extra : transplant_source : %C7g%CF%BD%08%B6%01%BCv%3AK%1E%07%EF%F9%CB%F6%B7%E7%EB

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/window/ApplicationDictionaryCachedStructures.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/window/ApplicationDictionaryCachedStructures.java
@@ -326,6 +326,10 @@ public class ApplicationDictionaryCachedStructures implements Serializable {
 
   private void initializeMetadata(Parameter metadata) {
     initializeDALObject(metadata);
+    if (metadata.getApplicationElement() != null) {
+      initializeDALObject(metadata.getApplicationElement().getADElementTrlList());
+    }
+
     if (metadata.getReference() != null) {
       initializeDALObject(metadata.getReference());
       initializeReference(metadata.getReference());
