dafe4a348bf570742edac060b1a4fbd9e0aa4a6e
Alvaro Ferraz <alvaro.ferraz@openbravo.com>
2016-12-13 12:48:49 +0100

    Related to issue 34699: Code review improvements
    Use ad_isorgincluded instead of ad_org_isinnaturaltree.
    
    --HG--
    extra : rebase_source : 8f1fc5daaec52b75ed0cca1bbee46d5639ba49c7

--- src/org/openbravo/erpCommon/ad_reports/ReportValuationStock.java
+++ src/org/openbravo/erpCommon/ad_reports/ReportValuationStock.java
@@ -90,9 +90,9 @@ public class ReportValuationStock extends HttpSecureAppServlet {
       String strDate = vars.getGlobalVariable("inpDate", "ReportValuationStock|Date",
           DateTimeData.today(this));
       String strOrganization = vars.getGlobalVariable("inpOrg",
-          "ReportValuationStock|Organization", vars.getOrg(), IsIDFilter.instance);
+          "ReportValuationStock|Organization", "", IsIDFilter.instance);
       String strWarehouse = vars.getGlobalVariable("inpmWarehouseId",
-          "ReportValuationStock|Warehouse", vars.getWarehouse(), IsIDFilter.instance);
+          "ReportValuationStock|Warehouse", "", IsIDFilter.instance);
       String strCategoryProduct = vars.getGlobalVariable("inpCategoryProduct",
           "ReportValuationStock|CategoryProduct", "", IsIDFilter.instance);
       String strCurrencyId = OBCurrencyUtils.getOrgCurrency(vars.getOrg());
