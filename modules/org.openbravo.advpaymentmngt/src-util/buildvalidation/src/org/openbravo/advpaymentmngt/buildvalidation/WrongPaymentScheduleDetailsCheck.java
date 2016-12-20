7aef010e6e285f7778c6838092d7cc30320d15d5
Sanjota <sanjota.nelagi@promantia.com>
2016-12-20 10:51:01 +0530

    Fixes issue 34718:Modify WrongPaymentScheduleDetailsCheck.java buildValidation
    Modify WrongPaymentScheduleDetailsCheck.java buildValidation to execute it only if updating from before this changeset:
    https://code.openbravo.com/erp/devel/main/rev/11602bd040b1

--- modules/org.openbravo.advpaymentmngt/src-util/buildvalidation/src/org/openbravo/advpaymentmngt/buildvalidation/WrongPaymentScheduleDetailsCheck.java
+++ modules/org.openbravo.advpaymentmngt/src-util/buildvalidation/src/org/openbravo/advpaymentmngt/buildvalidation/WrongPaymentScheduleDetailsCheck.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2011 Openbravo SLU
+ * All portions are Copyright (C) 2011-2016 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -21,8 +21,10 @@ package org.openbravo.advpaymentmngt.buildvalidation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openbravo.base.ExecutionLimits;
 import org.openbravo.buildvalidation.BuildValidation;
 import org.openbravo.database.ConnectionProvider;
+import org.openbravo.modulescript.OpenbravoVersion;
 
 public class WrongPaymentScheduleDetailsCheck extends BuildValidation {
 
@@ -89,4 +91,11 @@ public class WrongPaymentScheduleDetailsCheck extends BuildValidation {
 
     }
   }
+
+  @Override
+  protected ExecutionLimits getBuildValidationLimits() {
+    return new ExecutionLimits("A918E3331C404B889D69AA9BFAFB23AC", null, new OpenbravoVersion(3, 0,
+        22964));
+  }
 }
+
