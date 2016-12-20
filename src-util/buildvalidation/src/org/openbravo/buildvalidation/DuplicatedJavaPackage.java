1409f6c2ce52d754340c83936a2613713a55dbed
Sanjota <sanjota.nelagi@promantia.com>
2016-12-20 10:42:14 +0530

    Fixes issue 34718 :Modify DuplicatedJavaPackage buildValidation
    Modify DuplicatedJavaPackage buildValidation to execute it only if updating from before this changeset:
    https://code.openbravo.com/erp/devel/main/rev/be6925f7d4e7

--- src-util/buildvalidation/src/org/openbravo/buildvalidation/DuplicatedJavaPackage.java
+++ src-util/buildvalidation/src/org/openbravo/buildvalidation/DuplicatedJavaPackage.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010 Openbravo SLU
+ * All portions are Copyright (C) 2010-2016 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -21,7 +21,9 @@ package org.openbravo.buildvalidation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openbravo.base.ExecutionLimits;
 import org.openbravo.database.ConnectionProvider;
+import org.openbravo.modulescript.OpenbravoVersion;
 
 /**
  * This validation is related to this issue 14143. A unique constraint has been added to
@@ -50,5 +52,10 @@ public class DuplicatedJavaPackage extends BuildValidation {
       return handleError(e);
     }
     return errors;
+  }
+  
+  @Override
+  protected ExecutionLimits getBuildValidationLimits() {
+    return new ExecutionLimits("0", null, new OpenbravoVersion(2, 50, 18132));
   }
 }
