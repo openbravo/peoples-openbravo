279b10d56bc22739becfb524091c43345ed5d524
Adrián Romero <adrianromero@openbravo.com>
2016-12-22 17:27:43 +0100

    [Monitor] Add button in POS Terminal to open hardware URL page

---
+++ web/org.openbravo.retail.posterminal/js/utils/openhardwareurl.js
@@ -0,0 +1,33 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2016 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+/*global OB */
+
+OB.POS = window.OB.POS || {};
+
+OB.POS.openHardwareURL = function (data, view) {
+
+  var grid = view.view.viewGrid;
+  var selectedRecords = grid.getSelectedRecords();
+  var i;
+  var url;
+  var parser;
+
+  if (selectedRecords.length > 0) {
+    isc.confirm(OB.I18N.getLabel('OBPOS_OpenHardwareURLConfirmation'), function (value) {
+      if (value !== null && value) {
+        for (i = 0; i < selectedRecords.length; i++) {
+          parser = document.createElement('a');
+          parser.href = selectedRecords[i].hardwareurl;
+          window.open(parser.protocol + '//' + parser.host);
+        }
+      }
+    });
+  }
+};
