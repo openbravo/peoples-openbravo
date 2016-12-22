2f3acd3138d562bd596f1edfcf365b5de36ffbb0
Adrián Romero <adrianromero@openbravo.com>
2016-12-22 18:10:00 +0100

    [Monitor] Add button in Hardware URL to open hardware URL page

--- web/org.openbravo.retail.posterminal/js/utils/openhardwareurl.js
+++ web/org.openbravo.retail.posterminal/js/utils/openhardwareurl.js
@@ -9,25 +9,37 @@
 
 /*global OB */
 
-OB.POS = window.OB.POS || {};
 
-OB.POS.openHardwareURL = function (data, view) {
+(function () {
+  OB.POS = window.OB.POS || {};
 
-  var grid = view.view.viewGrid;
-  var selectedRecords = grid.getSelectedRecords();
-  var i;
-  var url;
-  var parser;
+  function openURL(data, view, field) {
 
-  if (selectedRecords.length > 0) {
-    isc.confirm(OB.I18N.getLabel('OBPOS_OpenHardwareURLConfirmation'), function (value) {
-      if (value !== null && value) {
-        for (i = 0; i < selectedRecords.length; i++) {
-          parser = document.createElement('a');
-          parser.href = selectedRecords[i].hardwareurl;
-          window.open(parser.protocol + '//' + parser.host);
+    var grid = view.view.viewGrid;
+    var selectedRecords = grid.getSelectedRecords();
+    var i;
+    var url;
+    var parser;
+
+    if (selectedRecords.length > 0) {
+      isc.confirm(OB.I18N.getLabel('OBPOS_OpenHardwareURLConfirmation'), function (value) {
+        if (value !== null && value) {
+          for (i = 0; i < selectedRecords.length; i++) {
+            parser = document.createElement('a');
+            parser.href = selectedRecords[i][field];
+            window.open(parser.protocol + '//' + parser.host);
+          }
         }
-      }
-    });
+      });
+    }
   }
-};
+
+  OB.POS.openTerminalHardwareURL = function (data, view) {
+    openURL(data, view, 'hardwareurl');
+  };
+
+  OB.POS.openHardwareURL = function (data, view) {
+    openURL(data, view, 'hardwareURL');
+  };
+
+}());
