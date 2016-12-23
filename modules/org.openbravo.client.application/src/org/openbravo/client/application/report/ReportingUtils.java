a569383242f941246794ee1b1407333161156e6b
Carlos Aristu <carlos.aristu@openbravo.com>
2016-12-23 13:28:44 +0100

    [multi-pool] Use OBDal read-only instance when needed in the reporting engine

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/report/ReportingUtils.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/report/ReportingUtils.java
@@ -976,8 +976,8 @@ public class ReportingUtils {
             }
           }
         } else {
-          jasperPrint = JasperFillManager.fillReport(jReport, parameters, OBDal.getInstance()
-              .getConnection());
+          jasperPrint = JasperFillManager.fillReport(jReport, parameters, OBDal
+              .getReadOnlyInstance().getConnection());
         }
       } else {
         jasperPrint = JasperFillManager.fillReport(jasperFilePath, parameters);
@@ -1219,7 +1219,7 @@ public class ReportingUtils {
    *          Map of parameters where the standard process definition parameters are added.
    */
   private static void addProcessDefinitionParameters(Map<String, Object> parameters) {
-    parameters.put(JASPER_PARAM_HBSESSION, OBDal.getInstance().getSession());
+    parameters.put(JASPER_PARAM_HBSESSION, OBDal.getReadOnlyInstance().getSession());
     parameters.put(JASPER_PARAM_OBCONTEXT, OBContext.getOBContext());
 
     {
@@ -1372,7 +1372,7 @@ public class ReportingUtils {
       this.extension = extension;
       OBContext.setAdminMode(true);
       try {
-        FileType type = OBDal.getInstance().get(FileType.class, strFileTypeId);
+        FileType type = OBDal.getReadOnlyInstance().get(FileType.class, strFileTypeId);
         if (type != null) {
           fileType = type.getFormat();
         } else {
