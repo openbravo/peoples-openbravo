e9bc0849d74a748d9eb6d73ed5f223d4841c7cda
Mark <markmm82@gmail.com>
2017-01-09 14:36:47 -0500

    Related to issue 34620: Backed out changeset 6edf67f27c25
    --HG--
    extra : rebase_source : 6a4e1236a46668b3e131ed50e29fda331d3cd0a9

--- src/org/openbravo/erpCommon/ad_reports/ReportWarehouseControl.java
+++ src/org/openbravo/erpCommon/ad_reports/ReportWarehouseControl.java
@@ -71,23 +71,11 @@ public class ReportWarehouseControl extends HttpSecureAppServlet {
     PrintWriter out = response.getWriter();
     XmlDocument xmlDocument = null;
 
-    ReportWarehouseControlData[] data = null;
-    int limit = 0;
-    int countLines = 0;
-    if(!vars.commandIn("DEFAULT")){
-      limit = Integer.parseInt(Utility.getPreference(vars, "ReportsLimit", ""));
-      if(limit > 0){
-        countLines = Integer.valueOf(ReportWarehouseControlData.countLines(this,
-          Utility.getContext(this, vars, "#User_Client", "ReportWarehouseControl"),
-          Utility.getContext(this, vars, "#AccessibleOrgTree", "ReportWarehouseControl"),
-            strDateFrom, DateTimeData.nDaysAfter(this, strDateTo, "1"), strReferential));
-      }
-      data = ReportWarehouseControlData.select(this,
+    ReportWarehouseControlData[] data = ReportWarehouseControlData.select(this,
         Utility.getContext(this, vars, "#User_Client", "ReportWarehouseControl"),
         Utility.getContext(this, vars, "#AccessibleOrgTree", "ReportWarehouseControl"),
-        strDateFrom, DateTimeData.nDaysAfter(this, strDateTo, "1"), strReferential, 0, limit);
-    }
-    
+        strDateFrom, DateTimeData.nDaysAfter(this, strDateTo, "1"), strReferential);
+
     if (data == null || data.length == 0 || vars.commandIn("DEFAULT")) {
       String discard[] = { "sectionDescription" };
       xmlDocument = xmlEngine.readXmlTemplate(
@@ -131,15 +119,6 @@ public class ReportWarehouseControl extends HttpSecureAppServlet {
     }
     {
       OBError myMessage = vars.getMessage("ReportWarehouseControl");
-      if (limit > 0 && countLines > limit) {
-        myMessage = new OBError();
-        myMessage.setType("Warning");
-        myMessage.setTitle("");
-        String msgbody = Utility.messageBD(this, "ReportsLimitBody", vars.getLanguage());
-        msgbody = msgbody.replace("@limit@", String.valueOf(limit));
-        msgbody = msgbody.replace("@rows@", String.valueOf(countLines));
-        myMessage.setMessage(msgbody);
-      }
       vars.removeMessage("ReportWarehouseControl");
       if (myMessage != null) {
         xmlDocument.setParameter("messageType", myMessage.getType());
@@ -161,7 +140,6 @@ public class ReportWarehouseControl extends HttpSecureAppServlet {
     xmlDocument.setData("structure1", data);
     out.println(xmlDocument.print());
     out.close();
-    
   }
 
   public String getServletInfo() {
