c22be35e1fd124377a1693cb86262d851e078feb
Asier Lostalé <asier.lostale@openbravo.com>
2017-01-13 10:03:39 +0100

    fixed bug 34939: 3.0 selectors cannot be used in classic mode
      When a 3.0 selector was included within classic mode, the JS failed to load.
    
      The problem was cancelAndReplace.js was included within classic mode JS but it
      depends on resources not available in this mode.
    
      Because cancel and replace is not designed to work in classic mode, its JS is
      now not included in this mode.
    
    --HG--
    extra : rebase_source : 29ff7c78370563d8cf181a8168aa39c3e3c45841

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/ApplicationComponentProvider.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/ApplicationComponentProvider.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2016 Openbravo SLU
+ * All portions are Copyright (C) 2010-2017 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -486,7 +486,7 @@ public class ApplicationComponentProvider extends BaseComponentProvider {
     globalResources.add(createStaticResource("web/js/productServices.js", true));
 
     // Cancel and Replace
-    globalResources.add(createStaticResource("web/js/cancelAndReplace.js", true));
+    globalResources.add(createStaticResource("web/js/cancelAndReplace.js", false));
 
     return globalResources;
   }
