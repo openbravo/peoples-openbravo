359bfe74c206bd37a557459e612dd5d407def9bf
Carlos Aristu <carlos.aristu@openbravo.com>
2017-01-18 17:26:10 +0100

    [multi-pool] Removed unused variable

--- src/org/openbravo/dal/core/DalThreadHandler.java
+++ src/org/openbravo/dal/core/DalThreadHandler.java
@@ -19,9 +19,6 @@
 
 package org.openbravo.dal.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Encapsulates a thread so that when the thread returns the session/transaction is
  * closed/committed/rolledback. It also ensures that the OBContext is removed from the thread.
@@ -33,7 +30,6 @@ import org.slf4j.LoggerFactory;
  */
 
 public abstract class DalThreadHandler extends ThreadHandler {
-  private static final Logger log = LoggerFactory.getLogger(DalThreadHandler.class);
 
   /** @see ThreadHandler#doBefore */
   @Override
