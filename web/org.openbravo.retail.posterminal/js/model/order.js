a62131fc1b6f7b706386cbeb7cecd446a52b2858
Eduardo Becerra <ebec70@yahoo.com>
2017-01-26 11:00:02 +0100

    Fixed issue 35025: Optmize call to get BPLocation

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2013-2016 Openbravo S.L.U.
+ * Copyright (C) 2013-2017 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -1025,9 +1025,6 @@
         this.set('replacedorder_documentNo', _order.get('replacedorder_documentNo'));
       }
 
-      if (_order.get('replacedorder_documentNo')) {
-        this.set('replacedorder_documentNo', _order.get('replacedorder_documentNo'));
-      }
       if (_order.get('replacedorder')) {
         this.set('replacedorder', _order.get('replacedorder'));
       }
