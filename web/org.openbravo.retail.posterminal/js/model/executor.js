d32a2893fd949387e0ae81a0007e9a7ec874967d
Ranjith S R <ranjith@qualiantech.com>
2017-01-30 17:27:00 +0530

    Fixes issue 35077 : Adding orderClause for Discount
    * default orderClause "_idx" is not stable when incremental refresh happens.
    So for discount calcualtion, discount should be ordered by priority and idx

--- web/org.openbravo.retail.posterminal/js/model/executor.js
+++ web/org.openbravo.retail.posterminal/js/model/executor.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2015 Openbravo S.L.U.
+ * Copyright (C) 2012-2017 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -214,6 +214,7 @@ OB.Model.DiscountsExecutor = OB.Model.Executor.extend({
 
     criteria = {
       '_whereClause': whereClause,
+      '_orderByClause': 'priority is null, priority, _idx',
       params: this.convertParams(evt, line, receipt, this.paramsTranslation)
     };
 
