6bff705999d64981762d3b93fef7a861ac5aa777
Carlos Aristu <carlos.aristu@openbravo.com>
2017-02-01 19:45:46 +0100

    [xlsx] Use ExportType enum to retrieve content types

--- src/org/openbravo/erpCommon/utility/DownloadReport.java
+++ src/org/openbravo/erpCommon/utility/DownloadReport.java
@@ -26,8 +26,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.openbravo.base.exception.OBException;
 import org.openbravo.base.secureApp.HttpSecureAppServlet;
 import org.openbravo.base.secureApp.VariablesSecureApp;
+import org.openbravo.client.application.report.ReportingUtils.ExportType;
 import org.openbravo.utils.FileUtility;
 
 public class DownloadReport extends HttpSecureAppServlet {
@@ -53,17 +55,11 @@ public class DownloadReport extends HttpSecureAppServlet {
     String filename = report.substring(0, pos);
     pos = report.lastIndexOf(".");
     String extension = report.substring(pos);
-    if (extension.equalsIgnoreCase(".pdf")) {
-      response.setContentType("application/pdf");
-    } else if (extension.equalsIgnoreCase(".csv")) {
-      response.setContentType("text/csv");
-    } else if (extension.equalsIgnoreCase(".txt")) {
-      response.setContentType("text/plain");
-    } else if (extension.equalsIgnoreCase(".xls")) {
-      response.setContentType("application/vnd.ms-excel");
-    } else if (extension.equalsIgnoreCase(".xlsx")) {
-      response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-    } else {
+    try {
+      ExportType exportType = ExportType.getExportType(extension.substring(1));
+      response.setContentType(exportType.getContentType());
+    } catch (OBException ignore) {
+      // unsupported export type, use application/x-download content type
       response.setContentType("application/x-download");
     }
     response.setHeader("Content-Disposition", "attachment; filename=" + filename + extension);
