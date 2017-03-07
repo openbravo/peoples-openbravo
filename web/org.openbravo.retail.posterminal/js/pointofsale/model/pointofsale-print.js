4de43eb432c9db6f6f205729c22ba9e73b22e8bc
Adrián Romero <adrianromero@openbravo.com>
2017-03-07 11:36:41 +0530

    Fixes issue 0035379: [SER-QA 2496] Cancelled documents cannot print the negative document no

--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-print.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-print.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2013-2016 Openbravo S.L.U.
+ * Copyright (C) 2013-2017 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -420,7 +420,7 @@
         } // order property.
       }
       if (receipt.get('canceledorder')) {
-        var negativeDocNo = receipt.get('negativeDocNo').documentNo;
+        var negativeDocNo = receipt.get('negativeDocNo');
         receipt.get('canceledorder').set('ordercanceled', true);
         receipt.get('canceledorder').set('negativeDocNo', negativeDocNo);
         me.print(receipt.get('canceledorder'), args);
