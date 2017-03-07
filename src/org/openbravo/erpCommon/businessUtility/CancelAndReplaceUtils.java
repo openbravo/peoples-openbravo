08b7276aec41646b5fe3d47a92bf6936af809a9a
Víctor Martínez Romanos <victor.martinez@openbravo.com>
2017-03-07 12:36:31 +0100

    Fixed bug 35442: Explicity set isSOTRX in netting shipments
    The netting shipments were created without explicity setting the isSOTRX information. If not set, the database automatically sets it to Y.
    However when an EntityEventObserver is executed, this value is null and could raise a NPE.
    
    Solution sets the isSOTRX value exactly the same as the order from which it was created. Right now it will be always Y, but done this way to reuse this method in purchase flows if necessary.
    
    --HG--
    extra : transplant_source : v%D2%25%FC%1C%9Dc%80%A3%9D%D9-%406%D7%D8L%21%BA%DA

--- src/org/openbravo/erpCommon/businessUtility/CancelAndReplaceUtils.java
+++ src/org/openbravo/erpCommon/businessUtility/CancelAndReplaceUtils.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2016 Openbravo SLU
+ * All portions are Copyright (C) 2016-2017 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  *************************************************************************
@@ -635,6 +635,7 @@ public class CancelAndReplaceUtils {
     nettingGoodsShipment.setDocumentAction("CO");
     nettingGoodsShipment.setMovementType("C-");
     nettingGoodsShipment.setProcessGoodsJava("--");
+    nettingGoodsShipment.setSalesTransaction(oldOrder.isSalesTransaction());
     String nettingGoodsShipmentDocumentNo = FIN_Utility.getDocumentNo(
         nettingGoodsShipment.getDocumentType(), ShipmentInOut.TABLE_NAME);
     nettingGoodsShipment.setDocumentNo(nettingGoodsShipmentDocumentNo);
