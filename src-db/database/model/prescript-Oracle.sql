29c52f64650e7706ae19d875e476a5b962668742
Víctor Martínez Romanos <victor.martinez@openbravo.com>
2017-03-08 12:50:21 +0100

    Fixed bug 33581: slow defaults retrieval in add payment
    The FIN_Payment table didn't have an index on the c_bpartner_id column, thus forcing a sequencial scan in any query run over this table.
    Added an index on the c_bpartner_id column to fix that problem.
    
    This index should fix most of the cases, however when for example a concrete business partner has an important percentage of the records in that table, the previous index would be useless, and the db might decide to run a seq. scan.
    To control this scenario we have extended the previous index with a function to distinguish credit payments (generated credit != used credit). This way we could filter by the credit records for the business partner directly using the index.
    Note that the function obequals is declared in the prescript because it must be defined as IMMUTABLE/DETERMINISTIC and this syntaxis is not supported yet by the DBSM.
    
    The AdvPaymentMngtDao.getCustomerPaymentsWithCredit() has been rewritten to take advantage of the new index.

--- src-db/database/model/prescript-Oracle.sql
+++ src-db/database/model/prescript-Oracle.sql
@@ -234,3 +234,38 @@ begin
   RETURN p_date + p_hours/24 + p_minutes/1440 + p_seconds/86400;
 end ADD_HMS;
 /-- END
+
+create or replace
+FUNCTION OBEQUALS(p_number_a IN NUMBER, p_number_b IN NUMBER) RETURN CHAR DETERMINISTIC
+
+AS
+/*************************************************************************
+* The contents of this file are subject to the Openbravo  Public  License
+* Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
+* Version 1.1  with a permitted attribution clause; you may not  use this
+* file except in compliance with the License. You  may  obtain  a copy of
+* the License at http://www.openbravo.com/legal/license.html
+* Software distributed under the License  is  distributed  on  an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+* License for the specific  language  governing  rights  and  limitations
+* under the License.
+* The Original Code is Openbravo ERP.
+* The Initial Developer of the Original Code is Openbravo SLU
+* All portions are Copyright (C) 2017 Openbravo SLU
+* All Rights Reserved.
+* Contributor(s):  ______________________________________.
+************************************************************************/
+/**
+* Returns 'Y' when both numbers are equals, else returns 'N'
+* This function is used as index in FIN_Payment table
+**/
+ v_dif NUMBER;
+begin
+    v_dif := coalesce(p_number_a, 0) - coalesce(p_number_b, 0);
+    IF (v_dif = 0) THEN
+     return 'Y';
+    ELSE
+     return 'N';
+    END IF;
+end OBEQUALS;
+/-- END
