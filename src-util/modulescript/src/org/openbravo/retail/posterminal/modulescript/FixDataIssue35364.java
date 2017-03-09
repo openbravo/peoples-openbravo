3e6bff9841349eedc3dd41490b1e32750c31b8c5
Mario Castello <mario.castello@peoplewalking.com>
2017-03-09 11:25:32 -0600

    Related to issue 35364: Added new modulescript to fix data

---
+++ src-util/modulescript/src/org/openbravo/retail/posterminal/modulescript/FixDataIssue35364.java
@@ -0,0 +1,56 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2016-2017 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+package org.openbravo.retail.posterminal.modulescript;
+
+import java.sql.PreparedStatement;
+
+import org.apache.log4j.Logger;
+import org.openbravo.database.ConnectionProvider;
+import org.openbravo.modulescript.ModuleScript;
+import org.openbravo.modulescript.ModuleScriptExecutionLimits;
+import org.openbravo.modulescript.OpenbravoVersion;
+
+public class FixDataIssue35364 extends ModuleScript {
+
+  private static final Logger log4j = Logger.getLogger(FixDataIssue35364.class);
+  private static final String RETAIL_PACK_MODULE_ID = "03FAB282A7BF47D3B1B242AC67F7845B";
+
+  @Override
+  public void execute() {
+    try {
+      final StringBuilder sql = new StringBuilder();
+      sql.append("update obpos_paymentmethodcashup set amounttokeep = 0 where amounttokeep is null; ");
+      sql.append("update obpos_paymentmethodcashup set startingcash = 0 where startingcash is null; ");
+      sql.append("update obpos_paymentmethodcashup set totalsales = 0 where totalsales is null; ");
+      sql.append("update obpos_paymentmethodcashup set totalreturns = 0 where totalreturns is null; ");
+
+      ConnectionProvider cp = getConnectionProvider();
+      PreparedStatement ps = cp.getPreparedStatement(sql.toString());
+      ps.executeUpdate();
+    } catch (Exception e) {
+      handleError(e);
+    }
+  }
+
+  @Override
+  protected ModuleScriptExecutionLimits getModuleScriptExecutionLimits() {
+    // The module script needs to be executed only when updating from a version
+    // lower than 3.0RR17Q2 (Retail pack)(1.8.2700)
+    return new ModuleScriptExecutionLimits(RETAIL_PACK_MODULE_ID, null, new OpenbravoVersion(1, 8,
+        2700));
+  }
+
+  public static void main(String[] args) {
+
+    // This method is provided for testing purposes.
+    FixDataIssue35364 t = new FixDataIssue35364();
+    t.execute();
+  }
+}
