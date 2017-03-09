c7312e9d9ed2d866c99ccccc631fb33a535ddd36
Carlos Aristu <carlos.aristu@openbravo.com>
2017-03-09 17:06:21 +0100

    [login-cleanup] Moved required code from shortcuts.js to login.js

--- web/js/login.js
+++ web/js/login.js
@@ -117,7 +117,8 @@ function loginResult(paramXMLParticular, XMLHttpRequestObj) {
 }
 
 function processResult(result) {
-  var target = '_self', command, shouldContinue = true;
+  var target = '_self',
+      command, shouldContinue = true;
   if (result.showMessage) {
     shouldContinue = setLoginMessage(result.messageType, result.messageTitle, result.messageText);
     if (!shouldContinue) {
@@ -235,7 +236,7 @@ function checkBrowserCompatibility() {
 }
 
 /**
- * Checks if the browser is a recommended one
+ * Checks if the browser is a recommended one.
  */
 
 function checkRecommendedBrowser() {
@@ -285,4 +286,50 @@ function addInputChangeCheck(input) {
   setObjAttribute(input, 'oncut', 'checkInputKeyDown(this); return true;');
   setObjAttribute(input, 'oncopy', 'checkInputKeyDown(this); return true;');
   setObjAttribute(input, 'onpaste', 'checkInputKeyDown(this); return true;');
+}
+
+/**
+ * Used to activate the key-press handling. Must be called after set the keys global array <em>keyArray</em>.
+ */
+
+function enableEditionShortcuts() {
+  try {
+    this.keyArray = new Array();
+    getEditionShortcuts('applicationCommonKeys');
+    getEditionShortcuts('windowCommonKeys');
+    getEditionShortcuts('editionSpecificKeys');
+    getEditionShortcuts('genericTreeKeys');
+    enableDefaultAction();
+  } catch (e) {}
+  keyDownManagement();
+  keyUpManagement();
+}
+
+/**
+ * Defines the keys array for all the application.
+ */
+
+function getEditionShortcuts(type) {
+  if (type == null || type == "" || type == "null") {
+    return;
+  } else if (type == 'applicationCommonKeys') {
+    // don't override browser shortcuts in case of MDI environment
+    if (!isWindowInMDIContext) {
+      this.keyArray.splice(keyArray.length - 1, 0, new keyArrayItem("M", "executeMenuButton('buttonExpand');executeMenuButton('buttonCollapse');", null, "ctrlKey+shiftKey", false, 'onkeydown'), new keyArrayItem("U", "executeMenuButton('buttonUserOptions');", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("Q", "executeMenuButton('buttonQuit');", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("F8", "executeMenuButton('buttonAlerts');", null, null, false, 'onkeydown'), new keyArrayItem("F9", "menuShowHide();", null, null, false, 'onkeydown'), new keyArrayItem("I", "executeWindowButton('buttonAbout');", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("H", "executeWindowButton('buttonHelp');", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("R", "executeWindowButton('buttonRefresh');", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("BACKSPACE", "executeWindowButton('buttonBack');", null, "ctrlKey+shiftKey", false, 'onkeydown'));
+    } else {
+      var LayoutMDI = getFrame('LayoutMDI');
+      if (typeof LayoutMDI.OB.Layout.ClassicOBCompatibility.Keyboard === "object" && typeof LayoutMDI.OB.Layout.ClassicOBCompatibility.Keyboard.getMDIKS === "function") {
+        var MDIKeyJSON = LayoutMDI.OB.Layout.ClassicOBCompatibility.Keyboard.getMDIKS();
+        for (var i = 0; i < MDIKeyJSON.length; i++) {
+          this.keyArray.splice(keyArray.length - 1, 0, new keyArrayItem(MDIKeyJSON[i].key, [MDIKeyJSON[i].action, MDIKeyJSON[i].funcParam], null, MDIKeyJSON[i].auxKey, false, 'onkeydown'));
+        }
+      }
+    }
+  } else if (type == 'windowCommonKeys') {
+    this.keyArray.splice(keyArray.length - 1, 0, new keyArrayItem("M", "putFocusOnMenu();", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("F10", "swichSelectedArea();", null, null, false, 'onkeydown'), new keyArrayItem("N", "executeWindowButton('linkButtonNew',true);", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("N", "executeWindowButton('linkButtonSave_Next',true);", null, "ctrlKey+shiftKey", false, 'onkeydown'), new keyArrayItem("G", "executeWindowButton('linkButtonSave_Relation',true);", null, "ctrlKey+shiftKey", false, 'onkeydown'), new keyArrayItem("S", "executeWindowButton('linkButtonSave',true);", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("S", "executeWindowButton('linkButtonSave_New',true);", null, "ctrlKey+shiftKey", false, 'onkeydown'), new keyArrayItem("D", "executeWindowButton('linkButtonDelete');", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("Z", "executeWindowButton('linkButtonUndo');", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("A", "executeWindowButton('linkButtonAttachment');", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("F", "executeWindowButton('linkButtonSearch');executeWindowButton('linkButtonSearchFiltered');", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("HOME", "executeWindowButton('linkButtonFirst',true);", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("END", "executeWindowButton('linkButtonLast',true);", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("LEFTARROW", "executeWindowButton('linkButtonPrevious',true);", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("RIGHTARROW", "executeWindowButton('linkButtonNext',true);", null, "ctrlKey", false, 'onkeydown'), new keyArrayItem("L", "executeWindowButton('linkButtonRelatedInfo');", null, "ctrlKey", false, 'onkeydown'));
+  } else if (type == 'editionSpecificKeys') {
+    this.keyArray.splice(keyArray.length - 1, 0, new keyArrayItem("TAB", "windowTabKey(true);", null, null, false, 'onkeydown'), new keyArrayItem("TAB", "windowTabKey(false);", null, null, false, 'onkeyup'), new keyArrayItem("TAB", "windowShiftTabKey(true);", null, "shiftKey", false, 'onkeydown'), new keyArrayItem("TAB", "windowShiftTabKey(false);", null, "shiftKey", false, 'onkeyup'), new keyArrayItem("ENTER", "windowCtrlShiftEnterKey();", null, "ctrlKey+shiftKey", false, 'onkeydown'), new keyArrayItem("ENTER", "windowCtrlEnterKey();", null, "ctrlKey", true, 'onkeydown'), new keyArrayItem("ENTER", "windowEnterKey();", null, null, true, 'onkeydown'), new keyArrayItem("G", "executeWindowButton('buttonRelation');", null, "ctrlKey", false, 'onkeydown'));
+  } else if (type == 'genericTreeKeys') {
+    this.keyArray.splice(keyArray.length - 1, 0, new keyArrayItem("UPARROW", "windowUpKey();", null, null, true, 'onkeydown'), new keyArrayItem("RIGHTARROW", "windowRightKey();", null, null, true, 'onkeydown'), new keyArrayItem("DOWNARROW", "windowDownKey();", null, null, true, 'onkeydown'), new keyArrayItem("LEFTARROW", "windowLeftKey();", null, null, true, 'onkeydown'), new keyArrayItem("SPACE", "windowSpaceKey();", null, null, true, 'onkeydown'));
+  }
 }
