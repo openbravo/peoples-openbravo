8ded136c18a803c8fc5b870f732169f60c273ccd
Inigo Sanchez <inigo.sanchez@openbravo.com>
2017-03-10 10:24:11 +0100

    Fixed issue 35464: Adaptive filtering does not work for Table references.
    The problem was that adaptive filtering (filter in client when all data is locally available) did
    not work for Table references when filtering by text instead of by selecting a value in the drop
    down list. The issue that causes this regression was not taking into account adaptive filtering.
    
    This problem has been fixed by taking into account this case in getDisplayProperty method. Now
    getDisplayProperty method manages properly when all the data are in the client (Adaptive filtering).

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-fk-filter.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-fk-filter.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2011-2016 Openbravo SLU
+ * All portions are Copyright (C) 2011-2017 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -579,7 +579,9 @@ isc.OBFKFilterTextItem.addProperties({
   getDisplayProperty: function () {
     var theGrid = this.grid,
         name = this.name;
-    if (theGrid && name && theGrid.getField(name) && theGrid.getField(name).displayProperty) {
+    // it is used willFetchData to identified when adaptive filtering is not occurred.
+    // see issue https://issues.openbravo.com/view.php?id=35426
+    if (theGrid && name && theGrid.getField(name) && theGrid.getField(name).displayProperty && this.form.grid.sourceWidget.willFetchData()) {
       return name + OB.Constants.FIELDSEPARATOR + theGrid.getField(name).displayProperty;
     }
     return null;
