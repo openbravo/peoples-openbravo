b25a3c7801370ffc5599f2bb4cb4f7b0bc37442a
Stefan Hühner <stefan.huehner@openbravo.com>
2017-03-13 07:48:48 +0100

    fixed bug 35490: SystemService logs a ERROR line always

--- src/org/openbravo/service/system/SystemService.java
+++ src/org/openbravo/service/system/SystemService.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2009-2015 Openbravo SLU 
+ * All portions are Copyright (C) 2009-2017 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -168,7 +168,9 @@ public class SystemService implements OBSingleton {
         sb.append(err);
       }
     }
-    log.error(sb.toString());
+    if (sb.length() > 0) {
+      log.error(sb.toString());
+    }
     return sb.toString();
   }
 
