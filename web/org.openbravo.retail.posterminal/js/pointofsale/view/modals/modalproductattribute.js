4824a528fe9c365b31f8b94b5c74994cd4683392
Yogas Karnik <yogas.karnik@precognis.com>
2017-03-14 10:34:23 +0100

    [SupportForAttributes] Attribute Set modal popup

---
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/modals/modalproductattribute.js
@@ -0,0 +1,109 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2013-2017 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+/*global OB, moment, enyo */
+
+enyo.kind({
+  kind: 'OB.UI.ModalAction',
+  name: 'OB.UI.ModalProductAttributes',
+  i18nHeader: 'OBPOS_ProductAttributeValueDialogTitle',
+  bodyContent: {
+    components: [{
+      kind: 'enyo.Input',
+      type: 'text',
+      attributes: {
+    	maxlength: 70
+      },
+      style: 'text-align: center;width: 400px; height: 40px;',
+      name: 'valueAttribute',
+      selectOnFocus: true,
+      isFirstFocus: true,
+      value: ''
+    }]
+  },
+  bodyButtons: {
+	components: [{
+	  kind: 'OB.UI.ModalDialogButton',
+	  i18nContent: 'OBMOBC_LblOk',
+	  isDefaultAction: true,
+	  ontap: 'saveAttribute'
+	}, {
+	  kind: 'OB.UI.ModalDialogButton',
+	  i18nContent: 'OBPOS_LblClear',
+	  ontap: 'clearAction'
+    }, {
+	  kind: 'OB.UI.ModalDialogButton',
+	  i18nContent: 'OBMOBC_LblCancel',
+	  ontap: 'cancelAction'
+    }]
+  }, 
+  
+	 /*
+	 * - LOT: expression starts with L followed by alphanumeric lot name
+	 * - SERIAL and LOT: expression starts with L followed by alphanumeric lot name and serial number
+	 * - EXPIRATION DATE: expression should contain a date format (dd-MM-yyyy)
+	 * - LOT and SERIAL and EXPIRATION DATE - LJAN17_#6969_28-02-2018
+	 * 
+	 */
+  validateAttribute : function(attribute){
+	var valueAttribute = attribute;
+    var pattern = "/^L|[0-9a-zA-Z]*#*[0-9_a-zA-Z]*";
+    return (valueAttribute.match(pattern)) ? true : false;
+  },
+  
+  saveAttribute: function(inSender, inEvent) {
+    var me = this;
+    var attributeValue = this.$.bodyContent.$.valueAttribute.getValue();
+    
+    if (attributeValue) {
+      if (this.validateAttribute(attributeValue)){
+    	me.owner.model.get('order').get('lines').at(0).set('attributeValue',attributeValue);
+    	me.owner.model.get('order').save();
+      } else {
+    	OB.UTIL.showError(OB.I18N.getLabel('OBPOS_NotValidAttribute'));
+      }
+    }
+    return true;
+  },
+  clearAction: function () {
+    this.$.bodyContent.$.valueAttribute.setValue(null);
+    return true;
+  },
+  cancel: function() {
+	  var currentLine=this.owner.model.get('order').get('lines').at(0);
+	  if(currentLine){
+		  this.owner.model.get('order').deleteLine(currentLine);
+		  this.owner.model.get('order').save();
+	  }
+      this.hide();
+      return true;
+  },
+  
+  executeBeforeHide: function() {},
+  executeOnShow: function() {
+	var me = this;
+	this.$.bodyButtons.saveAttribute = function(inSender, inEvent) {
+		me.saveAttribute(inSender, inEvent);
+		me.hide();
+	};
+	this.$.bodyButtons.clearAction = function() {
+		me.clearAction();
+	};
+	this.$.bodyButtons.cancelAction = function() {
+		me.cancel();
+	};
+  },
+  executeOnHide: function () {
+	this.$.bodyContent.$.valueAttribute.setValue(null);
+  }
+});
+OB.UI.WindowView.registerPopup('OB.OBPOSPointOfSale.UI.PointOfSale', {
+  kind: 'OB.UI.ModalProductAttributes',
+  name: 'OB.UI.ModalProductAttributes'
+});
