fed153d8f1c8debc87d593b57d914da7ae619537
Víctor Martínez Romanos <victor.martinez@openbravo.com>
2017-03-22 14:16:46 +0100

    Fixed issue 35590: AD_IsOrgIncluded performance improvements
    Created AD_Org_Tree table to store the plain parent organization tree.
    The unique constraint defined in this table is the key to get the performance improvement.
    This table is populated when the organization is set as ready. In that moment we ensure its parent organization tree won’t change anymore in the future.
    
    The AD_IsOrgIncluded function has been totally rewritten to read the AD_Org_Tree table, so there is no need to recursively iterate over the AD_TreeNode table anymore.
    Important: after this changeset the AD_IsOrgIncluded function will only work for organizations set as ready!
    
    A new AD_IsOrgIncluded_TreeNode is created as a copy of the previous AD_IsOrgIncluded code to be used only by the processes working with non ready organizations.
    The Set as Ready process (AD_ORG_READY) has been updated to use this legacy function when required.
    
    A new module script PopulateADOrgTree.java has been developed to populate the new table for existing instances.
    Sampledata has been updated too.
    
    A set of automatic tests have been created to ensure the function returns the same values after the refactor, and that we get a real performance improvement.
    The improvement heavily depends on the organization tree structure (the more levels the better it will perform), parameters (the farther the passed orgs are the better it will perform) and query/table over the function is called from (the bigger table the better it will perform). In local testings we get improvements from 2,70x to 200x
    
    --HG--
    extra : rebase_source : 29549294748212b1d6f93ebfed0ec4e74d035852

--- src-test/src/org/openbravo/test/db/model/functions/Ad_isorgincludedTest.java
+++ src-test/src/org/openbravo/test/db/model/functions/Ad_isorgincludedTest.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2015-2016 Openbravo SLU
+ * All portions are Copyright (C) 2015-2017 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -35,62 +35,62 @@ public class Ad_isorgincludedTest extends OBBaseTest {
   /**
    * All Organization *
    */
-  private static final String ORG_O = "0";
+  protected static final String ORG_0 = "0";
 
   /**
    * QA Testing Client
    */
-  private static final String CLIENT_A = "4028E6C72959682B01295A070852010D";
+  protected static final String CLIENT_QA = "4028E6C72959682B01295A070852010D";
 
   /**
    * Main Organization
    */
-  private static final String ORG_A = "43D590B4814049C6B85C6545E8264E37";
+  protected static final String ORG_QA_MAIN = "43D590B4814049C6B85C6545E8264E37";
 
   /**
    * Spain Organization
    */
-  private static final String ORG_A1 = "357947E87C284935AD1D783CF6F099A1";
+  protected static final String ORG_QA_SPAIN = "357947E87C284935AD1D783CF6F099A1";
 
   /**
    * F&B International Group Client
    */
-  private static final String CLIENT_B = "23C59575B9CF467C9620760EB255B389";
+  protected static final String CLIENT_FB = "23C59575B9CF467C9620760EB255B389";
 
   /**
    * F&B International Group Organization
    */
-  private static final String ORG_B = "19404EAD144C49A0AF37D54377CF452D";
+  protected static final String ORG_FB_FBGROUP = "19404EAD144C49A0AF37D54377CF452D";
 
   /**
    * F&B US, Inc.
    */
-  private static final String ORG_B1 = "2E60544D37534C0B89E765FE29BC0B43";
+  protected static final String ORG_FB_US = "2E60544D37534C0B89E765FE29BC0B43";
 
   /**
    * F&B US East Coast
    */
-  private static final String ORG_B11 = "7BABA5FF80494CAFA54DEBD22EC46F01";
+  protected static final String ORG_FB_EAST = "7BABA5FF80494CAFA54DEBD22EC46F01";
 
   /**
    * F&B US West Coast
    */
-  private static final String ORG_B12 = "BAE22373FEBE4CCCA24517E23F0C8A48";
+  protected static final String ORG_FB_WEST = "BAE22373FEBE4CCCA24517E23F0C8A48";
 
   /**
    * F&B España, S.A.
    */
-  private static final String ORG_B2 = "B843C30461EA4501935CB1D125C9C25A";
+  protected static final String ORG_FB_SPAIN = "B843C30461EA4501935CB1D125C9C25A";
 
   /**
    * F&B España - Región Norte
    */
-  private static final String ORG_B21 = "E443A31992CB4635AFCAEABE7183CE85";
+  protected static final String ORG_FB_NORTE = "E443A31992CB4635AFCAEABE7183CE85";
 
   /**
    * F&B España - Región Sur
    */
-  private static final String ORG_B22 = "DC206C91AA6A4897B44DA897936E0EC3";
+  protected static final String ORG_FB_SUR = "DC206C91AA6A4897B44DA897936E0EC3";
 
   /**
    * Case I: Distinct Organization in the same branch with different levels.
@@ -120,42 +120,47 @@ public class Ad_isorgincludedTest extends OBBaseTest {
   public void testIsOrgIncluded() {
 
     // Case I
-    assertEquals("Level 1 Organization", 1, isOrgIncluded(ORG_O, ORG_O, CLIENT_B));
-    assertEquals("Level 2 Organization", 2, isOrgIncluded(ORG_B, ORG_O, CLIENT_B));
-    assertEquals("Level 3 Organization", 3, isOrgIncluded(ORG_B1, ORG_O, CLIENT_B));
-    assertEquals("Level 4 Organization", 4, isOrgIncluded(ORG_B12, ORG_O, CLIENT_B));
+    assertEquals("Level 1 Organization", 1, isOrgIncluded(ORG_0, ORG_0, CLIENT_FB));
+    assertEquals("Level 2 Organization", 2, isOrgIncluded(ORG_FB_FBGROUP, ORG_0, CLIENT_FB));
+    assertEquals("Level 3 Organization", 3, isOrgIncluded(ORG_FB_US, ORG_0, CLIENT_FB));
+    assertEquals("Level 4 Organization", 4, isOrgIncluded(ORG_FB_WEST, ORG_0, CLIENT_FB));
 
     // Case II
-    assertTrue(isOrgIncluded(ORG_B12, ORG_B2, CLIENT_A) == -1);
-    assertTrue(isOrgIncluded(ORG_B11, ORG_B2, CLIENT_A) == -1);
-    assertTrue(isOrgIncluded(ORG_B21, ORG_B1, CLIENT_A) == -1);
-    assertTrue(isOrgIncluded(ORG_B22, ORG_B1, CLIENT_A) == isOrgIncluded(ORG_B11, ORG_B2, CLIENT_A));
+    assertTrue(isOrgIncluded(ORG_FB_WEST, ORG_FB_SPAIN, CLIENT_QA) == -1);
+    assertTrue(isOrgIncluded(ORG_FB_EAST, ORG_FB_SPAIN, CLIENT_QA) == -1);
+    assertTrue(isOrgIncluded(ORG_FB_NORTE, ORG_FB_US, CLIENT_QA) == -1);
+    assertTrue(isOrgIncluded(ORG_FB_SUR, ORG_FB_US, CLIENT_QA) == isOrgIncluded(ORG_FB_EAST,
+        ORG_FB_SPAIN, CLIENT_QA));
 
     // Case III
-    assertTrue(isOrgIncluded(ORG_A, ORG_A1, CLIENT_A) == -1);
-    assertTrue(isOrgIncluded(ORG_B1, ORG_B12, CLIENT_A) == -1);
+    assertTrue(isOrgIncluded(ORG_QA_MAIN, ORG_QA_SPAIN, CLIENT_QA) == -1);
+    assertTrue(isOrgIncluded(ORG_FB_US, ORG_FB_WEST, CLIENT_QA) == -1);
 
     // Case IV
-    assertTrue(isOrgIncluded(ORG_A1, ORG_A, CLIENT_B) == -1);
-    assertTrue(isOrgIncluded(ORG_B1, ORG_B, CLIENT_A) == -1);
+    assertTrue(isOrgIncluded(ORG_QA_SPAIN, ORG_QA_MAIN, CLIENT_FB) == -1);
+    assertTrue(isOrgIncluded(ORG_FB_US, ORG_FB_FBGROUP, CLIENT_QA) == -1);
 
     // Case V
-    assertTrue(isOrgIncluded(ORG_A, ORG_A, CLIENT_A) == 1);
+    assertTrue(isOrgIncluded(ORG_QA_MAIN, ORG_QA_MAIN, CLIENT_QA) == 1);
 
     // Case VI
-    assertTrue(isOrgIncluded("ABC", ORG_B, CLIENT_A) == -1);
-
+    assertTrue(isOrgIncluded("ABC", ORG_FB_FBGROUP, CLIENT_QA) == -1);
   }
 
-  private int isOrgIncluded(String orgId, String parentOrgId, String clientId) {
+  protected int isOrgIncluded(String orgId, String parentOrgId, String clientId) {
+    return callOrgIncludedFunction(orgId, parentOrgId, clientId, "AD_ISORGINCLUDED");
+  }
+
+  protected int callOrgIncludedFunction(String orgId, String parentOrgId, String clientId,
+      String functionName) {
     int value = 0;
     try {
       final List<Object> parameters = new ArrayList<Object>();
       parameters.add(orgId);
       parameters.add(parentOrgId);
       parameters.add(clientId);
-      value = ((BigDecimal) CallStoredProcedure.getInstance().call("AD_ISORGINCLUDED", parameters,
-          null)).intValue();
+      value = ((BigDecimal) CallStoredProcedure.getInstance().call(functionName, parameters, null))
+          .intValue();
     } catch (Exception e) {
       throw new IllegalStateException(e);
     }
