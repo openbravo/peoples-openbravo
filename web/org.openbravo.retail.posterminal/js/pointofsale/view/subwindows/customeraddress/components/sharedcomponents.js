e3f158d870763f557df9d7f1e8d2ef2ee1e4bad0
Mario Castello <mario.castello@peoplewalking.com>
2017-03-28 11:40:41 -0600

    Fixed issue 0035502: [SERQA 2604] Implement onChange event for Country Selector
    - Added onchange handlers and onSetValue events in customercomboproperty and customeraddrcomboproperty

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/subwindows/customeraddress/components/sharedcomponents.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/subwindows/customeraddress/components/sharedcomponents.js
@@ -401,10 +401,12 @@ enyo.kind({
     onLoadValue: 'loadValue',
     onSaveChange: 'saveChange',
     onSetValue: 'valueSet',
-    onRetrieveValues: 'retrieveValue'
+    onRetrieveValues: 'retrieveValue',
+    onchange: 'change'
   },
   events: {
-    onSaveProperty: ''
+    onSaveProperty: '',
+    onSetValues: ''
   },
   components: [{
     kind: 'OB.UI.List',
@@ -439,6 +441,7 @@ enyo.kind({
     this.$.customerAddrCombo.setCollection(this.collection);
     this.fetchDataFunction(inEvent);
   },
+  change: function () {},
   dataReadyFunction: function (data, inEvent) {
     var index = 0,
         result = null;
