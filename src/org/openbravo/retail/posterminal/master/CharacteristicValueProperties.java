544f18acdb0906723a2831ab2cb1ff8537385086
Ranjith S R <ranjith@qualiantech.com>
2017-03-31 15:54:12 +0530

    Fixes issue 35530 : Added Tree level for Product Characteristics
    * Refactored Code (Duplicate DAL queries replaced with childrenList)
    * Removed Tree Node Query for Remote Mode
    * If Child(which also children) is selected and searched, then if we open the popup Children should not be shown for the selected child
    
    --HG--
    extra : rebase_source : 84a95990c94cea9894123b3b62d3b994aea0fb71

--- src/org/openbravo/retail/posterminal/master/CharacteristicValueProperties.java
+++ src/org/openbravo/retail/posterminal/master/CharacteristicValueProperties.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2015 Openbravo S.L.U.
+ * Copyright (C) 2015-2017 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openbravo.client.kernel.ComponentProvider.Qualifier;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.erpCommon.businessUtility.Preferences;
 import org.openbravo.mobile.core.model.HQLProperty;
 import org.openbravo.mobile.core.model.ModelExtension;
 
@@ -20,17 +22,34 @@ public class CharacteristicValueProperties extends ModelExtension {
 
   @Override
   public List<HQLProperty> getHQLProperties(Object params) {
-    ArrayList<HQLProperty> list = new ArrayList<HQLProperty>() {
-      private static final long serialVersionUID = 1L;
-      {
-        add(new HQLProperty("cv.id", "id"));
-        add(new HQLProperty("cv.name", "name"));
-        add(new HQLProperty("cv.characteristic.id", "characteristic_id"));
-        add(new HQLProperty("node.reportSet", "parent"));
-        add(new HQLProperty("cv.name", "_identifier"));
-        add(new HQLProperty("cv.active", "active"));
-      }
-    };
+
+    ArrayList<HQLProperty> list = null;
+    try {
+      list = new ArrayList<HQLProperty>() {
+        private static final long serialVersionUID = 1L;
+
+        {
+          add(new HQLProperty("cv.id", "id"));
+          add(new HQLProperty("cv.name", "name"));
+          add(new HQLProperty("cv.characteristic.id", "characteristic_id"));
+          boolean isRemote = false;
+          isRemote = "Y".equals(Preferences.getPreferenceValue("OBPOS_remote.product", true,
+              OBContext.getOBContext().getCurrentClient(), OBContext.getOBContext()
+                  .getCurrentOrganization(), OBContext.getOBContext().getUser(), OBContext
+                  .getOBContext().getRole(), null));
+          if (!isRemote) {
+            add(new HQLProperty("node.reportSet", "parent"));
+          } else {
+            add(new HQLProperty("'0'", "parent"));
+          }
+          add(new HQLProperty("cv.summaryLevel", "summaryLevel"));
+          add(new HQLProperty("cv.name", "_identifier"));
+          add(new HQLProperty("cv.active", "active"));
+        }
+      };
+    } catch (org.openbravo.erpCommon.utility.PropertyException e) {
+      e.printStackTrace();
+    }
     return list;
   }
 
