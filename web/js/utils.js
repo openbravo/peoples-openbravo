e9e47d7b9941f8cad8b204dc780a90f29487ddca
Carlos Aristu <carlos.aristu@openbravo.com>
2017-04-06 16:30:38 +0200

    related to issue 35386: updated revision control number

--- web/js/utils.js
+++ web/js/utils.js
@@ -105,7 +105,7 @@ function isDebugEnabled() {
 * Return a number that would be checked at the Login screen to know if the file is cached with the correct version
 */
 function getCurrentRevision() {
-  var number = '29468';
+  var number = '31853';
   return number;
 }
 
