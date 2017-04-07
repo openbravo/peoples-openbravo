284a475992433a47d577f0e66c159b2b201ec172
Carlos Aristu <carlos.aristu@openbravo.com>
2017-04-07 15:36:57 +0200

    [login-perf] Removed unneeded commas

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/navbar/ob-user-profile-widget.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/navbar/ob-user-profile-widget.js
@@ -159,25 +159,25 @@ isc.OBUserProfile.addProperties({
     roleField = isc.addProperties({
       name: 'role',
       title: OB.I18N.getLabel('UINAVBA_Role'),
-      sortField: 'role',
+      sortField: 'role'
     }, comboBoxFieldProperties);
 
     orgField = isc.addProperties({
       name: 'organization',
       title: OB.I18N.getLabel('UINAVBA_Organization'),
-      sortField: 'organization',
+      sortField: 'organization'
     }, comboBoxFieldProperties);
 
     warehouseField = isc.addProperties({
       name: 'warehouse',
       title: OB.I18N.getLabel('UINAVBA_Warehouse'),
-      sortField: 'warehouse',
+      sortField: 'warehouse'
     }, comboBoxFieldProperties);
 
     languageField = isc.addProperties({
       name: 'language',
       title: OB.I18N.getLabel('UINAVBA_Language'),
-      sortField: 'language',
+      sortField: 'language'
     }, comboBoxFieldProperties);
 
     checkboxFieldProperties = {
