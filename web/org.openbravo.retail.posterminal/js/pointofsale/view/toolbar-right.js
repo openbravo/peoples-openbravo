2ec715540b6ebf05f34f26ecc321fea9d522dd1d
Ranjith S R <ranjith@qualiantech.com>
2017-04-11 14:22:13 +0530

    Related to issue 35707 : Scanning Focus set to true in Search tab, if "Use External Input" is enabled

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/toolbar-right.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/toolbar-right.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2016 Openbravo S.L.U.
+ * Copyright (C) 2012-2017 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -395,6 +395,7 @@ enyo.kind({
         keyboard: false,
         edit: false
       });
+      OB.MobileApp.view.scanningFocus(true);
     }
   },
   initComponents: function () {
