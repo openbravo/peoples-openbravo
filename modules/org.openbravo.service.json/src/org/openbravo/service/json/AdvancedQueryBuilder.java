adfd5e2cfc174c0c418e64e0f236373a9ad54711
Inigo Sanchez <inigo.sanchez@openbravo.com>
2017-05-09 10:11:24 +0200

    Fixed issue 35953: Fetching data when it is sorting by a null FK is not working
    Fetching data when it was sorting by a null foreign key was not retrieving any record when should
    had been retrieved data.
    
    When AdvancedQueryBuilder class is created order by clause and it is working in resolve joins a
    canUseInnerJoin method is invoked. This method is used to check if the left join can be safely
    replaced with inner join when it is building order by clause. This method is not take into account
    all the posibilities.
    
    The problem has been resolved by take into account when any property of the list is not mandatory
    and is not primitive. In this case left join can not be replaced with inner join. The rest of the
    cases can be safely replaced it.

--- modules/org.openbravo.service.json/src/org/openbravo/service/json/AdvancedQueryBuilder.java
+++ modules/org.openbravo.service.json/src/org/openbravo/service/json/AdvancedQueryBuilder.java
@@ -1578,8 +1578,12 @@ public class AdvancedQueryBuilder {
     if (properties.size() == 0) {
       return false;
     }
-    Property firstProperty = properties.get(0);
-    return !firstProperty.isPrimitive() && firstProperty.isMandatory();
+    for (Property property : properties) {
+      if (!property.isPrimitive() && !property.isMandatory()) {
+        return false;
+      }
+    }
+    return true;
   }
 
   // Creates a Hibernate concatenation if there are multiple identifierproperties
