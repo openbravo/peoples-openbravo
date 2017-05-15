435a74b50d23184e36f0a614ddc0c50f2df4ef97
Miguel de Juana <miguel.dejuana@openbravo.com>
2017-05-15 17:59:08 +0200

    Related issue 35830, related issue 35623
    It is breaking I35623_OpenReceiptAndPaymentProvider
    
    Backed out changeset abe131fd8ece

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/keyboard-toolbars.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/keyboard-toolbars.js
@@ -93,8 +93,7 @@ enyo.kind({
     }
 
     if (OB.DEC.compare(amount) > 0) {
-      var provider, receiptToPay, me = this,
-          paymentLine, order;
+      var provider, receiptToPay, me = this;
       if (this.model.get('leftColumnViewManager').isOrder()) {
         receiptToPay = this.receipt;
       }
@@ -109,42 +108,20 @@ enyo.kind({
         provider = paymentMethod.refundProvider;
       }
 
-      paymentLine = new OB.Model.PaymentLine({
-        'kind': key,
-        'name': name,
-        'amount': amount,
-        'rate': rate,
-        'mulrate': mulrate,
-        'isocode': isocode,
-        'allowOpenDrawer': paymentMethod.allowopendrawer,
-        'isCash': paymentMethod.iscash,
-        'openDrawer': paymentMethod.openDrawer,
-        'printtwice': paymentMethod.printtwice
-      });
-
       if (provider) {
-        order = new OB.Model.Order(receiptToPay.attributes);
-        order.addPayment(paymentLine, function () {
-          receiptToPay.trigger('checkValidPayments', order, function (result) {
-            if (result) {
-              me.doShowPopup({
-                popup: 'modalpayment',
-                args: {
-                  'receipt': receiptToPay,
-                  'provider': provider,
-                  'key': key,
-                  'name': name,
-                  'paymentMethod': paymentMethod,
-                  'amount': amount,
-                  'rate': rate,
-                  'mulrate': mulrate,
-                  'isocode': isocode
-                }
-              });
-            } else {
-              me.receipt.stopAddingPayments = false;
-            }
-          });
+        this.doShowPopup({
+          popup: 'modalpayment',
+          args: {
+            'receipt': receiptToPay,
+            'provider': provider,
+            'key': key,
+            'name': name,
+            'paymentMethod': paymentMethod,
+            'amount': amount,
+            'rate': rate,
+            'mulrate': mulrate,
+            'isocode': isocode
+          }
         });
       } else {
         // Calculate total amount to pay with selected PaymentMethod  
@@ -177,7 +154,18 @@ enyo.kind({
             currency: '',
             symbolAtRight: true
           });
-          this.model.addPayment(paymentLine, callback);
+          this.model.addPayment(new OB.Model.PaymentLine({
+            'kind': key,
+            'name': name,
+            'amount': amount,
+            'rate': rate,
+            'mulrate': mulrate,
+            'isocode': isocode,
+            'allowOpenDrawer': paymentMethod.allowopendrawer,
+            'isCash': paymentMethod.iscash,
+            'openDrawer': paymentMethod.openDrawer,
+            'printtwice': paymentMethod.printtwice
+          }), callback);
         }
       }
     }
