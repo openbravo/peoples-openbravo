037797ec735fae4688ff69002f4cdfffbbedc228
Jorge Garcia <jorge.garcia@openbravo.com>
2017-06-20 09:48:11 +0200

    Related to issue 36122: [SERQA 2908]If a partial payment is added, buttons and external input are not blocked.
    Backed out changeset c92cd921eee5.

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -3344,7 +3344,6 @@
 
       payments = this.get('payments');
       total = OB.DEC.abs(this.getTotal());
-      OB.UTIL.showLoading(true);
       OB.UTIL.HookManager.executeHooks('OBPOS_preAddPayment', {
         paymentToAdd: payment,
         payments: payments,
@@ -3358,7 +3357,6 @@
               order.save();
               order.trigger('saveCurrent');
             }
-            OB.UTIL.showLoading(false);
             if (callback instanceof Function) {
               callback(order);
             }
