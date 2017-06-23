46fad6cba230f1bd0d773f1b4f83345032e243c6
Asier Martirena <asier.martirena@openbravo.com>
2017-06-23 09:09:43 +0200

    [Service by Tranche] Backed out changeset fe161fa93c23
    --HG--
    extra : rebase_source : cb558fbda3021bb4e05691b4b29c777b8149c317

--- src/org/openbravo/retail/posterminal/ServicesHQLCriteria.java
+++ src/org/openbravo/retail/posterminal/ServicesHQLCriteria.java
@@ -35,7 +35,7 @@ public class ServicesHQLCriteria extends HQLCriteriaProcess {
           + "or product.includedProducts is null) "
           + "and ((product.includedProductCategories = 'Y' and not exists (select 1 from ServiceProductCategory spc where product.id = spc.product.id and spc.productCategory.id =  '$2' )) "
           + "or (product.includedProductCategories = 'N' and exists (select 1 from ServiceProductCategory spc where product.id = spc.product.id and spc.productCategory.id = '$2' )) "
-          + "or product.includedProductCategories is null) and (product.ispricerulebased = 'N' or (product.quantityRule in ('UQ','PP') and exists" //
+          + "or product.includedProductCategories is null) and (product.ispricerulebased = 'N' or (product.quantityRule = 'UQ' and exists" //
           + "(select 1" //
           + "     from ServicePriceRuleVersion sprv" //
           + "     where sprv.product.id = product.id" //
@@ -56,6 +56,27 @@ public class ServicesHQLCriteria extends HQLCriteriaProcess {
           + "             or sprv.obposMinimum <= " + totalAmountSelected //
           + "             and sprv.obposMaximum >= " + totalAmountSelected //
           + "      and sprv.active = true))) " //
+          + "or " //
+          + "(product.quantityRule = 'PP' and exists" //
+          + "(select 1" //
+          + "     from ServicePriceRuleVersion sprv" //
+          + "     where sprv.product.id = product.id" //
+          + "     and sprv.validFromDate =" //
+          + "             (select max(sprv2.validFromDate)" //
+          + "             from ServicePriceRuleVersion sprv2" //
+          + "             where sprv2.product.id = product.id" //
+          + "             and sprv2.validFromDate <= now()" //
+          + "             and sprv2.active = true)" //
+          + "     and" //
+          + "             (sprv.obposMinimum is null" //
+          + "             and sprv.obposMaximum is null" //
+          + "             or sprv.obposMinimum is null" //
+          + "             and sprv.obposMaximum >= " + maximumSelected //
+          + "             or sprv.obposMinimum <= " + minimumSelected //
+          + "             and sprv.obposMaximum is null" //
+          + "             or sprv.obposMinimum <= " + minimumSelected //
+          + "             and sprv.obposMaximum >= " + maximumSelected //
+          + "      and sprv.active = true)))" //
           + "))"; //
     } catch (JSONException e) {
       throw new OBException(e);
