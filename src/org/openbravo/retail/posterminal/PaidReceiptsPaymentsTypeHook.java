217d6c528cd775b2f11ba4707a132832f7132d0f
Aaron Calero <aaron.calero@openbravo.com>
2017-07-07 09:59:23 +0200

    Related to issue 31624: [Digital Coupons] Code Review: Changed definition of hooks to be abstract classes

--- src/org/openbravo/retail/posterminal/PaidReceiptsPaymentsTypeHook.java
+++ src/org/openbravo/retail/posterminal/PaidReceiptsPaymentsTypeHook.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2016 Openbravo S.L.U.
+ * Copyright (C) 2017 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -11,7 +11,7 @@ package org.openbravo.retail.posterminal;
 
 import org.codehaus.jettison.json.JSONArray;
 
-public interface PaidReceiptsPaymentsTypeHook {
+public abstract class PaidReceiptsPaymentsTypeHook {
 
-  public void exec(JSONArray paymentTypes, String paymentId) throws Exception;
+  public abstract void exec(JSONArray paymentTypes, String paymentId) throws Exception;
 }
