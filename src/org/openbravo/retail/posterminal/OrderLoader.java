b4a7b277a704ff06058891bd987bebe0509c0402
Asier Martirena <asier.martirena@openbravo.com>
2017-07-31 13:43:08 +0200

    Fixed issue 36548: In OrderLoader the delivery logic has been moved to the shipments logic
    --HG--
    extra : rebase_source : e8c8ec6da190f8327d7a28980d6be897a85a7089

--- src/org/openbravo/retail/posterminal/OrderLoader.java
+++ src/org/openbravo/retail/posterminal/OrderLoader.java
@@ -234,6 +234,7 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
 
       initializeVariables(jsonorder);
       Order order = null;
+      OrderLine orderLine = null;
       ShipmentInOut shipment = null;
       Invoice invoice = null;
       boolean createInvoice = false;
@@ -342,6 +343,7 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
         } else if (!newLayaway && (creditpaidLayaway || fullypaidLayaway)) {
           order = OBDal.getInstance().get(Order.class, jsonorder.getString("id"));
           order.setObposAppCashup(jsonorder.getString("obposAppCashup"));
+          order.setDelivered(true);
           if (jsonorder.has("oBPOSNotInvoiceOnCashUp")) {
             order.setOBPOSNotInvoiceOnCashUp(jsonorder.getBoolean("oBPOSNotInvoiceOnCashUp"));
           }
@@ -349,7 +351,13 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
           OBQuery<OrderLine> queryOls = OBDal.getInstance().createQuery(OrderLine.class,
               olsHqlWhereClause);
           queryOls.setNamedParameter("orderId", order.getId());
-          lineReferences.addAll(queryOls.list());
+          List<OrderLine> lstResultOL = queryOls.list();
+
+          for (int i = 0; i < lstResultOL.size(); i++) {
+            orderLine = lstResultOL.get(i);
+            orderLine.setDeliveredQuantity(orderLine.getOrderedQuantity());
+            lineReferences.add(orderLine);
+          }
         } else if (partialpaidLayaway) {
           order = OBDal.getInstance().get(Order.class, jsonorder.getString("id"));
           if (!jsonorder.has("channel")) {
@@ -403,8 +411,6 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
                 "OBPOS_WarehouseNotStorageBin", OBContext.getOBContext().getLanguage()
                     .getLanguage()));
           }
-
-          order.setDelivered(true);
 
           shipment = OBProvider.getInstance().get(ShipmentInOut.class);
           createShipment(shipment, order, jsonorder);
@@ -1140,10 +1146,6 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
 
       AttributeSetInstance oldAttributeSetValues = null;
 
-      if (pendingQty.compareTo(BigDecimal.ZERO) > 0) {
-        orderLine.setDeliveredQuantity(orderLine.getOrderedQuantity());
-      }
-
       if (negativeLine && pendingQty.compareTo(BigDecimal.ZERO) > 0) {
         lineNo += 10;
         Locator binForReturn = null;
@@ -1254,8 +1256,6 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
             objShipmentInOutLine.setMovementQuantity(objShipmentInOutLine.getMovementQuantity()
                 .add(pendingQty));
             OBDal.getInstance().save(objShipmentInOutLine);
-            orderLine.setDeliveredQuantity(orderLine.getDeliveredQuantity().add(pendingQty));
-
           } else {
             addShipmentline(shipment, shplineentity, orderlines.getJSONObject(i), orderLine,
                 jsonorder, lineNo, pendingQty, loc, oldAttributeSetValues, i);
@@ -1283,6 +1283,7 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
     line.setSalesOrderLine(orderLine);
 
     orderLine.getMaterialMgmtShipmentInOutLineList().add(line);
+
     line.setMovementQuantity(qty);
     line.setStorageBin(bin);
     if (jsonOrderLine.has("attributeValue") && jsonOrderLine.get("attributeValue") != null) {
@@ -1343,10 +1344,6 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
     int pricePrecision = order.getCurrency().getObposPosprecision() == null ? order.getCurrency()
         .getPricePrecision().intValue() : order.getCurrency().getObposPosprecision().intValue();
 
-    if (doCancelAndReplace && !newLayaway && !notpaidLayaway && !partialpaidLayaway) {
-      order.setDelivered(true);
-    }
-
     for (int i = 0; i < orderlines.length(); i++) {
 
       OrderLine orderline = OBProvider.getInstance().get(OrderLine.class);
@@ -1355,9 +1352,6 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
         orderline.setId(jsonOrderLine.getString("id"));
         orderline.setNewOBObject(true);
       }
-
-      // make sure it always has a value
-      orderline.setDeliveredQuantity(BigDecimal.ZERO);
 
       JSONPropertyToEntity.fillBobFromJSON(ModelProvider.getInstance().getEntity(OrderLine.class),
           orderline, jsonorder, jsonorder.getLong("timezoneOffset"));
@@ -1372,7 +1366,8 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
       orderline.setLineNetAmount(BigDecimal.valueOf(jsonOrderLine.getDouble("net")).setScale(
           pricePrecision, RoundingMode.HALF_UP));
 
-      if (doCancelAndReplace && !newLayaway && !notpaidLayaway && !partialpaidLayaway) {
+      if (createShipment
+          || (doCancelAndReplace && !newLayaway && !notpaidLayaway && !partialpaidLayaway)) {
         // shipment is created or is a C&R and is not a layaway, so all is delivered
         orderline.setDeliveredQuantity(orderline.getOrderedQuantity());
       }
@@ -1674,6 +1669,9 @@ public class OrderLoader extends POSDataSynchronizationProcess implements
     order.setProcessed(true);
     order.setProcessNow(false);
     order.setObposSendemail((jsonorder.has("sendEmail") && jsonorder.getBoolean("sendEmail")));
+    if (!newLayaway && !isQuotation && !isDeleted) {
+      order.setDelivered(true);
+    }
 
     if (!doCancelAndReplace) {
       if (order.getDocumentNo().indexOf("/") > -1) {
