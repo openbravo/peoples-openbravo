8da7f779a5597fa88860432d4ccd704f42e25e3f
Atul Gaware <atul.gaware@openbravo.com>
2017-09-21 20:24:21 +0530

    Fixes issue 36723: New storage bins are created with Undefined-Overissue status instead of Undefined by default
    ** Set id of Undefined inventory status as default value for column m_inventorystatus_id
    in m_locator table as well as tables and columns in application dictionary
    
    ** Adapt Initialize Inventory Status modulescript to set Undefined-overdue status to
    locator of the clients where allow negative stock flag is set as Yes

--- src-util/modulescript/src/org/openbravo/modulescript/InitializeInventoryStatus.java
+++ src-util/modulescript/src/org/openbravo/modulescript/InitializeInventoryStatus.java
@@ -26,9 +26,12 @@ import org.openbravo.utils.FormatUtilities;
 public class InitializeInventoryStatus extends ModuleScript {
   
   final static String INVENTORY_STATUS_NO_NEGATIVE_STOCK = "7B3DC15A20234C418D26EECDC5D59003";
+  final static String INVENTORY_STATUS_NEGATIVE_STOCK = "0";
 
   @Override
-  // Initialize the Inventory Status for Clients with allowNegativeStock = 'Y'
+  // Initialize the Inventory Status of Locators of clients
+  // with allowNegativeStock = 'Y' as  "Undefined-overissue"
+  // and with allowNegativeStock = 'N' as "Undefined"
   public void execute() {
     try {
       ConnectionProvider cp = getConnectionProvider();
@@ -36,6 +39,8 @@ public class InitializeInventoryStatus extends ModuleScript {
       for(int i =0; i< clients.length;i++){
         if (!InitializeInventoryStatusData.isNegativeStockAllowed(cp, clients[i].adClientId)){
           InitializeInventoryStatusData.initializeInventoryStatus(cp, INVENTORY_STATUS_NO_NEGATIVE_STOCK, clients[i].adClientId);
+        } else {
+          InitializeInventoryStatusData.initializeInventoryStatus(cp, INVENTORY_STATUS_NEGATIVE_STOCK, clients[i].adClientId);
         }
       }
     } catch (Exception e) {
@@ -45,7 +50,7 @@ public class InitializeInventoryStatus extends ModuleScript {
   
   @Override
   protected ModuleScriptExecutionLimits getModuleScriptExecutionLimits() {
-    return new ModuleScriptExecutionLimits("0", new OpenbravoVersion(3,0,31774), 
+    return new ModuleScriptExecutionLimits("0", new OpenbravoVersion(3,0,32778),
         null);
   }
 }
