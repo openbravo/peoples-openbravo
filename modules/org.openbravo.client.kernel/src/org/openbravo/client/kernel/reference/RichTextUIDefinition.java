72e2d4145b1f0d765cd23492feeea1f22cc84ad0
Inigo Sanchez <inigo.sanchez@openbravo.com>
2017-10-11 17:34:31 +0200

    Fixed issue 37051: Rich text area fields causes visibility problems in the grid
    The problem was that the rich text area fields generated visibility problems in the grid under some circumstances.
    (e.g. when a rich text area field has a carriage return or html tags such as div, br,etc.). A common usecase could
    be when a text is copied from a website.
    
    This visibility problems occurred because in form view this type of fields are visualized as a rich text editor and
    html tags are supported but in grid view are visualized as a normal field in the grid and this html tags are not
    properly handled by SC.
    
    In order to avoid this grid visibility problems, when showing a rich text area field in grid view, now it is escaped
    the html code. Visibility in form view is not changed. Now the problem has been fixed by taking account this situation
    in getGridFieldProperties method of the RichTextUIDefinition reference.

--- modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/reference/RichTextUIDefinition.java
+++ modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/reference/RichTextUIDefinition.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2011 Openbravo SLU
+ * All portions are Copyright (C) 2010-2017 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -50,8 +50,7 @@ public class RichTextUIDefinition extends TextUIDefinition {
       rowSpan = 2L;
 
     return super.getGridFieldProperties(field)
-        + ",  escapeHTML: false , canEdit: false , rowSpan : " + rowSpan + " , colSpan : "
-        + colSpan;
+        + ",  escapeHTML: true , canEdit: false , rowSpan : " + rowSpan + " , colSpan : " + colSpan;
   }
 
 }
