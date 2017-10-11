ffbbdc1b16fbc0974ef46a01d685eec0a0fea89e
David Miguelez <david.miguelez@openbravo.com>
2017-10-11 11:12:21 +0200

    Related to Issue 36693. Code Review Changes:
    * Improved error message when standard precission is a negative number
    * Improved some Java Doc
    * Removed some unnecessary Java Doc
    * Reordering tags in definition of constants to compile with standards
    * Rearranged Code to improve redability
    * Added log.error when file has not been found

--- src/org/openbravo/erpCommon/ad_callouts/SL_Currency_StdPrecision.java
+++ src/org/openbravo/erpCommon/ad_callouts/SL_Currency_StdPrecision.java
@@ -38,22 +38,22 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
- * Callout used to validate the currency standard precision is correct. This is, validate with
- * specified currency precision in ISO 4217 Currency codes. If the new precision is higher than
- * expected in the specification, then a warning is showed to user.
+ * Callout used to validate that the currency standard precision is correct. This is, validate with
+ * the specified currency precision in ISO 4217 Currency codes. If the new precision is higher than
+ * expected in the specification, then a warning is shown to user.
  * 
  * @author Mark
  *
  */
 public class SL_Currency_StdPrecision extends SimpleCallout {
 
-  private final static int DEFAULT_CURRENCY_STANDARD_PRECISION = 2;
-  private final static String ISO_4217_XML_FILE_NAME = "ISO_4217.xml";
-  private final static String SOURCE_RELATIVE_PATH = "/src/org/openbravo/erpCommon/ad_callouts/";
-  private final static String CURRENCY_TAG_NAME = "CcyNtry";
-  private final static String CURRENCY_ISO_CODE_TAG_NAME = "Ccy";
-  private final static String CURRENCY_PRECISION_TAG_NAME = "CcyMnrUnts";
-  private final static String CURRENCY_PRECISION_NOT_ASSIGNED = "N.A.";
+  private static final int DEFAULT_CURRENCY_STANDARD_PRECISION = 2;
+  private static final String ISO_4217_XML_FILE_NAME = "ISO_4217.xml";
+  private static final String SOURCE_RELATIVE_PATH = "/src/org/openbravo/erpCommon/ad_callouts/";
+  private static final String CURRENCY_TAG_NAME = "CcyNtry";
+  private static final String CURRENCY_ISO_CODE_TAG_NAME = "Ccy";
+  private static final String CURRENCY_PRECISION_TAG_NAME = "CcyMnrUnts";
+  private static final String CURRENCY_PRECISION_NOT_ASSIGNED = "N.A.";
 
   @Override
   protected void execute(CalloutInfo info) throws ServletException {
@@ -63,7 +63,7 @@ public class SL_Currency_StdPrecision extends SimpleCallout {
     final String paramISOCode = info.getStringParameter("inpisoCode");
 
     if (StringUtils.isNotEmpty(paramStandardPrecision) && StringUtils.isNotEmpty(paramISOCode)) {
-      final int stdPrecision = Integer.valueOf(paramStandardPrecision).intValue();
+      final int stdPrecision = Integer.parseInt(paramStandardPrecision);
       if (stdPrecision < 0) {
         info.addResult("inpstdprecision", DEFAULT_CURRENCY_STANDARD_PRECISION);
         info.showError(Utility.messageBD(this, "CurrencyStdPrecisionNegative",
@@ -80,9 +80,9 @@ public class SL_Currency_StdPrecision extends SimpleCallout {
   }
 
   /**
-   * Found defined currency precision for specific ISO Code in ISO 4217 XML specification. If there
-   * is no currency defined for ISO Code parameter, then it returns the default standard precision
-   * value, that is usually 2.
+   * Find defined currency precision for specific ISO Code in ISO 4217 XML specification. If there
+   * is no currency defined for ISO Code parameter, then it returns 2 as the default standard
+   * precision value.
    * 
    * @param paramISOCode
    *          Currency ISO Code identifier to be found
@@ -100,63 +100,23 @@ public class SL_Currency_StdPrecision extends SimpleCallout {
 
       if (iso4217XML.exists()) {
         Document doc = createDocumentFromFile(iso4217XML);
-
         // Get all currency nodes
         NodeList ccyList = doc.getElementsByTagName(CURRENCY_TAG_NAME);
-        int ccyCount = ccyList.getLength();
-
-        // Iterate them to find currency specification for currency iso code param
-        for (int currentIndex = 0; currentIndex < ccyCount; currentIndex++) {
-          Node ccyNode = ccyList.item(currentIndex);
-
-          // Checks the current node is an ELEMENT NODE
-          if (ccyNode.getNodeType() == Node.ELEMENT_NODE) {
-
-            // Gets the ISO Code and precision of current node
-            String ccyIsoCode = getFirstValueWithTagName(ccyNode, CURRENCY_ISO_CODE_TAG_NAME);
-            String ccyPrecision = getFirstValueWithTagName(ccyNode, CURRENCY_PRECISION_TAG_NAME);
-
-            // If current currency matches with passed as parameter and it is precision defined on
-            // XML specification then return it.
-            if (StringUtils.isNotEmpty(ccyIsoCode) && StringUtils.equals(paramISOCode, ccyIsoCode)
-                && StringUtils.isNotEmpty(ccyPrecision)
-                && !StringUtils.equals(ccyPrecision, CURRENCY_PRECISION_NOT_ASSIGNED)) {
-              return Integer.valueOf(ccyPrecision).intValue();
-            }
-          }
-        }
+        return getPrecissionForCurrencyFromList(paramISOCode, ccyList);
+      } else {
+        log4j.error("SL_Currency_StdPrecision: No ISO0417 XML file found: " + SOURCE_RELATIVE_PATH
+            + ISO_4217_XML_FILE_NAME);
+        return DEFAULT_CURRENCY_STANDARD_PRECISION;
       }
+
     } catch (Exception e) {
       throw new OBException(e.getMessage(), e);
     } finally {
       log4j.debug("SL_Currency_StdPrecision - Ending getCurrencyPrecisionInISO4217Spec at: "
           + new Date());
     }
-
-    // Return the default currency standard precision
-    return DEFAULT_CURRENCY_STANDARD_PRECISION;
   }
 
-  /**
-   * Returns the first tag value for specific tagName. If no tag is found then it returns null
-   * 
-   * @param ccyNode
-   *          The node from the tagName will be searched
-   * @param tagName
-   *          The tagName to be found
-   * @return
-   */
-  private String getFirstValueWithTagName(Node ccyNode, String tagName) {
-    Element element = (Element) ccyNode;
-    NodeList tags = element.getElementsByTagName(tagName);
-    return tags.getLength() > 0 ? tags.item(0).getTextContent() : null;
-  }
-
-  /**
-   * Load the XML file with ISO 4217 Specification
-   * 
-   * @return The XML file
-   */
   private File loadISO4217XMLFile() {
     long t1 = System.currentTimeMillis();
     String obDir = OBPropertiesProvider.getInstance().getOpenbravoProperties()
@@ -167,16 +127,6 @@ public class SL_Currency_StdPrecision extends SimpleCallout {
     return iso4217XML;
   }
 
-  /**
-   * Creates a document to parse an XML file
-   * 
-   * @param xmlFile
-   *          The XML File to be parsed
-   * @return The created document
-   * @throws ParserConfigurationException
-   * @throws SAXException
-   * @throws IOException
-   */
   private Document createDocumentFromFile(File xmlFile) throws ParserConfigurationException,
       SAXException, IOException {
     long t1 = System.currentTimeMillis();
@@ -187,5 +137,39 @@ public class SL_Currency_StdPrecision extends SimpleCallout {
     long t2 = System.currentTimeMillis();
     log4j.debug("SL_Currency_StdPrecision - createDocumentFromFile took: " + (t2 - t1) + " ms");
     return doc;
+  }
+
+  private int getPrecissionForCurrencyFromList(String paramISOCode, NodeList ccyList) {
+    // Iterate them to find currency specification for currency iso code param
+    for (int currentIndex = 0; currentIndex < ccyList.getLength(); currentIndex++) {
+      Node ccyNode = ccyList.item(currentIndex);
+
+      // Checks the current node is an ELEMENT NODE
+      if (ccyNode.getNodeType() == Node.ELEMENT_NODE) {
+
+        // Gets the ISO Code and precision of current node
+        String ccyIsoCode = getFirstValueWithTagName(ccyNode, CURRENCY_ISO_CODE_TAG_NAME);
+        String ccyPrecision = getFirstValueWithTagName(ccyNode, CURRENCY_PRECISION_TAG_NAME);
+
+        if (isSameCurrencyAsParameterAndIsPrecissionDefined(paramISOCode, ccyIsoCode, ccyPrecision)) {
+          return Integer.parseInt(ccyPrecision);
+        }
+      }
+    }
+    // If no match has been found or no precision defined, return the default value
+    return DEFAULT_CURRENCY_STANDARD_PRECISION;
+  }
+
+  private String getFirstValueWithTagName(Node ccyNode, String tagName) {
+    Element element = (Element) ccyNode;
+    NodeList tags = element.getElementsByTagName(tagName);
+    return tags.getLength() > 0 ? tags.item(0).getTextContent() : null;
+  }
+
+  private boolean isSameCurrencyAsParameterAndIsPrecissionDefined(String paramISOCode,
+      String ccyIsoCode, String ccyPrecision) {
+    return StringUtils.isNotEmpty(ccyIsoCode) && StringUtils.equals(paramISOCode, ccyIsoCode)
+        && StringUtils.isNotEmpty(ccyPrecision)
+        && !StringUtils.equals(ccyPrecision, CURRENCY_PRECISION_NOT_ASSIGNED);
   }
 }
