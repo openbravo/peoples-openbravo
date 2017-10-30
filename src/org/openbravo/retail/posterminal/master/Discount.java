13962393286b33eddf860d4d4bd9ca59c1fa850f
Ranjith S R <ranjith@qualiantech.com>
2017-10-30 20:12:30 +0530

    Fixed issue 37125 : Validate incrementalUpdateCriteria for DiscountFilterProduct

--- src/org/openbravo/retail/posterminal/master/Discount.java
+++ src/org/openbravo/retail/posterminal/master/Discount.java
@@ -104,8 +104,10 @@ public class Discount extends ProcessHQLQuery {
     hql += "      where pap.active = true and pap.priceAdjustment = p ";
     hql += "      and pap.product.id = ppl.product.id ";
     hql += "      and ppl.active = true and pap.product.active = true ";
-    hql += "      and ((ppl.$incrementalUpdateCriteria) ";
-    hql += "      or (pap.product.$incrementalUpdateCriteria)) ";
+    if (addIncrementalUpdateFilter) {
+      hql += "      and ((ppl.$incrementalUpdateCriteria) ";
+      hql += "      or (pap.product.$incrementalUpdateCriteria)) ";
+    }
     hql += "      and ppl.obretcoProductlist.id ='" + productList.getId() + "'))) ";
     hql += "  ) ";
 
