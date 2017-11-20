dd7b8d025539705c4729d03d00ddf03efef4b582
Jorge Garcia <jorge.garcia@openbravo.com>
2017-11-20 12:01:56 +0100

    Related to issue 36694. Related to issue 36693
     * Fix problem with log messages in the new API
     * Added copyright to ISOCurrencyPrecision
     * Added class Javadoc to ISOCurrencyPrecision
     * Use getResourceAsStream to read the xml file with the standard
       currency precisions.
    
    --HG--
    extra : rebase_source : 7d926d7e27fc4b694faa19b42f6961861e200a9c

--- src/org/openbravo/erpCommon/utility/ISOCurrencyPrecision.java
+++ src/org/openbravo/erpCommon/utility/ISOCurrencyPrecision.java
@@ -1,7 +1,26 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License.
+ * The Original Code is Openbravo ERP.
+ * The Initial Developer of the Original Code is Openbravo SLU
+ * All portions are Copyright (C) 2017 Openbravo SLU
+ * All Rights Reserved.
+ * Contributor(s):  ______________________________________.
+ *************************************************************************
+ */
+
 package org.openbravo.erpCommon.utility;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -11,18 +30,25 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.openbravo.base.exception.OBException;
-import org.openbravo.base.session.OBPropertiesProvider;
+import org.openbravo.erpCommon.ad_callouts.SL_Currency_StdPrecision;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+/**
+ * Returns the standard currency precission for a specific currency defined in
+ * ISO 4217. If not found in ISO, returns a standard precision of 2.
+ *
+ * @see SL_Currency_StdPrecision
+ *
+ * @author markmm82
+ * @author jorge-garcia
+ */
 public class ISOCurrencyPrecision {
-  private static Logger log4j = Logger.getLogger(HttpsUtils.class);
+  private static Logger log4j = Logger.getLogger(ISOCurrencyPrecision.class);
   private static final int DEFAULT_CURRENCY_STANDARD_PRECISION = 2;
-  private static final String ISO_4217_XML_FILE_NAME = "ISO_4217.xml";
-  private static final String SOURCE_RELATIVE_PATH = "/src/org/openbravo/erpCommon/ad_callouts/";
   private static final String CURRENCY_TAG_NAME = "CcyNtry";
   private static final String CURRENCY_ISO_CODE_TAG_NAME = "Ccy";
   private static final String CURRENCY_PRECISION_TAG_NAME = "CcyMnrUnts";
@@ -39,53 +65,42 @@ public class ISOCurrencyPrecision {
    *         for currency standard precision
    */
   public static int getCurrencyPrecisionInISO4217Spec(String paramISOCode) {
-
-    log4j.debug("SL_Currency_StdPrecision - Starting getCurrencyPrecisionInISO4217Spec at: "
-        + new Date());
+    log4j.debug("Starting getCurrencyPrecisionInISO4217Spec at: " + new Date());
 
     try {
-      // Load the XML file with ISO 4217 Specification
-      File iso4217XML = loadISO4217XMLFile();
-
-      if (iso4217XML.exists()) {
-        Document doc = createDocumentFromFile(iso4217XML);
+      Document doc = getISOCurrencyDocument();
+      if (doc != null) {
         // Get all currency nodes
         NodeList ccyList = doc.getElementsByTagName(CURRENCY_TAG_NAME);
         return getPrecisionForCurrencyFromList(paramISOCode, ccyList);
       } else {
-        log4j.error("SL_Currency_StdPrecision: No ISO0417 XML file found: " + SOURCE_RELATIVE_PATH
-            + ISO_4217_XML_FILE_NAME);
+        log4j.error("No ISO0417 XML file found.");
         return DEFAULT_CURRENCY_STANDARD_PRECISION;
       }
-
     } catch (Exception e) {
       throw new OBException(e.getMessage(), e);
     } finally {
-      log4j.debug("SL_Currency_StdPrecision - Ending getCurrencyPrecisionInISO4217Spec at: "
-          + new Date());
+      log4j.debug("Ending getCurrencyPrecisionInISO4217Spec at: " + new Date());
     }
   }
 
-  private static File loadISO4217XMLFile() {
+  private static Document getISOCurrencyDocument() throws IOException,
+      ParserConfigurationException, SAXException {
     long t1 = System.currentTimeMillis();
-    String obDir = OBPropertiesProvider.getInstance().getOpenbravoProperties()
-        .getProperty("source.path");
-    File iso4217XML = new File(obDir + SOURCE_RELATIVE_PATH, ISO_4217_XML_FILE_NAME);
-    long t2 = System.currentTimeMillis();
-    log4j.debug("SL_Currency_StdPrecision - loadISO4217XMLFile took: " + (t2 - t1) + " ms");
-    return iso4217XML;
-  }
+    try (InputStream isoXMLDoc = ISOCurrencyPrecision.class
+        .getResourceAsStream("../ad_callouts/ISO_4217.xml")) {
+      if (isoXMLDoc == null) {
+        return null;
+      }
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 
-  private static Document createDocumentFromFile(File xmlFile) throws ParserConfigurationException,
-      SAXException, IOException {
-    long t1 = System.currentTimeMillis();
-    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-    Document doc = dBuilder.parse(xmlFile);
-    doc.getDocumentElement().normalize();
-    long t2 = System.currentTimeMillis();
-    log4j.debug("SL_Currency_StdPrecision - createDocumentFromFile took: " + (t2 - t1) + " ms");
-    return doc;
+      Document doc = dBuilder.parse(isoXMLDoc);
+      doc.getDocumentElement().normalize();
+      long t2 = System.currentTimeMillis();
+      log4j.debug("createDocumentFromFile took: " + (t2 - t1) + " ms");
+      return doc;
+    }
   }
 
   private static int getPrecisionForCurrencyFromList(String paramISOCode, NodeList ccyList) {
