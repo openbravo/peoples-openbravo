8704057cbbb05e1b2f75a90e4fa35451b579cb83
Asier Lostalé <asier.lostale@openbravo.com>
2017-11-22 16:15:31 +0100

    fixed bug 37363: linked items fails in Organization window
      All virtual entities generated for lazy initialization of computed columns include
      client/org properties, causing linked items to fail as they were handled as standard
      windows.
    
      They are now excluded.

--- src/org/openbravo/erpCommon/utility/UsedByLink.java
+++ src/org/openbravo/erpCommon/utility/UsedByLink.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2001-2016 Openbravo SLU 
+ * All portions are Copyright (C) 2001-2017 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -685,7 +685,7 @@ public class UsedByLink extends HttpSecureAppServlet {
           if (!property.isOneToMany() && property.getColumnName() != null
               && property.getTargetEntity() != null
               && property.getTargetEntity().getTableName().equalsIgnoreCase(tableName)
-              && !property.isComputedColumn()) {
+              && !property.isComputedColumn() && !property.getEntity().isVirtualEntity()) {
             // Datasource tables are skipped
             linkedTableId = property.getEntity().getTableId();
             linkedTableObject = OBDal.getInstance().get(Table.class, linkedTableId);
