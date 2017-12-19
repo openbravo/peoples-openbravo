09bcff27a7343bed0338d41e1be587a9d962ffce
Jorge Garcia <jorge.garcia@openbravo.com>
2017-12-19 15:45:04 +0100

    Fixed issue 37044: [SER QA 3365] Wrong ticket amount when action made before ticket total amount calculated
    The solution for this issue is to show a blocking screen during the calculation
    of the ticket. Exists two ways to activate the new functionality:
      * At POS Terminal Type window, there is a new check called “Block screen
         when processing ticket”. By deafult is uncheked
      * At preference level, called "Block screen when processing ticket on slow
         devices". By default, is set to ‘Y’
    
    If the conditions are met, a screen is shown during the calculation of the receipt
    and the WebPOS is blocked. If one of these conditions are met, the blocking
    screen is shown:
      * If the value Block screen when processing ticket” is checked
      * If the preference “Block screen when processing ticket on slow devices” is
         set to ‘Y’ and the benchmark is less than 1000.
    
    When the calculateReceipt is started, a transparent blocking screen is show to
    block the webPOS. If the receipt calculation takes more than one second, a
    semitransparent screen with a popupmessage is show to indicate the calculation
    is ongoing. At the end of the calculateReceipt, all blocking screen are removed.
    
    --HG--
    extra : rebase_source : 6c2e372ff8636e7fadabfaff9bce7145563b60cf

--- web/org.openbravo.retail.posterminal/js/utils/preScanningFocusHook.js
+++ web/org.openbravo.retail.posterminal/js/utils/preScanningFocusHook.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2016 Openbravo S.L.U.
+ * Copyright (C) 2016-2017 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -9,7 +9,7 @@
 
 OB.UTIL.HookManager.registerHook('OBMOBC_PreScanningFocus', function (args, callbacks) {
   if (OB.UTIL.RfidController.isRfidConfigured() && OB.UTIL.RfidController.get('rfidWebsocket')) {
-    if (args.scanMode === true && !OB.UTIL.RfidController.get('isRFIDEnabled') && OB.UTIL.RfidController.get('reconnectOnScanningFocus') && (OB.MobileApp.view.originalRFIDMode || (OB.MobileApp.view.originalRFIDMode === false && OB.MobileApp.model.get('lastPaneShown') !== 'payment'))) {
+    if ((OB.UTIL.isNullOrUndefined(args.scanMode) || args.scanMode === true) && !OB.UTIL.RfidController.get('isRFIDEnabled') && OB.UTIL.RfidController.get('reconnectOnScanningFocus') && (OB.MobileApp.view.originalRFIDMode || (OB.MobileApp.view.originalRFIDMode === false && OB.MobileApp.model.get('lastPaneShown') !== 'payment'))) {
       OB.UTIL.RfidController.connectRFIDDevice();
     } else if (args.scanMode === false && OB.UTIL.RfidController.get('isRFIDEnabled')) {
       OB.UTIL.RfidController.disconnectRFIDDevice();
