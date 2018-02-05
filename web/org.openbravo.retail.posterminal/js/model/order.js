bd4b0cc976545053e16d85af6316f3b52b64fccd
Ranjith S R <ranjith@qualiantech.com>
2018-02-05 14:02:31 +0530

    Fixed issue 37507 : Refactor PreSyncReceipt for receipts and multiorders
    * Added PreSyncReceipt for deleting ticket if savedelete ticket prefernece is enabled

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -5216,16 +5216,21 @@
           model.get('lines').at(i).set('lineGrossAmount', 0);
         }
         model.set('hasbeenpaid', 'Y');
-        OB.MobileApp.model.updateDocumentSequenceWhenOrderSaved(model.get('documentnoSuffix'), model.get('quotationnoSuffix'), model.get('returnnoSuffix'), function () {
-          model.save(function () {
-            if (orderList) {
-              orderList.deleteCurrent();
-              orderList.synchronizeCurrentOrder();
-            }
-            model.setIsCalculateGrossLockState(false);
-            if (callback && callback instanceof Function) {
-              callback();
-            }
+        OB.UTIL.HookManager.executeHooks('OBPOS_PreSyncReceipt', {
+          receipt: model,
+          model: model
+        }, function (args) {
+          OB.MobileApp.model.updateDocumentSequenceWhenOrderSaved(model.get('documentnoSuffix'), model.get('quotationnoSuffix'), model.get('returnnoSuffix'), function () {
+            model.save(function () {
+              if (orderList) {
+                orderList.deleteCurrent();
+                orderList.synchronizeCurrentOrder();
+              }
+              model.setIsCalculateGrossLockState(false);
+              if (callback && callback instanceof Function) {
+                callback();
+              }
+            });
           });
         });
       }
