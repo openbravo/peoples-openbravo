c0ad616ba40aac624b52932aea7f1d5faea72190
Mark <markmm82@gmail.com>
2018-02-08 16:23:59 -0500

    Fixes issue 37843: OrderEventHandler does useless and repeated queries when not needed.
    OrderEventHandler class has been refactorized:
    - Removed unused vars.
    - Renamed newWarehouseId to newWarehouse as it store a Warehouse object instead of an ID
    - Renamed oldWarehouseId to oldWarehouse as it store a Warehouse object instead of an ID
    - Only executed query to get preferences after verify that values have changed. This way
      it avoids to execute unneeded queries.
    - Saved the orderLineCriteria.list() in a variable to avoid execute the same query more than once.
    - Merged the three used loops updating fields in only one that updates fields if needed.
    - Only loop is executed if at least one of the expected fields is changed and it is not
      activated the related preference.
    - Created new methods to make code more cleaned and reuse in similar cases.
    
    --HG--
    extra : rebase_source : 3c3deea265205b943d809be85a6a538ab2ddb3a2

--- src/org/openbravo/event/OrderEventHandler.java
+++ src/org/openbravo/event/OrderEventHandler.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2013-2017 Openbravo SLU 
+ * All portions are Copyright (C) 2013-2018 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -19,9 +19,11 @@
 package org.openbravo.event;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.enterprise.event.Observes;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
@@ -29,6 +31,7 @@ import org.hibernate.criterion.Restrictions;
 import org.openbravo.base.model.Entity;
 import org.openbravo.base.model.ModelProvider;
 import org.openbravo.base.model.Property;
+import org.openbravo.base.structure.BaseOBObject;
 import org.openbravo.client.kernel.event.EntityDeleteEvent;
 import org.openbravo.client.kernel.event.EntityPersistenceEventObserver;
 import org.openbravo.client.kernel.event.EntityUpdateEvent;
@@ -53,8 +56,7 @@ public class OrderEventHandler extends EntityPersistenceEventObserver {
     return entities;
   }
 
-  public void onUpdate(@Observes
-  EntityUpdateEvent event) {
+  public void onUpdate(@Observes EntityUpdateEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
@@ -65,65 +67,38 @@ public class OrderEventHandler extends EntityPersistenceEventObserver {
     final Property warehouseProperty = orderEntity.getProperty(Order.PROPERTY_WAREHOUSE);
     final Property businessPartnerProperty = orderEntity
         .getProperty(Order.PROPERTY_BUSINESSPARTNER);
-    String syncDateOrdered = null, syncDateDelivered = null, syncWarehouse = null;
     String orderId = (String) event.getTargetInstance().getId();
     Date newOrderDate = (Date) event.getCurrentState(orderDateProperty);
     Date oldOrderDate = (Date) event.getPreviousState(orderDateProperty);
     Date newScheduledDate = (Date) event.getCurrentState(scheduledDateProperty);
     Date oldScheduledDate = (Date) event.getPreviousState(scheduledDateProperty);
-    Warehouse newWarehouseId = (Warehouse) event.getCurrentState(warehouseProperty);
-    Warehouse oldWarehouseId = (Warehouse) event.getPreviousState(warehouseProperty);
+    Warehouse newWarehouse = (Warehouse) event.getCurrentState(warehouseProperty);
+    Warehouse oldWarehouse = (Warehouse) event.getPreviousState(warehouseProperty);
     String newBPId = ((BusinessPartner) event.getCurrentState(businessPartnerProperty)).getId();
     String oldBPId = ((BusinessPartner) event.getPreviousState(businessPartnerProperty)).getId();
 
-    // Check whether the preference is set to sync with order header
-    try {
-      syncDateOrdered = Preferences.getPreferenceValue("DoNotSyncDateOrdered", true, OBContext
-          .getOBContext().getCurrentClient(), OBContext.getOBContext().getCurrentOrganization(),
-          OBContext.getOBContext().getUser(), OBContext.getOBContext().getRole(), null);
-    } catch (PropertyException e) {
-      // if property not found, sync the ordered date
-      syncDateOrdered = Preferences.NO;
-    }
-    try {
-      syncDateDelivered = Preferences.getPreferenceValue("DoNotSyncDateDelivered", true, OBContext
-          .getOBContext().getCurrentClient(), OBContext.getOBContext().getCurrentOrganization(),
-          OBContext.getOBContext().getUser(), OBContext.getOBContext().getRole(), null);
-    } catch (PropertyException e) {
-      // if property not found, sync the delivered date
-      syncDateDelivered = Preferences.NO;
-    }
     OBCriteria<OrderLine> orderLineCriteria = OBDal.getInstance().createCriteria(OrderLine.class);
     orderLineCriteria.add(Restrictions.eq(OrderLine.PROPERTY_SALESORDER,
         OBDal.getInstance().get(Order.class, orderId)));
-    if (orderLineCriteria.count() > 0) {
-      if (newOrderDate.compareTo(oldOrderDate) != 0 && !Preferences.YES.equals(syncDateOrdered)) {
-        for (OrderLine lines : orderLineCriteria.list()) {
-          lines.setOrderDate(newOrderDate);
-        }
-      }
-      if (newScheduledDate != null && oldScheduledDate != null
-          && newScheduledDate.compareTo(oldScheduledDate) != 0
-          && !Preferences.YES.equals(syncDateDelivered)) {
-        for (OrderLine lines : orderLineCriteria.list()) {
-          lines.setScheduledDeliveryDate(newScheduledDate);
-        }
-      }
-      // check preferences is set to sync warehouse in header and lines
-      if (newWarehouseId != null && oldWarehouseId != null
-          && !newWarehouseId.getId().equals(oldWarehouseId.getId())) {
-        try {
-          syncWarehouse = Preferences.getPreferenceValue("DoNotSyncWarehouse", true, OBContext
-              .getOBContext().getCurrentClient(),
-              OBContext.getOBContext().getCurrentOrganization(),
-              OBContext.getOBContext().getUser(), OBContext.getOBContext().getRole(), null);
-        } catch (PropertyException e) {
-          // if property not found, sync the warehouse
-          syncWarehouse = Preferences.NO;
-        }
-        if (!Preferences.YES.equals(syncWarehouse)) {
-          for (OrderLine lines : orderLineCriteria.list()) {
-            lines.setWarehouse(newWarehouseId);
+    List<OrderLine> orderLines = orderLineCriteria.list();
+    if (CollectionUtils.isNotEmpty(orderLines)) {
+      boolean syncOrderDate = isDateChangedAndPreferenceIsNotActivated(newOrderDate, oldOrderDate,
+          "DoNotSyncDateOrdered");
+      boolean syncDeliveredDate = isDateChangedAndPreferenceIsNotActivated(newScheduledDate,
+          oldScheduledDate, "DoNotSyncDateDelivered");
+      boolean syncWarehouse = isIdChangedAndPreferenceIsNotActivated(newWarehouse, oldWarehouse,
+          "DoNotSyncWarehouse");
+
+      if (syncOrderDate || syncDeliveredDate || syncWarehouse) {
+        for (OrderLine lines : orderLines) {
+          if (syncOrderDate) {
+            lines.setOrderDate(newOrderDate);
+          }
+          if (syncDeliveredDate) {
+            lines.setScheduledDeliveryDate(newScheduledDate);
+          }
+          if (syncWarehouse) {
+            lines.setWarehouse(newWarehouse);
           }
         }
       }
@@ -140,8 +115,71 @@ public class OrderEventHandler extends EntityPersistenceEventObserver {
     }
   }
 
-  public void onDelete(@Observes
-  EntityDeleteEvent event) {
+  /**
+   * Returns if the field should be sync or not taking into account the date has been changed and
+   * passed preference doesn't exists or it is not active
+   * 
+   * @param newDate
+   *          The new date value
+   * @param oldDate
+   *          The old date value
+   * @param preferenceSearchKey
+   *          The preference search key
+   * @return true if the field should be updated or false if not
+   */
+  private boolean isDateChangedAndPreferenceIsNotActivated(Date newDate, Date oldDate,
+      String preferenceSearchKey) {
+    boolean syncField = false;
+    if (newDate != null && oldDate != null && newDate.compareTo(oldDate) != 0) {
+      syncField = !StringUtils.equals(Preferences.YES, getPreferenceValue(preferenceSearchKey));
+    }
+    return syncField;
+  }
+
+  /**
+   * Returns if the field should be sync or not taking into account the object ID has been changed
+   * and passed preference doesn't exists or it is not active. It compares IDs for any BaseOBObject
+   * 
+   * @param newObj
+   *          The new object value
+   * @param oldObj
+   *          The old object value
+   * @param preferenceSearchKey
+   *          The preference search key
+   * @return true if the field should be updated or false if not
+   */
+  private boolean isIdChangedAndPreferenceIsNotActivated(BaseOBObject newObj, BaseOBObject oldObj,
+      String preferenceSearchKey) {
+    boolean syncField = false;
+    if (newObj != null && oldObj != null
+        && !StringUtils.equals(newObj.getId().toString(), oldObj.getId().toString())) {
+      syncField = !StringUtils.equals(Preferences.YES, getPreferenceValue(preferenceSearchKey));
+    }
+    return syncField;
+  }
+
+  /**
+   * Returns the property value of a preference with a certain search key, or Preferences.NO if the
+   * property can't be found.
+   * 
+   * @param preferenceKey
+   *          The preference search key
+   * @return Preferences.NO If the propery is not found or the property value if it exists
+   */
+  private String getPreferenceValue(String preferenceKey) {
+    String syncField;
+    try {
+      syncField = Preferences.getPreferenceValue(preferenceKey, true, OBContext.getOBContext()
+          .getCurrentClient(), OBContext.getOBContext().getCurrentOrganization(), OBContext
+          .getOBContext().getUser(), OBContext.getOBContext().getRole(), null);
+    } catch (PropertyException e) {
+      // if property not found, sync the field
+      syncField = Preferences.NO;
+    }
+    return syncField;
+  }
+
+  public void onDelete(@Observes EntityDeleteEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
