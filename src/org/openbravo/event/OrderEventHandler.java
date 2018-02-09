3240e9b1d5318964d6177e5c69002abc05ec1291
David Miguelez <david.miguelez@openbravo.com>
2018-02-09 12:43:18 +0100

    Related to Issue 37843. Code Review changes.
    Refactor code to improve redability:
    * Encapsulated order parameters into a sub class that can be
      shared between methods
    * Split code of methods to reduce cognitive complexity
    * Removed comments since the code is self explanatory enough
    
    --HG--
    extra : rebase_source : 581e1eea0d591be7564fb2cdfca951497369b166

--- src/org/openbravo/event/OrderEventHandler.java
+++ src/org/openbravo/event/OrderEventHandler.java
@@ -48,6 +48,9 @@ import org.openbravo.model.common.order.OrderLine;
 
 public class OrderEventHandler extends EntityPersistenceEventObserver {
 
+  private static final String DO_NOT_SYNC_WAREHOUSE_PREFERENCE = "DoNotSyncWarehouse";
+  private static final String DO_NOT_SYNC_DATE_DELIVERED_PREFERENCE = "DoNotSyncDateDelivered";
+  private static final String DO_NOT_SYNC_DATE_ORDERED_PREFERENCE = "DoNotSyncDateOrdered";
   private static Entity[] entities = { ModelProvider.getInstance().getEntity(Order.ENTITY_NAME) };
   protected Logger logger = Logger.getLogger(this.getClass());
 
@@ -60,123 +63,17 @@ public class OrderEventHandler extends EntityPersistenceEventObserver {
     if (!isValidEvent(event)) {
       return;
     }
-    final Entity orderEntity = ModelProvider.getInstance().getEntity(Order.ENTITY_NAME);
-    final Property orderDateProperty = orderEntity.getProperty(Order.PROPERTY_ORDERDATE);
-    final Property scheduledDateProperty = orderEntity
-        .getProperty(Order.PROPERTY_SCHEDULEDDELIVERYDATE);
-    final Property warehouseProperty = orderEntity.getProperty(Order.PROPERTY_WAREHOUSE);
-    final Property businessPartnerProperty = orderEntity
-        .getProperty(Order.PROPERTY_BUSINESSPARTNER);
-    String orderId = (String) event.getTargetInstance().getId();
-    Date newOrderDate = (Date) event.getCurrentState(orderDateProperty);
-    Date oldOrderDate = (Date) event.getPreviousState(orderDateProperty);
-    Date newScheduledDate = (Date) event.getCurrentState(scheduledDateProperty);
-    Date oldScheduledDate = (Date) event.getPreviousState(scheduledDateProperty);
-    Warehouse newWarehouse = (Warehouse) event.getCurrentState(warehouseProperty);
-    Warehouse oldWarehouse = (Warehouse) event.getPreviousState(warehouseProperty);
-    String newBPId = ((BusinessPartner) event.getCurrentState(businessPartnerProperty)).getId();
-    String oldBPId = ((BusinessPartner) event.getPreviousState(businessPartnerProperty)).getId();
 
-    OBCriteria<OrderLine> orderLineCriteria = OBDal.getInstance().createCriteria(OrderLine.class);
-    orderLineCriteria.add(Restrictions.eq(OrderLine.PROPERTY_SALESORDER,
-        OBDal.getInstance().get(Order.class, orderId)));
-    List<OrderLine> orderLines = orderLineCriteria.list();
+    OrderParameters orderParameters = getOrderParameters(event);
+
+    List<OrderLine> orderLines = getOrderLines(orderParameters);
     if (CollectionUtils.isNotEmpty(orderLines)) {
-      boolean syncOrderDate = isDateChangedAndPreferenceIsNotActivated(newOrderDate, oldOrderDate,
-          "DoNotSyncDateOrdered");
-      boolean syncDeliveredDate = isDateChangedAndPreferenceIsNotActivated(newScheduledDate,
-          oldScheduledDate, "DoNotSyncDateDelivered");
-      boolean syncWarehouse = isIdChangedAndPreferenceIsNotActivated(newWarehouse, oldWarehouse,
-          "DoNotSyncWarehouse");
-
-      if (syncOrderDate || syncDeliveredDate || syncWarehouse) {
-        for (OrderLine lines : orderLines) {
-          if (syncOrderDate) {
-            lines.setOrderDate(newOrderDate);
-          }
-          if (syncDeliveredDate) {
-            lines.setScheduledDeliveryDate(newScheduledDate);
-          }
-          if (syncWarehouse) {
-            lines.setWarehouse(newWarehouse);
-          }
-        }
-      }
+      updateOrderLinesValues(orderParameters, orderLines);
     }
 
-    // Remove discount information
-    if (!StringUtils.equals(newBPId, oldBPId)) {
-      StringBuilder deleteHql = new StringBuilder();
-      deleteHql.append(" delete from " + OrderDiscount.ENTITY_NAME);
-      deleteHql.append(" where " + OrderDiscount.PROPERTY_SALESORDER + ".id = :orderId");
-      Query deleteQry = OBDal.getInstance().getSession().createQuery(deleteHql.toString());
-      deleteQry.setParameter("orderId", orderId);
-      deleteQry.executeUpdate();
+    if (businessPartnerHasChanged(orderParameters)) {
+      removeDiscountInformationFromOrder(orderParameters);
     }
-  }
-
-  /**
-   * Returns if the field should be sync or not taking into account the date has been changed and
-   * passed preference doesn't exists or it is not active
-   * 
-   * @param newDate
-   *          The new date value
-   * @param oldDate
-   *          The old date value
-   * @param preferenceSearchKey
-   *          The preference search key
-   * @return true if the field should be updated or false if not
-   */
-  private boolean isDateChangedAndPreferenceIsNotActivated(Date newDate, Date oldDate,
-      String preferenceSearchKey) {
-    boolean syncField = false;
-    if (newDate != null && oldDate != null && newDate.compareTo(oldDate) != 0) {
-      syncField = !StringUtils.equals(Preferences.YES, getPreferenceValue(preferenceSearchKey));
-    }
-    return syncField;
-  }
-
-  /**
-   * Returns if the field should be sync or not taking into account the object ID has been changed
-   * and passed preference doesn't exists or it is not active. It compares IDs for any BaseOBObject
-   * 
-   * @param newObj
-   *          The new object value
-   * @param oldObj
-   *          The old object value
-   * @param preferenceSearchKey
-   *          The preference search key
-   * @return true if the field should be updated or false if not
-   */
-  private boolean isIdChangedAndPreferenceIsNotActivated(BaseOBObject newObj, BaseOBObject oldObj,
-      String preferenceSearchKey) {
-    boolean syncField = false;
-    if (newObj != null && oldObj != null
-        && !StringUtils.equals(newObj.getId().toString(), oldObj.getId().toString())) {
-      syncField = !StringUtils.equals(Preferences.YES, getPreferenceValue(preferenceSearchKey));
-    }
-    return syncField;
-  }
-
-  /**
-   * Returns the property value of a preference with a certain search key, or Preferences.NO if the
-   * property can't be found.
-   * 
-   * @param preferenceKey
-   *          The preference search key
-   * @return Preferences.NO If the propery is not found or the property value if it exists
-   */
-  private String getPreferenceValue(String preferenceKey) {
-    String syncField;
-    try {
-      syncField = Preferences.getPreferenceValue(preferenceKey, true, OBContext.getOBContext()
-          .getCurrentClient(), OBContext.getOBContext().getCurrentOrganization(), OBContext
-          .getOBContext().getUser(), OBContext.getOBContext().getRole(), null);
-    } catch (PropertyException e) {
-      // if property not found, sync the field
-      syncField = Preferences.NO;
-    }
-    return syncField;
   }
 
   public void onDelete(@Observes EntityDeleteEvent event) {
@@ -189,5 +86,221 @@ public class OrderEventHandler extends EntityPersistenceEventObserver {
     if (quotation != null) {
       quotation.setDocumentStatus("UE");
     }
+  }
+
+  private OrderParameters getOrderParameters(final EntityUpdateEvent event) {
+    OrderParameters orderParameters = new OrderParameters();
+    final Entity orderEntity = ModelProvider.getInstance().getEntity(Order.ENTITY_NAME);
+    final Property orderDateProperty = orderEntity.getProperty(Order.PROPERTY_ORDERDATE);
+    final Property scheduledDateProperty = orderEntity
+        .getProperty(Order.PROPERTY_SCHEDULEDDELIVERYDATE);
+    final Property warehouseProperty = orderEntity.getProperty(Order.PROPERTY_WAREHOUSE);
+    final Property businessPartnerProperty = orderEntity
+        .getProperty(Order.PROPERTY_BUSINESSPARTNER);
+
+    orderParameters.setOrderId((String) event.getTargetInstance().getId());
+    orderParameters.setNewOrderDate((Date) event.getCurrentState(orderDateProperty));
+    orderParameters.setOldOrderDate((Date) event.getPreviousState(orderDateProperty));
+    orderParameters.setNewScheduledDate((Date) event.getCurrentState(scheduledDateProperty));
+    orderParameters.setOldScheduledDate((Date) event.getPreviousState(scheduledDateProperty));
+    orderParameters.setNewWarehouse((Warehouse) event.getCurrentState(warehouseProperty));
+    orderParameters.setOldWarehouse((Warehouse) event.getPreviousState(warehouseProperty));
+    orderParameters.setNewBPId(((BusinessPartner) event.getCurrentState(businessPartnerProperty))
+        .getId());
+    orderParameters.setOldBPId(((BusinessPartner) event.getPreviousState(businessPartnerProperty))
+        .getId());
+
+    return orderParameters;
+  }
+
+  private List<OrderLine> getOrderLines(final OrderParameters orderParameters) {
+    OBCriteria<OrderLine> orderLineCriteria = OBDal.getInstance().createCriteria(OrderLine.class);
+    orderLineCriteria.add(Restrictions.eq(OrderLine.PROPERTY_SALESORDER,
+        OBDal.getInstance().get(Order.class, orderParameters.getOrderId())));
+    return orderLineCriteria.list();
+  }
+
+  private void updateOrderLinesValues(final OrderParameters orderParameters,
+      final List<OrderLine> orderLines) {
+    final boolean syncOrderDate = isOrderDateChangedAndPreferenceIsNotActivated(orderParameters);
+    final boolean syncDeliveredDate = isScheduledDateChangedAndPreferenceIsNotActivated(orderParameters);
+    final boolean syncWarehouse = isWarehouseChangedAndPreferenceIsNotActivated(orderParameters);
+
+    if (syncOrderDate || syncDeliveredDate || syncWarehouse) {
+      for (OrderLine lines : orderLines) {
+        if (syncOrderDate) {
+          lines.setOrderDate(orderParameters.getNewOrderDate());
+        }
+        if (syncDeliveredDate) {
+          lines.setScheduledDeliveryDate(orderParameters.getNewScheduledDate());
+        }
+        if (syncWarehouse) {
+          lines.setWarehouse(orderParameters.getNewWarehouse());
+        }
+      }
+    }
+  }
+
+  private boolean isOrderDateChangedAndPreferenceIsNotActivated(
+      final OrderParameters orderParameters) {
+    boolean syncField = false;
+    if (areDatesDifferent(orderParameters.getNewOrderDate(), orderParameters.getOldOrderDate())) {
+      syncField = !StringUtils.equals(Preferences.YES,
+          getPreferenceValue(DO_NOT_SYNC_DATE_ORDERED_PREFERENCE));
+    }
+    return syncField;
+  }
+
+  private boolean isScheduledDateChangedAndPreferenceIsNotActivated(
+      final OrderParameters orderParameters) {
+    boolean syncField = false;
+    if (areDatesDifferent(orderParameters.getNewScheduledDate(),
+        orderParameters.getOldScheduledDate())) {
+      syncField = !StringUtils.equals(Preferences.YES,
+          getPreferenceValue(DO_NOT_SYNC_DATE_DELIVERED_PREFERENCE));
+    }
+    return syncField;
+  }
+
+  private boolean isWarehouseChangedAndPreferenceIsNotActivated(
+      final OrderParameters orderParameters) {
+    boolean syncField = false;
+    if (areObjectsDifferent(orderParameters.getNewWarehouse(), orderParameters.getOldWarehouse())) {
+      syncField = !StringUtils.equals(Preferences.YES,
+          getPreferenceValue(DO_NOT_SYNC_WAREHOUSE_PREFERENCE));
+    }
+    return syncField;
+  }
+
+  private boolean areDatesDifferent(final Date newDate, final Date oldDate) {
+    return newDate != null && oldDate != null && newDate.compareTo(oldDate) != 0;
+  }
+
+  private boolean areObjectsDifferent(final BaseOBObject newObject, final BaseOBObject oldObject) {
+    return newObject != null && oldObject != null
+        && !StringUtils.equals(newObject.getId().toString(), oldObject.getId().toString());
+  }
+
+  private String getPreferenceValue(final String preferenceKey) {
+    String syncField;
+    try {
+      syncField = Preferences.getPreferenceValue(preferenceKey, true, OBContext.getOBContext()
+          .getCurrentClient(), OBContext.getOBContext().getCurrentOrganization(), OBContext
+          .getOBContext().getUser(), OBContext.getOBContext().getRole(), null);
+    } catch (PropertyException e) {
+      // if property not found, sync the field
+      syncField = Preferences.NO;
+    }
+    return syncField;
+  }
+
+  private boolean businessPartnerHasChanged(final OrderParameters orderParameters) {
+    return !StringUtils.equals(orderParameters.getNewBPId(), orderParameters.getOldBPId());
+  }
+
+  private void removeDiscountInformationFromOrder(final OrderParameters orderParameters) {
+    StringBuilder deleteHql = new StringBuilder();
+    deleteHql.append(" delete from " + OrderDiscount.ENTITY_NAME);
+    deleteHql.append(" where " + OrderDiscount.PROPERTY_SALESORDER + ".id = :orderId");
+    Query deleteQry = OBDal.getInstance().getSession().createQuery(deleteHql.toString());
+    deleteQry.setParameter("orderId", orderParameters.getOrderId());
+    deleteQry.executeUpdate();
+  }
+
+  private class OrderParameters {
+    String orderId;
+    Date newOrderDate;
+    Date oldOrderDate;
+    Date newScheduledDate;
+    Date oldScheduledDate;
+    Warehouse newWarehouse;
+    Warehouse oldWarehouse;
+    String newBPId;
+    String oldBPId;
+
+    public OrderParameters() {
+      this.orderId = null;
+      this.newOrderDate = null;
+      this.oldOrderDate = null;
+      this.newScheduledDate = null;
+      this.oldScheduledDate = null;
+      this.newWarehouse = null;
+      this.oldWarehouse = null;
+      this.newBPId = null;
+      this.oldBPId = null;
+    }
+
+    public String getOrderId() {
+      return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+      this.orderId = orderId;
+    }
+
+    public Date getNewOrderDate() {
+      return newOrderDate;
+    }
+
+    public void setNewOrderDate(Date newOrderDate) {
+      this.newOrderDate = newOrderDate;
+    }
+
+    public Date getOldOrderDate() {
+      return oldOrderDate;
+    }
+
+    public void setOldOrderDate(Date oldOrderDate) {
+      this.oldOrderDate = oldOrderDate;
+    }
+
+    public Date getNewScheduledDate() {
+      return newScheduledDate;
+    }
+
+    public void setNewScheduledDate(Date newScheduledDate) {
+      this.newScheduledDate = newScheduledDate;
+    }
+
+    public Date getOldScheduledDate() {
+      return oldScheduledDate;
+    }
+
+    public void setOldScheduledDate(Date oldScheduledDate) {
+      this.oldScheduledDate = oldScheduledDate;
+    }
+
+    public Warehouse getNewWarehouse() {
+      return newWarehouse;
+    }
+
+    public void setNewWarehouse(Warehouse newWarehouse) {
+      this.newWarehouse = newWarehouse;
+    }
+
+    public Warehouse getOldWarehouse() {
+      return oldWarehouse;
+    }
+
+    public void setOldWarehouse(Warehouse oldWarehouse) {
+      this.oldWarehouse = oldWarehouse;
+    }
+
+    public String getNewBPId() {
+      return newBPId;
+    }
+
+    public void setNewBPId(String newBPId) {
+      this.newBPId = newBPId;
+    }
+
+    public String getOldBPId() {
+      return oldBPId;
+    }
+
+    public void setOldBPId(String oldBPId) {
+      this.oldBPId = oldBPId;
+    }
+
   }
 }
