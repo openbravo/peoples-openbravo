69b99fc44428fcd311c27bbf2904c76e2afc5f59
Ranjith S R <ranjith@qualiantech.com>
2018-02-13 15:05:42 +0530

    Fixed issue 37887 : Invoices should be created without time in accounting date and invoice date fields
    * Time should be truncated fom Date while creating invoice through cashup or receipt

--- src/org/openbravo/retail/posterminal/OrderGroupingProcessor.java
+++ src/org/openbravo/retail/posterminal/OrderGroupingProcessor.java
@@ -82,7 +82,7 @@ public class OrderGroupingProcessor {
 
     long t0 = System.currentTimeMillis();
     long t1, t2, t3, t4, t5, t6, t7, t8, t9, t10;
-    final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+    final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
     final String strUserId = OBContext.getOBContext().getUser().getId();
     final String strCurrentDate = dateFormatter.format(currentDate);
     final String strLang = RequestContext.get().getVariablesSecureApp().getLanguage();
