be8ddc66ca3f965d84f797c52d6df9e4d849bf78
Ranjith S R <ranjith@qualiantech.com>
2018-02-18 14:30:11 +0530

    Fixed issue 37921 : Added Keyboard keyMatcher for Cashup and Cash Management keyboards
    * Both keyboard should allow numeric values and decimal

--- web/org.openbravo.retail.posterminal/js/cashmgmt/view/cashmgmtkeyboard.js
+++ web/org.openbravo.retail.posterminal/js/cashmgmt/view/cashmgmtkeyboard.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2015 Openbravo S.L.U.
+ * Copyright (C) 2012-2018 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -140,6 +140,9 @@ enyo.kind({
       });
       args.context.showToolbar('cashMgmtToolbar');
     });
-
+  },
+  initComponents: function () {
+    this.inherited(arguments);
+    this.keyMatcher = new RegExp('^([0-9]|\\' + OB.Format.defaultDecimalSymbol + ')$', 'g');
   }
 });
