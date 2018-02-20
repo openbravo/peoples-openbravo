8a7df492695822363d55009fb841cadd5ff7ac68
Asier Lostalé <asier.lostale@openbravo.com>
2018-02-20 10:49:22 +0100

    [org] fixed AllowedOrganizationsTest
      * adapting it to current org tree structure in sample data
      * making parameterized to execute always all trees

--- src-test/src/org/openbravo/test/security/AllowedOrganizationsTest.java
+++ src-test/src/org/openbravo/test/security/AllowedOrganizationsTest.java
@@ -19,16 +19,27 @@
 
 package org.openbravo.test.security;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static com.google.common.collect.Sets.newHashSet;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.security.OrganizationStructureProvider;
 import org.openbravo.test.base.OBBaseTest;
+
+import com.google.common.collect.ImmutableMap;
 
 /**
  * Tests computation of natural tree of an organization. This is used to compute the readable
@@ -40,37 +51,74 @@ import org.openbravo.test.base.OBBaseTest;
  * @author mtaal
  */
 
+@RunWith(Parameterized.class)
 public class AllowedOrganizationsTest extends OBBaseTest {
+  private static final String MAIN = "0";
+  private static final String FB_GROUP = "19404EAD144C49A0AF37D54377CF452D";
+  private static final String US = "2E60544D37534C0B89E765FE29BC0B43";
+  private static final String US_EST = "7BABA5FF80494CAFA54DEBD22EC46F01";
+  private static final String US_WEST = "BAE22373FEBE4CCCA24517E23F0C8A48";
+  private static final String ESP = "B843C30461EA4501935CB1D125C9C25A";
+  private static final String ESP_SUR = "DC206C91AA6A4897B44DA897936E0EC3";
+  private static final String ESP_NORTE = "E443A31992CB4635AFCAEABE7183CE85";
+
+  private static final Map<String, String> ORG_NAMES = ImmutableMap.<String, String> builder()
+      .put(MAIN, "Main") //
+      .put(FB_GROUP, "F&B International Group") //
+      .put(US, "F&B US, Inc.") //
+      .put(US_EST, "F&B US East Coast") //
+      .put(US_WEST, "F&B US West Coast") //
+      .put(ESP, "F&B España, S.A") //
+      .put(ESP_SUR, "F&B España - Región Sur") //
+      .put(ESP_NORTE, "F&B España - Región Norte").build();
+
+  private static final Map<String, Set<String>> ORG_TREES = ImmutableMap
+      .<String, Set<String>> builder()
+      .put(FB_GROUP, newHashSet(MAIN, FB_GROUP, US, US_EST, US_WEST, ESP, ESP_SUR, ESP_NORTE))
+      .put(US, newHashSet(MAIN, FB_GROUP, US, US_EST, US_WEST))
+      .put(US_WEST, newHashSet(MAIN, FB_GROUP, US, US_WEST))
+      .put(US_EST, newHashSet(MAIN, FB_GROUP, US, US_EST))
+      .put(ESP, newHashSet(MAIN, FB_GROUP, ESP, ESP_SUR, ESP_NORTE))
+      .put(ESP_SUR, newHashSet(MAIN, FB_GROUP, ESP, ESP_SUR))
+      .put(ESP_NORTE, newHashSet(MAIN, FB_GROUP, ESP, ESP_NORTE)) //
+      .put("Dummy", newHashSet("Dummy")) // special case: non-existent org returns itself
+      .build();
+
+  @Parameter(0)
+  public String testingOrgName;
+
+  @Parameter(1)
+  public String testingOrgId;
+
+  @Parameter(2)
+  public Set<String> expectedNaturalTree;
+
+  @Parameters(name = "Natural tree of {0}")
+  public static Collection<Object[]> parameters() throws IOException {
+    final Collection<Object[]> allTrees = new ArrayList<>();
+
+    for (Entry<String, Set<String>> tree : ORG_TREES.entrySet()) {
+      allTrees.add(new Object[] { //
+          ORG_NAMES.get(tree.getKey()), //
+              tree.getKey(), //
+              tree.getValue() //
+          });
+    }
+
+    return allTrees;
+  }
 
   /**
    * Tests valid organizations trees for different organizations.
    */
   @Test
-  @Ignore("Fix test with new org tree map")
   public void testOrganizationTree() {
     setTestAdminContext();
 
     final OrganizationStructureProvider osp = new OrganizationStructureProvider();
     osp.setClientId(TEST_CLIENT_ID);
 
-    checkResult("1000001", osp, new String[] { "1000001" });
-    checkResult("1000002", osp, new String[] { "1000003", "1000004", "1000000", "0", "1000002" });
-    checkResult("1000003", osp, new String[] { "1000003", "1000000", "0", "1000002" });
-    checkResult("1000004", osp, new String[] { "1000004", "1000000", "0", "1000002" });
-    checkResult("1000005", osp, new String[] { "1000009", "1000006", "0", "1000000", "1000008",
-        "1000005", "1000007" });
-    checkResult("1000006", osp, new String[] { "1000009", "1000006", "0", "1000000", "1000008",
-        "1000005" });
-    checkResult("1000007", osp, new String[] { "1000000", "0", "1000005", "1000007" });
-    checkResult("1000008", osp, new String[] { "1000000", "1000006", "0", "1000008", "1000005" });
-    checkResult("1000009", osp, new String[] { "1000009", "1000006", "0", "1000000", "1000005" });
-  }
-
-  private void checkResult(String id, OrganizationStructureProvider osp, String[] values) {
-    final Set<String> result = osp.getNaturalTree(id);
-    assertEquals(values.length, result.size());
-    for (final String value : values) {
-      assertTrue(result.contains(value));
-    }
+    assertThat("Natural tree for " + ORG_NAMES.get(testingOrgId), osp.getNaturalTree(testingOrgId),
+        is(expectedNaturalTree));
   }
 }
