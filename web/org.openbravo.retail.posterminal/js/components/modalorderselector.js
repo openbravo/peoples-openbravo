7d3574f81a86c7c87a167ebb52392aba3532b2a8
Ranjith S R <ranjith@qualiantech.com>
2018-03-01 11:22:00 +0530

    Fixed issue 38012 : Added hook OBPOS_RenderSelectorLine in Order Selector Line Render

--- web/org.openbravo.retail.posterminal/js/components/modalorderselector.js
+++ web/org.openbravo.retail.posterminal/js/components/modalorderselector.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2017 Openbravo S.L.U.
+ * Copyright (C) 2017-2018 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -106,7 +106,12 @@ enyo.kind({
       break;
     }
     this.applyStyle('padding', '5px');
-    this.render();
+
+    OB.UTIL.HookManager.executeHooks('OBPOS_RenderSelectorLine', {
+      selectorLine: this
+    }, function (args) {
+      me.render();
+    });
   }
 });
 
