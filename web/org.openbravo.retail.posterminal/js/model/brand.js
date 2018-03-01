a6afa3cf1df25c49eeae34404bb469459322bc74
Ranjith S R <ranjith@qualiantech.com>
2018-03-01 11:28:56 +0530

    Fixed issue 37534 : Added index to Product and Brand table

--- web/org.openbravo.retail.posterminal/js/model/brand.js
+++ web/org.openbravo.retail.posterminal/js/model/brand.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2013-2015 Openbravo S.L.U.
+ * Copyright (C) 2013-2018 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -34,6 +34,14 @@
     column: '_identifier',
     filter: true,
     type: 'TEXT'
+  }]);
+
+  Brand.addIndex([{
+    name: 'obpos_in_brandproduct',
+    columns: [{
+      name: 'm_product_id',
+      sort: 'asc'
+    }]
   }]);
 
   OB.Data.Registry.registerModel(Brand);
