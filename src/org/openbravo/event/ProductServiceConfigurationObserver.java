89b86c3bf2a165485ba2e67458dcdec4232e8246
Eduardo Argal Guibert <eduardo.argal@openbravo.com>
2018-03-19 12:45:28 +0100

    [Product Services] Adds a validation to avoid editing service related configuration when there is an open order
    --HG--
    extra : transplant_source : Q%E7Y%22d/%02%F5Ip%DD%FEgI%11%D5%E8I/%18

---
+++ src/org/openbravo/event/ProductServiceConfigurationObserver.java
@@ -0,0 +1,93 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html 
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License. 
+ * The Original Code is Openbravo ERP. 
+ * The Initial Developer of the Original Code is Openbravo SLU 
+ * All portions are Copyright (C) 2015 Openbravo SLU 
+ * All Rights Reserved. 
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
+ */
+
+package org.openbravo.event;
+
+import javax.enterprise.event.Observes;
+
+import org.apache.log4j.Logger;
+import org.openbravo.base.exception.OBException;
+import org.openbravo.base.model.Entity;
+import org.openbravo.base.model.ModelProvider;
+import org.openbravo.base.model.Property;
+import org.openbravo.client.kernel.event.EntityPersistenceEventObserver;
+import org.openbravo.client.kernel.event.EntityUpdateEvent;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.dal.service.OBQuery;
+import org.openbravo.erpCommon.utility.OBMessageUtils;
+import org.openbravo.model.common.order.Order;
+import org.openbravo.model.common.order.OrderLine;
+import org.openbravo.model.common.plm.Product;
+
+public class ProductServiceConfigurationObserver extends EntityPersistenceEventObserver {
+
+  private static Entity[] entities = { ModelProvider.getInstance().getEntity(Product.ENTITY_NAME) };
+  protected Logger logger = Logger.getLogger(this.getClass());
+
+  @Override
+  protected Entity[] getObservedEntities() {
+    return entities;
+  }
+
+  public void onUpdate(@Observes EntityUpdateEvent event) {
+    if (!isValidEvent(event)) {
+      return;
+    }
+    checkConfiguration(event);
+  }
+
+  private void checkConfiguration(EntityUpdateEvent event) {
+    final Entity productEntity = ModelProvider.getInstance().getEntity(Product.ENTITY_NAME);
+    final Property linkedToProductProperty = productEntity
+        .getProperty(Product.PROPERTY_LINKEDTOPRODUCT);
+    final Property qtyRuleProperty = productEntity.getProperty(Product.PROPERTY_QUANTITYRULE);
+    // final Property idProperty = productEntity.getProperty(Product.PROPERTY_ID);
+    final Boolean linkedToProduct = (Boolean) event.getCurrentState(linkedToProductProperty);
+    final String qtyRule = (String) event.getCurrentState(qtyRuleProperty);
+    final Boolean linkedToProduct_Previous = (Boolean) event
+        .getPreviousState(linkedToProductProperty);
+    final String qtyRule_Previous = (String) event.getPreviousState(qtyRuleProperty);
+    final Product product = OBDal.getInstance().get(Product.class, (String) event.getId());
+    if (linkedToProduct_Previous != linkedToProduct || !qtyRule_Previous.equals(qtyRule)) {
+      checkNotDeliveredOrders(product);
+    }
+  }
+
+  private void checkNotDeliveredOrders(Product product) {
+    StringBuilder hql = new StringBuilder();
+    hql.append("as ol ");
+    hql.append("join ol." + OrderLine.PROPERTY_SALESORDER + " as o ");
+    hql.append("where o." + Order.PROPERTY_DOCUMENTSTATUS);
+    hql.append(" = 'CO' ");
+    hql.append("and ol." + OrderLine.PROPERTY_PRODUCT);
+    hql.append(" = :product ");
+    hql.append("and ol." + OrderLine.PROPERTY_DELIVEREDQUANTITY + "<> ol."
+        + OrderLine.PROPERTY_ORDEREDQUANTITY);
+    OBQuery<OrderLine> notDeliveredOrderLineQuery = OBDal.getInstance().createQuery(
+        OrderLine.class, hql.toString());
+    notDeliveredOrderLineQuery.setNamedParameter("product", product);
+    notDeliveredOrderLineQuery.setMaxResult(1);
+    OrderLine notDeliveredOrderLine = notDeliveredOrderLineQuery.uniqueResult();
+    if (notDeliveredOrderLine != null) {
+    	String [] params = {notDeliveredOrderLine.getSalesOrder().getDocumentNo()};
+      throw new OBException(OBMessageUtils.getI18NMessage("ServiceCannotBeModified", params));
+    }
+  }
+
+}
