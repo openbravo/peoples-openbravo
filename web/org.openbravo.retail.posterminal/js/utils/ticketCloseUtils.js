8ca184fd22bc1b5a29b7296e25fefa8dabd8b225
Gorka Gil <gorka.gil@openbravo.com>
2018-03-20 19:00:27 +0100

    Fixes issue 38025: Added preference OBPOS_alwaysCreateNewReceiptAfterPayReceipt
    --HG--
    extra : rebase_source : 089197ec6b89ca6f41bf14272a05b9377e6e00f0

--- web/org.openbravo.retail.posterminal/js/utils/ticketCloseUtils.js
+++ web/org.openbravo.retail.posterminal/js/utils/ticketCloseUtils.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2017 Openbravo S.L.U.
+ * Copyright (C) 2017-2018 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -78,7 +78,11 @@
                     OB.error("The receipt has been modified while it was being closed:\n" + diffStringified + "\n");
                   }
 
-                  orderList.deleteCurrent();
+                  if (OB.MobileApp.model.hasPermission('OBPOS_alwaysCreateNewReceiptAfterPayReceipt', true)) {
+                    orderList.deleteCurrent(true);
+                  } else {
+                    orderList.deleteCurrent();
+                  }
                   receipt.setIsCalculateReceiptLockState(false);
                   receipt.setIsCalculateGrossLockState(false);
 
