935f4ca490bbed1a8fe5abbfac1d8d1e98b369e4
Mark <markmm82@gmail.com>
2018-04-10 22:49:57 -0400

    [clfr] Fixed M_InOut_PE_HQL HQL Table to get better performance and to adapt to Shipment/Rceipts be the case
    Renamed InOutLinePEHQLInjector to InOutLinePEHQLInjector0.
    Created InOutLinePEHQLInjector1 to get the correct movement quantity according the
    document is a sales transaction or not.
    Created InOutLinePEHQLInjector2 to get the correct alternative movement quantity according the
    document is a sales transaction or not.

---
+++ src/org/openbravo/common/inserters/InOutLinePEHQLInjector2.java
@@ -0,0 +1,46 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License.
+ * The Original Code is Openbravo ERP.
+ * The Initial Developer of the Original Code is Openbravo SLU
+ * All portions are Copyright (C) 2018 Openbravo SLU
+ * All Rights Reserved.
+ * Contributor(s):  ______________________________________.
+ *************************************************************************
+ */
+package org.openbravo.common.inserters;
+
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.openbravo.service.datasource.hql.HQLInserterQualifier;
+import org.openbravo.service.datasource.hql.HqlInserter;
+
+@HQLInserterQualifier.Qualifier(tableId = "631D227DC83A4898BBD041D46D829D27", injectionId = "2")
+public class InOutLinePEHQLInjector2 extends HqlInserter {
+
+  @Override
+  public String insertHql(Map<String, String> requestParameters,
+      Map<String, Object> queryNamedParameters) {
+    boolean isSalesTransaction = StringUtils.equals(
+        requestParameters.get("@Invoice.salesTransaction@"), "true");
+
+    StringBuilder hql = new StringBuilder();
+
+    if (isSalesTransaction) {
+      hql.append("e.operativeQuantity - sum(coalesce(case when (i.documentStatus <> 'CO') then 0 ELSE M_GET_CONVERTED_AUMQTY(p.id, il.invoicedQuantity, aum.id) END, 0))");
+    } else {
+      hql.append("e.operativeQuantity - M_GET_CONVERTED_AUMQTY(p.id, sum(coalesce(mi.quantity,0)), aum.id)");
+    }
+
+    return hql.toString();
+  }
+}
