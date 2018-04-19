7000e157ccef69c700ed0b13ad43945429ad5f8d
Asier Lostalé <asier.lostale@openbravo.com>
2018-04-19 12:09:11 +0200

    fixes 38407: error message activating heartbeat in PG10
      Starting from PostgreSQL 10, server version string includes also package, being
      a longer String it doesn't fit current column size.
    
      Fixed by getting just the first word of the version (which is the actual server
      version) skipping the rest that refers to the package.
    
      Examples:
    
       9.3.21 -> 9.3.21
       10.3 (Ubuntu 10.3-1.pgdg16.04+1) -> 10.3
       11devel (Debian 11~~devel~20180418.2231-1~573.git5372c2c.pgdg90+1) -> 11devel

--- src/org/openbravo/erpCommon/utility/SystemInfo.java
+++ src/org/openbravo/erpCommon/utility/SystemInfo.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2008-2017 Openbravo SLU 
+ * All portions are Copyright (C) 2008-2018 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -444,6 +444,7 @@ public class SystemInfo {
       databaseVersion = getVersion(SystemInfoData.selectOracleVersion(conn));
     } else {
       databaseVersion = SystemInfoData.selectPostregresVersion(conn);
+      databaseVersion = databaseVersion.split(" ")[0];
     }
     return databaseVersion;
   }
