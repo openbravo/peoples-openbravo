28efd344d4a84db301452d096d619f4538fd8787
Mark <markmm82@gmail.com>
2018-05-21 15:12:14 -0400

    [clfr] Refactorized the transformer to include the Create From Sales Order flow
    Included the logic of the Create Lines From Sales Order in the transformer.
    Refactorized the transformer to be more legible.

--- src/org/openbravo/common/inserters/OrderLinePEHQLTransformer.java
+++ src/org/openbravo/common/inserters/OrderLinePEHQLTransformer.java
@@ -28,91 +28,93 @@ import org.openbravo.service.datasource.hql.HqlQueryTransformer;
 
 @ComponentProvider.Qualifier("7EB9FFD7BD4E4113A13A096EB879D358")
 public class OrderLinePEHQLTransformer extends HqlQueryTransformer {
+  private boolean isSalesTransaction;
 
   @Override
   public String transformHqlQuery(String _hqlQuery, Map<String, String> requestParameters,
       Map<String, Object> queryNamedParameters) {
-    boolean isSalesTransaction = StringUtils.equals(
-        requestParameters.get("@Invoice.salesTransaction@"), "true");
+    isSalesTransaction = StringUtils.equals(requestParameters.get("@Invoice.salesTransaction@"),
+        "true");
     final String strInvoicePriceListId = requestParameters.get("@Invoice.priceList@");
     final PriceList priceList = OBDal.getInstance().get(PriceList.class, strInvoicePriceListId);
     final String strBusinessPartnerId = requestParameters.get("@Invoice.businessPartner@");
     final String strCurrencyId = requestParameters.get("@Invoice.currency@");
     final String strInvoiceId = requestParameters.get("@Invoice.id@");
 
-    // SELECT CALCULATED Quantities
-    StringBuilder orderedQuantityHql = new StringBuilder();
-    if (isSalesTransaction) {
-
-    } else {
-      orderedQuantityHql.append(" e.orderedQuantity-SUM(COALESCE(m.quantity,0))-COALESCE(");
-      orderedQuantityHql.append(" (SELECT SUM(COALESCE(ci.invoicedQuantity, 0))");
-      orderedQuantityHql.append("  FROM OrderLine co");
-      orderedQuantityHql.append("    LEFT JOIN co.invoiceLineList ci");
-      orderedQuantityHql.append("  WHERE ci.invoice.id= :invId");
-      orderedQuantityHql.append("    AND co.id = e.id");
-      orderedQuantityHql.append("  GROUP BY ci.salesOrderLine.id , co.orderedQuantity),0)");
+    queryNamedParameters.put("issotrx", isSalesTransaction);
+    queryNamedParameters.put("bp", strBusinessPartnerId);
+    queryNamedParameters.put("plIncTax", priceList.isPriceIncludesTax());
+    queryNamedParameters.put("cur", strCurrencyId);
+    if (!isSalesTransaction) {
       queryNamedParameters.put("invId", strInvoiceId);
     }
 
-    StringBuilder operativeQuantityHql = new StringBuilder();
+    String transformedHql = _hqlQuery.replace("@fromClause@", getFromClauseHQL());
+    transformedHql = transformedHql.replace("@whereClause@", getWhereClauseHQL());
+    transformedHql = transformedHql.replace("@groupByClause@", getGroupByHQL());
+    transformedHql = transformedHql.replace("@orderedQuantity@", getOrderedQuantityHQL());
+    transformedHql = transformedHql.replace("@operativeQuantity@", getOperativeQuantityHQL());
+    transformedHql = transformedHql.replace("@orderQuantity@", getOrderQuantityHQL());
+    return transformedHql;
+  }
+
+  private String getGroupByHQL() {
+    StringBuilder groupByClause = new StringBuilder();
+    groupByClause.append("  o.id,");
+    groupByClause.append("  o.documentNo,");
+    groupByClause.append("  o.orderDate,");
+    groupByClause.append("  o.grandTotalAmount,");
+    groupByClause.append("  e.orderedQuantity,");
+    groupByClause.append("  uom.id,");
+    groupByClause.append("  uom.symbol,");
+    groupByClause.append("  p.id,");
+    groupByClause.append("  p.name,");
+    groupByClause.append("  e.lineNo,");
+    groupByClause.append("  e.id,");
+    groupByClause.append("  e.organization.id,");
+    groupByClause.append("  e.taxableAmount, ");
+    groupByClause.append("  COALESCE(e.asset.id, o.asset.id),");
+    groupByClause.append("  COALESCE(e.project.id, o.project.id),");
+    groupByClause.append("  COALESCE(e.costcenter.id, o.costcenter.id),");
+    groupByClause.append("  COALESCE(e.stDimension.id, o.stDimension.id),");
+    groupByClause.append("  COALESCE(e.ndDimension.id, o.ndDimension.id),");
+    groupByClause.append("  e.explode,");
+    groupByClause.append("  bomParent.id,");
+    groupByClause.append("  aum.id,");
+    groupByClause.append("  e.operativeQuantity,");
+    groupByClause.append("  dt.id,");
+    groupByClause.append("  pl.id,");
+    groupByClause.append("  bp.id,");
+    groupByClause.append("  il.id,");
     if (isSalesTransaction) {
-
+      groupByClause.append("  e.invoicedQuantity");
     } else {
-      operativeQuantityHql
-          .append(" e.operativeQuantity-SUM(COALESCE(to_number(M_GET_CONVERTED_AUMQTY(p.id, m.quantity, aum.id)),0))-COALESCE(");
-      operativeQuantityHql
-          .append(" (SELECT SUM(COALESCE(to_number(M_GET_CONVERTED_AUMQTY(p.id, ci.invoicedQuantity, aum.id)), 0))");
-      operativeQuantityHql.append("  FROM OrderLine co");
-      operativeQuantityHql.append("    LEFT JOIN co.invoiceLineList ci");
-      operativeQuantityHql.append("  WHERE ci.invoice.id = :invId");
-      operativeQuantityHql.append("    AND co.id = e.id");
-      operativeQuantityHql.append("  GROUP BY ci.salesOrderLine.id , co.orderedQuantity),0)");
-      queryNamedParameters.put("invId", strInvoiceId);
+      groupByClause.append("  e.orderDate,");
+      groupByClause.append("  e.orderUOM.id,");
+      groupByClause.append("  m.id");
+      groupByClause
+          .append(" HAVING ((e.explode='Y') OR ((e.orderedQuantity-SUM(COALESCE(m.quantity,0))) <> 0))");
     }
+    return groupByClause.toString();
+  }
 
-    StringBuilder orderQuantityHql = new StringBuilder();
-    if (isSalesTransaction) {
-      orderQuantityHql.append(" COALESCE(il.orderQuantity ");
-      orderQuantityHql
-          .append(" , e.orderQuantity * TO_NUMBER(C_DIVIDE((e.orderedQuantity - coalesce(e.invoicedQuantity,0)), e.orderedQuantity)))");
-    } else {
-      orderQuantityHql.append(" COALESCE(il.orderQuantity ");
-      orderQuantityHql
-          .append(" , e.orderQuantity * TO_NUMBER(C_DIVIDE((e.orderedQuantity - coalesce(e.invoicedQuantity,0)), e.orderedQuantity)))");
-    }
-
-    // FROM CLAUSE Section
-    StringBuilder fromClause = new StringBuilder();
-    if (isSalesTransaction) {
-
-    } else {
-      fromClause.append(" OrderLine e");
-      fromClause.append(" join e.salesOrder o");
-      fromClause.append(" join o.priceList pl");
-      fromClause.append(" join e.uOM uom");
-      fromClause.append(" join e.product p");
-      fromClause.append(" join o.businessPartner bp");
-      fromClause.append(" join o.documentType dt");
-      fromClause.append(" left join e.bOMParent bomParent");
-      fromClause.append(" left join e.operativeUOM aum");
-      fromClause
-          .append(" left join e.procurementPOInvoiceMatchList m with m.invoiceLine.id is not null");
-      fromClause.append(" left join e.goodsShipmentLine il");
-    }
-
-    // WHERE CONDITIONS
+  private String getWhereClauseHQL() {
     StringBuilder whereClause = new StringBuilder();
     whereClause.append(" and o.salesTransaction = :issotrx");
     whereClause.append(" and bp.id = :bp");
     whereClause.append(" and pl.priceIncludesTax = :plIncTax");
     whereClause.append(" and o.currency.id = :cur");
-    queryNamedParameters.put("issotrx", isSalesTransaction);
-    queryNamedParameters.put("bp", strBusinessPartnerId);
-    queryNamedParameters.put("plIncTax", priceList.isPriceIncludesTax());
-    queryNamedParameters.put("cur", strCurrencyId);
     if (isSalesTransaction) {
-
+      whereClause.append(" and (");
+      whereClause.append("     ic.term in ('D', 'S') and ic.qtydelivered <> 0");
+      whereClause.append("  or (ic.term = 'I' AND EXISTS");
+      whereClause.append("   (SELECT 1");
+      whereClause.append("    FROM OrderLine ol2");
+      whereClause.append("    WHERE ol2.salesOrder = o.id");
+      whereClause.append("    GROUP BY ol2.id ");
+      whereClause.append("    HAVING SUM(ol2.orderedQuantity) - SUM(ol2.invoicedQuantity) <> 0))");
+      whereClause.append("  or (ic.term = 'O' and ic.qtyordered = ic.qtydelivered)");
+      whereClause.append(" )");
     } else {
       whereClause.append(" and o.documentStatus in ('CO', 'CL')");
       whereClause.append(" and o.invoiceTerms <> 'N'");
@@ -135,52 +137,83 @@ public class OrderLinePEHQLTransformer extends HqlQueryTransformer {
       whereClause.append("  WHERE co2.salesOrder.id = o.id");
       whereClause.append("  GROUP BY co2.orderedQuantity, co2.id");
       whereClause.append("  HAVING (co2.orderedQuantity-SUM(COALESCE(mp2.quantity,0))) <> 0)");
-      queryNamedParameters.put("invId", strInvoiceId);
     }
+    return whereClause.toString();
+  }
 
-    // GROUP BY Section
-    StringBuilder groupByClause = new StringBuilder();
+  private String getFromClauseHQL() {
+    StringBuilder fromClause = new StringBuilder();
     if (isSalesTransaction) {
-
+      fromClause.append(" InvoiceCandidateV ic");
+      fromClause.append(" join ic.order o");
+      fromClause.append(" join o.orderLineList as e");
     } else {
-      groupByClause.append("   o.id,");
-      groupByClause.append("   o.documentNo,");
-      groupByClause.append("   o.orderDate,");
-      groupByClause.append("   o.grandTotalAmount,");
-      groupByClause.append("   e.orderDate,");
-      groupByClause.append("   uom.id,");
-      groupByClause.append("   uom.symbol,");
-      groupByClause.append("   p.id,");
-      groupByClause.append("   p.name,");
-      groupByClause.append("   e.lineNo,");
-      groupByClause.append("   e.id,");
-      groupByClause.append("   e.organization.id,");
-      groupByClause.append("   e.taxableAmount,");
-      groupByClause.append("   e.cancelPriceAdjustment,");
-      groupByClause.append("   COALESCE(e.asset.id, o.asset.id),");
-      groupByClause.append("   COALESCE(e.project.id, o.project.id),");
-      groupByClause.append("   COALESCE(e.costcenter.id, o.costcenter.id),");
-      groupByClause.append("   COALESCE(e.stDimension.id, o.stDimension.id),");
-      groupByClause.append("   COALESCE(e.ndDimension.id, o.ndDimension.id),");
-      groupByClause.append("   e.explode,");
-      groupByClause.append("   bomParent.id,");
-      groupByClause.append("   e.orderUOM.id,");
-      groupByClause.append("   e.operativeQuantity,");
-      groupByClause.append("   dt.id,");
-      groupByClause.append("   aum.id,");
-      groupByClause.append("   pl.id,");
-      groupByClause.append("   bp.id,");
-      groupByClause.append("   m.id, il.id");
-      groupByClause
-          .append(" HAVING ((e.explode='Y') OR ((e.orderedQuantity-SUM(COALESCE(m.quantity,0))) <> 0))");
+      fromClause.append(" OrderLine e");
+      fromClause.append(" join e.salesOrder o");
     }
+    fromClause.append(" join o.priceList pl");
+    fromClause.append(" join e.uOM uom");
+    fromClause.append(" join e.product p");
+    fromClause.append(" join o.businessPartner bp");
+    fromClause.append(" join o.documentType dt");
+    fromClause.append(" left join e.operativeUOM aum");
+    fromClause.append(" left join e.bOMParent bomParent");
+    fromClause.append(" left join e.goodsShipmentLine il");
 
-    String transformedHql = _hqlQuery.replace("@fromClause@", fromClause.toString());
-    transformedHql = transformedHql.replace("@whereClause@", whereClause.toString());
-    transformedHql = transformedHql.replace("@groupByClause@", groupByClause.toString());
-    transformedHql = transformedHql.replace("@orderedQuantity@", orderedQuantityHql.toString());
-    transformedHql = transformedHql.replace("@operativeQuantity@", operativeQuantityHql.toString());
-    transformedHql = transformedHql.replace("@orderQuantity@", orderQuantityHql.toString());
-    return transformedHql;
+    if (!isSalesTransaction) {
+      fromClause
+          .append(" left join e.procurementPOInvoiceMatchList m with m.invoiceLine.id is not null");
+
+    }
+    return fromClause.toString();
+  }
+
+  private String getOrderQuantityHQL() {
+    StringBuilder orderQuantityHql = new StringBuilder();
+    if (isSalesTransaction) {
+      orderQuantityHql.append(" COALESCE(il.orderQuantity ");
+      orderQuantityHql
+          .append(" , e.orderQuantity * TO_NUMBER(C_DIVIDE((e.orderedQuantity - coalesce(e.invoicedQuantity,0)), e.orderedQuantity)))");
+    } else {
+      orderQuantityHql.append(" COALESCE(il.orderQuantity ");
+      orderQuantityHql
+          .append(" , e.orderQuantity * TO_NUMBER(C_DIVIDE((e.orderedQuantity - coalesce(e.invoicedQuantity,0)), e.orderedQuantity)))");
+    }
+    return orderQuantityHql.toString();
+  }
+
+  private String getOperativeQuantityHQL() {
+    StringBuilder operativeQuantityHql = new StringBuilder();
+    if (isSalesTransaction) {
+      operativeQuantityHql
+          .append(" e.operativeQuantity - SUM(COALESCE(to_number(M_GET_CONVERTED_AUMQTY(p.id, e.invoicedQuantity, aum.id)), 0))");
+    } else {
+      operativeQuantityHql
+          .append(" e.operativeQuantity-SUM(COALESCE(to_number(M_GET_CONVERTED_AUMQTY(p.id, m.quantity, aum.id)),0))-COALESCE(");
+      operativeQuantityHql
+          .append(" (SELECT SUM(COALESCE(to_number(M_GET_CONVERTED_AUMQTY(p.id, ci.invoicedQuantity, aum.id)), 0))");
+      operativeQuantityHql.append("  FROM OrderLine co");
+      operativeQuantityHql.append("    LEFT JOIN co.invoiceLineList ci");
+      operativeQuantityHql.append("  WHERE ci.invoice.id = :invId");
+      operativeQuantityHql.append("    AND co.id = e.id");
+      operativeQuantityHql.append("  GROUP BY ci.salesOrderLine.id , co.orderedQuantity),0)");
+    }
+    return operativeQuantityHql.toString();
+  }
+
+  private String getOrderedQuantityHQL() {
+    StringBuilder orderedQuantityHql = new StringBuilder();
+    if (isSalesTransaction) {
+      orderedQuantityHql.append(" e.orderedQuantity-COALESCE(e.invoicedQuantity,0)");
+    } else {
+      orderedQuantityHql.append(" e.orderedQuantity-SUM(COALESCE(m.quantity,0))-COALESCE(");
+      orderedQuantityHql.append(" (SELECT SUM(COALESCE(ci.invoicedQuantity, 0))");
+      orderedQuantityHql.append("  FROM OrderLine co");
+      orderedQuantityHql.append("    LEFT JOIN co.invoiceLineList ci");
+      orderedQuantityHql.append("  WHERE ci.invoice.id= :invId");
+      orderedQuantityHql.append("    AND co.id = e.id");
+      orderedQuantityHql.append("  GROUP BY ci.salesOrderLine.id , co.orderedQuantity),0)");
+    }
+    return orderedQuantityHql.toString();
   }
 }
