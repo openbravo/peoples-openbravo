a0c48a7dc2aa1b32e2fbae0629d2f57516f32829
Víctor Martínez Romanos <victor.martinez@openbravo.com>
2018-05-28 17:49:42 +0200

    [clfr] Avoid storing Hibernate object as class variable This might create problems if clearing session. Reordered methods in the way they are used. Declared private class for ordering as static

--- src/org/openbravo/common/actionhandler/createlinesfromprocess/CreateInvoiceLinesFromProcess.java
+++ src/org/openbravo/common/actionhandler/createlinesfromprocess/CreateInvoiceLinesFromProcess.java
@@ -50,15 +50,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CreateInvoiceLinesFromProcess {
+  private static final Logger log = LoggerFactory.getLogger(CreateInvoiceLinesFromProcess.class);
+
   @Inject
   @Any
   private Instance<CreateLinesFromProcessImplementationInterface> createLinesFromProcessHooks;
 
-  private static final Logger log = LoggerFactory.getLogger(CreateInvoiceLinesFromProcess.class);
-
   // The class of the objects from which the invoice lines will be created
   private Class<? extends BaseOBObject> linesFromClass;
-  private Invoice processingInvoice;
   // Last Line number of the Processing Line
   private Long lastLineNo = 0L;
   private JSONArray selectedLines;
@@ -80,6 +79,7 @@ public class CreateInvoiceLinesFromProcess {
    * @param selectedLinesFromClass
    *          The class of the lines being copied (Order/Shipment/Receipt)
    * @param currentInvoice
+   *          The invoice currently being created
    * @return The number of the lines properly copied
    */
   public int createInvoiceLinesFromDocumentLines(final JSONArray selectedLinesParam,
@@ -89,8 +89,7 @@ public class CreateInvoiceLinesFromProcess {
 
     OBContext.setAdminMode(true);
     try {
-      processingInvoice = currentInvoice;
-      return createLinesFromSelectedLines();
+      return createLinesFromSelectedLines(currentInvoice);
     } catch (Exception e) {
       throw new OBException(e);
     } finally {
@@ -111,9 +110,9 @@ public class CreateInvoiceLinesFromProcess {
     }
   }
 
-  private int createLinesFromSelectedLines() {
+  private int createLinesFromSelectedLines(final Invoice currentInvoice) {
     // Initialize the line number with the last one in the processing invoice.
-    lastLineNo = getLastLineNoOfCurrentInvoice();
+    lastLineNo = getLastLineNoOfCurrentInvoice(currentInvoice);
     int createdInvoiceLinesCount = 0;
     for (int index = 0; index < selectedLines.length(); index++) {
       JSONObject selectedLine = getPickEditLineValuesInPosition(index);
@@ -121,15 +120,45 @@ public class CreateInvoiceLinesFromProcess {
       if (CreateLinesFromUtil.isOrderLineWithRelatedShipmentReceiptLines(copiedLine, selectedLine)) {
         processCopiedLineShipmentInOut(copiedLine);
       } else {
-        InvoiceLine newInvoiceLine = createLineFromSelectedLineAndRunHooks(copiedLine, selectedLine);
-        processingInvoice.getInvoiceLineList().add(newInvoiceLine);
+        InvoiceLine newInvoiceLine = createLineFromSelectedLineAndRunHooks(currentInvoice,
+            copiedLine, selectedLine);
+        currentInvoice.getInvoiceLineList().add(newInvoiceLine);
         OBDal.getInstance().save(newInvoiceLine);
-        OBDal.getInstance().save(processingInvoice);
+        OBDal.getInstance().save(currentInvoice);
         createdInvoiceLinesCount++;
         OBDal.getInstance().flush();
       }
     }
     return createdInvoiceLinesCount;
+  }
+
+  /**
+   * Returns the max invoice line number defined in the invoice to which the lines are going to be
+   * added
+   *
+   * @return The max invoice line number
+   */
+  private Long getLastLineNoOfCurrentInvoice(final Invoice currentInvoice) {
+    OBCriteria<InvoiceLine> obc = OBDal.getInstance().createCriteria(InvoiceLine.class);
+    obc.add(Restrictions.eq(InvoiceLine.PROPERTY_INVOICE, currentInvoice));
+    obc.setProjection(Projections.max(InvoiceLine.PROPERTY_LINENO));
+    Long lineNumber = 0L;
+    obc.setMaxResults(1);
+    Object o = obc.uniqueResult();
+    if (o != null) {
+      lineNumber = (Long) o;
+    }
+    return lineNumber;
+  }
+
+  private JSONObject getPickEditLineValuesInPosition(final int index) {
+    try {
+      return selectedLines.getJSONObject(index);
+    } catch (JSONException e) {
+      log.error(OBMessageUtils.messageBD("CreateLinesFromOrderError"),
+          "Error in CreateLinesFromProcess when reading a JSONObject", e);
+      throw new OBException(e);
+    }
   }
 
   /**
@@ -149,98 +178,7 @@ public class CreateInvoiceLinesFromProcess {
     }
   }
 
-  private JSONObject getPickEditLineValuesInPosition(final int index) {
-    try {
-      return selectedLines.getJSONObject(index);
-    } catch (JSONException e) {
-      log.error(OBMessageUtils.messageBD("CreateLinesFromOrderError"),
-          "Error in CreateLinesFromProcess when reading a JSONObject", e);
-      throw new OBException(e);
-    }
-  }
-
-  /**
-   * Creates a new invoice line from an existing line
-   * 
-   * @param copiedLine
-   *          The BaseOBObject representing the selected object in the PE
-   * @param selectedLine
-   *          The JSONOBject representing the selected object in the PE
-   * @return The created invoice line
-   */
-  private InvoiceLine createLineFromSelectedLineAndRunHooks(BaseOBObject copiedLine,
-      final JSONObject pickExecuteLineValues) {
-    InvoiceLine newInvoiceLine = OBProvider.getInstance().get(InvoiceLine.class);
-
-    // Always increment the lineNo when adding a new invoice line
-    newInvoiceLine.setLineNo(nextLineNo());
-    newInvoiceLine.setInvoice(processingInvoice);
-
-    // Execute Hooks to perform operations
-    executeHooks(pickExecuteLineValues, copiedLine, newInvoiceLine);
-
-    return newInvoiceLine;
-  }
-
-  private Long nextLineNo() {
-    lastLineNo = lastLineNo + 10L;
-    return lastLineNo;
-  }
-
-  private void executeHooks(JSONObject pickExecuteLineValues, final BaseOBObject line,
-      InvoiceLine newInvoiceLine) {
-    try {
-      if (createLinesFromProcessHooks != null) {
-        final List<CreateLinesFromProcessImplementationInterface> hooks = new ArrayList<>();
-        for (CreateLinesFromProcessImplementationInterface hook : createLinesFromProcessHooks
-            .select(new ComponentProvider.Selector(
-                CreateLinesFromProcessImplementationInterface.CREATE_LINES_FROM_PROCESS_HOOK_QUALIFIER))) {
-          if (hook != null) {
-            hooks.add(hook);
-          }
-        }
-
-        Collections.sort(hooks, new CreateLinesFromHookComparator());
-        for (CreateLinesFromProcessImplementationInterface hook : hooks) {
-          hook.exec(newInvoiceLine, pickExecuteLineValues, line);
-        }
-      }
-    } catch (Exception e) {
-      log.error("Error in CreateLinesFromProcess executing hooks.", e);
-      throw new OBException(e);
-    }
-  }
-
-  private class CreateLinesFromHookComparator implements
-      Comparator<CreateLinesFromProcessImplementationInterface> {
-    @Override
-    public int compare(CreateLinesFromProcessImplementationInterface a,
-        CreateLinesFromProcessImplementationInterface b) {
-      return a.getOrder() < b.getOrder() ? -1 : a.getOrder() == b.getOrder() ? 0 : 1;
-    }
-  }
-
-  /**
-   * Returns the max invoice line number defined in the invoice to which the lines are going to be
-   * added
-   *
-   * @return The max invoice line number
-   */
-  private Long getLastLineNoOfCurrentInvoice() {
-    OBCriteria<InvoiceLine> obc = OBDal.getInstance().createCriteria(InvoiceLine.class);
-    obc.add(Restrictions.eq(InvoiceLine.PROPERTY_INVOICE, processingInvoice));
-    obc.setProjection(Projections.max(InvoiceLine.PROPERTY_LINENO));
-    Long lineNumber = 0L;
-    obc.setMaxResults(1);
-    Object o = obc.uniqueResult();
-    if (o != null) {
-      lineNumber = (Long) o;
-    }
-    return lineNumber;
-  }
-
   private void processCopiedLineShipmentInOut(BaseOBObject copiedLine) {
-
     StringBuilder shipmentHQLQuery = new StringBuilder(" as il");
     shipmentHQLQuery.append(" join il.shipmentReceipt sh");
     shipmentHQLQuery.append(" where il.salesOrderLine.id = :orderLineId");
@@ -285,6 +223,69 @@ public class CreateInvoiceLinesFromProcess {
       return line;
     } catch (JSONException e) {
       throw new OBException(e);
+    }
+  }
+
+  /**
+   * Creates a new invoice line from an existing line
+   * 
+   * @param currentInvoice
+   *          The invoice currently being created
+   * @param copiedLine
+   *          The BaseOBObject representing the selected object in the PE
+   * @param selectedLine
+   *          The JSONOBject representing the selected object in the PE
+   * @return The created invoice line
+   */
+  private InvoiceLine createLineFromSelectedLineAndRunHooks(final Invoice currentInvoice,
+      final BaseOBObject copiedLine, final JSONObject pickExecuteLineValues) {
+    InvoiceLine newInvoiceLine = OBProvider.getInstance().get(InvoiceLine.class);
+
+    // Always increment the lineNo when adding a new invoice line
+    newInvoiceLine.setLineNo(nextLineNo());
+    newInvoiceLine.setInvoice(currentInvoice);
+
+    // Execute Hooks to perform operations
+    executeHooks(pickExecuteLineValues, copiedLine, newInvoiceLine);
+
+    return newInvoiceLine;
+  }
+
+  private Long nextLineNo() {
+    lastLineNo = lastLineNo + 10L;
+    return lastLineNo;
+  }
+
+  private void executeHooks(JSONObject pickExecuteLineValues, final BaseOBObject line,
+      InvoiceLine newInvoiceLine) {
+    try {
+      if (createLinesFromProcessHooks != null) {
+        final List<CreateLinesFromProcessImplementationInterface> hooks = new ArrayList<>();
+        for (CreateLinesFromProcessImplementationInterface hook : createLinesFromProcessHooks
+            .select(new ComponentProvider.Selector(
+                CreateLinesFromProcessImplementationInterface.CREATE_LINES_FROM_PROCESS_HOOK_QUALIFIER))) {
+          if (hook != null) {
+            hooks.add(hook);
+          }
+        }
+
+        Collections.sort(hooks, new CreateLinesFromHookComparator());
+        for (CreateLinesFromProcessImplementationInterface hook : hooks) {
+          hook.exec(newInvoiceLine, pickExecuteLineValues, line);
+        }
+      }
+    } catch (Exception e) {
+      log.error("Error in CreateLinesFromProcess executing hooks.", e);
+      throw new OBException(e);
+    }
+  }
+
+  private static class CreateLinesFromHookComparator implements
+      Comparator<CreateLinesFromProcessImplementationInterface> {
+    @Override
+    public int compare(CreateLinesFromProcessImplementationInterface a,
+        CreateLinesFromProcessImplementationInterface b) {
+      return a.getOrder() < b.getOrder() ? -1 : a.getOrder() == b.getOrder() ? 0 : 1;
     }
   }
 }
