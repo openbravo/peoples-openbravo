98e68d3b4ae36c664c68d0354a73db21cedcf1ad
Carlos Aristu <carlos.aristu@openbravo.com>
2018-05-29 12:36:13 +0200

    fixes big 38489: Display field of table reference can't be retrieved by some DS
      Some datasources (like those extending ReadOnlyDataSourceService) use the DataToJsonConverter.convertToJsonObjects() method to return its result. When converting BaseOBObjects this method was not using the related Property to place its corresponding value into the resulting JSON.
    
      Now we make use of the Property, this allows to retrieve the "Display Field" if the Property is a table reference, when invoking the DataToJsonConverter.addBaseOBObject() method.
    
    --HG--
    extra : amend_source : d02cffb20a43c7ce57aa4f5d6d2e208aba8aea6b

--- modules/org.openbravo.service.json/src/org/openbravo/service/json/DataToJsonConverter.java
+++ modules/org.openbravo.service.json/src/org/openbravo/service/json/DataToJsonConverter.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2009-2017 Openbravo SLU 
+ * All portions are Copyright (C) 2009-2018 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -109,7 +109,9 @@ public class DataToJsonConverter {
           }
           final Object value = dataInstance.get(key);
           if (value instanceof BaseOBObject) {
-            addBaseOBObject(jsonObject, null, key, null, (BaseOBObject) value);
+            Property referencedProperty = property != null ? property.getReferencedProperty()
+                : null;
+            addBaseOBObject(jsonObject, property, key, referencedProperty, (BaseOBObject) value);
           } else {
             Object convertedValue = null;
             if (value != null && property != null && property.isPrimitive()) {
