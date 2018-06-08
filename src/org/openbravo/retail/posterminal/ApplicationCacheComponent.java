56159b4ffd420b0ac5f020b1619083e5eefda064
Javier Rodriguez <javier.rodriguez@openbravo.com>
2018-06-08 10:07:21 +0200

    Fixed issue 38720: displayreceipt.xml is now in manifest
    --HG--
    extra : rebase_source : 558136469bab962bda33336e445a274aa72a2740

--- src/org/openbravo/retail/posterminal/ApplicationCacheComponent.java
+++ src/org/openbravo/retail/posterminal/ApplicationCacheComponent.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2012-2017 Openbravo SLU
+ * All portions are Copyright (C) 2012-2018 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -84,6 +84,7 @@ public class ApplicationCacheComponent extends MobileCoreApplicationCacheCompone
     resources.add("../../web/org.openbravo.retail.posterminal/res/welcome.xml");
     resources.add("../../web/org.openbravo.retail.posterminal/res/goodbye.xml");
     resources.add("../../web/org.openbravo.retail.posterminal/res/checkdrawerstatus.xml");
+    resources.add("../../web/org.openbravo.retail.posterminal/res/displayreceipt.xml");
 
     // App Icon
     resources.add("../../web/images/favicon.ico");
