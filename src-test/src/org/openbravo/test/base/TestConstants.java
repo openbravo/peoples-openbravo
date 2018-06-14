3035f134596553bd8f077e27c65377883e564bf3
Asier Lostalé <asier.lostale@openbravo.com>
2018-06-14 16:49:46 +0200

    related to bug 38761: rename inner class to prevent collisions
    --HG--
    extra : rebase_source : 1b24f43d7bde03b8273b6e41cbc7c42d1d2387ab

--- src-test/src/org/openbravo/test/base/TestConstants.java
+++ src-test/src/org/openbravo/test/base/TestConstants.java
@@ -22,16 +22,21 @@ package org.openbravo.test.base;
 /** Some constants to be used in tests */
 public class TestConstants {
 
-  public static class Org {
+  public static class Orgs {
     public static final String MAIN = "0";
     public static final String FB_GROUP = "19404EAD144C49A0AF37D54377CF452D";
+
     public static final String US = "2E60544D37534C0B89E765FE29BC0B43";
     public static final String US_EST = "7BABA5FF80494CAFA54DEBD22EC46F01";
     public static final String US_WEST = "BAE22373FEBE4CCCA24517E23F0C8A48";
+
     public static final String ESP = "B843C30461EA4501935CB1D125C9C25A";
     public static final String ESP_SUR = "DC206C91AA6A4897B44DA897936E0EC3";
     public static final String ESP_NORTE = "E443A31992CB4635AFCAEABE7183CE85";
-    public static final String ESP_ADMIN_ROLE_ID = "F3196A30B53A42778727B2852FF90C24";
+  }
+
+  public static class Roles {
+    public static final String ESP_ADMIN = "F3196A30B53A42778727B2852FF90C24";
   }
 
   private TestConstants() {
