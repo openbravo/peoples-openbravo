119c6fdcf4ecd4699324d623d952cb8a5035301a
Atul Gaware <atul.gaware@openbravo.com>
2018-06-14 06:34:24 +0530

    Fixes Issue 38735: incorrect query in ReservationUtils
    HQL query in getReservationStockFromStorageDetail method
    in ReservationUtils.java had ")" end brace at end with out
    "(" start brace in the query. This is not correct but was
    allowed in hibernate version 3.6, but hibernate version 5.3
    does not allow such error in query. Hence the query is modified
    by removing the ")" end brace.

--- src/org/openbravo/materialmgmt/ReservationUtils.java
+++ src/org/openbravo/materialmgmt/ReservationUtils.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2012-2017 Openbravo SLU
+ * All portions are Copyright (C) 2012-2018 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  *************************************************************************
@@ -266,7 +266,7 @@ public class ReservationUtils {
     hql.append(" and coalesce(rs.storageBin, r.storageBin) = :storageBin");
     hql.append(" and coalesce(rs.attributeSetValue, r.attributeSetValue) = :attributeSetValue");
     hql.append(" and r.uOM = :uom");
-    hql.append(" and rs.quantity > rs.released)");
+    hql.append(" and rs.quantity > rs.released");
 
     Query query = OBDal.getInstance().getSession().createQuery(hql.toString());
     query.setParameter("product", storageDetail.getProduct());
