fe00cfef9180f813ca321febb201acdda1cc1594
Alejandro <alekosmp86@gmail.com>
2018-09-04 15:02:09 -0400

    Fixes issue 39219: Order is imported without lines if Supr is pressed more than once after Done is launched.
    * After Done is launched if the external inputs are active, they are disabled, and
    re-enabled once the process has finished.
    
    --HG--
    extra : rebase_source : 95623d3223f4eaab52f95acaf8b75d8f91a4c218

--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
@@ -386,11 +386,18 @@ OB.OBPOSPointOfSale.Model.PointOfSale = OB.Model.TerminalWindowModel.extend({
       OB.UTIL.TicketCloseUtils.paymentAccepted(receipt, orderList, function () {
         if (OB.MobileApp.view.openedPopup === null) {
           enyo.$.scrim.hide();
+          if (OB.MobileApp.restoreUseBarcodeProperty) {
+            OB.MobileApp.model.attributes.useBarcode = true;
+          }
         }
       });
     }, this);
 
     receipt.on('paymentDone', function (openDrawer) {
+      if (OB.MobileApp.model.get('useBarcode')) {
+        OB.MobileApp.model.set('useBarcode', false);
+        OB.MobileApp.restoreUseBarcodeProperty = true;
+      }
       receipt.trigger('disableDoneButton');
       if (receipt.get('paymentDone')) {
         return true;
