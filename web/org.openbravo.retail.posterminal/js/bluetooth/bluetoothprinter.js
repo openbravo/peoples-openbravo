58c9ef3a0f57b2d2401545263919b195a079c74c
Adrián Romero <adrianromero@openbravo.com>
2018-09-11 12:43:25 +0200

    [Bluetooth] Adding support for barcode printing and text formatting

--- web/org.openbravo.retail.posterminal/js/bluetooth/bluetoothprinter.js
+++ web/org.openbravo.retail.posterminal/js/bluetooth/bluetoothprinter.js
@@ -13,8 +13,8 @@
 
   window.OB = window.OB || {};
 
-  function padStart(txt, length) {
-    var result = txt.padStart(length);
+  function padStart(txt, length, character) {
+    var result = txt.padStart(length, character);
     if (result.length > length) {
       return result.substring(result.length - length);
     } else {
@@ -22,8 +22,8 @@
     }
   }
 
-  function padEnd(txt, length) {
-    var result = txt.padEnd(length);
+  function padEnd(txt, length, character) {
+    var result = txt.padEnd(length, character);
     if (result.length > length) {
       return result.substring(0, length);
     } else {
@@ -31,11 +31,10 @@
     }
   }
 
-  function padCenter(txt, length) {
+  function padCenter(txt, length, character) {
     var midlength = (length + txt.length) / 2;
-    return padEnd(padStart(txt, midlength), length);
+    return padEnd(padStart(txt, midlength, character), length, character);
   }
-
 
   function append(a1, a2) {
     var tmp = new Uint8Array(a1.length + a2.length);
@@ -105,6 +104,8 @@
       if (el.nodeName === 'line') {
         printerdoc = append(printerdoc, this.processLine(el));
         printerdoc = append(printerdoc, OB.ESCPOS.NEW_LINE);
+      } else if (el.nodeName === 'barcode') {
+        printerdoc = append(printerdoc, this.processBarcode(el));
       }
     }.bind(this));
 
@@ -113,12 +114,26 @@
 
   OB.BluetoothPrinter.prototype.processLine = function (dom) {
     var line = new Uint8Array();
+    var fontsize = dom.getAttribute('size');
+
+    if (fontsize === '1') {
+      line = append(line, OB.ESCPOS.CHAR_SIZE_1);
+    } else if (fontsize === '2') {
+      line = append(line, OB.ESCPOS.CHAR_SIZE_2);
+    } else if (fontsize === '3') {
+      line = append(line, OB.ESCPOS.CHAR_SIZE_3);
+    } else {
+      line = append(line, OB.ESCPOS.CHAR_SIZE_0);
+    }
 
     Array.from(dom.children).forEach(function (el) {
       if (el.nodeName === 'text') {
         var txt = el.textContent;
         var len = parseInt(el.getAttribute('length'), 10) || txt.length;
         var align = el.getAttribute('align');
+        var bold = el.getAttribute('bold');
+        var uderline = el.getAttribute('underline');
+
         if (align === 'right') {
           txt = padStart(txt, len);
         } else if (align === 'center') {
@@ -126,10 +141,77 @@
         } else {
           txt = padEnd(txt, len);
         }
+
+        if (bold === 'true') {
+          line = append(line, OB.ESCPOS.BOLD_SET);
+        }
+        if (uderline === 'true') {
+          line = append(line, OB.ESCPOS.UNDERLINE_SET);
+        }
         line = append(line, encoder.encode(txt));
+        if (bold === 'true') {
+          line = append(line, OB.ESCPOS.BOLD_RESET);
+        }
+        if (uderline === 'true') {
+          line = append(line, OB.ESCPOS.UNDERLINE_RESET);
+        }
+
       }
     }.bind(this));
 
+    return line;
+  };
+
+  OB.BluetoothPrinter.prototype.processBarcode = function (el) {
+    var line = new Uint8Array();
+    var position = el.getAttribute('position');
+    var type = el.getAttribute('type');
+    var barcode = el.textContent;
+
+    line = append(line, OB.ESCPOS.CENTER_JUSTIFICATION);
+
+    line = append(line, OB.ESCPOS.NEW_LINE);
+    line = append(line, OB.ESCPOS.BAR_HEIGHT);
+    if (position === 'none') {
+      line = append(line, OB.ESCPOS.BAR_POSITIONNONE);
+    } else {
+      line = append(line, OB.ESCPOS.BAR_POSITIONDOWN);
+    }
+
+    if (type === 'EAN13') {
+      line = append(line, OB.ESCPOS.BAR_WIDTH3);
+
+      line = append(line, OB.ESCPOS.BAR_HRIFONT1);
+      line = append(line, OB.ESCPOS.BAR_CODE02);
+
+      barcode = padStart(barcode, 13, '0');
+      barcode = barcode.substring(0, 12);
+      line = append(line, encoder.encode(barcode));
+      line = append(line, new Uint8Array([0x00]));
+    } else if (type === 'CODE128') {
+      if (barcode.length > 14) {
+        line = append(line, OB.ESCPOS.BAR_WIDTH1);
+      } else if (barcode.length > 8) {
+        line = append(line, OB.ESCPOS.BAR_WIDTH2);
+      } else {
+        line = append(line, OB.ESCPOS.BAR_WIDTH3);
+      }
+      line = append(line, OB.ESCPOS.BAR_HRIFONT1);
+      line = append(line, OB.ESCPOS.BAR_CODE128);
+
+      var barcode128 = OB.ESCPOS.transCode128(barcode);
+      line = append(line, new Uint8Array([barcode128.length + 2]));
+      line = append(line, OB.ESCPOS.BAR_CODE128TYPE);
+      line = append(line, barcode128);
+      line = append(line, new Uint8Array([0x00]));
+    } else { // Unknown barcode type
+      line = append(line, encoder.encode(type));
+      line = append(line, encoder.encode(': '));
+      line = append(line, encoder.encode(barcode));
+    }
+
+    line = append(line, OB.ESCPOS.NEW_LINE);
+    line = append(line, OB.ESCPOS.LEFT_JUSTIFICATION);
     return line;
   };
 
--- web/org.openbravo.retail.posterminal/js/bluetooth/escpos.js
+++ web/org.openbravo.retail.posterminal/js/bluetooth/escpos.js
@@ -24,6 +24,216 @@
     BOLD_SET: new Uint8Array([0x1B, 0x45, 0x01]),
     BOLD_RESET: new Uint8Array([0x1B, 0x45, 0x00]),
     UNDERLINE_SET: new Uint8Array([0x1B, 0x2D, 0x01]),
-    UNDERLINE_RESET: new Uint8Array([0x1B, 0x2D, 0x00])
+    UNDERLINE_RESET: new Uint8Array([0x1B, 0x2D, 0x00]),
+
+    CENTER_JUSTIFICATION: new Uint8Array([0x1B, 0x61, 0x01]),
+    LEFT_JUSTIFICATION: new Uint8Array([0x1B, 0x61, 0x00]),
+    RIGHT_JUSTIFICATION: new Uint8Array([0x1B, 0x61, 0x02]),
+
+    BAR_HEIGHT: new Uint8Array([0x1D, 0x68, 0x40]),
+    BAR_WIDTH3: new Uint8Array([0x1D, 0x77, 0x03]),
+    BAR_WIDTH2: new Uint8Array([0x1D, 0x77, 0x02]),
+    BAR_WIDTH1: new Uint8Array([0x1D, 0x77, 0x01]),
+    BAR_POSITIONDOWN: new Uint8Array([0x1D, 0x48, 0x02]),
+    BAR_POSITIONNONE: new Uint8Array([0x1D, 0x48, 0x00]),
+    BAR_HRIFONT1: new Uint8Array([0x1D, 0x66, 0x01]),
+    BAR_CODE02: new Uint8Array([0x1D, 0x6B, 0x02]),
+    BAR_CODE128: new Uint8Array([0x1D, 0x6B, 0x49]),
+    BAR_CODE128TYPE: new Uint8Array([0x7B, 0x42]),
+
+    transCode128: function (txt) {
+
+      function transCode128Char(c) {
+        switch (c) {
+        case '/':
+          return 0x2F;
+        case '0':
+          return 0x30;
+        case '1':
+          return 0x31;
+        case '2':
+          return 0x32;
+        case '3':
+          return 0x33;
+        case '4':
+          return 0x34;
+        case '5':
+          return 0x35;
+        case '6':
+          return 0x36;
+        case '7':
+          return 0x37;
+        case '8':
+          return 0x38;
+        case '9':
+          return 0x39;
+        case 'A':
+          return 0x41;
+        case 'B':
+          return 0x42;
+        case 'C':
+          return 0x43;
+        case 'D':
+          return 0x44;
+        case 'E':
+          return 0x45;
+        case 'F':
+          return 0x46;
+        case 'G':
+          return 0x47;
+        case 'H':
+          return 0x48;
+        case 'I':
+          return 0x49;
+        case 'J':
+          return 0x4A;
+        case 'K':
+          return 0x4B;
+        case 'L':
+          return 0x4C;
+        case 'M':
+          return 0x4D;
+        case 'N':
+          return 0x4E;
+        case 'O':
+          return 0x4F;
+        case 'P':
+          return 0x50;
+        case 'Q':
+          return 0x51;
+        case 'R':
+          return 0x52;
+        case 'S':
+          return 0x53;
+        case 'T':
+          return 0x54;
+        case 'U':
+          return 0x55;
+        case 'V':
+          return 0x56;
+        case 'W':
+          return 0x57;
+        case 'X':
+          return 0x58;
+        case 'Y':
+          return 0x59;
+        case 'Z':
+          return 0x5A;
+        case '[':
+          return 0x5B;
+        case '\\':
+          return 0x5C;
+        case ']':
+          return 0x5D;
+        case '^':
+          return 0x5E;
+        case '_':
+          return 0x5F;
+        case '\'':
+          return 0x60;
+        case 'a':
+          return 0x61;
+        case 'b':
+          return 0x62;
+        case 'c':
+          return 0x63;
+        case 'd':
+          return 0x64;
+        case 'e':
+          return 0x65;
+        case 'f':
+          return 0x66;
+        case 'g':
+          return 0x67;
+        case 'h':
+          return 0x68;
+        case 'i':
+          return 0x69;
+        case 'j':
+          return 0x6A;
+        case 'k':
+          return 0x6B;
+        case 'l':
+          return 0x6C;
+        case 'm':
+          return 0x6D;
+        case 'n':
+          return 0x6E;
+        case 'o':
+          return 0x6F;
+        case 'p':
+          return 0x70;
+        case 'q':
+          return 0x71;
+        case 'r':
+          return 0x72;
+        case 's':
+          return 0x73;
+        case 't':
+          return 0x74;
+        case 'u':
+          return 0x75;
+        case 'v':
+          return 0x76;
+        case 'w':
+          return 0x77;
+        case 'x':
+          return 0x78;
+        case 'y':
+          return 0x79;
+        case 'z':
+          return 0x7A;
+        case '{':
+          return 0x7B;
+        case '|':
+          return 0x7C;
+        case '}':
+          return 0x7D;
+        case '~':
+          return 0x7E;
+        case ':':
+          return 0x3A;
+        case ';':
+          return 0x3B;
+        case '?':
+          return 0x3F;
+        case '.':
+          return 0x2E;
+        case '=':
+          return 0x3D;
+        case '-':
+          return 0x2D;
+        case '<':
+          return 0x3C;
+        case '>':
+          return 0x3E;
+        case '*':
+          return 0x2A;
+        case '+':
+          return 0x2B;
+        case '!':
+          return 0x21;
+        case '%':
+          return 0x25;
+        case '&':
+          return 0x26;
+        case '#':
+          return 0x23;
+        default:
+          return 0x30;
+        }
+      }
+
+      if (txt) {
+        var result = [];
+        var i;
+        for (i = 0; i < txt.length; i++) {
+          result.push(transCode128Char(txt.charAt(i)));
+        }
+        return new Uint8Array(result);
+      } else {
+        return new Uint8Array();
+      }
+    }
   };
 }());
