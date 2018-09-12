a4e3a22c74921be0aac5060c65075bb0ff119799
Asier Lostalé <asier.lostale@openbravo.com>
2018-09-12 15:02:46 +0200

    fixed issue 39276: ProcessSchedulingTest fails depending on system locale
      In case VariablesSecureApp is initialized without setting a language, default
      it to en_US rather than relying on system settings.

--- src/org/openbravo/base/secureApp/VariablesSecureApp.java
+++ src/org/openbravo/base/secureApp/VariablesSecureApp.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2001-2017 Openbravo S.L.U.
+ * Copyright (C) 2001-2018 Openbravo S.L.U.
  * Licensed under the Apache Software License version 2.0
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to  in writing,  software  distributed
@@ -121,8 +121,7 @@ public class VariablesSecureApp extends VariablesBase {
     this.user = strUser;
     this.role = strRole;
     if (StringUtils.isEmpty(strLanguage)) {
-      this.language = System.getProperty("user.language") + "_"
-          + System.getProperty("user.country");
+      this.language = "en_US";
     } else {
       this.language = strLanguage;
     }
