4a8acdb8e517c53933df522be4cd15a6d9076a3e
Aaron Calero <aaron.calero@openbravo.com>
2018-09-13 16:42:35 +0200

    [Cancel Layaway] Fixed issue: Order Date is not correctly shown on the CL document
    --HG--
    extra : transplant_source : %DA%A7%F5%C0e%C7%28%D2%F8Q%F1t%F0%C6X%B6%8D%A4%8E%03

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -4278,9 +4278,9 @@
                       me.set('creditAmount', OB.DEC.Zero);
                     }
                     me.set('canceledorder', clonedReceipt);
+                    me.set('orderDate', new Date());
                     me.set('documentNo', me.get('documentNo') + '*R*');
                     me.unset('generateInvoice');
-                    me.set('orderDate', new Date());
                     me.set('nettingPayment', OB.DEC.sub(me.getPayment(), me.getGross()));
                     me.set('payment', OB.DEC.abs(me.getNettingPayment()));
                     me.get('payments').reset();
