0d695754931bcfaba20e23b0cf1b35349b6f5e2d
Ranjith S R <ranjith@qualiantech.com>
2018-09-19 11:19:54 +0530

    Fixed issue 39302 : Removed callback argument in PrePayment hook and postPayment hook
    --HG--
    extra : rebase_source : fc999ec2580bcc63d285b794a67b1155e14597db

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -4540,7 +4540,7 @@
     },
 
     addPayment: function (payment, callback) {
-      var payments, total, i, max, p, order, paymentSign;
+      var payments, total, i, max, p, order, paymentSign, finalCallback;
 
       if (this.get('isPaid') && !payment.get('isReversePayment') && !this.get('doCancelAndReplace') && this.getPrePaymentQty() === OB.DEC.sub(this.getTotal(), this.getCredit()) && !this.isNewReversed()) {
         OB.UTIL.showWarning(OB.I18N.getLabel('OBPOS_CannotIntroducePayment'));
@@ -4566,13 +4566,18 @@
         return;
       }
 
+      finalCallback = function () {
+        if (callback instanceof Function) {
+          callback(order);
+        }
+      };
+
       payments = this.get('payments');
       total = OB.DEC.abs(this.getTotal());
       OB.UTIL.HookManager.executeHooks('OBPOS_preAddPayment', {
         paymentToAdd: payment,
         payments: payments,
-        receipt: this,
-        callback: callback
+        receipt: this
       }, function (args) {
         var executeFinalCallback = function (saveChanges) {
             if (saveChanges) {
@@ -4584,12 +4589,9 @@
             OB.UTIL.HookManager.executeHooks('OBPOS_postAddPayment', {
               paymentAdded: payment,
               payments: payments,
-              receipt: order,
-              callback: callback
-            }, function (args) {
-              if (args.callback instanceof Function) {
-                args.callback(order);
-              }
+              receipt: order
+            }, function (args2) {
+              finalCallback();
             });
             };
 
@@ -6957,8 +6959,7 @@
       }
     },
     addPayment: function (payment, callback) {
-      var payments, total;
-      var i, max, p, order;
+      var i, max, p, order, payments, total, finalCallback;
 
       if (!OB.DEC.isNumber(payment.get('amount'))) {
         OB.UTIL.showWarning(OB.I18N.getLabel('OBPOS_MsgPaymentAmountError'));
@@ -6974,25 +6975,27 @@
         return;
       }
 
+      finalCallback = function () {
+        if (callback instanceof Function) {
+          callback(order);
+        }
+      };
+
       payments = this.get('payments');
       total = OB.DEC.abs(this.getTotal());
       order = this;
       OB.UTIL.HookManager.executeHooks('OBPOS_preAddPayment', {
         paymentToAdd: payment,
         payments: payments,
-        receipt: this,
-        callback: callback
+        receipt: this
       }, function (args) {
         var executeFinalCallback = function () {
             OB.UTIL.HookManager.executeHooks('OBPOS_postAddPayment', {
               paymentAdded: payment,
               payments: payments,
-              receipt: order,
-              callback: callback
-            }, function (args) {
-              if (args.callback instanceof Function) {
-                args.callback(order);
-              }
+              receipt: order
+            }, function (args2) {
+              finalCallback();
             });
             };
 
