5dd2338c0ce7767002fc8640d7854ec11d51e262
Asier Lostalé <asier.lostale@openbravo.com>
2018-10-09 09:26:17 +0200

    [session] updated copyright

--- src/org/openbravo/erpCommon/ad_forms/ShowSessionVariablesStructureData.java
+++ src/org/openbravo/erpCommon/ad_forms/ShowSessionVariablesStructureData.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2001-2010 Openbravo S.L.U.
+ * All portions are Copyright (C) 2001-2018 Openbravo S.L.U.
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
--- src/org/openbravo/erpCommon/utility/reporting/printing/AttachContent.java
+++ src/org/openbravo/erpCommon/utility/reporting/printing/AttachContent.java
@@ -10,7 +10,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2009-2010 Openbravo SLU 
+ * All portions are Copyright (C) 2009-2018 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************/
