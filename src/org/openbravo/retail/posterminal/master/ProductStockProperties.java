c3620e8452d689cc8ec61b8782dd071abf05a204
Jorge Garcia <jorge.garcia@openbravo.com>
2018-10-19 13:08:00 +0200

    Fixed issue 39278: A new ProcessHQLQuery to export the product stock by assortment is needed
    Added a new processHQLQuery to get the stock of the products loaded by the
    WebPOS in the warehouses on hand of the organization.
    
    Defined a new mobile service for this new request.
    
    --HG--
    extra : rebase_source : 0402bbb8848dd41d57770cb0286395cb88007a17

---
+++ src/org/openbravo/retail/posterminal/master/ProductStockProperties.java
@@ -0,0 +1,34 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2018 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+package org.openbravo.retail.posterminal.master;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openbravo.client.kernel.ComponentProvider.Qualifier;
+import org.openbravo.mobile.core.model.HQLProperty;
+import org.openbravo.mobile.core.model.ModelExtension;
+
+@Qualifier(ProductStock.productStockPropertyExtension)
+public class ProductStockProperties extends ModelExtension {
+
+  @Override
+  public List<HQLProperty> getHQLProperties(Object params) {
+
+    List<HQLProperty> list = new ArrayList<HQLProperty>();
+    list.add(new HQLProperty("storagedetail.product.id", "m_product_id"));
+    list.add(new HQLProperty("warehouse.id", "m_warehouse_id"));
+    list.add(new HQLProperty("sum(storagedetail.quantityOnHand) + sum(storagedetail.reservedQty)",
+        "availableStock", false));
+
+    return list;
+
+  }
+}
