8317a045563c8422baaaa7f50dd8e2236e1699df
Asier Martirena <asier.martirena@openbravo.com>
2018-10-31 12:17:21 +0100

    [Omnichannel] Updated license and changed the interface for an abstract class

--- src/org/openbravo/retail/posterminal/OrderLoaderCreateOrderlineHook.java
+++ src/org/openbravo/retail/posterminal/OrderLoaderCreateOrderlineHook.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2016 Openbravo S.L.U.
+ * Copyright (C) 2018 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -17,6 +17,6 @@ import org.openbravo.model.common.order.OrderLine;
  * process. They can modify jsonorder parameter.
  * 
  */
-public interface OrderLoaderCreateOrderlineHook {
-  public void exec(JSONObject jsonorderLine, OrderLine orderLine) throws Exception;
+public abstract class OrderLoaderCreateOrderlineHook {
+  public abstract void exec(JSONObject jsonorderLine, OrderLine orderLine) throws Exception;
 }
