82afe4f5fb726cedac4597e8a0a6077c13f36617
Armaignac <collazoandy4@gmail.com>
2018-11-14 17:18:10 -0500

    Fixes issue 39631: A new property is required in DiscountFilterProductProperties to select the product search key
    A new property was added to select the product search key

--- src/org/openbravo/retail/posterminal/master/DiscountFilterProductProperties.java
+++ src/org/openbravo/retail/posterminal/master/DiscountFilterProductProperties.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2014 Openbravo S.L.U.
+ * Copyright (C) 2014-2018 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -35,6 +35,7 @@ public class DiscountFilterProductProperties extends ModelExtension {
         add(new HQLProperty("ap.id", "id"));
         add(new HQLProperty("ap.priceAdjustment.id", "priceAdjustment"));
         add(new HQLProperty("ap.product.id", "product"));
+        add(new HQLProperty("ap.product.searchKey", "searchKey"));
         add(new HQLProperty("ap.obdiscIsGift", "obdiscIsGift"));
         add(new HQLProperty("ap.obdiscQty", "obdiscQty"));
         add(new HQLProperty("ap.obdiscGifqty", "obdiscGifqty"));
