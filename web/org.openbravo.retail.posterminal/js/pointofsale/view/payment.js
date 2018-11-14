f4edf56ea1adb1951f85172100558de9abfa7762
Asier Martirena <asier.martirena@openbravo.com>
2018-11-14 17:23:38 +0100

    [Omnichannel] The 'Sell on Credit' button must be shown if the ticket is not synchronized and is not a CL

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/payment.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/payment.js
@@ -458,11 +458,8 @@ enyo.kind({
 
     // The terminal allows to pay on credit
     var visible = OB.MobileApp.model.get('terminal').allowpayoncredit;
-    // And is a loaded layaway or a regular order (no new layaway and no voided layaway)
-    // this.receipt.get('orderType') === 2 --> New layaway
-    // this.receipt.get('orderType') === 3 --> Canceled layaway
-    // this.receipt.get('isLayaway') --> Loaded layaway
-    visible = visible && ((this.receipt.get('isLayaway') || this.receipt.get('orderType') !== 2) && this.receipt.get('orderType') !== 3);
+    // And is a layaway or a new order (no loaded order)
+    visible = visible && !this.receipt.get('isPaid') && this.receipt.get('orderType') !== 3;
     // And receipt has not been paid
     visible = visible && !this.receipt.getPaymentStatus().done;
     // And Business Partner exists and is elegible to sell on credit.
