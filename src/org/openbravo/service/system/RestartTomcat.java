f5b6ad68e7581a6702d564eba07fc327bfb062b9
Javier Armendáriz <javier.armendariz@openbravo.com>
2018-11-21 07:56:31 +0100

    Related to issue 39504: Using old log4j API in RestartTomcat task.
    Using the new API at this point is problematic as it causes problems because
    the class is compiled using the new libraries but at this moment Tomcat still
    has the old libraries until it restarts.

--- src/org/openbravo/service/system/RestartTomcat.java
+++ src/org/openbravo/service/system/RestartTomcat.java
@@ -21,8 +21,7 @@ package org.openbravo.service.system;
 
 import java.io.File;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.Logger;
 import org.openbravo.base.exception.OBException;
 import org.openbravo.base.session.OBPropertiesProvider;
 
@@ -36,7 +35,7 @@ import org.openbravo.base.session.OBPropertiesProvider;
  * @author mtaal
  */
 public class RestartTomcat {
-  private static final Logger log = LogManager.getLogger();
+  private static final Logger log = Logger.getLogger(RestartTomcat.class);
 
   /**
    * Method is called from the tomcat.restart tasks, this method again starts the tomcat.restart.do
