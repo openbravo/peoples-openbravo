c5a322f0682f210e57e5dd51869ae4763cbdcbd1
Javier Armendáriz <javier.armendariz@openbravo.com>
2018-11-21 07:56:31 +0100

    Related to issue 39504: ExecutionLimitBaseProcess should still use old logger.
    To avoid problems when recompiling where a subclass (i.e. BuildValidation) uses
    Logger but it is not recompiled as it has no changes so it still references old
    logger.

--- src-core/src/org/openbravo/base/ExecutionLimitBaseProcess.java
+++ src-core/src/org/openbravo/base/ExecutionLimitBaseProcess.java
@@ -14,15 +14,16 @@ package org.openbravo.base;
 import java.io.File;
 import java.util.Map;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.Logger;
 import org.openbravo.database.CPStandAlone;
 import org.openbravo.database.ConnectionProvider;
 import org.openbravo.modulescript.OpenbravoVersion;
 
 public abstract class ExecutionLimitBaseProcess {
 
-  protected static final Logger log4j = LogManager.getLogger();
+  // Do not update to log4j2. It may cause problems when upgrading from a version already compiled
+  // log4j 1.x Logger
+  protected static final Logger log4j = Logger.getLogger(ExecutionLimitBaseProcess.class);
 
   private ConnectionProvider cp;
 
