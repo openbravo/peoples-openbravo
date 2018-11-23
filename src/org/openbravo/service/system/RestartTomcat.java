4525598d8606af73d5b074f7ce205fe1aa271b17
Javier Armendáriz <javier.armendariz@openbravo.com>
2018-11-23 09:27:56 +0100

    Related to issue 39504: Adding comment in RestartTomcat.
    --HG--
    extra : rebase_source : d08a1d8fc0f02b66117535c9264bcbcbe2224520
    extra : amend_source : aec5cb8ecc1f970134ae92a792487bf6b168a438

--- src/org/openbravo/service/system/RestartTomcat.java
+++ src/org/openbravo/service/system/RestartTomcat.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2008-2010 Openbravo SLU 
+ * All portions are Copyright (C) 2008-2018 Openbravo SLU
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -35,6 +35,7 @@ import org.openbravo.base.session.OBPropertiesProvider;
  * @author mtaal
  */
 public class RestartTomcat {
+  // Do not upgrade to log4j2 API. I may cause problems when upgrading via GUI
   private static final Logger log = Logger.getLogger(RestartTomcat.class);
 
   /**
