8fe064412e95398537e1a9e1cc8806f9f0c4c10c
Sandra Huguet <sandra.huguet@openbravo.com>
2019-01-21 16:40:53 +0100

    [ADTR] Added buildvalidation Added buildvalidation in order to check if more than one document type per type and store.

---
+++ src-util/buildvalidation/src/org/openbravo/retail/posterminal/buildvalidation/DocumentTypesinStore.java
@@ -0,0 +1,54 @@
+/*
+************************************************************************************
+* Copyright (C) 2019 Openbravo S.L.U.
+* Licensed under the Openbravo Commercial License version 1.0
+* You may obtain a copy of the License at
+http://www.openbravo.com/legal/obcl.html
+* or in the legal folder of this module distribution.
+************************************************************************************
+*/
+package org.openbravo.retail.posterminal.buildvalidation;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.openbravo.buildvalidation.BuildValidation;
+import org.openbravo.base.ExecutionLimits;
+import org.openbravo.database.ConnectionProvider;
+import org.openbravo.modulescript.OpenbravoVersion;
+
+/**
+ * This validation is related to Assignation of document types refactor
+ * 
+ */
+public class DocumentTypesinStore extends BuildValidation {
+
+  private static final String RETAIL_POSTERMINAL_MODULE_ID = "FF808181326CC34901326D53DBCF0018";
+
+  @Override
+  public List<String> execute() {
+    try {
+      List<String> errors = new ArrayList<String>();
+      ConnectionProvider cp = getConnectionProvider();
+      DocumentTypesinStoreData[] data = DocumentTypesinStoreData.validate(cp);
+      for (int i = 0; i < data.length; i++) {
+        String msg = "\nPlease, review the configuration of Document Types in the store "
+            + data[i].name
+            + " . You have more that one Document Type for the same type in the store.";
+        errors.add(msg);
+      }
+      return errors;
+    } catch (Exception e) {
+      return handleError(e);
+    }
+  }
+
+  @Override
+  protected boolean executeOnInstall() {
+    return false;
+  }
+
+//  @Override
+//  protected ExecutionLimits getBuildValidationLimits() {
+//    return new ExecutionLimits(RETAIL_POSTERMINAL_MODULE_ID, null, new OpenbravoVersion(1, 2, 5900));
+//  }
+}
