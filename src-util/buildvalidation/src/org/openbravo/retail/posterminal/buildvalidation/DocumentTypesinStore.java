1b9a8aff1752121b891f6f95af1921d32ad70dbc
Sandra Huguet <sandra.huguet@openbravo.com>
2019-01-22 09:56:00 +0100

    [ADTR] added in buildvalidation all types of document types

--- src-util/buildvalidation/src/org/openbravo/retail/posterminal/buildvalidation/DocumentTypesinStore.java
+++ src-util/buildvalidation/src/org/openbravo/retail/posterminal/buildvalidation/DocumentTypesinStore.java
@@ -29,10 +29,31 @@ public class DocumentTypesinStore extends BuildValidation {
     try {
       List<String> errors = new ArrayList<String>();
       ConnectionProvider cp = getConnectionProvider();
-      DocumentTypesinStoreData[] data = DocumentTypesinStoreData.validate(cp);
-      for (int i = 0; i < data.length; i++) {
+      DocumentTypesinStoreData[] dataDocType = DocumentTypesinStoreData.validateDocumentType(cp);
+      for (int i = 0; i < dataDocType.length; i++) {
         String msg = "\nPlease, review the configuration of Document Types in the store "
-            + data[i].name
+            + dataDocType[i].name
+            + " . You have more that one Document Type for the same type in the store.";
+        errors.add(msg);
+      }
+      DocumentTypesinStoreData[] dataDocTypeReturns = DocumentTypesinStoreData.validateDocumentTypeReturns(cp);
+      for (int i = 0; i < dataDocTypeReturns.length; i++) {
+        String msg = "\nPlease, review the configuration of Document Types for Returns in the store "
+            + dataDocTypeReturns[i].name
+            + " . You have more that one Document Type for the same type in the store.";
+        errors.add(msg);
+      }
+      DocumentTypesinStoreData[] dataDocTypeRecon = DocumentTypesinStoreData.validateDocumentTypeReconciliation(cp);
+      for (int i = 0; i < dataDocTypeRecon.length; i++) {
+        String msg = "\nPlease, review the configuration of Document Types for Reconciliations in the store "
+            + dataDocTypeRecon[i].name
+            + " . You have more that one Document Type for the same type in the store.";
+        errors.add(msg);
+      }
+      DocumentTypesinStoreData[] dataDocTypeQuo = DocumentTypesinStoreData.validateDocumentTypeQuotations(cp);
+      for (int i = 0; i < dataDocTypeQuo.length; i++) {
+        String msg = "\nPlease, review the configuration of Document Types for Quotations in the store "
+            + dataDocTypeQuo[i].name
             + " . You have more that one Document Type for the same type in the store.";
         errors.add(msg);
       }
