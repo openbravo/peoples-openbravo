5a18e736dc115824bd40c456343fc37db5cceb81
Álvaro Ferraz <alvaro.ferraz@openbravo.com>
2019-01-24 18:34:17 +0100

    [CrossStore] Make store filter mandatory

--- web/org.openbravo.retail.posterminal/js/components/modalorderselector.js
+++ web/org.openbravo.retail.posterminal/js/components/modalorderselector.js
@@ -30,6 +30,11 @@ enyo.kind({
     if (!this.initialized) {
       this.inherited(arguments);
       this.getFilterSelectorTableHeader().clearFilter();
+      var store = _.find(OB.Model.OrderFilter.getProperties(), function (prop) {
+        return prop.name === 'store';
+      }, this);
+      store.preset.id = OB.MobileApp.model.get('terminal').organization;
+      store.preset.name = 'This Store (' + OB.MobileApp.model.get('terminal').organization$_identifier + ')';
     }
   },
   init: function (model) {
