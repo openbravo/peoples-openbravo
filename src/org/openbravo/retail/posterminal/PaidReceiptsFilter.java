7ce4bb161a7b9c703335615f498da97cfc7c9dab
Javier Rodriguez <javier.rodriguez@openbravo.com>
2019-01-25 13:29:21 +0100

    Fixed issue 39996: in PaidReceiptsFilters.java, the boolean variable isPayOpenTicket is always false
    Now, if the query has the orderTypeFilter as payOpenTickets, isPayOpenTickets will be true

--- src/org/openbravo/retail/posterminal/PaidReceiptsFilter.java
+++ src/org/openbravo/retail/posterminal/PaidReceiptsFilter.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2017-2018 Openbravo S.L.U.
+ * Copyright (C) 2017-2019 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -84,7 +84,9 @@ public class PaidReceiptsFilter extends ProcessHQLQueryValidated {
       isVerifiedReturns = true;
       break;
     case "payOpenTickets":
-      orderTypeHql = "and ord.grandtotal>0 and ord.documentType.sOSubType <> 'OB' and ord.documentStatus <> 'CL'";
+      orderTypeHql = "and ord.grandTotalAmount>0 and ord.documentType.sOSubType <> 'OB' and ord.documentStatus <> 'CL'";
+      isPayOpenTicket = true;
+      break;
     default:
       orderTypeHql = "";
     }
