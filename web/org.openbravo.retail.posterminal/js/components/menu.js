15ff49b49111072e10599b8b01eb97faf1333f87
Armaignac <collazoandy4@gmail.com>
2019-02-02 00:48:10 -0500

    [CrossStore] Cancel and Replace
    Hide Cancel and Replace menu option if the receipt is from another store

--- web/org.openbravo.retail.posterminal/js/components/menu.js
+++ web/org.openbravo.retail.posterminal/js/components/menu.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2018 Openbravo S.L.U.
+ * Copyright (C) 2012-2019 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -1255,8 +1255,10 @@ enyo.kind({
         this.hide();
       } else if (OB.MobileApp.model.hasPermission('OBPOS_payments.hideCancelAndReplace', true) && !haspayments) {
         this.hide();
-      } else {
-        this.show();
+      } else if (OB.UTIL.isCrossStoreReceipt(receipt)){
+        this.hide();
+      }else{
+    	this.show();
       }
     } else {
       this.hide();
