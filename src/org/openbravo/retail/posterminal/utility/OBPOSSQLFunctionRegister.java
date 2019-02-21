30d0a289f09fb0125594c42691c777e59e84dc04
Aaron Calero <aaron.calero@openbravo.com>
2019-02-21 14:13:35 +0100

    Fixed issue 40212: [Loyalty Programs] Added hqlaggdist to the list of available hql functions
    --HG--
    extra : rebase_source : a90079dfbba80050808c4bd324f165457a58f332
    extra : amend_source : 37614c7037bb306514d48963bfcb3c461232ea41

--- src/org/openbravo/retail/posterminal/utility/OBPOSSQLFunctionRegister.java
+++ src/org/openbravo/retail/posterminal/utility/OBPOSSQLFunctionRegister.java
@@ -18,7 +18,10 @@ import org.hibernate.dialect.function.SQLFunction;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
 import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.type.StandardBasicTypes;
+import org.openbravo.base.session.OBPropertiesProvider;
 import org.openbravo.dal.core.SQLFunctionRegister;
+import org.openbravo.database.ConnectionProviderImpl;
+import org.openbravo.erpCommon.utility.SystemInfo;
 import org.openbravo.service.db.DalConnectionProvider;
 
 /**
@@ -26,6 +29,8 @@ import org.openbravo.service.db.DalConnectionProvider;
  */
 @ApplicationScoped
 public class OBPOSSQLFunctionRegister implements SQLFunctionRegister {
+
+  final static String RDBMS = new DalConnectionProvider(false).getRDBMS();
 
   @Override
   public Map<String, SQLFunction> getSQLFunctions() {
@@ -36,12 +41,12 @@ public class OBPOSSQLFunctionRegister implements SQLFunctionRegister {
         new StandardSQLFunction("obpos_currency_rate", StandardBasicTypes.STRING));
     sqlFunctions.put("get_pricelist_version",
         new SQLFunctionTemplate(StandardBasicTypes.STRING, getPriceFunction()));
+    sqlFunctions.put("hqlaggdist",
+        new SQLFunctionTemplate(StandardBasicTypes.STRING, getAggregationSQL()));
     return sqlFunctions;
   }
 
   private String getPriceFunction() {
-
-    final String RDBMS = new DalConnectionProvider(false).getRDBMS();
 
     String func = " (select m_pricelist_version_id" + " from m_pricelist_version plv "
         + " where plv.m_pricelist_id = ?1"
@@ -55,6 +60,32 @@ public class OBPOSSQLFunctionRegister implements SQLFunctionRegister {
       func = func + " limit 1)";
     }
     return func;
+  }
+
+  private String getAggregationSQL() {
+    if ("ORACLE".equals(RDBMS)) {
+      if (is11R2orNewer() && true) {
+        return "RTRIM(REGEXP_REPLACE(listagg(to_char(?1), ',') WITHIN GROUP (ORDER BY ?1),'([^,]*)(,\\1)+($|,)','\\1\\3'),',')";
+      } else {
+        return "wm_concat(distinct to_char(?1))";
+      }
+    } else {
+      return "array_to_string(array_agg(distinct ?1), ',')";
+    }
+  }
+
+  private boolean is11R2orNewer() {
+    String dbVersion = null;
+    try {
+      dbVersion = SystemInfo.getDatabaseVersion(
+          new ConnectionProviderImpl(OBPropertiesProvider.getInstance().getOpenbravoProperties()));
+    } catch (Exception ignore) {
+    }
+    if (dbVersion == null) {
+      return false;
+    }
+    int version = Integer.parseInt(dbVersion.replaceAll("\\.", "").substring(0, 3));
+    return version >= 112;
   }
 
 }
