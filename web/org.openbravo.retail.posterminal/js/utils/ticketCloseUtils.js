706fc00f915287e40a0b0e2aa3a36a1277c9c993
Gorka Gil <gorka.gil@openbravo.com>
2019-02-25 16:10:01 +0100

    Related to issue 41214: Add missing finish to completeReceipt process
    --HG--
    extra : amend_source : 23d1a19217370b09fbb80f2cd8310e3c87db1ba5
    extra : transplant_source : %ED%89%C2%C2%C4%82%2B%95g%04%AD%20%EF%27%FC%17%C5L%5D%E5

--- web/org.openbravo.retail.posterminal/js/utils/ticketCloseUtils.js
+++ web/org.openbravo.retail.posterminal/js/utils/ticketCloseUtils.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2017-2018 Openbravo S.L.U.
+ * Copyright (C) 2017-2019 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -218,6 +218,7 @@
             orderList.synchronizeCurrentOrder();
 
           }
+          OB.UTIL.ProcessController.finish('completeReceipt', execution);
           triggerClosedCallback();
         }
       });
