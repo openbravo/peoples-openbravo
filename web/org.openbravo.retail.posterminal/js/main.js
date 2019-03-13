075689541bfaaaa70610a3531d0c10e892e483dc
Javier Rodriguez <javier.rodriguez@openbravo.com>
2019-03-13 16:34:54 +0100

    Fixed issue 40374: Unable to use Ipad with more than 8000 products storaged locally

--- web/org.openbravo.retail.posterminal/js/main.js
+++ web/org.openbravo.retail.posterminal/js/main.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2017 Openbravo S.L.U.
+ * Copyright (C) 2012-2019 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -49,7 +49,7 @@
   OB.UTIL.VersionManagement.current.posterminal = {
     WebSQLDatabase: {
       name: 'WEBPOS',
-      size: 4 * 1024 * 1024,
+      size: 50 * 1024 * 1024,
       displayName: 'Openbravo Web POS'
     }
   };
