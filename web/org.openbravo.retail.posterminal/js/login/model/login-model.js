3f9b25f1f369aff411a8bae1e2db0534b43fcc2c
Miguel de Juana <miguel.dejuana@openbravo.com>
2019-04-02 10:21:33 +0200

    Fixed issue 0040388: Two cash ups in local storage if the backoffice session is closed during the cash up
    - If we do not synchronize the cashup during cashup process (because we lost the session and we need to login again), the cashup is processed when we login again. If we have to synchronize a Cashup during the login we will create 2 new cashups. One because the login process creates one if there is not a Cashup not processed and another one created by the postProcessFunction which creates a new cashup after synchronizing a cashup.
    Just avoid to create a new Cashup in postProcessFunction if we already habe created a new one.
    - Add a hook to be able to close till when synchronizing the cashup in the login process
    
    --HG--
    extra : rebase_source : a3ec11346f596862914b270792174f0fa064d45a

--- web/org.openbravo.retail.posterminal/js/login/model/login-model.js
+++ web/org.openbravo.retail.posterminal/js/login/model/login-model.js
@@ -308,26 +308,44 @@
           OB.UTIL.deleteCashUps(dataToSync, tx, requestCallback);
         },
         postProcessingFunction: function (data, callback) {
-          OB.UTIL.initCashUp(function () {
-            var cashUpId = data.at(0).get('id');
-            OB.Dal.find(OB.Model.CashUp, {
-              id: cashUpId
-            }, function (cU) {
-              if (cU.length !== 0) {
-                if (!cU.at(0).get('objToSend')) {
-                  OB.UTIL.composeCashupInfo(data, null, function () {
-                    OB.MobileApp.model.runSyncProcess();
-                  });
-                }
-              }
-            });
+          function endCallback() {
             // Get Cashup id, if objToSend is not filled compose and synchronize
             OB.UTIL.calculateCurrentCash(function () {
               if (callback) {
                 callback();
               }
             }, null, data.at(0));
-          }, null, true, data.at(0));
+          }
+          if (data.at(0).get('isprocessed') === 'N') {
+            return endCallback();
+          }
+
+          OB.Dal.find(OB.Model.CashUp, {
+            'isprocessed': 'N'
+          }, function (currentCashup) {
+            if (currentCashup.length === 0) {
+              OB.UTIL.initCashUp(function () {
+                var cashUpId = data.at(0).get('id');
+                OB.Dal.find(OB.Model.CashUp, {
+                  id: cashUpId
+                }, function (cU) {
+                  if (cU.length !== 0) {
+                    if (!cU.at(0).get('objToSend')) {
+                      OB.UTIL.composeCashupInfo(data, null, function () {
+                        OB.MobileApp.model.runSyncProcess();
+                      });
+                    }
+                  }
+                });
+                endCallback();
+              }, null, true, data.at(0));
+            } else {
+              endCallback();
+              OB.UTIL.HookManager.executeHooks('OBPOS_PostProcessingNoNewCashup', {}, function () {
+                return;
+              });
+            }
+          });
         },
         // skip the syncing of the cashup if it is the same as the last one
         preSendModel: function (me, dataToSync) {
