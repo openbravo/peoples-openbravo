c54a08f9486ebc2f1c305a9bb364819a943f3410
Adrián Romero <adrianromero@openbravo.com>
2019-04-11 14:06:51 +0200

    [Bluetooth N USB] Implement Star TSP 100 printing image function

--- web/org.openbravo.retail.posterminal/js/webprinter/usbprinters/startsp100.js
+++ web/org.openbravo.retail.posterminal/js/webprinter/usbprinters/startsp100.js
@@ -25,6 +25,38 @@
       this.DRAWER_OPEN = new Uint8Array([0x1C]);
 
       this.PARTIAL_CUT_1 = new Uint8Array([0x1B, 0x64, 0x30]);
+
+      this.transImage = function (imagedata) {
+
+        var line = new Uint8Array();
+        var result = [];
+        var i, j, p, d;
+
+        var width = imagedata.width;
+
+        result.push(0x1B, 0x30); // Image Begin
+        for (i = 0; i < imagedata.height; i = i + 8) {
+          result.push(0x1B, 0x4B); // Image Line Header
+          result.push(width & 255, width >> 8);
+
+          for (j = 0; j < width; j++) {
+            p = 0x00;
+            for (d = 0; d < 8; d++) {
+              p = p << 1;
+              if (this.isBlack(imagedata, j, i + d)) {
+                p = p | 0x01;
+              }
+            }
+            result.push(p);
+          }
+          result.push(0x0D, 0x0A);
+        }
+        result.push(0x1B, 0x7A, 0x01); // Image End
+        line = OB.ARRAYS.append(line, this.CENTER_JUSTIFICATION);
+        line = OB.ARRAYS.append(line, new Uint8Array(result));
+        line = OB.ARRAYS.append(line, this.LEFT_JUSTIFICATION);
+        return line;
+      };
       };
 
   OB.PRINTERTYPES.GENERICUSB.register({
