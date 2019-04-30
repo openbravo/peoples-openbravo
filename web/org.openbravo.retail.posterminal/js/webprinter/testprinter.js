33f6b58734f1d7f771f6a2791ae8e69c2ffec461
Adrián Romero <adrianromero@openbravo.com>
2019-05-01 01:25:40 +0200

    [Bluetooth N USB] More detailed test receipt

--- web/org.openbravo.retail.posterminal/js/webprinter/testprinter.js
+++ web/org.openbravo.retail.posterminal/js/webprinter/testprinter.js
@@ -17,16 +17,34 @@ enyo.kind({
   '<output>' + //
   '<ticket>' + //
   '  <image>ticket-image.png</image>' + //
+  '  <line></line>' + //  
   '  <line size="1">' + //
-  '  <text align="center" length="42">Sample Receipt</text>' + //
+  '  <text>Sample Receipt</text>' + //
   '  </line>' + //
   '  <line></line>' + //
   '  <line>' + //
-  '    <text align="left">Thank you.</text>' + //
+  '  <text>1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890</text>' + //
+  '  </line>' + //
+  '  <line></line>' + //
+  '  <line>' + //
+  '  <text>Normal size</text>' + //
+  '  </line>' + //
+  '  <line size="1">' + //
+  '  <text>Size 1</text>' + //
+  '  </line>' + //
+  '  <line size="2">' + //
+  '  <text>Size 2</text>' + //
+  '  </line>' + //
+  '  <line size="3">' + //
+  '  <text>Size 3</text>' + //
   '  </line>' + //
   '  <line></line>' + //
   '  <barcode type="CODE128" position="center">ABCDE12345</barcode>' + //
   '  <qr position="center">ABCDE12345</qr>' + //
+  '  <line></line>' + //
+  '  <line>' + //
+  '    <text align="left">Thank you.</text>' + //
+  '  </line>' + //
   '</ticket>' + //
   '</output>',
   checkReceipt: function (ev) {
