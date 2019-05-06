3c853618225e02b783ae34ea12c72cb589f7d1b9
Aaron Calero <aaron.calero@openbravo.com>
2019-05-06 12:06:54 +0200

    [Delivery Rates] Fixed licensing

--- src/org/openbravo/retail/posterminal/ServicePriceRuleVersionHQLRangeCriteria.java
+++ src/org/openbravo/retail/posterminal/ServicePriceRuleVersionHQLRangeCriteria.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2018 Openbravo S.L.U.
+ * Copyright (C) 2019 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -18,6 +18,7 @@ import org.openbravo.mobile.core.process.HQLCriteriaProcess;
 @Qualifier("ServicePriceRuleVersion_RangeFilter")
 public class ServicePriceRuleVersionHQLRangeCriteria extends HQLCriteriaProcess {
 
+  @Override
   public String getHQLFilter(String params) {
     return " ((sprv.obposMinimum is null or sprv.obposMinimum <= $1) and (sprv.obposMaximum is null or sprv.obposMaximum >= $1))";
   }
