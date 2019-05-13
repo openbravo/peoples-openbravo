01993a0ca0c5765e939f9430ec611178885b3951
Adrián Romero <adrianromero@openbravo.com>
2019-05-13 13:29:47 +0200

    [UI Configuration] Adding action *Edit Line*

---
+++ web/org.openbravo.retail.posterminal/js/actions/editline.js
@@ -0,0 +1,24 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2019 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+/*global OB */
+
+(function () {
+
+  OB.MobileApp.actionsRegistry.register(
+  new OB.Actions.ViewMethodAction({
+    window: 'retail.pointofsale',
+    name: 'editLine',
+    permission: 'OBPOS_ActionButtonDescription',
+    properties: {
+      i18nContent: 'OBPOS_LblDescription'
+    }
+  }));
+
+}());
