88fb990f6688f2e3b54756d439917931d0c3c6c7
Víctor Martínez Romanos <victor.martinez@openbravo.com>
2019-05-21 16:06:30 +0200

    [delvmodsmerge] Moved term package

---
+++ src/org/openbravo/retail/posterminal/term/DeliveryModes.java
@@ -0,0 +1,54 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2018-2019 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+package org.openbravo.retail.posterminal.term;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.retail.posterminal.term.QueryTerminalProperty;
+
+public class DeliveryModes extends QueryTerminalProperty {
+
+  @Override
+  protected boolean isAdminMode() {
+    return true;
+  }
+
+  @Override
+  protected List<String> getQuery(JSONObject jsonsent) throws JSONException {
+    final StringBuilder hqlDeliveryModes = new StringBuilder();
+    hqlDeliveryModes.append("SELECT list.searchKey AS id, ");
+    hqlDeliveryModes.append("COALESCE((SELECT trl.name ");
+    hqlDeliveryModes.append("FROM list.aDListTrlList AS trl ");
+    hqlDeliveryModes.append("WHERE trl.language = '");
+    hqlDeliveryModes.append(OBContext.getOBContext().getLanguage());
+    hqlDeliveryModes.append("'), list.name) AS name ");
+    hqlDeliveryModes.append("FROM ADList AS list ");
+    hqlDeliveryModes.append("WHERE list.reference.id = '41D44C94D0AC41DEBA9A2BEB0EAF059C' ");
+    hqlDeliveryModes.append("AND list.$readableSimpleCriteria ");
+    hqlDeliveryModes.append("AND list.$activeCriteria ");
+    hqlDeliveryModes.append("ORDER BY list.sequenceNumber");
+
+    return Arrays.asList(hqlDeliveryModes.toString());
+  }
+
+  @Override
+  public String getProperty() {
+    return "deliveryModes";
+  }
+
+  @Override
+  public boolean returnList() {
+    return false;
+  }
+
+}
---
+++ src/org/openbravo/retail/posterminal/term/DeliveryPaymentMode.java
@@ -0,0 +1,68 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2019 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+package org.openbravo.retail.posterminal.term;
+
+import javax.servlet.ServletException;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.security.OrganizationStructureProvider;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.model.common.enterprise.Organization;
+import org.openbravo.retail.posterminal.term.JSONTerminalProperty;
+import org.openbravo.service.json.JsonConstants;
+
+public class DeliveryPaymentMode extends JSONTerminalProperty {
+
+  final String DEFAULTDELIVERYPAYMENTMODE = "PT";
+
+  @Override
+  public JSONObject exec(JSONObject jsonsent) throws JSONException, ServletException {
+    JSONObject result = new JSONObject();
+    OBContext.setAdminMode(true);
+
+    try {
+      Organization currentOrg = OBContext.getOBContext().getCurrentOrganization();
+      String deliveryPaymentMode = currentOrg.getObrdmDeliverypaymentmode();
+
+      if (deliveryPaymentMode == null) {
+        OrganizationStructureProvider osp = OBContext.getOBContext()
+            .getOrganizationStructureProvider(OBContext.getOBContext().getCurrentClient().getId());
+        String parentOrgId = osp.getParentOrg(currentOrg.getId());
+        Organization parentOrg;
+        while (parentOrgId != null && deliveryPaymentMode == null) {
+          parentOrg = OBDal.getInstance().get(Organization.class, parentOrgId);
+          deliveryPaymentMode = parentOrg.getObrdmDeliverypaymentmode();
+          parentOrgId = osp.getParentOrg(parentOrgId);
+        }
+      }
+
+      result.put(JsonConstants.RESPONSE_DATA,
+          deliveryPaymentMode != null ? deliveryPaymentMode : DEFAULTDELIVERYPAYMENTMODE);
+      result.put(JsonConstants.RESPONSE_STATUS, JsonConstants.RPCREQUEST_STATUS_SUCCESS);
+
+      return result;
+
+    } finally {
+      OBContext.restorePreviousMode();
+    }
+  }
+
+  @Override
+  public String getProperty() {
+    return "deliveryPaymentMode";
+  }
+
+  @Override
+  protected boolean bypassPreferenceCheck() {
+    return true;
+  }
+
+}
