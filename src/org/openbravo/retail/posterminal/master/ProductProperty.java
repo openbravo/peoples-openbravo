4c6cfa69effcea6ea8703332493e81a372c0d0aa
Víctor Martínez Romanos <victor.martinez@openbravo.com>
2019-05-21 15:57:13 +0200

    [delvmodsmerge] Moved data package to master (pending DeliveryServiceHQLCriteria)

---
+++ src/org/openbravo/retail/posterminal/master/ProductProperty.java
@@ -0,0 +1,29 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2018-2019 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+package org.openbravo.retail.posterminal.master;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.openbravo.client.kernel.ComponentProvider.Qualifier;
+import org.openbravo.mobile.core.model.HQLProperty;
+import org.openbravo.mobile.core.model.ModelExtension;
+import org.openbravo.retail.posterminal.master.Product;
+
+@Qualifier(Product.productPropertyExtension)
+public class ProductProperty extends ModelExtension {
+
+  @Override
+  public List<HQLProperty> getHQLProperties(Object params) {
+    return Arrays.asList(new HQLProperty("product.obrdmDeliveryMode", "obrdmDeliveryMode"),
+        new HQLProperty("product.obrdmDeliveryModeLyw", "obrdmDeliveryModeLyw"),
+        new HQLProperty("product.obrdmIsdeliveryservice", "obrdmIsdeliveryservice"));
+  }
+}
