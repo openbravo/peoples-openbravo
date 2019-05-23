652dc6af1cd1d5f69cb146b1bd1c21d1cde52827
Asier Lostalé <asier.lostale@openbravo.com>
2019-05-23 17:23:01 +0200

    [cr-ws] reimplemented Register Module process

---
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/businesslogic/RegisterModuleActionHandler.java
@@ -0,0 +1,76 @@
+package org.openbravo.client.application.businesslogic;
+
+import java.util.Map;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.base.exception.OBException;
+import org.openbravo.client.application.process.BaseProcessActionHandler;
+import org.openbravo.client.application.process.ResponseActionsBuilder;
+import org.openbravo.client.application.process.ResponseActionsBuilder.MessageType;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.erpCommon.utility.OBMessageUtils;
+import org.openbravo.model.ad.module.Module;
+import org.openbravo.service.centralrepository.CentralRepository;
+import org.openbravo.service.centralrepository.CentralRepository.Service;
+
+public class RegisterModuleActionHandler extends BaseProcessActionHandler {
+
+  @Override
+  protected JSONObject doExecute(Map<String, Object> parameters, String content) {
+    JSONObject registationInfo = getRegistrationInfo(content);
+
+    // Do not keep connection open while communicating with WS
+    OBDal.getInstance().commitAndClose();
+
+    JSONObject crResponse = CentralRepository.post(Service.REGISTER_MODULE, registationInfo);
+
+    try {
+      ResponseActionsBuilder rb = getResponseBuilder();
+      String msg = OBMessageUtils
+          .getI18NMessage(crResponse.getJSONObject("response").getString("msg"));
+      if (crResponse.getBoolean("success")) {
+        rb.showMsgInProcessView(MessageType.SUCCESS, OBMessageUtils.getI18NMessage("ProcessOK"),
+            msg);
+        Module module = OBDal.getInstance()
+            .get(Module.class, registationInfo.getJSONObject("module").get("id"));
+        module.setRegisterModule(true);
+      } else {
+        rb.showMsgInProcessView(MessageType.ERROR, OBMessageUtils.getI18NMessage("Error"), msg,
+            true).retryExecution();
+      }
+
+      return rb.build();
+    } catch (JSONException e) {
+      throw new OBException(e);
+    }
+  }
+
+  private JSONObject getRegistrationInfo(String content) {
+    JSONObject r = new JSONObject();
+    try {
+      JSONObject req = new JSONObject(content);
+      JSONObject params = req.getJSONObject("_params");
+      r.put("user", params.getString("user"));
+      r.put("password", params.getString("password"));
+
+      Module module = OBDal.getInstance().get(Module.class, req.getString("AD_Module_ID"));
+      JSONObject jsonModule = new JSONObject();
+      jsonModule.put("id", module.getId());
+      jsonModule.put("name", module.getName());
+      jsonModule.put("javapackage", module.getJavaPackage());
+      jsonModule.put("author", module.getAuthor());
+      jsonModule.put("type", module.getType());
+      jsonModule.put("help", module.getHelpComment());
+      if (!module.getModuleDBPrefixList().isEmpty()) {
+        jsonModule.put("dbPrefix", module.getModuleDBPrefixList().get(0));
+      }
+      jsonModule.put("description", module.getDescription());
+      r.put("module", jsonModule);
+    } catch (JSONException e) {
+      throw new OBException(e);
+    }
+    return r;
+  }
+
+}
---
+++ src/org/openbravo/service/centralrepository/CentralRepository.java
@@ -0,0 +1,69 @@
+package org.openbravo.service.centralrepository;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.base.exception.OBException;
+
+public class CentralRepository {
+  private static final String BUTLER_API_URL = "https://butler.openbravo.com/openbravo/api/";
+
+  private static final Logger log = LogManager.getLogger();
+
+  public enum Service {
+    REGISTER_MODULE("register");
+
+    private String endpoint;
+
+    private Service(String endpoint) {
+      this.endpoint = endpoint;
+    }
+  }
+
+  public static JSONObject post(Service service, JSONObject content) {
+    StringEntity requestEntity = new StringEntity(content.toString(), ContentType.APPLICATION_JSON);
+
+    HttpPost postMethod = new HttpPost(BUTLER_API_URL + service.endpoint);
+    postMethod.setEntity(requestEntity);
+    try (CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpResponse rawResponse = httpclient.execute(postMethod)) {
+
+      String result = new BufferedReader(
+          new InputStreamReader(rawResponse.getEntity().getContent())).lines()
+              .collect(Collectors.joining("\n"));
+      JSONObject r = new JSONObject(result);
+      JSONObject msg = new JSONObject();
+      msg.put("success", rawResponse.getStatusLine().getStatusCode() == 200);
+      msg.put("responseCode", rawResponse.getStatusLine().getStatusCode());
+      msg.put("response", r);
+
+      System.out.println(msg.toString(1));
+      return msg;
+    } catch (Exception e) {
+      log.error("Error communicating with Central Repository service {}", service);
+      log.debug("Failed content sent to CR {}", content);
+      JSONObject r = new JSONObject();
+      try {
+        r.put("msg", e.getMessage());
+        JSONObject msg = new JSONObject();
+        msg.put("sucess", false);
+        msg.put("responseCode", 500);
+        msg.put("response", r);
+        return msg;
+      } catch (JSONException e1) {
+        throw new OBException(e1);
+      }
+    }
+  }
+}
