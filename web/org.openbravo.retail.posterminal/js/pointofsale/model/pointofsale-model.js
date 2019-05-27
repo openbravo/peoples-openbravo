45625707cb5c009040aceb1fc7dd92df589bf675
Armaignac <collazoandy4@gmail.com>
2019-05-27 16:41:18 -0400

    [CrossStoreAssortment] Take into account if line is Verified Return before set organization and warehouse information

--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
@@ -682,7 +682,7 @@ OB.OBPOSPointOfSale.Model.PointOfSale = OB.Model.TerminalWindowModel.extend({
       if (!receipt.get('isEditable') || receipt.get('cloningReceipt')) {
         return;
       }
-      if (line.get('qty') < 0 && OB.UTIL.isNullOrUndefined(line.get('canceledLine'))) {
+      if (line.get('qty') < 0 && OB.UTIL.isNullOrUndefined(line.get('canceledLine')) && OB.UTIL.isNullOrUndefined(line.get('isVerifiedReturn'))) {
         line.set('organization', terminalOrganization);
         line.set('warehouse', terminalWarehouse);
         line.get('product').set('crossStore', false);
