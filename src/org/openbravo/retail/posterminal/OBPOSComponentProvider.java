f2883b21c2ebec6df603c2b6d0ff1e3e822c6526
Asier Martirena <asier.martirena@openbravo.com>
2019-06-06 11:14:17 +0200

    Related to issue 40847: Removed the deleted file from the component provider
    --HG--
    extra : rebase_source : 93685283b0f03f1bcf1389bae39004ca8e3e6e8b

--- src/org/openbravo/retail/posterminal/OBPOSComponentProvider.java
+++ src/org/openbravo/retail/posterminal/OBPOSComponentProvider.java
@@ -222,8 +222,8 @@ public class OBPOSComponentProvider extends BaseComponentProvider {
         "utils/deliveryModeUtils", "hookPreOrderSave", "hookPreDeleteLine",
         "hookPostUndo_DeleteLine", "receiptEdit", "receiptMultiLineEdit", "hookNewReceipt",
         "hookRenderOrderLine", "hookPrePayment", "hookPreSetPaymentsToReceipt",
-        "hookPreAddProductWithoutStock", "hookPostAddProductToOrder", "components/menuissue",
-        "model/dmorderfilter", "model/ordertoissue", "utils/utils",
+        "hookPostAddProductToOrder", "components/menuissue", "model/dmorderfilter",
+        "model/ordertoissue", "utils/utils",
 
         "model/ordertoselectorissue", "components/orderselector",
         "components/modalwarehouseselector", "components/orderlineinfopopup",
