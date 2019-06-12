cca2c79da033bfaf326cfa725882f2e2d3a74a4c
Sandra Huguet <sandra.huguet@openbravo.com>
2019-06-12 09:45:27 +0200

    related to issue 41062 code review changes use new Date() instead of Calendar

--- modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/dao/AdvPaymentMngtDao.java
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/dao/AdvPaymentMngtDao.java
@@ -21,7 +21,6 @@ package org.openbravo.advpaymentmngt.dao;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -690,7 +689,7 @@ public class AdvPaymentMngtDao {
       BigDecimal writeoffAmount, BigDecimal debtAmount) {
     final FIN_PaymentScheduleDetail newPaymentScheduleDetail = (FIN_PaymentScheduleDetail) DalUtil
         .copy(paymentScheduleDetail);
-    newPaymentScheduleDetail.setCreationDate(Calendar.getInstance().getTime());
+    newPaymentScheduleDetail.setCreationDate(new Date());
     newPaymentScheduleDetail.setAmount(writeoffAmount);
     newPaymentScheduleDetail.setDoubtfulDebtAmount(debtAmount);
     OBDal.getInstance().save(newPaymentScheduleDetail);
