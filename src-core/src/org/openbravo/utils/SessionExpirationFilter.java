615fc0199fdbc2305f62134a038ded478cb9caaa
Asier Lostalé <asier.lostale@openbravo.com>
2019-07-02 08:51:03 +0200

    related to issue 41217: code cleanup   - removes some clutter   - adds JavaDoc

--- src-core/src/org/openbravo/utils/SessionExpirationFilter.java
+++ src-core/src/org/openbravo/utils/SessionExpirationFilter.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2008-2010 Openbravo S.L.U.
+ * Copyright (C) 2008-2019 Openbravo S.L.U.
  * Licensed under the Apache Software License version 2.0
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed
@@ -23,6 +23,12 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+/**
+ * Servlet filter that handles session expiration. There are requests such as alert ping that should
+ * not be taken into account as active request to prevent session expiration, that's why we cannot
+ * rely on Tomcat's default mechanism. This filter takes them into account to reset expiration time
+ * accordingly.
+ */
 public class SessionExpirationFilter implements Filter {
 
   @Override
@@ -35,32 +41,17 @@ public class SessionExpirationFilter implements Filter {
     HttpServletRequest hReq = (HttpServletRequest) req;
 
     HttpSession session = hReq.getSession(false);
-    if (null != session) {
+    if (session != null) {
       Date expirationDate = (Date) session.getAttribute("expirationDate");
 
-      if (expirationDate == null) {
-        expirationDate = new Date(System.currentTimeMillis() + 1000000); // only
-        // for
-        // make
-        // false
-        // "expirationDate.before(new Date())"
-        // in
-        // the
-        // first
-        // execution
-      }
-
-      if (expirationDate.before(new Date())) {
+      if (expirationDate != null && expirationDate.before(new Date())) {
         session.invalidate();
-        session = null;
       } else {
         // ignore requests marked as both ajaxCall and ignoreForSessionTimeout
         String isAjaxCall = hReq.getParameter("IsAjaxCall");
         String ignoreForSessionTimeout = hReq.getParameter("ignoreForSessionTimeout");
-        boolean ignoreForTimeout = "1".equals(isAjaxCall) && ("1".equals(ignoreForSessionTimeout));
-        if (ignoreForTimeout) {
-          // Do nothing; don't update the session timestamp
-        } else {
+        boolean ignoreForTimeout = "1".equals(isAjaxCall) && "1".equals(ignoreForSessionTimeout);
+        if (!ignoreForTimeout) {
           session.setAttribute("expirationDate",
               new Date(System.currentTimeMillis() + session.getMaxInactiveInterval() * 1000));
         }
