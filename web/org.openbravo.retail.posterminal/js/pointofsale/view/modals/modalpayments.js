79e210a839f5f75422c9e68efaf7780168393705
Miguel de Juana <miguel.dejuana@openbravo.com>
2019-07-15 11:36:22 +0200

    Fixed issue 0041301: A gift card can be reimbursed many times not taking into account it's available amount (gift card / certificate / credit notes)
    - Mark selectItem before hiding the popup to avoid reopening select Payment popup

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/modals/modalpayments.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/modals/modalpayments.js
@@ -103,8 +103,8 @@ enyo.kind({
                   tap: function() {
                     if (!item.disabled) {
                       var dialog = this.owner.owner.owner;
-                      dialog.doHideThisPopup();
                       dialog.selectItem = true;
+                      dialog.doHideThisPopup();
                       dialog.bubble('onPaymentChanged', {
                         payment: this.payment,
                         status: this.payment.payment.searchKey,
