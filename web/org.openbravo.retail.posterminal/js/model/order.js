85f7a160daf7e9684148b647328e2a012dd6f484
Ranjith S R <ranjith@qualiantech.com>
2019-08-03 18:38:13 +0530

    Fixed issue 41519 : Good shipment not generated after create a sales from a quotation
    * Property 'obposQtytodeliver' was removed from every line after create a sales order

--- web/org.openbravo.retail.posterminal/js/model/order.js
+++ web/org.openbravo.retail.posterminal/js/model/order.js
@@ -6629,6 +6629,7 @@
             line.unset('grossListPrice');
             line.unset('grossUnitPrice');
             line.unset('lineGrossAmount');
+            line.unset('obposQtytodeliver');
             idMap[line.get('id')] = OB.UTIL.get_UUID();
             line.set('id', idMap[line.get('id')]);
             if (line.get('hasRelatedServices')) {
