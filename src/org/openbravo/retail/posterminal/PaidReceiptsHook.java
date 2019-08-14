08a82ef16b50a8510a5cd517004dd3112f253563
Álvaro Ferraz <alvaro.ferraz@openbravo.com>
2019-08-14 08:37:45 +0200

    Fixes issue 41646: Add new hook to PaidReceipts.java
    --HG--
    extra : rebase_source : 226a8d08f001cf1e4a115fb783fe723ccfd25996

---
+++ src/org/openbravo/retail/posterminal/PaidReceiptsHook.java
@@ -0,0 +1,21 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2019 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+package org.openbravo.retail.posterminal;
+
+import org.codehaus.jettison.json.JSONObject;
+
+public interface PaidReceiptsHook {
+
+  /**
+   * Executes the hook logic on the Paid Receipts process
+   */
+  public abstract void exec(final String orderId, final JSONObject paidReceipt) throws Exception;
+
+}
