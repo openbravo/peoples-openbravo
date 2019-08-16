a4f8fe945ead092ed08c41547dede994d3492189
Antonio Moreno Perez <antonio.moreno@openbravo.com>
2019-08-16 14:31:50 +0200

    [NewDiscountEngine] Cache will also be recalculated when ticket is changed

--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
@@ -1067,7 +1067,7 @@ OB.OBPOSPointOfSale.Model.PointOfSale = OB.Model.TerminalWindowModel.extend({
     });
 
     receipt.on(
-      'change:bp',
+      'change:bp change:documentNo',
       function() {
         if (!receipt.get('isEditable') || receipt.get('lines').length === 0) {
           return;
