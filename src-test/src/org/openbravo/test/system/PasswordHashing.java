2b4a64af3588e38f9f2f23fda255c27321b62385
Asier Lostalé <asier.lostale@openbravo.com>
2019-09-04 08:21:43 +0200

    [pwd] hashing algorith definition
    --HG--
    extra : rebase_source : f9ce30411e970afbba328cf6d26ba26ec15f54c0

---
+++ src-test/src/org/openbravo/test/system/PasswordHashing.java
@@ -0,0 +1,40 @@
+package org.openbravo.test.system;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.openbravo.base.secureApp.PasswordHash;
+
+public class PasswordHashing {
+
+  @Test
+  public void sha1IsAKnownAlgorithm() {
+    assertThat(PasswordHash.getAlgorithm("whatever").getClass().getSimpleName(), is("SHA1"));
+  }
+
+  @Test
+  public void sha512SaltIsAKnownAlgorithm() {
+    assertThat(PasswordHash.getAlgorithm("1$salt$hash").getClass().getSimpleName(),
+        is("SHA512Salt"));
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void unknownAlgorithmsThrowException() {
+    assertThat(PasswordHash.getAlgorithm("2$salt$hash").getClass().getSimpleName(),
+        is("SHA512Salt"));
+  }
+
+  @Test
+  public void oldHashesWork() {
+    String sha1HashedOpenbravo = "PwOd6SgWF74HY4u51bfrUxjtB9g=";
+    assertThat(PasswordHash.matches("openbravo", sha1HashedOpenbravo), is(true));
+  }
+
+  @Test
+  public void newHashesWork() {
+    String sha512SaltedOpenbravo = "1$anySalt$iyWvhlUpOrXFPPeRVzWXXR/B4hQ5qs8ZjCLUPoncJIKHRy5HZeXm9/r20qXg8tRgKcfC8bp/u5fPPQ9qA/hheQ==";
+    assertThat(PasswordHash.matches("openbravo", sha512SaltedOpenbravo), is(true));
+  }
+
+}
---
+++ src/org/openbravo/base/secureApp/PasswordHash.java
@@ -0,0 +1,121 @@
+package org.openbravo.base.secureApp;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.criterion.Restrictions;
+import org.jboss.weld.exceptions.IllegalStateException;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.model.ad.access.User;
+
+public abstract class PasswordHash {
+  public static final Logger log = LogManager.getLogger();
+
+  private static final Map<Integer, PasswordHash> INSTANCES = Map.of(0, new SHA1(), 1,
+      new SHA512Salt());
+
+  private PasswordHash() {
+  }
+
+  public static PasswordHash getAlgorithm(String hash) {
+    PasswordHash algorithm = INSTANCES.get(getVersion(hash));
+
+    if (algorithm == null) {
+      throw new IllegalStateException(
+          "Hashing alorightm version " + getVersion(hash) + " is not implemented");
+    }
+
+    return algorithm;
+  }
+
+  public static User getUserId(String userName, String password) {
+    OBContext.setAdminMode(false);
+    try {
+      // TODO: ensure we can use DAL at this point
+      User user = (User) OBDal.getInstance()
+          .createCriteria(User.class)
+          .add(Restrictions.eq(User.PROPERTY_USERNAME, userName))
+          .uniqueResult();
+
+      if (user == null) {
+        return null;
+      }
+
+      PasswordHash algorithm = getAlgorithm(user.getPassword());
+      if (algorithm.check(user.getPassword(), password)) {
+        return user;
+      } else {
+        return null;
+      }
+    } finally {
+      OBContext.restorePreviousMode();
+    }
+  }
+
+  public static boolean matches(String plainTextPassword, String hashedPassword) {
+    PasswordHash algorithm = getAlgorithm(hashedPassword);
+    log.trace("Checking password with algorithm {}", () -> algorithm.getClass().getSimpleName());
+    return algorithm.check(plainTextPassword, hashedPassword);
+  }
+
+  protected abstract boolean check(String plainTextPassowed, String hashedPassword);
+
+  protected String hash(String plainText, String salt) {
+    try {
+      MessageDigest md = MessageDigest.getInstance(getHashingBaseAlgorithm());
+      if (salt != null) {
+        md.update(salt.getBytes(StandardCharsets.UTF_8));
+      }
+      byte[] bytes = md.digest(plainText.getBytes(StandardCharsets.UTF_8));
+      return new String(org.apache.commons.codec.binary.Base64.encodeBase64(bytes),
+          StandardCharsets.UTF_8);
+    } catch (NoSuchAlgorithmException e) {
+      log.error("Error getting hashing algorithm", e);
+      return "";
+    }
+  }
+
+  protected abstract String getHashingBaseAlgorithm();
+
+  private static int getVersion(String hash) {
+    int idx = hash.indexOf('$');
+    if (idx == -1) {
+      return 0;
+    }
+    return Integer.parseInt(hash.substring(0, idx));
+  }
+
+  private static class SHA1 extends PasswordHash {
+    @Override
+    protected String getHashingBaseAlgorithm() {
+      return "SHA-1";
+    }
+
+    @Override
+    protected boolean check(String plainTextPassword, String hashedPassword) {
+      return hash(plainTextPassword, null).equals(hashedPassword);
+    }
+
+  }
+
+  private static class SHA512Salt extends PasswordHash {
+    @Override
+    protected String getHashingBaseAlgorithm() {
+      return "SHA-512";
+    }
+
+    @Override
+    protected boolean check(String plainTextPassword, String hashedPassword) {
+      String[] hashParts = hashedPassword.split("\\$");
+      String salt = hashParts[1];
+      String orginalHash = hashParts[2];
+
+      return hash(plainTextPassword, salt).equals(orginalHash);
+    }
+  }
+}
