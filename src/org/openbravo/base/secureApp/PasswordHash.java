6af38d049249a7ea1c9229d76181edd356050d26
Asier Lostalé <asier.lostale@openbravo.com>
2019-09-10 15:41:53 +0200

    [pwd] refactor PasswordHash to differentiate it from algorithms
    --HG--
    extra : rebase_source : f50deee1415b24815f5a9dd1e96f12a1fe502d60

--- src/org/openbravo/base/secureApp/PasswordHash.java
+++ src/org/openbravo/base/secureApp/PasswordHash.java
@@ -21,14 +21,14 @@ public abstract class PasswordHash {
   public static final Logger log = LogManager.getLogger();
   private static final int DEFAULT_CURRENT_ALGORITHM_VERSION = 1;
 
-  private static final Map<Integer, PasswordHash> INSTANCES = Map.of(0, new SHA1(), 1,
+  private static final Map<Integer, HashingAlgorithm> INSTANCES = Map.of(0, new SHA1(), 1,
       new SHA512Salt());
 
   private PasswordHash() {
   }
 
-  public static PasswordHash getAlgorithm(String hash) {
-    PasswordHash algorithm = INSTANCES.get(getVersion(hash));
+  public static HashingAlgorithm getAlgorithm(String hash) {
+    HashingAlgorithm algorithm = INSTANCES.get(getVersion(hash));
 
     if (algorithm == null) {
       throw new IllegalStateException(
@@ -55,7 +55,7 @@ public abstract class PasswordHash {
         return Optional.empty();
       }
 
-      PasswordHash algorithm = getAlgorithm(user.getPassword());
+      HashingAlgorithm algorithm = getAlgorithm(user.getPassword());
 
       if (!algorithm.check(password, user.getPassword())) {
         // invalid password
@@ -76,39 +76,15 @@ public abstract class PasswordHash {
     }
   }
 
-  public static PasswordHash getDefaultAlgorithm() {
+  public static HashingAlgorithm getDefaultAlgorithm() {
     return INSTANCES.get(DEFAULT_CURRENT_ALGORITHM_VERSION);
   }
 
-  public abstract String generateHash(String password);
-
-  protected abstract int getAlgorithmVersion();
-
   public static boolean matches(String plainTextPassword, String hashedPassword) {
-    PasswordHash algorithm = getAlgorithm(hashedPassword);
+    HashingAlgorithm algorithm = getAlgorithm(hashedPassword);
     log.trace("Checking password with algorithm {}", () -> algorithm.getClass().getSimpleName());
     return algorithm.check(plainTextPassword, hashedPassword);
   }
-
-  protected abstract boolean check(String plainTextPassowed, String hashedPassword);
-
-  protected String hash(String plainText, String salt) {
-    try {
-      MessageDigest md = MessageDigest.getInstance(getHashingBaseAlgorithm());
-      if (salt != null) {
-        md.update(salt.getBytes(StandardCharsets.UTF_8));
-      }
-
-      byte[] bytes = md.digest(plainText.getBytes(StandardCharsets.UTF_8));
-
-      return Base64.getEncoder().encodeToString(bytes);
-    } catch (NoSuchAlgorithmException e) {
-      log.error("Error getting hashing algorithm", e);
-      return "";
-    }
-  }
-
-  protected abstract String getHashingBaseAlgorithm();
 
   private static int getVersion(String hash) {
     int idx = hash.indexOf('$');
@@ -118,7 +94,33 @@ public abstract class PasswordHash {
     return Integer.parseInt(hash.substring(0, idx));
   }
 
-  private static class SHA1 extends PasswordHash {
+  public abstract static class HashingAlgorithm {
+    public abstract String generateHash(String password);
+
+    protected abstract int getAlgorithmVersion();
+
+    protected abstract boolean check(String plainTextPassowed, String hashedPassword);
+
+    protected abstract String getHashingBaseAlgorithm();
+
+    protected String hash(String plainText, String salt) {
+      try {
+        MessageDigest md = MessageDigest.getInstance(getHashingBaseAlgorithm());
+        if (salt != null) {
+          md.update(salt.getBytes(StandardCharsets.UTF_8));
+        }
+
+        byte[] bytes = md.digest(plainText.getBytes(StandardCharsets.UTF_8));
+
+        return Base64.getEncoder().encodeToString(bytes);
+      } catch (NoSuchAlgorithmException e) {
+        log.error("Error getting hashing algorithm", e);
+        return "";
+      }
+    }
+  }
+
+  private static class SHA1 extends HashingAlgorithm {
     @Override
     protected String getHashingBaseAlgorithm() {
       return "SHA-1";
@@ -141,7 +143,7 @@ public abstract class PasswordHash {
 
   }
 
-  private static class SHA512Salt extends PasswordHash {
+  private static class SHA512Salt extends HashingAlgorithm {
     private static final Random RANDOM = new SecureRandom();
 
     @Override
