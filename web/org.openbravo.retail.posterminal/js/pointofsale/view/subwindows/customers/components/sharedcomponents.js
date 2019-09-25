45da6056abf84d82c3f6fb68ed537283cc0d9bd4
David Baz Fayos <david.baz@openbravo.com>
2019-09-25 16:13:18 +0200

    [skin] Use OB.UI.FormElement messages/errors in Customer and Address popups

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/subwindows/customers/components/sharedcomponents.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/subwindows/customers/components/sharedcomponents.js
@@ -434,10 +434,13 @@ enyo.kind({
         if (item.coreElement.mandatory) {
           var value = customer.get(item.coreElement.modelProperty);
           if (!value) {
+            item.setMessage(OB.I18N.getLabel('OBMOBC_LblMandatoryField'), true);
             if (errors) {
               errors += ', ';
             }
             errors += OB.I18N.getLabel(item.coreElement.i18nLabel);
+          } else {
+            item.setMessage();
           }
         }
       });
