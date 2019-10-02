f754e552c3f42ece5b4607a9a4ef13f88d35a0c4
Asier Lostalé <asier.lostale@openbravo.com>
2019-10-02 13:08:14 +0200

    [pwd] javadoc
    --HG--
    extra : amend_source : d6074106673f6dbcd2195ce0d9694e3df51b08f5

--- src/org/openbravo/authentication/hashing/HashingAlgorithm.java
+++ src/org/openbravo/authentication/hashing/HashingAlgorithm.java
@@ -26,7 +26,18 @@ import java.util.Base64;
 /** Algorithm used to hash password to store in Database */
 public abstract class HashingAlgorithm {
 
-  /** Generates a hash using current algorithm */
+  /**
+   * Generates a hash using current algorithm, hashes should look like: {@code version$salt$hashed}
+   * where:
+   * <p>
+   * <ul>
+   * <li>{@code version}: is a numeric value that determines the version of the
+   * {@code HashingAlgorithm} that performed the hash. It must match the value returned by
+   * {@link #getAlgorithmVersion()}.
+   * <li>{@code salt}: salt used in the hash.
+   * <li>{@code hashed}: actual hash, which is computed by {@link #hash(String, String)}.
+   * </ul>
+   */
   protected abstract String generateHash(String password);
 
   /**
@@ -43,6 +54,10 @@ public abstract class HashingAlgorithm {
   /** Returns the low level algorithm used to perform the hashing. */
   protected abstract MessageDigest getHashingBaseAlgorithm();
 
+  /**
+   * Performs the low level hashing of {@code plainText} salted with {@code salt} value. Salt can be
+   * null in which case will be ignored.
+   */
   protected final String hash(String plainText, String salt) {
     MessageDigest md = getHashingBaseAlgorithm();
     if (salt != null) {
