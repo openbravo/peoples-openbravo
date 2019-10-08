c23ad24dfc26d29509221fb7f9f24210017d083c
Ranjith S R <ranjith@qualiantech.com>
2019-10-08 12:47:18 +0530

    Related to issue 41744 : Added locationModel to BusinessPartner when updating Location

--- web/org.openbravo.retail.posterminal/js/components/bplocation.js
+++ web/org.openbravo.retail.posterminal/js/components/bplocation.js
@@ -1034,6 +1034,7 @@ enyo.kind({
               dataBps.set('postalCode', model.get('postalCode'));
               dataBps.set('cityName', model.get('cityName'));
               dataBps.set('countryName', model.get('countryName'));
+              dataBps.set('locationBillModel', model);
             }
             if (model.get('isShipTo')) {
               dataBps.set('shipLocId', model.get('id'));
@@ -1041,6 +1042,7 @@ enyo.kind({
               dataBps.set('shipPostalCode', model.get('postalCode'));
               dataBps.set('shipCityName', model.get('cityName'));
               dataBps.set('shipCountryName', model.get('countryName'));
+              dataBps.set('locationModel', model);
             }
           } else {
             dataBps.set('locId', model.get('id'));
@@ -1048,6 +1050,8 @@ enyo.kind({
             dataBps.set('postalCode', model.get('postalCode'));
             dataBps.set('cityName', model.get('cityName'));
             dataBps.set('countryName', model.get('countryName'));
+            dataBps.set('locationModel', model);
+            dataBps.set('locationBillModel', model);
           }
 
           if (me.target.startsWith('filterSelectorButton_')) {
