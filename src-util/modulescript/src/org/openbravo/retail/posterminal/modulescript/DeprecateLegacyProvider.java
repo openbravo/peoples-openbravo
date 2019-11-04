b5064fdbc1640f918cb7743fce4c712d03198dbb
Adrián Romero <adrianromero@openbravo.com>
2019-11-04 17:40:22 +0100

    Fixes issue 0042065: Deprecate legacy Payment Provider and Refund Provider * Improves logic in modulescript
    --HG--
    extra : rebase_source : 53abaeb689d75c6f71860240791e91dd02aecb69

--- src-util/modulescript/src/org/openbravo/retail/posterminal/modulescript/DeprecateLegacyProvider.java
+++ src-util/modulescript/src/org/openbravo/retail/posterminal/modulescript/DeprecateLegacyProvider.java
@@ -8,13 +8,12 @@
  */
 package org.openbravo.retail.posterminal.modulescript;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import org.openbravo.database.ConnectionProvider;
 import org.openbravo.modulescript.ModuleScript;
 import org.openbravo.modulescript.ModuleScriptExecutionLimits;
-import org.openbravo.modulescript.OpenbravoVersion;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Timestamp;
 
 public class DeprecateLegacyProvider extends ModuleScript {
   private static final String RETAIL_MODULE_ID = "FF808181326CC34901326D53DBCF0018";
@@ -24,52 +23,28 @@ public class DeprecateLegacyProvider extends ModuleScript {
     // 0042065: Deprecate legacy Payment Provider and Refund Provider
     try {
       ConnectionProvider cp = getConnectionProvider();
-      Timestamp now = new Timestamp(System.currentTimeMillis());
-      
-      PreparedStatement select = cp.getPreparedStatement("select 1 from OBPOS_PAYMENTGROUP where obpos_paymentgroup_id = '0'");
-      ResultSet s = select.executeQuery();
-      boolean exists = s.next();
-      s.close();
-      select.close();
-      
-      if (!exists) {
-        PreparedStatement ps0 = cp
-            .getPreparedStatement(      
-        "insert into OBPOS_PAYMENTGROUP (" + 
-        "  obpos_paymentgroup_id," + 
-        "  ad_client_id," + 
-        "  ad_org_id," + 
-        "  isactive," + 
-        "  created," + 
-        "  createdby," + 
-        "  updated," + 
-        "  updatedby," + 
-        "  provider," + 
-        "  name," + 
-        "  description" + 
-        ") values (?,?,?,?,?,?,?,?,?,?,?)");
-        ps0.setString(1, "0");
-        ps0.setString(2, "0");
-        ps0.setString(3, "0");
-        ps0.setString(4, "Y");
-        ps0.setTimestamp(5, now);
-        ps0.setString(6, "100");
-        ps0.setTimestamp(7, now);
-        ps0.setString(8, "100");
-        ps0.setString(9, "OBPOS_LEGACYPROVIDER");
-        ps0.setString(10, "Legacy Payment Provider");
-        ps0.setString(11, "System legacy provider for backward compatibility with then Payment Provider and Refund Provider definitions.");
-      
-        ps0.executeUpdate();
-        ps0.close();
+
+      try (
+          PreparedStatement select = cp.getPreparedStatement(
+              "select 1 from OBPOS_PAYMENTGROUP where obpos_paymentgroup_id = '0'");
+          ResultSet s = select.executeQuery()) {
+        boolean exists = s.next();
+
+        if (!exists) {
+          try (PreparedStatement ps0 = cp.getPreparedStatement("insert into OBPOS_PAYMENTGROUP (" //
+              + " obpos_paymentgroup_id,  ad_client_id,  ad_org_id,  isactive,  created,  createdby,  updated,  updatedby,  provider,  name,  description" //
+              + ") values (" //
+              + " '0', '0', '0', 'Y', now(), '100', now(), '100', 'OBPOS_LEGACYPROVIDER', 'Legacy Payment Provider'," //
+              + " 'System legacy provider for backward compatibility with then Payment Provider and Refund Provider definitions.')")) {
+            ps0.executeUpdate();
+          }
+          try (PreparedStatement ps1 = cp.getPreparedStatement("UPDATE OBPOS_APP_PAYMENT_TYPE" //
+              + " SET OBPOS_PAYMENTGROUP_ID = '0'" //
+              + " WHERE OBPOS_PAYMENTGROUP_ID IS NULL AND (PAYMENTPROVIDER IS NOT NULL OR REFUNDPROVIDER IS NOT NULL)")) {
+            ps1.executeUpdate();
+          }
+        }
       }
-      
-      PreparedStatement ps1 = cp
-          .getPreparedStatement( //
-              "UPDATE OBPOS_APP_PAYMENT_TYPE SET OBPOS_PAYMENTGROUP_ID = '0' " + //
-              "WHERE OBPOS_PAYMENTGROUP_ID IS NULL AND (PAYMENTPROVIDER IS NOT NULL OR REFUNDPROVIDER IS NOT NULL)");
-      ps1.executeUpdate();
-      ps1.close();
     } catch (Exception e) {
       handleError(e);
     }
@@ -77,15 +52,9 @@ public class DeprecateLegacyProvider extends ModuleScript {
 
   @Override
   protected ModuleScriptExecutionLimits getModuleScriptExecutionLimits() {
-    // Always, as it contains a record that must exist
     return new ModuleScriptExecutionLimits(RETAIL_MODULE_ID, null, null);
   }
-  
-  @Override
-  protected boolean executeOnInstall() {
-    return false;
-  }
-  
+
   public static void main(String[] args) {
     // This method is provided for testing purposes.
     DeprecateLegacyProvider t = new DeprecateLegacyProvider();
