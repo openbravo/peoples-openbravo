86327566170ed18089df1539beecd44920b7700f
Carlos Aristu <carlos.aristu@openbravo.com>
2019-11-12 12:54:27 +0100

    [IndexedDB] Use renamed constant + missing copyright text

--- src/org/openbravo/retail/posterminal/term/ReturnReasonProperties.java
+++ src/org/openbravo/retail/posterminal/term/ReturnReasonProperties.java
@@ -1,3 +1,11 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2019 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
 package org.openbravo.retail.posterminal.term;
 
 import java.util.ArrayList;
@@ -7,7 +15,7 @@ import org.openbravo.client.kernel.ComponentProvider.Qualifier;
 import org.openbravo.mobile.core.model.HQLProperty;
 import org.openbravo.mobile.core.model.ModelExtension;
 
-@Qualifier(ReturnReason.returnReasonPropertyExtension)
+@Qualifier(ReturnReason.RETURNREASONPROPERTYEXTENSION)
 public class ReturnReasonProperties extends ModelExtension {
 
   @Override
