3067a6653c1e504a5845d7b9a3d66e0effda208c
Carlos Aristu <carlos.aristu@openbravo.com>
2019-11-13 19:14:34 +0100

    fixes bug 42270: window can be not initialized if default saved view is missing
      Having a window with several saved view personalizations, if the default saved view is not provided by the WindowSettingsActionHandler then the header's grid was not properly initialized, leaving it in an inconsistent state which did not allow to save records.
    
      Note that it already exists a mechanism that tries to avoid this scenario by deleting the configuration used to mark a personalization as default in case it does not exists (WindowPersonalizationEventHandler). But this inconsistent state can be also reached in case the window personalization is manually deleted through the database (i.e., by deleting the corresponding obuiapp_uipersonalization record).
    
      To fix this problem now we are forcing the grid initialization in case the default view is not found.
    
    --HG--
    extra : rebase_source : 5a8940f60ac5e791cc117c91f2f52718653079e3

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/main/ob-standard-window.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/main/ob-standard-window.js
@@ -687,7 +687,8 @@ isc.OBStandardWindow.addProperties({
       persDefaultValue,
       views,
       currentView = this.activeView || this.view,
-      length;
+      length,
+      formPersonalizationApplied = false;
 
     // only personalize if there is a professional license
     if (!OB.Utilities.checkProfessionalLicense(null, true)) {
@@ -781,6 +782,7 @@ isc.OBStandardWindow.addProperties({
               this.views[i].viewForm.getDataSource()
             ) {
               this.lastViewApplied = true;
+              formPersonalizationApplied = true;
               OB.Personalization.personalizeForm(
                 personalization.forms[this.views[i].tabId],
                 this.views[i].viewForm
@@ -789,13 +791,16 @@ isc.OBStandardWindow.addProperties({
           }
         }
       }
-    } else {
-      if (this.view.dataLoadDelayedForDefaultSavedView) {
-        // it might happen that the load of the initial grid data was delayed because it had a
-        // default saved view, but then the default saved view is not returned by the WindowSettingsActionHandler.
-        // in that case, detect it and load the grid now
-        this.view.viewGrid.fetchData(this.view.viewGrid.getCriteria());
-      }
+    }
+    if (
+      this.view.dataLoadDelayedForDefaultSavedView &&
+      !defaultView &&
+      !formPersonalizationApplied
+    ) {
+      // it might happen that the load of the initial grid data was delayed because it had a
+      // default saved view, but then the default saved view is not returned by the WindowSettingsActionHandler.
+      // in that case, detect it and load the grid now
+      this.view.viewGrid.fetchData(this.view.viewGrid.getCriteria());
     }
 
     // restore focus as the focusitem may have been hidden now
