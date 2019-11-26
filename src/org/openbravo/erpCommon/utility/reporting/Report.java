3f8b0431e8ef6c063b50ebca0dde369519389dfd
Carlos Aristu <carlos.aristu@openbravo.com>
2019-11-26 18:58:41 +0100

    fixes BUG-42399: toolbar reports should not use read-only pool
      The reports generated with the print toolbar button which are based in
    the PrintController engine, should not make use of the read-only pool.
    This is because more than reports they are rendering information of the
    transactional data.
    
       This should have been fixed as part of [1], but that fix is missing the
    part of the Report information retrieval.
    
    [1] https://issues.openbravo.com/view.php?id=37933

--- src/org/openbravo/erpCommon/utility/reporting/Report.java
+++ src/org/openbravo/erpCommon/utility/reporting/Report.java
@@ -1,17 +1,20 @@
 /*
- * ************************************************************************ The
- * contents of this file are subject to the Openbravo Public License Version 1.1
- * (the "License"), being the Mozilla Public License Version 1.1 with a
- * permitted attribution clause; you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- * http://www.openbravo.com/legal/license.html Software distributed under the
- * License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND,
- * either express or implied. See the License for the specific language
- * governing rights and limitations under the License. The Original Code is
- * Openbravo ERP. The Initial Developer of the Original Code is Openbravo SLU All
- * portions are Copyright (C) 2001-2018 Openbravo SLU All Rights Reserved.
- * Contributor(s): ______________________________________.
- * ***********************************************************************
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html 
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License. 
+ * The Original Code is Openbravo ERP. 
+ * The Initial Developer of the Original Code is Openbravo SLU 
+ * All portions are Copyright (C) 2001-2019 Openbravo SLU 
+ * All Rights Reserved. 
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
  */
 package org.openbravo.erpCommon.utility.reporting;
 
@@ -86,8 +89,8 @@ public class Report implements Serializable {
   public Report(DocumentType documentType, String documentId, String strLanguage, String templateId,
       boolean multiReport, OutputTypeEnum outputTypeString)
       throws ReportingException, ServletException {
-    this(DalConnectionProvider.getReadOnlyConnectionProvider(), documentType, documentId,
-        strLanguage, templateId, multiReport, outputTypeString);
+    this(new DalConnectionProvider(false), documentType, documentId, strLanguage, templateId,
+        multiReport, outputTypeString);
   }
 
   public Report(ConnectionProvider connectionProvider, DocumentType documentType, String documentId,
