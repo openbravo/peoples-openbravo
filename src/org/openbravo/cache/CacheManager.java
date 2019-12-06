9a025fa2e512551970eaa7afc954d5bfc79d0650
Mauricio Peccorini <mauricio.peccorini@openbravo.com>
2019-12-06 17:25:08 +0100

    [cache] Change constructor argument to abstract getter method

--- src/org/openbravo/cache/CacheManager.java
+++ src/org/openbravo/cache/CacheManager.java
@@ -7,37 +7,30 @@ import java.util.concurrent.ConcurrentMap;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public abstract class CacheManager<K,V> {
-  
+public abstract class CacheManager<K, V> {
+
   private final ConcurrentMap<K, V> cache = new ConcurrentHashMap<K, V>();
-  private String cacheIdentifier;
   private Date lastInvalidation;
-  
-  public CacheManager(String cacheIdentifier){
-    this.cacheIdentifier = cacheIdentifier;
-  }
-  
-  public String getCacheIdentifier() {
-    return cacheIdentifier;
-  }
-  
+
+  public abstract String getCacheIdentifier();
+
   public boolean containsKey(K key) {
     return cache.containsKey(key);
   }
-  
+
   public V getEntry(K key) {
     return cache.get(key);
   }
-  
+
   public void putEntry(K key, V value) {
     cache.put(key, value);
   }
-  
+
   public void invalidate() {
     cache.clear();
     lastInvalidation = new Date();
   }
-  
+
   public void invalidateIfExpired(Date expirationDate) {
     if (lastInvalidation == null || lastInvalidation.compareTo(expirationDate) < 0) {
       cache.clear();
