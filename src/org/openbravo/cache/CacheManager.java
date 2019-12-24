dceac00a2a70323f6405342f3386f00134dd528b
Mauricio Peccorini <mauricio.peccorini@openbravo.com>
2019-12-24 14:03:34 +0100

    [cache] Add javadoc to CacheManager

--- src/org/openbravo/cache/CacheManager.java
+++ src/org/openbravo/cache/CacheManager.java
@@ -8,6 +8,22 @@ import java.util.Date;
 
 import javax.enterprise.util.AnnotationLiteral;
 
+/**
+ * A CacheManager holds frequently used data in memory and provides the means of removing it when
+ * the data has changed in the underlying data store.
+ * 
+ * Invalidation will usually be invoked by the CacheInvalidationBackgroundManager, a process that
+ * checks for generic cache invalidation events and works across several, non-clustered, application
+ * servers.
+ * 
+ * For a CacheManager to be injected into the CacheInvalidationBackgroundManager it must:
+ * <li>Be annotated with @ApplicationContext.
+ * <li>Be annotated with a @CacheManager.Qualifier whose String parameter corresponds to the search
+ * key specified in the AD_CACHE table.
+ * <li>Implement the getEntry and putEntry methods in a thread safe manner.
+ * 
+ * @see org.openbravo.cache.CacheInvalidationBackgroundManager
+ */
 public interface CacheManager<K, V> {
 
   V getEntry(K key);
