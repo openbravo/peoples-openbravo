d4c1628fe0f735bf6140d23e1ca5e692c67b2661
Álvaro Ferraz <alvaro.ferraz@openbravo.com>
2020-01-21 12:58:15 +0100

    [indexedDB-masterdata-tax] Do not calculate taxes when opening a receipt

--- web/org.openbravo.retail.posterminal/js/data/dataordertaxes.js
+++ web/org.openbravo.retail.posterminal/js/data/dataordertaxes.js
@@ -10,67 +10,168 @@
 (function() {
   const calculateTaxes = function(receipt) {
     try {
+      if (!receipt.get('isEditable') && !receipt.get('forceCalculateTaxes')) {
+        return regenerateTaxesInfo(receipt);
+      }
+
       const taxes = OB.Taxes.Pos.calculateTaxes(receipt);
       const lineError = taxes.lines.find(lineTax => lineTax.error);
       if (lineError) {
         const line = receipt
           .get('lines')
           .find(line => line.id === lineError.id);
-        const reason = OB.I18N.getLabel('OBPOS_TaxNotFound_Message', [
-          receipt.get('bp').get('name') ||
-            OB.I18N.getLabel('OBPOS_LblEmptyAddress'),
-          receipt.get('bp').get('shipLocName') ||
-            OB.I18N.getLabel('OBPOS_LblEmptyAddress')
-        ]);
-        showTaxCalculationError(receipt, line, reason);
-      } else {
-        receipt.set(
-          {
-            gross: taxes.header.grossAmount,
-            net: taxes.header.netAmount,
-            taxes: taxes.header.taxes
-          },
-          {
-            silent: true
-          }
-        );
-        receipt.get('lines').forEach(line => {
-          const lineTax = taxes.lines.find(lineTax => lineTax.id === line.id);
-          if (receipt.get('priceIncludesTax')) {
-            line.set(
-              {
-                net: lineTax.netAmount,
-                discountedNet: lineTax.netAmount,
-                pricenet: lineTax.netPrice,
-                tax: lineTax.tax,
-                taxLines: lineTax.taxes
-              },
-              {
-                silent: true
-              }
-            );
-          } else {
-            line.set(
-              {
-                gross: lineTax.grossAmount,
-                discountedGross: lineTax.grossAmount,
-                tax: lineTax.tax,
-                taxLines: lineTax.taxes
-              },
-              {
-                silent: true
-              }
-            );
-          }
-        });
+        return showTaxCalculationError(receipt, line);
       }
+
+      receipt.set(
+        {
+          gross: taxes.header.grossAmount,
+          net: taxes.header.netAmount,
+          taxes: taxes.header.taxes
+        },
+        {
+          silent: true
+        }
+      );
+      receipt.get('lines').forEach(line => {
+        const lineTax = taxes.lines.find(lineTax => lineTax.id === line.id);
+        if (receipt.get('priceIncludesTax')) {
+          line.set(
+            {
+              net: lineTax.netAmount,
+              discountedNet: lineTax.netAmount,
+              pricenet: lineTax.netPrice,
+              tax: lineTax.tax,
+              taxLines: lineTax.taxes
+            },
+            {
+              silent: true
+            }
+          );
+        } else {
+          line.set(
+            {
+              gross: lineTax.grossAmount,
+              discountedGross: lineTax.grossAmount,
+              tax: lineTax.tax,
+              taxLines: lineTax.taxes
+            },
+            {
+              silent: true
+            }
+          );
+        }
+      });
     } catch (error) {
       throw OB.I18N.getLabel('OBPOS_TaxCalculationError_Message');
     }
   };
 
-  const showTaxCalculationError = function(receipt, line, reason) {
-    var title = OB.I18N.getLabel('OBPOS_TaxNotFound_Header');
+  const regenerateTaxesInfo = function(receipt) {
+    const value = _.map(receipt.get('lines').models, function(line) {
+      var discAmt,
+        lineObj = {};
+      _.forEach(line.get('taxes'), function(tax) {
+        lineObj[tax.taxId] = {
+          amount: tax.taxAmount,
+          name: tax.identifier,
+          net: tax.taxableAmount,
+          rate: tax.taxRate
+        };
+        line.set(
+          {
+            taxLines: lineObj,
+            taxAmount: OB.DEC.add(line.get('taxAmount') || 0, tax.taxAmount)
+          },
+          {
+            silent: true
+          }
+        );
+      });
+      discAmt = line.get('promotions').reduce(function(memo, disc) {
+        return OB.DEC.add(memo, disc.actualAmt || disc.amt || 0);
+      }, 0);
+      line.set(
+        {
+          net: receipt.get('priceIncludesTax')
+            ? line.get('linenetamount')
+            : line.get('net')
+        },
+        {
+          silent: true
+        }
+      );
+      if (receipt.get('priceIncludesTax')) {
+        line.set(
+          {
+            discountedNet: line.get('net'),
+            discountedLinePrice: OB.DEC.add(
+              line.get('net'),
+              line.get('taxAmount')
+            )
+          },
+          {
+            silent: true
+          }
+        );
+      } else {
+        line.set(
+          {
+            gross: OB.DEC.add(line.get('net'), line.get('taxAmount')),
+            discountedNet: OB.DEC.sub(line.get('net'), discAmt),
+            discountedNetPrice: OB.DEC.sub(line.get('net'), discAmt),
+            discountedGross: OB.DEC.add(
+              OB.DEC.sub(line.get('net'), discAmt),
+              line.get('taxAmount')
+            )
+          },
+          {
+            silent: true
+          }
+        );
+      }
+    });
+
+    if (value.length !== receipt.get('lines').length) {
+      OB.debug('The number of original lines of the receipt has change!');
+      return;
+    }
+
+    receipt.set(
+      'net',
+      receipt.get('lines').reduce(function(memo, line) {
+        return OB.DEC.add(memo, line.get('discountedNet'));
+      }, 0),
+      {
+        silent: true
+      }
+    );
+
+    var taxesColl = {};
+    _.forEach(receipt.get('receiptTaxes'), function(receiptTax) {
+      var taxObj = {
+        amount: receiptTax.amount,
+        cascade: receiptTax.cascade,
+        docTaxAmount: receiptTax.docTaxAmount,
+        lineNo: receiptTax.lineNo,
+        name: receiptTax.name,
+        net: receiptTax.net,
+        rate: receiptTax.rate,
+        taxBase: receiptTax.taxBase
+      };
+      taxesColl[receiptTax.taxid] = taxObj;
+    });
+    receipt.set('taxes', taxesColl);
+  };
+
+  const showTaxCalculationError = function(receipt, line) {
+    const title = OB.I18N.getLabel('OBPOS_TaxNotFound_Header');
+    const reason = OB.I18N.getLabel('OBPOS_TaxNotFound_Message', [
+      receipt.get('bp').get('name') ||
+        OB.I18N.getLabel('OBPOS_LblEmptyAddress'),
+      receipt.get('bp').get('shipLocName') ||
+        OB.I18N.getLabel('OBPOS_LblEmptyAddress')
+    ]);
     OB.error(title + ':' + reason);
     receipt.set(
       'taxes',
