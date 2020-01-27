da4a65c1848bebdf5b519be743db03441fe02e90
Ranjith S R <ranjith@qualiantech.com>
2020-01-27 13:48:25 +0530

    Related to issue 42329 : Backed out changeset: b04744f9bbb8

--- src/org/openbravo/retail/posterminal/master/BusinessPartner.java
+++ src/org/openbravo/retail/posterminal/master/BusinessPartner.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2020 Openbravo S.L.U.
+ * Copyright (C) 2012-2017 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -22,8 +22,6 @@ import org.openbravo.client.kernel.ComponentProvider.Qualifier;
 import org.openbravo.dal.core.OBContext;
 import org.openbravo.erpCommon.businessUtility.Preferences;
 import org.openbravo.erpCommon.utility.PropertyException;
-import org.openbravo.mobile.core.model.HQLEntity;
-import org.openbravo.mobile.core.model.HQLProperty;
 import org.openbravo.mobile.core.model.HQLPropertyList;
 import org.openbravo.mobile.core.model.ModelExtension;
 import org.openbravo.mobile.core.model.ModelExtensionUtils;
@@ -53,8 +51,6 @@ public class BusinessPartner extends ProcessHQLQuery {
   protected List<String> getQuery(JSONObject jsonsent) throws JSONException {
 
     boolean isRemote = false;
-    boolean useGroupBy = false;
-    String entitiesJoined, groupByExpression = "";
     try {
       OBContext.setAdminMode(false);
       isRemote = "Y".equals(Preferences.getPreferenceValue("OBPOS_remote.customer", true,
@@ -69,31 +65,16 @@ public class BusinessPartner extends ProcessHQLQuery {
 
     HQLPropertyList regularBusinessPartnerHQLProperties = ModelExtensionUtils
         .getPropertyExtensions(extensions);
-    List<HQLEntity> entityExtensions = ModelExtensionUtils.getEntityExtensions(extensions);
-    entitiesJoined = ModelExtensionUtils.getHQLEntitiesJoined(entityExtensions);
-    for (HQLProperty property : regularBusinessPartnerHQLProperties.getProperties()) {
-      if (property.isIncludeInGroupBy()) {
-        groupByExpression = regularBusinessPartnerHQLProperties.getHqlGroupBy();
-        useGroupBy = true;
-        break;
-      }
-    }
-
     String hql = "SELECT " + regularBusinessPartnerHQLProperties.getHqlSelect() //
         + "FROM BusinessPartner AS bp " //
         + "join bp.priceList AS plist " //
         + "left outer join bp.language AS lang " //
         + "left outer join bp.greeting AS grt " //
         + "left outer join bp.aDUserList AS ulist " //
-        + entitiesJoined //
         + "WHERE $filtersCriteria AND " //
         + "bp.customer = true AND " + "(bp.$incrementalUpdateCriteria) AND "
         + "bp.$readableSimpleClientCriteria AND " + "bp.$naturalOrgCriteria AND "
         + "(not exists (select 1 from ADUser usr where usr.businessPartner = bp)) ";
-    if (useGroupBy) {
-      hql += "GROUP BY " + groupByExpression;
-      hql += "HAVING $havingCriteria ";
-    }
     if (isRemote) {
       hql += "ORDER BY bp.name, bp.id";
     } else {
@@ -106,15 +87,10 @@ public class BusinessPartner extends ProcessHQLQuery {
         + "left outer join bp.language AS lang " //
         + "left outer join bp.greeting AS grt " //
         + "left outer join bp.aDUserList AS ulist " //
-        + entitiesJoined //
         + "WHERE $filtersCriteria AND " //
         + "bp.customer = true AND " + "bp.$readableSimpleClientCriteria AND "
         + "bp.$naturalOrgCriteria AND " + "(bp.$incrementalUpdateCriteria) AND "
         + "(ulist.id in (select max(ulist2.id) from ADUser as ulist2 where ulist2.businessPartner=bp)) ";
-    if (useGroupBy) {
-      hql2 += "GROUP BY " + groupByExpression;
-      hql2 += "HAVING $havingCriteria ";
-    }
     if (isRemote) {
       hql2 += "ORDER BY bp.name, bp.id";
     } else {
