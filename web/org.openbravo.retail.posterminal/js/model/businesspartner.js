8ed581ca9ccea521b8936e8332a155664ae4e8cc
Ranjith S R <ranjith@qualiantech.com>
2020-01-30 13:09:30 +0530

    Fixed issue 43023 : Customer Location should be listed by last creationDate

--- web/org.openbravo.retail.posterminal/js/model/businesspartner.js
+++ web/org.openbravo.retail.posterminal/js/model/businesspartner.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2019 Openbravo S.L.U.
+ * Copyright (C) 2012-2020 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -299,7 +299,7 @@
           operator: OB.Dal.EQ,
           value: bpId || this.get('id')
         },
-        _orderByClause: 'c_bpartner_location_id desc'
+        _orderByClause: 'creationDate desc'
       };
       if (OB.MobileApp.model.hasPermission('OBPOS_remote.customer', true)) {
         var filterBpartnerId = {
