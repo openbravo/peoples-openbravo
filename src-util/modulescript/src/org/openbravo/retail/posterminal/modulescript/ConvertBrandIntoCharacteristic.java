9aad50ae9f1f28c8798cb8af3e74b69a877d5e38
Sandra Huguet <sandra.huguet@openbravo.com>
2020-02-18 09:22:43 +0100

    [IndexedDB-masterdata-product] fix modulescript

--- src-util/modulescript/src/org/openbravo/retail/posterminal/modulescript/ConvertBrandIntoCharacteristic.java
+++ src-util/modulescript/src/org/openbravo/retail/posterminal/modulescript/ConvertBrandIntoCharacteristic.java
@@ -10,9 +10,10 @@
 package org.openbravo.retail.posterminal.modulescript;
 
 import java.util.UUID;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+
 import org.openbravo.database.ConnectionProvider;
 import org.openbravo.modulescript.ModuleScript;
 import org.openbravo.modulescript.ModuleScriptExecutionLimits;
@@ -27,38 +28,43 @@ public class ConvertBrandIntoCharacteristic extends ModuleScript {
   public void execute() {
     try {
       ConnectionProvider cp = getConnectionProvider();
-      if (!ConvertBrandIntoCharacteristicData.hasPreference(cp)) {
-        // create characteristic
-        ConvertBrandIntoCharacteristicData[] data = ConvertBrandIntoCharacteristicData
-            .selectBrandByClient(cp);
-        for (ConvertBrandIntoCharacteristicData brand : data) {
-          String id = UUID.randomUUID().toString().replace("-", "").toUpperCase();
-          ConvertBrandIntoCharacteristicData.insertCharacteristic(cp.getConnection(), cp, id,
-              brand.adClientId);
-          // create characteristic value
-          ConvertBrandIntoCharacteristicData[] data2 = ConvertBrandIntoCharacteristicData
-              .selectBrands(cp, brand.adClientId);
-          for (ConvertBrandIntoCharacteristicData brand2 : data2) {
-            ConvertBrandIntoCharacteristicData.insertCharacteristicValues(cp.getConnection(), cp,
-                brand2.mBrandId, brand2.adClientId, brand2.adOrgId, id, brand2.name);
-            ConvertBrandIntoCharacteristicData.insertTreeNode(cp.getConnection(), cp, brand2.mBrandId, 
-                brand2.adClientId, brand2.adOrgId);
+      if (!ConvertBrandIntoCharacteristicData.existBrand(cp)) {
+        if (!ConvertBrandIntoCharacteristicData.hasPreference(cp)) {
+          // create characteristic
+          ConvertBrandIntoCharacteristicData[] data = ConvertBrandIntoCharacteristicData
+              .selectBrandByClient(cp);
+          for (ConvertBrandIntoCharacteristicData brand : data) {
+            String id = UUID.randomUUID().toString().replace("-", "").toUpperCase();
+            ConvertBrandIntoCharacteristicData.insertCharacteristic(cp.getConnection(), cp, id,
+                brand.adClientId);
+            // create characteristic value
+            ConvertBrandIntoCharacteristicData[] data2 = ConvertBrandIntoCharacteristicData
+                .selectBrands(cp, brand.adClientId);
+            for (ConvertBrandIntoCharacteristicData brand2 : data2) {
+              ConvertBrandIntoCharacteristicData.insertCharacteristicValues(cp.getConnection(), cp,
+                  brand2.mBrandId, brand2.adClientId, brand2.adOrgId, id, brand2.name);
+              ConvertBrandIntoCharacteristicData.insertTreeNode(cp.getConnection(), cp,
+                  brand2.mBrandId, brand2.adClientId, brand2.adOrgId);
+            }
+            // create product characteristic value
+            ConvertBrandIntoCharacteristicData[] data3 = ConvertBrandIntoCharacteristicData
+                .selectProductBrands(cp, brand.adClientId);
+            for (ConvertBrandIntoCharacteristicData brand3 : data3) {
+              ConvertBrandIntoCharacteristicData.insertProductCharacteristic(cp.getConnection(), cp,
+                  brand3.adClientId, brand3.adOrgId, brand3.mProductId, id);
+              ConvertBrandIntoCharacteristicData.insertProductCharacteristicValue(
+                  cp.getConnection(), cp, brand3.adClientId, brand3.adOrgId, brand3.mProductId, id,
+                  brand3.mBrandId);
+              ConvertBrandIntoCharacteristicData.updateCharacteristicDesc(cp.getConnection(), cp,
+                  brand3.name, brand3.mProductId);
+            }
           }
-          // create product characteristic value
-          ConvertBrandIntoCharacteristicData[] data3 = ConvertBrandIntoCharacteristicData
-              .selectProductBrands(cp, brand.adClientId);
-          for (ConvertBrandIntoCharacteristicData brand3 : data3) {
-            ConvertBrandIntoCharacteristicData.insertProductCharacteristic(cp.getConnection(), cp,
-                brand3.adClientId, brand3.adOrgId, brand3.mProductId, id);
-            ConvertBrandIntoCharacteristicData.insertProductCharacteristicValue(cp.getConnection(),
-                cp, brand3.adClientId, brand3.adOrgId, brand3.mProductId, id, brand3.mBrandId);
-            ConvertBrandIntoCharacteristicData.updateCharacteristicDesc (cp.getConnection(),
-                cp, brand3.name, brand3.mProductId);
-          }
+          ConvertBrandIntoCharacteristicData.createPreference(cp);
         }
-        ConvertBrandIntoCharacteristicData.createPreference(cp);
       }
-    } catch (Exception e) {
+    } catch (
+
+    Exception e) {
       log4j.error("Errors converting brand into characteristic");
       handleError(e);
     }
