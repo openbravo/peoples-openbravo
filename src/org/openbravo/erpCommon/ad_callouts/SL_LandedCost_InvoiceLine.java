f543a1f94db01aa13f971e9c9d75eb7755d00159
Iñigo Lerga <inigo.lerga@openbravo.com>
2020-02-19 13:16:36 +0100

    fixes BUG-43261:Applied new HQL Style

--- src/org/openbravo/erpCommon/ad_callouts/SL_LandedCost_InvoiceLine.java
+++ src/org/openbravo/erpCommon/ad_callouts/SL_LandedCost_InvoiceLine.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2014-2018 Openbravo SLU 
+ * All portions are Copyright (C) 2014-2020 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -22,8 +22,6 @@ import java.math.BigDecimal;
 
 import javax.servlet.ServletException;
 
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.openbravo.base.secureApp.VariablesSecureApp;
 import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.service.OBDal;
@@ -52,14 +50,18 @@ public class SL_LandedCost_InvoiceLine extends SimpleCallout {
     BigDecimal totalAssigned = BigDecimal.ZERO;
     OBContext.setAdminMode(false);
     try {
-      final StringBuilder hqlString = new StringBuilder();
-      hqlString.append("select coalesce(sum(e.amount),0)");
-      hqlString.append(" from LandedCostCost as e");
-      hqlString.append(" where e.invoiceLine = :invoiceLine");
-      final Session session = OBDal.getInstance().getSession();
-      final Query<BigDecimal> query = session.createQuery(hqlString.toString(), BigDecimal.class);
-      query.setParameter("invoiceLine", invoiceLine);
-      totalAssigned = (BigDecimal) query.uniqueResult();
+      //@formatter:off
+      final String hql =
+                    "select coalesce(sum(e.amount),0)" +
+                    "  from LandedCostCost as e" +
+                    " where e.invoiceLine.id = :invoiceLineId";
+      //@formatter:on
+
+      totalAssigned = OBDal.getInstance()
+          .getSession()
+          .createQuery(hql, BigDecimal.class)
+          .setParameter("invoiceLineId", invoiceLine.getId())
+          .uniqueResult();
 
     } finally {
       OBContext.restorePreviousMode();
