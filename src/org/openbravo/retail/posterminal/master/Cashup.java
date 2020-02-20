bed0ef9684c91bea3b8b61912e3909b24fd81c94
Ranjith S R <ranjith@qualiantech.com>
2020-02-20 08:52:47 +0530

    Fixed issue 43229 : FinAccount should be validated when generating Cashup
    * For Terminal with Only one Payment Method (Leave as credit),
      FinAccount will not be available.

--- src/org/openbravo/retail/posterminal/master/Cashup.java
+++ src/org/openbravo/retail/posterminal/master/Cashup.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2014-2019 Openbravo S.L.U.
+ * Copyright (C) 2014-2020 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -285,8 +285,10 @@ public class Cashup extends JSONProcessSimple {
           .add(Restrictions.eq(FIN_FinaccTransaction.PROPERTY_OBPOSAPPCASHUP, cashupObj));
       cashMgmTransCriteria
           .add(Restrictions.in(FIN_FinaccTransaction.PROPERTY_GLITEM, glItemList.toArray()));
-      cashMgmTransCriteria
-          .add(Restrictions.in(FIN_FinaccTransaction.PROPERTY_ACCOUNT, finAcctList.toArray()));
+      if (finAcctList.size() > 0) {
+        cashMgmTransCriteria
+            .add(Restrictions.in(FIN_FinaccTransaction.PROPERTY_ACCOUNT, finAcctList.toArray()));
+      }
 
       List<FIN_FinaccTransaction> cashMgmtList = cashMgmTransCriteria.list();
       for (BaseOBObject cashMgmt : cashMgmtList) {
