845a80dc7c3bff925d326e346c6f6ec072b385cd
Ranjith S R <ranjith@qualiantech.com>
2020-02-20 16:01:19 +0530

    Fixed issue 42611 : Receipt lines table scroll should be updated when render finished
    * Backed out changeset: e66f9402a17c
    * Backed out changeset: c531255e4b5d

--- web/org.openbravo.retail.posterminal/js/components/order.js
+++ web/org.openbravo.retail.posterminal/js/components/order.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2013-2019 Openbravo S.L.U.
+ * Copyright (C) 2013-2020 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -969,7 +969,6 @@ enyo.kind({
       'paintTaxes',
       function() {
         this.setTaxes();
-        this.$.listOrderLines.setScrollAfterAdd();
       },
       this
     );
@@ -1072,6 +1071,13 @@ enyo.kind({
       'add remove reset',
       function() {
         this.doAdjustOrderCaption();
+      },
+      this
+    );
+    this.order.get('lines').on(
+      'updatedView',
+      function() {
+        this.$.listOrderLines.setScrollAfterAdd();
       },
       this
     );
