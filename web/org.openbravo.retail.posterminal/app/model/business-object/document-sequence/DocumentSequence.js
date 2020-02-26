bf9c1ea837f4077ec3edcd15f3836145d02a7cfb
openbravo <openbravo@por0965.mydomain>
2020-02-26 14:32:42 +0100

    [indexedDB-state-docseq] Add DocumentSequence.js

---
+++ web/org.openbravo.retail.posterminal/app/model/business-object/document-sequence/DocumentSequence.js
@@ -0,0 +1,19 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2020 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+/**
+ * @fileoverview defines the Document Sequence model
+ */
+
+(() => {
+  /**
+   * Initial state of the document sequence model
+   */
+  OB.App.StateAPI.registerModel('DocumentSequence', {});
+})();
