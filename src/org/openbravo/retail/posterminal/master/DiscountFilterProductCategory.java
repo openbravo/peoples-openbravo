aabadb6f45a50d77996cb47ccd4f033f32e77425
Carlos Aristu <carlos.aristu@openbravo.com>
2020-03-05 10:23:45 +0100

    [IndexedDB-masterdata] Use new utility methods to reduce code duplication

--- src/org/openbravo/retail/posterminal/master/DiscountFilterProductCategory.java
+++ src/org/openbravo/retail/posterminal/master/DiscountFilterProductCategory.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2019 Openbravo S.L.U.
+ * Copyright (C) 2012-2020 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -10,7 +10,6 @@ package org.openbravo.retail.posterminal.master;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
@@ -20,7 +19,6 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.openbravo.client.kernel.ComponentProvider.Qualifier;
 import org.openbravo.mobile.core.master.MasterDataProcessHQLQuery.MasterDataModel;
-import org.openbravo.mobile.core.model.HQLProperty;
 import org.openbravo.mobile.core.model.HQLPropertyList;
 import org.openbravo.mobile.core.model.ModelExtension;
 import org.openbravo.mobile.core.model.ModelExtensionUtils;
@@ -52,10 +50,7 @@ public class DiscountFilterProductCategory extends Discount {
 
   @Override
   public List<String> getMasterDataModelProperties() {
-    return ModelExtensionUtils.getPropertyExtensions(extensions)
-        .getProperties()
-        .stream()
-        .map(HQLProperty::getHqlProperty)
-        .collect(Collectors.toList());
+    return getPropertiesFrom(extensions);
+
   }
 }
--- src/org/openbravo/retail/posterminal/master/DiscountFilterRole.java
+++ src/org/openbravo/retail/posterminal/master/DiscountFilterRole.java
@@ -1,17 +1,15 @@
 /*
  ************************************************************************************
- * Copyright (C) 2013-2019 Openbravo S.L.U.
+ * Copyright (C) 2013-2020 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
  ************************************************************************************
  */
-
 package org.openbravo.retail.posterminal.master;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
@@ -21,7 +19,6 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.openbravo.client.kernel.ComponentProvider.Qualifier;
 import org.openbravo.mobile.core.master.MasterDataProcessHQLQuery.MasterDataModel;
-import org.openbravo.mobile.core.model.HQLProperty;
 import org.openbravo.mobile.core.model.HQLPropertyList;
 import org.openbravo.mobile.core.model.ModelExtension;
 import org.openbravo.mobile.core.model.ModelExtensionUtils;
@@ -53,10 +50,7 @@ public class DiscountFilterRole extends Discount {
 
   @Override
   public List<String> getMasterDataModelProperties() {
-    return ModelExtensionUtils.getPropertyExtensions(extensions)
-        .getProperties()
-        .stream()
-        .map(HQLProperty::getHqlProperty)
-        .collect(Collectors.toList());
+    return getPropertiesFrom(extensions);
+
   }
 }
