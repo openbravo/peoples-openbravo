387f434ea52242fe2ec44bae64761359cc4a7e43
Mark Molina <mark.molina@doceleguas.com>
2020-03-13 22:57:07 -0300

    Exposed ExportOrder as webservice. - Created ExportOrder as a new web service that extends PaidReceipts and updates   properties name  of the output JSON to be more accurated with ones of the input   request of ExternalOrderLoader webservice - Updated swagger doc for these changes

---
+++ src/org/openbravo/retail/posterminal/ExportOrder.java
@@ -0,0 +1,140 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2020 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+package org.openbravo.retail.posterminal;
+
+import javax.servlet.ServletException;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.base.exception.OBException;
+
+public class ExportOrder extends PaidReceipts {
+
+  @Override
+  public JSONObject exec(JSONObject jsonsent) throws JSONException, ServletException {
+    JSONObject result = super.exec(jsonsent);
+    return transform(result);
+  }
+
+  private JSONObject transform(JSONObject result) {
+    try {
+      JSONObject transformedResult = result;
+      if (transformedResult.has("data")) {
+        transformOrders(transformedResult.getJSONArray("data"));
+      }
+      return transformedResult;
+    } catch (Exception e) {
+      throw new OBException("Error transforming order for exporting", e);
+    }
+  }
+
+  private void transformOrders(JSONArray data) throws JSONException {
+    for (int i = 0; i < data.length(); i++) {
+      JSONObject order = (JSONObject) data.get(i);
+      renameStringPropertyInObject(order, "orderid", "id");
+      transformLines(order);
+      transformPayments(order);
+      transformOrderTaxes(order);
+    }
+  }
+
+  private void transformLines(JSONObject order) throws JSONException {
+    if (order.has("receiptLines")) {
+      renameArrayPropertyInObject(order, "receiptLines", "lines");
+      JSONArray orderLines = order.getJSONArray("lines");
+      for (int i = 0; i < orderLines.length(); i++) {
+        JSONObject line = (JSONObject) orderLines.get(i);
+        renameStringPropertyInObject(line, "id", "productId");
+        renameStringPropertyInObject(line, "name", "product");
+        renameStringPropertyInObject(line, "lineId", "id");
+        renameNumericPropertyInObject(line, "quantity", "qty");
+        renameNumericPropertyInObject(line, "lineGrossAmount", "grossAmount");
+        renameNumericPropertyInObject(line, "linenetamount", "netAmount");
+        transformOrderLineTaxes(line);
+        transformOrderLinePromotions(line);
+      }
+    }
+  }
+
+  private void transformPayments(JSONObject order) throws JSONException {
+    if (order.has("receiptPayments")) {
+      renameArrayPropertyInObject(order, "receiptPayments", "payments");
+      JSONArray orderPayments = order.getJSONArray("payments");
+      for (int i = 0; i < orderPayments.length(); i++) {
+        JSONObject payment = (JSONObject) orderPayments.get(i);
+        renameNumericPropertyInObject(payment, "paymentAmount", "paidAmount");
+      }
+    }
+  }
+
+  private void transformOrderTaxes(JSONObject order) throws JSONException {
+    if (order.has("receiptTaxes")) {
+      renameArrayPropertyInObject(order, "receiptTaxes", "taxes");
+      JSONArray orderTaxes = order.getJSONArray("taxes");
+      for (int i = 0; i < orderTaxes.length(); i++) {
+        JSONObject tax = (JSONObject) orderTaxes.get(i);
+        renameStringPropertyInObject(tax, "taxid", "id");
+        renameStringPropertyInObject(tax, "identifier", "name");
+        renameNumericPropertyInObject(tax, "taxRate", "rate");
+      }
+    }
+  }
+
+  private void transformOrderLineTaxes(JSONObject line) throws JSONException {
+    if (line.has("taxes")) {
+      renameArrayPropertyInObject(line, "taxes", "taxLines");
+      JSONArray taxLines = line.getJSONArray("taxLines");
+      for (int i = 0; i < taxLines.length(); i++) {
+        JSONObject lineTax = (JSONObject) taxLines.get(i);
+        renameStringPropertyInObject(lineTax, "taxId", "id");
+        renameStringPropertyInObject(lineTax, "identifier", "name");
+        renameNumericPropertyInObject(lineTax, "taxRate", "rate");
+      }
+    }
+  }
+
+  private void transformOrderLinePromotions(JSONObject line) throws JSONException {
+    if (line.has("promotions")) {
+      JSONArray linePromotions = line.getJSONArray("promotions");
+      for (int i = 0; i < linePromotions.length(); i++) {
+        JSONObject linePromotion = (JSONObject) linePromotions.get(i);
+        renameStringPropertyInObject(linePromotion, "ruleId", "discountRule");
+        renameNumericPropertyInObject(linePromotion, "amt", "amount");
+        renameNumericPropertyInObject(linePromotion, "qtyOffer", "quantity");
+      }
+    }
+  }
+
+  private void renameArrayPropertyInObject(JSONObject object, String propertyNameToReplace,
+      String newPropertyName) throws JSONException {
+    if (object.has(propertyNameToReplace)) {
+      object.put(newPropertyName, object.getJSONArray(propertyNameToReplace));
+      object.remove(propertyNameToReplace);
+    }
+  }
+
+  private void renameStringPropertyInObject(JSONObject object, String propertyNameToReplace,
+      String newPropertyName) throws JSONException {
+    if (object.has(propertyNameToReplace)) {
+      object.put(newPropertyName, object.getString(propertyNameToReplace));
+      object.remove(propertyNameToReplace);
+    }
+  }
+
+  private void renameNumericPropertyInObject(JSONObject object, String propertyNameToReplace,
+      String newPropertyName) throws JSONException {
+    if (object.has(propertyNameToReplace)) {
+      object.put(newPropertyName, object.getDouble(propertyNameToReplace));
+      object.remove(propertyNameToReplace);
+    }
+  }
+
+}
