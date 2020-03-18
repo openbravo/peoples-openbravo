61aed2c158d28d4d9091e6c677f812005edd3e30
Álvaro Ferraz <alvaro.ferraz@openbravo.com>
2020-03-18 15:32:28 +0100

    [indexedDB-state-docseq] Recalculate orderType before calling setTicketDocumentNo()

--- web/org.openbravo.retail.posterminal/js/utils/ticketCloseUtils.js
+++ web/org.openbravo.retail.posterminal/js/utils/ticketCloseUtils.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2017-2019 Openbravo S.L.U.
+ * Copyright (C) 2017-2020 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -123,12 +123,12 @@
         ) {
           receipt.setOrderType('OBPOS_receipt.return', OB.DEC.One, {
             applyPromotions: false,
-            saveOrder: false
+            saveOrder: true
           });
         } else {
           receipt.setOrderType('', OB.DEC.Zero, {
             applyPromotions: false,
-            saveOrder: false
+            saveOrder: true
           });
         }
       }
