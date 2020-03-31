fffa39ce6105a34b41a9bf6b1390784d67a29652
Víctor Martínez Romanos <victor.martinez@openbravo.com>
2020-03-31 12:58:39 +0200

    fixed BUG-43581: Create Lines From support for trigger exceptions
    In case of errors during the process we rollback and close the current transaction. This is a general improvement that makes sense to the process.
    
    In the JSONObject returned to the UI in case of exceptions, we try to get the message text to display to the end user. This is only possible because the previous locked transaction is closed, so DAL opens a new one to retrieve the error message.
    
    Note that this code works for both db and java exceptions

--- src/org/openbravo/common/actionhandler/createlinesfromprocess/CreateInvoiceLinesFromHandler.java
+++ src/org/openbravo/common/actionhandler/createlinesfromprocess/CreateInvoiceLinesFromHandler.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2018 Openbravo SLU 
+ * All portions are Copyright (C) 2018-2020 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -31,6 +31,7 @@ import org.openbravo.client.application.process.BaseProcessActionHandler;
 import org.openbravo.client.application.process.ResponseActionsBuilder.MessageType;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.model.common.invoice.Invoice;
+import org.openbravo.service.db.DbUtility;
 
 /**
  * Abstract class to be implemented by any process that creates invoice lines from any Openbravo
@@ -75,9 +76,11 @@ abstract class CreateInvoiceLinesFromHandler<T extends BaseOBObject>
   }
 
   private JSONObject showExceptionInViewAndRetry(Exception e) {
+    final Throwable ex = DbUtility
+        .getUnderlyingSQLException(e.getCause() != null ? e.getCause() : e);
     return getResponseBuilder()
-        .showMsgInProcessView(MessageType.ERROR, OBMessageUtils.messageBD("error"), e.getMessage(),
-            true)
+        .showMsgInProcessView(MessageType.ERROR, OBMessageUtils.messageBD("error"),
+            OBMessageUtils.parseTranslation(ex.getMessage()), true)
         .retryExecution()
         .build();
   }
