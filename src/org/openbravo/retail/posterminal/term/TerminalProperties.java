f3807a9f424f29c9a53362e2f2b44a31ac410fa6
Álvaro Ferraz <alvaro.ferraz@openbravo.com>
2020-03-31 14:10:47 +0200

    [indexedDB-state-docseq] Remove aggregated invoice sequence from Touchpoint window

--- src/org/openbravo/retail/posterminal/term/TerminalProperties.java
+++ src/org/openbravo/retail/posterminal/term/TerminalProperties.java
@@ -72,8 +72,6 @@ public class TerminalProperties extends ModelExtension {
     list.add(new HQLProperty("pos.fullretinvdocnoPrefix", "fullReturnInvoiceDocNoPrefix"));
     list.add(new HQLProperty("pos.simpinvdocnoPrefix", "simplifiedInvoiceDocNoPrefix"));
     list.add(new HQLProperty("pos.simpretinvdocnoPrefix", "simplifiedReturnInvoiceDocNoPrefix"));
-    list.add(new HQLProperty("pos.aggrinvdocnoPrefix", "aggregatedInvoiceDocNoPrefix"));
-    list.add(new HQLProperty("pos.aggrretinvdocnoPrefix", "aggregatedReturnInvoiceDocNoPrefix"));
     list.add(new HQLProperty("pos.obposTerminaltype.allowpayoncredit", "allowpayoncredit"));
     list.add(new HQLProperty("pos.obposTerminaltype.multiChange", "multiChange"));
     list.add(new HQLProperty("org.obposCountDiffLimit", "organizationCountDiffLimit"));
