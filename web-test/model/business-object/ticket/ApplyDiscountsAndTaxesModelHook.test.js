9714a95e2678ff1427006c7a860ca27fe742331b
Carlos Aristu <carlos.aristu@openbravo.com>
2020-04-24 13:41:16 +0200

    [indexeddb-ticketActions] Refactor model hook
      - Refactored discounts calculation logic
      - Include tax calculation logic.
      - Rename hook
      - Adapt test cases

---
+++ web-test/model/business-object/ticket/ApplyDiscountsAndTaxesModelHook.test.js
@@ -0,0 +1,199 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2020 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+/* global global */
+
+OB = {
+  App: {
+    Class: {}
+  },
+  Discounts: {
+    Pos: {
+      ruleImpls: [],
+      bpSets: []
+    }
+  },
+  Taxes: {
+    Pos: {
+      ruleImpls: []
+    }
+  }
+};
+
+global.lodash = require('../../../../../org.openbravo.mobile.core/web/org.openbravo.mobile.core/lib/vendor/lodash-4.17.15');
+const deepfreeze = require('../../../../../org.openbravo.mobile.core/web/org.openbravo.mobile.core/lib/vendor/deepfreeze-2.0.0');
+require('../../../../../org.openbravo.mobile.core/web/org.openbravo.mobile.core/app/model/application-state/StateAPI');
+OB.App.StateAPI.registerModel('Ticket');
+require('../../../../web/org.openbravo.retail.posterminal/app/model/business-object/ticket/ApplyDiscountsAndTaxesModelHook');
+
+describe('Apply Discounts and Taxes Model Hook', () => {
+  const ticket = {
+    id: '6FD3CDDDBB2A3805895853BB22F2E9F7',
+    qty: 1,
+    priceIncludesTax: true,
+    lines: [
+      {
+        id: 'BB66D8D151964A8A39586FCD7D9A394D',
+        qty: 1,
+        price: 300
+      }
+    ]
+  };
+
+  const discountResult = {
+    lines: {
+      BB66D8D151964A8A39586FCD7D9A394D: {
+        discounts: {
+          finalLinePrice: 270,
+          finalUnitPrice: 270,
+          promotions: [
+            {
+              ruleId: 'C26B841C84B14FE2AB1A334DD3672E87',
+              discountType: '697A7AB9FD9C4EE0A3E891D3D3CCA0A7',
+              name: 'GPS_10_per',
+              applyNext: true,
+              amt: 30,
+              qtyOffer: 1
+            }
+          ]
+        }
+      }
+    }
+  };
+
+  const taxesResult = {
+    header: {
+      id: '6FD3CDDDBB2A3805895853BB22F2E9F7',
+      grossAmount: 270,
+      netAmount: 223.14,
+      taxes: {
+        '5235D8E99A2749EFA17A5C92A52AEFC6': {
+          id: '5235D8E99A2749EFA17A5C92A52AEFC6',
+          net: 223.14,
+          amount: 46.86,
+          name: 'Entregas IVA 21%',
+          docTaxAmount: 'D',
+          rate: 21,
+          taxBase: null,
+          cascade: false,
+          lineNo: 10
+        }
+      }
+    },
+    lines: [
+      {
+        id: 'BB66D8D151964A8A39586FCD7D9A394D',
+        grossAmount: 270,
+        netAmount: 223.14,
+        grossPrice: 270,
+        netPrice: 223.14,
+        qty: 1,
+        tax: '5235D8E99A2749EFA17A5C92A52AEFC6',
+        taxRate: 1.21,
+        taxes: {
+          '5235D8E99A2749EFA17A5C92A52AEFC6': {
+            id: '5235D8E99A2749EFA17A5C92A52AEFC6',
+            net: 223.14,
+            amount: 46.86,
+            name: 'Entregas IVA 21%',
+            docTaxAmount: 'D',
+            rate: 21,
+            taxBase: null,
+            cascade: false,
+            lineNo: 10
+          }
+        }
+      }
+    ]
+  };
+
+  const setDiscountsEngineResultAs = obj => {
+    OB.Discounts.applyDiscounts = jest.fn().mockReturnValue(obj);
+  };
+
+  const setTaxesEngineResultAs = obj => {
+    OB.Taxes.Pos.translateTaxes = jest.fn().mockReturnValue(obj);
+    OB.Taxes.Pos.applyTaxes = jest.fn().mockReturnValue(obj);
+  };
+
+  let hook;
+  let payload;
+
+  beforeAll(() => {
+    const modelHook = OB.App.StateAPI.Ticket.modelHooks[0];
+    hook = modelHook.hook;
+    payload = modelHook.generatePayload;
+  });
+
+  it('apply discounts and taxes', () => {
+    setDiscountsEngineResultAs(discountResult);
+    setTaxesEngineResultAs(taxesResult);
+
+    const result = hook(deepfreeze(ticket), payload());
+
+    expect(result).toEqual({
+      id: '6FD3CDDDBB2A3805895853BB22F2E9F7',
+      gross: 270,
+      net: 223.14,
+      priceIncludesTax: true,
+      qty: 1,
+      lines: [
+        {
+          id: 'BB66D8D151964A8A39586FCD7D9A394D',
+          qty: 1,
+          gross: 300,
+          discountedGrossAmount: 270,
+          discountedNetAmount: 223.14,
+          lineRate: 1.21,
+          net: 223.14,
+          price: 300,
+          pricenet: 223.14,
+          promotions: [
+            {
+              ruleId: 'C26B841C84B14FE2AB1A334DD3672E87',
+              amt: 30,
+              discountType: '697A7AB9FD9C4EE0A3E891D3D3CCA0A7',
+              name: 'GPS_10_per',
+              qtyOffer: 1,
+              applyNext: true,
+              calculatedOnDiscountEngine: true
+            }
+          ],
+          tax: '5235D8E99A2749EFA17A5C92A52AEFC6',
+          taxLines: {
+            '5235D8E99A2749EFA17A5C92A52AEFC6': {
+              id: '5235D8E99A2749EFA17A5C92A52AEFC6',
+              amount: 46.86,
+              cascade: false,
+              docTaxAmount: 'D',
+              lineNo: 10,
+              name: 'Entregas IVA 21%',
+              net: 223.14,
+              rate: 21,
+              taxBase: null
+            }
+          }
+        }
+      ],
+      taxes: {
+        '5235D8E99A2749EFA17A5C92A52AEFC6': {
+          id: '5235D8E99A2749EFA17A5C92A52AEFC6',
+          amount: 46.86,
+          cascade: false,
+          docTaxAmount: 'D',
+          lineNo: 10,
+          name: 'Entregas IVA 21%',
+          net: 223.14,
+          rate: 21,
+          taxBase: null
+        }
+      }
+    });
+  });
+});
---
+++ web/org.openbravo.retail.posterminal/app/model/business-object/ticket/ApplyDiscountsAndTaxesModelHook.js
@@ -0,0 +1,86 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2020 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+
+/**
+ * @fileoverview Defines a hook for the Ticket model in charge of checking the ticket discounts
+ * and recalculating the taxes in order to maintain all the ticket data consistent.
+ */
+
+OB.App.StateAPI.Ticket.addModelHook({
+  generatePayload: () => {
+    return {
+      discountRules: [...OB.Discounts.Pos.ruleImpls],
+      taxRules: [...OB.Taxes.Pos.ruleImpls],
+      bpSets: { ...OB.Discounts.Pos.bpSets }
+    };
+  },
+
+  hook: (ticket, payload) => {
+    const newTicket = { ...ticket };
+    const { priceIncludesTax } = newTicket;
+
+    // calculate discounts
+    const discountsResult = OB.Discounts.applyDiscounts(
+      newTicket,
+      payload.discountRules,
+      payload.bpSets
+    );
+
+    // set the discount calculation result into the ticket
+    newTicket.lines = newTicket.lines.map(line => {
+      const discounts =
+        discountsResult.lines[line.id] &&
+        discountsResult.lines[line.id].discounts;
+      const newLine = {
+        ...line,
+        gross: line.qty * line.price,
+        discountedGrossAmount: line.qty * line.price,
+        net: line.qty * line.pricenet,
+        discountedNetAmount: line.qty * line.pricenet
+      };
+      if (discounts) {
+        if (priceIncludesTax) {
+          newLine.discountedGrossAmount = discounts.finalLinePrice;
+        } else {
+          newLine.discountedNetAmount = discounts.finalLinePrice;
+        }
+        newLine.promotions = discounts.promotions.map(promotion => ({
+          ...promotion,
+          calculatedOnDiscountEngine: true
+        }));
+      }
+      return newLine;
+    });
+
+    // recalculate taxes
+    const taxesResult = OB.Taxes.Pos.applyTaxes(newTicket, payload.taxRules);
+
+    // set the tax calculation result into the ticket
+    newTicket.gross = taxesResult.header.grossAmount;
+    newTicket.net = taxesResult.header.netAmount;
+    newTicket.taxes = taxesResult.header.taxes;
+    taxesResult.lines.forEach(taxLine => {
+      const line = newTicket.lines.find(l => l.id === taxLine.id);
+      if (priceIncludesTax) {
+        line.net = taxLine.netAmount;
+        line.discountedNetAmount = taxLine.netAmount;
+        line.pricenet = taxLine.netPrice;
+      } else {
+        line.gross = taxLine.grossAmount;
+        line.discountedGrossAmount = taxLine.grossAmount;
+        line.price = taxLine.grossPrice;
+      }
+      line.lineRate = taxLine.taxRate;
+      line.tax = taxLine.tax;
+      line.taxLines = taxLine.taxes;
+    });
+
+    return newTicket;
+  }
+});
