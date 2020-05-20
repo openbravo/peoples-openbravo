38a1eaf15146c4e209fe593eeaf2d29c34725788
Carlos Aristu <carlos.aristu@openbravo.com>
2020-05-20 08:28:53 +0200

    [indexeddb-ticketActions] TicketUtils improvements
      - Do not need to use a function definition
      - Set promotions property always

--- web/org.openbravo.retail.posterminal/app/model/business-object/ticket/TicketUtils.js
+++ web/org.openbravo.retail.posterminal/app/model/business-object/ticket/TicketUtils.js
@@ -9,75 +9,72 @@
 /**
  * @fileoverview Define utility functions for the Ticket Model
  */
-(function TicketUtilsDefinition() {
-  OB.App.StateAPI.Ticket.registerUtilityFunctions({
-    /**
-     * Applies the discounts and taxes to the given ticket
-     *
-     * @param {object} ticket - The ticket whose discounts and taxes will be calculated
-     * @param {object} settings - The calculation settings, which include:
-     *             * discountRules - The discount rules to be considered
-     *             * taxRules - The tax rules to be considered
-     *             * bpSets - The businessPartner sets
-     * @returns The ticket with the result of the discounts and taxes calculation
-     */
-    applyDiscountsAndTaxes(ticket, settings) {
-      const newTicket = { ...ticket };
-      const { priceIncludesTax } = newTicket;
 
-      // calculate discounts
-      const discountsResult = OB.Discounts.Pos.applyDiscounts(
-        newTicket,
-        settings.discountRules,
-        settings.bpSets
-      );
+OB.App.StateAPI.Ticket.registerUtilityFunctions({
+  /**
+   * Applies the discounts and taxes to the given ticket
+   *
+   * @param {object} ticket - The ticket whose discounts and taxes will be calculated
+   * @param {object} settings - The calculation settings, which include:
+   *             * discountRules - The discount rules to be considered
+   *             * taxRules - The tax rules to be considered
+   *             * bpSets - The businessPartner sets
+   * @returns The ticket with the result of the discounts and taxes calculation
+   */
+  applyDiscountsAndTaxes(ticket, settings) {
+    const newTicket = { ...ticket };
+    const { priceIncludesTax } = newTicket;
 
-      // set the discount calculation result into the ticket
-      newTicket.lines = newTicket.lines.map(line => {
-        const discounts =
-          discountsResult.lines[line.id] &&
-          discountsResult.lines[line.id].discounts;
-        const newLine = {
-          ...line,
-          discountedGrossAmount:
-            discounts && priceIncludesTax
-              ? discounts.finalLinePrice
-              : OB.DEC.mul(line.qty, line.price),
-          discountedNetAmount:
-            discounts && !priceIncludesTax
-              ? discounts.finalLinePrice
-              : OB.DEC.mul(line.qty, line.pricenet)
-        };
-        if (discounts) {
-          newLine.promotions = discounts.promotions;
-        }
-        return newLine;
-      });
+    // calculate discounts
+    const discountsResult = OB.Discounts.Pos.applyDiscounts(
+      newTicket,
+      settings.discountRules,
+      settings.bpSets
+    );
 
-      // recalculate taxes
-      const taxesResult = OB.Taxes.Pos.applyTaxes(newTicket, settings.taxRules);
+    // set the discount calculation result into the ticket
+    newTicket.lines = newTicket.lines.map(line => {
+      const discounts =
+        discountsResult.lines[line.id] &&
+        discountsResult.lines[line.id].discounts;
+      const newLine = {
+        ...line,
+        discountedGrossAmount:
+          discounts && priceIncludesTax
+            ? discounts.finalLinePrice
+            : OB.DEC.mul(line.qty, line.price),
+        discountedNetAmount:
+          discounts && !priceIncludesTax
+            ? discounts.finalLinePrice
+            : OB.DEC.mul(line.qty, line.pricenet),
+        promotions: discounts ? discounts.promotions : []
+      };
+      return newLine;
+    });
 
-      // set the tax calculation result into the ticket
-      newTicket.grossAmount = taxesResult.header.grossAmount;
-      newTicket.netAmount = taxesResult.header.netAmount;
-      newTicket.taxes = taxesResult.header.taxes;
-      taxesResult.lines.forEach(taxLine => {
-        const line = newTicket.lines.find(l => l.id === taxLine.id);
-        if (priceIncludesTax) {
-          line.netAmount = taxLine.netAmount;
-          line.discountedNetAmount = taxLine.netAmount;
-          line.netPrice = taxLine.netPrice;
-        } else {
-          line.grossAmount = taxLine.grossAmount;
-          line.discountedGrossAmount = taxLine.grossAmount;
-          line.grossPrice = taxLine.grossPrice;
-        }
-        line.taxRate = taxLine.taxRate;
-        line.tax = taxLine.tax;
-        line.taxes = taxLine.taxes;
-      });
+    // recalculate taxes
+    const taxesResult = OB.Taxes.Pos.applyTaxes(newTicket, settings.taxRules);
 
-      return newTicket;
-    }
-  });
-})();
+    // set the tax calculation result into the ticket
+    newTicket.grossAmount = taxesResult.header.grossAmount;
+    newTicket.netAmount = taxesResult.header.netAmount;
+    newTicket.taxes = taxesResult.header.taxes;
+    taxesResult.lines.forEach(taxLine => {
+      const line = newTicket.lines.find(l => l.id === taxLine.id);
+      if (priceIncludesTax) {
+        line.netAmount = taxLine.netAmount;
+        line.discountedNetAmount = taxLine.netAmount;
+        line.netPrice = taxLine.netPrice;
+      } else {
+        line.grossAmount = taxLine.grossAmount;
+        line.discountedGrossAmount = taxLine.grossAmount;
+        line.grossPrice = taxLine.grossPrice;
+      }
+      line.taxRate = taxLine.taxRate;
+      line.tax = taxLine.tax;
+      line.taxes = taxLine.taxes;
+    });
+
+    return newTicket;
+  }
+});
