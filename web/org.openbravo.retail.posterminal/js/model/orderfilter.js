e9f0aed67d76d8a6fd799262018cf3ec62348e21
Guillermo Alvarez de Eulate <guillermo.alvarez@openbravo.com>
2020-05-27 18:26:22 +0200

    [extbp] Show extBpIdentifier details in openreceipts dialog

--- web/org.openbravo.retail.posterminal/js/model/orderfilter.js
+++ web/org.openbravo.retail.posterminal/js/model/orderfilter.js
@@ -87,6 +87,13 @@
       }
     },
     {
+      name: 'externalBusinessPartnerReference',
+      column: 'externalBusinessPartnerReference',
+      filter: false,
+      type: 'TEXT',
+      caption: 'OBPOS_LblCustomer'
+    },
+    {
       name: 'orderDateFrom',
       column: 'orderDate',
       filter: true,
