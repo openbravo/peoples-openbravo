openbravo/peoples-openbravo

commit 6c615e5b4cc4b1459146fc36f6b77a1224eab9d5

[extbp] filter is changed to column extbpreference only when businesspartner is involved in query

web/org.openbravo.retail.posterminal/js/utils/externalBpUtils.js
@@ -30,10 +30,10 @@ OB.UTIL.modalCustomer = function() {
 };
 
 OB.UTIL.filterColumn = function(fullFlt) {
-  if (OB.UTIL.externalBp()) {
+  if (fullFlt.column === 'businessPartner' && OB.UTIL.externalBp()) {
     return 'externalBusinessPartnerReference';
   } else {
-    return fullFlt.name;
+    return fullFlt.column;
   }
 };
 

commit 6c08b28b5463736670bb58cf962c7dc635a97c9e

[extbps] revert wrong change introduced in 6c615e5b4cc4b1459146fc36f6b77a1224eab9d5

web/org.openbravo.retail.posterminal/js/utils/externalBpUtils.js
@@ -33,7 +33,7 @@ OB.UTIL.filterColumn = function(fullFlt) {
   if (fullFlt.column === 'businessPartner' && OB.UTIL.externalBp()) {
     return 'externalBusinessPartnerReference';
   } else {
-    return fullFlt.column;
+    return fullFlt.name;
   }
 };
 