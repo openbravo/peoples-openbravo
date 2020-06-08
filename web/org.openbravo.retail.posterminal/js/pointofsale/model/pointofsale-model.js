57fb0766709ae0898c8eece6f02cd1c79af79e40
Gorka Gil <gorka.gil@openbravo.com>
2020-06-08 17:34:32 +0200

    [indexedDB-state-cashup] fix error in the merge, remove models that were already removed

--- web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/model/pointofsale-model.js
@@ -15,12 +15,7 @@ OB.OBPOSPointOfSale.UI = OB.OBPOSPointOfSale.UI || {};
 
 //Window model
 OB.OBPOSPointOfSale.Model.PointOfSale = OB.Model.TerminalWindowModel.extend({
-  models: [
-    OB.Model.Order,
-    OB.Model.CancelLayaway,
-    OB.Model.CurrencyPanel,
-    OB.Model.OfflinePrinter
-  ],
+  models: [OB.Model.Order, OB.Model.CancelLayaway],
 
   loadUnpaidOrders: function(loadUnpaidOrdersCallback) {
     // Shows a modal window with the orders pending to be paid
