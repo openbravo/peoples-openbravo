7c82df743eeb7f971ec94c61875852d0f8f20112
Ranjith S R <ranjith@qualiantech.com>
2020-07-02 17:01:05 +0530

    Fixed issue-44372: Updated Invoice Address to Customer when assigning to the receipt

--- web/org.openbravo.retail.posterminal/js/pointofsale/view/subwindows/customeraddress/customeraddrdetailview.js
+++ web/org.openbravo.retail.posterminal/js/pointofsale/view/subwindows/customeraddress/customeraddrdetailview.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2019 Openbravo S.L.U.
+ * Copyright (C) 2012-2020 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -291,6 +291,11 @@ enyo.kind({
     this.customer.set('cityName', this.customerAddr.get('cityName'));
     this.customer.set('locationModel', this.customerAddr);
     this.customer.set('countryName', this.customerAddr.get('countryName'));
+    this.customer.set('locationBillModel', this.customerAddr);
+    if (OB.UTIL.isNullOrUndefined(this.customer.get('locations'))) {
+      this.customer.set('locations', []);
+    }
+    this.customer.get('locations').push(this.customerAddr.clone());
     this.model.get('order').trigger('change:bp', this.model.get('order'));
     this.doChangeBusinessPartner({
       businessPartner: this.customer,
