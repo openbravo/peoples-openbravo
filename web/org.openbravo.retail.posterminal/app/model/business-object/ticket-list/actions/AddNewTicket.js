0208be5902be6e405538732264c6d578042bf789
Carlos Aristu <carlos.aristu@openbravo.com>
2020-07-27 13:40:52 +0200

    [ticket-list] use OB.App.TerminalProperty

--- web/org.openbravo.retail.posterminal/app/model/business-object/ticket-list/actions/AddNewTicket.js
+++ web/org.openbravo.retail.posterminal/app/model/business-object/ticket-list/actions/AddNewTicket.js
@@ -40,13 +40,13 @@ OB.App.StateAPI.Global.addNewTicket.addActionPreparation(
   async (state, payload) => {
     const newPayload = { ...payload };
     newPayload.businessPartner = JSON.parse(
-      JSON.stringify(OB.MobileApp.model.get('businessPartner'))
+      JSON.stringify(OB.App.TerminalProperty.get('businessPartner'))
     );
-    newPayload.terminal = OB.MobileApp.model.get('terminal');
-    newPayload.session = OB.MobileApp.model.get('session');
-    newPayload.orgUserId = OB.MobileApp.model.get('orgUserId');
-    newPayload.pricelist = OB.MobileApp.model.get('pricelist');
-    newPayload.contextUser = OB.MobileApp.model.get('context').user;
+    newPayload.terminal = OB.App.TerminalProperty.get('terminal');
+    newPayload.session = OB.App.TerminalProperty.get('session');
+    newPayload.orgUserId = OB.App.TerminalProperty.get('orgUserId');
+    newPayload.pricelist = OB.App.TerminalProperty.get('pricelist');
+    newPayload.contextUser = OB.App.TerminalProperty.get('context').user;
 
     return newPayload;
   }
