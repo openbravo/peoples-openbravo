d42c611dc294b3a3d807f781e04742cd689f19b5
Carlos Aristu <carlos.aristu@openbravo.com>
2020-08-18 17:12:50 +0200

    [ticket-list] add jsdoc

--- web/org.openbravo.retail.posterminal/app/model/business-object/ticket-list/TicketListUtils.js
+++ web/org.openbravo.retail.posterminal/app/model/business-object/ticket-list/TicketListUtils.js
@@ -9,6 +9,17 @@
 
 (function TicketListUtilsDefinition() {
   OB.App.StateAPI.TicketList.registerUtilityFunctions({
+    /**
+     * Removes a ticket from the list
+     *
+     * @param {object[]} ticketList - The ticket list
+     * @param {object} ticketList - The ticket to remove
+     * @param {object} payload - Additional information
+     *
+     * @returns {object} - An object with the following properties:
+     *                   * ticketList: the resulting ticket list after removing the ticket
+     *                   * ticket: the new current ticket in case former current ticket should be replaced
+     */
     removeTicket(ticketList, ticket, payload) {
       const multiTicketListIds = (payload.multiTicketList || []).map(t => t.id);
       let newTicket = { ...ticket };
@@ -39,6 +50,12 @@
       return { ticketList: newTicketList, ticket: newTicket };
     },
 
+    /**
+     * Retrieves all the tickets of the ticket list.
+     * The list includes the current ticket which is returned at the first position of the list
+     *
+     * @returns {object[]} all the tickets in the ticket list
+     */
     getAllTickets() {
       const currentTicket = OB.App.State.getState().Ticket;
       if (currentTicket && Object.keys(currentTicket).length !== 0) {
@@ -47,12 +64,26 @@
       return [...OB.App.State.getState().TicketList];
     },
 
+    /**
+     * Retrieves all the tickets of the ticket list that belong to the provided session
+     *
+     * @param {string} session - The session ID
+     *
+     * @returns {object[]} the tickets that belong to the provided session
+     */
     getSessionTickets(session) {
       return OB.App.State.TicketList.Utils.getAllTickets().filter(
         ticket => ticket.session === session
       );
     },
 
+    /**
+     * Retrieves all the tickets of the ticket list that belong to a session different from the provided one
+     *
+     * @param {string} session - The session ID
+     *
+     * @returns {object[]} the tickets that do not belong to the provided session
+     */
     getOtherSessionsTickets(session) {
       return OB.App.State.TicketList.Utils.getAllTickets().filter(
         ticket => ticket.session !== session
