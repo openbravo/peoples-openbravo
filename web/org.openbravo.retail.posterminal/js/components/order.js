2ee8f45f5cabaff7efaf2e8475b7ee4a94ea5a54
Sandra Huguet <sandra.huguet@openbravo.com>
2020-09-01 14:04:28 +0200

    [ticket-delete] fix checkServicesToDelete

--- web/org.openbravo.retail.posterminal/js/components/order.js
+++ web/org.openbravo.retail.posterminal/js/components/order.js
@@ -663,78 +663,7 @@ enyo.kind({
   processesToListen: ['calculateReceipt'],
   processStarted: function() {},
   checkServicesToDelete: function() {
-    var removedServices = [],
-      servicesToBeDeleted = [];
-    removedServices.push(OB.I18N.getLabel('OBPOS_ServiceRemoved'));
-    _.each(
-      OB.MobileApp.model.receipt.get('lines').models,
-      function(line) {
-        var trancheValues = [],
-          totalAmountSelected = 0,
-          minimumSelected = Infinity,
-          maximumSelected = 0,
-          uniqueQuantityServiceToBeDeleted,
-          asPerProductServiceToBeDeleted;
-
-        if (line.get('obposIsDeleted')) {
-          return;
-        }
-
-        if (line.has('relatedLines') && line.get('relatedLines').length > 0) {
-          _.each(
-            line.get('relatedLines'),
-            function(line2) {
-              if (
-                !line2.deferred &&
-                !line.get('originalOrderLineId') &&
-                OB.MobileApp.model.receipt.get('lines').get(line2.orderlineId)
-              ) {
-                line2 = OB.MobileApp.model.receipt
-                  .get('lines')
-                  .get(line2.orderlineId).attributes;
-              }
-              trancheValues = OB.UI.SearchServicesFilter.prototype.calculateTranche(
-                line2,
-                trancheValues
-              );
-            },
-            this
-          );
-          totalAmountSelected = trancheValues[0];
-          minimumSelected = trancheValues[1];
-          maximumSelected = trancheValues[2];
-          uniqueQuantityServiceToBeDeleted =
-            line.get('product').get('quantityRule') === 'UQ' &&
-            ((line.has('serviceTrancheMaximum') &&
-              totalAmountSelected > line.get('serviceTrancheMaximum')) ||
-              (line.has('serviceTrancheMinimum') &&
-                totalAmountSelected < line.get('serviceTrancheMinimum')));
-          asPerProductServiceToBeDeleted =
-            line.get('product').get('quantityRule') === 'PP' &&
-            ((line.has('serviceTrancheMaximum') &&
-              maximumSelected > line.get('serviceTrancheMaximum')) ||
-              (line.has('serviceTrancheMinimum') &&
-                minimumSelected < line.get('serviceTrancheMinimum')));
-          if (
-            (!line.has('deliveredQuantity') ||
-              line.get('deliveredQuantity') <= 0) &&
-            (uniqueQuantityServiceToBeDeleted || asPerProductServiceToBeDeleted)
-          ) {
-            servicesToBeDeleted.push(line);
-            removedServices.push(line.get('product').get('_identifier'));
-          }
-        }
-      },
-      this
-    );
-    if (servicesToBeDeleted.length > 0) {
-      OB.MobileApp.model.receipt.deleteLinesFromOrder(servicesToBeDeleted);
-      OB.MobileApp.model.receipt.set('undo', null);
-      OB.UTIL.showConfirmation.display(
-        OB.I18N.getLabel('OBPOS_ServiceRemovedHeader'),
-        removedServices
-      );
-    }
+    OB.UTIL.checkServicesToDelete();
   },
   processFinished: function(process, execution, processesInExec) {
     this.checkServicesToDelete();
--- web/org.openbravo.retail.posterminal/js/utils/servicesUtils.js
+++ web/org.openbravo.retail.posterminal/js/utils/servicesUtils.js
@@ -123,8 +123,74 @@ OB.UTIL.servicesFilter = async function(
 };
 
 OB.UTIL.handlePriceRuleBasedServices = function(order) {
-  OB.MobileApp.view.$.containerWindow
-    .getRoot()
-    .$.multiColumn.$.leftPanel.$.receiptview.$.orderview.checkServicesToDelete();
+  OB.UTIL.checkServicesToDelete();
   order.trigger('updateServicePrices');
+};
+
+OB.UTIL.checkServicesToDelete = function() {
+  var removedServices = [],
+    servicesToBeDeleted = [];
+  removedServices.push(OB.I18N.getLabel('OBPOS_ServiceRemoved'));
+
+  OB.MobileApp.model.receipt.get('lines').models.forEach(line => {
+    var trancheValues = [],
+      totalAmountSelected = 0,
+      minimumSelected = Infinity,
+      maximumSelected = 0,
+      uniqueQuantityServiceToBeDeleted,
+      asPerProductServiceToBeDeleted;
+
+    if (line.get('obposIsDeleted')) {
+      return;
+    }
+
+    if (line.has('relatedLines') && line.get('relatedLines').length > 0) {
+      line.get('relatedLines').forEach(line2 => {
+        if (
+          !line2.deferred &&
+          !line.get('originalOrderLineId') &&
+          OB.MobileApp.model.receipt.get('lines').get(line2.orderlineId)
+        ) {
+          line2 = OB.MobileApp.model.receipt.get('lines').get(line2.orderlineId)
+            .attributes;
+        }
+        trancheValues = OB.UI.SearchServicesFilter.prototype.calculateTranche(
+          line2,
+          trancheValues
+        );
+      });
+
+      totalAmountSelected = trancheValues[0];
+      minimumSelected = trancheValues[1];
+      maximumSelected = trancheValues[2];
+      uniqueQuantityServiceToBeDeleted =
+        line.get('product').get('quantityRule') === 'UQ' &&
+        ((line.has('serviceTrancheMaximum') &&
+          totalAmountSelected > line.get('serviceTrancheMaximum')) ||
+          (line.has('serviceTrancheMinimum') &&
+            totalAmountSelected < line.get('serviceTrancheMinimum')));
+      asPerProductServiceToBeDeleted =
+        line.get('product').get('quantityRule') === 'PP' &&
+        ((line.has('serviceTrancheMaximum') &&
+          maximumSelected > line.get('serviceTrancheMaximum')) ||
+          (line.has('serviceTrancheMinimum') &&
+            minimumSelected < line.get('serviceTrancheMinimum')));
+      if (
+        (!line.has('deliveredQuantity') ||
+          line.get('deliveredQuantity') <= 0) &&
+        (uniqueQuantityServiceToBeDeleted || asPerProductServiceToBeDeleted)
+      ) {
+        servicesToBeDeleted.push(line);
+        removedServices.push(line.get('product').get('_identifier'));
+      }
+    }
+  });
+  if (servicesToBeDeleted.length > 0) {
+    OB.MobileApp.model.receipt.deleteLinesFromOrder(servicesToBeDeleted);
+    OB.MobileApp.model.receipt.set('undo', null);
+    OB.UTIL.showConfirmation.display(
+      OB.I18N.getLabel('OBPOS_ServiceRemovedHeader'),
+      removedServices
+    );
+  }
 };
