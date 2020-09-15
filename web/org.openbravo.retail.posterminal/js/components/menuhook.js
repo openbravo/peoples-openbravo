14433c8c536955a569eacd7868152ee7eb12c3c1
Ranjith S R <ranjith@qualiantech.com>
2020-09-15 16:42:03 +0530

    Related to ISSUE-44907: Hook PostWindowNavigate callback should be called * In Hook PostWindowNavigate, MasterData should be checked if user loggedin in online mode

--- web/org.openbravo.retail.posterminal/js/components/menuhook.js
+++ web/org.openbravo.retail.posterminal/js/components/menuhook.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2014-2019 Openbravo S.L.U.
+ * Copyright (C) 2014-2020 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -35,5 +35,8 @@ OB.UTIL.HookManager.registerHook('OBMOBC_PostWindowNavigate', function(
   callbacks
 ) {
   // Refresh Master Data
-  OB.UTIL.checkRefreshMasterDataOnNavigate();
+  if (!OB.MobileApp.model.get('loggedOffline')) {
+    OB.UTIL.checkRefreshMasterDataOnNavigate();
+  }
+  OB.UTIL.HookManager.callbackExecutor(args, callbacks);
 });
