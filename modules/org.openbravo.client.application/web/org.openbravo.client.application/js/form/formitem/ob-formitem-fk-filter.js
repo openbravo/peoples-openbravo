ad087664d113f758c3e007508f3e22183a383e61
Cristian Berner <cristian.berner@openbravo.com>
2020-10-02 14:44:28 +0200

    Fixes ISSUE-45177: filter field hover not escaping String
    Filter field in grid view was not escaping the sequence of chars. It was
    using the provided String as is. This fixes it by escaping the String
    provided, similar to how Cell hover tooltip in grid is handled.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-fk-filter.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-fk-filter.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2011-2019 Openbravo SLU
+ * All portions are Copyright (C) 2011-2020 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -376,7 +376,7 @@ isc.OBFKFilterTextItem.addProperties({
   },
 
   itemHoverHTML: function() {
-    return this.getElementValue();
+    return this.getElementValue().asHTML();
   },
 
   mapValueToDisplay: function(value) {
