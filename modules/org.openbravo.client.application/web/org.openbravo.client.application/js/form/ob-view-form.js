71477140859701da7bbaf84a8491dd3dcff7349d
Cristian Berner <cristian.berner@openbravo.com>
2020-10-27 18:07:06 +0100

    Fixes ISSUE-45332: Record can be saved with missing mandatory field data
    If a mandatory field in a collapsed field group had no value or invalid
    values, the record could be saved by the auto-save when changing focus
    to a child tab.
    
    The approach taken here is to also validate hiddenFields that are not
    present because of being in a collapsed field group.
    
    form.validate method can be found in smartclient DynamicForm and it
    accepts as first argument a boolean indicating to validate or no hidden
    fields, it is now set to true on ob-view-form, fixing this issue.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/ob-view-form.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/ob-view-form.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2019 Openbravo SLU
+ * All portions are Copyright (C) 2010-2020 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -2263,7 +2263,8 @@ OB.ViewFormProperties = {
 
   validateForm: function() {
     var form = this;
-    if (!form.validate()) {
+    const validateHiddenFields = true;
+    if (!form.validate(validateHiddenFields)) {
       form.handleFieldErrors(null);
       form.view.standardWindow.autoSaveDone(form.view, false);
       form.isSaving = false;
