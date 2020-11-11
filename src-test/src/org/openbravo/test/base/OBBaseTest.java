ec48f7cfe1b6f5dfefab7101c3d97393400882ee
Stefan Huehner <stefan.huehner@openbravo.com>
2020-11-11 18:35:04 +0100

    Fixes ISSUE-45429: Improve unbalanced adminMode warnings in OBBaseTest.
    OBBaseTest did already check if a test-case left adminMode enabled
    instead of handling it properly.
    However that detection was not integration with the existing OBContext
    code for doing the same logic at the end of an http-request.
    
    Reuse that existing code allows to reuse the debugging ADMIN_TRACE_SIZE
    logic from OBContext also to debug Junit test issues.

--- src-test/src/org/openbravo/test/base/OBBaseTest.java
+++ src-test/src/org/openbravo/test/base/OBBaseTest.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2014-2019 Openbravo SLU 
+ * All portions are Copyright (C) 2014-2020 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -139,6 +139,7 @@ public class OBBaseTest {
           && !OBContext.getOBContext().getUser().getId().equals("0")
           && !OBContext.getOBContext().getRole().getId().equals("0")
           && OBContext.getOBContext().isInAdministratorMode()) {
+        OBContext.clearAdminModeStack();
         OBContext.restorePreviousMode();
         throw new IllegalStateException(
             "Test case should take care of reseting admin mode correctly in a finally block, use OBContext.restorePreviousMode");
