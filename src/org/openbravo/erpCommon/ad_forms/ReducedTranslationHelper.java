8daa8e0b80ea9c77e7878232d5c21fbe196b8bde
Atul Gaware <atul.gaware@openbravo.com>
2020-11-17 22:17:01 +0530

    [TRL] Avoid dupplicate code of method getLinkToWindowsAvailableForReducedTranslationFromADColumn
    **Using empty string "" as extraWhereClause parameter

--- src/org/openbravo/erpCommon/ad_forms/ReducedTranslationHelper.java
+++ src/org/openbravo/erpCommon/ad_forms/ReducedTranslationHelper.java
@@ -65,7 +65,7 @@ class ReducedTranslationHelper {
         break;
       case "AD_ELEMENT":
                // Window directly or indirectly linked to menu entry with ad_element
-        sql += " AND (EXISTS " + getLinkToWindowsAvailableForReducedTranslationFromADColumn("o.AD_ELEMENT_ID", "AD_ELEMENT_ID")
+        sql += " AND (EXISTS " + getLinkToWindowsAvailableForReducedTranslationFromADColumn("o.AD_ELEMENT_ID", "AD_ELEMENT_ID", "")
                // Process Definition Param with an ad_element
              + "    OR EXISTS " + getLinkToProcessesAvailableForReducedTranslationFromProcessParam(ProcessParamType.OBUIAPP_PARAMETER, "o.AD_ELEMENT_ID", "AD_ELEMENT_ID")
                // Process Param with an ad_element
@@ -87,7 +87,7 @@ class ReducedTranslationHelper {
              + "                WHERE o.AD_Process_ID = m.AD_Process_ID "
              + "                AND m.translation_Strategy is null) "
                 // Indirect menu entry through a process in another window
-             + "      OR EXISTS " + getLinkToWindowsAvailableForReducedTranslationFromADColumn("o.AD_Process_ID", "AD_Process_ID")
+             + "      OR EXISTS " + getLinkToWindowsAvailableForReducedTranslationFromADColumn("o.AD_Process_ID", "AD_Process_ID", "")
              + "     ) ";
         break;
       case "OBUIAPP_PROCESS":
@@ -98,7 +98,7 @@ class ReducedTranslationHelper {
              + "                WHERE o.OBUIAPP_PROCESS_ID = m.EM_OBUIAPP_PROCESS_ID "
              + "                AND m.translation_Strategy is null) "
              // Indirect menu entry through a process definition in another window
-             + "      OR EXISTS " + getLinkToWindowsAvailableForReducedTranslationFromADColumn("o.OBUIAPP_PROCESS_ID", "EM_OBUIAPP_PROCESS_ID")
+             + "      OR EXISTS " + getLinkToWindowsAvailableForReducedTranslationFromADColumn("o.OBUIAPP_PROCESS_ID", "EM_OBUIAPP_PROCESS_ID", "")
              + "     ) ";
         break;
       case "AD_MENU":
@@ -177,25 +177,6 @@ class ReducedTranslationHelper {
    * translation from Column -> Field -> Tab -> Window.
    */
   private static String getLinkToWindowsAvailableForReducedTranslationFromADColumn(
-      final String referencedColumnToExternalQuery, final String linkThroughADColumn) {
-    //@formatter:off
-    return " (SELECT 1 "
-         + "  FROM AD_COLUMN c, "
-         + "  AD_FIELD f, "
-         + "  AD_TAB t "
-         + "  WHERE c.AD_COLUMN_ID = f.AD_COLUMN_ID "
-         + "  AND f.AD_TAB_ID = t.AD_TAB_ID "
-         + "  AND " + referencedColumnToExternalQuery + " = c." + linkThroughADColumn
-         + "  AND " + getRecordsInWindowsAvailableForReducedTranslation("t") + " "
-         + " ) ";
-    //@formatter:on
-  }
-
-  /*
-   * Utility method that tries to find a (direct or indirect) window available for reduced
-   * translation from Column -> Field -> Tab -> Window.
-   */
-  private static String getLinkToWindowsAvailableForReducedTranslationFromADColumn(
       final String referencedColumnToExternalQuery, final String linkThroughADColumn,
       final String extraWhereClause) {
     //@formatter:off
@@ -229,7 +210,7 @@ class ReducedTranslationHelper {
          + "                  WHERE m." + processParamType.linkToMenuColum + " = pp." + processParamType.processPrimaryKeyColumn
          + "                  AND  m.translation_Strategy is null) "
                    // ...indirectly linked to a window menu entry
-         + "       OR EXISTS " + getLinkToWindowsAvailableForReducedTranslationFromADColumn("pp." + processParamType.processPrimaryKeyColumn, processParamType.linkToMenuColum)
+         + "       OR EXISTS " + getLinkToWindowsAvailableForReducedTranslationFromADColumn("pp." + processParamType.processPrimaryKeyColumn, processParamType.linkToMenuColum, "")
          + "      ) "
          + " ) ";
     //@formatter:on
