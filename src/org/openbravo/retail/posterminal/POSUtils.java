336a1fcac948f2eb481716ebc3973247b764cede
Rafael Queralta <rafaelcuba81@gmail.com>
2020-12-12 21:46:28 -0500

    Fixed ISSUE-45518: Added two more filters by terminal and order payment method to The query to obtain the default payment method

--- src/org/openbravo/retail/posterminal/POSUtils.java
+++ src/org/openbravo/retail/posterminal/POSUtils.java
@@ -561,10 +561,12 @@ public class POSUtils {
     try {
       OBQuery<OBPOSAppPayment> paymentQuery = OBDal.getInstance()
           .createQuery(OBPOSAppPayment.class,
-              "as e where e.obposApplications.organization = :organization and e.financialAccount.currency = :currency order by e.id");
+              "as e where e.obposApplications.organization = :organization and e.obposApplications.id = :terminal and e.paymentMethod.paymentMethod.id = :paymentMethod and e.financialAccount.currency = :currency order by e.id");
       Organization organization = OBDal.getInstance()
           .get(Organization.class, jsonorder.getString("organization"));
       paymentQuery.setNamedParameter("organization", organization);
+      paymentQuery.setNamedParameter("terminal", order.getObposApplications().getId());
+      paymentQuery.setNamedParameter("paymentMethod", order.getPaymentMethod().getId());
       paymentQuery.setNamedParameter("currency", order.getOrganization().getCurrency());
       paymentQuery.setFilterOnReadableOrganization(false);
       paymentQuery.setMaxResult(1);
