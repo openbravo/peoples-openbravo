7dd2eeeea0be3b689e9fff98aacce0047c23cda0
Álvaro Ferraz <alvaro.ferraz@openbravo.com>
2020-12-23 10:02:07 +0100

    [loadTicket] Fix LoadTicketById test

--- web-test/model/business-object/ticket-list/LoadTicketById.test.js
+++ web-test/model/business-object/ticket-list/LoadTicketById.test.js
@@ -12,61 +12,9 @@ global.OB = { App: { Class: {} } };
 const deepfreeze = require('deepfreeze');
 require('../../../../../org.openbravo.mobile.core/web/org.openbravo.mobile.core/app/model/application-state/StateAPI');
 require('../../../../../org.openbravo.mobile.core/web/org.openbravo.mobile.core/app/model/application-state/ActionCanceled');
-require('../../../../web/org.openbravo.retail.posterminal/app/model/business-object/ticket-list/actions/LoadTicket');
+require('../../../../web/org.openbravo.retail.posterminal/app/model/business-object/ticket-list/actions/LoadTicketById');
 
 describe('Load Ticket actions', () => {
-  describe('Load Ticket using a Ticket object', () => {
-    let action;
-    beforeEach(() => {
-      action = OB.App.StateAPI.Global.loadTicket;
-    });
-
-    it('does not modify the state when ticket to load is the current ticket', () => {
-      const state = {
-        Ticket: { id: 'A' },
-        TicketList: []
-      };
-      const payload = {
-        ticket: { id: 'A' }
-      };
-      deepfreeze(state);
-
-      const newState = action(state, payload);
-      expect(newState).toEqual(state);
-    });
-
-    it('loads the ticket passed as payload and enqueues the current ticket', () => {
-      const state = {
-        Ticket: { id: 'A' },
-        TicketList: [{ id: 'B' }]
-      };
-      const payload = {
-        ticket: { id: 'B' }
-      };
-      deepfreeze(state);
-      const newState = action(state, payload);
-      expect(newState).toEqual({
-        Ticket: { id: 'B' },
-        TicketList: [{ id: 'A' }]
-      });
-    });
-
-    it('throws an error if provided ticket is not valid', () => {
-      const state = {
-        Ticket: { id: 'A' },
-        TicketList: [{ id: 'B' }]
-      };
-      const payload = {
-        ticket: { id: 'C' }
-      };
-      deepfreeze(state);
-
-      expect(() => {
-        action(state, payload);
-      }).toThrow();
-    });
-  });
-
   describe('Load Ticket by Id action', () => {
     let action;
     beforeEach(() => {
