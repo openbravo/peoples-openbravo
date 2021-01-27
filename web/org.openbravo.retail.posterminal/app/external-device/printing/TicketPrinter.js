118585fc69e1ddfa337f4b8793c1d4709a30ebbd
Carlos Aristu <carlos.aristu@openbravo.com>
2021-01-27 11:19:48 +0100

    [new-print-engine] move helper functions towards the end

--- web/org.openbravo.retail.posterminal/app/external-device/printing/TicketPrinter.js
+++ web/org.openbravo.retail.posterminal/app/external-device/printing/TicketPrinter.js
@@ -13,134 +13,6 @@
  */
 
 (function TicketPrinterDefinition() {
-  // determines if a ticket line should be printed or not
-  function isPrintable(line) {
-    const { product, baseNetUnitAmount, baseGrossUnitAmount } = line;
-    return (
-      product.productType !== 'S' ||
-      product.isPrintServices ||
-      baseNetUnitAmount ||
-      baseGrossUnitAmount
-    );
-  }
-
-  // generates a printable version of the provided ticket
-  // it also returns the backbone object of the printable version used for backward compatibility
-  function toPrintable(ticket) {
-    const printableTicket = { ...ticket };
-    if (!(printableTicket.orderDate instanceof Date)) {
-      printableTicket.orderDate = new Date(printableTicket.orderDate);
-    }
-
-    printableTicket.lines = printableTicket.lines.filter(line =>
-      isPrintable(line)
-    );
-
-    if (OB.App.State.Ticket.Utils.isNegative(printableTicket)) {
-      printableTicket.payments = printableTicket.payments.map(payment => {
-        if (!payment.isPrePayment && !payment.isReversePayment) {
-          return {
-            ...payment,
-            amount: -Math.abs(payment.amount),
-            origAmount: -Math.abs(payment.origAmount)
-          };
-        }
-        return payment;
-      });
-    }
-
-    if (printableTicket.canceledorder) {
-      delete printableTicket.canceledorder;
-    }
-
-    if (OB.App.StateBackwardCompatibility != null) {
-      const printableOrder = OB.App.StateBackwardCompatibility.getInstance(
-        'Ticket'
-      ).toBackboneObject(printableTicket);
-      return { printableTicket, printableOrder };
-    }
-
-    return { printableTicket };
-  }
-
-  // Include over-payment amount to total paid amount from change amount
-  function includeOverPaymentAmt(ticket) {
-    const newTicket = { ...ticket };
-
-    newTicket.payments = newTicket.payments.map(payment => {
-      const { paymentData } = payment;
-      const overPaymentAmounts = { amount: 0, origAmount: 0 };
-
-      if (paymentData && paymentData.key === payment.kind) {
-        // Do not print over-payment amount for CancelAndReplace's old-payment and MultiTicket payments
-        if (!ticket.doCancelAndReplace && !paymentData.isMultiTicketPayment) {
-          overPaymentAmounts.amount = isNumeric(paymentData.amount)
-            ? paymentData.amount
-            : 0;
-          overPaymentAmounts.origAmount = isNumeric(paymentData.origAmount)
-            ? paymentData.origAmount
-            : 0;
-        }
-      } else if (ticket.changePayments && ticket.changePayments.length > 0) {
-        // Do not print over-payment amount for MultiTicket payments
-        const changePayments = ticket.changePayments.filter(
-          chngpayment =>
-            chngpayment.key === payment.kind &&
-            !chngpayment.isMultiTicketPayment
-        );
-        const changeAmountPayment = changePayments.find(p =>
-          isNumeric(p.amount)
-        );
-        overPaymentAmounts.amount = changeAmountPayment
-          ? changeAmountPayment.amount
-          : 0;
-        const changeOrigAmountPayment = changePayments.find(p =>
-          isNumeric(p.origAmount)
-        );
-        overPaymentAmounts.origAmount = changeOrigAmountPayment
-          ? changeOrigAmountPayment.origAmount
-          : 0;
-      }
-
-      if (overPaymentAmounts.amount || overPaymentAmounts.origAmount) {
-        const { amount, origAmount } = overPaymentAmounts;
-        const newPayment = { ...payment };
-        newPayment.amount += ticket.isNegative ? -amount : amount;
-        newPayment.origAmount += ticket.isNegative ? -origAmount : origAmount;
-        return newPayment;
-      }
-
-      return payment;
-    });
-
-    return newTicket;
-  }
-
-  // checks whether a value is a numeric value
-  function isNumeric(value) {
-    return !Number.isNaN(Number(value));
-  }
-
-  // checks if a ticket should be printed twice. It should be printed twice when:
-  // - it is a return and the corresponding preference is enabled
-  // - or when one of the payment methods has the "printtwice" flag enabled
-  function shouldPrintTwice(ticket) {
-    const negativeLines = ticket.lines.filter(line => line.qty < 0);
-    const hasNegativeLines =
-      negativeLines.length === ticket.lines.length ||
-      (negativeLines.length > 0 &&
-        OB.App.Security.hasPermission(
-          'OBPOS_SalesWithOneLineNegativeAsReturns'
-        ));
-
-    return (
-      ((ticket.orderType === 1 || hasNegativeLines) &&
-        ticket.lines.length > 0 &&
-        !OB.App.Security.hasPermission('OBPOS_print.once')) ||
-      ticket.payments.some(payment => payment.printtwice)
-    );
-  }
-
   /**
    * This is an internal class used by the HardwareManagerEndpoint when consumming some of its messages.
    * It contains the logic to print ticket information through external devices.
@@ -367,4 +239,132 @@
   }
 
   OB.App.Class.TicketPrinter = TicketPrinter;
+
+  // determines if a ticket line should be printed or not
+  function isPrintable(line) {
+    const { product, baseNetUnitAmount, baseGrossUnitAmount } = line;
+    return (
+      product.productType !== 'S' ||
+      product.isPrintServices ||
+      baseNetUnitAmount ||
+      baseGrossUnitAmount
+    );
+  }
+
+  // generates a printable version of the provided ticket
+  // it also returns the backbone object of the printable version used for backward compatibility
+  function toPrintable(ticket) {
+    const printableTicket = { ...ticket };
+    if (!(printableTicket.orderDate instanceof Date)) {
+      printableTicket.orderDate = new Date(printableTicket.orderDate);
+    }
+
+    printableTicket.lines = printableTicket.lines.filter(line =>
+      isPrintable(line)
+    );
+
+    if (OB.App.State.Ticket.Utils.isNegative(printableTicket)) {
+      printableTicket.payments = printableTicket.payments.map(payment => {
+        if (!payment.isPrePayment && !payment.isReversePayment) {
+          return {
+            ...payment,
+            amount: -Math.abs(payment.amount),
+            origAmount: -Math.abs(payment.origAmount)
+          };
+        }
+        return payment;
+      });
+    }
+
+    if (printableTicket.canceledorder) {
+      delete printableTicket.canceledorder;
+    }
+
+    if (OB.App.StateBackwardCompatibility != null) {
+      const printableOrder = OB.App.StateBackwardCompatibility.getInstance(
+        'Ticket'
+      ).toBackboneObject(printableTicket);
+      return { printableTicket, printableOrder };
+    }
+
+    return { printableTicket };
+  }
+
+  // Include over-payment amount to total paid amount from change amount
+  function includeOverPaymentAmt(ticket) {
+    const newTicket = { ...ticket };
+
+    newTicket.payments = newTicket.payments.map(payment => {
+      const { paymentData } = payment;
+      const overPaymentAmounts = { amount: 0, origAmount: 0 };
+
+      if (paymentData && paymentData.key === payment.kind) {
+        // Do not print over-payment amount for CancelAndReplace's old-payment and MultiTicket payments
+        if (!ticket.doCancelAndReplace && !paymentData.isMultiTicketPayment) {
+          overPaymentAmounts.amount = isNumeric(paymentData.amount)
+            ? paymentData.amount
+            : 0;
+          overPaymentAmounts.origAmount = isNumeric(paymentData.origAmount)
+            ? paymentData.origAmount
+            : 0;
+        }
+      } else if (ticket.changePayments && ticket.changePayments.length > 0) {
+        // Do not print over-payment amount for MultiTicket payments
+        const changePayments = ticket.changePayments.filter(
+          chngpayment =>
+            chngpayment.key === payment.kind &&
+            !chngpayment.isMultiTicketPayment
+        );
+        const changeAmountPayment = changePayments.find(p =>
+          isNumeric(p.amount)
+        );
+        overPaymentAmounts.amount = changeAmountPayment
+          ? changeAmountPayment.amount
+          : 0;
+        const changeOrigAmountPayment = changePayments.find(p =>
+          isNumeric(p.origAmount)
+        );
+        overPaymentAmounts.origAmount = changeOrigAmountPayment
+          ? changeOrigAmountPayment.origAmount
+          : 0;
+      }
+
+      if (overPaymentAmounts.amount || overPaymentAmounts.origAmount) {
+        const { amount, origAmount } = overPaymentAmounts;
+        const newPayment = { ...payment };
+        newPayment.amount += ticket.isNegative ? -amount : amount;
+        newPayment.origAmount += ticket.isNegative ? -origAmount : origAmount;
+        return newPayment;
+      }
+
+      return payment;
+    });
+
+    return newTicket;
+  }
+
+  // checks whether a value is a numeric value
+  function isNumeric(value) {
+    return !Number.isNaN(Number(value));
+  }
+
+  // checks if a ticket should be printed twice. It should be printed twice when:
+  // - it is a return and the corresponding preference is enabled
+  // - or when one of the payment methods has the "printtwice" flag enabled
+  function shouldPrintTwice(ticket) {
+    const negativeLines = ticket.lines.filter(line => line.qty < 0);
+    const hasNegativeLines =
+      negativeLines.length === ticket.lines.length ||
+      (negativeLines.length > 0 &&
+        OB.App.Security.hasPermission(
+          'OBPOS_SalesWithOneLineNegativeAsReturns'
+        ));
+
+    return (
+      ((ticket.orderType === 1 || hasNegativeLines) &&
+        ticket.lines.length > 0 &&
+        !OB.App.Security.hasPermission('OBPOS_print.once')) ||
+      ticket.payments.some(payment => payment.printtwice)
+    );
+  }
 })();
