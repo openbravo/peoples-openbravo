8c23cb8dcb78968634a24382981dd173b580f1d1
Ranjith S R <ranjith@qualiantech.com>
2021-01-31 21:20:38 +0530

    Fixed ISSUE-45809 : Added method to check the execution of Custom Validation

--- src/org/openbravo/retail/posterminal/CustomInitialValidation.java
+++ src/org/openbravo/retail/posterminal/CustomInitialValidation.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2016 Openbravo S.L.U.
+ * Copyright (C) 2016-2021 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -14,4 +14,17 @@ import org.codehaus.jettison.json.JSONException;
 public abstract class CustomInitialValidation {
 
   public abstract void validation(OBPOSApplications posTerminal) throws JSONException;
+
+  /**
+   * Returns a boolean value which will be used to execute the InitialValidation.
+   * 
+   * If return's true, InitialValidation will be executed and vice versa
+   *
+   * @param appName
+   *          an application name
+   * @return boolean value (by default "true")
+   */
+  public boolean executeForApplication(String appName) throws JSONException {
+    return true;
+  }
 }
--- src/org/openbravo/retail/posterminal/InitialValidations.java
+++ src/org/openbravo/retail/posterminal/InitialValidations.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2020 Openbravo S.L.U.
+ * Copyright (C) 2012-2021 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.hibernate.criterion.Restrictions;
@@ -34,7 +35,7 @@ public class InitialValidations {
   public static void validateTerminal(OBPOSApplications posTerminal, JSONObject jsonsent)
       throws JSONException {
 
-    validateTerminal(posTerminal);
+    validateTerminal(posTerminal, jsonsent.getJSONObject("parameters").optString("appName"));
 
     // Check POS Cache Cash up is processed
     final JSONObject jsonCashUp = jsonsent.getJSONObject("parameters").optJSONObject("cashUpId");
@@ -47,8 +48,8 @@ public class InitialValidations {
     }
   }
 
-  public static void validateTerminal(OBPOSApplications posTerminal) throws JSONException {
-
+  public static void validateTerminal(OBPOSApplications posTerminal, String appName)
+      throws JSONException {
     if (posTerminal == null || null == POSUtils.getTerminal(posTerminal.getSearchKey())) {
       throw new JSONException("OBPOS_TerminalNotFound");
     }
@@ -282,7 +283,9 @@ public class InitialValidations {
     }
 
     for (CustomInitialValidation customInitialValidation : getCustomInitialValidations()) {
-      customInitialValidation.validation(posTerminal);
+      if (StringUtils.isEmpty(appName) || customInitialValidation.executeForApplication(appName)) {
+        customInitialValidation.validation(posTerminal);
+      }
     }
 
   }
