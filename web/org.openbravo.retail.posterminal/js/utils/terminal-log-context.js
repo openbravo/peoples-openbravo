92d4f102c0e3789538dd4ea55c16bc7d9d9da9ee
Gorka Gil <gorka.gil@openbravo.com>
2021-02-02 20:30:33 +0100

    Related to ISSUE-45732: fix terminal log in awo
    Move back the logic of calculate context to mobile.core making option the ticketid
    And create a action preparation in pos to set that ticketid

--- web/org.openbravo.retail.posterminal/js/utils/terminal-log-context.js
+++ web/org.openbravo.retail.posterminal/js/utils/terminal-log-context.js
@@ -8,94 +8,39 @@
  */
 
 (function() {
-  OB.UTIL = window.OB.UTIL || {};
-  OB.UTIL.TerminalLog = window.OB.UTIL.TerminalLog || {};
-
-  /**
-   * Get the context of the log line
-   *
-   * The context is calculated from the local storage variable terminalLogContext,
-   * that contains the window in which we are, or the documentno in case a ticket is open
-   * also is added the content of local storage variable terminalLogContextPopUp, that
-   * contains the opened popups.
-   *
-   * @param {string} state - The current state
-   * @param {string} contextFromId - Optional, an array containing the id of a web element splited by '_'
-   * @returns {string} The context for the log line
-   */
-  OB.UTIL.TerminalLog.calculateContext = function(payload) {
-    const { state, orderid, contextFromId } = payload;
-    let context = [];
-    let terminalLogContextPopUp;
-    let contextString;
-    let currentContext;
-    if (OB.UTIL.isNullOrUndefined(orderid)) {
-      currentContext = state.context;
-    } else if (state.context === 'retail.pointofsale') {
-      currentContext = orderid;
-    } else {
-      currentContext = state.context;
-    }
-    const currentContextPopup = state.contextPopup;
-    context.push(currentContext);
-    if (contextFromId && currentContextPopup.length === 0) {
-      context.push(contextFromId);
-    } else {
-      terminalLogContextPopUp = currentContextPopup;
-      if (terminalLogContextPopUp && terminalLogContextPopUp.length > 0) {
-        context = context.concat(terminalLogContextPopUp);
-      }
-    }
-    contextString = context.join('  |  ');
-    if (contextString.length > 250) {
-      contextString = `${contextString.substring(1, 245)}[...]`;
-    }
-    return contextString;
-  };
+  /*
+  Add action preparation to add the ticket id to the payload,
+  this is only for the pos,
+  it will be executed later the action preparation of mobile.core
+  and will use the ticketid to calculate the contex.
+  The priority of these action preparation is set to -100 so it is
+  executed before the default one that is 100, that is the one that
+  have the mobile.core action preparation
+  */
+  async function addTicketIdToPayload(terminalLog, payload, options) {
+    const newPayload = { ...payload };
+    newPayload.ticketid = options.globalState.Ticket.id;
+    return newPayload;
+  }
 
   OB.App.StateAPI.TerminalLog.addKey.addActionPreparation(
-    async (terminalLog, payload, options) => {
-      const newPayload = { ...payload };
-      newPayload.context = OB.UTIL.TerminalLog.calculateContext({
-        state: terminalLog,
-        orderid: options.globalState.Ticket.id,
-        contextFromId: payload.contextFromId
-      });
-      return newPayload;
-    }
+    addTicketIdToPayload,
+    undefined,
+    -100
   );
-
   OB.App.StateAPI.TerminalLog.addButtonClick.addActionPreparation(
-    async (terminalLog, payload, options) => {
-      const newPayload = { ...payload };
-      newPayload.context = OB.UTIL.TerminalLog.calculateContext({
-        state: terminalLog,
-        orderid: options.globalState.Ticket.id,
-        contextFromId: payload.contextFromId
-      });
-      return newPayload;
-    }
+    addTicketIdToPayload,
+    undefined,
+    -100
   );
-
   OB.App.StateAPI.TerminalLog.addProcess.addActionPreparation(
-    async (terminalLog, payload, options) => {
-      const newPayload = { ...payload };
-      newPayload.context = OB.UTIL.TerminalLog.calculateContext({
-        state: terminalLog,
-        orderid: options.globalState.Ticket.id
-      });
-      return newPayload;
-    }
+    addTicketIdToPayload,
+    undefined,
+    -100
   );
-
   OB.App.StateAPI.TerminalLog.addLog.addActionPreparation(
-    async (terminalLog, payload, options) => {
-      const newPayload = { ...payload };
-      newPayload.context = OB.UTIL.TerminalLog.calculateContext({
-        state: terminalLog,
-        orderid: options.globalState.Ticket.id
-      });
-      return newPayload;
-    }
+    addTicketIdToPayload,
+    undefined,
+    -100
   );
 })();
