7eee9ea5efbbb1648651f32c3bbc76cf6be51e25
Carlos Aristu <carlos.aristu@openbravo.com>
2021-03-04 12:48:10 +0100

    [offline-transitions] do not attend resource requests

--- web/org.openbravo.retail.posterminal/app/integration/remote-server/HardwareManagerServer.js
+++ web/org.openbravo.retail.posterminal/app/integration/remote-server/HardwareManagerServer.js
@@ -17,10 +17,6 @@
     }
 
     isAttendedURL(url) {
-      if (url.includes('/res/')) {
-        // attend requests for printing templates
-        return true;
-      }
       if (!this.hardwareURLs) {
         this.hardwareURLs = getHardwareURLs();
       }
