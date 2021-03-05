1688e11ec406f326b9d90465807a3713d6b44222
Álvaro Ferraz <alvaro.ferraz@openbravo.com>
2021-03-05 14:11:00 +0100

    [verifiedReturns] Add needed validations to returnTicket

--- web/org.openbravo.retail.posterminal/app/model/business-object/ticket/actions/ReturnTicket.js
+++ web/org.openbravo.retail.posterminal/app/model/business-object/ticket/actions/ReturnTicket.js
@@ -18,6 +18,47 @@
     });
     newTicket = OB.App.State.Ticket.Utils.updateTicketType(newTicket, payload);
 
+    // FIXME: add service approval -> order.setOrderType()
+    // FIXME: check line.promotions.obdiscAllowinnegativelines -> order.returnLine()
+
     return newTicket;
   });
+
+  OB.App.StateAPI.Ticket.returnTicket.addActionPreparation(
+    async (ticket, payload) => {
+      checkTicketRestrictions(ticket);
+      ticket.lines.forEach(line => {
+        checkTicketLineRestrictions(ticket, line);
+      });
+      return payload;
+    }
+  );
+
+  function checkTicketRestrictions(ticket) {
+    checkIsEditable(ticket);
+  }
+
+  function checkIsEditable(ticket) {
+    if (ticket.isEditable === false) {
+      throw new OB.App.Class.ActionCanceled({
+        title: 'OBPOS_modalNoEditableHeader',
+        errorConfirmation: 'OBPOS_modalNoEditableBody'
+      });
+    }
+  }
+
+  function checkTicketLineRestrictions(ticket, line) {
+    checkReturnable(ticket, line);
+  }
+
+  function checkReturnable(ticket, line) {
+    if (!line.product.returnable) {
+      throw new OB.App.Class.ActionCanceled({
+        title: 'OBPOS_UnreturnableProduct',
+        errorConfirmation: 'OBPOS_UnreturnableProductMessage',
+        // eslint-disable-next-line no-underscore-dangle
+        messageParams: [line.product._identifier]
+      });
+    }
+  }
 })();
