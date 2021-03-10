e3f11ef65a06ba4395f316b6d81d55323fd1ecfe
Mark <markmm82@gmail.com>
2021-03-10 09:04:12 -0300

    Fixes BUG-45493: Flush inventory to persist in database and avoid error: Key (m_inventory_id)=(<INVENTORY_ID>) is not present in table "m_inventory".

--- src/org/openbravo/materialmgmt/InventoryCountProcess.java
+++ src/org/openbravo/materialmgmt/InventoryCountProcess.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2012-2020 Openbravo SLU
+ * All portions are Copyright (C) 2012-2021 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -83,6 +83,8 @@ public class InventoryCountProcess implements Process {
       }
       inventory.setProcessNow(true);
       OBDal.getInstance().save(inventory);
+      OBDal.getInstance().flush();
+      OBDal.getInstance().refresh(inventory);
       if (SessionHandler.isSessionHandlerPresent()) {
         SessionHandler.getInstance().commitAndStart();
       }
