a2dd1ac1e487935461cca44ec3e5a0c099c979ac
Ranjith S R <ranjith@qualiantech.com>
2021-04-08 14:45:11 +0530

    Fixed ISSUE-46188: Added Message in RoundingPaymentType Callout * When Rounding field in Payment method of Touchpoint type is checked, Message should be shown from callout that Count Payment in Cash Up field will be unchecked and hidden

--- src/org/openbravo/retail/posterminal/ad_callouts/PaymentRoundingCallout.java
+++ src/org/openbravo/retail/posterminal/ad_callouts/PaymentRoundingCallout.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2020 Openbravo S.L.U.
+ * Copyright (C) 2020-2021 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -12,6 +12,7 @@ import javax.servlet.ServletException;
 
 import org.apache.commons.lang.StringUtils;
 import org.openbravo.erpCommon.ad_callouts.SimpleCallout;
+import org.openbravo.erpCommon.utility.OBMessageUtils;
 
 /**
  * Callout used to uncheck the Count Payment in Cash Up when payment method is rounding to properly
@@ -27,6 +28,7 @@ public class PaymentRoundingCallout extends SimpleCallout {
     final String rounding = info.getStringParameter("inpisrounding");
     if (StringUtils.equals(rounding, "Y")) {
       info.addResult("inpcountpaymentincashup", false);
+      info.addResult("MESSAGE", OBMessageUtils.messageBD("OBPOS_TouchpointTypeRoundingChecked"));
     }
   }
 }
