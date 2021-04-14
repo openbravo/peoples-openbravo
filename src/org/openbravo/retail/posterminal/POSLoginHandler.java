ec822d5540cd3dd038270819573e348ec30e8d93
Ranjith S R <ranjith@qualiantech.com>
2021-04-14 13:04:36 +0530

    Fixed ISSUE-46258: Default Language should be User Client Language instead of English when defaults are not defined

--- src/org/openbravo/retail/posterminal/POSLoginHandler.java
+++ src/org/openbravo/retail/posterminal/POSLoginHandler.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2019 Openbravo S.L.U.
+ * Copyright (C) 2012-2021 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -301,6 +301,11 @@ public class POSLoginHandler extends MobileCoreLoginHandler {
         return role.getOBPOSDefaultPosLanguage();
       }
     }
+
+    if (user.getClient().getLanguage() != null) {
+      return user.getClient().getLanguage();
+    }
+
     return OBDal.getInstance()
         .createCriteria(Language.class)
         .add(Restrictions.eq(Language.PROPERTY_LANGUAGE, "en_US"))
