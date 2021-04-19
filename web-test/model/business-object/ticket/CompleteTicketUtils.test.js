2cfaf22ccb537b93ac41e6d87ed150438b327e4f
Álvaro Ferraz <alvaro.ferraz@openbravo.com>
2021-04-19 09:00:00 +0200

    [cancelOrder] Fix CompleteTicketUtils test

--- web-test/model/business-object/ticket/CompleteTicketUtils.test.js
+++ web-test/model/business-object/ticket/CompleteTicketUtils.test.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2020 Openbravo S.L.U.
+ * Copyright (C) 2020-2021 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -384,9 +384,8 @@ describe('Check Ticket Cancelled', () => {
     const ticket = {
       businessPartner: { id: 'BP' },
       grossAmount: 100,
-      canceledorder: {
-        lines: [line]
-      },
+      isPaid: true,
+      lines: [line],
       payments: [{ origAmount: 100 }]
     };
     const payload = { businessPartner: { id: 'BP' } };
