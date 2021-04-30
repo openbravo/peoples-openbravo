87070f4b18dbf790c1b3e284404aad2752d25fc6
Álvaro Ferraz <alvaro.ferraz@openbravo.com>
2021-04-30 13:46:57 +0200

    [replaceOrder] Add tests for createReplaceTicket state action

--- web-test/model/business-object/ticket/actions/CreateReplaceTicket.test.js
+++ web-test/model/business-object/ticket/actions/CreateReplaceTicket.test.js
@@ -14,61 +14,84 @@ const deepfreeze = require('deepfreeze');
 
 describe('createReplaceTicket', () => {
   it('replaces ticket', () => {
-    const globalState = deepfreeze({ Cashup: { id: 'C' } });
+    OB.App.UUID.generate = jest.fn().mockReturnValue('0');
+    OB.App.State.DocumentSequence = {
+      Utils: {
+        calculateReplaceDocumentNumber: jest
+          .fn()
+          .mockImplementation(documentNo => `${documentNo}-1`)
+      }
+    };
     const ticket = deepfreeze({
       id: '1',
       documentNo: 'O1',
-      grossAmount: 100,
-      payment: 100,
-      lines: [{ id: '1', qty: 1, promotions: [], product: {} }]
+      lines: [{ id: '1', qty: 1 }]
     });
-    const payload = deepfreeze({ terminal: { terminalType: {} } });
+    const payload = deepfreeze({ terminal: { terminalType: {} }, context: {} });
     const newTicket = OB.App.StateAPI.Ticket.createReplaceTicket(
       ticket,
-      payload,
-      { globalState }
+      payload
     );
     expect(newTicket).toMatchObject({
-      cancelLayaway: true,
       canceledorder: {
         id: '1',
         documentNo: 'O1',
-        grossAmount: 100,
-        lines: [{ id: '1', qty: 1, promotions: [], product: {} }],
-        payment: 100
+        lines: [{ id: '1', qty: 1 }]
       },
       creditAmount: 0,
-      documentNo: 'O1*R*',
+      replacedorder: '1',
+      documentNo: 'O1-1',
+      replacedorder_documentNo: 'O1',
+      negativeDocNo: 'O1*R*',
       documentType: undefined,
-      fromLayaway: undefined,
-      grossAmount: 100,
-      id: undefined,
-      isEditable: false,
-      isLayaway: false,
+      id: '0',
+      hasbeenpaid: 'N',
       isPaid: false,
+      isEditable: true,
+      doCancelAndReplace: true,
+      isLayaway: false,
+      orderType: undefined,
       lines: [
         {
-          id: undefined,
-          canceledLine: '1',
-          qty: -1,
-          deliveredQuantity: undefined,
+          id: '0',
+          replacedorderline: '1',
+          qty: 1,
           invoicedQuantity: undefined,
-          obposCanbedelivered: true,
-          obposIspaid: false,
-          promotions: [],
-          product: {}
+          obposCanbedelivered: false,
+          obposIspaid: false
         }
       ],
-      nettingPayment: 0,
-      obposAppCashup: 'C',
-      orderType: 3,
       paidOnCredit: false,
       paidPartiallyOnCredit: false,
-      payment: 100,
-      payments: [],
       posTerminal: undefined
     });
   });
+
+  test.each`
+    ticket                                                                           | layawayorder | result
+    ${{ orderType: 0, lines: [] }}                                                   | ${undefined} | ${{ orderType: 0 }}
+    ${{ orderType: 0, isLayaway: false, lines: [] }}                                 | ${undefined} | ${{ orderType: 0 }}
+    ${{ orderType: 0, isLayaway: true, lines: [] }}                                  | ${undefined} | ${{ orderType: 2 }}
+    ${{ orderType: 0, lines: [{ deliveredQuantity: 0 }] }}                           | ${false}     | ${{ orderType: 0 }}
+    ${{ orderType: 0, lines: [{ deliveredQuantity: 1 }] }}                           | ${false}     | ${{ orderType: 0 }}
+    ${{ orderType: 0, lines: [{ deliveredQuantity: 0 }] }}                           | ${true}      | ${{ orderType: 2 }}
+    ${{ orderType: 0, lines: [{ deliveredQuantity: 1 }] }}                           | ${true}      | ${{ orderType: 0 }}
+    ${{ orderType: 0, lines: [{ deliveredQuantity: 0 }, { deliveredQuantity: 0 }] }} | ${true}      | ${{ orderType: 2 }}
+    ${{ orderType: 0, lines: [{ deliveredQuantity: 0 }, { deliveredQuantity: 1 }] }} | ${true}      | ${{ orderType: 0 }}
+  `(
+    'should use layaway orderType if original ticket is a layaway',
+    ({ ticket, layawayorder, result }) => {
+      const payload = deepfreeze({
+        terminal: { terminalType: { layawayorder } },
+        context: {}
+      });
+      const newTicket = OB.App.StateAPI.Ticket.createReplaceTicket(
+        deepfreeze(ticket),
+        payload
+      );
+      expect(newTicket).toMatchObject(result);
+    }
+  );
 
   test.each`
     isCrossStore | result
@@ -80,7 +103,6 @@ describe('createReplaceTicket', () => {
       OB.App.State.Ticket.Utils.isCrossStore = jest
         .fn()
         .mockReturnValue(isCrossStore);
-      const globalState = deepfreeze({ Cashup: {} });
       const ticket = deepfreeze({
         id: '1',
         organization: 'O',
@@ -91,41 +113,47 @@ describe('createReplaceTicket', () => {
         lines: []
       });
       const payload = deepfreeze({
-        terminal: { id: 'T', terminalType: { documentType: 'O2' } }
+        terminal: { id: 'T', terminalType: { documentType: 'O2' } },
+        context: {}
       });
       const newTicket = OB.App.StateAPI.Ticket.createReplaceTicket(
         ticket,
-        payload,
-        { globalState }
+        payload
       );
       expect(newTicket).toMatchObject(result);
     }
   );
 
   test.each`
-    lines                                                                                                    | result
-    ${[{ id: '1', qty: -1, promotions: [], product: {} }]}                                                   | ${{ lines: [] }}
-    ${[{ id: '1', qty: 1, promotions: [], product: {} }]}                                                    | ${{ lines: [{ qty: -1 }] }}
-    ${[{ id: '1', qty: 5, promotions: [], product: {} }, { id: '2', qty: -2, promotions: [], product: {} }]} | ${{ lines: [{ qty: -5 }] }}
-    ${[{ id: '1', qty: 5, deliveredQuantity: 0, promotions: [], product: {} }]}                              | ${{ lines: [{ qty: -5 }] }}
-    ${[{ id: '1', qty: 5, deliveredQuantity: 4, promotions: [], product: {} }]}                              | ${{ lines: [{ qty: -1 }] }}
-    ${[{ id: '1', qty: 5, deliveredQuantity: 5, promotions: [], product: {} }]}                              | ${{ lines: [] }}
-  `('should replace ticket lines', ({ lines, result }) => {
-    const globalState = deepfreeze({ Cashup: {} });
+    lines                                                                                                   | deferredLines    | result
+    ${[{ obposCanbedelivered: true }]}                                                                      | ${undefined}     | ${{ lines: [{ obposCanbedelivered: true }] }}
+    ${[{ obposCanbedelivered: false }]}                                                                     | ${undefined}     | ${{ lines: [{ obposCanbedelivered: true }] }}
+    ${[{ obposCanbedelivered: true, qty: 1, deliveredQuantity: 1 }]}                                        | ${undefined}     | ${{ lines: [{ obposCanbedelivered: true }] }}
+    ${[{ obposCanbedelivered: true, qty: 1, deliveredQuantity: 0 }]}                                        | ${undefined}     | ${{ lines: [{ obposCanbedelivered: true }] }}
+    ${[{ obposCanbedelivered: false, qty: 1, deliveredQuantity: 1 }]}                                       | ${undefined}     | ${{ lines: [{ obposCanbedelivered: true }] }}
+    ${[{ obposCanbedelivered: false, qty: 1, deliveredQuantity: 0 }]}                                       | ${undefined}     | ${{ lines: [{ obposCanbedelivered: false }] }}
+    ${[{ isEditable: true, isDeletable: true }]}                                                            | ${undefined}     | ${{ lines: [{ isEditable: true, isDeletable: true }] }}
+    ${[{ isEditable: false, isDeletable: false }]}                                                          | ${undefined}     | ${{ lines: [{ isEditable: false, isDeletable: false }] }}
+    ${[{ id: '1', isEditable: true, isDeletable: true }, { id: '2', isEditable: true, isDeletable: true }]} | ${[{ id: '1' }]} | ${{ lines: [{ isEditable: false, isDeletable: false }, { isEditable: true, isDeletable: true }] }}
+    ${[{ id: '1' }, { id: '2', relatedLines: [{ orderId: '1', orderlineId: '1' }] }]}                       | ${undefined}     | ${{ lines: [{ id: '0' }, { id: '0', relatedLines: [{ orderId: '0', orderlineId: '0' }] }] }}
+    ${[{ id: '1', relatedLines: [{ orderId: '1', orderlineId: '2' }] }, { id: '2' }]}                       | ${undefined}     | ${{ lines: [{ id: '0', relatedLines: [{ orderId: '0', orderlineId: '0' }] }, { id: '0' }] }}
+    ${[{ id: '1' }, { id: '2', relatedLines: [{ orderId: '1', orderlineId: '3' }] }]}                       | ${undefined}     | ${{ lines: [{ id: '0' }, { id: '0', relatedLines: [{ orderId: '0', orderlineId: '3' }] }] }}
+    ${[{ id: '1', relatedLines: [{ orderId: '1', orderlineId: '3' }] }, { id: '2' }]}                       | ${undefined}     | ${{ lines: [{ id: '0', relatedLines: [{ orderId: '0', orderlineId: '3' }] }, { id: '0' }] }}
+  `('should replace ticket lines', ({ lines, deferredLines, result }) => {
     const ticket = deepfreeze({
       id: '1',
       documentNo: 'O1',
-      grossAmount: 100,
-      payment: 100,
+      hasServices: true,
       lines: lines
     });
     const payload = deepfreeze({
-      terminal: { id: 'T', terminalType: { documentType: 'O2' } }
+      terminal: { terminalType: {} },
+      context: {},
+      deferredLines
     });
     const newTicket = OB.App.StateAPI.Ticket.createReplaceTicket(
       ticket,
-      payload,
-      { globalState }
+      payload
     );
     expect(newTicket).toMatchObject(result);
   });
