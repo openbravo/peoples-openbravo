75dbb51faa05fb06429dfdf960d60405560c744d
Cristian Berner <cristian.berner@openbravo.com>
2021-05-17 11:57:12 +0200

    Fixes ISSUE-46702: Not possible to create a new record in form view if id is present in tab
    This was due to how validation is handled in form, to avoid ISSUE-45301
    a validateHiddenFields flag has been set to true to validate all hidden
    fields that may be present in the form but under collapsed field groups.
    This also checks all hidden fields, for example in this case identifier.
    Which on new records is always undefined, until the record is saved and
    an id is received.
    
    To fix this, a check is done at validateField level, if the field is hidden and not displayed, validation will be skipped.
    An alternative would be to only validate displayed fields at this level,
    but this may be more prone to errors.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/ob-view-form.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/ob-view-form.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2020 Openbravo SLU
+ * All portions are Copyright (C) 2010-2021 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -2682,5 +2682,14 @@ OB.ViewFormProperties = {
         item.updateAlwaysTakeSpace(true);
       }
     }
+  },
+
+  validateField: function(field, validators, value, record, options) {
+    if (field.hiddenInForm && !field.displayed) {
+      // Only validate fields that are hidden but are displayed, that means, those are potentially in a collapsed field group
+      // if field is hidden but not displayed, it should not be validated.
+      return true;
+    }
+    return this.Super('validateField', arguments);
   }
 };
