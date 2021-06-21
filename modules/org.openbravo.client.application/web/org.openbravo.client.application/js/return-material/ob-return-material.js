a2e709accfd1db23af11f54448e33204f66a28a7
Mark <markmm82@gmail.com>
2021-06-21 17:01:17 -0300

    Fixes BUG-46886: Fix already shipped quantity in RTVS
    The already shipped quantity was only be calculated correctly if preference "Enable UOM Management" was configured, in other cases process was not taking into account already shipped quantities.
    To fix this, if the preference is not configured any conversion is required so the already shipped qty will be the sum of movement quantities in all the lines.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/return-material/ob-return-material.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/return-material/ob-return-material.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2011-2019 Openbravo SLU
+ * All portions are Copyright (C) 2011-2021 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -356,6 +356,10 @@ OB.RM.RMShipmentSelectionChange = function(grid, record, state) {
                 ? shippedQty.add(movementQuantity.multiply(rate))
                 : shippedQty.add(movementQuantity.divide(rate));
           }
+        } else {
+          shippedQty = shippedQty.add(
+            new BigDecimal(String(editedRecord.movementQuantity))
+          );
         }
       }
     }
