5fd0c6696e53295417f3d9362ecd0c7ec3ead5cc
Álvaro Ferraz <alvaro.ferraz@openbravo.com>
2021-08-16 18:16:06 +0200

    [prepayments] Override ticket delivery mode with payload information in SetDeliveryMode state action

--- web/org.openbravo.retail.posterminal/app/model/business-object/ticket/actions/SetDeliveryMode.js
+++ web/org.openbravo.retail.posterminal/app/model/business-object/ticket/actions/SetDeliveryMode.js
@@ -17,40 +17,17 @@
     (ticket, payload) => {
       const newTicket = { ...ticket };
 
-      if (payload.obrdmDeliveryModeProperty) {
-        newTicket.obrdmDeliveryModeProperty = payload.obrdmDeliveryModeProperty;
-      }
-      if (payload.obrdmDeliveryDateProperty) {
-        newTicket.obrdmDeliveryDateProperty = payload.obrdmDeliveryDateProperty;
-      }
-      if (payload.obrdmDeliveryTimeProperty) {
-        newTicket.obrdmDeliveryTimeProperty = payload.obrdmDeliveryTimeProperty;
-      }
-      if (payload.obrdmDeliveryDateProperty === null) {
-        newTicket.obrdmDeliveryDateProperty = undefined;
-      }
-      if (payload.obrdmDeliveryTimeProperty === '') {
-        newTicket.obrdmDeliveryTimeProperty = undefined;
-      }
+      newTicket.obrdmDeliveryModeProperty = payload.obrdmDeliveryModeProperty;
+      newTicket.obrdmDeliveryDateProperty = payload.obrdmDeliveryDateProperty;
+      newTicket.obrdmDeliveryTimeProperty = payload.obrdmDeliveryTimeProperty;
 
       newTicket.lines = newTicket.lines.map(line => {
         let newLine = { ...line };
 
-        if (payload.obrdmDeliveryModeProperty) {
-          newLine.obrdmDeliveryMode = payload.obrdmDeliveryModeProperty;
-        }
-        if (payload.obrdmDeliveryDateProperty) {
-          newLine.obrdmDeliveryDate = payload.obrdmDeliveryDateProperty;
-        }
-        if (payload.obrdmDeliveryTimeProperty) {
-          newLine.obrdmDeliveryTime = payload.obrdmDeliveryTimeProperty;
-        }
-        if (payload.obrdmDeliveryDateProperty === null) {
-          newLine.obrdmDeliveryDate = undefined;
-        }
-        if (payload.obrdmDeliveryTimeProperty === '') {
-          newLine.obrdmDeliveryTime = undefined;
-        }
+        newLine.obrdmDeliveryMode = payload.obrdmDeliveryModeProperty;
+        newLine.obrdmDeliveryDate = payload.obrdmDeliveryDateProperty;
+        newLine.obrdmDeliveryTime = payload.obrdmDeliveryTimeProperty;
+
         newLine = OB.App.State.Ticket.Utils.setDeliveryMode(newTicket, newLine)
           .line;
 
