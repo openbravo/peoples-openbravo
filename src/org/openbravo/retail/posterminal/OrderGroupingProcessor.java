c43f4466576e53789124e065e294d23cc3e0704d
Guillermo Gil <guillermo.gil@openbravo.com>
2021-10-08 08:19:12 +0200

    Fixed ISSUE-47829: Everytime a cashup is done an unneeded log is written
    Changed log from info to debug.
    In case it is wanted to be written it can be configured from BO

--- src/org/openbravo/retail/posterminal/OrderGroupingProcessor.java
+++ src/org/openbravo/retail/posterminal/OrderGroupingProcessor.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2020 Openbravo S.L.U.
+ * Copyright (C) 2012-2021 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -298,7 +298,7 @@ public class OrderGroupingProcessor {
     log.debug("time execution flush : " + (t10 - t9));
     log.debug("time execution total: " + (t10 - t0));
 
-    log.info(
+    log.debug(
         "Cash up " + cashUp.getIdentifier() + ": Invoice genarated. Total time: " + (t10 - t0));
 
     JSONObject jsonResponse = new JSONObject();
