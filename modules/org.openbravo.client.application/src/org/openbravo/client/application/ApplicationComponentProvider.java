31eef65ef8a67283ef00e9ef33520e8be53593a5
Javier Armendáriz <javier.armendariz@openbravo.com>
2021-11-09 23:09:24 +0100

    [file-upload] Reference shows Smartclient's file upload widget

--- modules/org.openbravo.client.application/src/org/openbravo/client/application/ApplicationComponentProvider.java
+++ modules/org.openbravo.client.application/src/org/openbravo/client/application/ApplicationComponentProvider.java
@@ -150,6 +150,8 @@ public class ApplicationComponentProvider extends BaseComponentProvider {
         true));
     globalResources.add(createStaticResource(
         "web/org.openbravo.client.application/js/form/formitem/ob-formitem-pickeditgrid.js", true));
+    globalResources.add(createStaticResource(
+        "web/org.openbravo.client.application/js/form/formitem/ob-formitem-fileupload.js", true));
 
     globalResources.add(createStaticResource(
         "web/org.openbravo.client.application/js/toolbar/ob-toolbar.js", false));
--- modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/reference/FileUploadUIDefinition.java
+++ modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/reference/FileUploadUIDefinition.java
@@ -1,58 +1,11 @@
 package org.openbravo.client.kernel.reference;
 
-import org.openbravo.model.ad.ui.Field;
-
-
 /**
- *
+ * UI Definition of the file upload field
  */
 public class FileUploadUIDefinition extends UIDefinition {
-    @Override
-    public String getParentType() {
-        return "image";
-    }
-
-    @Override
-    public String getFormEditorType() {
-        return "OBFileItem";
-    }
-
-    @Override
-    public String getTypeProperties() {
-        return "shortDisplayFormatter: function(value, field, component, record) {" + "return \"\";"
-                + "},";
-    }
-
-    @Override
-    public String getGridFieldProperties(Field field) {
-        return super.getGridFieldProperties(field) + ", canGroupBy: false";
-    }
-
-//    @Override
-//    public String getFieldProperties(Field field) {
-//        String fieldProperties = super.getFieldProperties(field);
-//        try {
-//
-//            NumberFormat f = Utility.getFormat(RequestContext.get().getVariablesSecureApp(),
-//                    "amountInform");
-//            BigDecimal maxsize = field.getColumn().getObfblFilemaxsize();
-//            String maxsizeformat = maxsize == null ? null : f.format(maxsize);
-//
-//            JSONObject obj;
-//            if (fieldProperties.equals("")) {
-//                obj = new JSONObject();
-//            } else {
-//                obj = new JSONObject(fieldProperties);
-//            }
-//
-//            obj.put("fileExtensions", field.getColumn().getObfblFileextensions());
-//            obj.put("fileMaxSize", maxsize);
-//            obj.put("fileMaxSizeFormat", maxsizeformat);
-//            obj.put("fileMaxSizeUnit", field.getColumn().getObfblFilemaxsizeunit());
-//            return obj.toString();
-//        } catch (Exception e) { // ignore
-//            log.error("There was an error when calculating the properties of an File BLOB field", e);
-//            return fieldProperties;
-//        }
-//    }
+  @Override
+  public String getFormEditorType() {
+    return "OBFileUpload";
+  }
 }
