fc827a11d9c97690c860a87d6210760a98278249
Javier Armendáriz <javier.armendariz@openbravo.com>
2021-11-25 23:31:23 +0100

    [file-upload] Fixed input styling that changes text size on focus

--- modules/org.openbravo.client.application/web/org.openbravo.userinterface.smartclient/openbravo/skins/Default/org.openbravo.client.application/ob-form-styles.js
+++ modules/org.openbravo.client.application/web/org.openbravo.userinterface.smartclient/openbravo/skins/Default/org.openbravo.client.application/ob-form-styles.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2016 Openbravo SLU
+ * All portions are Copyright (C) 2010-2021 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -632,6 +632,15 @@ isc.OBImageSelector.addProperties({
  =======================================================================*/
 
 isc.OBPickEditGridItem.addProperties({
+  cellStyle: 'OBFormField',
+  titleStyle: 'OBFormFieldLabel'
+});
+
+/* =====================================================================
+ * OBFormItem - Process File Upload
+ =======================================================================*/
+
+isc.OBFileUpload.addProperties({
   cellStyle: 'OBFormField',
   titleStyle: 'OBFormFieldLabel'
 });
