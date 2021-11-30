d6f78d1de06c2823e257f8f0e774a888f8c073f9
Ranjith S R <ranjith@qualiantech.com>
2021-11-30 16:20:16 +0530

    Fixed ISSUE-47529: Added translation for Country MasterData

--- src/org/openbravo/retail/posterminal/master/Country.java
+++ src/org/openbravo/retail/posterminal/master/Country.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2016-2020 Openbravo S.L.U.
+ * Copyright (C) 2016-2021 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -10,7 +10,9 @@ package org.openbravo.retail.posterminal.master;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
@@ -19,6 +21,7 @@ import javax.inject.Inject;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.openbravo.client.kernel.ComponentProvider.Qualifier;
+import org.openbravo.dal.core.OBContext;
 import org.openbravo.mobile.core.master.MasterDataProcessHQLQuery;
 import org.openbravo.mobile.core.master.MasterDataProcessHQLQuery.MasterDataModel;
 import org.openbravo.mobile.core.model.HQLPropertyList;
@@ -56,6 +59,13 @@ public class Country extends MasterDataProcessHQLQuery {
         + "and c.$readableSimpleClientCriteria and c.$activeCriteria order by c.name asc";
 
     return Arrays.asList(new String[] { hql });
+  }
+
+  @Override
+  protected Map<String, Object> getParameterValues(JSONObject jsonsent) throws JSONException {
+    final Map<String, Object> paramValues = new HashMap<>();
+    paramValues.put("languageId", OBContext.getOBContext().getLanguage().getId());
+    return paramValues;
   }
 
   @Override
--- src/org/openbravo/retail/posterminal/master/CountryProperties.java
+++ src/org/openbravo/retail/posterminal/master/CountryProperties.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2016 Openbravo S.L.U.
+ * Copyright (C) 2016-2021 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openbravo.client.kernel.ComponentProvider.Qualifier;
+import org.openbravo.dal.core.OBContext;
 import org.openbravo.mobile.core.model.HQLProperty;
 import org.openbravo.mobile.core.model.ModelExtension;
 
@@ -21,9 +22,13 @@ public class CountryProperties extends ModelExtension {
 
   @Override
   public List<HQLProperty> getHQLProperties(Object params) {
+    String nameTrl = "c.name";
+    if (OBContext.hasTranslationInstalled()) {
+      nameTrl = "coalesce ((select t.name from CountryTrl t where t.country=c and t.language.id= :languageId), c.name) ";
+    }
     ArrayList<HQLProperty> list = new ArrayList<HQLProperty>();
     list.add(new HQLProperty("c.id", "id"));
-    list.add(new HQLProperty("c.name", "_identifier"));
+    list.add(new HQLProperty(nameTrl, "_identifier"));
     return list;
   }
 
