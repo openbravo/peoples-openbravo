48e23b7e4e4de8e62134e7f531e9d0e826b4c5e6
Rafael Queralta <rafaelcuba81@gmail.com>
2021-12-09 15:52:33 -0500

    Fixed BUG-47978: Allow discounts from the keyboard by configuring the user interface

--- web/org.openbravo.retail.posterminal/js/actions/discount.js
+++ web/org.openbravo.retail.posterminal/js/actions/discount.js
@@ -27,36 +27,87 @@
         var active = isEditable && currentView === 'order';
         active = active && view.model.get('order').get('lines').length > 0;
         active =
-          active &&
-          OB.Discounts.Pos.manualRuleImpls &&
-          OB.Discounts.Pos.manualRuleImpls.length > 0;
+          (active &&
+            (OB.Discounts.Pos.manualRuleImpls &&
+              OB.Discounts.Pos.manualRuleImpls.length > 0)) ||
+          !OB.MobileApp.model.get('permissions')[
+            'OBPOS_retail.discountkeyboard'
+          ];
         return active;
       },
       command: function(view) {
+        var keyboard =
+          view.$.multiColumn.$.rightPanel.$.rightBottomPanel.$.keyboard;
         var receipt = view.model.get('order');
         var selectedReceiptLine = view.state.readCommandState({
           name: 'selectedReceiptLine'
         });
-
-        if (receipt.get('isEditable') === false) {
-          view.doShowPopup({
-            popup: 'modalNotEditableOrder'
-          });
-          return;
-        }
-
-        if (!selectedReceiptLine) {
-          return false;
-        }
-
-        view.discountsMode(view, {
-          tabPanel: 'edit',
-          keyboard: 'toolbardiscounts',
-          edit: false,
-          options: {
-            discounts: true
+        if (
+          !OB.MobileApp.model.get('permissions')[
+            'OBPOS_retail.discountkeyboard'
+          ]
+        ) {
+          if (!keyboard.validateReceipt(keyboard, true)) {
+            return true;
           }
-        });
+          keyboard.receipt.set('undo', null);
+          keyboard.receipt.set('multipleUndo', true);
+          const percentage = OB.I18N.parseNumber(keyboard.getString());
+          if (
+            OB.DEC.compare(percentage) > 0 &&
+            OB.DEC.compare(OB.DEC.sub(percentage, OB.DEC.number(100))) <= 0
+          ) {
+            keyboard.selectedModels.forEach(line => {
+              const lineIds = [line.get('id')];
+              const price = OB.DEC.toNumber(
+                new BigDecimal(String(line.get('price')))
+                  .multiply(
+                    new BigDecimal(
+                      String(OB.DEC.sub(OB.DEC.number(100), percentage))
+                    )
+                  )
+                  .divide(
+                    new BigDecimal('100'),
+                    OB.DEC.getScale(),
+                    OB.DEC.getRoundingMode()
+                  )
+              );
+
+              OB.App.State.Ticket.setLinePrice({ lineIds, price })
+                .then(() => {
+                  OB.UTIL.handlePriceRuleBasedServices(keyboard.receipt);
+                  OB.UTIL.TicketUtils.printLinesOfTicket(
+                    keyboard.receipt,
+                    lineIds
+                  );
+                })
+                .catch(OB.App.View.ActionCanceledUIHandler.handle);
+            });
+          }
+          keyboard.receipt.set('multipleUndo', null);
+          keyboard.lastStatus = '';
+          keyboard.setStatus('');
+        } else {
+          if (receipt.get('isEditable') === false) {
+            view.doShowPopup({
+              popup: 'modalNotEditableOrder'
+            });
+            return;
+          }
+
+          if (!selectedReceiptLine) {
+            return false;
+          }
+
+          view.discountsMode(view, {
+            tabPanel: 'edit',
+            keyboard: 'toolbardiscounts',
+            edit: false,
+            options: {
+              discounts: true
+            }
+          });
+        }
       }
     })
   );
