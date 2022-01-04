e69f0e8aefb9231efb5802a227ad46da5fe871b8
Javier Armendáriz <javier.armendariz@openbravo.com>
2022-01-04 11:08:11 +0100

    [kept-cash] Created required fields and kept cash ticket template

--- src/org/openbravo/retail/posterminal/term/TerminalProperties.java
+++ src/org/openbravo/retail/posterminal/term/TerminalProperties.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2013-2021 Openbravo S.L.U.
+ * Copyright (C) 2013-2022 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -105,6 +105,7 @@ public class TerminalProperties extends ModelExtension {
     list.add(new HQLProperty("org.oBPOSApplytckdiscatorder", "obposApplyTicketDiscountsToOrder"));
 
     addTemplateProperty(Organization.PROPERTY_OBPOSCASHUPTEMPLATE, "printCashUpTemplate", list);
+    addTemplateProperty(Organization.PROPERTY_OBPOSKEPTCASHTEMPLATE, "printCashupKeptCashTemplate", list);
     addTemplateProperty(Organization.PROPERTY_OBPOSCASHMGMTEMPLATE, "printCashMgmTemplate", list);
     addTemplateProperty(Organization.PROPERTY_OBPOSTICKETTEMPLATE, "printTicketTemplate", list);
     addTemplateProperty(Organization.PROPERTY_OBPOSRETURNTEMPLATE, "printReturnTemplate", list);
