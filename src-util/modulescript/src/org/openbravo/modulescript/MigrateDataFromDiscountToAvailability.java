5bf737e6218c8b3fe1910d977ddf4a20b035a906
Javier Etxarri <javier.echarri@openbravo.com>
2022-01-14 15:57:50 +0100

    Modify modules script: not insert allweekdays si no tiene starting y ending + display logic tab availability

--- src-util/modulescript/src/org/openbravo/modulescript/MigrateDataFromDiscountToAvailability.java
+++ src-util/modulescript/src/org/openbravo/modulescript/MigrateDataFromDiscountToAvailability.java
@@ -23,54 +23,66 @@ import org.openbravo.database.ConnectionProvider;
 public class MigrateDataFromDiscountToAvailability extends ModuleScript {
 
   @Override
-  public void execute() {
+  public void execute() { 
     try {
-      ConnectionProvider cp = getConnectionProvider();
-      boolean isADOrgTreeDataCreated = MigrateDataFromDiscountToAvailabilityData.isDataInOfferAvailabilityTableInserted(cp);
+      ConnectionProvider cp = getConnectionProvider(); 
+      boolean isADOrgTreeDataCreated = MigrateDataFromDiscountToAvailabilityData.isDataInOfferAvailabilityTableInserted(cp); 
       if (!isADOrgTreeDataCreated) {
         MigrateDataFromDiscountToAvailabilityData[] results = MigrateDataFromDiscountToAvailabilityData.select(cp);
         
-        for (int i = 0; i < results.length; i++) {
+        for (int i = 0; i < results.length; i++) { 
           MigrateDataFromDiscountToAvailabilityData result = results[i];
-          
-          if ("Y".equals(result.allweekdays)) {
+          boolean updateDiscountData = false;
+          if ("Y".equals(result.allweekdays) && (!"".equals(result.startingtime)|| !"".equals(result.endingtime))) {
+            updateDiscountData = true;
             MigrateDataFromDiscountToAvailabilityData.insertAvailableDateTime(cp, result.adClientId, 
                 result.adOrgId, result.mOfferId, "0", result.startingtime, result.endingtime);
           }else {
             if ("Y".equals(result.monday)) {
+              updateDiscountData = true;
               MigrateDataFromDiscountToAvailabilityData.insertAvailableDateTime(cp, result.adClientId, 
                   result.adOrgId, result.mOfferId, "1", result.startingtimemonday, result.endingtimemonday);
             }
             
             if ("Y".equals(result.tuesday)) {
+              updateDiscountData = true;
               MigrateDataFromDiscountToAvailabilityData.insertAvailableDateTime(cp, result.adClientId, 
                   result.adOrgId, result.mOfferId, "2", result.startingtimetuesday, result.endingtimetuesday);
             }
             
             if ("Y".equals(result.wednesday)) {
+              updateDiscountData = true;
               MigrateDataFromDiscountToAvailabilityData.insertAvailableDateTime(cp, result.adClientId, 
                   result.adOrgId, result.mOfferId, "3", result.startingtimewednesday, result.endingtimewednesday);
             }
             
             if ("Y".equals(result.thursday)) {
+              updateDiscountData = true;
               MigrateDataFromDiscountToAvailabilityData.insertAvailableDateTime(cp, result.adClientId, 
                   result.adOrgId, result.mOfferId, "4", result.startingtimethursday, result.endingtimethursday);
             }
             
             if ("Y".equals(result.friday)) {
+              updateDiscountData = true;
               MigrateDataFromDiscountToAvailabilityData.insertAvailableDateTime(cp, result.adClientId, 
                   result.adOrgId, result.mOfferId, "5", result.startingtimefriday, result.endingtimefriday);
             }
             
             if ("Y".equals(result.saturday)) {
+              updateDiscountData = true;
               MigrateDataFromDiscountToAvailabilityData.insertAvailableDateTime(cp, result.adClientId, 
                   result.adOrgId, result.mOfferId, "6", result.startingtimesaturday, result.endingtimesaturday);
             }
             
             if ("Y".equals(result.sunday)) {
+              updateDiscountData = true;
               MigrateDataFromDiscountToAvailabilityData.insertAvailableDateTime(cp, result.adClientId,
                   result.adOrgId, result.mOfferId, "7", result.startingtimesunday, result.endingtimesunday);
             }
+          }
+          
+          if (updateDiscountData) {
+            MigrateDataFromDiscountToAvailabilityData.setWeekAndWeekDaysAsEmpty(cp, result.mOfferId);
           }
         }
       }
