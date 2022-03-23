683a4aff33ef0eb4eabdb074880f157b9e25912c
Jorge Garcia <jorge.garcia@openbravo.com>
2022-03-23 09:39:29 +0100

    [prepayments] addPayment managePrePaymentChange verification should be done only in old POS

--- web/org.openbravo.retail.posterminal/js/actionpreparation/AddPaymentCheck.js
+++ web/org.openbravo.retail.posterminal/js/actionpreparation/AddPaymentCheck.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2021 Openbravo S.L.U.
+ * Copyright (C) 2021-2022 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -20,5 +20,22 @@
     },
     async (ticket, payload) => payload,
     130
+  );
+
+  /**
+   * This action preparation verifies if is necessary to show a popup to indicate what to do with a prepayment change
+   * This check is only done in old POS
+   */
+  OB.App.StateAPI.Ticket.addPayment.addActionPreparation(
+    async (ticket, payload) => {
+      const newPayload = { ...payload };
+
+      return OB.App.State.Ticket.Utils.managePrePaymentChange(
+        ticket,
+        newPayload
+      );
+    },
+    async (ticket, payload) => payload,
+    150
   );
 })();
