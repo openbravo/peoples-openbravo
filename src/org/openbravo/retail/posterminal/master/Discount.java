a1d984ee41c366830023a52597ba0d6400f6ed5f
Meriem Azaf <meriem.azaf@openbravo.com>
2022-04-08 11:12:31 +0200

    Fixes ISSUE-48939: Masterdata model that use extensions should take them into account when calculating the hash Masterdata model definition in the backend includes the list of properties that should be taken into account to calculate a hash. That hash will then be used to determine when those properties have changed, in order to force a full refresh.
    Currently there are two methods that return the list of fields to take into account for the hash: getPropertiesFrom
    - one that accepts the list of ModelExtension and that returns the list of all properties included in those extensions
    - another one that accepts an entity and that returns all the properties of the given entity
    
    The second one SHOULD NEVER be used in a master data model that uses ModelExtension, because if a new extension is included, or a new property in an existing extension is included, the new properties will not be included in the hash, and the full master data refresh that should take place will not happen.
    To fix this, we search where the second method used and check if this module use the ModelExtension and after do the change.

--- src/org/openbravo/retail/posterminal/master/Discount.java
+++ src/org/openbravo/retail/posterminal/master/Discount.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2012-2021 Openbravo S.L.U.
+ * Copyright (C) 2012-2022 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -264,6 +264,6 @@ public class Discount extends MasterDataProcessHQLQuery {
 
   @Override
   public List<String> getMasterDataModelProperties() {
-    return getPropertiesFrom(PriceAdjustment.class);
+    return getPropertiesFrom(extensions);
   }
 }
--- src/org/openbravo/retail/posterminal/master/TaxCategory.java
+++ src/org/openbravo/retail/posterminal/master/TaxCategory.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2018-2020 Openbravo S.L.U.
+ * Copyright (C) 2018-2022 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -55,6 +55,6 @@ public class TaxCategory extends MasterDataProcessHQLQuery {
 
   @Override
   public List<String> getMasterDataModelProperties() {
-    return getPropertiesFrom(org.openbravo.model.financialmgmt.tax.TaxCategory.class);
+    return getPropertiesFrom(extensions);
   }
 }
