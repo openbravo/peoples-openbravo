4e92d61ef7386e9da0562cddcd418143984df66a
Carlos Aristu <carlos.aristu@openbravo.com>
2022-04-11 18:48:41 +0200

    related to BUG-48954: flag to identify the ExternalOrderLoader flow

--- src/org/openbravo/retail/posterminal/ExternalOrderLoader.java
+++ src/org/openbravo/retail/posterminal/ExternalOrderLoader.java
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2015-2021 Openbravo S.L.U.
+ * Copyright (C) 2015-2022 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -508,6 +508,9 @@ public class ExternalOrderLoader extends OrderLoader {
           order.put("doCancelAndReplace", true);
           order.put("bypassConcurrentModificationCheck", true);
         }
+
+        order.put("orderChannel", "FromExternal");
+
         transformOrder(data.getJSONObject(i));
       }
       if (log.isDebugEnabled()) {
