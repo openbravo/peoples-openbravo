openbravo/peoples-openbravo

commit 2d1e219013bf482f68d4c6809f367dcad8f28e7d

Fixes ISSUE-45086: Remove tika-core library and use Java7 functionality instead
tika-core 0.9 has some critical vulnerabilities, it is removed and
substituted by Java 7 guessContentTypeFromStream functionality, that
has the same behaviour, althought is a bit more limited in the list of
MIME types available.

svg files were not detectable using URLConnection.guessContentType API,
as an alternative xml is extracted from the File being checked and if
the root element is svg, then it is assumed that the xml is an SVG.

src/org/openbravo/erpCommon/utility/MimeTypeUtil.java
@@ -11,30 +11,39 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2011 Openbravo SLU
+ * All portions are Copyright (C) 2010-2020 Openbravo SLU
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
  */
 package org.openbravo.erpCommon.utility;
 
-import org.apache.tika.Tika;
-import org.apache.tika.mime.MimeType;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dom4j.Element;
+import org.openbravo.dal.xml.XMLUtil;
 
 /**
- * Utility class to detect MIME type based on data array. This class is based on Apache Tika part of
- * the Lucene project. http://lucene.apache.org/tika/
+ * Utility class to detect MIME type based on data array.
  * 
  * @author iperdomo
- * 
  */
 public class MimeTypeUtil {
   private static MimeTypeUtil instance = new MimeTypeUtil();
-  private static Tika tika;
 
-  private static void init() {
-    tika = new Tika();
-  }
+  private static final Logger logger = LogManager.getLogger();
+
+  // MIME type to be returned instead of null
+  private static final String NULL_MIME_TYPE = "application/octet-stream";
 
   /**
    * Returns the instance of the MimeTypeUtil class
@@ -42,39 +51,62 @@ public class MimeTypeUtil {
    * @return MimeTypeUtil instance
    */
   public static MimeTypeUtil getInstance() {
-    if (tika == null) {
-      init();
-    }
     return instance;
   }
 
-  public static void setInstace(MimeTypeUtil ins) {
-    MimeTypeUtil.instance = ins;
-  }
-
   /**
-   * Returns a MimeType object based on the byte array provided as parameter
+   * Returns the MIME type name, (e.g. image/png) based on the byte array passed as parameter.
+   * Returns application/octet-stream if no better match is found.
    * 
    * @param data
    *          byte array from which we want to detect the MIME type
-   * @return MimeType representation
-   * @deprecated This method will always return <b>null</b>
-   */
-  @Deprecated
-  public MimeType getMimeType(byte[] data) {
-    return null;
-  }
-
-  /**
-   * Returns the MIME type name, e.g. image/png based on the byte array passed as parameter. Returns
-   * application/octet-stream if no better match is found.
-   * 
-   * @param data
-   *          byte array from which we want to detect the MIME type
-   * @return A MIME type name, e.g. "image/png"
+   * @return MIME Type Name detected or application/octet-stream
    */
   public String getMimeTypeName(byte[] data) {
-    return tika.detect(data);
+    return getMimeTypeName(new BufferedInputStream(new ByteArrayInputStream(data)));
   }
 
+  /**
+   * Returns the MIME type name, (e.g. image/png) based on the file content passed as parameter.
+   * Returns application/octet-stream if no better match is found.
+   *
+   * @param file
+   *          file from which we want to detect the MIME type
+   * @return MIME Type Name detected or application/octet-stream
+   */
+  public String getMimeTypeName(File file) {
+    try {
+      return getMimeTypeName(new BufferedInputStream(new FileInputStream(file)));
+    } catch (FileNotFoundException ex) {
+      logger.error("File {} has not been found on Mime Type detection.", file.getName(), ex);
+      return NULL_MIME_TYPE;
+    }
+  }
+
+  /**
+   * Returns the MIME type name, (e.g. image/png) based on the InputStream passed as parameter.
+   * Returns application/octet-stream if no match is found.
+   *
+   * @param is
+   *          InputStream used to determine MIME type
+   * @return MIME Type Name detected or application/octet-stream
+   */
+  private String getMimeTypeName(InputStream is) {
+    try {
+      String mimeType = URLConnection.guessContentTypeFromStream(is);
+      if (mimeType == null) {
+        return NULL_MIME_TYPE;
+      }
+      if ("application/xml".equals(mimeType)) {
+        Element xmlRootElement = XMLUtil.getInstance().getRootElement(is);
+        if (xmlRootElement != null && "svg".equals(xmlRootElement.getName())) {
+          return "image/svg+xml";
+        }
+      }
+      return mimeType;
+    } catch (IOException ex) {
+      logger.error("Failed to detect MimeType.", ex);
+      return NULL_MIME_TYPE;
+    }
+  }
 }

modules/org.openbravo.client.application/src/org/openbravo/client/application/attachment/AttachImplementationManager.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2015-2019 Openbravo SLU
+ * All portions are Copyright (C) 2015-2020 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -40,7 +40,6 @@ import javax.inject.Inject;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.tika.Tika;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.hibernate.HibernateException;
@@ -62,6 +61,7 @@ import org.openbravo.dal.security.SecurityChecker;
 import org.openbravo.dal.service.OBCriteria;
 import org.openbravo.dal.service.OBDal;
 import org.openbravo.dal.service.OBDao;
+import org.openbravo.erpCommon.utility.MimeTypeUtil;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.model.ad.datamodel.Table;
 import org.openbravo.model.ad.domain.List;
@@ -159,11 +159,7 @@ public class AttachImplementationManager {
         attachment.setUpdated(new Date());
       }
       attachment.setOrganization(org);
-      String strDataType = null;
-      try {
-        strDataType = new Tika().detect(file);
-      } catch (IOException ignore) {
-      }
+      String strDataType = MimeTypeUtil.getInstance().getMimeTypeName(file);
 
       if (strDataType != null && strDataType.length() <= DATA_TYPE_MAX_LENGTH) {
         attachment.setDataType(strDataType);

src-test/src/org/openbravo/test/mimetypes/MimeTypeTest.java
@@ -11,15 +11,18 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2014 Openbravo SLU
+ * All portions are Copyright (C) 2010-2020 Openbravo SLU
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
  */
 package org.openbravo.test.mimetypes;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
-import org.openbravo.dal.service.OBCriteria;
 import org.openbravo.dal.service.OBDal;
 import org.openbravo.erpCommon.utility.MimeTypeUtil;
 import org.openbravo.model.ad.utility.Image;
@@ -33,19 +36,15 @@ import org.openbravo.test.base.OBBaseTest;
 public class MimeTypeTest extends OBBaseTest {
 
   @Test
-  public void testGetImageMimeTypes() {
+  public void testAllImageMimeTypesShouldBeDetected() {
     setSystemAdministratorContext();
-
-    OBCriteria<Image> obc = OBDal.getInstance().createCriteria(Image.class);
-    for (Image img : obc.list()) {
+    List<Image> images = OBDal.getInstance().createCriteria(Image.class).list();
+    for (Image img : images) {
       byte[] imageBytes = img.getBindaryData();
       if (imageBytes != null) {
-        try {
-          String t = MimeTypeUtil.getInstance().getMimeTypeName(imageBytes);
-          System.out.println(t);
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
+        String resMimeType = MimeTypeUtil.getInstance().getMimeTypeName(imageBytes);
+        String expectedMimeType = img.getMimetype();
+        assertEquals("MIME type detected is not correct.", expectedMimeType, resMimeType);
       }
     }
   }

src-util/modulescript/src/org/openbravo/modulescript/ConvertImages.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2012-2015 Openbravo SLU
+ * All portions are Copyright (C) 2012-2020 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -19,9 +19,11 @@
 package org.openbravo.modulescript;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import javax.imageio.ImageIO;
 import java.io.File;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -37,13 +39,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import javax.imageio.ImageIO;
-import org.apache.tika.Tika;
-import org.apache.tika.mime.MimeType;
 
 public class ConvertImages extends ModuleScript {
 
   private static final Logger log4j = LogManager.getLogger();
-  private Tika tika;
   
   @Override
   public void execute() {
@@ -83,15 +82,13 @@ public class ConvertImages extends ModuleScript {
           is.close();
           ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
           BufferedImage rImage = ImageIO.read(bis);
+          String imageMimeType = URLConnection.guessContentTypeFromStream(new BufferedInputStream(new ByteArrayInputStream(bytes)));
           String qupdate="UPDATE ad_image set name='Image', binarydata=?, width=?, height=?, mimetype=? where ad_image_id=?";
           PreparedStatement ps=cp.getPreparedStatement(qupdate);
           ps.setObject(1, bytes);
           ps.setLong(2, rImage.getWidth());
           ps.setLong(3, rImage.getHeight());
-          if (tika==null) {
-            tika=new Tika();
-          }
-          ps.setString(4, tika.detect(bytes));
+          ps.setString(4, imageMimeType);
           ps.setString(5, imageid);
           ps.executeUpdate();
           cp.releasePreparedStatement(ps);

src/org/openbravo/dal/xml/XMLUtil.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2008-2019 Openbravo SLU 
+ * All portions are Copyright (C) 2008-2020 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -19,6 +19,7 @@
 
 package org.openbravo.dal.xml;
 
+import java.io.InputStream;
 import java.io.StringWriter;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -32,6 +33,7 @@ import javax.xml.transform.sax.TransformerHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.Namespace;
@@ -103,6 +105,28 @@ public class XMLUtil implements OBSingleton {
     reader.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
     reader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
     return reader;
+  }
+
+  /**
+   * Parses provided InputStream into XML and extracts root element
+   * 
+   * @param in
+   *          InputStream XML
+   * @return Element root element
+   */
+  public Element getRootElement(InputStream in) {
+    Element rootElement = null;
+    try {
+      SAXReader reader = newSAXReader();
+      // Most SVG image files come with <Doctype> declaration. This option is set to false to allow
+      // its usage and be able to parse those SVGs
+      reader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false);
+      Document doc = reader.read(in);
+      rootElement = doc.getRootElement();
+    } catch (DocumentException | SAXException ex) {
+      log.error("Failed to parse XML input for extracting root element.", ex);
+    }
+    return rootElement;
   }
 
   /** @return a new secure {@link TransformerFactory} */

src/org/openbravo/erpCommon/info/ImageInfoBLOB.java
@@ -11,7 +11,7 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2009-2017 Openbravo SLU 
+ * All portions are Copyright (C) 2009-2020 Openbravo SLU 
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************

commit 280b2f81703961e4b79d78b0641d260f71c86d14

Fixes ISSUE-49075: can't import a bmp file
After switching from using tika library to using the jdk URLConnection
guessContentTypeFromStream implementation to determine the mimetype of
an uploaded file, this broke. The URLConnection implementation is not
able to guess if a file is a bmp image or not, it will detect it as a
binary file. To fix this, if it is not detected using this API, ImageIO
API is used to try and read the file, if that's successful, then the
file is a bmp image file.

Regression introduced by commit 2d1e219013bf482f68d4c6809f367dcad8f28e7d

src/org/openbravo/erpCommon/utility/MimeTypeUtil.java
@@ -11,13 +11,14 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2010-2020 Openbravo SLU
+ * All portions are Copyright (C) 2010-2022 Openbravo SLU
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
  */
 package org.openbravo.erpCommon.utility;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -25,6 +26,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLConnection;
+
+import javax.imageio.ImageIO;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -101,6 +104,10 @@ public class MimeTypeUtil {
     try {
       String mimeType = URLConnection.guessContentTypeFromStream(is);
       if (mimeType == null) {
+        // Check if it is an image, if so, it will be a binary image(.bmp)
+        if (isImage(is)) {
+          return "image/bmp";
+        }
         return NULL_MIME_TYPE;
       }
       if ("application/xml".equals(mimeType)) {
@@ -113,6 +120,22 @@ public class MimeTypeUtil {
     } catch (IOException ex) {
       logger.error("Failed to detect MimeType.", ex);
       return NULL_MIME_TYPE;
+    }
+  }
+
+  /**
+   * Returns if the input stream is an image or not
+   * 
+   * @param is
+   *          Provided input stream
+   * @return true if it is readable as an image, false otherwise
+   */
+  private boolean isImage(InputStream is) {
+    try {
+      BufferedImage bi = ImageIO.read(is);
+      return bi != null;
+    } catch (IOException e) {
+      return false; // not an image
     }
   }
 }