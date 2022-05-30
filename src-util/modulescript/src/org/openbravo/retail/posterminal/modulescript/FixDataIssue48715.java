50b27b67356a7546591fd47efcc34630ab8dd7d9
Eduardo Becerra (OB) <eduardo.becerra@openbravo.com>
2022-05-30 07:29:21 +0000

    fixed ISSUE-48715: Add db-checks to avoid wrong values in timeout preference/configuration

---
+++ src-util/modulescript/src/org/openbravo/retail/posterminal/modulescript/FixDataIssue48715.java
@@ -0,0 +1,42 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2022 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
+package org.openbravo.retail.posterminal.modulescript;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import org.openbravo.database.ConnectionProvider;
+import org.openbravo.modulescript.ModuleScript;
+import org.openbravo.modulescript.ModuleScriptExecutionLimits;
+import org.openbravo.modulescript.OpenbravoVersion;
+
+public class FixDataIssue48715 extends ModuleScript {
+  private static final String RETAIL_PACK_MODULE_ID = "03FAB282A7BF47D3B1B242AC67F7845B";
+
+  @Override
+  public void execute() {
+    try {
+      ConnectionProvider cp = getConnectionProvider();
+      FixDataIssue48715Data.fixModelObjectPara(cp);
+      FixDataIssue48715Data.fixPreference(cp);
+    } catch (Exception e) {
+      handleError(e);
+    }
+  }
+
+  @Override
+  protected ModuleScriptExecutionLimits getModuleScriptExecutionLimits() {
+    return new ModuleScriptExecutionLimits(RETAIL_PACK_MODULE_ID, null,
+        new OpenbravoVersion(1, 8, 222000));
+  }
+
+  @Override
+  protected boolean executeOnInstall() {
+    return false;
+  }
+}
