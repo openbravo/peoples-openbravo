1ebe86328ab944a73c792637397b31971ea42a34
Jorge Wederago <jorge.wederago@openbravo.com>
2022-06-10 09:04:26 +0200

    Related to ISSUE-49409: Pay open Tickets: Allow mixing positive and negative transaction

--- web-test/model/business-object/ticket/actions/CompleteMultiTicket.test.js
+++ web-test/model/business-object/ticket/actions/CompleteMultiTicket.test.js
@@ -1,6 +1,6 @@
 /*
  ************************************************************************************
- * Copyright (C) 2020 Openbravo S.L.U.
+ * Copyright (C) 2020-2022 Openbravo S.L.U.
  * Licensed under the Openbravo Commercial License version 1.0
  * You may obtain a copy of the License at http://www.openbravo.com/legal/obcl.html
  * or in the legal folder of this module distribution.
@@ -18,6 +18,11 @@ require('../../global/SetupGlobalUtils');
 const deepfreeze = require('deepfreeze');
 
 describe('Complete Multi ticket action', () => {
+  beforeAll(() => {
+    OB = OB || {};
+    OB.UTIL = OB.UTIL || {};
+    OB.UTIL.getDefaultCashPaymentMethod = jest.fn().mockReturnValue({});
+  });
   it('should complete nulti ticket, print it and synchronize it', () => {
     const ticketList = deepfreeze([]);
     const ticket = deepfreeze({});
