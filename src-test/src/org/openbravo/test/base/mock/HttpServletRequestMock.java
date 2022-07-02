55b773e0a14aa51cbcd140a0a41c5b68cff337b8
Javier López <javier.lopez@precognis.com>
2022-07-02 09:53:19 +0000

    Fixes ISSUE-49510: HttpServletRequestMock.getSession(boolean arg0) returns null instead of the active session

--- src-test/src/org/openbravo/test/base/mock/HttpServletRequestMock.java
+++ src-test/src/org/openbravo/test/base/mock/HttpServletRequestMock.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2017-2019 Openbravo SLU 
+ * All portions are Copyright (C) 2017-2022 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -379,7 +379,10 @@ public class HttpServletRequestMock implements HttpServletRequest {
 
   @Override
   public HttpSession getSession(boolean arg0) {
-    return null;
+    if (session == null && !arg0) {
+      return null;
+    }
+    return getSession();
   }
 
   @Override
