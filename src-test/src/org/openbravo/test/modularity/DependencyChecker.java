e6eb86022acdf3dd6672e626ac4c73cbbcfd6108
Asier Lostalé <asier.lostale@openbravo.com>
2022-10-10 13:20:55 +0200

    [dep-check] optimizes dependency initialization bypassing ob3

--- src-test/src/org/openbravo/test/modularity/DependencyChecker.java
+++ src-test/src/org/openbravo/test/modularity/DependencyChecker.java
@@ -28,32 +28,34 @@ import org.openbravo.model.ad.utility.DataSetTable;
 import org.openbravo.test.base.OBBaseTest;
 
 public class DependencyChecker extends OBBaseTest {
+  private static final Logger log = LogManager.getLogger();
   private static final Entity MODULE_ENTITY = ModelProvider.getInstance().getEntity(Module.class);
 
   private static final Set<String> OB3 = Set.of( //
-      "0", //
-      "F8D1B3ECB3474E8DA5C216473C840DF1", //
-      "FF8080813129ADA401312CA1222A0005", //
-      "FF8080812D842086012D844F3CC0003E", //
-      "5EB4F15C80684ACA904756BDC12ADBE5", //
-      "0A060B2AF1974E8EAA8DB61388E9AECC", //
-      "C70732EA90A14EC0916078B85CC33D2D", //
-      "2758CD25B2704AF6BBAD10365FC82C06", //
-      "8A098711BB324335A19833286BDB093D", //
-      "9BA0836A3CD74EE4AB48753A47211BCC", //
-      "3A3A943684D64DEF9EC39F588A656848", //
-      "D393BE6F22BB44B7B728259B34FC795A", //
-      "96998CBC42744B3DBEE28AC8095C9335", //
-      "2A5EE903D7974AC298C0504FBC4501A7", //
-      "A918E3331C404B889D69AA9BFAFB23AC", //
-      "8A34B301DC524EA3A07513DF9F42CC90", //
-      "EC356CEE3D46416CA1EBEEB9AB82EDB9", //
-      "7E48CDD73B7E493A8BED4F7253E7C989", //
-      "883B5872CA0548F9AF2BBBE7D2DDFA61", //
-      "0138E7A89B5E4DC3932462252801FFBC", //
-      "4B828F4D03264080AA1D2057B13F613C", //
-      "FF8080813141B198013141B86DD70003", //
-      "A44B9BA75C354D8FB2E3F7D6EB6BFDC4");
+      "0", // org.openbravo
+      "0138E7A89B5E4DC3932462252801FFBC", // org.openbravo.v3
+      "0A060B2AF1974E8EAA8DB61388E9AECC", // org.openbravo.client.querylist
+      "2758CD25B2704AF6BBAD10365FC82C06", // org.openbravo.client.myob
+      "2A5EE903D7974AC298C0504FBC4501A7", // org.openbravo.financial.paymentreport
+      "3A3A943684D64DEF9EC39F588A656848", // org.openbravo.reports.ordersawaitingdelive
+      "4B828F4D03264080AA1D2057B13F613C", // org.openbravo.client.kernel
+      "5EB4F15C80684ACA904756BDC12ADBE5", // org.openbravo.userinterface.selector
+      "7E48CDD73B7E493A8BED4F7253E7C989", // org.openbravo.v3.framework
+      "883B5872CA0548F9AF2BBBE7D2DDFA61", // org.openbravo.v3.datasets
+      "8A098711BB324335A19833286BDB093D", // org.openbravo.apachejdbcconnectionpool
+      "8A34B301DC524EA3A07513DF9F42CC90", // org.openbravo.utility.cleanup.log
+      "96998CBC42744B3DBEE28AC8095C9335", // org.openbravo.userinterface.skin.250to300C
+      "9BA0836A3CD74EE4AB48753A47211BCC", // org.openbravo.client.application
+      "A44B9BA75C354D8FB2E3F7D6EB6BFDC4", // org.openbravo.service.datasource
+      "A918E3331C404B889D69AA9BFAFB23AC", // org.openbravo.advpaymentmngt
+      "C70732EA90A14EC0916078B85CC33D2D", // org.openbravo.base.weld
+      "D393BE6F22BB44B7B728259B34FC795A", // org.openbravo.client.htmlwidget
+      "EC356CEE3D46416CA1EBEEB9AB82EDB9", // org.openbravo.userinterface.smartclient
+      "F8D1B3ECB3474E8DA5C216473C840DF1", // org.openbravo.service.json
+      "FF8080812D842086012D844F3CC0003E", // org.openbravo.client.widgets
+      "FF8080813129ADA401312CA1222A0005", // org.openbravo.service.integration.google
+      "FF8080813141B198013141B86DD70003" // org.openbravo.service.integration.openid
+  );
 
   @Test
   public void checkDependencies() {
@@ -126,10 +128,15 @@ public class DependencyChecker extends OBBaseTest {
   }
 
   private static Map<String, Set<String>> getAllModuleDependencies() {
+    log.info("Calculating all dependencies...");
     Map<String, Set<String>> deps = new HashMap<>();
-    for (Module mod : OBDal.getInstance().createCriteria(Module.class).list()) {
+    for (Module mod : OBDal.getInstance()
+        .createCriteria(Module.class)
+        .add(Restrictions.not(Restrictions.in(Module.PROPERTY_ID, OB3)))
+        .list()) {
       deps.put(mod.getId(), KernelUtils.getInstance().getAncestorsDependencyTree(mod));
     }
+    log.info("Dependencies calculated");
     return deps;
 
   }
