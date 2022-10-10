8079fd6521f69c6ed856fc8d2a28714000ef3f9f
Asier Lostalé <asier.lostale@openbravo.com>
2022-10-10 11:45:03 +0200

    [dep-check] prevents validations on core pack

--- src-test/src/org/openbravo/test/modularity/DependencyChecker.java
+++ src-test/src/org/openbravo/test/modularity/DependencyChecker.java
@@ -23,6 +23,31 @@ import org.openbravo.test.base.OBBaseTest;
 public class DependencyChecker extends OBBaseTest {
   private static final Entity MODULE_ENTITY = ModelProvider.getInstance().getEntity(Module.class);
 
+  private static final Set<String> OB3 = Set.of( //
+      "0", //
+      "F8D1B3ECB3474E8DA5C216473C840DF1", //
+      "FF8080813129ADA401312CA1222A0005", //
+      "FF8080812D842086012D844F3CC0003E", //
+      "5EB4F15C80684ACA904756BDC12ADBE5", //
+      "0A060B2AF1974E8EAA8DB61388E9AECC", //
+      "C70732EA90A14EC0916078B85CC33D2D", //
+      "2758CD25B2704AF6BBAD10365FC82C06", //
+      "8A098711BB324335A19833286BDB093D", //
+      "9BA0836A3CD74EE4AB48753A47211BCC", //
+      "3A3A943684D64DEF9EC39F588A656848", //
+      "D393BE6F22BB44B7B728259B34FC795A", //
+      "96998CBC42744B3DBEE28AC8095C9335", //
+      "2A5EE903D7974AC298C0504FBC4501A7", //
+      "A918E3331C404B889D69AA9BFAFB23AC", //
+      "8A34B301DC524EA3A07513DF9F42CC90", //
+      "EC356CEE3D46416CA1EBEEB9AB82EDB9", //
+      "7E48CDD73B7E493A8BED4F7253E7C989", //
+      "883B5872CA0548F9AF2BBBE7D2DDFA61", //
+      "0138E7A89B5E4DC3932462252801FFBC", //
+      "4B828F4D03264080AA1D2057B13F613C", //
+      "FF8080813141B198013141B86DD70003", //
+      "A44B9BA75C354D8FB2E3F7D6EB6BFDC4");
+
   @Test
   public void checkDependencies() {
     setSystemAdministratorContext();
@@ -79,6 +104,13 @@ public class DependencyChecker extends OBBaseTest {
 
   private boolean isDependency(String referencedModuleId, String baseModuleId,
       Map<String, Set<String>> allDeps) {
+    // OB3 pack is already messed up! It's not worth to try to fix it.
+    // note we also allow here incorrect dependencies to any ob3 module (ie. defining dep on core
+    // but making using of client.kernell stuff)
+    if (OB3.contains(baseModuleId)) {
+      return true;
+    }
+
     return allDeps.get(baseModuleId).contains(referencedModuleId);
   }
 
