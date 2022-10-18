829176af51eed85599f12c86acef5bf06d8cc538
Cristian Berner <cristian.berner@openbravo.com>
2022-10-18 11:53:51 +0000

    Fixes FR-48335: AllJrxmlCompilation test should skip hwmanager printing reports
    Those type of jrxmls were including hwmanager only classes and libraries,
    resulting in always generating errors when the test executed and this type of files were present,
    so to deal with this part, we skip the files that include com.openbravo.pos functionality in them.

--- src-test/src/org/openbravo/test/reporting/AllJrxmlCompilation.java
+++ src-test/src/org/openbravo/test/reporting/AllJrxmlCompilation.java
@@ -11,13 +11,14 @@
  * under the License. 
  * The Original Code is Openbravo ERP. 
  * The Initial Developer of the Original Code is Openbravo SLU 
- * All portions are Copyright (C) 2017-2018 Openbravo SLU 
+ * All portions are Copyright (C) 2017-2022 Openbravo SLU
  * All Rights Reserved. 
  * Contributor(s):  ______________________________________.
  ************************************************************************
  */
 package org.openbravo.test.reporting;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
@@ -31,6 +32,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openbravo.base.session.OBPropertiesProvider;
@@ -49,6 +52,10 @@ import net.sf.jasperreports.engine.JRException;
  *
  */
 public class AllJrxmlCompilation extends WeldBaseTest {
+  // The following constant is used to exclude/skip those jrxml templates that have hwmanager logic
+  private static final String HWM_TEST_PREFIX = "com.openbravo.pos";
+
+  private static final Logger log = LogManager.getLogger();
 
   private static final List<Path> REPORTS = parameters();
 
@@ -80,7 +87,6 @@ public class AllJrxmlCompilation extends WeldBaseTest {
 
   private static Collection<Path> getJrxmlTemplates(String dir) throws IOException {
     final Collection<Path> allJasperFiles = new ArrayList<>();
-
     final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("regex:.*\\.jrxml");
     Path basePath = Paths.get(
         OBPropertiesProvider.getInstance().getOpenbravoProperties().getProperty("source.path"),
@@ -88,7 +94,22 @@ public class AllJrxmlCompilation extends WeldBaseTest {
     Files.walkFileTree(basePath, new SimpleFileVisitor<Path>() {
       @Override
       public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (matcher.matches(file)) {
+        if (Files.exists(file) && matcher.matches(file)) {
+          try (BufferedReader fileReader = Files.newBufferedReader(file)) {
+            while (fileReader.ready()) {
+              String line = fileReader.readLine();
+              if (line.contains(HWM_TEST_PREFIX)) {
+                // Ignore files that contain hwmanager logic as these are not intended to be
+                // compiled in backoffice
+                log.info("Skipped JRXML file(hwmanager report): " + file.toString());
+                return FileVisitResult.CONTINUE;
+              }
+            }
+          } catch (IOException ignore) {
+            // Files that were not able to be read and parsed because of an IOException we will
+            // try to compile
+          }
+
           allJasperFiles.add(file);
         }
         return FileVisitResult.CONTINUE;
