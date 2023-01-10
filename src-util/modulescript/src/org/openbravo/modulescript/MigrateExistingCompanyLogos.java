54a9fd888fa36eba9ae6512e1221556d4725cce2
Javier Armendáriz <javier.armendariz@openbravo.com>
2023-01-10 14:14:55 +0100

    [company-logo] Removed log and improved support with Oracle in module script

--- src-util/modulescript/src/org/openbravo/modulescript/MigrateExistingCompanyLogos.java
+++ src-util/modulescript/src/org/openbravo/modulescript/MigrateExistingCompanyLogos.java
@@ -19,22 +19,19 @@
 package org.openbravo.modulescript;
 
 import org.openbravo.database.ConnectionProvider;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 
 /**
  * This module script is used to upgrade the existing company logos to fit the new structure
  * introduced in https://issues.openbravo.com/view.php?id=51238
  *
- * The general criteria followed here is that we only migrate the logo used for documents,
- * and for the company logo we will prefer using a vectorial version, and in case there is
- * no vectorial image available, pick the largest image.
+ * The general criteria followed here is that we only migrate the logo used for documents, and for
+ * the company logo we will prefer using a vectorial version, and in case there is no vectorial
+ * image available, pick the largest image.
  *
- * This migration will only be done when logo images are found in the deprecated logo fields. If none,
- * new logo fields won't be updated
+ * This migration will only be done when logo images are found in the deprecated logo fields. If
+ * none, new logo fields won't be updated
  */
 public class MigrateExistingCompanyLogos extends ModuleScript {
-  static Logger log4j = LogManager.getLogger();
 
   @Override
   public void execute() {
@@ -56,67 +53,69 @@ public class MigrateExistingCompanyLogos extends ModuleScript {
     String logoForDocsSysInfo = MigrateExistingCompanyLogosForDocsData
         .selectLogoForDocumentsInSystemInfo(cp);
 
-    // For the company logo we'll pick the first vectorial image, and if none is vectorial,
-    // we'll pick the largest from the fields below by height:
-    // - your_company_big_image,
-    // - your_company_login_image,
-    // - your_company_menu_image
+    /**
+     * For the company logo we'll pick the first vectorial image, and if none is vectorial, we'll
+     * pick the largest from the following fields below by height: your_company_big_image,
+     * your_company_login_image, your_company_menu_image
+     */
     String companyLogoSysInfo = MigrateExistingCompanyLogosData
         .selectVectorialCompanyLogoInSystemInfo(cp);
     if (companyLogoSysInfo == null) {
-      companyLogoSysInfo = MigrateExistingCompanyLogosData.selectLargestCompanyLogoInSystemInfo(cp);
+      MigrateExistingCompanyLogosData[] companyLogosInSystemInfo = MigrateExistingCompanyLogosData
+          .selectCompanyLogosInSystemInfoOrderedBySize(cp);
+      if (companyLogosInSystemInfo.length > 0) {
+        companyLogoSysInfo = companyLogosInSystemInfo[0].adImageId;
+      }
     }
 
     // Update logos if found any
     if (logoExists(logoForDocsSysInfo) || logoExists(companyLogoSysInfo)) {
-      log4j.warn("Updating logos in sys info " + logoForDocsSysInfo + " and " + companyLogoSysInfo);
       MigrateExistingCompanyLogosData.updateLogosInSystemInfo(cp, companyLogoSysInfo,
-              logoForDocsSysInfo);
-    }
-    else {
-      log4j.warn("skipping update in sysinfo");
+          logoForDocsSysInfo);
     }
   }
 
   private void migrateLogosInClientInfo(ConnectionProvider cp) throws Exception {
-    // From the list of all Client with clientInfo entries we pick your_company_document_image and set this
-    // image as the new company_logo_for_docs.
-    //
-    // For the company logo we'll pick the first vectorial image, and if none is vectorial,
-    // we'll pick the largest from the fields below by height:
-    // - your_company_menu_image
-    // - your_company_big_image
+    /**
+     * From the list of all Client with clientInfo entries we pick your_company_document_image and
+     * set this image as the new company_logo_for_docs.
+     *
+     * For the company logo we'll pick the first vectorial image, and if none is vectorial, we'll
+     * pick the largest from the following fields by height: your_company_menu_image,
+     * your_company_big_image
+     */
     MigrateExistingCompanyLogosForDocsData[] docLogosPerClient = MigrateExistingCompanyLogosForDocsData
         .selectLogoForDocumentsInClients(cp);
     for (MigrateExistingCompanyLogosForDocsData docLogo : docLogosPerClient) {
       String companyLogoForClient = MigrateExistingCompanyLogosData
           .selectVectorialCompanyLogoInClientInfo(cp, docLogo.adClientId);
       if (companyLogoForClient == null) {
-        companyLogoForClient = MigrateExistingCompanyLogosData.selectLargestLogoInClientInfo(cp,
-            docLogo.adClientId);
+        MigrateExistingCompanyLogosData[] companyLogosInClientInfo = MigrateExistingCompanyLogosData
+            .selectLogosInClientInfoOrderedBySize(cp, docLogo.adClientId);
+        if (companyLogosInClientInfo.length > 0) {
+          companyLogoForClient = companyLogosInClientInfo[0].adImageId;
+        }
       }
 
       if (logoExists(docLogo.yourCompanyDocumentImage) || logoExists(companyLogoForClient)) {
-        log4j.warn("Updating logos in client info " + docLogo.yourCompanyDocumentImage + " and " + companyLogoForClient);
         MigrateExistingCompanyLogosData.updateLogosInClientInfo(cp, companyLogoForClient,
-                docLogo.yourCompanyDocumentImage, docLogo.adClientId);
-      }
-      else {
-        log4j.warn("skipping update in client");
+            docLogo.yourCompanyDocumentImage, docLogo.adClientId);
       }
     }
   }
 
   private void migrateLogosInOrgInfo(ConnectionProvider cp) throws Exception {
-    // From the list of all organizations with orgInfo entry that has a document logo
-    // we'll pick the image referenced in your_company_document_image to be the value of
-    // company_logo_for_docs of the given organization
+    /**
+     * From the list of all organizations with orgInfo entry that has a document logo we'll pick the
+     * image referenced in your_company_document_image to be the value of company_logo_for_docs of
+     * the given organization
+     */
     MigrateExistingCompanyLogosForDocsData[] docLogosPerOrg = MigrateExistingCompanyLogosForDocsData
         .selectLogoForDocumentsInOrganizations(cp);
     for (MigrateExistingCompanyLogosForDocsData docLogo : docLogosPerOrg) {
       if (logoExists(docLogo.yourCompanyDocumentImage)) {
         MigrateExistingCompanyLogosData.updateLogosInOrgInfo(cp, docLogo.yourCompanyDocumentImage,
-                docLogo.adOrgId);
+            docLogo.adOrgId);
       }
     }
   }
@@ -133,6 +132,6 @@ public class MigrateExistingCompanyLogos extends ModuleScript {
 
   @Override
   protected ModuleScriptExecutionLimits getModuleScriptExecutionLimits() {
-    return new ModuleScriptExecutionLimits("0", null, new OpenbravoVersion(3, 0, 224901));
+    return new ModuleScriptExecutionLimits("0", null, new OpenbravoVersion(3, 0, 231901));
   }
 }
