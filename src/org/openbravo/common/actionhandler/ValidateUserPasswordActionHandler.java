ec37f6f21e577a09922ee2e0710120c66d3a620e
Augusto Mauch <augusto.mauch@openbravo.com>
2023-05-02 15:47:45 +0200

    Fixes ISSUE-52051: Uses standard license in ValidateUserPasswordActionHandler

--- src/org/openbravo/common/actionhandler/ValidateUserPasswordActionHandler.java
+++ src/org/openbravo/common/actionhandler/ValidateUserPasswordActionHandler.java
@@ -1,13 +1,20 @@
 /*
- ************************************************************************************
- * Copyright (C) 2023 Openbravo S.L.U.
- * Licensed under the Apache Software License version 2.0
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to  in writing,  software  distributed
- * under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
- * CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
- * specific language governing permissions and limitations under the License.
- ************************************************************************************
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html 
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License. 
+ * The Original Code is Openbravo ERP. 
+ * The Initial Developer of the Original Code is Openbravo SLU 
+ * All portions are Copyright (C) 2023 Openbravo SLU 
+ * All Rights Reserved. 
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
  */
 package org.openbravo.common.actionhandler;
 
