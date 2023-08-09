ede9911f2c460f4622a3bf35bd74882f823faf3e
Javier López <javier.lopez@precognis.com>
2023-08-09 09:01:18 +0000

    Fixes ISSUE-52984: Update Organizations subtab dates.

--- src/org/openbravo/event/PriceAdjustmentHandler.java
+++ src/org/openbravo/event/PriceAdjustmentHandler.java
@@ -20,16 +20,20 @@
 package org.openbravo.event;
 
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
 
 import javax.enterprise.event.Observes;
 
 import org.openbravo.base.exception.OBException;
 import org.openbravo.base.model.Entity;
 import org.openbravo.base.model.ModelProvider;
+import org.openbravo.base.model.Property;
 import org.openbravo.client.kernel.event.EntityNewEvent;
 import org.openbravo.client.kernel.event.EntityPersistenceEvent;
 import org.openbravo.client.kernel.event.EntityPersistenceEventObserver;
 import org.openbravo.client.kernel.event.EntityUpdateEvent;
+import org.openbravo.dal.service.OBDal;
 import org.openbravo.model.pricing.priceadjustment.PriceAdjustment;
 
 /**
@@ -39,24 +43,36 @@ public class PriceAdjustmentHandler extends EntityPersistenceEventObserver {
   private static final Entity[] ENTITIES = {
       ModelProvider.getInstance().getEntity(PriceAdjustment.ENTITY_NAME) };
 
+  private final Entity priceAdjustmentEntity = ModelProvider.getInstance()
+      .getEntity(PriceAdjustment.ENTITY_NAME);
+  private final Property startingDateProperty = priceAdjustmentEntity
+      .getProperty(PriceAdjustment.PROPERTY_STARTINGDATE);
+  private final Property endingDateProperty = priceAdjustmentEntity
+      .getProperty(PriceAdjustment.PROPERTY_ENDINGDATE);
+
   @Override
   protected Entity[] getObservedEntities() {
     return ENTITIES;
   }
 
   public void onSave(@Observes EntityNewEvent event) {
+    if (!isValidEvent(event)) {
+      return;
+    }
     validateData(event);
   }
 
   public void onUpdate(@Observes EntityUpdateEvent event) {
-    validateData(event);
-  }
-
-  private void validateData(EntityPersistenceEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
+    validateData(event);
+    updateOrganizationDates((PriceAdjustment) event.getTargetInstance(),
+        (Date) event.getPreviousState(startingDateProperty),
+        (Date) event.getPreviousState(endingDateProperty));
+  }
 
+  private void validateData(EntityPersistenceEvent event) {
     final Entity offerEntity = ModelProvider.getInstance()
         .getEntity(event.getTargetInstance().getEntityName());
     final BigDecimal maxQty = (BigDecimal) event
@@ -69,6 +85,34 @@ public class PriceAdjustmentHandler extends EntityPersistenceEventObserver {
       if (priorityRule == null) {
         throw new OBException("@SelectPriorityRule@");
       }
+    }
+  }
+
+  private void updateOrganizationDates(PriceAdjustment discount, Date previousStartingDate,
+      Date previousEndingDate) {
+    final boolean isStartingDateChanged = !Objects.equals(previousStartingDate,
+        discount.getStartingDate());
+    final boolean isEndingDateChanged = !Objects.equals(previousEndingDate,
+        discount.getEndingDate());
+    if (isStartingDateChanged || isEndingDateChanged) {
+    //@formatter:off
+      final String hql =
+          "update from PricingAdjustmentOrganization e" +
+          "  set e.startingDate = :startingDate," +
+          "    e.endingDate = :endingDate," +
+          "    e.updated = :updated," +
+          "    e.updatedBy = :updatedBy" +
+          " where e.priceAdjustment.id = :discountId";
+      //@formatter:on
+      OBDal.getInstance()
+          .getSession()
+          .createQuery(hql)
+          .setParameter("discountId", discount.getId())
+          .setParameter("startingDate", discount.getStartingDate())
+          .setParameter("endingDate", discount.getEndingDate())
+          .setParameter("updated", discount.getUpdated())
+          .setParameter("updatedBy", discount.getUpdatedBy())
+          .executeUpdate();
     }
   }
 }
