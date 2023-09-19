8c6458be6e3c40a4a4a099143ab88ac08f58ff5d
Javier Armendáriz <javier.armendariz@openbravo.com>
2023-09-19 16:11:33 +0200

    [org-timezone] Adding a function to transform a Date to a ZonedDateTime that includes a Time Zone

--- src-test/src/org/openbravo/erpCommon/utility/OBDateUtilsTest.java
+++ src-test/src/org/openbravo/erpCommon/utility/OBDateUtilsTest.java
@@ -21,12 +21,19 @@ package org.openbravo.erpCommon.utility;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.text.ParseException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 import org.junit.Test;
+import org.openbravo.model.common.enterprise.Organization;
 import org.openbravo.service.json.JsonUtils;
 import org.openbravo.test.base.OBBaseTest;
 
@@ -71,4 +78,52 @@ public class OBDateUtilsTest extends OBBaseTest {
     assertThat(dateTrucanted, equalTo(dateWithoutMinutes));
   }
 
+  @Test
+  public void convertServerDateToTimezoneProvidedByOrganization() {
+    Date serverDate = Date.from(Instant.parse("2023-09-18T10:00:00Z"));
+    Organization organization = mock(Organization.class);
+    when(organization.getTimezone()).thenReturn("Asia/Tokyo");
+    ZonedDateTime tokyoDate = ZonedDateTime.of(2023, 9, 18, 19, 0, 0, 0, ZoneId.of("Asia/Tokyo"));
+
+    ZonedDateTime convertedDate = OBDateUtils.convertFromServerToOrgDateTime(serverDate,
+        organization);
+
+    assertThat(convertedDate.toString(), equalTo(tokyoDate.toString()));
+  }
+
+  @Test
+  public void convertServerDateToDefinedTimezone() {
+    Date serverDate = Date.from(Instant.parse("2023-09-18T10:00:00Z"));
+    ZonedDateTime tokyoDate = ZonedDateTime.of(2023, 9, 18, 19, 0, 0, 0, ZoneId.of("Asia/Tokyo"));
+
+    ZonedDateTime convertedDate = OBDateUtils.convertFromServerToOrgDateTime(serverDate,
+        "Asia/Tokyo");
+
+    assertThat(convertedDate.toString(), equalTo(tokyoDate.toString()));
+  }
+
+  @Test
+  public void zonedDateTimeIsProperlyFormatted() {
+    ZonedDateTime tokyoDate = ZonedDateTime.of(2023, 9, 18, 19, 0, 0, 0, ZoneId.of("Asia/Tokyo"));
+
+    assertThat(OBDateUtils.formatZonedDateTime(tokyoDate, "dd-MM-yyyy HH:mm:ss"),
+        equalTo("18-09-2023 19:00:00"));
+  }
+
+  @Test
+  public void convertServerDateToTimeWillReturnNullWhenTimezoneIsNotAvailable() {
+    Date serverDate = Date.from(Instant.parse("2023-09-18T10:00:00Z"));
+    Organization organization = mock(Organization.class);
+    when(organization.getTimezone()).thenReturn(null); // Org has no timezone defined
+
+    ZonedDateTime convertedDate = OBDateUtils.convertFromServerToOrgDateTime(serverDate,
+        organization);
+
+    assertNull(convertedDate);
+  }
+
+  @Test
+  public void formatZonedDateTimeWillReturnEmptyStringWhenDateIsNull() {
+    assertThat(OBDateUtils.formatZonedDateTime(null, "dd-MM-yyyy HH:mm:ss"), equalTo(""));
+  }
 }
