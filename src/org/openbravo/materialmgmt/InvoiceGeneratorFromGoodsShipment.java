8c2f943cb5e00d1a30161fd73c1e45f1fffb78f1
Atul Gaware <atul.gaware@openbravo.com>
2024-01-08 08:45:26 +0000

    Fixes BUG-0054104: Error when create the invoice from the shipment

--- src/org/openbravo/materialmgmt/InvoiceGeneratorFromGoodsShipment.java
+++ src/org/openbravo/materialmgmt/InvoiceGeneratorFromGoodsShipment.java
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2018-2022 Openbravo SLU
+ * All portions are Copyright (C) 2018-2024 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  *************************************************************************
@@ -26,6 +26,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.codehaus.jettison.json.JSONArray;
@@ -326,7 +327,16 @@ public class InvoiceGeneratorFromGoodsShipment {
   private BigDecimal getQtyToInvoice(final OrderLine orderLine) {
     OBDal.getInstance().getSession().refresh(orderLine); // Necessary to get updated deliveredQty
     final BigDecimal qtyShip = orderLine.getDeliveredQuantity(); // Previous shipments + this one
-    final BigDecimal qtyPreviouslyInvoiced = orderLine.getInvoicedQuantity();
+    BigDecimal qtyPreviouslyInvoiced = orderLine.getInvoicedQuantity();
+
+    // Calculate QtyInvoiced for OrderLine in this same invoice
+    if (invoice != null) {
+      for (InvoiceLine invLine : invoice.getInvoiceLineList()) {
+        if (StringUtils.equals(invLine.getSalesOrderLine().getId(), orderLine.getId())) {
+          qtyPreviouslyInvoiced = qtyPreviouslyInvoiced.add(invLine.getInvoicedQuantity());
+        }
+      }
+    }
 
     BigDecimal qtyToInvoice = qtyShip.subtract(qtyPreviouslyInvoiced);
 
