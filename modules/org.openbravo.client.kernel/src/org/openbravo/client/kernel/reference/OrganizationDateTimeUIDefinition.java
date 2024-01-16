903b04b32b80edeb2c29d022663cc7ba9ab99faf
Eugen Hamuraru <eugen.hamuraru@openbravo.com>
2024-01-16 10:51:40 +0100

    [org-tz-fiels] remove unnecessary createFromClassicString method

--- modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/reference/OrganizationDateTimeUIDefinition.java
+++ modules/org.openbravo.client.kernel/src/org/openbravo/client/kernel/reference/OrganizationDateTimeUIDefinition.java
@@ -18,18 +18,7 @@
  */
 package org.openbravo.client.kernel.reference;
 
-import java.time.ZonedDateTime;
-import java.util.Optional;
-
-import org.openbravo.base.model.Entity;
-import org.openbravo.base.model.ModelProvider;
-import org.openbravo.base.structure.BaseOBObject;
-import org.openbravo.client.kernel.RequestContext;
-import org.openbravo.dal.service.OBDal;
-import org.openbravo.data.Sqlc;
-import org.openbravo.erpCommon.utility.OBDateUtils;
 import org.openbravo.model.ad.ui.Field;
-import org.openbravo.model.common.enterprise.Organization;
 
 /**
  * UI definition to represent organization time zone fields
@@ -42,36 +31,5 @@ public class OrganizationDateTimeUIDefinition extends StringUIDefinition {
     properties = removeAttributeFromString(properties, "canSort");
     properties = removeAttributeFromString(properties, "canFilter");
     return properties + ", canSort: false, canFilter: false";
-  }
-
-  @Override
-  public synchronized Object createFromClassicString(String value) {
-    try {
-      BaseOBObject bob = getBOBFromCurrentContext();
-      Optional<Organization> org = OBDateUtils.getTimeZoneOrganization(bob);
-      String timezoneId = org.isPresent() && org.get().getTimezone() != null
-          ? org.get().getTimezone()
-          : null;
-      if (timezoneId != null) {
-        ZonedDateTime zonedDateTime = OBDateUtils
-            .convertFromServerToOrgDateTime(OBDateUtils.getDate(value), timezoneId);
-        return OBDateUtils.formatZonedDateTime(zonedDateTime);
-      }
-      return null;
-    } catch (Exception ex) {
-      log.error("Could not convert {} into the organization timezone", value, ex);
-      return null;
-    }
-  }
-
-  private BaseOBObject getBOBFromCurrentContext() {
-    String entityName = RequestContext.get().getRequestParameter("_entityName");
-    Entity entity = ModelProvider.getInstance().getEntity(entityName);
-    String idColumn = RequestContext.get().getRequestParameter("keyColumnName") != null
-        ? RequestContext.get().getRequestParameter("keyColumnName")
-        : entity.getIdProperties().get(0).getColumnName();
-    String id = RequestContext.get()
-        .getRequestParameter("inp" + Sqlc.TransformaNombreColumna(idColumn));
-    return (BaseOBObject) OBDal.getInstance().get(entity.getMappingClass(), id);
   }
 }
