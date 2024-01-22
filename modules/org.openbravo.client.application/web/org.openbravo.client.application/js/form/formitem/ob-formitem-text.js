53e1bd9519e42fab1cdc295eb1e7863abdc2645c
Meriem Azaf <meriem.azaf@openbravo.com>
2024-01-22 15:10:39 +0100

    Fixes ISSUE-54427: JS error when clicking on a text field
    JS error when clicking on a text field, this is a regression caused by this part https://gitlab.com/openbravo/product/openbravo/-/commit/f20ab1b69f12506d5bf82a1aa0e2a20cf42f25a5#321abadeb3f917e932e789b8defbca4ada0b9556_110_110 .
    
    So we returned the code of this part to it's old version.

--- modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-text.js
+++ modules/org.openbravo.client.application/web/org.openbravo.client.application/js/form/formitem/ob-formitem-text.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2011-2023 Openbravo SLU
+ * All portions are Copyright (C) 2011-2024 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
@@ -113,6 +113,9 @@ isc.OBTextItem.addProperties({
       value = this.getValue();
     } else if (this.mask) {
       value = this.mask;
+    }
+    if (value === undefined) {
+      return value;
     }
     // unescape before escaping to prevent escaping text that is already escaped
     unescapedDisplayValue = value.unescapeHTML();
