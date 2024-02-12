d9738809fb36b1436457351e63b961577a66c81f
Víctor Martínez Romanos <victor.martinez@openbravo.com>
2024-02-12 21:11:56 +0100

    [purchaseAssortment] Code review improvements
    Avoid creating manual HTML/XML code, and instead using the Standard UI
    for Report and Processes.
    
    This requires to create a new column (instead of reusing the existing DocAction)
    to avoid issues with WAD.
    
    The old Report and Process linked to M_Requisition_Post has been deprecated, and
    the related fields hidden.
    
    The M_REQUISITION_POST now sets the value of new column, so the button in the UI
    shows the right label in each moment.

--- src/org/openbravo/erpCommon/ad_actionButton/ProcessRequisition.java
+++ src/org/openbravo/erpCommon/ad_actionButton/ProcessRequisition.java
@@ -18,215 +18,32 @@
  */
 package org.openbravo.erpCommon.ad_actionButton;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.openbravo.base.exception.OBException;
-import org.openbravo.base.filter.IsIDFilter;
-import org.openbravo.base.secureApp.HttpSecureAppServlet;
-import org.openbravo.base.secureApp.VariablesSecureApp;
-import org.openbravo.dal.core.OBContext;
-import org.openbravo.dal.service.OBDal;
-import org.openbravo.data.FieldProvider;
-import org.openbravo.erpCommon.reference.PInstanceProcessData;
 import org.openbravo.erpCommon.utility.OBError;
-import org.openbravo.erpCommon.utility.Utility;
+import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.materialmgmt.RequisitionProcessor;
-import org.openbravo.model.ad.process.ProcessInstance;
-import org.openbravo.model.ad.ui.Process;
-import org.openbravo.model.procurement.Requisition;
-import org.openbravo.xmlEngine.XmlDocument;
+import org.openbravo.materialmgmt.RequisitionProcessor.Action;
+import org.openbravo.scheduling.ProcessBundle;
+import org.openbravo.service.db.DalBaseProcess;
 
-/*
- * Process the  transaction document Status of the Requisition through the Requisition Processor.
+/**
+ * Handler to process a Requisition. It calls the {@link RequisitionProcessor} and prints the result
+ * as a message in the UI
  */
-public class ProcessRequisition extends HttpSecureAppServlet {
-  private static final long serialVersionUID = 1L;
-  private static final String M_REQUISITION_POST_ID = "1004400003";
-  private static final String M_REQUISITION_TABLE_ID = "800212";
-  private static final String REQUISITION_DOCUMENT_ACTION = "135";
-
+public class ProcessRequisition extends DalBaseProcess {
   @Override
-  public void doPost(final HttpServletRequest request, final HttpServletResponse response)
-      throws ServletException, IOException {
-    final VariablesSecureApp vars = new VariablesSecureApp(request);
-
+  public void doExecute(ProcessBundle bundle) throws Exception {
+    final String requisitionId = (String) bundle.getParams().get("M_Requisition_ID");
+    final Action action = Action.getByValue((String) bundle.getParams().get("action"));
+    OBError msg = new OBError();
     try {
-      if (vars.commandIn("DEFAULT")) {
-        defaultProcessRequisition(response, vars);
-      } else if (vars.commandIn("SAVE_BUTTONDocAction1004400003")) {
-        processRequisition(response, vars);
-      }
+      RequisitionProcessor.process(requisitionId, action);
+      msg.setType("Success");
+      msg.setMessage(OBMessageUtils.messageBD("Success"));
     } catch (Exception e) {
-
+      msg.setType("Error");
+      msg.setMessage(e.getMessage());
     }
+
+    bundle.setResult(msg);
   }
-
-  private void defaultProcessRequisition(final HttpServletResponse response,
-      final VariablesSecureApp vars) throws ServletException, IOException {
-    final String strWindowId = vars.getGlobalVariable("inpwindowId", "ProcessRequisition|Window_ID",
-        IsIDFilter.instance);
-    final String strTabId = vars.getGlobalVariable("inpTabId", "ProcessRequisition|Tab_ID",
-        IsIDFilter.instance);
-
-    final String strM_Requisition_ID = vars.getGlobalVariable("inpmRequisitionId",
-        strWindowId + "|M_Requisition_ID", "", IsIDFilter.instance);
-
-    final String strdocaction = vars.getStringParameter("inpdocaction");
-    final String strProcessing = vars.getStringParameter("inpprocessing", "Y");
-    final String strOrg = vars.getRequestGlobalVariable("inpadOrgId", "ProcessRequisition|Org_ID",
-        IsIDFilter.instance);
-    final String strClient = vars.getStringParameter("inpadClientId", IsIDFilter.instance);
-
-    final String strdocstatus = vars.getRequiredStringParameter("inpdocstatus");
-    final int accesslevel = 1;
-
-    if ((org.openbravo.erpCommon.utility.WindowAccessData.hasReadOnlyAccess(this, vars.getRole(),
-        strTabId))
-        || !(Utility.isElementInList(
-            Utility.getContext(this, vars, "#User_Client", strWindowId, accesslevel), strClient)
-            && Utility.isElementInList(
-                Utility.getContext(this, vars, "#User_Org", strWindowId, accesslevel), strOrg))) {
-      final OBError myError = Utility.translateError(this, vars, vars.getLanguage(),
-          Utility.messageBD(this, "NoWriteAccess", vars.getLanguage()));
-      vars.setMessage(strTabId, myError);
-      printPageClosePopUp(response, vars);
-    } else {
-      printPageDocAction(response, vars, strM_Requisition_ID, strdocaction, strProcessing,
-          strdocstatus, M_REQUISITION_TABLE_ID, strWindowId);
-    }
-  }
-
-  void printPageDocAction(final HttpServletResponse response, final VariablesSecureApp vars,
-      final String strM_Requisition_ID, final String strdocaction, final String strProcessing,
-      final String strdocstatus, final String stradTableId, final String strWindowId)
-      throws IOException {
-    log4j.debug("Output: Button process 1004400003");
-
-    response.setContentType("text/html; charset=UTF-8");
-    final PrintWriter out = response.getWriter();
-    final Requisition requisition = (Requisition) OBDal.getInstance()
-        .getProxy(Requisition.ENTITY_NAME, strM_Requisition_ID);
-
-    final XmlDocument xmlDocument = fillXmlDocumentPageDocAction(vars, strM_Requisition_ID,
-        strdocaction, strProcessing, strdocstatus, stradTableId, strWindowId, requisition);
-
-    out.println(xmlDocument.print());
-    out.close();
-
-  }
-
-  private XmlDocument fillXmlDocumentPageDocAction(final VariablesSecureApp vars,
-      final String strM_Requisition_ID, final String strdocaction, final String strProcessing,
-      final String strdocstatus, final String stradTableId, final String strWindowId,
-      final Requisition requisition) {
-    final String[] discard = { "newDiscard" };
-    final XmlDocument xmlDocument = xmlEngine
-        .readXmlTemplate("org/openbravo/erpCommon/ad_actionButton/ProcessRequisition", discard)
-        .createXmlDocument();
-
-    xmlDocument.setParameter("key", strM_Requisition_ID);
-    xmlDocument.setParameter("processing", strProcessing);
-    xmlDocument.setParameter("form", "ProcessRequisition.html");
-    xmlDocument.setParameter("window", strWindowId);
-    xmlDocument.setParameter("css", vars.getTheme());
-    xmlDocument.setParameter("language", "defaultLang=\"" + vars.getLanguage() + "\";");
-    xmlDocument.setParameter("dateDisplayFormat", vars.getSessionValue("#AD_SqlDateFormat"));
-    xmlDocument.setParameter("directory", "var baseDirectory = \"" + strReplaceWith + "/\";\n");
-    xmlDocument.setParameter("processId", M_REQUISITION_POST_ID);
-    xmlDocument.setParameter("cancel", Utility.messageBD(this, "Cancel", vars.getLanguage()));
-    xmlDocument.setParameter("ok", Utility.messageBD(this, "OK", vars.getLanguage()));
-
-    final OBError myMessage = vars.getMessage(M_REQUISITION_POST_ID);
-    vars.removeMessage(M_REQUISITION_POST_ID);
-    if (myMessage != null) {
-      xmlDocument.setParameter("messageType", myMessage.getType());
-      xmlDocument.setParameter("messageTitle", myMessage.getTitle());
-      xmlDocument.setParameter("messageMessage", myMessage.getMessage());
-    }
-
-    String processDescription = null;
-    try {
-      OBContext.setAdminMode(true);
-      final Process process = (Process) OBDal.getInstance()
-          .getProxy(Process.ENTITY_NAME, M_REQUISITION_POST_ID);
-      processDescription = process.getDescription();
-    } finally {
-      OBContext.restorePreviousMode();
-    }
-
-    xmlDocument.setParameter("docstatus", strdocstatus);
-    xmlDocument.setParameter("adTableId", stradTableId);
-    xmlDocument.setParameter("processId", M_REQUISITION_POST_ID);
-    xmlDocument.setParameter("processDescription", processDescription);
-    xmlDocument.setParameter("docaction", strdocaction);
-    final FieldProvider[] dataDocAction = ActionButtonUtility.docAction(this, vars, strdocaction,
-        REQUISITION_DOCUMENT_ACTION, strdocstatus, strProcessing, stradTableId);
-    xmlDocument.setData("reportdocaction", "liststructure", dataDocAction);
-    final StringBuilder dact = new StringBuilder();
-    if (dataDocAction != null) {
-      dact.append("var arrDocAction = new Array(\n");
-      for (int i = 0; i < dataDocAction.length; i++) {
-        dact.append("new Array(\"" + dataDocAction[i].getField("id") + "\", \""
-            + dataDocAction[i].getField("name") + "\", \""
-            + dataDocAction[i].getField("description") + "\")\n");
-        if (i < dataDocAction.length - 1) {
-          dact.append(",\n");
-        }
-      }
-      dact.append(");");
-    } else {
-      dact.append("var arrDocAction = null");
-    }
-    xmlDocument.setParameter("array", dact.toString());
-    return xmlDocument;
-  }
-
-  private void generateErrorProcessReceipt(final HttpServletResponse response,
-      final VariablesSecureApp vars, final String strTabId, Exception ex) throws IOException {
-    final OBError myMessage;
-    myMessage = Utility.translateError(this, vars, vars.getLanguage(), ex.getMessage());
-    if (!myMessage.isConnectionAvailable()) {
-      bdErrorConnection(response);
-    } else {
-      vars.setMessage(strTabId, myMessage);
-    }
-  }
-
-  private void processRequisition(final HttpServletResponse response, final VariablesSecureApp vars)
-      throws ServletException, IOException {
-    final String strWindowId = vars.getGlobalVariable("inpwindowId", "ProcessRequisition|Window_ID",
-        IsIDFilter.instance);
-    final String strTabId = vars.getGlobalVariable("inpTabId", "ProcessRequisition|Tab_ID",
-        IsIDFilter.instance);
-    final String strM_Requisition_ID = vars.getGlobalVariable("inpKey",
-        strWindowId + "|M_Requisition_ID", "");
-    final String strdocaction = vars.getStringParameter("inpdocaction");
-    OBError myMessage = null;
-    try {
-      RequisitionProcessor requisitionProcessor = new RequisitionProcessor();
-      ProcessInstance pinstance = requisitionProcessor.processRequisition(strM_Requisition_ID,
-          strdocaction);
-
-      final PInstanceProcessData[] pinstanceData = PInstanceProcessData.select(this,
-          pinstance.getId());
-      myMessage = Utility.getProcessInstanceMessage(this, vars, pinstanceData);
-      log4j.debug(myMessage.getMessage());
-      vars.setMessage(strTabId, myMessage);
-
-      String strWindowPath = Utility.getTabURL(strTabId, "R", true);
-      if (strWindowPath.equals("")) {
-        strWindowPath = strDefaultServlet;
-      }
-      printPageClosePopUp(response, vars, strWindowPath);
-
-    } catch (OBException ex) {
-      generateErrorProcessReceipt(response, vars, strTabId, ex);
-    }
-  }
-
 }
--- src/org/openbravo/materialmgmt/RequisitionProcessor.java
+++ src/org/openbravo/materialmgmt/RequisitionProcessor.java
@@ -18,62 +18,88 @@
  */
 package org.openbravo.materialmgmt;
 
+import java.util.Arrays;
+
+import org.apache.commons.lang.StringUtils;
 import org.openbravo.base.exception.OBException;
 import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.service.OBDal;
+import org.openbravo.erpCommon.utility.OBError;
+import org.openbravo.erpCommon.utility.OBMessageUtils;
 import org.openbravo.model.ad.process.ProcessInstance;
-import org.openbravo.model.ad.ui.Process;
 import org.openbravo.model.procurement.Requisition;
 import org.openbravo.service.db.CallProcess;
 import org.openbravo.synchronization.event.SynchronizationEvent;
 
-/*
- * Process the  transaction document Status of the Requisition.
+/**
+ * Central place to process requisitions from Java. It calls the M_Requisition_Post procedure and
+ * manage the Push API events. In case of error an {@link OBException} is thrown
  */
 public class RequisitionProcessor {
-  private static final String PROCESS_M_REQUISITION_POST = "1004400003";
+  private static final String M_REQUISITION_POST_ID = "1004400003";
 
-  public ProcessInstance processRequisition(String strM_Requisition_ID, String strdocaction)
-      throws OBException {
-    try {
+  /**
+   * Valid actions when processing a requisition
+   */
+  public enum Action {
+    COMPLETE("CO"), REACTIVATE("RE"), CLOSE("CL");
 
-      final Requisition requisition = (Requisition) OBDal.getInstance()
-          .getProxy(Requisition.ENTITY_NAME, strM_Requisition_ID);
-      requisition.setDocumentAction(strdocaction);
-      OBDal.getInstance().save(requisition);
-      OBDal.getInstance().flush();
+    private final String value;
 
-      final ProcessInstance pinstance = CallProcess.getInstance()
-          .call(getProcessRequisition(), strM_Requisition_ID, null);
-
-      OBDal.getInstance().commitAndClose();
-
-      triggerPushEvent(strM_Requisition_ID);
-      return pinstance;
-
-    } catch (Exception e) {
-      throw new OBException("Error processing requisition", e);
+    private Action(final String value) {
+      this.value = value;
     }
 
+    /**
+     * Utility method to get the Action from a searchkey, like for example 'CO'
+     */
+    public static Action getByValue(String searchKey) {
+      return Arrays.stream(Action.values())
+          .filter(e -> searchKey != null && searchKey.equals(e.value))
+          .findFirst()
+          .orElseThrow(() -> new RuntimeException(
+              "Action not valid for processing a requisition: " + searchKey));
+    }
   }
 
-  private Process getProcessRequisition() {
-    Process process = null;
+  /**
+   * Process the given requisition. After that the requisition is refreshed from database and
+   * returned. Any Push API event is launched if processed successfully.
+   * 
+   * @param requisitionId
+   *          requisition Id
+   * @param action
+   *          the action to perform when processing the requisition: complete, reactivate or close.
+   * @throws OBException
+   *           in case of error, a exception with the message is thrown
+   */
+  public static Requisition process(final String requisitionId, final Action action)
+      throws OBException {
+    OBContext.setAdminMode();
     try {
-      OBContext.setAdminMode(true);
-      process = (Process) OBDal.getInstance()
-          .getProxy(Process.ENTITY_NAME, PROCESS_M_REQUISITION_POST);
+      final Requisition requisition = (Requisition) OBDal.getInstance()
+          .getProxy(Requisition.ENTITY_NAME, requisitionId);
+      requisition.setDocumentAction(action.value);
+      OBDal.getInstance().flush(); // Necessary for the M_Requisition_Post
+
+      callMRequisitionPost(requisitionId);
+      OBDal.getInstance().refresh(requisition);
+
+      SynchronizationEvent.getInstance().triggerEvent("API_Process_Requisition", requisitionId);
+
+      return requisition;
     } finally {
       OBContext.restorePreviousMode();
     }
-    return process;
   }
 
-  public void triggerPushEvent(String requisitionId) throws OBException {
-    try {
-      SynchronizationEvent.getInstance().triggerEvent("API_Process_Requisition", requisitionId);
-    } catch (Exception e) {
-      throw new OBException("Error triggering Push API event for requisition", e);
+  private static void callMRequisitionPost(final String requisitionId) {
+    final org.openbravo.model.ad.ui.Process process = OBDal.getInstance()
+        .get(org.openbravo.model.ad.ui.Process.class, M_REQUISITION_POST_ID);
+    final ProcessInstance pinstance = CallProcess.getInstance().call(process, requisitionId, null);
+    final OBError result = OBMessageUtils.getProcessInstanceMessage(pinstance);
+    if (StringUtils.equals("Error", result.getType())) {
+      throw new OBException(result.getMessage());
     }
   }
 }
