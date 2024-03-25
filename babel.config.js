220e31ec3f71a6944c568d4c8f660cd68d4017d1
Cristian Berner <cristian.berner@openbravo.com>
2024-03-25 14:54:15 +0100

    [vite-cra-alternative] Update copyright years

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@
  * under the License.
  * The Original Code is Openbravo ERP.
  * The Initial Developer of the Original Code is Openbravo SLU
- * All portions are Copyright (C) 2020-2021 Openbravo SLU
+ * All portions are Copyright (C) 2020-2024 Openbravo SLU
  * All Rights Reserved.
  * Contributor(s):  ______________________________________.
  ************************************************************************
