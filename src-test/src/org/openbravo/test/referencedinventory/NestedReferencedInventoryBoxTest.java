ef32e04ba57ae0db016dcd8e119bae0ca354b2d6
Atul Gaware <atul.gaware@openbravo.com>
2024-03-28 15:44:34 +0530

    [nestedhutests] Add basic junit test for Nested HU

---
+++ src-test/src/org/openbravo/test/referencedinventory/NestedReferencedInventoryBoxTest.java
@@ -0,0 +1,93 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License.
+ * The Original Code is Openbravo ERP.
+ * The Initial Developer of the Original Code is Openbravo SLU
+ * All portions are Copyright (C) 2024 Openbravo SLU
+ * All Rights Reserved.
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
+ */
+
+package org.openbravo.test.referencedinventory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.math.BigDecimal;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.junit.Test;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.materialmgmt.refinventory.BoxProcessor;
+import org.openbravo.materialmgmt.refinventory.ReferencedInventoryUtil.SequenceType;
+import org.openbravo.model.common.enterprise.Organization;
+import org.openbravo.model.common.plm.Product;
+import org.openbravo.model.materialmgmt.onhandquantity.ReferencedInventory;
+import org.openbravo.model.materialmgmt.onhandquantity.ReferencedInventoryType;
+import org.openbravo.model.materialmgmt.onhandquantity.StorageDetail;
+import org.openbravo.model.materialmgmt.transaction.InternalMovement;
+
+/**
+ * This is class to test Nested Referenced Inventory Functionalities.
+ *
+ */
+
+public class NestedReferencedInventoryBoxTest extends ReferencedInventoryTest {
+
+  @Test
+  public void testBox() throws Exception {
+    createReferencedInventory();
+  }
+
+  /**
+   * create referenced inventory and do boxing transaction
+   */
+
+  private void createReferencedInventory() throws Exception {
+    final ReferencedInventoryType refInvType = ReferencedInventoryTestUtils
+        .createReferencedInventoryType(OBDal.getInstance().getProxy(Organization.class, "0"),
+            SequenceType.NONE, null);
+    final ReferencedInventory refInv = ReferencedInventoryTestUtils
+        .createReferencedInventory(ReferencedInventoryTestUtils.QA_SPAIN_ORG_ID, refInvType);
+    final JSONArray selectedStorageDetailsJS = new JSONArray();
+    String toBinId = BINS[0];
+    for (String[] product : PRODUCTS) {
+      selectedStorageDetailsJS.put(addProductInBox(refInv, product[0], product[1]).get(0));
+    }
+    final InternalMovement boxMovement = new BoxProcessor(refInv, selectedStorageDetailsJS, toBinId)
+        .createAndProcessGoodsMovement();
+
+    OBDal.getInstance().refresh(boxMovement);
+    OBDal.getInstance().refresh(refInv);
+
+    assertsGoodsMovementIsProcessed(boxMovement);
+    assertsGoodsMovementNumberOfLines(boxMovement, 3);
+    assertThat("Nested Referenced Inventory does not have Unique Items Count equal to 3",
+        refInv.getUniqueItemsCount(), equalTo(3L));
+  }
+
+  /**
+   * Add items in referenced inventory during boxing transaction
+   */
+
+  private JSONArray addProductInBox(final ReferencedInventory refInv, final String productId,
+      final String attributeSetInstanceId) throws Exception {
+    final Product product = ReferencedInventoryTestUtils.cloneProduct(productId);
+    final BigDecimal receivedQty = new BigDecimal("10");
+    ReferencedInventoryTestUtils.receiveProduct(product, receivedQty, attributeSetInstanceId);
+    final StorageDetail storageDetail1 = ReferencedInventoryTestUtils
+        .getUniqueStorageDetail(product);
+    final JSONArray storageDetailsJS = ReferencedInventoryTestUtils
+        .getStorageDetailsToBoxJSArray(storageDetail1, receivedQty);
+    return storageDetailsJS;
+  }
+}
