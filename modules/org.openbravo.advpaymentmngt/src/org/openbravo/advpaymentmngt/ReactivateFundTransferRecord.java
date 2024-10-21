f279066e3910603df230b77262e2c136ed86263e
Gorka Gil <go.gil@orisha.com>
2024-10-21 13:52:09 +0200

    [FundsTransfer] Add Reactivate button
    * Also fix sequence and read only logic for documentno field

---
+++ modules/org.openbravo.advpaymentmngt/src/org/openbravo/advpaymentmngt/ReactivateFundTransferRecord.java
@@ -0,0 +1,34 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.0  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License.
+ * The Original Code is Openbravo ERP.
+ * The Initial Developer of the Original Code is Openbravo SLU
+ * All portions are Copyright (C) 2024 Openbravo SLU
+ * All Rights Reserved.
+ * Contributor(s):  
+ *************************************************************************
+ */
+package org.openbravo.advpaymentmngt;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openbravo.scheduling.ProcessBundle;
+import org.openbravo.service.db.DalBaseProcess;
+
+public class ReactivateFundTransferRecord extends DalBaseProcess {
+
+  private static final Logger log = LogManager.getLogger();
+
+  @Override
+  protected void doExecute(ProcessBundle bundle) throws Exception {
+
+  }
+}
