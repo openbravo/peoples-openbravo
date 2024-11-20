197c16548c45c1ced18a42bf9ceac6a0cc3ebc8a
Ignacio De Andrés <i.deandres@orisha.com>
2024-11-20 15:55:58 +0100

    [Forgot password] Improvements in the ForgotPasswordService

--- src/org/openbravo/authentication/ForgotPasswordService.java
+++ src/org/openbravo/authentication/ForgotPasswordService.java
@@ -9,9 +9,12 @@
 package org.openbravo.authentication;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,11 +24,14 @@ import org.apache.logging.log4j.Logger;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.hibernate.criterion.Restrictions;
+import org.openbravo.base.provider.OBProvider;
 import org.openbravo.dal.core.OBContext;
 import org.openbravo.dal.service.OBDal;
+import org.openbravo.email.EmailEventManager;
 import org.openbravo.email.EmailUtils;
 import org.openbravo.mobile.core.process.WebServiceAbstractServlet;
 import org.openbravo.model.ad.access.User;
+import org.openbravo.model.ad.access.UserPasswordResetToken;
 import org.openbravo.model.ad.system.Client;
 import org.openbravo.model.common.enterprise.EmailServerConfiguration;
 import org.openbravo.model.common.enterprise.Organization;
@@ -38,6 +44,9 @@ import org.openbravo.model.common.enterprise.Organization;
 public class ForgotPasswordService extends WebServiceAbstractServlet {
   private static final Logger log = LogManager.getLogger();
 
+  @Inject
+  private EmailEventManager emailManager;
+
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
@@ -47,16 +56,20 @@ public class ForgotPasswordService extends WebServiceAbstractServlet {
       JSONObject body = new JSONObject(
           request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
 
-      String strOrgId = body.getString("org");
+      String strAppName = body.optString("appName");
+      if (strAppName.isEmpty()) {
+        throw new Exception("The request has not been done in the scope of POS2");
+      }
+
+      String strOrgId = body.optString("organization");
+      if (strOrgId.isEmpty()) {
+        throw new Exception("The request has not an organization defined");
+      }
+
+      String strClientId = body.optString("client");
+
       Organization org = OBDal.getInstance().get(Organization.class, strOrgId);
-      String strClientId = body.getString("client");
       Client client = OBDal.getInstance().get(Client.class, strClientId);
-
-      Boolean resetPasswordEnabled = client.getClientInformationList()
-          .get(0)
-          .isEnableResetPasswordByEmail();
-
-      result.put("resetPasswordEnabled", resetPasswordEnabled);
 
       EmailServerConfiguration emailConfig = EmailUtils.getEmailConfiguration(org, client);
       if (emailConfig == null) {
@@ -65,28 +78,64 @@ public class ForgotPasswordService extends WebServiceAbstractServlet {
           result.put("hasEmailConfiguration", false);
         }
       }
+
       if (emailConfig != null) {
         result.put("hasEmailConfiguration", true);
         String userOrEmail = body.getString("userOrEmail");
-        User user = (User) OBDal.getInstance()
+        List<User> users = OBDal.getInstance()
             .createCriteria(User.class)
             .add(Restrictions.or(Restrictions.eq(User.PROPERTY_USERNAME, userOrEmail),
                 Restrictions.eq(User.PROPERTY_EMAIL, userOrEmail)))
             .setFilterOnActive(true)
             .setFilterOnReadableClients(false)
             .setFilterOnReadableOrganization(false)
-            .uniqueResult();
-        if (user != null) {
-          String email = user.getEmail();
+            .list();
+
+        if (users == null) {
+          return;
         }
+        if (users.size() > 1) {
+          // More than one user has the same email, so in this case no email has to be sent
+          throw new Exception("More than one user has the same email configured");
+        }
+
+        boolean userCompliesWithRules = checkUser(users.get(0));
+        if (!userCompliesWithRules) {
+          throw new Exception("The selected user does not comply with the expected rules");
+        }
+
+        User user = users.get(0);
+        generateAndPersistToken(user, client, org);
+        // JSONObject test = new JSONObject();
+        // test.put("a", "aaaaaaaaaa");
+        // emailManager.sendEmail("passwordRequest", user.getEmail(), test);
+
       }
     } catch (JSONException ex) {
+      log.error("Error parsing JSON", ex);
+      result = new JSONObject(Map.of("error", ex.getMessage()));
+    } catch (Exception ex) {
       log.error("Error with forgot password service", ex);
       result = new JSONObject(Map.of("error", ex.getMessage()));
     } finally {
       OBContext.restorePreviousMode();
     }
     writeResult(response, new JSONObject(Map.of("response", result)).toString());
+  }
+
+  private void generateAndPersistToken(User user, Client client, Organization org) {
+    String token = UUID.randomUUID().toString();
+    UserPasswordResetToken resetToken = OBProvider.getInstance().get(UserPasswordResetToken.class);
+    resetToken.setClient(client);
+    resetToken.setOrganization(org);
+    resetToken.setUsertoken(token);
+    resetToken.setUser(user);
+
+    OBDal.getInstance().save(resetToken);
+  }
+
+  private boolean checkUser(User user) {
+    return user.isActive() && !user.getEmail().isEmpty() && !user.isLocked() && !user.isSsoonly();
   }
 
 }
