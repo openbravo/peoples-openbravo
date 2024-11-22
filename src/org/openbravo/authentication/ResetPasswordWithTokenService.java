4230b3e6bf7617d6daa8f269f753fe36cb1b24da
Ignacio De Andrés <i.deandres@orisha.com>
2024-11-22 09:15:16 +0100

    [Forgot password] Added new ResetPasswordWithTokenService

---
+++ src/org/openbravo/authentication/ResetPasswordWithTokenService.java
@@ -0,0 +1,43 @@
+package org.openbravo.authentication;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.mobile.core.process.WebServiceAbstractServlet;
+
+@SuppressWarnings("serial")
+public class ResetPasswordWithTokenService extends WebServiceAbstractServlet {
+  private static final Logger log = LogManager.getLogger();
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    JSONObject result = new JSONObject();
+    try {
+      OBContext.setAdminMode(true);
+      JSONObject body = new JSONObject(
+          request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
+      String token = body.optString("token");
+      String newPwd = body.optString("newPassword");
+      result.put("token", token);
+      result.put("newPwd", newPwd);
+
+    } catch (JSONException ex) {
+      log.error("Error parsing JSON", ex);
+      result = new JSONObject(Map.of("error", ex.getMessage()));
+    } finally {
+      OBContext.restorePreviousMode();
+      writeResult(response, new JSONObject(Map.of("response", result)).toString());
+    }
+  }
+}
