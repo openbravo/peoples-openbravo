e6587c8dd143e8d2d31857c945b68738c44c762a
Ander Flores <a.flores@orisha.com>
2024-11-22 12:43:54 +0100

    [Forgot password] Fixed copyright message

--- src/org/openbravo/authentication/ResetPasswordWithTokenService.java
+++ src/org/openbravo/authentication/ResetPasswordWithTokenService.java
@@ -1,3 +1,11 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2024 Openbravo S.L.U.
+ * Licensed under the Openbravo Commercial License version 1.0
+ * You may obtain a copy of the License at https://www.openbravo.com/legal/obcl.html
+ * or in the legal folder of this module distribution.
+ ************************************************************************************
+ */
 package org.openbravo.authentication;
 
 import java.io.IOException;
